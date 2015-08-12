53cadc7a5bb794715bd39a17237de2fa96ace37d
Keith Bostic <keith@wiredtiger.com>
2015-08-12 11:26:36 -0400

    Flip function order, remove the need for the function prototypes.

--- src/btree/bt_stat.c
+++ src/btree/bt_stat.c
@@ -8,98 +8,6 @@
 
 #include "wt_internal.h"
 
-static int  __stat_page(WT_SESSION_IMPL *, WT_PAGE *, WT_DSRC_STATS *);
-static void __stat_page_col_var(WT_SESSION_IMPL *, WT_PAGE *, WT_DSRC_STATS *);
-static void __stat_page_row_int(WT_SESSION_IMPL *, WT_PAGE *, WT_DSRC_STATS *);
-static void __stat_page_row_leaf(WT_SESSION_IMPL *, WT_PAGE *, WT_DSRC_STATS *);
-
-/*
- * __wt_btree_stat_init --
- *	Initialize the Btree statistics.
- */
-int
-__wt_btree_stat_init(WT_SESSION_IMPL *session, WT_CURSOR_STAT *cst)
-{
-	WT_BM *bm;
-	WT_BTREE *btree;
-	WT_DECL_RET;
-	WT_DSRC_STATS *stats;
-	WT_REF *next_walk;
-
-	btree = S2BT(session);
-	bm = btree->bm;
-	stats = &btree->dhandle->stats;
-
-	WT_RET(bm->stat(bm, session, stats));
-
-	WT_STAT_SET(session, stats, btree_fixed_len, btree->bitcnt);
-	WT_STAT_SET(session, stats, btree_maximum_depth, btree->maximum_depth);
-	WT_STAT_SET(session, stats, btree_maxintlpage, btree->maxintlpage);
-	WT_STAT_SET(session, stats, btree_maxintlkey, btree->maxintlkey);
-	WT_STAT_SET(session, stats, btree_maxleafpage, btree->maxleafpage);
-	WT_STAT_SET(session, stats, btree_maxleafkey, btree->maxleafkey);
-	WT_STAT_SET(session, stats, btree_maxleafvalue, btree->maxleafvalue);
-
-	/* Everything else is really, really expensive. */
-	if (!F_ISSET(cst, WT_CONN_STAT_ALL))
-		return (0);
-
-	/*
-	 * Clear the statistics we're about to count.
-	 */
-	WT_STAT_SET(session, stats, btree_column_deleted, 0);
-	WT_STAT_SET(session, stats, btree_column_fix, 0);
-	WT_STAT_SET(session, stats, btree_column_internal, 0);
-	WT_STAT_SET(session, stats, btree_column_variable, 0);
-	WT_STAT_SET(session, stats, btree_entries, 0);
-	WT_STAT_SET(session, stats, btree_overflow, 0);
-	WT_STAT_SET(session, stats, btree_row_internal, 0);
-	WT_STAT_SET(session, stats, btree_row_leaf, 0);
-
-	next_walk = NULL;
-	while ((ret = __wt_tree_walk(session, &next_walk, NULL, 0)) == 0 &&
-	    next_walk != NULL) {
-		WT_WITH_PAGE_INDEX(session,
-		    ret = __stat_page(session, next_walk->page, stats));
-		WT_RET(ret);
-	}
-	return (ret == WT_NOTFOUND ? 0 : ret);
-}
-
-/*
- * __stat_page --
- *	Stat any Btree page.
- */
-static int
-__stat_page(WT_SESSION_IMPL *session, WT_PAGE *page, WT_DSRC_STATS *stats)
-{
-	/*
-	 * All internal pages and overflow pages are trivial, all we track is
-	 * a count of the page type.
-	 */
-	switch (page->type) {
-	case WT_PAGE_COL_FIX:
-		WT_STAT_INCR(session, stats, btree_column_fix);
-		WT_STAT_INCRV(
-		    session, stats, btree_entries, page->pg_fix_entries);
-		break;
-	case WT_PAGE_COL_INT:
-		WT_STAT_INCR(session, stats, btree_column_internal);
-		break;
-	case WT_PAGE_COL_VAR:
-		__stat_page_col_var(session, page, stats);
-		break;
-	case WT_PAGE_ROW_INT:
-		__stat_page_row_int(session, page, stats);
-		break;
-	case WT_PAGE_ROW_LEAF:
-		__stat_page_row_leaf(session, page, stats);
-		break;
-	WT_ILLEGAL_VALUE(session);
-	}
-	return (0);
-}
-
 /*
  * __stat_page_col_var --
  *	Stat a WT_PAGE_COL_VAR page.
@@ -271,4 +179,91 @@ __stat_page_row_leaf(
 
 	WT_STAT_INCRV(session, stats, btree_entries, entry_cnt);
 	WT_STAT_INCRV(session, stats, btree_overflow, ovfl_cnt);
+}
+
+/*
+ * __stat_page --
+ *	Stat any Btree page.
+ */
+static int
+__stat_page(WT_SESSION_IMPL *session, WT_PAGE *page, WT_DSRC_STATS *stats)
+{
+	/*
+	 * All internal pages and overflow pages are trivial, all we track is
+	 * a count of the page type.
+	 */
+	switch (page->type) {
+	case WT_PAGE_COL_FIX:
+		WT_STAT_INCR(session, stats, btree_column_fix);
+		WT_STAT_INCRV(
+		    session, stats, btree_entries, page->pg_fix_entries);
+		break;
+	case WT_PAGE_COL_INT:
+		WT_STAT_INCR(session, stats, btree_column_internal);
+		break;
+	case WT_PAGE_COL_VAR:
+		__stat_page_col_var(session, page, stats);
+		break;
+	case WT_PAGE_ROW_INT:
+		__stat_page_row_int(session, page, stats);
+		break;
+	case WT_PAGE_ROW_LEAF:
+		__stat_page_row_leaf(session, page, stats);
+		break;
+	WT_ILLEGAL_VALUE(session);
+	}
+	return (0);
+}
+
+/*
+ * __wt_btree_stat_init --
+ *	Initialize the Btree statistics.
+ */
+int
+__wt_btree_stat_init(WT_SESSION_IMPL *session, WT_CURSOR_STAT *cst)
+{
+	WT_BM *bm;
+	WT_BTREE *btree;
+	WT_DECL_RET;
+	WT_DSRC_STATS *stats;
+	WT_REF *next_walk;
+
+	btree = S2BT(session);
+	bm = btree->bm;
+	stats = &btree->dhandle->stats;
+
+	WT_RET(bm->stat(bm, session, stats));
+
+	WT_STAT_SET(session, stats, btree_fixed_len, btree->bitcnt);
+	WT_STAT_SET(session, stats, btree_maximum_depth, btree->maximum_depth);
+	WT_STAT_SET(session, stats, btree_maxintlpage, btree->maxintlpage);
+	WT_STAT_SET(session, stats, btree_maxintlkey, btree->maxintlkey);
+	WT_STAT_SET(session, stats, btree_maxleafpage, btree->maxleafpage);
+	WT_STAT_SET(session, stats, btree_maxleafkey, btree->maxleafkey);
+	WT_STAT_SET(session, stats, btree_maxleafvalue, btree->maxleafvalue);
+
+	/* Everything else is really, really expensive. */
+	if (!F_ISSET(cst, WT_CONN_STAT_ALL))
+		return (0);
+
+	/*
+	 * Clear the statistics we're about to count.
+	 */
+	WT_STAT_SET(session, stats, btree_column_deleted, 0);
+	WT_STAT_SET(session, stats, btree_column_fix, 0);
+	WT_STAT_SET(session, stats, btree_column_internal, 0);
+	WT_STAT_SET(session, stats, btree_column_variable, 0);
+	WT_STAT_SET(session, stats, btree_entries, 0);
+	WT_STAT_SET(session, stats, btree_overflow, 0);
+	WT_STAT_SET(session, stats, btree_row_internal, 0);
+	WT_STAT_SET(session, stats, btree_row_leaf, 0);
+
+	next_walk = NULL;
+	while ((ret = __wt_tree_walk(session, &next_walk, NULL, 0)) == 0 &&
+	    next_walk != NULL) {
+		WT_WITH_PAGE_INDEX(session,
+		    ret = __stat_page(session, next_walk->page, stats));
+		WT_RET(ret);
+	}
+	return (ret == WT_NOTFOUND ? 0 : ret);
 }
