wiredtiger/wiredtiger

commit 79823db674d0e4aea37e06ee9bb0ede23d61845d

WT-2182: A different approach to the problem addressed in 846125e; instead of setting the empty WT_REF we're currently evicting from WT_REF_LOCKED to WT_REF_DELETED after __split_parent fails, set it before we call __split_parent, that way there's a lot less cleanup and __split_parent itself is simpler.

src/btree/bt_split.c
@@ -702,14 +702,14 @@ __split_parent(WT_SESSION_IMPL *session, WT_REF *ref, WT_REF **ref_new,
 	uint64_t split_gen;
 	uint32_t i, j;
 	uint32_t deleted_entries, parent_entries, result_entries;
-	bool complete, empty_parent;
+	bool complete;
 
 	parent = ref->home;
 
 	alloc_index = pindex = NULL;
 	parent_decr = 0;
 	parent_entries = 0;
-	complete = empty_parent = false;
+	complete = false;
 
 	/* The parent page will be marked dirty, make sure that will succeed. */
 	WT_RET(__wt_page_modify_init(session, parent));
@@ -722,10 +722,10 @@ __split_parent(WT_SESSION_IMPL *session, WT_REF *ref, WT_REF **ref_new,
 	parent_entries = pindex->entries;
 
 	/*
-	 * Remove any refs to deleted pages while we are splitting, we have
-	 * the internal page locked down, and are copying the refs into a new
-	 * array anyway.  Switch them to the special split state, so that any
-	 * reading thread will restart.
+	 * Remove any refs to deleted pages while we are splitting, we have the
+	 * internal page locked down, and are copying the refs into a new array
+	 * anyway. Switch them to the special split state, so that any reading
+	 * thread will restart.
 	 */
 	for (deleted_entries = 0, i = 0; i < parent_entries; ++i) {
 		next_ref = pindex->index[i];
@@ -739,18 +739,16 @@ __split_parent(WT_SESSION_IMPL *session, WT_REF *ref, WT_REF **ref_new,
 
 	/*
 	 * The final entry count consists of the original count, plus any new
-	 * pages, less any WT_REFs we're removing (deleted entries plus the
-	 * entry we're replacing).
+	 * pages, less any WT_REFs we're removing.
 	 */
-	result_entries = (parent_entries + new_entries) - (deleted_entries + 1);
+	result_entries = (parent_entries + new_entries) - deleted_entries;
 
 	/*
 	 * If there are no remaining entries on the parent, give up, we can't
 	 * leave an empty internal page. Mark it to be evicted soon and clean
-	 * up any references that have changed state.
+	 * up the references that have changed state.
 	 */
 	if (result_entries == 0) {
-		empty_parent = true;
 		__wt_page_evict_soon(parent);
 		goto err;
 	}
@@ -919,14 +917,6 @@ err:	/*
 		}
 
 		__wt_free_ref_index(session, NULL, alloc_index, false);
-
-		/*
-		 * The split couldn't proceed because the parent would be empty,
-		 * return EBUSY so our caller knows to unlock the WT_REF that's
-		 * being deleted, but don't be noisy, there's nothing wrong.
-		 */
-		if (empty_parent)
-			return (EBUSY);
 	}
 
 	if (ret != 0 && ret != WT_PANIC)

src/evict/evict_page.c
@@ -151,13 +151,14 @@ done:	if (((inmem_split && ret == 0) || (forced_eviction && ret == EBUSY)) &&
 static int
 __evict_delete_ref(WT_SESSION_IMPL *session, WT_REF *ref, bool closing)
 {
-	WT_DECL_RET;
 	WT_PAGE *parent;
 	WT_PAGE_INDEX *pindex;
 	uint32_t ndeleted;
 
 	if (__wt_ref_is_root(ref))
 		return (0);
+
+	WT_PUBLISH(ref->state, WT_REF_DELETED);
 
 	/*
 	 * Avoid doing reverse splits when closing the file, it is
@@ -179,20 +180,9 @@ __evict_delete_ref(WT_SESSION_IMPL *session, WT_REF *ref, bool closing)
 		 * something is busy, be sure that the page still ends up
 		 * marked deleted.
 		 */
-		if (ndeleted > pindex->entries / 10 && pindex->entries > 1) {
-			if ((ret = __wt_split_reverse(session, ref)) == 0)
-				return (0);
-			WT_RET_BUSY_OK(ret);
-
-			/*
-			 * The child must be locked after a failed reverse
-			 * split.
-			 */
-			WT_ASSERT(session, ref->state == WT_REF_LOCKED);
-		}
+		if (ndeleted > pindex->entries / 10 && pindex->entries > 1)
+			WT_RET(__wt_split_reverse(session, ref));
 	}
-
-	WT_PUBLISH(ref->state, WT_REF_DELETED);
 	return (0);
 }
 
@@ -222,7 +212,7 @@ __wt_evict_page_clean_update(
 	if (ref->addr == NULL) {
 		WT_WITH_PAGE_INDEX(session,
 		    ret = __evict_delete_ref(session, ref, closing));
-		WT_RET_BUSY_OK(ret);
+		WT_RET(ret);
 	} else
 		WT_PUBLISH(ref->state, WT_REF_DISK);
 
@@ -268,7 +258,7 @@ __evict_page_dirty_update(WT_SESSION_IMPL *session, WT_REF *ref, bool closing)
 		ref->addr = NULL;
 		WT_WITH_PAGE_INDEX(session,
 		    ret = __evict_delete_ref(session, ref, closing));
-		WT_RET_BUSY_OK(ret);
+		WT_RET(ret);
 		break;
 	case WT_PM_REC_MULTIBLOCK:			/* Multiple blocks */
 		/*

commit fc5a48cbf3c019bffd7ca9aa00784bcb9ec9d5f6

WT-2182: fix the result_entries calculation, I broke it in 79823db.

src/btree/bt_split.c
@@ -738,10 +738,11 @@ __split_parent(WT_SESSION_IMPL *session, WT_REF *ref, WT_REF **ref_new,
 	}
 
 	/*
-	 * The final entry count consists of the original count, plus any new
-	 * pages, less any WT_REFs we're removing.
+	 * The final entry count consists of the original count plus any new
+	 * pages, less any deleted WT_REFs and the WT_REF being replaced.
 	 */
-	result_entries = (parent_entries + new_entries) - deleted_entries;
+	result_entries =
+	    (parent_entries + new_entries) - (deleted_entries + 1);
 
 	/*
 	 * If there are no remaining entries on the parent, give up, we can't