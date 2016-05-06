7ebae240396cc9d7c69e60f356f62930162bd835
David Hows <david.hows@mongodb.com>
2016-05-06 10:46:10 +1000

    WT-2624 - Add MSVC2013 snprintf #defines

--- examples/c/ex_file_system.c
+++ examples/c/ex_file_system.c
@@ -31,6 +31,12 @@
 #include <errno.h>
 #include <stdlib.h>
 #include <string.h>
+#ifndef _WIN32
+#include <unistd.h>
+#else
+/* snprintf is not supported on <= VS2013 */
+#define	snprintf _snprintf
+#endif
 
 #include <wiredtiger.h>
 #include "queue_example.h"
