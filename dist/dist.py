e91bd77f3e3e4dd1344a93dbdaf8d36457aa46b8
Keith Bostic <keith.bostic@mongodb.com>
2016-05-30 16:05:51 -0400

    Add support for conditional file inclusion by adding a second argument on lines in dist/filelist.  Add a new automake conditional, POWERPC_HOST, set for the various PPC host CPUs.  Change dist/filelist to only compile the checksum/power8 files if POWERPC_HOST is set.
    Change makemake to use the second argument on lines in dist/filelist as
    a conditional, to optionally build the file.
    
    Change the Python source_files() function to only return the first
    whitespace separated token from any list of source files it reads
    (that's currently only needed by dist/filelist, but future change may
    need conditional compilation elsewhere).
    
    Remove the source_files() function "skip_includes" argument (it's not
    used anywhere and the path matching string is wrong).
    
    Update the sed scripts in s_define, s_funcs, s_prototypes, s_stat,
    s_typedef and s_win sed commands to ignore everything but the first
    token on the filelist line.
    
    Don't bother copying anything other than file names into the Windows
    build file.

--- dist/dist.py
+++ dist/dist.py
@@ -2,21 +2,18 @@ import filecmp, glob, os, re, shutil
 
 # source_files --
 #    Return a list of the WiredTiger source file names.
-def source_files(skip_includes=False):
-    if not skip_includes:
-        for line in glob.iglob('../src/include/*.[hi]'):
-            yield line
+def source_files():
     file_re = re.compile(r'^\w')
     for line in open('filelist', 'r'):
         if file_re.match(line):
-            yield os.path.join('..', line.rstrip())
+            yield os.path.join('..', line.split(None, 1)[0])
     # Return only the Windows-specific files in the Windows filelist
     for line in open('../build_win/filelist.win', 'r'):
         if 'os_win' in line and file_re.match(line):
-            yield os.path.join('..', line.rstrip())
+            yield os.path.join('..', line.split(None, 1)[0])
     for line in open('extlist', 'r'):
         if file_re.match(line):
-            yield os.path.join('..', line.rstrip())
+            yield os.path.join('..', line.split(None, 1)[0])
 
 # source_dirs --
 #    Return a list of the WiredTiger source directory names.
