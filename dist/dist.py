d1cbb5c2b6767976b69eaf06acd0e34aab2eae8c
Keith Bostic <keith.bostic@mongodb.com>
2016-06-01 15:41:49 -0400

    Merge the Windows and POSIX file lists, use POSIX_HOST and WINDOWS_HOST to mark source files needed for builds on those systems.

--- dist/dist.py
+++ dist/dist.py
@@ -9,10 +9,6 @@ def source_files():
     for line in open('filelist', 'r'):
         if file_re.match(line):
             yield os.path.join('..', line.split()[0])
-    # Return only the Windows-specific files in the Windows filelist
-    for line in open('../build_win/filelist.win', 'r'):
-        if 'os_win' in line and file_re.match(line):
-            yield os.path.join('..', line.split()[0])
     for line in open('extlist', 'r'):
         if file_re.match(line):
             yield os.path.join('..', line.split()[0])
