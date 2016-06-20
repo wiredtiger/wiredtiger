3eb8a062adaa5b359bfe831d8b0786490e4ceab4
Michael Cahill <michael.cahill@mongodb.com>
2016-06-20 16:39:44 +1000

    Clear the eviction walk if we don't find any candidates.
    Otherwise, we are keeping a page pinned in what might be an idle file,
    and tying up a hazard pointer that could prevent eviction from an active
    file (since the eviction server tracks how many hazard pointers it is
    using to avoid going over the limit).

--- src/evict/evict_lru.c
+++ src/evict/evict_lru.c
@@ -1423,11 +1423,15 @@ fast:		/* If the page can't be evicted, give up. */
 	 * If we happen to end up on the root page, clear it.  We have to track
 	 * hazard pointers, and the root page complicates that calculation.
 	 *
+	 * Likewise if we found no new candidates during the walk: there is no
+	 * point keeping a page pinned, since it may be the only candiate in an
+	 * idle tree.
+	 *
 	 * If we land on a page requiring forced eviction, move on to the next
 	 * page: we want this page evicted as quickly as possible.
 	 */
 	if ((ref = btree->evict_ref) != NULL) {
-		if (__wt_ref_is_root(ref))
+		if (__wt_ref_is_root(ref) || evict == start)
 			WT_RET(__evict_clear_walk(session));
 		else if (ref->page->read_gen == WT_READGEN_OLDEST)
 			WT_RET_NOTFOUND_OK(__wt_tree_walk_count(
