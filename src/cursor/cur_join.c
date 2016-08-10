3e48610ddc48a34abc9d4ad64962cc05d83a45cf
Don Anderson <dda@mongodb.com>
2016-08-10 10:21:57 -0400

    WT-2323 Allocate a transaction id at the beginning of join iteration.

--- src/cursor/cur_join.c
+++ src/cursor/cur_join.c
@@ -918,6 +918,10 @@ __curjoin_init_next(WT_SESSION_IMPL *session, WT_CURSOR_JOIN *cjoin,
 		    "join cursor has not yet been joined with any other "
 		    "cursors");
 
+	/* Allocate a transaction beforehand to guarantee consistent results. */
+	if (F_ISSET(&session->txn, WT_TXN_RUNNING))
+		WT_RET(__wt_txn_id_check(session));
+
 	if (F_ISSET((WT_CURSOR *)cjoin, WT_CURSTD_RAW))
 		config = &raw_cfg[0];
 	else
