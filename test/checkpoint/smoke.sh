f351648c9bf735a0ffe876c7c281f7b0e25d9aec
David Hows <david.hows@mongodb.com>
2016-09-27 16:04:49 +1000

    Remove further tests in cases of long runs

--- test/checkpoint/smoke.sh
+++ test/checkpoint/smoke.sh
@@ -6,17 +6,21 @@ set -e
 echo "checkpoint: 3 mixed tables"
 $TEST_WRAPPER ./t -T 3 -t m
 
-echo "checkpoint: 6 column-store tables"
-$TEST_WRAPPER ./t -T 6 -t c
+# If we are disabling long tests then we want only one checkpoint run
+if [ -z ${TESTUTIL_DISABLE_LONG_TESTS+x} ]; then
 
-echo "checkpoint: 6 LSM tables"
-$TEST_WRAPPER ./t -T 6 -t l
+	echo "checkpoint: 6 column-store tables"
+	$TEST_WRAPPER ./t -T 6 -t c
 
-echo "checkpoint: 6 mixed tables"
-$TEST_WRAPPER ./t -T 6 -t m
+	echo "checkpoint: 6 LSM tables"
+	$TEST_WRAPPER ./t -T 6 -t l
 
-echo "checkpoint: 6 row-store tables"
-$TEST_WRAPPER ./t -T 6 -t r
+	echo "checkpoint: 6 mixed tables"
+	$TEST_WRAPPER ./t -T 6 -t m
 
-echo "checkpoint: 6 row-store tables, named checkpoint"
-$TEST_WRAPPER ./t -c 'TeSt' -T 6 -t r
+	echo "checkpoint: 6 row-store tables"
+	$TEST_WRAPPER ./t -T 6 -t r
+
+	echo "checkpoint: 6 row-store tables, named checkpoint"
+	$TEST_WRAPPER ./t -c 'TeSt' -T 6 -t r
+fi
