385a9e7104c2a9e0e7957202037b7fe2a79a45e8
Alex Gorrod <alexander.gorrod@mongodb.com>
2016-09-28 10:35:41 +1000

    Switch script to use a simpler test.

--- test/checkpoint/smoke.sh
+++ test/checkpoint/smoke.sh
@@ -6,21 +6,20 @@ set -e
 echo "checkpoint: 3 mixed tables"
 $TEST_WRAPPER ./t -T 3 -t m
 
-# If we are disabling long tests then we want only one checkpoint run
-if [ -z ${TESTUTIL_DISABLE_LONG_TESTS+"false"} ]; then
+# We are done if short tests are requested
+test -z "$TESTUTIL_DISABLE_LONG_TESTS" || exit 0
 
-	echo "checkpoint: 6 column-store tables"
-	$TEST_WRAPPER ./t -T 6 -t c
+echo "checkpoint: 6 column-store tables"
+$TEST_WRAPPER ./t -T 6 -t c
 
-	echo "checkpoint: 6 LSM tables"
-	$TEST_WRAPPER ./t -T 6 -t l
+echo "checkpoint: 6 LSM tables"
+$TEST_WRAPPER ./t -T 6 -t l
 
-	echo "checkpoint: 6 mixed tables"
-	$TEST_WRAPPER ./t -T 6 -t m
+echo "checkpoint: 6 mixed tables"
+$TEST_WRAPPER ./t -T 6 -t m
 
-	echo "checkpoint: 6 row-store tables"
-	$TEST_WRAPPER ./t -T 6 -t r
+echo "checkpoint: 6 row-store tables"
+$TEST_WRAPPER ./t -T 6 -t r
 
-	echo "checkpoint: 6 row-store tables, named checkpoint"
-	$TEST_WRAPPER ./t -c 'TeSt' -T 6 -t r
-fi
+echo "checkpoint: 6 row-store tables, named checkpoint"
+$TEST_WRAPPER ./t -c 'TeSt' -T 6 -t r
