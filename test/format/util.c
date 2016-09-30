f06d8ccc782d1afc5990563f49cdf63398bb9590
Keith Bostic <keith.bostic@mongodb.com>
2016-09-30 08:35:13 -0400

    whitespace

--- test/format/util.c
+++ test/format/util.c
@@ -133,7 +133,7 @@ key_gen_insert(WT_RAND_STATE *rnd, WT_ITEM *key, uint64_t keyno)
 {
 	static const char * const suffix[15] = {
 	    "01", "02", "03", "04", "05",
-	    "06", "07", "08", "09", "10", 
+	    "06", "07", "08", "09", "10",
 	    "11", "12", "13", "14", "15"
 	};
 
