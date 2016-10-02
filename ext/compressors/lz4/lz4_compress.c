44dda8728c822398911e85853a3f1403bacf43f7
Keith Bostic <keith.bostic@mongodb.com>
2016-10-02 14:30:00 -0400

    WT-2880 Investigate using zstandard and zlib-compatible wrapper API as alternative to zlib for collection compression
    A bug found by inspection in LZ4 code: we're going to offset the
    destination buffer by sizeof(LZ4_PREFIX), so we need to offset the
    destination buffer length by the same amount.

--- ext/compressors/lz4/lz4_compress.c
+++ ext/compressors/lz4/lz4_compress.c
@@ -277,8 +277,9 @@ lz4_compress_raw(WT_COMPRESSOR *compressor, WT_SESSION *session,
 	(void)final;
 
 	sourceSize = (int)offsets[slots];		/* Type conversion */
-	targetDestSize =
-	    (int)((dst_len < page_max ? dst_len : page_max) - extra);
+	targetDestSize = (int)
+	    (((dst_len - sizeof(LZ4_PREFIX)) < page_max ?
+	    (dst_len - sizeof(LZ4_PREFIX)) : page_max) - extra);
 
 	/* Compress, starting after the prefix bytes. */
 	lz4_len = LZ4_compress_destSize((const char *)src,
