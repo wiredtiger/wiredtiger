06aa0acbfe12909274773d49a6ed62a3352b74c1
Keith Bostic <keith.bostic@mongodb.com>
2016-10-04 10:06:35 -0400

    WT-2948 simplify error handling by making epoch time return never fail

--- src/os_posix/os_time.c
+++ src/os_posix/os_time.c
@@ -12,7 +12,7 @@
  * __wt_epoch --
  *	Return the time since the Epoch.
  */
-int
+void
 __wt_epoch(WT_SESSION_IMPL *session, struct timespec *tsp)
 {
 	WT_DECL_RET;
@@ -20,8 +20,8 @@ __wt_epoch(WT_SESSION_IMPL *session, struct timespec *tsp)
 #if defined(HAVE_CLOCK_GETTIME)
 	WT_SYSCALL(clock_gettime(CLOCK_REALTIME, tsp), ret);
 	if (ret == 0)
-		return (0);
-	WT_RET_MSG(session, ret, "clock_gettime");
+		return;
+	WT_PANIC_MSG(session, ret, "clock_gettime");
 #elif defined(HAVE_GETTIMEOFDAY)
 	struct timeval v;
 
@@ -29,9 +29,9 @@ __wt_epoch(WT_SESSION_IMPL *session, struct timespec *tsp)
 	if (ret == 0) {
 		tsp->tv_sec = v.tv_sec;
 		tsp->tv_nsec = v.tv_usec * WT_THOUSAND;
-		return (0);
+		return;
 	}
-	WT_RET_MSG(session, ret, "gettimeofday");
+	WT_PANIC_MSG(session, ret, "gettimeofday");
 #else
 	NO TIME-OF-DAY IMPLEMENTATION: see src/os_posix/os_time.c
 #endif
