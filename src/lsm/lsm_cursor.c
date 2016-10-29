0f06c0ec8b0956daf14bbc07f26f07ae21a16371
Keith Bostic <keith.bostic@mongodb.com>
2016-10-29 14:37:16 -0400

    Associate a verbose error message with EINVAL returned from __wt_clsm_open() encountering an unexpected URI.

--- src/lsm/lsm_cursor.c
+++ src/lsm/lsm_cursor.c
@@ -1678,7 +1678,8 @@ __wt_clsm_open(WT_SESSION_IMPL *session,
 	lsm_tree = NULL;
 
 	if (!WT_PREFIX_MATCH(uri, "lsm:"))
-		return (EINVAL);
+		WT_RET_MSG(session, EINVAL,
+		    "uri prefix %s doesn't match expected \"lsm:\"", uri);
 
 	if (F_ISSET(S2C(session), WT_CONN_IN_MEMORY))
 		WT_RET_MSG(session, EINVAL,
