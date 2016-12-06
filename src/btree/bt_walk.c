80946209e5d99d5ebc629ebacab828292e51cbf6
Keith Bostic <keith.bostic@mongodb.com>
2016-12-06 07:35:12 -0500

    Remove an unnecessary ordered read.

--- src/btree/bt_walk.c
+++ src/btree/bt_walk.c
@@ -40,7 +40,7 @@ __ref_index_slot(WT_SESSION_IMPL *session,
 		 * just means the first retrieval (which sets the hint for
 		 * subsequent retrievals), is slower.
 		 */
-		WT_ORDERED_READ(slot, ref->pindex_hint);
+		slot = ref->pindex_hint;
 		if (slot >= entries)
 			slot = entries - 1;
 		if (pindex->index[slot] == ref)
