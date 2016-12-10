09f3e2359c831affc384dbc09a0d2da5b69e123d
Alexandra (Sasha)Fedorova <sasha@ece.ubc.ca>
2016-12-10 09:08:12 -0800

    Fix eviction thread ranges to make the csuite happy

--- dist/api_data.py
+++ dist/api_data.py
@@ -401,12 +401,12 @@ connection_runtime_config = [
                 pages from cache. The number of threads started will vary
                 depending on the current eviction load. Each eviction worker
                 thread uses a session from the configured session_max''',
-                min=1, max=8),
+                min=1, max=20),
             Config('threads_min', '1', r'''
                 minimum number of threads WiredTiger will start to help evict
                 pages from cache. The number of threads currently running will
                 vary depending on the current eviction load''',
-                min=1, max=8),
+                min=1, max=20),
             ]),
     Config('eviction_checkpoint_target', '5', r'''
         perform eviction at the beginning of checkpoints to bring the dirty
