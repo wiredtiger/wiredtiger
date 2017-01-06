b3dbf6805eea02ddd00718a0a9e053c8e9ce2a2c
David Hows <david.hows@mongodb.com>
2017-01-06 14:29:29 +1100

    Test fixes, typo fixes

--- src/utilities/util_truncate.c
+++ src/utilities/util_truncate.c
@@ -33,7 +33,8 @@ util_truncate(WT_SESSION *session, int argc, char *argv[])
 	if ((name = util_name(session, *argv, "table")) == NULL)
 		return (1);
 
-	ret = session->truncate(session, name, NULL, NULL, NULL);
+	if ((ret = session->truncate(session, name, NULL, NULL, NULL)) != 0)
+		return (util_err(session, ret, "%s: session.truncate", name));
 
 	free(name);
 	return (ret);
