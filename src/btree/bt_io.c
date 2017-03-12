072dcbf1b9afe67d5b3835144f1070076e7b8971
Keith Bostic <keith.bostic@mongodb.com>
2017-03-12 14:11:03 -0400

    warning: The right operand of '-' is a garbage value [clang-analyzer-core.UndefinedBinaryOperatorResult]

--- src/btree/bt_io.c
+++ src/btree/bt_io.c
@@ -184,6 +184,7 @@ __wt_bt_write(WT_SESSION_IMPL *session, WT_ITEM *buf,
 	int compression_failed;		/* Extension API, so not a bool. */
 	uint8_t *dst, *src;
 	bool data_checksum, encrypted;
+	bool timer;
 
 	btree = S2BT(session);
 	bm = btree->bm;
@@ -357,7 +358,8 @@ __wt_bt_write(WT_SESSION_IMPL *session, WT_ITEM *buf,
 		data_checksum = !compressed;
 		break;
 	}
-	if (!F_ISSET(session, WT_SESSION_INTERNAL))
+	timer = !F_ISSET(session, WT_SESSION_INTERNAL);
+	if (timer)
 		__wt_epoch(session, &start);
 
 	/* Call the block manager to write the block. */
@@ -367,7 +369,7 @@ __wt_bt_write(WT_SESSION_IMPL *session, WT_ITEM *buf,
 	    bm, session, ip, addr, addr_sizep, data_checksum, checkpoint_io));
 
 	/* Update some statistics now that the write is done */
-	if (!F_ISSET(session, WT_SESSION_INTERNAL)) {
+	if (timer) {
 		__wt_epoch(session, &stop);
 		WT_STAT_CONN_INCR(session, cache_write_app_count);
 		WT_STAT_CONN_INCRV(session, cache_write_app_time,
--- src/evict/evict_lru.c
+++ src/evict/evict_lru.c
@@ -2116,6 +2116,7 @@ __wt_cache_eviction_worker(WT_SESSION_IMPL *session, bool busy, u_int pct_full)
 	WT_TXN_GLOBAL *txn_global;
 	WT_TXN_STATE *txn_state;
 	uint64_t init_evict_count, max_pages_evicted;
+	bool timer;
 
 	conn = S2C(session);
 	cache = conn->cache;
@@ -2136,7 +2137,9 @@ __wt_cache_eviction_worker(WT_SESSION_IMPL *session, bool busy, u_int pct_full)
 	__wt_evict_server_wake(session);
 
 	/* Track how long application threads spend doing eviction. */
-	if (WT_STAT_ENABLED(session) && !F_ISSET(session, WT_SESSION_INTERNAL))
+	timer =
+	    WT_STAT_ENABLED(session) && !F_ISSET(session, WT_SESSION_INTERNAL);
+	if (timer)
 		__wt_epoch(session, &enter);
 
 	for (init_evict_count = cache->pages_evict;; ret = 0) {
@@ -2202,8 +2205,7 @@ __wt_cache_eviction_worker(WT_SESSION_IMPL *session, bool busy, u_int pct_full)
 		}
 	}
 
-err:	if (WT_STAT_ENABLED(session) &&
-	    !F_ISSET(session, WT_SESSION_INTERNAL)) {
+err:	if (timer) {
 		__wt_epoch(session, &leave);
 		WT_STAT_CONN_INCRV(session,
 		    application_cache_time, WT_TIMEDIFF_US(leave, enter));
