8f42fce1105b597d46f0864dbbc902fe89107451
Keith Bostic <keith.bostic@mongodb.com>
2017-03-12 14:25:44 -0400

    warning: Potential leak of memory pointed to by 'secretkey' [clang-analyzer-unix.Malloc]

--- src/utilities/util_main.c
+++ src/utilities/util_main.c
@@ -20,7 +20,43 @@ static const char *command;			/* Command name */
 #define	REC_LOGOFF	"log=(enabled=false)"
 #define	REC_RECOVER	"log=(recover=on)"
 
-static int usage(void);
+static void
+usage(void)
+{
+	fprintf(stderr,
+	    "WiredTiger Data Engine (version %d.%d)\n",
+	    WIREDTIGER_VERSION_MAJOR, WIREDTIGER_VERSION_MINOR);
+	fprintf(stderr,
+	    "global options:\n"
+	    "\t" "-C\t" "wiredtiger_open configuration\n"
+	    "\t" "-h\t" "database directory\n"
+	    "\t" "-L\t" "turn logging off for debug-mode\n"
+	    "\t" "-R\t" "run recovery if configured\n"
+	    "\t" "-V\t" "display library version and exit\n"
+	    "\t" "-v\t" "verbose\n");
+	fprintf(stderr,
+	    "commands:\n"
+	    "\t" "alter\t  alter an object\n"
+	    "\t" "backup\t  database backup\n"
+	    "\t" "compact\t  compact an object\n"
+	    "\t" "copyright copyright information\n"
+	    "\t" "create\t  create an object\n"
+	    "\t" "drop\t  drop an object\n"
+	    "\t" "dump\t  dump an object\n"
+	    "\t" "list\t  list database objects\n"
+	    "\t" "load\t  load an object\n"
+	    "\t" "loadtext  load an object from a text file\n"
+	    "\t" "printlog  display the database log\n"
+	    "\t" "read\t  read values from an object\n"
+	    "\t" "rebalance rebalance an object\n"
+	    "\t" "rename\t  rename an object\n"
+	    "\t" "salvage\t  salvage a file\n"
+	    "\t" "stat\t  display statistics for an object\n"
+	    "\t" "truncate  truncate an object, removing all content\n"
+	    "\t" "upgrade\t  upgrade an object\n"
+	    "\t" "verify\t  verify an object\n"
+	    "\t" "write\t  write values to an object\n");
+}
 
 int
 main(int argc, char *argv[])
@@ -73,6 +109,7 @@ main(int argc, char *argv[])
 			cmd_config = __wt_optarg;
 			break;
 		case 'E':			/* secret key */
+			free(secretkey);	/* lint: set more than once */
 			if ((secretkey = strdup(__wt_optarg)) == NULL) {
 				ret = util_err(NULL, errno, NULL);
 				goto err;
@@ -92,24 +129,27 @@ main(int argc, char *argv[])
 			break;
 		case 'V':			/* version */
 			printf("%s\n", wiredtiger_version(NULL, NULL, NULL));
-			return (EXIT_SUCCESS);
+			goto done;
 		case 'v':			/* verbose */
 			verbose = true;
 			break;
 		case '?':
 		default:
-			return (usage());
+			usage();
+			goto err;
 		}
 	if (logoff && recover) {
 		fprintf(stderr, "Only one of -L and -R is allowed.\n");
-		return (EXIT_FAILURE);
+		goto err;
 	}
 	argc -= __wt_optind;
 	argv += __wt_optind;
 
 	/* The next argument is the command name. */
-	if (argc < 1)
-		return (usage());
+	if (argc < 1) {
+		usage();
+		goto err;
+	}
 	command = argv[0];
 
 	/* Reset getopt. */
@@ -130,7 +170,7 @@ main(int argc, char *argv[])
 			func = util_compact;
 		else if (strcmp(command, "copyright") == 0) {
 			util_copyright();
-			return (EXIT_SUCCESS);
+			goto done;
 		} else if (strcmp(command, "create") == 0) {
 			func = util_create;
 			config = "create";
@@ -194,8 +234,10 @@ main(int argc, char *argv[])
 	default:
 		break;
 	}
-	if (func == NULL)
-		return (usage());
+	if (func == NULL) {
+		usage();
+		goto err;
+	}
 
 	/* Build the configuration string. */
 	len = 10;					/* some slop */
@@ -234,54 +276,19 @@ main(int argc, char *argv[])
 	/* Call the function. */
 	ret = func(session, argc, argv);
 
+	if (0) {
+err:		ret = EXIT_FAILURE;
+	}
+done:
+
 	/* Close the database. */
-err:	if (conn != NULL && (tret = conn->close(conn, NULL)) != 0 && ret == 0)
+	if (conn != NULL && (tret = conn->close(conn, NULL)) != 0 && ret == 0)
 		ret = tret;
 
 	free(p);
 	free(secretkey);
 
 	return (ret == 0 ? EXIT_SUCCESS : EXIT_FAILURE);
-}
-
-static int
-usage(void)
-{
-	fprintf(stderr,
-	    "WiredTiger Data Engine (version %d.%d)\n",
-	    WIREDTIGER_VERSION_MAJOR, WIREDTIGER_VERSION_MINOR);
-	fprintf(stderr,
-	    "global options:\n"
-	    "\t" "-C\t" "wiredtiger_open configuration\n"
-	    "\t" "-h\t" "database directory\n"
-	    "\t" "-L\t" "turn logging off for debug-mode\n"
-	    "\t" "-R\t" "run recovery if configured\n"
-	    "\t" "-V\t" "display library version and exit\n"
-	    "\t" "-v\t" "verbose\n");
-	fprintf(stderr,
-	    "commands:\n"
-	    "\t" "alter\t  alter an object\n"
-	    "\t" "backup\t  database backup\n"
-	    "\t" "compact\t  compact an object\n"
-	    "\t" "copyright copyright information\n"
-	    "\t" "create\t  create an object\n"
-	    "\t" "drop\t  drop an object\n"
-	    "\t" "dump\t  dump an object\n"
-	    "\t" "list\t  list database objects\n"
-	    "\t" "load\t  load an object\n"
-	    "\t" "loadtext  load an object from a text file\n"
-	    "\t" "printlog  display the database log\n"
-	    "\t" "read\t  read values from an object\n"
-	    "\t" "rebalance rebalance an object\n"
-	    "\t" "rename\t  rename an object\n"
-	    "\t" "salvage\t  salvage a file\n"
-	    "\t" "stat\t  display statistics for an object\n"
-	    "\t" "truncate  truncate an object, removing all content\n"
-	    "\t" "upgrade\t  upgrade an object\n"
-	    "\t" "verify\t  verify an object\n"
-	    "\t" "write\t  write values to an object\n");
-
-	return (EXIT_FAILURE);
 }
 
 /*
