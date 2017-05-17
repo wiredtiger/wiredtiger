86efa739fb22c6e741603a96ac77e09553ac931c
Michael Cahill <michael.cahill@mongodb.com>
2017-05-17 16:17:44 +1000

    Add timestamp testing to test/format.

--- test/format/ops.c
+++ test/format/ops.c
@@ -397,6 +397,42 @@ snap_check(WT_CURSOR *cursor,
 }
 
 /*
+ * commit_transaction --
+ *     Commit a transaction
+ */
+static void
+commit_transaction(TINFO *tinfo, WT_SESSION *session)
+{
+	WT_CONNECTION *conn;
+	char *commit_conf, config_buf[64];
+	uint64_t ts;
+
+	if (g.c_txn_timestamps) {
+		ts = __wt_atomic_addv64(&g.timestamp, 1);
+
+		/* Periodically bump the oldest timestamp. */
+		if (ts % 100 == 0) {
+			testutil_check(__wt_snprintf(
+			    config_buf, sizeof(config_buf),
+			    "oldest_timestamp=%x", ts));
+			conn = session->connection;
+			testutil_check(
+			    conn->set_timestamp(conn, config_buf));
+		}
+
+		testutil_check(__wt_snprintf(
+		    config_buf, sizeof(config_buf),
+		    "commit_timestamp=%x", ts));
+		commit_conf = config_buf;
+	} else
+		commit_conf = NULL;
+
+	testutil_check(
+	    session->commit_transaction(session, commit_conf));
+	++tinfo->commit;
+}
+
+/*
  * ops --
  *     Per-thread operations.
  */
@@ -456,9 +492,7 @@ ops(void *arg)
 		 */
 		if (intxn &&
 		    (tinfo->ops == ckpt_op || tinfo->ops == session_op)) {
-			testutil_check(
-			    session->commit_transaction(session, NULL));
-			++tinfo->commit;
+			commit_transaction(tinfo, session);
 			intxn = false;
 		}
 
@@ -808,9 +842,7 @@ update_instead_of_insert:
 		 */
 		switch (rnd) {
 		case 1: case 2: case 3: case 4:			/* 40% */
-			testutil_check(
-			    session->commit_transaction(session, NULL));
-			++tinfo->commit;
+			commit_transaction(tinfo, session);
 			break;
 		case 5:						/* 10% */
 			if (0) {
