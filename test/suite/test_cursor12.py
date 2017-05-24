361c6a9193b0449c919c05b583bcae20e5905c52
Keith Bostic <keith.bostic@mongodb.com>
2017-05-24 12:47:05 -0400

    Refactor the test, adding functions to build/check the modified values. Add a test with a large number of modify entries.

--- test/suite/test_cursor12.py
+++ test/suite/test_cursor12.py
@@ -132,26 +132,57 @@ class test_cursor12(wttest.WiredTigerTestCase):
     'o' : 'ABCDEFGH',           # overlap the end with incorrect size
     'f' : 'ABCDEFG01234567',
     'mods' : [['01234567', 7, 2000]]
+    },{                         # many updates
+    'o' : '-ABCDEFGHIJKLMNOPQRSTUVWXYZ-',
+    'f' : '-eeeeeeeeeeeeeeeeeeeeeeeeee-',
+    'mods' : [['a', 1,  1], ['a', 2,  1], ['a', 3,  1], ['a', 4,  1],
+              ['a', 5,  1], ['a', 6,  1], ['a', 7,  1], ['a', 8,  1],
+              ['a', 9,  1], ['a', 10, 1], ['a', 11, 1], ['a', 12, 1],
+              ['a', 13, 1], ['a', 14, 1], ['a', 15, 1], ['a', 16, 1],
+              ['a', 17, 1], ['a', 18, 1], ['a', 19, 1], ['a', 20, 1],
+              ['a', 21, 1], ['a', 22, 1], ['a', 23, 1], ['a', 24, 1],
+              ['a', 25, 1], ['a', 26, 1],
+              ['b', 1,  1], ['b', 2,  1], ['b', 3,  1], ['b', 4,  1],
+              ['b', 5,  1], ['b', 6,  1], ['b', 7,  1], ['b', 8,  1],
+              ['b', 9,  1], ['b', 10, 1], ['b', 11, 1], ['b', 12, 1],
+              ['b', 13, 1], ['b', 14, 1], ['b', 15, 1], ['b', 16, 1],
+              ['b', 17, 1], ['b', 18, 1], ['b', 19, 1], ['b', 20, 1],
+              ['b', 21, 1], ['b', 22, 1], ['b', 23, 1], ['b', 24, 1],
+              ['b', 25, 1], ['b', 26, 1],
+              ['c', 1,  1], ['c', 2,  1], ['c', 3,  1], ['c', 4,  1],
+              ['c', 5,  1], ['c', 6,  1], ['c', 7,  1], ['c', 8,  1],
+              ['c', 9,  1], ['c', 10, 1], ['c', 11, 1], ['c', 12, 1],
+              ['c', 13, 1], ['c', 14, 1], ['c', 15, 1], ['c', 16, 1],
+              ['c', 17, 1], ['c', 18, 1], ['c', 19, 1], ['c', 20, 1],
+              ['c', 21, 1], ['c', 22, 1], ['c', 23, 1], ['c', 24, 1],
+              ['c', 25, 1], ['c', 26, 1],
+              ['d', 1,  1], ['d', 2,  1], ['d', 3,  1], ['d', 4,  1],
+              ['d', 5,  1], ['d', 6,  1], ['d', 7,  1], ['d', 8,  1],
+              ['d', 9,  1], ['d', 10, 1], ['d', 11, 1], ['d', 12, 1],
+              ['d', 13, 1], ['d', 14, 1], ['d', 15, 1], ['d', 16, 1],
+              ['d', 17, 1], ['d', 18, 1], ['d', 19, 1], ['d', 20, 1],
+              ['d', 21, 1], ['d', 22, 1], ['d', 23, 1], ['d', 24, 1],
+              ['d', 25, 1], ['d', 26, 1],
+              ['e', 1,  1], ['e', 2,  1], ['e', 3,  1], ['e', 4,  1],
+              ['e', 5,  1], ['e', 6,  1], ['e', 7,  1], ['e', 8,  1],
+              ['e', 9,  1], ['e', 10, 1], ['e', 11, 1], ['e', 12, 1],
+              ['e', 13, 1], ['e', 14, 1], ['e', 15, 1], ['e', 16, 1],
+              ['e', 17, 1], ['e', 18, 1], ['e', 19, 1], ['e', 20, 1],
+              ['e', 21, 1], ['e', 22, 1], ['e', 23, 1], ['e', 24, 1],
+              ['e', 25, 1], ['e', 26, 1]]
     }
     ]
 
+    # Skip record number keys with LSM.
     def skip(self):
         return self.keyfmt == 'r' and 'lsm' in self.uri
 
-    # Smoke-test the modify API.
-    def test_modify_smoke(self):
-        if self.skip():
-            return
-
-        # Populate a database.
-        ds = SimpleDataSet(self,
-            self.uri, 100, key_format=self.keyfmt, value_format='u')
-        ds.populate()
-
+    # Create a set of modified records and verify in-memory reads.
+    def modify_load(self, ds, single):
         # For each test in the list:
         #       set the original value,
         #       apply modifications in order,
-        #       confirm the final state,
+        #       confirm the final state
         row = 10
         c = self.session.open_cursor(self.uri, None)
         for i in self.list:
@@ -172,65 +203,59 @@ class test_cursor12(wttest.WiredTigerTestCase):
             self.assertEquals(c.search(), 0)
             self.assertEquals(c.get_value(), i['f'])
 
-            row = row + 1
+            if not single:
+                row = row + 1
         c.close()
+
+    # Confirm the modified records are correct.
+    def modify_confirm(self, ds, single):
+        # For each test in the list:
+        #       confirm the final state is there.
+        row = 10
+        c = self.session.open_cursor(self.uri, None)
+        for i in self.list:
+            c.set_key(ds.key(row))
+            self.assertEquals(c.search(), 0)
+            self.assertEquals(c.get_value(), i['f'])
+
+            if not single:
+                row = row + 1
+        c.close()
+
+    # Smoke-test the modify API, operating on a group of records.
+    def test_modify_smoke(self):
+        if self.skip():
+            return
+
+        ds = SimpleDataSet(self,
+            self.uri, 100, key_format=self.keyfmt, value_format='u')
+        ds.populate()
+        self.modify_load(ds, False)
+
+    # Smoke-test the modify API, operating on a single record
+    def test_modify_smoke_single(self):
+        if self.skip():
+            return
+
+        ds = SimpleDataSet(self,
+            self.uri, 100, key_format=self.keyfmt, value_format='u')
+        ds.populate()
+        self.modify_load(ds, True)
 
     # Smoke-test the modify API, closing and re-opening the database.
     def test_modify_smoke_reopen(self):
         if self.skip():
             return
 
-        # Populate a database.
         ds = SimpleDataSet(self,
             self.uri, 100, key_format=self.keyfmt, value_format='u')
         ds.populate()
-
-        # For each test in the list:
-        #       set the original value,
-        #       apply modifications in order,
-        #       confirm the final state,
-        row = 10
-        c = self.session.open_cursor(self.uri, None)
-        for i in self.list:
-            c.set_key(ds.key(row))
-            c.set_value(i['o'])
-            self.assertEquals(c.update(), 0)
-            c.reset()
-
-            c.set_key(ds.key(row))
-            mods = []
-            for j in i['mods']:
-                mod = wiredtiger.Modify(j[0], j[1], j[2])
-                mods.append(mod)
-            self.assertEquals(c.modify(mods), 0)
-            c.reset()
-
-            c.set_key(ds.key(row))
-            self.assertEquals(c.search(), 0)
-            self.assertEquals(c.get_value(), i['f'])
-
-            row = row + 1
-        c.close()
+        self.modify_load(ds, False)
 
         # Flush to disk, forcing reconciliation.
-        #
-        # XXX KEITH
-        # I think this is a Python API problem, for some reason LSM can't
-        # reopen the connection if the final is an empty string.
-        if 'lsm' not in self.uri or i['f'] != '':
-            self.reopen_conn()
+        self.reopen_conn()
 
-        # For each test in the list:
-        #       confirm the final state
-        row = 10
-        c = self.session.open_cursor(self.uri, None)
-        for i in self.list:
-            c.set_key(ds.key(row))
-            self.assertEquals(c.search(), 0)
-            self.assertEquals(c.get_value(), i['f'])
-
-            row = row + 1
-        c.close()
+        self.modify_confirm(ds, False)
 
     # Smoke-test the modify API, recovering the database.
     def test_modify_smoke_recover(self):
@@ -251,34 +276,7 @@ class test_cursor12(wttest.WiredTigerTestCase):
             self.uri, 100, key_format=self.keyfmt, value_format='u')
         ds.populate()
         self.session.checkpoint()
-
-        # For each test in the list:
-        #       set the original value,
-        #       apply modifications in order,
-        #       confirm the final state,
-        #       confirm the final state is there.
-        row = 10
-        c = self.session.open_cursor(self.uri, None)
-        for i in self.list:
-            c.set_key(ds.key(row))
-            c.set_value(i['o'])
-            self.assertEquals(c.update(), 0)
-            c.reset()
-
-            c.set_key(ds.key(row))
-            mods = []
-            for j in i['mods']:
-                mod = wiredtiger.Modify(j[0], j[1], j[2])
-                mods.append(mod)
-            self.assertEquals(c.modify(mods), 0)
-            c.reset()
-
-            c.set_key(ds.key(row))
-            self.assertEquals(c.search(), 0)
-            self.assertEquals(c.get_value(), i['f'])
-
-            row = row + 1
-        c.close()
+        self.modify_load(ds, False)
 
         # Crash and recover in a new directory.
         newdir = 'RESTART'
@@ -288,17 +286,7 @@ class test_cursor12(wttest.WiredTigerTestCase):
         self.session = self.setUpSessionOpen(self.conn)
         self.session.verify(self.uri)
 
-        # For each test in the list:
-        #       confirm the final state is there.
-        row = 10
-        c = self.session.open_cursor(self.uri, None)
-        for i in self.list:
-            c.set_key(ds.key(row))
-            self.assertEquals(c.search(), 0)
-            self.assertEquals(c.get_value(), i['f'])
-
-            row = row + 1
-        c.close()
+        self.modify_confirm(ds, False)
 
     # Check that modify returns not-found after a delete.
     def test_modify_delete(self):
