f2d57d341a5072644a1695ef89be471153d86b3d
Sulabh Mahajan <sulabh.mahajan@mongodb.com>
2017-06-01 08:44:35 +1000

    Let EBUSY be returned immediately without further processing dhandles.

--- src/conn/conn_dhandle.c
+++ src/conn/conn_dhandle.c
@@ -199,8 +199,13 @@ __wt_conn_btree_sync_and_close(WT_SESSION_IMPL *session, bool final, bool force)
 			/* Reset the tree's eviction priority (if any). */
 			__wt_evict_priority_clear(session);
 		}
-		if (!marked_dead || final)
-			WT_TRET(__wt_checkpoint_close(session, final));
+		if (!marked_dead || final) {
+			ret = __wt_checkpoint_close(session, final);
+			if (ret == EBUSY)
+				WT_ERR(ret);
+			else
+				WT_TRET(ret);
+		}
 	}
 
 	WT_TRET(__wt_btree_close(session));
@@ -219,7 +224,7 @@ __wt_conn_btree_sync_and_close(WT_SESSION_IMPL *session, bool final, bool force)
 	    F_ISSET(dhandle, WT_DHANDLE_DEAD) ||
 	    !F_ISSET(dhandle, WT_DHANDLE_OPEN));
 
-	__wt_spin_unlock(session, &dhandle->close_lock);
+err:	__wt_spin_unlock(session, &dhandle->close_lock);
 
 	if (no_schema_lock)
 		F_CLR(session, WT_SESSION_NO_SCHEMA_LOCK);
