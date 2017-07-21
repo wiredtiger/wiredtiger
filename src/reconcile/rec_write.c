1f74be4326b5de2a399705678edf9c4cac8a4277
Vamsi Krishna <vamsi.krishna@mongodb.com>
2017-07-21 13:12:09 +1000

    WT-3308 Add statistics tracking around yield loops (#3496)
    If many threads spin in a yield loop the symptoms are difficult to diagnose - we end up consuming all kernel CPU, and throughput generally stalls.
    
    Add statistics to places where that is (now vanishingly unlikely) but theoretically possible.

--- src/reconcile/rec_write.c
+++ src/reconcile/rec_write.c
@@ -1690,7 +1690,7 @@ __rec_child_modify(WT_SESSION_IMPL *session,
 	 * not reserved for our exclusive use, there are other page states that
 	 * must be considered.
 	 */
-	for (;; __wt_yield())
+	for (;; __wt_yield()) {
 		switch (r->tested_ref_state = ref->state) {
 		case WT_REF_DISK:
 			/* On disk, not modified by definition. */
@@ -1801,6 +1801,8 @@ __rec_child_modify(WT_SESSION_IMPL *session,
 
 		WT_ILLEGAL_VALUE(session);
 		}
+		WT_STAT_CONN_INCR(session, child_modify_blocked_page);
+	}
 
 in_memory:
 	/*
