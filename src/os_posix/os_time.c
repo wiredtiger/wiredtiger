20ffb306fb0656ecd69a41a98736f653366b7606
Michael Cahill <michael.cahill@mongodb.com>
2017-07-27 16:17:29 +1000

    WT-3461 Use CLOCK_MONOTONIC for pthread_cond_timedwait if possible.
    Regardless, don't adjust the realtime clock before calculating when a
    timed sleep should end.  Otherwise, we can sleep for longer than
    expected by however much the clock changed.

--- src/os_posix/os_time.c
+++ src/os_posix/os_time.c
@@ -9,15 +9,55 @@
 #include "wt_internal.h"
 
 /*
+ * __wt_epoch_raw --
+ *	Return the time since the Epoch as reported by a system call.
+ */
+void
+__wt_epoch_raw(WT_SESSION_IMPL *session, struct timespec *tsp)
+{
+	WT_DECL_RET;
+
+	/*
+	 * This function doesn't return an error, but panics on failure (which
+	 * should never happen, it's done this way to simplify error handling
+	 * in the caller). However, some compilers complain about using garbage
+	 * values. Initializing the values avoids the complaint.
+	 */
+	tsp->tv_sec = 0;
+	tsp->tv_nsec = 0;
+
+#if defined(HAVE_CLOCK_GETTIME)
+	WT_SYSCALL_RETRY(clock_gettime(CLOCK_REALTIME, tsp), ret);
+	if (ret == 0)
+		return;
+	WT_PANIC_MSG(session, ret, "clock_gettime");
+#elif defined(HAVE_GETTIMEOFDAY)
+	{
+	struct timeval v;
+
+	WT_SYSCALL_RETRY(gettimeofday(&v, NULL), ret);
+	if (ret == 0) {
+		tsp->tv_sec = v.tv_sec;
+		tsp->tv_nsec = v.tv_usec * WT_THOUSAND;
+		return;
+	}
+	WT_PANIC_MSG(session, ret, "gettimeofday");
+	}
+#else
+	NO TIME-OF-DAY IMPLEMENTATION: see src/os_posix/os_time.c
+#endif
+}
+
+/*
  * __wt_epoch --
- *	Return the time since the Epoch.
+ *	Return the time since the Epoch, adjusted so it never appears to go
+ *	backwards.
  */
 void
 __wt_epoch(WT_SESSION_IMPL *session, struct timespec *tsp)
     WT_GCC_FUNC_ATTRIBUTE((visibility("default")))
 {
 	struct timespec tmp;
-	WT_DECL_RET;
 
 	/*
 	 * This function doesn't return an error, but panics on failure (which
@@ -33,30 +73,7 @@ __wt_epoch(WT_SESSION_IMPL *session, struct timespec *tsp)
 	 * value when we check for monotonic increasing time.  There are
 	 * many places we read into an unlocked global variable.
 	 */
-#if defined(HAVE_CLOCK_GETTIME)
-	WT_SYSCALL_RETRY(clock_gettime(CLOCK_REALTIME, &tmp), ret);
-	if (ret == 0) {
-		__wt_time_check_monotonic(session, &tmp);
-		tsp->tv_sec = tmp.tv_sec;
-		tsp->tv_nsec = tmp.tv_nsec;
-		return;
-	}
-	WT_PANIC_MSG(session, ret, "clock_gettime");
-#elif defined(HAVE_GETTIMEOFDAY)
-	{
-	struct timeval v;
-
-	WT_SYSCALL_RETRY(gettimeofday(&v, NULL), ret);
-	if (ret == 0) {
-		tmp.tv_sec = v.tv_sec;
-		tmp.tv_nsec = v.tv_usec * WT_THOUSAND;
-		__wt_time_check_monotonic(session, &tmp);
-		*tsp = tmp;
-		return;
-	}
-	WT_PANIC_MSG(session, ret, "gettimeofday");
-	}
-#else
-	NO TIME-OF-DAY IMPLEMENTATION: see src/os_posix/os_time.c
-#endif
+	__wt_epoch_raw(session, &tmp);
+	__wt_time_check_monotonic(session, &tmp);
+	*tsp = tmp;
 }
