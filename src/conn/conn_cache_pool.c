wiredtiger/wiredtiger

commit c08236a89486008f03539193dcb2026428ca6f75

clang static-analyzer complains we call __wt_config_gets() and trust it won't modify the value WT_CONFIG_ITEM if it returns WT_NOTFOUND.  Avoid by immediately copying the pool name.
There's no need to check updating when freeing pool_name, and we're less
likely to introduce bugs if we don't check.

src/conn/conn_cache_pool.c
@@ -77,12 +77,6 @@ __wt_cache_pool_config(WT_SESSION_IMPL *session, const char **cfg)
 			return (0);
 		}
 
-		if (__wt_config_gets(session,
-		    &cfg[1], "cache_size", &cval) != WT_NOTFOUND)
-			WT_RET_MSG(session, EINVAL,
-			    "Only one of cache_size and shared_cache can be "
-			    "in the configuration");
-
 		/*
 		 * NOTE: The allocations made when configuring and opening a
 		 * cache pool don't really belong to the connection that
@@ -91,6 +85,13 @@ __wt_cache_pool_config(WT_SESSION_IMPL *session, const char **cfg)
 		 * outside of the connection here.
 		 */
 		WT_RET(__wt_strndup(session, cval.str, cval.len, &pool_name));
+
+		if (__wt_config_gets(session,
+		    &cfg[1], "cache_size", &cval) != WT_NOTFOUND)
+			WT_ERR_MSG(session, EINVAL,
+			    "Only one of cache_size and shared_cache can be "
+			    "in the configuration");
+
 	}
 
 	__wt_spin_lock(session, &__wt_process.spinlock);
@@ -221,8 +222,7 @@ __wt_cache_pool_config(WT_SESSION_IMPL *session, const char **cfg)
 
 	F_SET(conn, WT_CONN_CACHE_POOL);
 err:	__wt_spin_unlock(session, &__wt_process.spinlock);
-	if (!updating)
-		__wt_free(session, pool_name);
+	__wt_free(session, pool_name);
 	if (ret != 0 && created) {
 		__wt_free(session, cp->name);
 		__wt_cond_destroy(session, &cp->cache_pool_cond);

commit 140d6a9d745c042876dbe539f73a88bf180bb1e0

My commit c08236a broke the spinlock code, we could attempt to unlock a spinlock we hadn't yet acquired. Fix a different way.

src/conn/conn_cache_pool.c
@@ -47,7 +47,7 @@ int
 __wt_cache_pool_config(WT_SESSION_IMPL *session, const char **cfg)
 {
 	WT_CACHE_POOL *cp;
-	WT_CONFIG_ITEM cval;
+	WT_CONFIG_ITEM cval, cval_tmp;
 	WT_CONNECTION_IMPL *conn, *entry;
 	WT_DECL_RET;
 	char *pool_name;
@@ -77,6 +77,12 @@ __wt_cache_pool_config(WT_SESSION_IMPL *session, const char **cfg)
 			return (0);
 		}
 
+		if (__wt_config_gets(session,
+		    &cfg[1], "cache_size", &cval_tmp) != WT_NOTFOUND)
+			WT_ERR_MSG(session, EINVAL,
+			    "Only one of cache_size and shared_cache can be "
+			    "in the configuration");
+
 		/*
 		 * NOTE: The allocations made when configuring and opening a
 		 * cache pool don't really belong to the connection that
@@ -85,12 +91,6 @@ __wt_cache_pool_config(WT_SESSION_IMPL *session, const char **cfg)
 		 * outside of the connection here.
 		 */
 		WT_RET(__wt_strndup(session, cval.str, cval.len, &pool_name));
-
-		if (__wt_config_gets(session,
-		    &cfg[1], "cache_size", &cval) != WT_NOTFOUND)
-			WT_ERR_MSG(session, EINVAL,
-			    "Only one of cache_size and shared_cache can be "
-			    "in the configuration");
 
 	}
 