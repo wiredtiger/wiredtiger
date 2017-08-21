c4570616756a2c50dd1d99359d311ac3a2c09f25
Keith Bostic <keith.bostic@mongodb.com>
2017-08-21 12:49:13 -0400

    Make major/minor messages consistent (instead of "unknown major version" and "illegal compatibility version", use "unsupported major version" and "unsupported major version"), and return ENOTSUP instead of EINVAL.
    Replace "single-threaded" with "quiescent", we don't require (or check),
    the API be single-threaded.

--- src/conn/conn_reconfig.c
+++ src/conn/conn_reconfig.c
@@ -39,10 +39,10 @@ __wt_conn_compat_config(WT_SESSION_IMPL *session, const char **cfg)
 	    &conn->compat_major, &conn->compat_minor, &patch) != 3)
 		WT_RET_MSG(session, EINVAL, "illegal compatibility release");
 	if (conn->compat_major > WIREDTIGER_VERSION_MAJOR)
-		WT_RET_MSG(session, EINVAL, "unknown major version");
+		WT_RET_MSG(session, ENOTSUP, "unsupported major version");
 	if (conn->compat_major == WIREDTIGER_VERSION_MAJOR &&
 	    conn->compat_minor > WIREDTIGER_VERSION_MINOR)
-		WT_RET_MSG(session, EINVAL, "illegal compatibility version");
+		WT_RET_MSG(session, ENOTSUP, "unsupported minor version");
 
 	/*
 	 * We're doing an upgrade or downgrade, check whether transactions are
@@ -51,7 +51,7 @@ __wt_conn_compat_config(WT_SESSION_IMPL *session, const char **cfg)
 	WT_RET(__wt_txn_activity_check(session, &txn_active));
 	if (txn_active)
 		WT_RET_MSG(session, ENOTSUP,
-		    "upgrade / downgrade must run single-threaded");
+		    "system must be quiescent for upgrade or downgrade");
 	return (0);
 }
 
