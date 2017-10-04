a26b3c62731b873c1ea91ef498dac1a69faa07d0
Keith Bostic <keith.bostic@mongodb.com>
2017-10-04 11:43:11 -0400

    Fix a couple of comments.

--- src/btree/col_srch.c
+++ src/btree/col_srch.c
@@ -93,7 +93,7 @@ __wt_col_search(WT_SESSION_IMPL *session,
 	 * the normal case where we are searching a tree, check the page's
 	 * parent keys before doing the full search, it's faster when the
 	 * cursor is being re-positioned.  Skip this if the page is being
-	 * re-instantiated in memory).
+	 * re-instantiated in memory.
 	 */
 	if (leaf != NULL) {
 		WT_ASSERT(session, search_recno != WT_RECNO_OOB);
