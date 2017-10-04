739796baa5352a9f276ccd16eab01c2c8e1d4ddd
Keith Bostic <keith.bostic@mongodb.com>
2017-10-04 10:16:22 -0400

    WT-3435 Fix lookaside eviction with timestamps.
    Give the checkpoint thread a lookaside table cursor.
    
    Don't give the sweep thread a lookaside table cursor.
    
    Check when opening a lookaside table cursor if the configuration
    requires one, rather than checking in each caller.
    
    Add a WT_THREAD_LOOKASIDE flag to the WT_THREAD_GROUP flags so the
    thread group code is general-purpose (don't assume any thread created
    by a thread group needs a lookaside cursor).

--- src/evict/evict_lru.c
+++ src/evict/evict_lru.c
@@ -460,6 +460,7 @@ int
 __wt_evict_create(WT_SESSION_IMPL *session)
 {
 	WT_CONNECTION_IMPL *conn;
+	uint32_t session_flags;
 
 	conn = S2C(session);
 
@@ -471,10 +472,12 @@ __wt_evict_create(WT_SESSION_IMPL *session)
 	 * Create the eviction thread group.
 	 * Set the group size to the maximum allowed sessions.
 	 */
+	session_flags = WT_THREAD_CAN_WAIT |
+	    WT_THREAD_LOOKASIDE | WT_THREAD_PANIC_FAIL;
 	WT_RET(__wt_thread_group_create(session, &conn->evict_threads,
 	    "eviction-server", conn->evict_threads_min, conn->evict_threads_max,
-	     WT_THREAD_CAN_WAIT | WT_THREAD_PANIC_FAIL, __wt_evict_thread_chk,
-	     __wt_evict_thread_run, __wt_evict_thread_stop));
+	    session_flags, __wt_evict_thread_chk, __wt_evict_thread_run,
+	    __wt_evict_thread_stop));
 
 #if defined(HAVE_DIAGNOSTIC) || defined(HAVE_VERBOSE)
 	/*
