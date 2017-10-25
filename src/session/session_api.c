86682a4d7e90f68e70ddb749008efd16b1a2d8b4
Sulabh Mahajan <sulabh.mahajan@mongodb.com>
2017-10-25 12:00:18 +1100

    fix long lines

--- src/session/session_api.c
+++ src/session/session_api.c
@@ -196,10 +196,13 @@ __session_close(WT_SESSION *wt_session, const char *config)
 
 	/* Close the file where we tracked long operations. */
 	if (F_ISSET(conn, WT_CONN_OPTRACK)) {
-		if (session->optrackbuf_ptr > 0 && session->optrack_fh != NULL) {
+		if (session->optrackbuf_ptr > 0 &&
+		    session->optrack_fh != NULL) {
 			WT_IGNORE_RET((int)__wt_optrack_flush_buffer(session));
-			WT_IGNORE_RET(__wt_close(session, &session->optrack_fh));
-			session->optrack_fh = NULL; /* Indicate file is closed */
+			WT_IGNORE_RET(__wt_close(session,
+			    &session->optrack_fh));
+			/* Indicate that the file is closed */
+			session->optrack_fh = NULL;
 			session->optrackbuf_ptr = 0;
 		}
 
