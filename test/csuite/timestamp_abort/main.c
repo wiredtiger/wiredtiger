dbdbc34c3f357cded6f88f2f1d3d0ad2480cb82c
Susan LoVerso <sue@mongodb.com>
2017-11-09 21:30:28 -0500

    Add volatile to the global array.

--- test/csuite/timestamp_abort/main.c
+++ test/csuite/timestamp_abort/main.c
@@ -75,7 +75,7 @@ static const char * const ckpt_file = "checkpoint_done";
 
 static bool compat, inmem, use_ts;
 static volatile uint64_t global_ts = 1;
-static uint64_t th_ts[MAX_TH];
+static volatile uint64_t th_ts[MAX_TH];
 
 #define	ENV_CONFIG_COMPAT	",compatibility=(release=\"2.9\")"
 #define	ENV_CONFIG_DEF						\
