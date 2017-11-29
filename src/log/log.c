4a15e418726d9c7492df14558774d187dad76d82
Sergei Turchanov <turchanov@farpost.com>
2017-11-29 14:58:19 +1000

    Follow more closely to the original behaviour which incremented prep_missed counter when pre-allocation was disabled

--- src/log/log.c
+++ src/log/log.c
@@ -978,10 +978,10 @@ __log_newfile(WT_SESSION_IMPL *session, bool conn_open, bool *created)
 	if (create_log) {
                 /*
                  * Do not increment missed prep logs counter when a hot backup
-                 * is in progress since we deliberately not using prep log files
-                 * in that case (see comments above).
+		 * is in progress and pre-allocation is enabled since
+		 * we deliberately not using prep log files in that case (see comments above).
                  */
-                if (!conn->hot_backup)
+		if ((conn->log_prealloc == 0) || !conn->hot_backup)
 			log->prep_missed++;
 		WT_RET(__wt_log_allocfile(
 		    session, log->fileid, WT_LOG_FILENAME));
