8e94b1ff8269c5bfe97eac3364716a23598282c0
Keith Bostic <keith.bostic@mongodb.com>
2017-12-08 09:39:10 -0500

    Change the api.h macro usage of the tracking macros to appear after we've initialized the session name so error messages will include the session name. Order the track operations macros vs. single-thread check macros consistently.
    For the optrack-header padding field, char isn't a fixed-size type, use
    uint8_t instead.
    
    Prefix macro-local variables tr and id_recorded with underscores, don't
    step on the function's namespace.
    
    Move the WT_OPTRACK_RECORD and the optrackbuf_ptr variable declaration
    and handling into WT_TRACK_OP, the work is the same in WT_TRACK_OP_INIT
    and WT_TRACK_OP_END.
    
    Move the __wt_optrack_record_funcid() call from the WT_TRACK_OP macro to
    WT_TRACK_OP_INIT so lint doesn't complain about dead code when WT_TRACK_OP
    is called at function exit.
    
    Don't take the address of a function name, it's not an lvalue.
    
    Don't pass two versions of the function address (a void * and uint64_t),
    to __wt_optrack_record_funcid(). Don't pass the size of the function
    argument to __wt_optrack_record_funcid(), it can figure it out.
    
    Don't bother with a newline buffer, just inline the string.
    
    Encapsulate tracking macro arguments in parenthesis to enforce operator
    ordering.
    
    Don't declare the __id_recorded variable as volatile, it's not needed
    and it guarantees a cache miss on function exit we otherwise might not
    have.

--- src/optrack/optrack.c
+++ src/optrack/optrack.c
@@ -13,20 +13,19 @@
  *	Record optrack function id
  */
 void
-__wt_optrack_record_funcid(WT_SESSION_IMPL *session,
-    uint64_t op_id, void *func, size_t funcsize, volatile bool *id_recorded)
+__wt_optrack_record_funcid(
+    WT_SESSION_IMPL *session, void *func, volatile bool *id_recorded)
 {
 	WT_CONNECTION_IMPL *conn;
 	wt_off_t fsize;
-	char endline[] = "\n";
-	char id_buf[sizeof(uint64_t)*2 + sizeof(char)*4];
+	char id_buf[sizeof(uint64_t) * 2 + sizeof(uint8_t) * 4];
 
 	conn = S2C(session);
 
 	__wt_spin_lock(session, &conn->optrack_map_spinlock);
 	if (!*id_recorded) {
 		WT_IGNORE_RET(__wt_snprintf(id_buf,
-		    sizeof(id_buf), "%p ", (void*)op_id));
+		    sizeof(id_buf), "%p ", func));
 		WT_IGNORE_RET(__wt_filesize(session,
 		    conn->optrack_map_fh, &fsize));
 		WT_IGNORE_RET(__wt_write(session,
@@ -35,11 +34,11 @@ __wt_optrack_record_funcid(WT_SESSION_IMPL *session,
 		WT_IGNORE_RET(__wt_filesize(session,
 		    conn->optrack_map_fh, &fsize));
 		WT_IGNORE_RET(__wt_write(session,
-		    conn->optrack_map_fh, fsize, funcsize - 1, func));
+		    conn->optrack_map_fh, fsize, sizeof(void *) - 1, func));
 		WT_IGNORE_RET(__wt_filesize(session,
 		    conn->optrack_map_fh, &fsize));
 		WT_IGNORE_RET(__wt_write(session,
-		    conn->optrack_map_fh, fsize, sizeof(endline) - 1, endline));
+		    conn->optrack_map_fh, fsize, 1, "\n"));
 		*id_recorded = true;
 	}
 	__wt_spin_unlock(session, &conn->optrack_map_spinlock);
