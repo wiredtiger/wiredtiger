af071a9af8ffd8e9661ec7a0331a2bfc71cb2cd2
Keith Bostic <keith.bostic@mongodb.com>
2017-12-13 15:39:27 -0500

    I introduced a comma in the map file in 35a05b8, put it all back to Sasha's original format.

--- src/optrack/optrack.c
+++ src/optrack/optrack.c
@@ -30,7 +30,7 @@ __wt_optrack_record_funcid(
 		*func_idp = ++conn->optrack_uid;
 
 		WT_ERR(__wt_buf_fmt(
-		    session, tmp, "%" PRIu16 ", %s\n", *func_idp, func));
+		    session, tmp, "%" PRIu16 " %s\n", *func_idp, func));
 		WT_ERR(__wt_filesize(session, conn->optrack_map_fh, &fsize));
 		WT_ERR(__wt_write(session,
 		    conn->optrack_map_fh, fsize, tmp->size, tmp->data));
