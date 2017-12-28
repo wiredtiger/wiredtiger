wiredtiger/wiredtiger

commit af1ff259e0434e410c3cd43b7597cb8d25e1a567

We can't free WT_REF.page_las when WT_REF_LIMBO pages switch to WT_REF_MEM pages without locking, threads may be concurrently reading the structure. This implies the WT_REF.page_las structure is present when pages are in the WT_REF_MEM state, which means it can collide with fast-delete page states, that is, WT_REF.page_del and WT_REF.page_las can both be set at the same time.
Grow the WT_REF structure to accomodate WT_REF.page_del and WT_REF.page_las
at the same time, growing from 48 to 56 bytes. This simplifies the WT_REF
discard code, we no longer have to check the WT_REF state.

When evicting a clean page, don't check the WT_REF.page_las field it won't
always be discarded, instead check if WT_REF.state is set to WT_REF_LIMBO.

src/btree/bt_read.c
@@ -561,10 +561,12 @@ skip_read:
 		 * The page is instantiated so we no longer need the lookaside
 		 * entries. Note we are discarding updates so the page must be
 		 * marked available even if these operations fail.
+		 *
+		 * Don't free WT_REF.page_las, there may be concurrent readers.
 		 */
 		WT_TRET(__wt_las_remove_block(
 		    session, NULL, btree->id, ref->page_las->las_pageid));
-		__wt_free(session, ref->page_las);
+
 		break;
 	}
 

src/evict/evict_page.c
@@ -269,11 +269,11 @@ __evict_page_clean_update(WT_SESSION_IMPL *session, WT_REF *ref, bool closing)
 	 * an address, it's a disk page; if it has no address, it's a deleted
 	 * page re-instantiated (for example, by searching) and never written.
 	 *
-	 * If evict a WT_REF_LIMBO reference, we get to here and should
+	 * If evicting a WT_REF_LIMBO reference, we get to here and should
 	 * transition back to WT_REF_LOOKASIDE.
 	 */
 	__wt_ref_out(session, ref);
-	if (ref->page_las != NULL)
+	if (ref->state == WT_REF_LIMBO)
 		WT_PUBLISH(ref->state, WT_REF_LOOKASIDE);
 	else if (ref->addr == NULL) {
 		WT_WITH_PAGE_INDEX(session,

src/include/btmem.h
@@ -827,16 +827,14 @@ struct __wt_ref {
 #undef	ref_ikey
 #define	ref_ikey	key.ikey
 
-	union {
-		WT_PAGE_DELETED	*page_del;	/* Deleted page information */
-		WT_PAGE_LOOKASIDE *page_las;	/* Lookaside information */
-	};
+	WT_PAGE_DELETED	  *page_del;	/* Deleted page information */
+	WT_PAGE_LOOKASIDE *page_las;	/* Lookaside information */
 };
 /*
  * WT_REF_SIZE is the expected structure size -- we verify the build to ensure
  * the compiler hasn't inserted padding which would break the world.
  */
-#define	WT_REF_SIZE	48
+#define	WT_REF_SIZE	56
 
 /*
  * WT_ROW --

src/btree/bt_discard.c
@@ -321,9 +321,8 @@ __wt_free_ref(
 	 * lookaside structure for lookaside references, but can see
 	 * page-deleted information in other cases (such as WT_REF_MEM).
 	 */
-	if (ref->state == WT_REF_LIMBO || ref->state == WT_REF_LOOKASIDE)
-		__wt_free(session, ref->page_las);
-	else if (ref->page_del != NULL) {
+	__wt_free(session, ref->page_las);
+	if (ref->page_del != NULL) {
 		__wt_free(session, ref->page_del->update_list);
 		__wt_free(session, ref->page_del);
 	}

commit c5c639ecd8e60d9b7e2a55e84e7a6298abec6b5e

Commit af1ff25 introduced a bug: during eviction, the WT_REF.state is set to WT_REF_LOCKED, so we can't check WT_REF_LIMBO to see if we should revert to WT_REF_LOOKASIDE. Add a boolean to the WT_PAGE_LOOKASIDE structure that flags if we should do that reversion.

src/btree/bt_read.c
@@ -584,6 +584,7 @@ skip_read:
 		if (__las_page_skip_locked(session, ref)) {
 			WT_STAT_CONN_INCR(
 			    session, cache_read_lookaside_skipped);
+			ref->page_las->eviction_to_lookaside = true;
 			final_state = WT_REF_LIMBO;
 			break;
 		}
@@ -605,6 +606,7 @@ skip_read:
 		WT_TRET(__wt_las_remove_block(
 		    session, NULL, btree->id, ref->page_las->las_pageid));
 
+		ref->page_las->eviction_to_lookaside = false;
 		break;
 	}
 

src/evict/evict_page.c
@@ -269,13 +269,17 @@ __evict_page_clean_update(WT_SESSION_IMPL *session, WT_REF *ref, bool closing)
 	 * an address, it's a disk page; if it has no address, it's a deleted
 	 * page re-instantiated (for example, by searching) and never written.
 	 *
-	 * If evicting a WT_REF_LIMBO reference, we get to here and should
-	 * transition back to WT_REF_LOOKASIDE.
+	 * If evicting a WT_REF_LIMBO reference, we get to here and transition
+	 * back to WT_REF_LOOKASIDE, else discarding any lookaside information.
 	 */
 	__wt_ref_out(session, ref);
-	if (ref->state == WT_REF_LIMBO)
+	if (ref->page_las != NULL &&
+	    ref->page_las->eviction_to_lookaside == false)
+		__wt_free(session, ref->page_las);
+	if (ref->page_las != NULL) {
+		ref->page_las->eviction_to_lookaside = false;
 		WT_PUBLISH(ref->state, WT_REF_LOOKASIDE);
-	else if (ref->addr == NULL) {
+	} else if (ref->addr == NULL) {
 		WT_WITH_PAGE_INDEX(session,
 		    ret = __evict_delete_ref(session, ref, closing));
 		WT_RET_BUSY_OK(ret);

src/include/btmem.h
@@ -232,14 +232,14 @@ struct __wt_ovfl_reuse {
  *	Related information for on-disk pages with lookaside entries.
  */
 struct __wt_page_lookaside {
-	uint64_t las_pageid;			/* Page ID in lookaside */
-	uint64_t las_max_txn;			/* Maximum transaction ID in
-						   lookaside */
-	WT_DECL_TIMESTAMP(min_timestamp)	/* Min timestamp in lookaside */
-	WT_DECL_TIMESTAMP(onpage_timestamp)	/* Max timestamp on page */
-	bool las_skew_newest;			/* On-page skewed to newest */
-	bool invalid;				/* History is required correct
-						   reads */
+	uint64_t las_pageid;		/* Page ID in lookaside */
+	uint64_t las_max_txn;		/* Max transaction ID in lookaside */
+	WT_DECL_TIMESTAMP(min_timestamp)/* Min timestamp in lookaside */
+					/* Max timestamp on page */
+	WT_DECL_TIMESTAMP(onpage_timestamp)
+	bool eviction_to_lookaside;	/* Revert to lookaside on eviction */
+	bool las_skew_newest;		/* On-page skewed to newest */
+	bool invalid;			/* History is required correct reads */
 };
 
 /*