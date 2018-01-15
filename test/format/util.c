6a668d2bd9e172028492c81506e2426cfbd9c58e
Michael Cahill <michael.cahill@mongodb.com>
2018-01-15 11:40:42 +1100

    WT-3799 Idle format threads should not pin timestamps.

--- test/format/util.c
+++ test/format/util.c
@@ -611,7 +611,7 @@ timestamp(void *arg)
 		 * Find the lowest committed timestamp. The timestamp thread
 		 * starts before the operational threads, wait for them.
 		 */
-		oldest_timestamp = UINT64_MAX;
+		oldest_timestamp = g.timestamp;
 		for (i = 0; i < g.c_threads; ++i) {
 			tinfo = tinfo_list[i];
 			this_ts = tinfo->timestamp;
@@ -619,14 +619,10 @@ timestamp(void *arg)
 			    this_ts < oldest_timestamp)
 				oldest_timestamp = this_ts;
 		}
-		if (oldest_timestamp == UINT64_MAX) {
-			__wt_sleep(1, 0);
-			continue;
-		}
 
 		/*
-		 * Don't get more than 100 transactions or more than 15 seconds
-		 * out of date.
+		 * If less than 100 transactions out of date, wait up to 15
+		 * seconds.
 		 */
 		WT_READ_BARRIER();
 		testutil_assert(oldest_timestamp <= g.timestamp);
