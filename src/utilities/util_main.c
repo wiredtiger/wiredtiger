bf248b721673e6ca3f7cf686ddb1c44ae4ffba1f
Keith Bostic <keith.bostic@mongodb.com>
2018-01-19 07:10:40 -0500

    Make the wt utility output format cleanly, minor wording fix.

--- src/utilities/util_main.c
+++ src/utilities/util_main.c
@@ -41,7 +41,7 @@ usage(void)
 	    "\t" "compact\t  compact an object\n"
 	    "\t" "copyright copyright information\n"
 	    "\t" "create\t  create an object\n"
-	    "\t" "downgrade\t  downgrade an database\n"
+	    "\t" "downgrade downgrade an object\n"
 	    "\t" "drop\t  drop an object\n"
 	    "\t" "dump\t  dump an object\n"
 	    "\t" "list\t  list database objects\n"
