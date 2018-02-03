5a9cee101b7dd450814625297219bd324d5a6fcf
Keith Bostic <keith.bostic@mongodb.com>
2018-02-03 12:25:47 -0500

    whitespace.

--- src/cache/cache_las.c
+++ src/cache/cache_las.c
@@ -403,7 +403,7 @@ __las_insert_block_verbose(WT_SESSION_IMPL *session, WT_MULTI *multi)
 
 /*
  * __wt_las_insert_block --
- *	Copy one set of saved updates into the database's lookaside buffer.
+ *	Copy one set of saved updates into the database's lookaside table.
  */
 int
 __wt_las_insert_block(WT_SESSION_IMPL *session, WT_CURSOR *cursor,
@@ -691,8 +691,7 @@ err:	if (locked)
 			WT_TRET(__wt_txn_rollback(las_session, NULL));
 	}
 	if (local_cursor)
-		WT_TRET(__wt_las_cursor_close(
-		    session, &cursor, session_flags));
+		WT_TRET(__wt_las_cursor_close(session, &cursor, session_flags));
 
 	__wt_cache_decr_check_uint64(session,
 	    &conn->cache->las_entry_count, remove_cnt,
