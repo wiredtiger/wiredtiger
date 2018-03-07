eca9f7714362c687fdec30fe368cbede1d6d9261
Vamsi Krishna <vamsi.krishna@mongodb.com>
2018-03-07 13:32:43 +1100

    disable prepare tests in test/format

--- test/format/ops.c
+++ test/format/ops.c
@@ -1091,7 +1091,8 @@ update_instead_of_chosen_op:
 		}
 
 		/* Prepare the transaction 10% of the time. */
-		if (mmrand(&tinfo->rnd, 1, 10) == 1) {
+		/* XXX: CONFIGURE PREPARE OFF FOR NOW */
+		if (mmrand(&tinfo->rnd, 1, 10) == 0) {
 			ret = prepare_transaction(tinfo, session);
 			testutil_assert(ret == 0 || ret == WT_PREPARE_CONFLICT);
 			if (ret == WT_PREPARE_CONFLICT)
