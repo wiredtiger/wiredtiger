ca0533b7cc968a0fad1cb377381c90dd670801c4
Michael Cahill <michael.cahill@mongodb.com>
2018-03-28 16:43:26 +1100

    Only get one snapshot if a read_timestamp is configured in begin_transaction.

--- src/txn/txn_timestamp.c
+++ src/txn/txn_timestamp.c
@@ -691,18 +691,20 @@ __wt_txn_parse_read_timestamp(WT_SESSION_IMPL *session, const char *cfg[])
 		txn_global = &S2C(session)->txn_global;
 		WT_RET(__wt_txn_parse_timestamp(session, "read", &ts, &cval));
 
-		/* Read timestamps imply / require snapshot isolation. */
+		/*
+		 * Read timestamps imply / require snapshot isolation.
+		 *
+		 * If we already have a snapshot, it may be too early
+		 * to match the timestamp.  Get a new one.
+		 */
 		if (!F_ISSET(txn, WT_TXN_RUNNING))
 			txn->isolation = WT_ISO_SNAPSHOT;
-		else if (txn->isolation != WT_ISO_SNAPSHOT)
+		else if (txn->isolation == WT_ISO_SNAPSHOT)
+			__wt_txn_get_snapshot(session);
+		else
 			WT_RET_MSG(session, EINVAL, "setting a read_timestamp"
 			    " requires a transaction running at snapshot"
 			    " isolation");
-		/*
-		 * If we already have a snapshot, it may be too early to match
-		 * the timestamp.  Get a new one.
-		 */
-		__wt_txn_get_snapshot(session);
 
 		/* Read timestamps can't change once set. */
 		if (F_ISSET(txn, WT_TXN_HAS_TS_READ))
