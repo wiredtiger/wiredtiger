ae760d5646c482216156be5a84abc1d747defbc9
Keith Bostic <keith.bostic@mongodb.com>
2018-03-29 11:57:03 -0400

    WT-4006 format should support stress timing configurations.

--- src/conn/conn_api.c
+++ src/conn/conn_api.c
@@ -1994,9 +1994,13 @@ __wt_timing_stress_config(WT_SESSION_IMPL *session, const char *cfg[])
 {
 	static const WT_NAME_FLAG stress_types[] = {
 		{ "checkpoint_slow",	WT_TIMING_STRESS_CHECKPOINT_SLOW },
-		{ "internal_page_split_race",
-		    WT_TIMING_STRESS_INTERNAL_PAGE_SPLIT_RACE },
-		{ "page_split_race",	WT_TIMING_STRESS_PAGE_SPLIT_RACE },
+		{ "split_race_1",	WT_TIMING_STRESS_SPLIT_RACE_1 },
+		{ "split_race_2",	WT_TIMING_STRESS_SPLIT_RACE_2 },
+		{ "split_race_3",	WT_TIMING_STRESS_SPLIT_RACE_3 },
+		{ "split_race_4",	WT_TIMING_STRESS_SPLIT_RACE_4 },
+		{ "split_race_5",	WT_TIMING_STRESS_SPLIT_RACE_5 },
+		{ "split_race_6",	WT_TIMING_STRESS_SPLIT_RACE_6 },
+		{ "split_race_7",	WT_TIMING_STRESS_SPLIT_RACE_7 },
 		{ NULL, 0 }
 	};
 	WT_CONFIG_ITEM cval, sval;
