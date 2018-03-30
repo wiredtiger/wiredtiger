ff85f6a96c725472ac3de7133e37c2b02d22b5d4
Keith Bostic <keith.bostic@mongodb.com>
2018-03-30 07:01:10 -0400

    We can do a fast check if the page is clean before acquiring the lock, there's no reason to block access to active pages if we know the check won't succeed.

--- src/btree/bt_delete.c
+++ src/btree/bt_delete.c
@@ -71,9 +71,13 @@ __wt_delete_page(WT_SESSION_IMPL *session, WT_REF *ref, bool *skipp)
 
 	*skipp = false;
 
-	/* If we have a clean page in memory, attempt to evict it. */
+	/*
+	 * If we have a clean page in memory, attempt to evict it. Do a fast
+	 * check for a dirty page, and then repeat the test once we're locked.
+	 */
 	previous_state = ref->state;
 	if ((previous_state == WT_REF_MEM || previous_state == WT_REF_LIMBO) &&
+	    !__wt_page_is_modified(ref->page) &&
 	    __wt_atomic_casv32(&ref->state, previous_state, WT_REF_LOCKED)) {
 		if (__wt_page_is_modified(ref->page)) {
 			ref->state = previous_state;
