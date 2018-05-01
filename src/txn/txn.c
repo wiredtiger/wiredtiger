4914ae7ee1bdab20f99f0302fdcc1e942bbc0483
Michael Cahill <michael.cahill@mongodb.com>
2018-05-01 16:27:05 +1000

    Avoid WT_IGNORE_RET: only works for int type.

--- src/txn/txn.c
+++ src/txn/txn.c
@@ -963,7 +963,7 @@ __wt_txn_commit(WT_SESSION_IMPL *session, const char *cfg[])
 	 * any cached snapshots have to be refreshed.
 	 */
 	if (!readonly)
-		WT_IGNORE_RET(__wt_gen_next(session, WT_GEN_COMMIT));
+		(void)__wt_gen_next(session, WT_GEN_COMMIT);
 
 #ifdef HAVE_TIMESTAMPS
 	/* First check if we've already committed something in the future. */
