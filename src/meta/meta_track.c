11e26b38b93282d998e586d91a81690384c35993
Alex Gorrod <alexander.gorrod@mongodb.com>
2018-05-11 14:00:49 +1000

    Really disable transactional schema operations

--- src/meta/meta_track.c
+++ src/meta/meta_track.c
@@ -8,7 +8,7 @@
 
 #include "wt_internal.h"
 
-#define	WT_ENABLE_SCHEMA_TXN 0
+#undef	WT_ENABLE_SCHEMA_TXN
 /*
  * WT_META_TRACK -- A tracked metadata operation: a non-transactional log,
  * maintained to make it easy to unroll simple metadata and filesystem
