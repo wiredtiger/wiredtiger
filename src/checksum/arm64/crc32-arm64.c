bb44dc0720c44a018ac1b00ead39eb2b92790bd8
Keith Bostic <keith.bostic@mongodb.com>
2018-07-12 07:26:01 -0400

    We support configuring WiredTiger with the gcc/clang -fvisibility=hidden flags, but that requires public APIs be specifically marked. Now we're not including <wiredtiger.h> in the checksum code, we need to call out those functions locally.

--- src/checksum/arm64/crc32-arm64.c
+++ src/checksum/arm64/crc32-arm64.c
@@ -86,7 +86,12 @@ __wt_checksum_hw(const void *chunk, size_t len)
 #endif
 
 extern uint32_t __wt_checksum_sw(const void *chunk, size_t len);
+#if defined(__GNUC__)
+extern uint32_t (*wiredtiger_crc32c_func(void))(const void *, size_t)
+    __attribute__((visibility("default")));
+#else
 extern uint32_t (*wiredtiger_crc32c_func(void))(const void *, size_t);
+#endif
 
 /*
  * wiredtiger_crc32c_func --
--- src/checksum/x86/crc32-x86.c
+++ src/checksum/x86/crc32-x86.c
@@ -120,7 +120,12 @@ __wt_checksum_hw(const void *chunk, size_t len)
 #endif /* HAVE_CRC32_HARDWARE */
 
 extern uint32_t __wt_checksum_sw(const void *chunk, size_t len);
+#if defined(__GNUC__)
+extern uint32_t (*wiredtiger_crc32c_func(void))(const void *, size_t)
+    __attribute__((visibility("default")));
+#else
 extern uint32_t (*wiredtiger_crc32c_func(void))(const void *, size_t);
+#endif
 
 /*
  * wiredtiger_crc32c_func --
