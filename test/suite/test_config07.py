c91b80412603f283532e267893f9238dd4a5ec0f
nehakhatri5 <30581300+nehakhatri5@users.noreply.github.com>
2018-08-10 17:06:26 +1000

    WT-4242 Wait for prep log creation in log file extend test. (#4209)

--- test/suite/test_config07.py
+++ test/suite/test_config07.py
@@ -26,7 +26,7 @@
 # ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 # OTHER DEALINGS IN THE SOFTWARE.
 
-import fnmatch, os
+import fnmatch, os, time
 import wiredtiger, wttest
 from wtscenario import make_scenarios
 
@@ -64,10 +64,17 @@ class test_config07(wttest.WiredTigerTestCase):
         cur.close()
 
     def checkLogFileSize(self, size):
-        logs = fnmatch.filter(os.listdir('.'), "*Prep*")
-        f = logs[-1]
-        file_size = os.stat(f).st_size
-        self.assertEqual(size, file_size)
+        # Wait for a log file to be preallocated. Avoid timing problems, but
+        # assert that a file is created within 1 minute.
+        for i in range(1,60):
+            logs = fnmatch.filter(os.listdir('.'), "*Prep*")
+            if logs:
+                f = logs[-1]
+                file_size = os.stat(f).st_size
+                self.assertEqual(size, file_size)
+                break
+            time.sleep(1)
+        self.assertTrue(logs)
 
     def test_log_extend(self):
         self.conn.close()
