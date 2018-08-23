95b4887958d2dd68cd5159eb34eeb740a98b1b22
Michael Cahill <michael.cahill@mongodb.com>
2018-08-23 15:44:53 +1000

    WT-4262 In eviction of internal pages, lock deleted children. (#4237)
    Elsewhere we assume that flipping ref->state from DELETED to LOCKED
    makes it safe to free ref->page_del.  If we're attempting to evict an
    internal page, we have it locked, so no thread can descend into it, but
    a walk could be attempting to couple into a child page, so we can't
    assume exclusive access to all of the children.

--- src/evict/evict_page.c
+++ src/evict/evict_page.c
@@ -450,6 +450,7 @@ static int
 __evict_child_check(WT_SESSION_IMPL *session, WT_REF *parent)
 {
 	WT_REF *child;
+	bool active;
 
 	WT_INTL_FOREACH_BEGIN(session, parent->page, child) {
 		switch (child->state) {
@@ -457,14 +458,26 @@ __evict_child_check(WT_SESSION_IMPL *session, WT_REF *parent)
 			break;
 		case WT_REF_DELETED:		/* Deleted */
 			/*
-			 * If the page was part of a truncate, transaction
-			 * rollback might switch this page into its previous
-			 * state at any time, so the delete must be resolved.
-			 * We don't have to lock the page, as no thread of
-			 * control can be running below our locked internal
-			 * page.
+			 * If the child page was part of a truncate,
+			 * transaction rollback might switch this page into its
+			 * previous state at any time, so the delete must be
+			 * resolved before the parent can be evicted.
+			 *
+			 * We have the internal page locked, which prevents a
+			 * search from descending into it.  However, a walk
+			 * from an adjacent leaf page could attempt to hazard
+			 * couple into a child page and free the page_del
+			 * structure as we are examining it.  Flip the state to
+			 * locked to make this check safe: if that fails, we
+			 * have raced with a read and should give up on
+			 * evicting the parent.
 			 */
-			if (__wt_page_del_active(session, child, true))
+			if (!__wt_atomic_casv32(
+			    &child->state, WT_REF_DELETED, WT_REF_LOCKED))
+				return (__wt_set_return(session, EBUSY));
+			active = __wt_page_del_active(session, child, true);
+			child->state = WT_REF_DELETED;
+			if (active)
 				return (__wt_set_return(session, EBUSY));
 			break;
 		case WT_REF_LOOKASIDE:
