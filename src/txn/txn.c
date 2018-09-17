fe4c59ebd356e21b6e0e89dce0b04aae6610d26a
Vamsi Krishna <vamsi.krishna@mongodb.com>
2018-09-17 16:03:11 +1000

    unused variable

--- src/txn/txn.c
+++ src/txn/txn.c
@@ -1072,7 +1072,7 @@ __wt_txn_rollback(WT_SESSION_IMPL *session, const char *cfg[])
 	WT_TXN_OP *op;
 	WT_UPDATE *upd;
 	u_int i;
-	bool prepare, readonly;
+	bool readonly;
 
 	WT_UNUSED(cfg);
 
@@ -1085,7 +1085,6 @@ __wt_txn_rollback(WT_SESSION_IMPL *session, const char *cfg[])
 		WT_TRET(txn->notify->notify(txn->notify, (WT_SESSION *)session,
 		    txn->id, 0));
 
-	prepare = F_ISSET(txn, WT_TXN_PREPARE);
 	/* Rollback updates. */
 	for (i = 0, op = txn->mod; i < txn->mod_count; i++, op++) {
 		/* Assert it's not an update to the lookaside file. */
