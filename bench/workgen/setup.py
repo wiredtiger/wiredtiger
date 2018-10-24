a1229a2509870309b6d0dd3033fd2b453143cc08
Don Anderson <dda@mongodb.com>
2018-10-24 14:38:29 +0000

    WT-4371 Change ordering of referenced libraries, it makes a difference on some systems.

--- bench/workgen/setup.py
+++ bench/workgen/setup.py
@@ -62,7 +62,7 @@ wt_ver = '%d.%d' % (WIREDTIGER_VERSION_MAJOR, WIREDTIGER_VERSION_MINOR)
 setup(name='workgen', version=wt_ver,
     ext_modules=[Extension('_workgen',
                 [os.path.join(dir, 'workgen_wrap.cxx')],
-        libraries=['wiredtiger', 'pthread', 'workgen'],
+        libraries=['workgen', 'wiredtiger', 'pthread'],
         extra_compile_args=extra_cflags,
     )],
     package_dir={'' : dir},
