fd172e3195bcc1856a79ca6eefed5f4550f354aa
Keith Bostic <keith.bostic@mongodb.com>
2018-10-28 15:34:57 -0400

    Another try, I added too many sets of braces.

--- examples/c/ex_all.c
+++ examples/c/ex_all.c
@@ -331,11 +331,9 @@ cursor_ops(WT_SESSION *session)
 	error_check(cursor->insert(cursor));
 	}
 
-	{
 	/* Modify requires an explicit transaction. */
 	error_check(session->begin_transaction(session, NULL));
 	{
-
 	/*! [Modify an existing record] */
 	WT_MODIFY entries[3];
 	const char *key = "some key";
@@ -365,9 +363,7 @@ cursor_ops(WT_SESSION *session)
 	error_check(cursor->modify(cursor, entries, 3));
 	/*! [Modify an existing record] */
 	}
-
 	error_check(session->commit_transaction(session, NULL));
-	}
 
 	{
 	/*! [Update an existing record or insert a new record] */
