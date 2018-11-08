be16d9e87cea675a96c22a83ed93cecb0f57f527
Don Anderson <dda@mongodb.com>
2018-11-08 12:02:41 -0500

    WT-4411 Added connection statistic for current total of cached cursors. Renamed data source statistic for current total.  Tweeked descriptions of other cache cursor statistics.  Added test to verify new statistic.

--- src/cursor/cur_std.c
+++ src/cursor/cur_std.c
@@ -617,7 +617,8 @@ __wt_cursor_cache(WT_CURSOR *cursor, WT_DATA_HANDLE *dhandle)
 
 	(void)__wt_atomic_sub32(&S2C(session)->open_cursor_count, 1);
 	WT_STAT_DATA_DECR(session, session_cursor_open);
-	WT_STAT_DATA_INCR(session, session_cursor_cached);
+	WT_STAT_DATA_INCR(session, session_cursors_cached);
+	WT_STAT_CONN_INCR(session, cursors_cached);
 	F_SET(cursor, WT_CURSTD_CACHED);
 	return (ret);
 }
@@ -642,7 +643,8 @@ __wt_cursor_reopen(WT_CURSOR *cursor, WT_DATA_HANDLE *dhandle)
 	}
 	(void)__wt_atomic_add32(&S2C(session)->open_cursor_count, 1);
 	WT_STAT_DATA_INCR(session, session_cursor_open);
-	WT_STAT_DATA_DECR(session, session_cursor_cached);
+	WT_STAT_DATA_DECR(session, session_cursors_cached);
+	WT_STAT_CONN_DECR(session, cursors_cached);
 
 	bucket = cursor->uri_hash % WT_HASH_ARRAY_SIZE;
 	TAILQ_REMOVE(&session->cursor_cache[bucket], cursor, q);
