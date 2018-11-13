ed06dad91a1b0ed25a6cba1826f7bcb2dac0ffaa
nehakhatri5 <neha.khatri@mongodb.com>
2018-11-13 13:55:01 +1100

    WT-4375 Define a new statistics cursor for statistics:session

--- src/include/cursor.h
+++ src/include/cursor.h
@@ -487,6 +487,7 @@ struct __wt_cursor_stat {
 		WT_DSRC_STATS dsrc_stats;
 		WT_CONNECTION_STATS conn_stats;
 		WT_JOIN_STATS_GROUP join_stats_group;
+		WT_OPERATION_STATS operation_stats;
 	} u;
 
 	const char **cfg;		/* Original cursor configuration */
---
+++ test/suite/test_stat07.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+#
+# Public Domain 2014-2018 MongoDB, Inc.
+# Public Domain 2008-2014 WiredTiger, Inc.
+#
+# This is free and unencumbered software released into the public domain.
+#
+# Anyone is free to copy, modify, publish, use, compile, sell, or
+# distribute this software, either in source code form or as a compiled
+# binary, for any purpose, commercial or non-commercial, and by any
+# means.
+#
+# In jurisdictions that recognize copyright laws, the author or authors
+# of this software dedicate any and all copyright interest in the
+# software to the public domain. We make this dedication for the benefit
+# of the public at large and to the detriment of our heirs and
+# successors. We intend this dedication to be an overt act of
+# relinquishment in perpetuity of all present and future rights to this
+# software under copyright law.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+
+import helper, wiredtiger, wttest
+from time import sleep
+
+# test_stat07.py
+#    Check that operation statistics are enabled or disabled when intended
+class test_stat06(wttest.WiredTigerTestCase):
+
+    def test_stats_on(self):
+        self.close_conn()
+        self.conn = self.wiredtiger_open(None, "statistics=(all)")
+        self.stats_enabled('all')
+
+    def test_fast_stats_on(self):
+        self.close_conn()
+        self.conn = self.wiredtiger_open(None, "statistics=(fast)")
+        self.stats_enabled('fast')
+
+    def test_stats_off(self):
+        self.close_conn()
+        self.conn = self.wiredtiger_open(None, "statistics=(none)")
+        self.stats_enabled('none')
+
+    def stats_enabled(self, stats_expected):
+        self.session = self.conn.open_session()
+        msg = '/database statistics configuration/'
+        if stats_expected == 'all':
+            found = False
+            op_stat_cur = self.session.open_cursor('statistics:session', None, 'statistics=(all)')
+            while op_stat_cur.next() == 0:
+                [desc, pvalue, value] = op_stat_cur.get_values()
+                found = True
+            self.assertEquals(found, True)
+        elif stats_expected == 'fast':
+            self.assertRaisesWithMessage(wiredtiger.WiredTigerError, lambda:
+                self.session.open_cursor('statistics:session', None, 'statistics=(all)'), msg)
+        else:
+            self.assertRaisesWithMessage(wiredtiger.WiredTigerError, lambda:
+                self.session.open_cursor('statistics:session', None, None), msg)
+            self.assertRaisesWithMessage(wiredtiger.WiredTigerError, lambda:
+                self.session.open_cursor('statistics:session', None, 'statistics=(all)'), msg)
+            self.assertRaisesWithMessage(wiredtiger.WiredTigerError, lambda:
+                self.session.open_cursor('statistics:session', None, 'statistics=(fast)'), msg)
+
+if __name__ == '__main__':
+    wttest.run()
