1b7333014ee58a0b08ed79c046bebb26f99e9412
Keith Bostic <keith.bostic@mongodb.com>
2018-11-26 08:58:33 -0500

    whitespace/typo

--- ext/compressors/lz4/lz4_compress.c
+++ ext/compressors/lz4/lz4_compress.c
@@ -70,8 +70,6 @@ typedef struct {
  *
  * The unused field is available for a mode flag if one is needed in the future,
  * we guarantee it's 0.
- *
- *
  */
 typedef struct {
 	uint32_t compressed_len;	/* True compressed length */
