55fe2cdb6e5a4f9f6648be00bcd49a2880efad97
Keith Bostic <keith.bostic@mongodb.com>
2018-12-06 16:39:46 -0500

    Minor logic rewrite to avoid clang-scan complaint: src/conn/conn_api.c:2618:34: error: this condition has identical branches [-Werror=duplicated-branches]

--- src/conn/conn_api.c
+++ src/conn/conn_api.c
@@ -2614,8 +2614,9 @@ wiredtiger_open(const char *home, WT_EVENT_HANDLER *event_handler,
 
 	WT_ERR(__wt_config_gets(session, cfg, "buffer_alignment", &cval));
 	if (cval.val == -1)
-		conn->buffer_alignment =
-		    (conn->direct_io == 0) ? 0 : WT_BUFFER_ALIGNMENT_DEFAULT;
+		conn->buffer_alignment = 0;
+		if (conn->direct_io != 0)
+			conn->buffer_alignment = WT_BUFFER_ALIGNMENT_DEFAULT;
 	else
 		conn->buffer_alignment = (size_t)cval.val;
 #ifndef HAVE_POSIX_MEMALIGN
