76230120cf1ead3510a8abbbe2eefe03dca8c109
Don Anderson <dda@mongodb.com>
2018-12-06 19:49:41 -0500

    WT-4438 Use accurate statistics for cursor cache totals in connection. (#4399)
    (cherry picked from commit dfd4f021aca05e62200454c564ecdfce1924478c)

--- src/include/stat.h
+++ src/include/stat.h
@@ -454,6 +454,7 @@ struct __wt_connection_stats {
 	int64_t fsync_io;
 	int64_t read_io;
 	int64_t write_io;
+	int64_t cursor_cached_count;
 	int64_t cursor_cache;
 	int64_t cursor_create;
 	int64_t cursor_insert;
@@ -471,8 +472,8 @@ struct __wt_connection_stats {
 	int64_t cursor_sweep_examined;
 	int64_t cursor_sweep;
 	int64_t cursor_update;
-	int64_t cursors_cached;
 	int64_t cursor_reopen;
+	int64_t cursor_open_count;
 	int64_t cursor_truncate;
 	int64_t dh_conn_handle_count;
 	int64_t dh_sweep_ref;
@@ -585,7 +586,6 @@ struct __wt_connection_stats {
 	int64_t rec_page_delete;
 	int64_t rec_split_stashed_bytes;
 	int64_t rec_split_stashed_objects;
-	int64_t session_cursor_open;
 	int64_t session_open;
 	int64_t session_query_ts;
 	int64_t session_table_alter_fail;
@@ -800,6 +800,7 @@ struct __wt_dsrc_stats {
 	int64_t cursor_insert;
 	int64_t cursor_modify;
 	int64_t cursor_next;
+	int64_t cursor_open_count;
 	int64_t cursor_prev;
 	int64_t cursor_remove;
 	int64_t cursor_reserve;
@@ -822,9 +823,7 @@ struct __wt_dsrc_stats {
 	int64_t rec_pages;
 	int64_t rec_pages_eviction;
 	int64_t rec_page_delete;
-	int64_t session_cursors_cached;
 	int64_t session_compact;
-	int64_t session_cursor_open;
 	int64_t txn_update_conflict;
 };
 
