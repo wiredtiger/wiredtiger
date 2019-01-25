8edf90b1f830fa5c5ad05fca4470aa791759c15f
Don Anderson <dda@mongodb.com>
2019-01-25 10:11:20 -0500

    Put .libs directory into LD_LIBRARY_PATH so it can be found by wt.

--- test/suite/run.py
+++ test/suite/run.py
@@ -65,6 +65,15 @@ else:
 sys.path.insert(1, os.path.join(wt_builddir, 'lang', 'python'))
 sys.path.insert(1, os.path.join(wt_disttop, 'lang', 'python'))
 
+# If we built with libtool, explicitly put its install directory in our library
+# search path. This only affects library loading for subprocesses, like 'wt'.
+libsdir = os.path.join(wt_builddir, '.libs')
+if os.path.isdir(libsdir):
+    env = dict(os.environ)
+    env['LD_LIBRARY_PATH'] = libsdir
+    if sys.platform == "darwin":
+        env['DYLD_LIBRARY_PATH'] = libsdir
+
 # Add all 3rd party directories: some have code in subdirectories
 for d in os.listdir(wt_3rdpartydir):
     for subdir in ('lib', 'python', ''):
