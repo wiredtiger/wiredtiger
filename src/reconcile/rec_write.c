8deb43dd5794d535b02227c0e7b4cbf39de3dd6c
nehakhatri5 <neha.khatri@mongodb.com>
2019-02-28 09:23:34 +1100

    Longline.

--- src/reconcile/rec_write.c
+++ src/reconcile/rec_write.c
@@ -1491,7 +1491,8 @@ __rec_upd_select(WT_SESSION_IMPL *session, WT_RECONCILE *r, WT_INSERT *ins,
 			 * WT_ASSERT(session,
 			 *    first_ts_upd->prepare_state ==
 			 *    WT_PREPARE_INPROGRESS ||
-			 *    first_ts_upd->start_ts <= first_ts_upd->durable_ts);
+			 *    first_ts_upd->start_ts <=
+			 *    first_ts_upd->durable_ts);
 			 */
 			if (r->unstable_timestamp < first_ts_upd->start_ts)
 				r->unstable_timestamp = first_ts_upd->start_ts;
