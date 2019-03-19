de5d67beb29cf45dfd49e578a87caf8b2559b1d8
Alex Cameron <alex.cameron@10gen.com>
2019-03-19 05:01:01 +0000

    Fix code generation placing a line break within an identifier.

--- dist/api_config.py
+++ dist/api_config.py
@@ -156,7 +156,8 @@ w = textwrap.TextWrapper(width=64, break_on_hyphens=False)
 w.wordsep_re = w.wordsep_simple_re = re.compile(r'(,)')
 
 # TextWrapper that wraps at whitespace.
-ws = textwrap.TextWrapper(width=64, break_on_hyphens=False)
+ws = textwrap.TextWrapper(width=64, break_on_hyphens=False,
+                          break_long_words=False)
 
 def checkstr(c):
     '''Generate the function reference and JSON string used by __wt_config_check
