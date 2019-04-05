0ac5f911bcbe17ef3c0d580d003a7adb5cb84441
Vamsi Krishna <vamsi.krishna@mongodb.com>
2019-04-05 16:49:13 +1100

    removed an empty line.

--- src/txn/txn.c
+++ src/txn/txn.c
@@ -835,7 +835,6 @@ __wt_txn_commit(WT_SESSION_IMPL *session, const char *cfg[])
 
 		WT_ASSERT(session,
 			   txn->prepare_timestamp <= txn->commit_timestamp);
-
 	} else {
 		if (F_ISSET(txn, WT_TXN_HAS_TS_PREPARE))
 			WT_ERR_MSG(session, EINVAL,
