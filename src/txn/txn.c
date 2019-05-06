9d2ccf6daea3d6455ddb5605ae2b4bccec4ecf3f
Luke Pearson <luke.pearson@mongodb.com>
2019-05-06 14:31:11 +1000

    WT-4643 Addressing review comments

--- src/txn/txn.c
+++ src/txn/txn.c
@@ -863,42 +863,40 @@ __wt_txn_commit(WT_SESSION_IMPL *session, const char *cfg[])
 	 * the stable/oldest timestamps can be moved beyond the commit/durable
 	 * timestamps after they are set.
 	 */
-	if (txn_global->has_stable_timestamp ||
-	    txn_global->has_oldest_timestamp) {
-		if (F_ISSET(txn, WT_TXN_HAS_TS_COMMIT)) {
-			if (txn->first_commit_timestamp <
-			    txn_global->oldest_timestamp) {
-				__wt_timestamp_to_string(
-				    txn->first_commit_timestamp, ts_string[0]);
-				__wt_timestamp_to_string(
-				    txn_global->oldest_timestamp, ts_string[1]);
-				WT_ERR_MSG(session, EINVAL,
-				    "commit timestamp %s less than oldest "
-				    "timestamp %s", ts_string[0], ts_string[1]);
-			}
-			if (!prepare && txn->first_commit_timestamp <=
-			    txn_global->stable_timestamp) {
-				__wt_timestamp_to_string(
-				    txn->first_commit_timestamp, ts_string[0]);
-				__wt_timestamp_to_string(
-				    txn_global->stable_timestamp, ts_string[1]);
-				WT_ERR_MSG(session, EINVAL,
-				    "commit timestamp %s less than or "
-				    "equal to stable timestamp %s",
-				    ts_string[0], ts_string[1]);
-			}
+	if (F_ISSET(txn, WT_TXN_HAS_TS_COMMIT)) {
+		if (txn_global->has_oldest_timestamp &&
+		    txn->first_commit_timestamp <
+		    txn_global->oldest_timestamp) {
+			__wt_timestamp_to_string(
+			    txn->first_commit_timestamp, ts_string[0]);
+			__wt_timestamp_to_string(
+			    txn_global->oldest_timestamp, ts_string[1]);
+			WT_ERR_MSG(session, EINVAL,
+			    "commit timestamp %s less than oldest "
+			    "timestamp %s", ts_string[0], ts_string[1]);
 		}
-		if (prepare && txn->durable_timestamp <=
+		if (!prepare && txn->first_commit_timestamp <=
 		    txn_global->stable_timestamp) {
 			__wt_timestamp_to_string(
-			    txn->durable_timestamp, ts_string[0]);
+			    txn->first_commit_timestamp, ts_string[0]);
 			__wt_timestamp_to_string(
 			    txn_global->stable_timestamp, ts_string[1]);
 			WT_ERR_MSG(session, EINVAL,
-			    "durable timestamp %s less than or "
+			    "commit timestamp %s less than or "
 			    "equal to stable timestamp %s",
 			    ts_string[0], ts_string[1]);
 		}
+	}
+	if (prepare && txn_global->has_stable_timestamp &&
+	    txn->durable_timestamp <= txn_global->stable_timestamp) {
+		__wt_timestamp_to_string(
+		    txn->durable_timestamp, ts_string[0]);
+		__wt_timestamp_to_string(
+		    txn_global->stable_timestamp, ts_string[1]);
+		WT_ERR_MSG(session, EINVAL,
+		    "durable timestamp %s less than or "
+		    "equal to stable timestamp %s",
+		    ts_string[0], ts_string[1]);
 	}
 
 	WT_ERR(__txn_commit_timestamps_assert(session));
