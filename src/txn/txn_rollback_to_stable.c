7f71e89622ae7a397677f7cc64a536afb145415e
Michael Cahill <michael.cahill@mongodb.com>
2019-05-13 16:09:05 +1000

    Change rollback_to_stable to update in-cache lookaside tracking.
    All updates newer than the rollback timestamp will be removed from the
    lookaside table, so we should also remove references to them from
    ref->page_las.  Without this, the page_las structs stay active for much
    longer than necessary, and potentially block the connection from
    closing.
    
    Also, track directly whether lookaside history has been read back into
    cache.  This simplifies tracking which page_las structures are still
    required.

--- src/txn/txn_rollback_to_stable.c
+++ src/txn/txn_rollback_to_stable.c
@@ -248,15 +248,29 @@ __txn_abort_newer_updates(
 	 */
 	local_read = false;
 	read_flags = WT_READ_WONT_NEED;
-	if (ref->page_las != NULL && ref->page_las->skew_newest &&
-	    rollback_timestamp < ref->page_las->unstable_durable_timestamp) {
-		/* Make sure get back a page with history, not limbo page */
-		WT_ASSERT(session,
-		    !F_ISSET(&session->txn, WT_TXN_HAS_SNAPSHOT));
-		WT_RET(__wt_page_in(session, ref, read_flags));
-		WT_ASSERT(session, ref->state != WT_REF_LIMBO &&
-		    ref->page != NULL && __wt_page_is_modified(ref->page));
-		local_read = true;
+	if (ref->page_las != NULL) {
+		if (ref->page_las->skew_newest && rollback_timestamp <
+		    ref->page_las->unstable_durable_timestamp) {
+			/*
+			 * Make sure get back a page with history, not limbo
+			 * page.
+			 */
+			WT_ASSERT(session,
+			    !F_ISSET(&session->txn, WT_TXN_HAS_SNAPSHOT));
+			WT_RET(__wt_page_in(session, ref, read_flags));
+			WT_ASSERT(session, ref->state != WT_REF_LIMBO &&
+			    ref->page != NULL &&
+			    __wt_page_is_modified(ref->page));
+			local_read = true;
+		}
+		if (ref->page_las->max_timestamp > rollback_timestamp)
+			ref->page_las->max_timestamp = rollback_timestamp;
+		if (ref->page_las->unstable_durable_timestamp >
+		    rollback_timestamp)
+			ref->page_las->unstable_durable_timestamp =
+			    rollback_timestamp;
+		if (ref->page_las->unstable_timestamp > rollback_timestamp)
+			ref->page_las->unstable_timestamp = rollback_timestamp;
 	}
 
 	/* Review deleted page saved to the ref */
