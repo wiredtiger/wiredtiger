a4861e0b9097a3ed9fc3092672c74877943d23a7
Michael Cahill <michael.cahill@mongodb.com>
2019-06-03 12:10:43 +1000

    Move import test to csuite

--- test/csuite/import/smoke.sh
+++ test/csuite/import/smoke.sh
@@ -11,7 +11,7 @@ format()
 {
 	rm -rf RUNDIR || exit 1
 
-	../format/t -1q -c ../format/CONFIG.stress \
+	$top_builddir/test/format/t -1q -c $top_srcdir/test/format/CONFIG.stress \
 	    backups=0 \
 	    checkpoints=1 \
 	    data_source=file \
@@ -26,11 +26,11 @@ verify()
 {
 	# Import and verify the object.
 	EXT="extensions=[\
-	../../ext/encryptors/rotn/.libs/libwiredtiger_rotn.so,\
-	../../ext/collators/reverse/.libs/libwiredtiger_reverse_collator.so]"
+	$top_builddir/ext/encryptors/rotn/.libs/libwiredtiger_rotn.so,\
+	$top_builddir/ext/collators/reverse/.libs/libwiredtiger_reverse_collator.so]"
 	egrep 'encryption=none' RUNDIR/CONFIG > /dev/null ||
 	    EXT="encryption=(name=rotn,keyid=7),$EXT"
-	wt="../../wt"
+	wt="$top_builddir/wt"
 
 	rm -rf FOREIGN && mkdir FOREIGN || exit 1
 	$wt -C "$EXT" -h FOREIGN create file:xxx || exit 1
