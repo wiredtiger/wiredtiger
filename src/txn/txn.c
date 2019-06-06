9d31152bd4898e681f5a97dcd8e00447c4218eb3
Luke Pearson <luke.pearson@mongodb.com>
2019-06-06 10:06:18 +1000

    WT-4580 Move back to reordered if statements

--- src/txn/txn.c
+++ src/txn/txn.c
@@ -529,14 +529,11 @@ __wt_txn_config(WT_SESSION_IMPL *session, const char *cfg[])
 
 	/* Check if prepared updates should be ignored during reads. */
 	WT_RET(__wt_config_gets_def(session, cfg, "ignore_prepare", 0, &cval));
-	if (cval.val)
-	{
-		if (cval.len > 0 &&
-		    WT_STRING_MATCH("force", cval.str, cval.len))
-			F_SET(txn, WT_TXN_IGNORE_PREPARE);
-		else
-			F_SET(txn, WT_TXN_IGNORE_PREPARE | WT_TXN_READONLY);
-	}
+	if (cval.len > 0 &&
+	    WT_STRING_MATCH("force", cval.str, cval.len))
+		F_SET(txn, WT_TXN_IGNORE_PREPARE);
+	else if (cval.val)
+		F_SET(txn, WT_TXN_IGNORE_PREPARE | WT_TXN_READONLY);
 
 	/*
 	 * Check if the prepare timestamp and the commit timestamp of a
