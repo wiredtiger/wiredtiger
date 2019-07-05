f41af7a9a51fb6a2017f44911421d203b0078f55
Alex Cameron <alex.cameron@10gen.com>
2019-07-05 06:17:20 +0000

    Free durable timestamp lock properly.

--- src/txn/txn.c
+++ src/txn/txn.c
@@ -1599,6 +1599,7 @@ __wt_txn_global_destroy(WT_SESSION_IMPL *session)
 	__wt_rwlock_destroy(session, &txn_global->rwlock);
 	__wt_rwlock_destroy(session, &txn_global->commit_timestamp_rwlock);
 	__wt_rwlock_destroy(session, &txn_global->read_timestamp_rwlock);
+	__wt_rwlock_destroy(session, &txn_global->durable_timestamp_rwlock);
 	__wt_rwlock_destroy(session, &txn_global->nsnap_rwlock);
 	__wt_rwlock_destroy(session, &txn_global->visibility_rwlock);
 	__wt_free(session, txn_global->states);
