98bfdd27847ed909d13d67609f561c48107cf873
Alex Cameron <alex.cameron@10gen.com>
2019-07-21 11:16:32 +0000

    More code fixes.

--- src/os_win/os_once.c
+++ src/os_win/os_once.c
@@ -12,7 +12,7 @@
  * __wt_init_once_callback --
  *	Global initialization, run once.
  */
-BOOL CALLBACK _wt_init_once_callback(
+BOOL CALLBACK __wt_init_once_callback(
     _Inout_      PINIT_ONCE InitOnce,
     _Inout_opt_  PVOID Parameter,
     _Out_opt_    PVOID *Context
@@ -38,6 +38,6 @@ __wt_once(void (*init_routine)(void))
 	INIT_ONCE once_control = INIT_ONCE_STATIC_INIT;
 	PVOID lpContext = NULL;
 
-	return !InitOnceExecuteOnce(&once_control, &_wt_init_once_callback,
+	return !InitOnceExecuteOnce(&once_control, &__wt_init_once_callback,
 	    init_routine, lpContext);
 }
