811228303913163a4439c46d6359ff11f9dbacd2
Alex Cameron <ascottcameron@gmail.com>
2019-09-03 07:17:11 +0000

    Revert sweep changes.

--- src/cache/cache_las.c
+++ src/cache/cache_las.c
@@ -998,14 +998,14 @@ __wt_las_sweep(WT_SESSION_IMPL *session)
     uint32_t las_btree_id, saved_btree_id, session_flags;
     uint8_t prepare_state, upd_type;
     int cmp, notused;
-    bool local_txn, locked;
+    bool local_txn, locked, removing_key_block;
 
     cache = S2C(session)->cache;
     cursor = NULL;
     sweep_key = &cache->las_sweep_key;
     remove_cnt = 0;
     session_flags = 0; /* [-Werror=maybe-uninitialized] */
-    local_txn = locked = false;
+    local_txn = locked = removing_key_block = false;
 
     WT_RET(__wt_scr_alloc(session, 0, &saved_key));
     saved_btree_id = 0;
@@ -1085,11 +1085,12 @@ __wt_las_sweep(WT_SESSION_IMPL *session)
         }
 
         /*
-         * Stop if we've processed enough entries either all we wanted or enough and there is a
-         * reader waiting.
+         * We only want to break between key blocks. Stop if we've processed enough entries either
+         * all we wanted or enough and there is a reader waiting and we're on a key boundary.
          */
         ++visit_cnt;
-        if (cnt == 0 || (visit_cnt > WT_LAS_SWEEP_ENTRIES && cache->las_reader))
+        if (!removing_key_block &&
+          (cnt == 0 || (visit_cnt > WT_LAS_SWEEP_ENTRIES && cache->las_reader)))
             break;
         if (cnt > 0)
             --cnt;
@@ -1107,6 +1108,10 @@ __wt_las_sweep(WT_SESSION_IMPL *session)
             WT_ERR(cursor->remove(cursor));
             ++remove_cnt;
             saved_key->size = 0;
+            /*
+             * Allow sweep to break while removing entries from a dead file.
+             */
+            removing_key_block = false;
             continue;
         }
 
@@ -1117,12 +1122,13 @@ __wt_las_sweep(WT_SESSION_IMPL *session)
           cursor->get_value(cursor, &durable_timestamp, &prepare_state, &upd_type, &las_value));
 
         /*
-         * Check to see if the btree id or key has changed this iteration, and if they have, setup
-         * context for safely removing obsolete updates.
+         * Check to see if the page or key has changed this iteration,
+         * and if they have, setup context for safely removing obsolete
+         * updates.
          *
-         * It's important to check for btree id boundaries explicitly because it is possible for the
-         * same key for a different btree to be positioned contiguously in lookaside. See WT-3982
-         * for details.
+         * It's important to check for page boundaries explicitly
+         * because it is possible for the same key to be at the start
+         * of the next block. See WT-3982 for details.
          */
         if (las_btree_id != saved_btree_id || saved_key->size != las_key.size ||
           memcmp(saved_key->data, las_key.data, las_key.size) != 0) {
@@ -1157,18 +1163,22 @@ __wt_las_sweep(WT_SESSION_IMPL *session)
              *  * The first entry is globally visible.
              *  * The entry wasn't from a prepared transaction.
              */
-            if (upd_type != WT_UPDATE_BIRTHMARK ||
-              !__wt_txn_visible_all(session, las_txnid, durable_timestamp) ||
-              prepare_state == WT_PREPARE_INPROGRESS)
-                continue;
-        } else
+            if (upd_type == WT_UPDATE_BIRTHMARK &&
+              __wt_txn_visible_all(session, las_txnid, durable_timestamp) &&
+              prepare_state != WT_PREPARE_INPROGRESS)
+                removing_key_block = true;
+            else
+                removing_key_block = false;
+        }
+
+        if (!removing_key_block)
             continue;
 
         __wt_verbose(session, WT_VERB_LOOKASIDE_ACTIVITY,
           "Sweep removing lookaside entry with "
           "btree ID: %" PRIu32 " saved key size: %" WT_SIZET_FMT " record type: %" PRIu8
-          " timestamp: %" PRIu64 " transaction ID: %" PRIu64,
-          las_btree_id, saved_key->size, upd_type, las_timestamp, las_txnid);
+          " transaction ID: %" PRIu64,
+          las_btree_id, saved_key->size, upd_type, las_txnid);
         WT_ERR(cursor->remove(cursor));
         ++remove_cnt;
     }
