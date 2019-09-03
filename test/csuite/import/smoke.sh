e1e1089b9a5d2b77e77ebfd6d457e1c765b816f1
Susan LoVerso <sue@mongodb.com>
2019-09-03 14:24:53 -0400

    Execute a no-op command to workaround memory leak.

--- test/csuite/import/smoke.sh
+++ test/csuite/import/smoke.sh
@@ -1,10 +1,11 @@
-#! /bin/dash
-# XXX - This should be /bin/sh but there is a bug in bash on many Linux systems that results
-# in a memory leak detection on immediate exit after a 'test' statement like below.
-# This is reported in https://bugzilla.redhat.com/show_bug.cgi?id=1746101
-# When that bug is fixed, this script should revert back on /bin/sh.
-
+#! /bin/sh
 set -e
+
+# XXX - The following line should not be necessary. But without it, the immediate
+# test and exit reveals a bug and memory leak in /bin/bash when run with valgrind.
+# That leak is reported in https://bugzilla.redhat.com/show_bug.cgi?id=1746101
+# When that bug is fixed, this script can remove the line
+date > /dev/null
 
 # Bypass this test for slow machines, valgrind
 test "$TESTUTIL_SLOW_MACHINE" = "1" && exit 0
