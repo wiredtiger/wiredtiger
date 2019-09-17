27914ac302b0bcba254980fc79153f0edc0189a1
Alex Cameron <alex.cameron@10gen.com>
2019-09-17 19:19:00 +0000

    Allow xray binary to be specified via variables.

--- bench/wtperf/runners/wtperf_xray.sh
+++ bench/wtperf/runners/wtperf_xray.sh
@@ -18,19 +18,31 @@ export XRAY_OPTIONS="patch_premain=true xray_mode=xray-basic verbosity=1"
 wtperf_out=$(./wtperf -O "$@")
 
 xray_log=$(ls xray-log.wtperf.*)
-num_logs=$(wc -w "$xray_log")
+num_logs=$(echo "$xray_log" | wc -w)
 if test "$num_logs" -ne "1"; then
     echo "$0: detected more than one xray log"
     exit 1
 fi
 
-llvm-xray-8 account $xray_log -top=10 -sortorder=dsc -instr_map ./wtperf > wtperf_account.txt
+if test -z "$XRAY_BINARY"; then
+    xray_bin="llvm-xray"
+    echo "$0: XRAY_BINARY is unset, defaulting to $xray_bin"
+else
+    xray_bin="$XRAY_BINARY"
+fi
+
+$xray_bin account $xray_log -top=10 -sortorder=dsc -instr_map ./wtperf > wtperf_account.txt
 
 # Use the -per-thread-stacks option to get the top 10 stacks for each thread.
 # We could use -aggregate-threads flag here so get the top stacks for all threads (omitting duplicates).
-llvm-xray-8 stack $xray_log -instr_map -per-thread-stacks ./wtperf > wtperf_stack.txt
+$xray_bin stack -per-thread-stacks $xray_log -instr_map ./wtperf > wtperf_stack.txt
 
 # Generate a DOT graph.
-llvm-xray-8 graph $xray_log -m ./wtperf -color-edges=sum -edge-label=sum | unflatten -f -l10 | dot -Tsvg -o wtperf_graph.svg
-llvm-xray-8 convert -symbolize -instr_map=./wtperf -output-format=trace_event $xray_log | gzip > wtperf_trace.txt.gz
-llvm-xray-8 stack $xray_log -instr_map ./wtperf -stack-format=flame -aggregation-type=time -all-stacks | ~/work/FlameGraph/flamegraph.pl > wtperf_flame.svg
+$xray_bin graph $xray_log -m ./wtperf -color-edges=sum -edge-label=sum | unflatten -f -l10 | dot -Tsvg -o wtperf_graph.svg
+$xray_bin convert -symbolize -instr_map=./wtperf -output-format=trace_event $xray_log | gzip > wtperf_trace.txt.gz
+
+if test -z "$FLAME_GRAPH_PATH"; then
+    echo "$0: FLAME_GRAPH_PATH is unset, skipping flame graph generation"
+else
+    $xray_bin stack $xray_log -instr_map ./wtperf -stack-format=flame -aggregation-type=time -all-stacks | "$FLAME_GRAPH_PATH" > wtperf_flame.svg
+fi
