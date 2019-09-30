e9331cdf431efeb75fa9222154b61fdb4c7882aa
Alex Cameron <alex.cameron@10gen.com>
2019-09-30 02:23:28 +0000

    Add comments and conditional to check existence of wtperf.

--- bench/wtperf/runners/wtperf_xray.sh
+++ bench/wtperf/runners/wtperf_xray.sh
@@ -9,18 +9,36 @@
 # wtperf_xray.sh <wtperf-config-file> [wtperf other args]
 #
 # Environment variables
-# XRAY_BINARY: The binary to use to inspect the xray log. (default: llvm-xray)
-# FLAME_GRAPH_PATH: The path to your copy of Brendan Gregg's FlameGraph script.
+# XRAY_BINARY --
+#	The binary to use to inspect the xray log. (default: llvm-xray)
+# FLAME_GRAPH_PATH --
+#	The path to your copy of Brendan Gregg's FlameGraph script. (optional)
 #
 # When this is complete you can find information in the following files:
-# XXX
+# wtperf_account.txt --
+#	The top 10 functions where the workload is spending the most time along
+#	with a count, min, max and some percentiles for each one.
+# wtperf_stacks.txt --
+#	The top 10 stack traces where the workload is spending the most time.
+#	This calculation is done separately per thread.
+# wtperf_graph.svg --
+#	A function call graph showing what functions call each other. The edges
+#	are labelled and coloured proportionally to represent the ratio of time
+#	spent in each function call.
+# wtperf_flame.svg --
+#	A graph visualising stack traces and the time spent within each stack
+#	frame. If FLAME_GRAPH_PATH is not specified, this graph won't be
+#	generated.
 #
 if test "$#" -lt "1"; then
 	echo "$0: must specify wtperf configuration to run"
 	exit 1
 fi
 
-# XXX add conditional to check existence of ./wtperf here.
+if ! test -f ./wtperf; then
+	echo "$0: could not find wtperf in current working directory"
+	exit 1
+fi
 
 # Check symbols to ensure we've compiled with XRay.
 objdump_out=$(objdump -h -j xray_instr_map ./wtperf)
