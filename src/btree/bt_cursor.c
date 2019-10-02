6504f3c2da3b536d36e8cbf7a92feafe01549033
Keith Bostic <keith.bostic@mongodb.com>
2019-10-02 00:24:09 +0000

    Tighten up the test a little bit, read-uncommitted works as far as we know.

--- src/btree/bt_cursor.c
+++ src/btree/bt_cursor.c
@@ -84,13 +84,13 @@ __cursor_page_pinned(WT_CURSOR_BTREE *cbt, bool search_operation)
         return (false);
 
     /*
-     * XXX No fast-path searches outside of snapshot isolation. Underlying transactional functions
-     * called by the fast and slow path code handle transaction IDs differently, resulting in
-     * different search results at lower isolation. This doesn't make a difference for the update
-     * functions, but in the case of a search, we can see different results based on the cursor's
+     * XXX No fast-path searches at read-committed isolation. Underlying transactional functions
+     * called by the fast and slow path search code handle transaction IDs differently, resulting in
+     * different search results at read-committed isolation. This makes no difference for the update
+     * functions, but in the case of a search, we will see different results based on the cursor's
      * initial location. See WT-5134 for the details.
      */
-    if (search_operation && session->txn.isolation != WT_ISO_SNAPSHOT)
+    if (search_operation && session->txn.isolation == WT_ISO_READ_COMMITTED)
         return (false);
 
     /*
