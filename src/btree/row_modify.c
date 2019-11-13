f5d668166ee391fe9b9870a40a3fdc4cecd18e80
Chenhao Qu <chenhao.qu@mongodb.com>
2019-11-13 01:05:52 +0000

    WT-5136 Refactor ternary and add some comments

--- src/btree/row_modify.c
+++ src/btree/row_modify.c
@@ -330,10 +330,21 @@ __wt_update_obsolete_check(
             if (upd->start_ts != WT_TS_NONE && upd->start_ts >= oldest && upd->start_ts < stable)
                 ++upd_unstable;
         } else {
-            if (first == NULL && upd->type == WT_UPDATE_BIRTHMARK)
-                first = upd_visible_all_seen ? prev : upd;
-            else if (first == NULL && WT_UPDATE_DATA_VALUE(upd))
-                first = upd;
+            if (first == NULL) {
+                /*
+                 * If we have seen a globally visible update before the birthmark, the birthmark can
+                 * be discarded.
+                 */
+                if (upd_visible_all_seen && upd->type == WT_UPDATE_BIRTHMARK)
+                    first = prev;
+                /*
+                 * We cannot discard the birthemark if it is the first golbally visible update as
+                 * the previous updates can be aborted resulting the entire update chain being
+                 * removed.
+                 */
+                else if (upd->type == WT_UPDATE_BIRTHMARK || WT_UPDATE_DATA_VALUE(upd))
+                    first = upd;
+            }
 
             upd_visible_all_seen = true;
         }
