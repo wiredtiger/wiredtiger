2c668380ee516c71940927f966de32df316512dc
Susan LoVerso <sue@mongodb.com>
2019-12-06 16:30:16 -0500

    Remove unused field.

--- src/include/cursor.h
+++ src/include/cursor.h
@@ -37,9 +37,8 @@
  */
 #define WT_BLKINCR_MAX 2
 struct __wt_blkincr {
-    const char *id;
+    const char *id;        /* User's name for this backup. */
     const char *ckpt_name; /* Requires WT-5115. All checkpoints must be this name */
-    uint64_t time;
     void *data;
 /* AUTOMATIC FLAG VALUE GENERATION START */
 #define WT_BLKINCR_INUSE 0x1u /* This entry is active */
