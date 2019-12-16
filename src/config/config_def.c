2b7cf1e00dcc1be9b058537ba232cbd62214af5a
Luke Chen <luke.chen@mongodb.com>
2019-12-16 11:22:40 +1100

    Revert "WT-4968 remove all committed and connection level commit timestamps (#5040)" (#5053)
    This reverts commit c2e9914074b72356e81034fa8544959f9e2ad3f8.

--- src/config/config_def.c
+++ src/config/config_def.c
@@ -33,8 +33,9 @@ static const WT_CONFIG_CHECK confchk_WT_CONNECTION_open_session[] = {
 
 static const WT_CONFIG_CHECK confchk_WT_CONNECTION_query_timestamp[] = {
   {"get", "string", NULL,
-    "choices=[\"all_durable\",\"last_checkpoint\",\"oldest\","
-    "\"oldest_reader\",\"pinned\",\"recovery\",\"stable\"]",
+    "choices=[\"all_committed\",\"all_durable\","
+    "\"last_checkpoint\",\"oldest\",\"oldest_reader\",\"pinned\","
+    "\"recovery\",\"stable\"]",
     NULL, 0},
   {NULL, NULL, NULL, NULL, NULL, 0}};
 
@@ -145,6 +146,7 @@ static const WT_CONFIG_CHECK confchk_WT_CONNECTION_reconfigure[] = {
   {NULL, NULL, NULL, NULL, NULL, 0}};
 
 static const WT_CONFIG_CHECK confchk_WT_CONNECTION_set_timestamp[] = {
+  {"commit_timestamp", "string", NULL, NULL, NULL, 0},
   {"durable_timestamp", "string", NULL, NULL, NULL, 0}, {"force", "boolean", NULL, NULL, NULL, 0},
   {"oldest_timestamp", "string", NULL, NULL, NULL, 0},
   {"stable_timestamp", "string", NULL, NULL, NULL, 0}, {NULL, NULL, NULL, NULL, NULL, 0}};
@@ -822,9 +824,9 @@ static const WT_CONFIG_ENTRY config_entries[] = {{"WT_CONNECTION.add_collator", 
     confchk_WT_CONNECTION_reconfigure, 26},
   {"WT_CONNECTION.rollback_to_stable", "", NULL, 0}, {"WT_CONNECTION.set_file_system", "", NULL, 0},
   {"WT_CONNECTION.set_timestamp",
-    "durable_timestamp=,force=false,oldest_timestamp=,"
-    "stable_timestamp=",
-    confchk_WT_CONNECTION_set_timestamp, 4},
+    "commit_timestamp=,durable_timestamp=,force=false,"
+    "oldest_timestamp=,stable_timestamp=",
+    confchk_WT_CONNECTION_set_timestamp, 5},
   {"WT_CURSOR.close", "", NULL, 0},
   {"WT_CURSOR.reconfigure", "append=false,overwrite=true", confchk_WT_CURSOR_reconfigure, 2},
   {"WT_SESSION.alter",
