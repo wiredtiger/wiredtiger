30790e551b5fd05ab0a3e386cf8fd11419a28a0b
Chenhao Qu <chenhao.qu@mongodb.com>
2019-12-23 06:23:32 +0000

    optimize code

--- src/reconcile/rec_visibility.c
+++ src/reconcile/rec_visibility.c
@@ -466,7 +466,7 @@ check_original_value:
     if (upd_select->upd != NULL &&
       (upd_select->upd_saved ||
           (vpack != NULL && vpack->ovfl && vpack->raw != WT_CELL_VALUE_OVFL_RM)))
-        WT_ERR(__rec_append_orig_value(session, page, first_upd, vpack));
+        WT_ERR(__rec_append_orig_value(session, page, upd_select->upd, vpack));
 
 err:
     __wt_scr_free(session, &tmp);
