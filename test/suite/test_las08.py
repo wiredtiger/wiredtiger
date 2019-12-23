58d9f57baa161889c50880af7be4cb02d57f313d
Chenhao Qu <chenhao.qu@mongodb.com>
2019-12-23 02:51:42 +0000

    Add a new test and fix some minor issues

--- test/suite/test_las08.py
+++ test/suite/test_las08.py
@@ -40,14 +40,20 @@ class test_las08(wttest.WiredTigerTestCase):
     # Force a small cache.
     conn_config = 'cache_size=50MB,statistics=(fast)'
     session_config = 'isolation=snapshot'
-    uri = "table:test_las06"
+    uri = "table:test_las08"
     key_format_values = [
         ('column_store', dict(key_format='r')),
-        ('row_store', dict(key_format='i'))
+        ('row_store', dict(key_format='i')),
+        ('row_store', dict(key_format='S')),
     ]
     scenarios = make_scenarios(key_format_values)
 
-    def check_ckpt_las(self, expected_data_value, expected_las_value):
+    def create_key(self, i):
+        if self.key_format == 'S':
+            return str(i)
+        return i
+
+    def check_ckpt_las(self, expected_data_value, expected_las_value, expected_start_ts, expected_stop_ts):
         session = self.conn.open_session(self.session_config)
         session.checkpoint()
         # Check the data file value
@@ -57,12 +63,14 @@ class test_las08(wttest.WiredTigerTestCase):
         cursor.close()
         # Check the history file value
         cursor = session.open_cursor("file:WiredTigerLAS.wt", None, 'checkpoint=WiredTigerCheckpoint')
-        for _, key, _, _, _, _, _, _, type, value in cursor:
+        for _, _, start_ts, _, stop_ts, _, _, _, type, value in cursor:
             # No WT_UPDATE_TOMBSTONE in history store
             self.assertNotEqual(type, 5)
             # WT_UPDATE_STANDARD
             if (type == 4):
                 self.assertEqual(value, expected_las_value + '\x00')
+                self.assertEqual(start_ts, expected_start_ts)
+                self.assertEqual(stop_ts, expected_stop_ts)
         cursor.close()
         session.close()
 
@@ -81,21 +89,21 @@ class test_las08(wttest.WiredTigerTestCase):
         cursor = self.session.open_cursor(self.uri)
         self.session.begin_transaction()
         for i in range(1, 10000):
-            cursor[i] = value1
+            cursor[self.createKey(i)] = value1
         self.session.commit_transaction('commit_timestamp=' + timestamp_str(2))
 
         # Load another 5Mb of data with a later timestamp.
         self.session.begin_transaction()
         for i in range(1, 10000):
-            cursor[i] = value2
+            cursor[self.createKey(i)] = value2
         self.session.commit_transaction('commit_timestamp=' + timestamp_str(3))
 
         # Uncommitted changes
         self.session.begin_transaction()
         for i in range(1, 11):
-            cursor[i] = value3
+            cursor[self.createKey(i)] = value3
 
-        self.check_ckpt_las(value2, value1)
+        self.check_ckpt_las(value2, value1, 2, 3)
 
     @unittest.skip("Temporarily disabled until update restore also writes history to history store")
     def test_prepared_updates_not_written_to_lookaside(self):
@@ -118,16 +126,16 @@ class test_las08(wttest.WiredTigerTestCase):
         # Load another 5Mb of data with a later timestamp.
         self.session.begin_transaction()
         for i in range(1, 10000):
-            cursor[i] = value2
+            cursor[self.createKey(i)] = value2
         self.session.commit_transaction('commit_timestamp=' + timestamp_str(3))
 
         # Prepare some updates
         self.session.begin_transaction()
         for i in range(1, 11):
-            cursor[i] = value3
+            cursor[self.createKey(i)] = value3
         self.session.prepare_transaction('prepare_timestamp=' + timestamp_str(5))
 
-        self.check_ckpt_las(value2, value1)
+        self.check_ckpt_las(value2, value1, 2, 3)
 
     @unittest.skip("Temparorily disabled until reading time pairs from cells implemented")
     def test_write_newest_version_to_data_store(self):
@@ -143,16 +151,44 @@ class test_las08(wttest.WiredTigerTestCase):
         cursor = self.session.open_cursor(self.uri)
         self.session.begin_transaction()
         for i in range(1, 10000):
-            cursor[i] = value1
+            cursor[self.createKey(i)] = value1
         self.session.commit_transaction('commit_timestamp=' + timestamp_str(2))
 
         # Load another 5Mb of data with a later timestamp.
         self.session.begin_transaction()
         for i in range(1, 10000):
-            cursor[i] = value2
+            cursor[self.createKey(i)] = value2
         self.session.commit_transaction('commit_timestamp=' + timestamp_str(3))
 
-        self.check_ckpt_las(value2, value1)
+        self.check_ckpt_las(value2, value1, 2, 3)
+
+    @unittest.skip("Temparorily disabled until reading time pairs from cells implemented")
+    def test_write_deleted_version_to_data_store(self):
+        # Create a small table.
+        create_params = 'key_format={},value_format=S'.format(self.key_format)
+        self.session.create(self.uri, create_params)
+
+        value1 = 'a' * 500
+        value2 = 'b' * 500
+
+        # Load 5Mb of data.
+        self.conn.set_timestamp('oldest_timestamp=' + timestamp_str(1))
+        cursor = self.session.open_cursor(self.uri)
+        self.session.begin_transaction()
+        for i in range(1, 10000):
+            cursor[self.createKey(i)] = value1
+        self.session.commit_transaction('commit_timestamp=' + timestamp_str(2))
+
+        # Load another 5Mb of data with a later timestamp.
+        self.session.begin_transaction()
+        for i in range(1, 10000):
+            cursor[self.createKey(i)] = value2
+        self.session.commit_transaction('commit_timestamp=' + timestamp_str(3))
+
+        # Drop table
+        self.session.drop(self.uri, 'commit_timestamp=' + timestamp_str(4))
+
+        self.check_ckpt_las(value2, value1, 2, 3)
 
 if __name__ == '__main__':
     wttest.run()
