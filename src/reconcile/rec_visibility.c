77e1fd323bb9d1719b52cbbfd202ed97673cf0c0
Chenhao Qu <quchenhao@users.noreply.github.com>
2019-12-30 09:38:04 +1100

    WT-5361 Fix choosing aborted transaction to write to disk (#5076)

--- src/reconcile/rec_visibility.c
+++ src/reconcile/rec_visibility.c
@@ -323,6 +323,10 @@ __wt_rec_upd_select(WT_SESSION_IMPL *session, WT_RECONCILE *r, WT_INSERT *ins, v
                 upd_select->stop_ts = upd->start_ts;
             if (upd->txnid != WT_TXN_NONE)
                 upd_select->stop_txn = upd->txnid;
+            /* Ignore all the aborted transactions. */
+            while (upd->next != NULL && upd->next->txnid == WT_TXN_ABORTED)
+                upd = upd->next;
+            WT_ASSERT(session, upd->next == NULL || upd->next->txnid != WT_TXN_ABORTED);
             upd_select->upd = upd = upd->next;
         }
         if (upd != NULL) {
@@ -393,7 +397,7 @@ __wt_rec_upd_select(WT_SESSION_IMPL *session, WT_RECONCILE *r, WT_INSERT *ins, v
          * is the same as it was the previous reconciliation. Otherwise lookaside can end up with
          * two birthmark records in the same update chain.
          */
-        WT_ERR(__rec_append_orig_value(session, page, first_upd, vpack));
+        WT_ERR(__rec_append_orig_value(session, page, upd, vpack));
         upd_select->upd = NULL;
     }
 
@@ -463,7 +467,7 @@ check_original_value:
     if (upd_select->upd != NULL &&
       (upd_select->upd_saved ||
           (vpack != NULL && vpack->ovfl && vpack->raw != WT_CELL_VALUE_OVFL_RM)))
-        WT_ERR(__rec_append_orig_value(session, page, first_upd, vpack));
+        WT_ERR(__rec_append_orig_value(session, page, upd_select->upd, vpack));
 
 err:
     __wt_scr_free(session, &tmp);
