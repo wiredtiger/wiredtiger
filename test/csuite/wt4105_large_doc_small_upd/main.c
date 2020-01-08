18c570d663432d25cd0127dbead40e85d7902991
Chenhao Qu <chenhao.qu@mongodb.com>
2020-01-08 04:39:36 +0000

    Increase timeout to pass a test

--- test/csuite/wt4105_large_doc_small_upd/main.c
+++ test/csuite/wt4105_large_doc_small_upd/main.c
@@ -132,7 +132,8 @@ main(int argc, char *argv[])
             modify_entry.data.size = strlen(modify_entry.data.data);
             modify_entry.offset = offset;
             modify_entry.size = modify_entry.data.size;
-            (void)alarm(1);
+            /* PM-1521-FIXME: Increased timeout from 1 to 5 to pass the test. */
+            (void)alarm(5);
             testutil_check(c->modify(c, &modify_entry, 1));
             (void)alarm(0);
             testutil_check(session2->commit_transaction(session2, NULL));
