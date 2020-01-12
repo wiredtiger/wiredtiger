70be7c8d7bb477d68628e983826af281ba3f83d6
Chenhao Qu <chenhao.qu@mongodb.com>
2020-01-12 23:55:30 +0000

    Move traits implementations to different files

--- src/reconcile/rec_traits.c
+++ src/reconcile/rec_traits.c
@@ -1,56 +1,5 @@
 #include "wt_internal.h"
-
-static int __rec_page_col_fix(
-  WT_SESSION_IMPL *session, WT_RECONCILE *r, WT_REF *ref, WT_SALVAGE_COOKIE *salvage);
-static int __rec_page_col_int(
-  WT_SESSION_IMPL *session, WT_RECONCILE *r, WT_REF *ref, WT_SALVAGE_COOKIE *salvage);
-static int __rec_page_row_int(
-  WT_SESSION_IMPL *session, WT_RECONCILE *r, WT_REF *ref, WT_SALVAGE_COOKIE *salvage);
-
-/*
- * __rec_page_col_fix --
- *     Reconcile page for col fix.
- */
-static int
-__rec_page_col_fix(
-  WT_SESSION_IMPL *session, WT_RECONCILE *r, WT_REF *ref, WT_SALVAGE_COOKIE *salvage)
-{
-    if (salvage != NULL)
-        WT_RET(__wt_rec_col_fix_slvg(session, r, ref, salvage));
-    else
-        WT_RET(__wt_rec_col_fix(session, r, ref));
-    return (0);
-}
-
-/*
- * __rec_page_col_int --
- *     Reconcile page for col int.
- */
-static int
-__rec_page_col_int(
-  WT_SESSION_IMPL *session, WT_RECONCILE *r, WT_REF *ref, WT_SALVAGE_COOKIE *salvage)
-{
-    WT_DECL_RET;
-
-    (void)salvage; /* Unused */
-    WT_WITH_PAGE_INDEX(session, ret = __wt_rec_col_int(session, r, ref));
-    return ret;
-}
-
-/*
- * __rec_page_row_int --
- *     Reconcile page for row int.
- */
-static int
-__rec_page_row_int(
-  WT_SESSION_IMPL *session, WT_RECONCILE *r, WT_REF *ref, WT_SALVAGE_COOKIE *salvage)
-{
-    WT_DECL_RET;
-
-    (void)salvage; /* Unused */
-    WT_WITH_PAGE_INDEX(session, ret = __wt_rec_row_int(session, r, ref->page));
-    return ret;
-}
+#include "rec_traits_details.h"
 
 const struct __wt_rec_traits REC_PAGE_COL_FIX_TRAITS = {
   &CACHE_LAS_COL_FIX_TRAITS, __rec_page_col_fix};
