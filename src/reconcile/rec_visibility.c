f54e4203093ab238ea14457109f603bbead6fec9
Alex Cameron <alex.cameron@10gen.com>
2020-01-15 04:20:33 +0000

    Reenable assertion since it no longer fires.

--- src/reconcile/rec_visibility.c
+++ src/reconcile/rec_visibility.c
@@ -127,8 +127,7 @@ __rec_append_orig_value(
      * do nothing).
      */
     if (upd->type == WT_UPDATE_BIRTHMARK) {
-        /* FIXME-PM-1521: temporarily disable the assert until we figured out what is wrong */
-        /* WT_ASSERT(session, append->start_ts == upd->start_ts && append->txnid == upd->txnid); */
+        WT_ASSERT(session, append->start_ts == upd->start_ts && append->txnid == upd->txnid);
         append->next = upd->next;
     }
 
