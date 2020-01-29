25137d0b4b410ad62c182fac378af2717acb64e3
Tammy Bailey <tammy.bailey@mongodb.com>
2020-01-29 23:40:29 +0000

    PR changes.

--- test/format/format.sh
+++ test/format/format.sh
@@ -214,9 +214,9 @@ report_failure()
 
 	echo "$name: job in $dir failed"
 	echo "$name: $dir log:"
-	sed 's/^/    > /' < $log
+	sed 's/^/    /' < $log
 	echo "$name: $dir/CONFIG:"
-	sed 's/^/    > /' < $dir/CONFIG
+	sed 's/^/    /' < $dir/CONFIG
 }
 
 # Resolve/cleanup completed jobs.
