f184f099f7dce2d11bf623f69d1af92e2a96a15a
Sulabh Mahajan <sulabh.mahajan@mongodb.com>
2020-01-30 15:49:33 +1100

    Handle not found error in verify correctly

--- src/btree/bt_vrfy.c
+++ src/btree/bt_vrfy.c
@@ -284,6 +284,7 @@ __verify_key_hs(WT_SESSION_IMPL *session, WT_ITEM *key, WT_VSTUFF *vs)
     }
 
 err:
+    WT_ERR_NOTFOUND_OK(ret);
     __wt_scr_free(session, &hs_key);
     WT_TRET(__wt_hs_cursor_close(session, session_flags));
 
