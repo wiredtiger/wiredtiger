00db146272488e336a4d8935f3df7efad293c664
Andrew McMenemy <andrew.mcmenemy03@gmail.com>
2020-02-06 05:12:06 +0000

    undid usage change after discussing with jeremy

--- src/utilities/util_verify.c
+++ src/utilities/util_verify.c
@@ -118,6 +118,6 @@ usage(void)
       "verify %s\n",
       progname, usage_prefix,
       "[-d dump_address | dump_blocks | dump_history | dump_layout | "
-      "dump_offsets=#,# | dump_pages] [-S timestamp] [-h] uri");
+      "dump_offsets=#,# | dump_pages] [-h] [-S] uri");
     return (1);
 }
