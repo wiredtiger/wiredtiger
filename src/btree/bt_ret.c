a16321679557388d7a4cd13bf12b058163a345a6
Chenhao Qu <chenhao.qu@mongodb.com>
2020-02-07 11:36:17 +0000

    Run s_all

--- src/btree/bt_ret.c
+++ src/btree/bt_ret.c
@@ -186,8 +186,8 @@ __wt_value_return_upd(WT_CURSOR_BTREE *cbt, WT_UPDATE *upd)
     }
 
     /*
-     * If there's no full update, the base item is the on-page item.
-     * If the update is a tombstone, the base item is an empty item.
+     * If there's no full update, the base item is the on-page item. If the update is a tombstone,
+     * the base item is an empty item.
      */
     if (upd == NULL) {
         /*
