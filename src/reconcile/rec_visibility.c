180eaba6c2705f6b4327d7f4a3015c030d078fcc
Haribabu Kommi <haribabu.kommi@mongodb.com>
2020-02-10 06:42:47 +0000

    WT-5558 use durable timestamp from the selected update

--- src/reconcile/rec_visibility.c
+++ src/reconcile/rec_visibility.c
@@ -376,7 +376,8 @@ __wt_rec_upd_select(WT_SESSION_IMPL *session, WT_RECONCILE *r, WT_INSERT *ins, v
              * WT-5469.
              */
             if (upd->start_ts <= upd_select->stop_ts && upd->txnid <= upd_select->stop_txn) {
-                upd_select->durable_ts = upd_select->start_ts = upd->start_ts;
+                upd_select->start_ts = upd->start_ts;
+                upd_select->durable_ts = upd->durable_ts;
                 upd_select->start_txn = upd->txnid;
             }
 
