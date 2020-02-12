8f6aa055d489bb9df674102a89985961636c44e2
Tammy Bailey <tammy.bailey@mongodb.com>
2020-02-12 16:41:49 +1100

    WT-5424 Add statistics for rollback to stable (#5243)

--- test/suite/test_rollback_to_stable03.py
+++ test/suite/test_rollback_to_stable03.py
@@ -76,9 +76,9 @@ class test_rollback_to_stable01(test_rollback_to_stable_base):
         self.check(valuea, uri, nrows, 20)
 
         stat_cursor = self.session.open_cursor('statistics:', None, None)
-        calls = stat_cursor[stat.conn.txn_rollback_to_stable][2]
-        upd_aborted = (stat_cursor[stat.conn.txn_rollback_upd_aborted][2] +
-            stat_cursor[stat.conn.txn_rollback_hs_removed][2])
+        calls = stat_cursor[stat.conn.txn_rts][2]
+        upd_aborted = (stat_cursor[stat.conn.txn_rts_upd_aborted][2] +
+            stat_cursor[stat.conn.txn_rts_hs_removed][2])
         stat_cursor.close()
         self.assertEqual(calls, 1)
         self.assertTrue(upd_aborted == nrows * 2)
