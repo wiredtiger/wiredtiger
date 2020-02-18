e69f354428534ad3e64c8bdabfead28f61b2bb64
Alex Cameron <alex.cameron@10gen.com>
2020-02-18 15:35:40 +1100

    WT-5610 Fix assertion for reconciling fixed length column store (#5278)

--- src/reconcile/rec_write.c
+++ src/reconcile/rec_write.c
@@ -311,11 +311,12 @@ __rec_write_page_status(WT_SESSION_IMPL *session, WT_RECONCILE *r)
             S2C(session)->modified = true;
 
         /*
-         * Eviction should only be here if following the history store or in-memory eviction path.
-         *
-         * PM-1521-FIXME: What will we do for in memory database?
+         * Eviction should only be here if allowing writes to history store or in the in-memory
+         * eviction case. Otherwise, we must be reconciling a fixed length column store page (which
+         * does not allow history store content).
          */
-        WT_ASSERT(session, !F_ISSET(r, WT_REC_EVICT) || F_ISSET(r, WT_REC_HS | WT_REC_IN_MEMORY));
+        WT_ASSERT(session, !F_ISSET(r, WT_REC_EVICT) ||
+            (F_ISSET(r, WT_REC_HS | WT_REC_IN_MEMORY) || page->type == WT_PAGE_COL_FIX));
 
         /*
          * We have written the page, but something prevents it from being evicted. If we wrote the
