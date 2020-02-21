cc5bafdda488cb0297f606bb9f47c12bec62c3be
Sulabh Mahajan <sulabh.mahajan@mongodb.com>
2020-02-21 14:12:28 +1100

    Add todo, check URI is not provided when verfying history store

--- src/session/session_api.c
+++ src/session/session_api.c
@@ -1579,6 +1579,10 @@ __session_verify(WT_SESSION *wt_session, const char *uri, const char *config)
     /* Block out checkpoints to avoid spurious EBUSY errors. */
     WT_ERR(__wt_config_gets(session, cfg, "history_store", &cval));
     if (cval.val == true) {
+        /* Can't give a URI with history store verification. */
+        if (uri != NULL)
+            WT_ERR_MSG(session, EINVAL, "URI not applicable when verifying the history store");
+
         WT_WITH_CHECKPOINT_LOCK(session,
           WT_WITH_SCHEMA_LOCK(session, ret = __wt_verify_history_store_tree(session, NULL)));
     } else {
@@ -1586,6 +1590,7 @@ __session_verify(WT_SESSION *wt_session, const char *uri, const char *config)
           WT_WITH_SCHEMA_LOCK(session, ret = __wt_schema_worker(session, uri, __wt_verify, NULL,
                                          cfg, WT_DHANDLE_EXCLUSIVE | WT_BTREE_VERIFY)));
         WT_ERR(ret);
+        /* TODO: WT-5643 Add history store verification for non file URI */
         if (WT_PREFIX_MATCH(uri, "file:"))
             WT_WITH_CHECKPOINT_LOCK(session,
               WT_WITH_SCHEMA_LOCK(session, ret = __wt_verify_history_store_tree(session, uri)));
