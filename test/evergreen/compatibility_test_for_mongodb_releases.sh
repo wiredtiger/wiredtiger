1163d422670dcd09a05643948a8da4ba3f4a6beb
Keith Bostic <keith.bostic@mongodb.com>
2020-03-23 19:12:05 +0000

    Switch from testing tagged releases to testing branch releases so we catch problems sooner.

--- test/evergreen/compatibility_test_for_mongodb_releases.sh
+++ test/evergreen/compatibility_test_for_mongodb_releases.sh
@@ -5,14 +5,6 @@
 
 set -e
 
-###########################################################################
-# Return the most recent version of a tagged release.
-###########################################################################
-get_tagged_release()
-{
-        echo "$(git tag | grep "^mongodb-$1.[0-9]" | sort -V | sed -e '$p' -e d)"
-}
-
 #############################################################
 # build_release:
 #       arg1: release
@@ -100,35 +92,27 @@ top="test-compatibility-run"
 rm -rf "$top" && mkdir "$top"
 cd "$top"
 
-# Build a release with a known name, then use it to figure out tagged release names.
+# Build the releases.
+(build_release mongodb-3.4)
+(build_release mongodb-3.6)
+(build_release mongodb-4.0)
+(build_release mongodb-4.2)
+(build_release mongodb-4.4)
 (build_release "develop")
 
-# Figure out the names of the releases we'll compare.
-cd develop
-rel34="$(get_tagged_release "3.4")"
-rel36="$(get_tagged_release "3.6")"
-rel40="$(get_tagged_release "4.0")"
-#rel42="$(get_tagged_release "4.2")"    XXX cannot handle 4.4 formats yet.
-rel42="mongodb-4.2"
-cd ..
-
-# Build the rest of the releases.
-(build_release "$rel34")
-(build_release "$rel36")
-(build_release "$rel40")
-(build_release "$rel42")
-
 # Run format in each release for supported access methods.
-(run_format "$rel34" "fix row var")
-(run_format "$rel36" "fix row var")
-(run_format "$rel40" "fix row var")
-(run_format "$rel42" "fix row var")
+(run_format mongodb-3.4 "fix row var")
+(run_format mongodb-3.6 "fix row var")
+(run_format mongodb-4.0 "fix row var")
+(run_format mongodb-4.2 "fix row var")
+(run_format mongodb-4.4 "row")
 (run_format "develop" "row")
 
 # Verify backward compatibility for supported access methods.
-(verify_backward $rel36 "$rel34" "fix row var")
-(verify_backward $rel40 "$rel36" "fix row var")
-(verify_backward $rel42 "$rel40" "fix row var")
-(verify_backward "develop" "$rel42" "row")
+(verify_backward mongodb-3.6 mongodb-3.4 "fix row var")
+(verify_backward mongodb-4.0 mongodb-3.6 "fix row var")
+(verify_backward mongodb-4.2 mongodb-4.0 "fix row var")
+(verify_backward mongodb-4.4 mongodb-4.2 "fix row var")
+(verify_backward develop mongodb-4.4 "row")
 
 exit 0
