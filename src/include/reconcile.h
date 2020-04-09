84a0a6408a595d777a4c9dd48b040e97d026f4ed
Chenhao Qu <chenhao.qu@mongodb.com>
2020-04-09 02:41:29 +0000

    WT-5978 Fix restore clean update chain if we have unstable prepared updates on the chain

--- src/include/reconcile.h
+++ src/include/reconcile.h
@@ -37,6 +37,10 @@ struct __wt_reconcile {
     wt_timestamp_t max_ondisk_ts;
     wt_timestamp_t min_skipped_ts;
 
+    /* FIXME: track the stable timestamp when reconciliation starts. Remove when PM-1524 completes.
+     */
+    wt_timestamp_t stable_ts;
+
     u_int updates_seen;     /* Count of updates seen. */
     u_int updates_unstable; /* Count of updates not visible_all. */
 
