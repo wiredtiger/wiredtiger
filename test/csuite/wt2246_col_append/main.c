87c7fd7f801042d10919da0746d1f7746bdda92c
Luke Chen <luke.chen@mongodb.com>
2020-04-21 14:45:03 +1000

    WT-5790 Disable test_wt2246_col_append

--- test/csuite/wt2246_col_append/main.c
+++ test/csuite/wt2246_col_append/main.c
@@ -94,6 +94,9 @@ main(int argc, char *argv[])
     uint64_t i, id;
     char buf[100];
 
+    /* Temporarily disable this test (WT-5790) */
+    return (EXIT_SUCCESS);
+
     /* Bypass this test for valgrind */
     if (testutil_is_flag_set("TESTUTIL_BYPASS_VALGRIND"))
         return (EXIT_SUCCESS);
