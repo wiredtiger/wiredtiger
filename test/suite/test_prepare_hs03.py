39a2a82e936d12343597f0dcbe81160a7d855d5a
Haribabu Kommi <haribabu.kommi@mongodb.com>
2020-04-24 06:16:52 +0000

    Remove unnecessary unittest imports

--- test/suite/test_prepare_hs03.py
+++ test/suite/test_prepare_hs03.py
@@ -27,7 +27,7 @@
 # OTHER DEALINGS IN THE SOFTWARE.
 
 from helper import copy_wiredtiger_home
-import unittest, wiredtiger, wttest
+import wiredtiger, wttest
 from wtdataset import SimpleDataSet
 import os, shutil
 from wtscenario import make_scenarios
