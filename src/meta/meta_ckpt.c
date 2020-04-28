092bf10ab336c3f31be52b0374df101c6219e1b7
Tammy Bailey <tammy.bailey@mongodb.com>
2020-04-28 04:21:42 +0000

    Fix for s_all.

--- src/meta/meta_ckpt.c
+++ src/meta/meta_ckpt.c
@@ -473,9 +473,9 @@ __wt_meta_get_oldest_ckpt_timestamp(
     /* Load any existing checkpoints into the array. */
     if ((ret = __wt_config_getones(session, config, "checkpoint", &v)) == 0) {
         __wt_config_subinit(session, &ckptconf, &v);
-        /* 
-         * There may be multiple checkpoints for the file. Loop through to find the minimum
-         * oldest start timestamp over all of them.
+        /*
+         * There may be multiple checkpoints for the file. Loop through to find the minimum oldest
+         * start timestamp over all of them.
          */
         for (; (ret = __wt_config_next(&ckptconf, &k, &v)) == 0;) {
             if (__wt_config_subgets(session, &v, "oldest_start_ts", &a) == 0 && a.len != 0 &&
