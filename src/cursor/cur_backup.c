1af457b8fb2631c8c5141630cdf1786174be6364
keitharnoldsmith <61710331+keitharnoldsmith@users.noreply.github.com>
2020-05-01 14:26:47 -0400

    WT-5242 Minimize checkpoints pinned during backup (#5590)
    Checkpoints have time values generated from a per-session monotonic
    clock.  Use these values to preserve only the checkpoints that may
    be needed by backup.  At the end of each checkpoint, we save a
    time value from the same clock.  When we open a backup cursor we
    use this save value as a limit on checkpoint preservation.  All
    checkpoints with times values less than or equal to that are kept
    until the backup cursor is closed.
    
    Add python tests for new behavior and update docs to reflect new way
    checkpoints and backup cursors interact.
    
    Eliminate unnecessary iteration searching for highest value in sorted array.
    
    Co-authored-by: Keith A. Smith <keith.smith@mongodb>
    (cherry picked from commit 19f3761b35482c91f5d50d643ebfb7d0e6316584)

--- src/cursor/cur_backup.c
+++ src/cursor/cur_backup.c
@@ -614,7 +614,7 @@ __backup_start(
      * Single thread hot backups: we're holding the schema lock, so we know we'll serialize with
      * other attempts to start a hot backup.
      */
-    if (conn->hot_backup && !is_dup)
+    if (conn->hot_backup_start != 0 && !is_dup)
         WT_RET_MSG(session, EINVAL, "there is already a backup cursor open");
 
     if (F_ISSET(session, WT_SESSION_BACKUP_DUP) && is_dup)
@@ -775,7 +775,7 @@ __backup_stop(WT_SESSION_IMPL *session, WT_CURSOR_BACKUP *cb)
     WT_TRET(__wt_backup_file_remove(session));
 
     /* Checkpoint deletion and next hot backup can proceed. */
-    WT_WITH_HOTBACKUP_WRITE_LOCK(session, conn->hot_backup = false);
+    WT_WITH_HOTBACKUP_WRITE_LOCK(session, conn->hot_backup_start = 0);
     F_CLR(session, WT_SESSION_BACKUP_CURSOR);
 
     return (ret);
