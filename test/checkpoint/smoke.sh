8ca814fdf952c13a39fff71dbc76880e4913b774
Ravi Giri <ravi.giri@mongodb.com>
2020-05-14 11:10:55 +1000

    Disable lsm, column-store and mix file_type in smoke.sh

--- test/checkpoint/smoke.sh
+++ test/checkpoint/smoke.sh
@@ -6,17 +6,17 @@ set -e
 test "$TESTUTIL_BYPASS_VALGRIND" = "1" && exit 0
 
 # Smoke-test checkpoints as part of running "make check".
-echo "checkpoint: 3 mixed tables"
-$TEST_WRAPPER ./t -T 3 -t m
+#echo "checkpoint: 3 mixed tables"
+#$TEST_WRAPPER ./t -T 3 -t m
 
-echo "checkpoint: 6 column-store tables"
-$TEST_WRAPPER ./t -T 6 -t c
+#echo "checkpoint: 6 column-store tables"
+#$TEST_WRAPPER ./t -T 6 -t c
 
-echo "checkpoint: 6 LSM tables"
-$TEST_WRAPPER ./t -T 6 -t l
+#echo "checkpoint: 6 LSM tables"
+#$TEST_WRAPPER ./t -T 6 -t l
 
-echo "checkpoint: 6 mixed tables"
-$TEST_WRAPPER ./t -T 6 -t m
+#echo "checkpoint: 6 mixed tables"
+#$TEST_WRAPPER ./t -T 6 -t m
 
 echo "checkpoint: 6 row-store tables"
 $TEST_WRAPPER ./t -T 6 -t r
@@ -30,8 +30,8 @@ $TEST_WRAPPER ./t -t r -W 3 -r 2 -D -s -x -n 100000 -k 100000 -C cache_size=100M
 echo "checkpoint: row-store tables, Sweep and timestamps"
 $TEST_WRAPPER ./t -t r -W 3 -r 2 -s -x -n 100000 -k 100000 -C cache_size=100MB
 
-echo "checkpoint: 3 mixed tables, with sweep"
-$TEST_WRAPPER ./t -T 3 -t m -W 3 -r 2 -s -n 100000 -k 100000
+#echo "checkpoint: 3 mixed tables, with sweep"
+#$TEST_WRAPPER ./t -T 3 -t m -W 3 -r 2 -s -n 100000 -k 100000
 
-echo "checkpoint: 3 mixed tables, with timestamps"
-$TEST_WRAPPER ./t -T 3 -t m -W 3 -r 2 -x -n 100000 -k 100000
+#echo "checkpoint: 3 mixed tables, with timestamps"
+#$TEST_WRAPPER ./t -T 3 -t m -W 3 -r 2 -x -n 100000 -k 100000
