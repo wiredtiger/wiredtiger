4c1fef6ea1f68562975b682b9ec76a2a3d18c822
Sulabh Mahajan <sulabh.mahajan@mongodb.com>
2020-05-28 14:33:44 +1000

    Mark a page modified before force evicting, hence forcing reconciliation

--- src/btree/bt_curprev.c
+++ src/btree/bt_curprev.c
@@ -646,10 +646,17 @@ __wt_btcur_prev(WT_CURSOR_BTREE *cbt, bool truncating)
          * and only saw deleted records, try to evict the page when we release it. Otherwise
          * repeatedly deleting from the beginning of a tree can have quadratic performance. Take
          * care not to force eviction of pages that are genuinely empty, in new trees.
+         *
+         * A visible stop timestamp could have been treated as a tombstone and accounted in the
+         * deleted count. Such a page might not have any new updates and be clean, but could benefit
+         * from reconciliation getting rid of the obsolete content. Hence mark the page dirty to
+         * force it through reconciliation.
          */
-        if (page != NULL && !WT_IS_HS(CUR2BT(cbt)) &&
-          (cbt->page_deleted_count > WT_BTREE_DELETE_THRESHOLD ||
-              (newpage && cbt->page_deleted_count > 0))) {
+        if (page != NULL && (cbt->page_deleted_count > WT_BTREE_DELETE_THRESHOLD ||
+                              (newpage && cbt->page_deleted_count > 0))) {
+            if (cbt->ref->page->modify == NULL)
+                WT_ERR(__wt_page_modify_init(session, page));
+            __wt_page_modify_set(session, page);
             __wt_page_evict_soon(session, cbt->ref);
             WT_STAT_CONN_INCR(session, cache_eviction_force_delete);
         }
