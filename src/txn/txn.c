ca9223b24775fdf0dc547d3a9f80a582c5dccde3
Chenhao Qu <chenhao.qu@mongodb.com>
2020-05-29 01:26:34 +0000

    Add a comment

--- src/txn/txn.c
+++ src/txn/txn.c
@@ -922,7 +922,9 @@ __txn_resolve_prepared_op(WT_SESSION_IMPL *session, WT_TXN_OP *op, bool commit, 
          * Scan the history store for the given btree and key with maximum start timestamp to let
          * the search point to the last version of the key. We must ignore tombstone in the history
          * store while retrieving the update from the history store to replace the update in the
-         * data store.
+         * data store. We also need to ignore visibility of the updates as we have already released
+         * our snapshot in prepare. Otherwise, we can't see updates with non-globally visible
+         * transaction ids.
          */
         F_SET(hs_cursor, WT_CURSTD_IGNORE_TOMBSTONE);
         F_SET(session, WT_SESSION_HS_IGNORE_VISIBILITY);
