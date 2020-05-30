18726eda2f48bd9fe66408b8f9ed90bdc8bd0738
Keith Bostic <keith.bostic@mongodb.com>
2020-05-29 18:37:08 -0700

    WT-6337 Temporarily disable format's history store cursor scan #5739
    * Disable format's HS cursor scan for now.
    
    * Typo.

--- test/format/ops.c
+++ test/format/ops.c
@@ -117,6 +117,8 @@ operations(u_int ops_seconds, bool lastrun)
     uint32_t i;
     bool running;
 
+    g.c_hs_cursor = 0; /* Disable the HS cursor scan for now. */
+
     conn = g.wts_conn;
 
     session = NULL; /* -Wconditional-uninitialized */
