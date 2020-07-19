4e21ea7b0d8ce37bce5309e69c2fe1fcfa66544b
Keith Bostic <keith.bostic@mongodb.com>
2020-07-19 01:30:05 +0000

    checkpoint

--- site_scons/wt_platform.py
+++ site_scons/wt_platform.py
@@ -1,24 +1,13 @@
 import os, platform
 from SCons.Script import *
 
-os_posix = False
-os_darwin = False
-os_linux = False
-os_windows = False
+# OS, Architecture
+os_posix = os.name == 'posix'
+os_darwin = platform.system() == 'Darwin'
+os_linux = platform.system() == 'Linux'
+os_windows = platform.system() == 'Windows'
 
-os_arm64 = False
-os_powerpc = False
-os_x86 = False
-os_zseries = False
-
-def wt_platform():
-    # OS, Architecture
-    os_posix = os.name == 'posix'
-    os_darwin = platform.system() == 'Darwin'
-    os_linux = platform.system() == 'Linux'
-    os_windows = platform.system() == 'Windows'
-
-    os_arm64 = platform.machine().startswith('arm')
-    os_powerpc = platform.machine().startswith('ppc')
-    os_x86 = platform.machine().startswith('x86')
-    os_zseries = platform.machine().startswith('zseries') # XXX: untested
+os_arm64 = platform.machine().startswith('arm')
+os_powerpc = platform.machine().startswith('ppc')
+os_x86 = platform.machine().startswith('x86')
+os_zseries = platform.machine().startswith('zseries') # XXX: untested
