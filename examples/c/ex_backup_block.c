209ecfdbfb175701b2dc2f5428b58d2d01bda9df
Susan LoVerso <sue@mongodb.com>
2020-08-07 10:38:13 -0400

    WT-6571 Lseek cannot use error_sys_check because it does not return an int. (Also fix a contraction that is flagged on multiple machines in s_string.)

--- examples/c/ex_backup_block.c
+++ examples/c/ex_backup_block.c
@@ -387,9 +387,11 @@ take_incr_backup(WT_SESSION *session, int i)
                     first = false;
                 }
 
-                error_sys_check(lseek(rfd, (wt_off_t)offset, SEEK_SET));
+                if (lseek(rfd, (wt_off_t)offset, SEEK_SET) == -1)
+                    testutil_die(errno, "lseek: read");
                 error_sys_check(rdsize = (size_t)read(rfd, tmp, (size_t)size));
-                error_sys_check(lseek(wfd, (wt_off_t)offset, SEEK_SET));
+                if (lseek(wfd, (wt_off_t)offset, SEEK_SET) == -1)
+                    testutil_die(errno, "lseek: write");
                 /* Use the read size since we may have read less than the granularity. */
                 error_sys_check(write(wfd, tmp, rdsize));
             } else {
