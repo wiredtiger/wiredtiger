19ff6b6ba5d80bf28c751e445458e3a706f9dc80
Chenhao Qu <chenhao.qu@mongodb.com>
2020-08-21 01:34:46 +0000

    Flag the insert cursor as well

--- src/history/hs.c
+++ src/history/hs.c
@@ -1609,6 +1609,12 @@ __hs_fixup_out_of_order_from_pos(WT_SESSION_IMPL *session, WT_CURSOR *hs_cursor,
             WT_WITHOUT_DHANDLE(session,
               ret = __wt_open_cursor(session, WT_HS_URI, NULL, open_cursor_cfg, &insert_cursor));
             WT_ERR(ret);
+
+            /* History store cursors should always ignore tombstones. */
+            F_SET(insert_cursor, WT_CURSTD_IGNORE_TOMBSTONE);
+
+            /* Set the flag to stop creating snapshots for history store cursors. */
+            F_SET((WT_CURSOR_BTREE *)insert_cursor, WT_CBT_NO_TXN);
         }
 
         /*
