20a212bf9d76cf5beeaa7784f41adf8c466a896a
Chenhao Qu <chenhao.qu@mongodb.com>
2020-08-25 02:18:32 +0000

    Remove duplicate code

--- test/suite/test_rollback_to_stable07.py
+++ test/suite/test_rollback_to_stable07.py
@@ -55,29 +55,6 @@ class test_rollback_to_stable07(test_rollback_to_stable_base):
         config = 'cache_size=5MB,statistics=(all),log=(enabled=true)'
         return config
 
-    def simulate_crash_restart(self, uri, olddir, newdir):
-        ''' Simulate a crash from olddir and restart in newdir. '''
-        # with the connection still open, copy files to new directory
-        shutil.rmtree(newdir, ignore_errors=True)
-        os.mkdir(newdir)
-        for fname in os.listdir(olddir):
-            fullname = os.path.join(olddir, fname)
-            # Skip lock file on Windows since it is locked
-            if os.path.isfile(fullname) and \
-                "WiredTiger.lock" not in fullname and \
-                "Tmplog" not in fullname and \
-                "Preplog" not in fullname:
-                shutil.copy(fullname, newdir)
-        #
-        # close the original connection and open to new directory
-        # NOTE:  This really cannot test the difference between the
-        # write-no-sync (off) version of log_flush and the sync
-        # version since we're not crashing the system itself.
-        #
-        self.close_conn()
-        self.conn = self.setUpConnectionOpen(newdir)
-        self.session = self.setUpSessionOpen(self.conn)
-
     def test_rollback_to_stable(self):
         nrows = 1000
 
