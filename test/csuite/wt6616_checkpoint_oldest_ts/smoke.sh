7a87a19a69d1fc28ad03e98cc7db131f56af4eaa
Chenhao Qu <chenhao.qu@mongodb.com>
2020-08-31 04:56:22 +0000

    Temporarily disbale the log test

--- test/csuite/wt6616_checkpoint_oldest_ts/smoke.sh
+++ test/csuite/wt6616_checkpoint_oldest_ts/smoke.sh
@@ -8,4 +8,5 @@ top_builddir=${top_builddir:-../../build_posix}
 top_srcdir=${top_srcdir:-../..}
 
 $TEST_WRAPPER $top_builddir/test/csuite/test_wt6616_checkpoint_oldest_ts
-$TEST_WRAPPER $top_builddir/test/csuite/test_wt6616_checkpoint_oldest_ts -l
+# FIXME-WT-6637: temporarily disabled until WT-6637 is fixed
+# $TEST_WRAPPER $top_builddir/test/csuite/test_wt6616_checkpoint_oldest_ts -l
