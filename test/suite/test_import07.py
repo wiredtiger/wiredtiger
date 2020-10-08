9f47c9e8c87e09a76414aec95cb7f54ab40c775d
Alex Cameron <alex.cameron@10gen.com>
2020-10-08 06:57:14 +0000

    Cleanup test.

--- test/suite/test_import07.py
+++ test/suite/test_import07.py
@@ -29,61 +29,45 @@
 # test_import07.py
 # Check that importing unsupported data sources returns an error.
 
-import os, shutil
-import wiredtiger, wttest
+import wiredtiger
 from test_import01 import test_import_base
+from wtscenario import make_scenarios
 
 class test_import07(test_import_base):
-    conn_config = 'cache_size=50MB,log=(enabled),statistics=(all)'
-    session_config = 'isolation=snapshot'
-
     original_db_file = 'original_db_file'
-
-    nrows = 100
-    ntables = 10
-    keys = [b'1', b'2', b'3', b'4', b'5', b'6']
-    values = [b'\x01\x02aaa\x03\x04', b'\x01\x02bbb\x03\x04', b'\x01\x02ccc\x03\x04',
-              b'\x01\x02ddd\x03\x04', b'\x01\x02eee\x03\x04', b'\x01\x02fff\x03\x04']
-    ts = [10*k for k in range(1, len(keys)+1)]
     create_config = 'allocation_size=512,key_format=u,log=(enabled=true),value_format=u'
 
-    def test_import_lsm_unsupported(self):
-        uri = 'lsm:' + self.original_db_file
-        self.session.create(uri, self.create_config)
+    ntables = 10
+    nrows = 100
+    scenarios = make_scenarios([
+        ('colgroup', dict(prefix='colgroup:')),
+        ('lsm', dict(prefix='lsm:')),
+        ('index', dict(prefix='index:')),
+    ])
 
-        # Add data and perform a checkpoint.
-        for i in range(0, len(self.keys)):
-            self.update(uri, self.keys[i], self.values[i], self.ts[i])
-        self.session.checkpoint()
-
-        # Export the metadata for the table.
-        c = self.session.open_cursor('metadata:', None, None)
-        original_db_file_config = c[uri]
-        c.close()
-
-        self.printVerbose(3, '\nFile configuration:\n' + original_db_file_config)
-
-        # Close the connection.
-        self.close_conn()
-
-        # Create a new database and connect to it.
-        newdir = 'IMPORT_DB'
-        shutil.rmtree(newdir, ignore_errors=True)
-        os.mkdir(newdir)
-        self.conn = self.setUpConnectionOpen(newdir)
-        self.session = self.setUpSessionOpen(self.conn)
-
+    def test_import_unsupported_data_source(self):
         # Make a bunch of files and fill them with data.
         self.populate(self.ntables, self.nrows)
         self.session.checkpoint()
 
-        # Copy over the datafiles for the object we want to import.
-        self.copy_file(self.original_db_file, '.', newdir)
+        # Export the metadata for one of the files we made.
+        # We just need an example of what a file configuration would typically look like.
+        cursor = self.session.open_cursor('metadata:', None, None)
+        for k, v in cursor:
+            if k.startswith('table:'):
+                example_db_file_config = cursor[k]
+                break
+        cursor.close()
 
         # Contruct the config string.
         import_config = 'import=(enabled,repair=false,file_metadata=(' + \
-            original_db_file_config + '))'
+            example_db_file_config + '))'
 
-        # Import the file.
+        # This uri doesn't exist however, for the purposes of our test, this is fine.
+        uri = self.prefix + self.original_db_file
+
+        # Before importing, we check whether the data source is a 'file' or 'table'.
+        # If not, we return ENOTSUP.
         self.assertRaisesWithMessage(wiredtiger.WiredTigerError,
-            lambda: self.session.create(uri, import_config), '/Operation not supported/')
+            lambda: self.session.create(uri, import_config),
+            '/Operation not supported/')
