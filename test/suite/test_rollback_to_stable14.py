bd4e5b999ed11220f1a19e946e855d19aea00f95
Alex Cameron <alex.cameron@10gen.com>
2020-10-16 08:41:29 +0000

    WT-6729 Create test reproducing the issue.

---
+++ test/suite/test_rollback_to_stable14.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+#
+# Public Domain 2014-2020 MongoDB, Inc.
+# Public Domain 2008-2014 WiredTiger, Inc.
+#
+# This is free and unencumbered software released into the public domain.
+#
+# Anyone is free to copy, modify, publish, use, compile, sell, or
+# distribute this software, either in source code form or as a compiled
+# binary, for any purpose, commercial or non-commercial, and by any
+# means.
+#
+# In jurisdictions that recognize copyright laws, the author or authors
+# of this software dedicate any and all copyright interest in the
+# software to the public domain. We make this dedication for the benefit
+# of the public at large and to the detriment of our heirs and
+# successors. We intend this dedication to be an overt act of
+# relinquishment in perpetuity of all present and future rights to this
+# software under copyright law.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+
+from test_rollback_to_stable01 import test_rollback_to_stable_base
+from wtdataset import SimpleDataSet
+
+def timestamp_str(t):
+    return '%x' % t
+
+# test_rollback_to_stable14
+# Test history store operations conflicting with rollback to stable.
+class test_rollback_to_stable14(test_rollback_to_stable_base):
+    session_config = 'isolation=snapshot'
+    prepare = False
+
+    def test_rollback_to_stable(self):
+        nrows = 1000
+
+        uri = 'table:rollback_to_stable14'
+        ds = SimpleDataSet(
+            self, uri, 0, key_format='i', value_format='S', config='log=(enabled=false)')
+        ds.populate()
+
+        for i in range(1, 10000):
+            value = str(i) * 100
+            self.large_updates(uri, value, ds, nrows, i * 10)
+
+            # Let's hope this happens while eviction is doing work.
+            if i % 10 == 0:
+                self.conn.set_timestamp('stable_timestamp=' + timestamp_str((i - 1) * 10))
+                self.conn.rollback_to_stable()
