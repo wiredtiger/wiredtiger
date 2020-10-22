3daa90b5341fc11bfa460bc8d72f946c22b149cb
Alex Cameron <alex.cameron@10gen.com>
2020-10-22 02:51:56 +0000

    Cleanup test and add comments.

--- test/suite/test_rollback_to_stable14.py
+++ test/suite/test_rollback_to_stable14.py
@@ -33,7 +33,8 @@ def timestamp_str(t):
     return '%x' % t
 
 # test_rollback_to_stable14
-# Test history store operations conflicting with rollback to stable.
+# Test history store operations conflicting with rollback to stable. We need to ensure that we're
+# blocking eviction prior to rollback to stable.
 class test_rollback_to_stable14(test_rollback_to_stable_base):
     session_config = 'isolation=snapshot'
     prepare = False
@@ -47,10 +48,17 @@ class test_rollback_to_stable14(test_rollback_to_stable_base):
         ds.populate()
 
         for i in range(1, 1000):
+            # Generate a value and timestamp based off the index.
             value = str(i) * 100
-            self.large_updates(uri, value, ds, nrows, i * 10)
+            ts = i * 10
 
-            # Let's hope this happens while eviction is doing work.
+            # Perform updates.
+            self.large_updates(uri, value, ds, nrows, ts)
+
+            # Every hundred updates, lets run rollback to stable. This is likely to happen during
+            # a history store eviction at least once.
             if i % 100 == 0:
-                self.conn.set_timestamp('stable_timestamp=' + timestamp_str((i - 1) * 10))
+                # Put the timestamp backwards so we can rollback the updates we just did.
+                stable_ts = (i - 1) * 10
+                self.conn.set_timestamp('stable_timestamp=' + timestamp_str(stable_ts))
                 self.conn.rollback_to_stable()
