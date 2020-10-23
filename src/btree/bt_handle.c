597a3c4ab5fa433d1ed05b7c229065622035f122
Alex Cameron <alex.cameron@10gen.com>
2020-10-23 01:39:55 +0000

    Fix check.

--- src/btree/bt_handle.c
+++ src/btree/bt_handle.c
@@ -543,9 +543,10 @@ __btree_conf(WT_SESSION_IMPL *session, WT_CKPT *ckpt)
      * write generation is relevant here since it should remain static across the entire run.
      */
     btree->write_gen = WT_MAX(ckpt->write_gen + 1, conn->base_write_gen);
+    WT_ASSERT(session, ckpt->write_gen >= ckpt->run_write_gen);
 
     /* If this is the first time opening the tree this run. */
-    if (F_ISSET(session, WT_SESSION_IMPORT) || ckpt->run_write_gen < conn->base_write_gen)
+    if (F_ISSET(session, WT_SESSION_IMPORT) || ckpt->run_write_gen <= conn->base_write_gen)
         btree->base_write_gen = btree->run_write_gen = btree->write_gen;
     else
         btree->base_write_gen = btree->run_write_gen = ckpt->run_write_gen;
