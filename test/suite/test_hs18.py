b95833582530c754c6518f633bd1d66409da7106
Alex Cameron <alex.cameron@10gen.com>
2020-10-23 03:34:58 +0000

    Call CURSOR::reset to let go of the page.

--- test/suite/test_hs18.py
+++ test/suite/test_hs18.py
@@ -37,7 +37,6 @@ class test_hs18(wttest.WiredTigerTestCase):
     conn_config = 'cache_size=5MB,eviction=(threads_max=1)'
     session_config = 'isolation=snapshot'
 
-    @unittest.skip("Currently hangs")
     def test_hs18(self):
         uri = 'table:test_hs18'
         self.session.create(uri, 'key_format=S,value_format=S')
@@ -96,6 +95,9 @@ class test_hs18(wttest.WiredTigerTestCase):
         self.assertEqual(cursor2.search(), 0)
         val2 = cursor2.get_value()
         self.assertEqual(val1, val2)
+
+        # Let go of the page since we want to evict.
+        cursor2.reset()
 
         # Insert a bunch of other contents to trigger eviction
         for i in range(10001, 11000):
