1cf77607901b366008d719695d3ad3ded03048f5
Alex Cameron <alex.cameron@10gen.com>
2020-10-29 00:13:43 +0000

    Quote character in script for consistency.

--- test/fuzz/fuzz_coverage.sh
+++ test/fuzz/fuzz_coverage.sh
@@ -52,7 +52,7 @@ combined_profdata_name="${fuzz_cov_name}.profdata"
 
 # Check that there is coverage data.
 ls *.profraw &> /dev/null
-if test $? -ne 0; then
+if test $? -ne "0"; then
 	echo "$0: could not find any .profraw files in the current directory"
 	echo "$0: ensure that -fprofile-instr-generate and -fcoverage-mapping are added to your CFLAGS and LINKFLAGS when configuring"
 	exit 1
