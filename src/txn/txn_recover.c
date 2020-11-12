23359634228d35cbe5e1b93771a9cb8894a607aa
Ravi Giri <ravi.giri@mongodb.com>
2020-11-12 17:27:57 +1100

    Remove updating the base write gen as part of recovery_setup_file

--- src/txn/txn_recover.c
+++ src/txn/txn_recover.c
@@ -548,8 +548,7 @@ err:
 
 /*
  * __recovery_setup_file --
- *     Set up the recovery slot for a file, track the largest file ID, and update the base write gen
- *     based on the file's configuration.
+ *     Set up the recovery slot for a file and track the largest file ID.
  */
 static int
 __recovery_setup_file(WT_RECOVERY *r, const char *uri, const char *config)
@@ -595,8 +594,7 @@ __recovery_setup_file(WT_RECOVERY *r, const char *uri, const char *config)
       (WT_IS_MAX_LSN(&r->max_ckpt_lsn) || __wt_log_cmp(&lsn, &r->max_ckpt_lsn) > 0))
         WT_ASSIGN_LSN(&r->max_ckpt_lsn, &lsn);
 
-    /* Update the base write gen based on this file's configuration. */
-    return (__wt_metadata_update_base_write_gen(r->session, config));
+    return (0);
 }
 
 /*
