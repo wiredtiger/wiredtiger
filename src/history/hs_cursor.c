b456d2db3ae744a0e0d14b2dd27691ce9673b326
Chenhao Qu <chenhao.qu@mongodb.com>
2020-12-07 04:30:14 +0000

    Revert a change

--- src/history/hs_cursor.c
+++ src/history/hs_cursor.c
@@ -54,7 +54,7 @@ __wt_hs_row_search(WT_CURSOR_BTREE *hs_cbt, WT_ITEM *srch_key, bool insert)
     }
 
 #ifdef HAVE_DIAGNOSTIC
-    WT_ERR(__wt_cursor_key_order_init(hs_cbt));
+    WT_TRET(__wt_cursor_key_order_init(hs_cbt));
 #endif
 
     if (0) {
