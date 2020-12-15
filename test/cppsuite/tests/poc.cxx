4a87eda6022a8c3740c3f3f647694c102ea557ec
Luke Pearson <luke.pearson@mongodb.com>
2020-12-16 09:57:44 +1100

    Make strings const, improve comment

--- test/cppsuite/tests/poc.cxx
+++ test/cppsuite/tests/poc.cxx
@@ -7,12 +7,12 @@ int main(int argc, char *argv[]) {
     WT_CONNECTION *conn;
     int ret = 0;
     // Setup basic test directory.
-    std::string default_dir = "WT_TEST";
+    const std::string default_dir = "WT_TEST";
 
-    // Csuite tests utilise a test_util.h command to make their directory, current that doesn't
+    // Csuite tests utilise a test_util.h command to make their directory, currently that doesn't
     // compile under c++ and some extra work will be needed to make it work. Its unclear if the
     // test framework will use test_util.h yet.
-    std::string mkdir_cmd = "mkdir " + default_dir;
+    const std::string mkdir_cmd = "mkdir " + default_dir;
     ret = system(mkdir_cmd.c_str());
     if (ret != 0)
         return (ret);
