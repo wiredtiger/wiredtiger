134a33789e2f5ec543a7bb8def5532c1de4547fb
Chenhao Qu <chenhao.qu@mongodb.com>
2020-12-18 02:13:41 +0000

    Fix compile

--- src/cursor/cur_hs.c
+++ src/cursor/cur_hs.c
@@ -565,7 +565,7 @@ __curhs_search_near(WT_CURSOR *cursor, int *exactp)
                 if (btree_id == hs_cursor->btree_id) {
                     if (F_ISSET(hs_cursor, WT_HS_CUR_KEY_SET)) {
                         WT_ERR(__wt_compare(
-                          session, NULL, hs_cursor->datastore_key, datastore_key, &cmp));
+                          session, NULL, &hs_cursor->datastore_key, datastore_key, &cmp));
                         if (cmp == 0) {
                             break;
                         }
@@ -603,7 +603,7 @@ __curhs_search_near(WT_CURSOR *cursor, int *exactp)
                 if (btree_id == hs_cursor->btree_id) {
                     if (F_ISSET(hs_cursor, WT_HS_CUR_KEY_SET)) {
                         WT_ERR(__wt_compare(
-                          session, NULL, hs_cursor->datastore_key, datastore_key, &cmp));
+                          session, NULL, &hs_cursor->datastore_key, datastore_key, &cmp));
                         if (cmp == 0) {
                             break;
                         }
