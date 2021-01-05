d785e5106e1855c953bbea850a06eab7a00db678
clarissecheah <65272339+clarissecheah@users.noreply.github.com>
2021-01-05 17:19:33 +1100

    Update reconcile_inline.h

--- src/include/reconcile_inline.h
+++ src/include/reconcile_inline.h
@@ -96,9 +96,7 @@ __rec_page_time_stats(WT_SESSION_IMPL *session, WT_RECONCILE *r)
           session, rec_time_window_bytes_ts, r->count_durable_start_ts * sizeof(wt_timestamp_t));
         WT_STAT_CONN_DATA_INCRV(
           session, rec_time_window_durable_start_ts, r->count_durable_start_ts);
-
         WT_STAT_CONN_DATA_INCR(session, rec_time_window_pages_durable_start_ts);
-
         r->rec_page_cell_with_ts = true;
     }
     if (r->count_start_ts != 0) {
