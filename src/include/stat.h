11a14ea45dc53605e336aed7a86bc66f0aeb8cc0
clarissecheah <65272339+clarissecheah@users.noreply.github.com>
2021-01-06 14:45:08 +1100

    Update stat.h

--- src/include/stat.h
+++ src/include/stat.h
@@ -243,8 +243,8 @@ __wt_stats_clear(void *stats_arg, int slot)
  * places.
  */
 #define WT_STAT_CONN_DATA_DECRV(session, fld, value) \
-    WT_STAT_CONN_DECRV(session, value);               \
-    WT_STAT_DATA_DECRV(session, value)
+    WT_STAT_CONN_DECRV(session,fld, value);               \
+    WT_STAT_DATA_DECRV(session,fld, value)
 #define WT_STAT_CONN_DATA_DECR(session, fld) WT_STAT_CONN_DATA_DECRV(session, fld, 1)
 
 #define WT_STAT_CONN_DATA_INCRV(session, fld, value) \
