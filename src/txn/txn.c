07c7e8b30b6ce43e677d6b80b0eaa6908bbf4530
Don Anderson <donald.anderson@mongodb.com>
2021-01-08 21:27:52 +0000

    Revert changes to txn code, no longer needed.

--- src/txn/txn.c
+++ src/txn/txn.c
@@ -721,9 +721,8 @@ __wt_txn_release(WT_SESSION_IMPL *session)
  *     Append the update older than the prepared update to the update chain
  */
 static int
-__txn_append_hs_record(WT_SESSION_IMPL *session, WT_CURSOR *hs_cursor, uint32_t hs_btree_id,
-  WT_ITEM *key, WT_PAGE *page, WT_UPDATE *chain, bool commit, WT_UPDATE **fix_updp,
-  bool *upd_appended)
+__txn_append_hs_record(WT_SESSION_IMPL *session, WT_CURSOR *hs_cursor, WT_ITEM *key, WT_PAGE *page,
+  WT_UPDATE *chain, bool commit, WT_UPDATE **fix_updp, bool *upd_appended)
 {
     WT_CURSOR_BTREE *hs_cbt;
     WT_DECL_ITEM(hs_key);
@@ -733,7 +732,7 @@ __txn_append_hs_record(WT_SESSION_IMPL *session, WT_CURSOR *hs_cursor, uint32_t 
     wt_timestamp_t durable_ts, hs_start_ts, hs_stop_durable_ts;
     size_t size, total_size;
     uint64_t hs_counter, type_full;
-    uint32_t this_hs_btree_id;
+    uint32_t hs_btree_id;
     int cmp;
     char ts_string[2][WT_TS_INT_STRING_SIZE];
 
@@ -750,10 +749,10 @@ __txn_append_hs_record(WT_SESSION_IMPL *session, WT_CURSOR *hs_cursor, uint32_t 
     WT_ERR(__wt_scr_alloc(session, 0, &hs_value));
 
     for (; ret == 0; ret = __wt_hs_cursor_prev(session, hs_cursor)) {
-        WT_ERR(hs_cursor->get_key(hs_cursor, &this_hs_btree_id, hs_key, &hs_start_ts, &hs_counter));
+        WT_ERR(hs_cursor->get_key(hs_cursor, &hs_btree_id, hs_key, &hs_start_ts, &hs_counter));
 
         /* Stop before crossing over to the next btree */
-        if (this_hs_btree_id != hs_btree_id) {
+        if (hs_btree_id != S2BT(session)->id) {
             ret = WT_NOTFOUND;
             goto done;
         }
@@ -1127,7 +1126,7 @@ __txn_resolve_prepared_op(WT_SESSION_IMPL *session, WT_TXN_OP *op, bool commit, 
      */
     if (F_ISSET(upd, WT_UPDATE_PREPARE_RESTORED_FROM_DS) && upd->type != WT_UPDATE_TOMBSTONE) {
         cbt = (WT_CURSOR_BTREE *)(*cursorp);
-        hs_btree_id = CUR2BT(cbt)->id;
+        hs_btree_id = S2BT(session)->id;
         /* Open a history store table cursor. */
         WT_ERR(__wt_hs_cursor_open(session));
         hs_cursor = session->hs_cursor;
@@ -1140,13 +1139,11 @@ __txn_resolve_prepared_op(WT_SESSION_IMPL *session, WT_TXN_OP *op, bool commit, 
                              session, hs_cursor, hs_btree_id, &op->u.op_row.key, WT_TS_MAX, NULL),
           true);
 
-        if (ret == 0) {
+        if (ret == 0)
             /* Not found if we cross the tree or key boundary. */
-            WT_WITH_BTREE(session, op->btree,
-              ret = __txn_append_hs_record(session, hs_cursor, hs_btree_id, &op->u.op_row.key,
-                cbt->ref->page, upd, commit, &fix_upd, &upd_appended));
-            WT_ERR_NOTFOUND_OK(ret, true);
-        }
+            WT_ERR_NOTFOUND_OK(__txn_append_hs_record(session, hs_cursor, &op->u.op_row.key,
+                                 cbt->ref->page, upd, commit, &fix_upd, &upd_appended),
+              true);
         if (ret == WT_NOTFOUND && !commit) {
             /*
              * Allocate a tombstone and prepend it to the row so when we reconcile the update chain
