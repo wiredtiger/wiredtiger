301e4d9eb1a9341b26eb73896a404b2c47cc0787
Chenhao Qu <chenhao.qu@mongodb.com>
2021-01-13 07:02:10 +0000

    Fix the inverted comparison

--- src/cursor/cur_hs.c
+++ src/cursor/cur_hs.c
@@ -634,7 +634,7 @@ __curhs_search_near(WT_CURSOR *cursor, int *exactp)
 
                 if (btree_id == hs_cursor->btree_id && F_ISSET(hs_cursor, WT_HS_CUR_KEY_SET)) {
                     WT_ERR(
-                      __wt_compare(session, NULL, hs_cursor->datastore_key, datastore_key, &cmp));
+                      __wt_compare(session, NULL, datastore_key, hs_cursor->datastore_key, &cmp));
                     if (cmp == 0)
                         break;
 
@@ -668,7 +668,7 @@ __curhs_search_near(WT_CURSOR *cursor, int *exactp)
 
                 if (btree_id == hs_cursor->btree_id && F_ISSET(hs_cursor, WT_HS_CUR_KEY_SET)) {
                     WT_ERR(
-                      __wt_compare(session, NULL, hs_cursor->datastore_key, datastore_key, &cmp));
+                      __wt_compare(session, NULL, datastore_key, hs_cursor->datastore_key, &cmp));
                     if (cmp == 0)
                         break;
 
