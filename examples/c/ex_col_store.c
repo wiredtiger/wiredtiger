3369336b138875bb224d9629692254bff9bb108e
Siddhartha Mahajan <siddhartha.mahajan@mongodb.com>
2021-01-15 03:17:00 +0000

    Fixed the listing error

--- examples/c/ex_col_store.c
+++ examples/c/ex_col_store.c
@@ -3,24 +3,24 @@
 static const char *home;
 
 typedef struct {
-    uint16_t longitude; /* change to double */
-    uint16_t latitude; /* change to double */
+    uint16_t longitude; 
+    uint16_t latitude; 
     char country[5];
 } LOCATION;
 
 typedef struct {
     char day[5]; 
     uint16_t hour;
-    uint8_t temp; /* change to double */
+    uint8_t temp; 
     uint8_t humidity;
-    uint16_t pressure; /* change to double */
+    uint16_t pressure; 
     uint8_t wind;
-    uint8_t feels_like_temp; /* change to double */
+    uint8_t feels_like_temp; 
     LOCATION location;
 } WEATHER;
 
 static WEATHER weather_data[] = {                               
-                                { "WED", 123, 20, 60, 1000, 20, 21, { 36, 174, "NZ" }},
+                                { "WED", 600, 20, 60, 1000, 20, 21, { 36, 174, "NZ" }},
                                 { "WED", 1200, 25, 65, 1001, 23, 21, { 36, 174, "NZ" }},
                                 { "WED", 1800, 18, 53, 1002, 18, 21, { 36, 174, "NZ" }},
                                 { "WED", 2400, 15, 50, 1002, 15, 21, { 36, 174, "NZ" }},
@@ -31,11 +31,33 @@ static WEATHER weather_data[] = {
                                 { "THU", 1100, 5, 28, 1050, 40, 7, {36, 140, "JPN"}},
                                 { "TUE", 1540, 25, 80, 800, 15, 21, {3, 31, "AU"} }, 
                                 { "FRI", 2110, 16, 40, 600, 30, 14, {36, 311, "CAN"}}, 
-                                { "MON", 0625, 30, 100, 1000, 5, 32, {42,3, "USA"}},
+                                { "MON", 625, 30, 100, 1000, 5, 32, {42,3, "USA"}},
                                 { "THU", 2231, 19, 100, 1100, 12, 20, {3,31, "AU"}},  
                                 { "WED", 2215, 22, 120, 950, 21, 34,{50,150, "NZ"}}, 
                                 { "", 0, 0, 0, 0, 0, 0, {0,0,""}}                                
                                 };  
+
+
+// int celcius_to_farenheit(int temp_in_celcius) {
+//     return (1.8 * temp_in_celcius) + 32.0;
+// }
+
+// void update_celcius_to_farenheit(WT_SESSION* session) {
+//     WT_CURSOR *cursor;
+//     int ret;
+//     uint8_t temp;
+
+//     error_check(session->open_cursor(session, "colgroup:weathertable:temprature", NULL, NULL, &cursor));
+
+//     while((ret = cursor->next(cursor) == 0)) {
+//         error_check(cursor->get_value(cursor, &temp));
+//         cursor->set_value(cursor, celcius_to_farenheit(temp));
+//         error_check(cursor->update(cursor));
+//     }
+
+//     scan_end_check(ret == WT_NOTFOUND);
+//     error_check(cursor->close(cursor)); 
+// } 
 
 int
 main(int argc, char* argv[]) 
@@ -47,13 +69,9 @@ main(int argc, char* argv[])
     int ret;
     uint64_t recno;
 
-    char day[5]; 
-    uint16_t hour;
-    uint8_t temp; /* change to double */
-    uint8_t humidity;
-    uint16_t pressure; /* change to double */
-    uint8_t wind;
-    uint8_t feels_like_temp; /* change to double */
+    const char* day; 
+    uint16_t hour, pressure;
+    uint8_t temp, humidity, wind, feels_like_temp; 
     LOCATION *location;
 
     home = example_setup(argc, argv);
@@ -67,7 +85,7 @@ main(int argc, char* argv[])
     /* create a table, with coloumns and colgroup */ 
     /*===Problem: placeholders if we use double, and not sure with the Location as a struct to use 'u'===*/
     error_check(session->create(session, "table:weathertable",
-    "key_format=r,value_format=3sBBBBBBu,columns=(id,day,hour,temp,humidity,pressure,wind,feels_like_temp,location),colgroups=(day_time,temperature,humidity_pressure,"
+    "key_format=r,value_format=5sHBBHBBu,columns=(id,day,hour,temp,humidity,pressure,wind,feels_like_temp,location),colgroups=(day_time,temperature,humidity_pressure,"
     "wind,feels_like_temp,location)"));
     
 
@@ -86,15 +104,9 @@ main(int argc, char* argv[])
         WT_ITEM loc;
         loc.data = &w->location;
         loc.size = sizeof(w->location);
-        // printf("lat: %" PRIu16 "\nlong: %" PRIu32 "\n", w->location.latitude, w->location.longitude);
+
         cursor->set_value(cursor, w->day, w->hour, w->temp, w->humidity, w->pressure,w->wind,w->feels_like_temp, &loc);
         error_check(cursor->insert(cursor));
-
-        printf("Interting...\n");
-        printf(
-            "day: %s : hour: %" PRIu16 ", temp: %" PRIu8 ", humidity: %" PRIu8 ", pressure: %" PRIu16 ", wind: %" PRIu8 ", feels like: %" PRIu8 "\n"
-            , w->day, w->hour, w->temp, w->humidity, w->pressure, w->wind, w->feels_like_temp
-        );
     }
     /* close cursor */
     error_check(cursor->close(cursor));    
@@ -103,13 +115,22 @@ main(int argc, char* argv[])
     while ((ret = cursor->next(cursor)) == 0) {
         error_check(cursor->get_key(cursor, &recno));
         error_check(cursor->get_value(cursor, &day, &hour, &temp, &humidity, &pressure, &wind, &feels_like_temp, &location));
-        printf("Reading\n");
-        printf("ID %" PRIu64, recno);
-        printf(
-            "day: %s : hour: %" PRIu16 ", temp: %" PRIu8 ", humidity: %" PRIu8 ", pressure: %" PRIu16 ", wind: %" PRIu8 ", feels like: %" PRIu8 
-            ", location: lat: %" PRIu16 " long: %" PRIu16 " %s\n", day, hour,
-            temp, humidity, pressure, wind, feels_like_temp, location->latitude, location-> longitude, location->country
-        );
+        printf("{\n"
+               "    ID: %" PRIu64 "\n"
+               "    day: %s\n"
+               "    hour: %" PRIu16 "\n"
+               "    temp: %" PRIu8 "\n"
+               "    humidity: %" PRIu8 "\n"
+               "    pressure: %" PRIu16 "\n"
+               "    wind: %" PRIu8  "\n"
+               "    feels like: %" PRIu8 "\n" 
+               "    location:  {\n"
+               "                  lat: %" PRIu16 "\n"
+               "                  long: %" PRIu16 "\n"
+               "                  country: %s\n"
+               "                }\n"
+               "}\n\n",recno, day, hour,temp, humidity, pressure, wind, feels_like_temp, 
+               location->latitude, location-> longitude, location->country);
     }
     scan_end_check(ret == WT_NOTFOUND);
     error_check(cursor->close(cursor));
