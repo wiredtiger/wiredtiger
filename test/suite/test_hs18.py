86f4066333d01f42833b1670302f2949d971def6
Sean04 <sean.watt@mongodb.com>
2021-02-01 02:56:27 +0000

    Re-enabled string scenario

--- test/suite/test_hs18.py
+++ test/suite/test_hs18.py
@@ -39,7 +39,7 @@ class test_hs18(wttest.WiredTigerTestCase):
     session_config = 'isolation=snapshot'
     key_format_values = [
         ('column', dict(key_format='r')),
-        # ('string', dict(key_format='S'))
+        ('string', dict(key_format='S'))
     ]
     scenarios = make_scenarios(key_format_values)
 
