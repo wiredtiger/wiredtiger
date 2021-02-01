4845c7c62d134b0afa4ced194c809dc410ba6ace
monica.ng <monica.ng@mongodb.com>
2021-02-01 05:04:16 +0000

    Working on RTS test

--- test/suite/test_temp.py
+++ test/suite/test_temp.py
@@ -3,9 +3,13 @@ from helper import copy_wiredtiger_home
 import wiredtiger, wttest, unittest
 from wtdataset import SimpleDataSet
 from wtscenario import make_scenarios
+from test_rollback_to_stable01 import test_rollback_to_stable_base
+
+def timestamp_str(t):
+    return '%x' % t
 
 class tmp_test(wttest.WiredTigerTestCase):
-    conn_config = 'cache_size=5MB,eviction=(threads_max=1)'
+    conn_config = 'cache_size=500MB,eviction=(threads_max=1)'
     session_config = 'isolation=snapshot'
     key_format_values = [
         ('column', dict(key_format='r')),
@@ -19,6 +23,10 @@ class tmp_test(wttest.WiredTigerTestCase):
         create_params = 'key_format={},value_format=S'.format(self.key_format)
         self.session.create(uri, create_params)
         cursor =  self.session.open_cursor(uri)
+
+        # Pin oldest and stable to timestamp 1.
+        self.conn.set_timestamp('oldest_timestamp=' + timestamp_str(1) +
+            ',stable_timestamp=' + timestamp_str(1))
 
         tmp_value = 'aaaaa' * 100
         tmp_value2 = 'bbbbb' * 100
@@ -46,8 +54,11 @@ class tmp_test(wttest.WiredTigerTestCase):
         cursor[1] = tmp_value5
         self.session.commit_transaction('commit_timestamp=' + timestamp_str(10))
 
+        self.conn.set_timestamp('stable_timestamp=' + timestamp_str(7))
+        self.conn.rollback_to_stable()
+        self.check(tmp_value3, uri, 1, 9)
 
-        session.close()
+        self.session.close()
         
 
 if __name__ == '__main__':
