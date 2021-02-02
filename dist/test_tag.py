fed4a2c62dd48ac9a70f385be9209871e37de9ec
Etienne Petrel <etienne.petrel@mongodb.com>
2021-02-02 04:37:24 +0000

    WT-6947 Removed camel case syntax

--- dist/test_tag.py
+++ dist/test_tag.py
@@ -3,25 +3,25 @@ import sys
 
 ##### LOCAL VARIABLES AND CONSTANTS #####
 component = ""
-testingArea = ""
-testType = ""
+testing_area = ""
+test_type = ""
 
-isEnd = False
-isFileIgnored = False
-isFileTagged = False
-isStart = False
-showInfo = False
-showMissingFiles = False
+is_end = False
+is_file_ignored = False
+is_file_tagged = False
+is_start = False
+show_info = False
+show_missing_files = False
 
-nbIgnoredFiles = 0
-nbMissingFiles = 0
-nbValidFiles = 0
+nb_ignored_files = 0
+nb_missing_files = 0
+nb_valid_files = 0
 
-sortedTags = []
-testFiles = []
+sorted_tags = []
+test_files = []
 
-taggedFiles = {}
-validTags = {}
+tagged_files = {}
+valid_tags = {}
 
 END_TAG = "[END_TAGS]"
 IGNORE_FILE = "ignored_file"
@@ -38,9 +38,9 @@ for arg in sys.argv:
         print("\t-p\tShow files with no test tags")
         exit()
     elif arg == "-i":
-        showInfo = True
+        show_info = True
     elif arg == "-p":
-        showMissingFiles = True
+        show_missing_files = True
 #####
 
 ##### GET ALL TEST FILES #####
@@ -49,35 +49,35 @@ for root, dirs, files in os.walk("../test/"):
     for file in files:
         filename = os.path.join('/'.join(path), file)
         if filename.endswith("main.c") or filename.endswith(".py"):
-            testFiles.append(filename)
+            test_files.append(filename)
 #####
 
 ##### RETRIEVE VALID TAGS #####
-validationFile = open("test_tags.ok", "r")
+validation_file = open("test_tags.ok", "r")
 
 # The file has the following pattern
 # <COMPONENT>:<TESTING_TYPE>:<TESTING_AREA>:<DESCRIPTION>
 # A tag is made of the three first values: COMPONENT, TEST_TYPE and TESTING_AREA
-tags = validationFile.readlines()
+tags = validation_file.readlines()
 tags = [tag.replace('\n', '') for tag in tags]
 
 for tag in tags:
-    currentLine = tag.split(':')
+    current_line = tag.split(':')
     # Createa key value pair <TAG>:<DESCRIPTION>
-    validTags[':'.join(currentLine[:NB_TAG_ARGS])] = ':'.join(currentLine[NB_TAG_ARGS:])
+    valid_tags[':'.join(current_line[:NB_TAG_ARGS])] = ':'.join(current_line[NB_TAG_ARGS:])
 
-validationFile.close()
+validation_file.close()
 #####
 
 ##### PARSE TEST FILES #####
-for filename in testFiles:
-    inputFile = open(filename, "r")
-    lines = inputFile.readlines()
+for filename in test_files:
+    input_file = open(filename, "r")
+    lines = input_file.readlines()
 
-    isStart = False
-    isEnd = False
-    isFileIgnored = False
-    isFileTagged = False
+    is_start = False
+    is_end = False
+    is_file_ignored = False
+    is_file_tagged = False
 
     # Read line by line
     for line in lines:
@@ -89,7 +89,7 @@ for filename in testFiles:
         # Check if line is valid
         if not line:
             # Check if invalid line after START_TAG
-            if isStart == True:
+            if is_start == True:
                 print("Error syntax in file " + filename)
                 exit()
             else:
@@ -98,106 +98,106 @@ for filename in testFiles:
         # Check if end of test tag
         if END_TAG in line:
             # END_TAG should not be before START_TAG
-            if isStart == False:
+            if is_start == False:
                 print("Error syntax in file " + filename + ". Unexpected tag: " + END_TAG)
                 exit()
             # END_TAG should not be met before a test tag
-            if isFileIgnored == False and isFileTagged == False:
+            if is_file_ignored == False and is_file_tagged == False:
                 print("Error syntax in file " + filename + ". Missing test tag.")
                 exit()
-            isEnd = True
-            nbValidFiles = nbValidFiles + 1
+            is_end = True
+            nb_valid_files = nb_valid_files + 1
             # Go to next file
             break
 
         # Check if start of test tag
         if START_TAG in line:
             # Only one START_TAG is allowed
-            if isStart == True:
+            if is_start == True:
                 print("Error syntax in file " + filename + ". Unexpected tag: " + START_TAG)
                 exit()
-            isStart = True
+            is_start = True
             continue
 
-        if isStart == True:
+        if is_start == True:
             # Check if file is ignored
-            if isFileIgnored == True:
+            if is_file_ignored == True:
                 print("Unexpected value in ignored file: " + filename)
                 exit()
             if line == IGNORE_FILE:
-                nbIgnoredFiles = nbIgnoredFiles + 1
-                isFileIgnored = True
+                nb_ignored_files = nb_ignored_files + 1
+                is_file_ignored = True
                 continue
             # Check if current tag is valid
-            if not line in validTags:
+            if not line in valid_tags:
                 print("Tag is not valid ! Add the new tag to test_tags.ok:\n" + line)
                 exit()
             else:
-                isFileTagged = True
+                is_file_tagged = True
             # Check if current tag has already matched test files
-            if line in taggedFiles:
-                taggedFiles[line].append(filename)
+            if line in tagged_files:
+                tagged_files[line].append(filename)
             else:
-                taggedFiles[line] = [filename]
+                tagged_files[line] = [filename]
 
-    if isFileIgnored == False and isFileTagged == False:
-        nbMissingFiles = nbMissingFiles + 1
-        if showMissingFiles == True:
+    if is_file_ignored == False and is_file_tagged == False:
+        nb_missing_files = nb_missing_files + 1
+        if show_missing_files == True:
             print("Missing test tag in file: " + filename)
 
-    inputFile.close()
+    input_file.close()
 #####
 
 ##### GENERATE OUTPUT #####
-outputFile = open("../test/test_coverage.md", "w")
+output_file = open("../test/test_coverage.md", "w")
 
 # Table headers
-outputFile.write("|Component|Test Type|Testing Area|Description|Existing tests|" + '\n')
-outputFile.write("|---|---|---|---|---|" + '\n')
+output_file.write("|Component|Test Type|Testing Area|Description|Existing tests|" + '\n')
+output_file.write("|---|---|---|---|---|" + '\n')
 
 # Sort tags
-sortedTags = list(taggedFiles.keys())
-sortedTags.sort()
+sorted_tags = list(tagged_files.keys())
+sorted_tags.sort()
 
-for tag in sortedTags:
+for tag in sorted_tags:
     # Split line
-    currentLine = tag.split(":")
+    current_line = tag.split(":")
 
     # Parse tag
-    component = currentLine[0]
-    testType = currentLine[1]
-    testingArea = currentLine[2]
+    component = current_line[0]
+    test_type = current_line[1]
+    testing_area = current_line[2]
 
     # Format output
     component = component.replace("_", " ").title()
-    testType = testType.replace("_", " ").title()
-    testingArea = testingArea.replace("_", " ").title()
+    test_type = test_type.replace("_", " ").title()
+    testing_area = testing_area.replace("_", " ").title()
 
     # Relative path to test file
     link = ""
-    for name in taggedFiles[tag]:
+    for name in tagged_files[tag]:
         link += "[" + name + "](" + name + "), "
     # Remove the extra ", " at the end
     link = link[:-2]
 
     # Write to output
-    outputFile.write('|' + component + '|' + testType + '|' + \
-                     testingArea + '|' + validTags[tag] + '|' \
+    output_file.write('|' + component + '|' + test_type + '|' + \
+                     testing_area + '|' + valid_tags[tag] + '|' \
                      + link + '\n')
 
-outputFile.close()
+output_file.close()
 #####
 
 ##### STATS #####
-if showInfo == True:
-    print("Tagged files:\t" + str(nbValidFiles - nbIgnoredFiles))
-    print("Missing files:\t" + str(nbMissingFiles))
-    print("Ignored files:\t" + str(nbIgnoredFiles))
-    print("Total files:\t" + str(nbValidFiles + nbMissingFiles))
+if show_info == True:
+    print("Tagged files:\t" + str(nb_valid_files - nb_ignored_files))
+    print("Missing files:\t" + str(nb_missing_files))
+    print("Ignored files:\t" + str(nb_ignored_files))
+    print("Total files:\t" + str(nb_valid_files + nb_missing_files))
 #####
 
 # Enforce tagging
-#if nbMissingFiles > 0:
-#    print("Files missing a tag: " + str(nbMissingFiles))
-#    if showMissingFiles == False:
+#if nb_missing_files > 0:
+#    print("Files missing a tag: " + str(nb_missing_files))
+#    if show_missing_files == False:
 #        print("Call \'python test_tag.py -p\' to list all files with no tags")
