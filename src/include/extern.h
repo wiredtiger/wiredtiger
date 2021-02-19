fc0435496843b2d8ff133ccbd20f6618e0259ed6
Don Anderson <donald.anderson@mongodb.com>
2021-02-19 00:46:47 +0000

    Remove __wt_storage_source_config from unused function list.

--- src/include/extern.h
+++ src/include/extern.h
@@ -1401,8 +1401,6 @@ extern int __wt_statlog_create(WT_SESSION_IMPL *session, const char *cfg[])
   WT_GCC_FUNC_DECL_ATTRIBUTE((warn_unused_result));
 extern int __wt_statlog_destroy(WT_SESSION_IMPL *session, bool is_close)
   WT_GCC_FUNC_DECL_ATTRIBUTE((warn_unused_result));
-extern int __wt_storage_source_config(WT_SESSION_IMPL *session, const char *config,
-  WT_STORAGE_SOURCE **storage_sourcep, int *ownp) WT_GCC_FUNC_DECL_ATTRIBUTE((warn_unused_result));
 extern int __wt_str_name_check(WT_SESSION_IMPL *session, const char *str)
   WT_GCC_FUNC_DECL_ATTRIBUTE((warn_unused_result));
 extern int __wt_strndup(WT_SESSION_IMPL *session, const void *str, size_t len, void *retp)
