3c5b1559c6838e183335fa7071345649a3e8e9b2
Jie Chen <jie.chen@mongodb.com>
2021-02-26 04:38:48 +0000

    Added cursor close

--- src/history/hs_conn.c
+++ src/history/hs_conn.c
@@ -60,16 +60,16 @@ __hs_cleanup_las(WT_SESSION_IMPL *session)
 int
 __wt_hs_get_btree(WT_SESSION_IMPL *session, WT_BTREE **hs_btreep)
 {
-    WT_CURSOR_HS *hs_cursor;
+    WT_CURSOR *hs_cursor;
+    WT_CURSOR_BTREE *cbt;
     WT_DECL_RET;
     *hs_btreep = NULL;
 
     WT_RET(__wt_curhs_open(session, NULL, &hs_cursor));
-    *hs_btreep = CUR2BT(hs_cursor->file_cursor);
+    cbt = __wt_hs_cbt(hs_cursor);
+    *hs_btreep = CUR2BT(cbt);
     WT_ASSERT(session, *hs_btreep != NULL);
-
-    WT_TRET(__wt_hs_cursor_close(session));
-
+    WT_TRET(hs_cursor->close(hs_cursor));
     return (ret);
 }
 
