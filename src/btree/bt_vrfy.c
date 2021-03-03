558b591edd01c77e1f28d3f52376631b10f3c107
Haseeb Bokhari <haseeb.bokhari@mongodb.com>
2021-03-03 04:31:59 +0000

    Use UINT64_MAX for maximum counter value

--- src/btree/bt_vrfy.c
+++ src/btree/bt_vrfy.c
@@ -794,7 +794,7 @@ __verify_key_hs(
      * Open a history store cursor positioned at the end of the data store key (the newest record)
      * and iterate backwards until we reach a different key or btree.
      */
-    hs_cursor->set_key(hs_cursor, 4, hs_btree_id, tmp1, WT_TS_MAX, WT_TXN_MAX);
+    hs_cursor->set_key(hs_cursor, 4, hs_btree_id, tmp1, WT_TS_MAX, UINT64_MAX);
     ret = __wt_curhs_search_near_before(session, hs_cursor);
 
     for (; ret == 0; ret = hs_cursor->prev(hs_cursor)) {
