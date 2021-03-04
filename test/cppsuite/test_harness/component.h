d1c22041e28722ee18dca91ced9a809d5fa7ab63
Luke Pearson <luke.pearson@mongodb.com>
2021-03-04 15:13:04 +1100

    More typos

--- test/cppsuite/test_harness/component.h
+++ test/cppsuite/test_harness/component.h
@@ -37,7 +37,7 @@ namespace test_harness {
 class component {
     public:
     /*
-     * The load function should perform all tasked required to setup the component for the main
+     * The load function should perform all tasks required to setup the component for the main
      * phase of the test. An example operation performed in the load phase would be populating a
      * database.
      */
