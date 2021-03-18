73dd70bfbf0a96720a8ad7eb36884d1c238df926
Etienne Petrel <etienne.petrel@mongodb.com>
2021-03-18 05:30:04 +0000

    WT-7268 Added missing brackets

--- test/cppsuite/tests/poc.cxx
+++ test/cppsuite/tests/poc.cxx
@@ -91,7 +91,7 @@ main(int argc, char *argv[])
         poc_test(cfg, trace_level).run();
     } catch (const std::exception &e) {
         std::cout << e.what() << std::endl;
-        return -1;
+        return (-1);
     }
 
     return (0);
