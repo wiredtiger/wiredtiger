fd3fa279aa3524f79f9e537c6607be5db21685f8
Alison Felizzi <alison.felizzi@mongodb.com>
2021-03-24 03:52:28 +0000

    X86 platform specific configurations
    Using the initial set of base CMake configuration variables,
    defined default configuration values for our x86 Linux and Darwin
    platforms. We intend to initialise early with these configurations
    to ensure the rest of the CMake build will appropriately build
    WiredTiger with the correct settings in mind.

---
+++ build_cmake/configs/x86/darwin/config.cmake
@@ -0,0 +1,13 @@
+#
+# Public Domain 2014-present MongoDB, Inc.
+# Public Domain 2008-2014 WiredTiger, Inc.
+#  All rights reserved.
+#
+#  See the file LICENSE for redistribution information
+#
+
+cmake_minimum_required(VERSION 3.11.0)
+
+set(WT_ARCH "x86" CACHE STRING "")
+set(WT_OS "darwin" CACHE STRING "")
+set(WT_POSIX ON CACHE BOOL "")
