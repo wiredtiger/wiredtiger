e1a8477b8e8d012f806134bbecd265ade45121aa
Alison Felizzi <alison.felizzi@mongodb.com>
2021-03-30 05:28:41 +0000

    Added clarifying comment on usage of '_GNU_SOURCE'
    Added comment clarifying on the use of _GNU_SOURCE for linux
    builds.

--- build_cmake/configs/x86/linux/config.cmake
+++ build_cmake/configs/x86/linux/config.cmake
@@ -12,6 +12,9 @@ set(WT_ARCH "x86" CACHE STRING "")
 set(WT_OS "linux" CACHE STRING "")
 set(WT_POSIX ON CACHE BOOL "")
 
+# Linux requires '_GNU_SOURCE' to be defined for access to GNU/Linux extension functions
+# e.g. Access to O_DIRECT on Linux. Append this macro to our compiler flags for Linux-based
+# builds.
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -D_GNU_SOURCE" CACHE STRING "" FORCE)
 
 # Linux requires buffers aligned to 4KB boundaries for O_DIRECT to work.
