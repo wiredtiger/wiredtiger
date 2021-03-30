02c98f6cbd48031395a4543ade36e1f846c98465
Alison Felizzi <alison.felizzi@mongodb.com>
2021-03-30 05:55:41 +0000

    Doc strings and comments with complete sentences
    Revised the comments and document strings throughout the CMake
    source and ensured they are complete sentences ended with a
    period. This adhering to styling conventions.

--- build_cmake/install/install.cmake
+++ build_cmake/install/install.cmake
@@ -10,24 +10,24 @@ include(GNUInstallDirs)
 
 # Library installs
 
-# Define the public headers for wiredtiger library to be used when installing the target
+# Define the public headers for wiredtiger library to be used when installing the target.
 set_property(
     TARGET wiredtiger
     PROPERTY PUBLIC_HEADER
     ${CMAKE_BINARY_DIR}/include/wiredtiger.h
     ${CMAKE_SOURCE_DIR}/src/include/wiredtiger_ext.h
 )
-# Set the version property of the wiredtiger library so we can export a versioned install
+# Set the version property of the wiredtiger library so we can export a versioned install.
 set_target_properties(wiredtiger PROPERTIES VERSION "${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_PATCH}")
 
-# Install the wiredtiger library target
+# Install the wiredtiger library target.
 install(TARGETS wiredtiger
     LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}
     ARCHIVE DESTINATION ${CMAKE_INSTALL_LIBDIR}
     PUBLIC_HEADER DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}
 )
 
-# Create our wiredtiger pkgconfig (for POSIX builds)
+# Create our wiredtiger pkgconfig (for POSIX builds).
 if(WT_POSIX)
     configure_file(${CMAKE_CURRENT_LIST_DIR}/wiredtiger.pc.in wiredtiger.pc @ONLY)
     install(
@@ -36,7 +36,7 @@ if(WT_POSIX)
     )
 endif()
 
-# Install our wiredtiger compressor extensions (provided we have enabled/built them)
+# Install our wiredtiger compressor extensions (provided we have enabled/built them).
 if(HAVE_BUILTIN_EXTENSION_LZ4 OR ENABLE_LZ4)
     install(TARGETS wiredtiger_lz4
         LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}
