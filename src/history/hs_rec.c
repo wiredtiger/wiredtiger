162b928848d5aa7cffc67a661518baa423ec04c4
Chenhao Qu <chenhao.qu@mongodb.com>
2021-04-06 04:34:15 +0000

    WT-7384 Fix an assert fire when inserting to the history store

--- src/history/hs_rec.c
+++ src/history/hs_rec.c
@@ -142,13 +142,15 @@ __hs_insert_record(WT_SESSION_IMPL *session, WT_CURSOR *cursor, WT_BTREE *btree,
               &upd_type_full_diag, existing_val));
             WT_ERR(__wt_compare(session, NULL, existing_val, hs_value, &cmp));
             /*
-             * We shouldn't be inserting the same value again for the key unless coming from a
-             * different transaction. If the updates are from the same transaction, the start
-             * timestamp for each update should be different.
+             * We shouldn't be inserting the same value again for the key unless it comes from a
+             * different transaction or it is previously deleted by rollback to stable or
+             * prepared rollback. If the updates are from the same transaction, the start timestamp
+             * for each update should be different.
              */
             if (cmp == 0)
                 WT_ASSERT(session,
-                  tw->start_txn == WT_TXN_NONE ||
+                  __wt_txn_tw_stop_visible_all(session, hs_cbt->upd_value->tw) ||
+                    tw->start_txn == WT_TXN_NONE ||
                     tw->start_txn != hs_cbt->upd_value->tw.start_txn ||
                     tw->start_ts != hs_cbt->upd_value->tw.start_ts);
             counter = hs_counter + 1;
