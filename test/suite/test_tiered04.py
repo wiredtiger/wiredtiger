f608d5b0f2f2b195832e4eca0e096130b3c2a09f
Susan LoVerso <sue@mongodb.com>
2021-04-20 17:01:20 -0400

    Comment out the reconfigure portion of the test.

--- test/suite/test_tiered04.py
+++ test/suite/test_tiered04.py
@@ -125,14 +125,14 @@ class test_tiered04(wttest.WiredTigerTestCase):
         self.session.flush_tier('force=true')
         calls = self.get_stat(stat.conn.flush_tier, None)
         self.assertEqual(calls, 4)
-        new = self.retention * 2
-        config = 'tiered_storage=(local_retention=%d)' % new
-        self.conn.reconfigure(config)
-        self.session.flush_tier(None)
-        retain = self.get_stat(stat.conn.tiered_retention, None)
-        calls = self.get_stat(stat.conn.flush_tier, None)
-        self.assertEqual(retain, new)
-        self.assertEqual(calls, 5)
+        #new = self.retention * 2
+        #config = 'tiered_storage=(local_retention=%d)' % new
+        #self.conn.reconfigure(config)
+        #self.session.flush_tier(None)
+        #retain = self.get_stat(stat.conn.tiered_retention, None)
+        #calls = self.get_stat(stat.conn.flush_tier, None)
+        #self.assertEqual(retain, new)
+        #self.assertEqual(calls, 5)
 
 if __name__ == '__main__':
     wttest.run()
