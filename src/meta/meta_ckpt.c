ea0d3588a2c61a92a8672cbb07a57813b9601fcc
Jie Chen <jie.chen@mongodb.com>
2021-05-12 10:40:21 +1000

    Address final PR changes

--- src/meta/meta_ckpt.c
+++ src/meta/meta_ckpt.c
@@ -14,7 +14,7 @@ static int __ckpt_load(WT_SESSION_IMPL *, WT_CONFIG_ITEM *, WT_CONFIG_ITEM *, WT
 static int __ckpt_named(WT_SESSION_IMPL *, const char *, const char *, WT_CKPT *);
 static int __ckpt_set(WT_SESSION_IMPL *, const char *, const char *, bool);
 static int __ckpt_version_chk(WT_SESSION_IMPL *, const char *, const char *);
-
+static int meta_blk_mods_load(WT_SESSION_IMPL *, const char *, WT_CKPT *, bool);
 /*
  * __ckpt_load_blk_mods --
  *     Load the block information from the config string.
@@ -495,12 +495,12 @@ __ckpt_copy_blk_mods(WT_SESSION_IMPL *session, WT_CKPT *src_ckpt, WT_CKPT *dst_c
 }
 
 /*
- * __wt_meta_blk_mods_load --
+ * meta_blk_mods_load --
  *     Load the block mods for a given checkpoint and set up all the information to store. Load from
  *     either the metadata or from a base checkpoint.
  */
 int
-__wt_meta_blk_mods_load(
+meta_blk_mods_load(
   WT_SESSION_IMPL *session, const char *config, WT_CKPT *base_ckpt, WT_CKPT *ckpt, bool rename)
 {
     /*
@@ -674,8 +674,8 @@ __meta_ckptlist_allocate_new_ckpt(
     }
 
     /* Either load block mods from the config, or from the previous checkpoint. */
-    WT_RET(__wt_meta_blk_mods_load(
-      session, config, (slot == 0 ? NULL : &ckptbase[slot - 1]), ckpt, false));
+    WT_RET(
+      meta_blk_mods_load(session, config, (slot == 0 ? NULL : &ckptbase[slot - 1]), ckpt, false));
     WT_ASSERT(session, ckpt->block_metadata != NULL);
 
     return (0);
@@ -1334,7 +1334,7 @@ __wt_reset_blkmod(WT_SESSION_IMPL *session, const char *orig_config, WT_ITEM *bu
      * backup information to indicate copying the entire file in its bitmap.
      */
     /* First load any existing backup information into a temp checkpoint structure. */
-    WT_RET(__wt_meta_blk_mods_load(session, orig_config, &ckpt, true));
+    WT_RET(meta_blk_mods_load(session, orig_config, &ckpt, true));
 
     /* Take the checkpoint structure and generate the metadata string. */
     ret = __wt_ckpt_blkmod_to_meta(session, buf, &ckpt);
