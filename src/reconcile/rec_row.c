6e0845d01c71d196d303d9f258bd87d4c682066f
Keith Bostic <keith.bostic@mongodb.com>
2021-05-23 00:56:44 +0000

    Cache the HS cursor for the entire page reconciliation, there's no point to doing an open/close cycle on every key that requires a HS update.
    Lift the complex test limiting when we update the HS on key removal out of the main loop, it's
    chock full of cache misses, at best.

--- src/reconcile/rec_row.c
+++ src/reconcile/rec_row.c
@@ -726,23 +726,39 @@ __wt_rec_row_leaf(
     uint64_t slvg_skip;
     uint32_t i;
     uint8_t key_prefix;
-    bool dictionary, key_onpage_ovfl, ovfl_key;
+    bool dictionary, hs_clear, key_onpage_ovfl, ovfl_key;
     void *copy;
     const void *key_data;
 
     btree = S2BT(session);
-    hs_cursor = NULL;
     page = pageref->page;
     twp = NULL;
     upd = NULL;
     slvg_skip = salvage == NULL ? 0 : salvage->skip;
 
-    cbt = &r->update_modify_cbt;
-    cbt->iface.session = (WT_SESSION *)session;
-
     key = &r->k;
     val = &r->v;
     vpack = &_vpack;
+
+    cbt = &r->update_modify_cbt;
+    cbt->iface.session = (WT_SESSION *)session;
+
+    /*
+     * When removing a key due to a tombstone with a durable timestamp of "none", also remove the
+     * history store contents associated with that key. It's safe to do even if we fail
+     * reconciliation after the removal, the history store content must be obsolete in order for us
+     * to consider removing the key.
+     *
+     * Ignore if this is metadata, as metadata doesn't have any history.
+     *
+     * Some code paths, such as schema removal, involve deleting keys in metadata and assert that
+     * they shouldn't open new dhandles. In those cases we won't ever need to blow away history
+     * store content, so we can skip this.
+     */
+    hs_cursor = NULL;
+    hs_clear = F_ISSET(S2C(session), WT_CONN_HS_OPEN) &&
+      !F_ISSET(session, WT_SESSION_NO_DATA_HANDLES) && !WT_IS_HS(btree->dhandle) &&
+      !WT_IS_METADATA(btree->dhandle);
 
     WT_RET(__wt_rec_split_init(session, r, page, 0, btree->maxleafpage_precomp));
 
@@ -898,33 +914,19 @@ __wt_rec_row_leaf(
                 }
 
                 /*
-                 * If we're removing a key due to a tombstone with a durable timestamp of "none",
-                 * also remove the history store contents associated with that key. Even if we fail
-                 * reconciliation after this point, we're safe to do this. The history store content
-                 * must be obsolete in order for us to consider removing the key.
-                 *
-                 * Ignore if this is metadata, as metadata doesn't have any history.
-                 *
-                 * Some code paths, such as schema removal, involve deleting keys in metadata and
-                 * assert that they shouldn't open new dhandles. In those cases we won't ever need
-                 * to blow away history store content, so we can skip this.
+                 * When removing a key due to a tombstone with a durable timestamp of "none", also
+                 * remove the history store contents associated with that key.
                  */
-                if (twp->durable_stop_ts == WT_TS_NONE && F_ISSET(S2C(session), WT_CONN_HS_OPEN) &&
-                  !F_ISSET(session, WT_SESSION_NO_DATA_HANDLES) && !WT_IS_HS(btree->dhandle) &&
-                  !WT_IS_METADATA(btree->dhandle)) {
+                if (twp->durable_stop_ts == WT_TS_NONE && hs_clear) {
                     WT_ERR(__wt_row_leaf_key(session, page, rip, tmpkey, true));
 
-                    /*
-                     * FIXME-WT-7053: we will hit the dhandle deadlock if we open multiple history
-                     * store cursors in reconciliation. Once it is fixed, we can move the open and
-                     * close of the history store cursor inside the delete key function.
-                     */
-                    WT_ERR(__wt_curhs_open(session, NULL, &hs_cursor));
+                    /* Open a history store cursor if we don't yet have one. */
+                    if (hs_cursor == NULL)
+                        WT_ERR(__wt_curhs_open(session, NULL, &hs_cursor));
+
                     /* From WT_TS_NONE to delete all the history store content of the key. */
                     WT_ERR(__wt_hs_delete_key_from_ts(
                       session, hs_cursor, btree->id, tmpkey, WT_TS_NONE, false));
-                    WT_ERR(hs_cursor->close(hs_cursor));
-                    hs_cursor = NULL;
 
                     WT_STAT_CONN_INCR(session, cache_hs_key_truncate_onpage_removal);
                     WT_STAT_DATA_INCR(session, cache_hs_key_truncate_onpage_removal);
