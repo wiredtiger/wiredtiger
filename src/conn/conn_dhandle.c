55fe9a0f700bd0ca848b930dec1242eba378940e
Etienne Petrel <77254506+etienneptl@users.noreply.github.com>
2021-05-24 08:14:02 +1000

    WT-7135 Additional checks to detect when writing corrupted metadata. (#6571)
    (cherry picked from commit 9acb906193234331be4978d07bb3ffe710ece6ed)

--- src/conn/conn_dhandle.c
+++ src/conn/conn_dhandle.c
@@ -26,6 +26,9 @@ __conn_dhandle_config_clear(WT_SESSION_IMPL *session)
         __wt_free(session, *a);
     __wt_free(session, dhandle->cfg);
     __wt_free(session, dhandle->meta_base);
+#ifdef HAVE_DIAGNOSTIC
+    __wt_free(session, dhandle->orig_meta_base);
+#endif
 }
 
 /*
@@ -79,18 +82,17 @@ __conn_dhandle_config_set(WT_SESSION_IMPL *session)
          */
         cfg[0] = metaconf;
         cfg[1] = "checkpoint=()";
-        cfg[2] = NULL;
+        cfg[2] = "checkpoint_backup_info=()";
+        cfg[3] = NULL;
         WT_ERR(__wt_strdup(session, WT_CONFIG_BASE(session, file_meta), &dhandle->cfg[0]));
         WT_ASSERT(session, dhandle->meta_base == NULL);
-        /*
-         * First collapse and overwrite any checkpoint information because we do not know the name
-         * or how many checkpoints may be in this metadata. So first we have to set the string to
-         * the empty checkpoint string and call collapse to overwrite anything existing.
-         */
+#ifdef HAVE_DIAGNOSTIC
+        WT_ASSERT(session, dhandle->orig_meta_base == NULL);
+#endif
         WT_ERR(__wt_config_collapse(session, cfg, &tmp));
         /*
-         * Now strip out the checkpoint and checkpoint LSN items from the configuration string and
-         * that is now our base metadata string.
+         * Now strip out the checkpoint related items from the configuration string and that is now
+         * our base metadata string.
          */
         cfg[0] = tmp;
         cfg[1] = NULL;
@@ -103,6 +105,14 @@ __conn_dhandle_config_set(WT_SESSION_IMPL *session)
     }
     dhandle->cfg[1] = metaconf;
     dhandle->meta_base = base;
+    dhandle->meta_base_length = base == NULL ? 0 : strlen(base);
+#ifdef HAVE_DIAGNOSTIC
+    /*  Save the original metadata value for further check to avoid writing corrupted data. */
+    if (base == NULL)
+        dhandle->orig_meta_base = NULL;
+    else
+        WT_ERR(__wt_strdup(session, base, &dhandle->orig_meta_base));
+#endif
     return (0);
 
 err:
