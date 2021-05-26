b372c55df6a8d740f287d2bbac8cb93b8f2e68b6
Don Anderson <donald.anderson@mongodb.com>
2021-05-26 18:05:19 +0000

    Remove old/unneeded code. Added FIXME comment.

--- src/block/block_tiered.c
+++ src/block/block_tiered.c
@@ -19,31 +19,11 @@ __block_switch_writeable(WT_SESSION_IMPL *session, WT_BLOCK *block, uint64_t obj
 
     WT_ERR(__wt_close(session, &block->fh));
 
+    /*
+     * FIXME-WT-7470: write lock while opening a new write handle.
+     */
     WT_ERR(block->opener->open(
       block->opener, session, object_id, WT_FS_OPEN_FILE_TYPE_DATA, block->file_flags, &block->fh));
-
-#if 0
-    /*
-     * TODO: tiered: examine this prototype code, and see if we should use any of its ideas. It uses
-     * a different code path to create a new object file, and sets up a checkpoint entry.
-     */
-
-    /* Bump to a new file ID. */
-    ++block->objectid;
-    WT_ERR(__wt_buf_fmt(session, tmp, "%s.%08" PRIu32, block->name, block->objectid));
-    filename = tmp->data;
-
-    WT_WITH_BUCKET_STORAGE(session->bucket_storage, session, {
-        ret = __wt_open(session, filename, WT_FS_OPEN_FILE_TYPE_DATA,
-          WT_FS_OPEN_CREATE | block->file_flags, &block->fh);
-    });
-    WT_ERR(ret);
-    WT_ERR(__wt_desc_write(session, block->fh, block->allocsize));
-
-    block->size = block->allocsize;
-    __wt_block_ckpt_destroy(session, &block->live);
-    WT_ERR(__wt_block_ckpt_init(session, &block->live, "live"));
-#endif
 
 err:
     return (ret);
@@ -59,14 +39,15 @@ __wt_block_tiered_fh(WT_SESSION_IMPL *session, WT_BLOCK *block, uint32_t object_
     WT_DECL_ITEM(tmp);
     WT_DECL_RET;
 
-    /* TODO: tiered: fh readlock; we may want a reference count on each file handle given out. */
-
+    /*
+     * FIXME-WT-7470: take a read lock to get a handle, and a write lock to open a handle or extend
+     * the array.
+     */
     if (object_id * sizeof(WT_FILE_HANDLE *) < block->ofh_alloc &&
       (*fhp = block->ofh[object_id]) != NULL)
         return (0);
 
-    /* TODO: tiered: fh writelock */
-    /* Ensure the array goes far enough. */
+    /* Ensure the array is big enough. */
     WT_RET(__wt_realloc_def(session, &block->ofh_alloc, object_id + 1, &block->ofh));
     if (object_id >= block->max_objectid)
         block->max_objectid = object_id + 1;
@@ -107,20 +88,18 @@ __wt_block_switch_object(
 
 /*
  * __wt_block_tiered_load --
- *     Set up log-structured processing when loading a new root page.
+ *     Set up object file processing when loading a new root page.
  */
 int
 __wt_block_tiered_load(WT_SESSION_IMPL *session, WT_BLOCK *block, WT_BLOCK_CKPT *ci)
 {
-    /*
-     * TODO: tiered: this call currently advances the object id, that's probably not appropriate for
-     * readonly opens. Perhaps it's also not appropriate for opening at an older checkpoint?
-     */
-    if (block->has_objects) {
+    WT_UNUSED(session);
+
+    if (block->has_objects)
         block->objectid = ci->root_objectid;
 
-        /* Advance to the next file for future changes. */
-        WT_RET(__block_switch_writeable(session, block, ci->root_objectid + 1));
-    }
+    /*
+     * TODO: tiered: there will be more here.
+     */
     return (0);
 }
