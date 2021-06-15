920f54a966b739224e33cc37372b1021197b22df
Alexey Anisimov <alexey.anisimov@mongodb.com>
2021-06-15 04:20:25 +0000

    Updated comment in test_bulk03.py

--- test/suite/test_bulk03.py
+++ test/suite/test_bulk03.py
@@ -26,7 +26,7 @@
 # ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 # OTHER DEALINGS IN THE SOFTWARE.
 #
-# test_bulk04.py
+# test_bulk03.py
 #       colgroup bulk-cursor test.
 #
 
