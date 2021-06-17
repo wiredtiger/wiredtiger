9fefc1b85d24acabf8ad183777caccee68fc164a
Jie Chen <jie.chen@mongodb.com>
2021-06-17 13:53:31 +1000

    Added start and stop limits for all flag generation, and updated flags to offset from start and stop as the limit

--- dist/flags.py
+++ dist/flags.py
@@ -18,30 +18,44 @@ def flag_declare(name):
 
         lcnt = 0
         parsing = False
+        start_flag = 0
         for line in f:
             lcnt = lcnt + 1
             if line.find('AUTOMATIC FLAG VALUE GENERATION START') != -1:
+                m = re.search("\d+", line)
+                if m == None:
+                    print(name + ": automatic flag generation start line " + str(lcnt) +\
+                        "contains no start value", file=sys.stderr)
+                    sys.exit(1)
+                start_flag = int(m.group(0))
                 header = line
                 defines = []
                 parsing = True
             elif line.find('AUTOMATIC FLAG VALUE GENERATION STOP') != -1:
-                # We only support 64 bits.
-                if len(defines) > 64:
+                m = re.search("\d+", line)
+                if m == None:
+                    print(name + ": automatic flag generation stop line " + str(lcnt) +\
+                        "contains no stop value", file=sys.stderr)
+                    sys.exit(1)
+                limit = int(m.group(0))
+                if len(defines) > limit:
                     print(name + ": line " + str(lcnt) +\
-                          ": exceeds maximum 64 bit flags", file=sys.stderr)
+                          ": exceeds maximum {0} limit bit flags".format(limit), file=sys.stderr)
                     sys.exit(1)
 
                 # Calculate number of hex bytes, create format string
-                fmt = "0x%%0%dxu" % ((len(defines) + 3) / 4)
+                fmt = "0x%%0%dxu" % ((start_flag + len(defines) + 3) / 4)
 
+                # Generate the flags starting from an offset set from the start value.
                 tfile.write(header)
-                v = 1
+                v = 1 << start_flag
                 for d in sorted(defines):
                     tfile.write(re.sub("0x[01248u]*", fmt % v, d))
                     v = v * 2
                 tfile.write(line)
 
                 parsing = False
+                start_flag = 0
             elif parsing and line.find('#define') == -1:
                 print(name + ": line " + str(lcnt) +\
                       ": unexpected flag line, no #define", file=sys.stderr)
