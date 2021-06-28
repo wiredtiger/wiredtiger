3645aa6f46322cc0a2495f45444f3c27c2774c38
Keith Smith <keith.smith@mongodb.com>
2021-06-28 23:21:22 +0000

    Remove unused prefix variable from several tiered tests.

--- test/suite/test_tiered08.py
+++ test/suite/test_tiered08.py
@@ -50,7 +50,6 @@ class test_tiered08(wttest.WiredTigerTestCase):
     bucket = "mybucket"
     bucket_prefix = "pfx_"
     extension_name = "local_store"
-    prefix = "pfx-"
 
     def conn_config(self):
         if not os.path.exists(self.bucket):
@@ -59,7 +58,7 @@ class test_tiered08(wttest.WiredTigerTestCase):
           'statistics=(all),' + \
           'tiered_storage=(auth_token=%s,' % self.auth_token + \
           'bucket=%s,' % self.bucket + \
-          'bucket_prefix=%s,' % self.prefix + \
+          'bucket_prefix=%s,' % self.bucket_prefix + \
           'name=%s),tiered_manager=(wait=0)' % self.extension_name
 
     # Load the local store extension, but skip the test if it is missing.
