258693149cbac5accf565847515eb39c1d1bde37
Chenhao Qu <chenhao.qu@mongodb.com>
2021-06-29 04:05:14 +0000

    Fix crash

--- src/reconcile/rec_visibility.c
+++ src/reconcile/rec_visibility.c
@@ -668,7 +668,7 @@ __wt_rec_upd_select(WT_SESSION_IMPL *session, WT_RECONCILE *r, WT_INSERT *ins, v
      */
     WT_ASSERT(session, upd_select->upd == NULL || upd_select->upd->txnid != WT_TXN_ABORTED);
     /* We should never select an update that has been written to the history store. */
-    WT_ASSERT(session, upd_select->upd == NULL || !F_ISSET(upd, WT_UPDATE_HS));
+    WT_ASSERT(session, upd_select->upd == NULL || !F_ISSET(upd_select->upd, WT_UPDATE_HS));
     WT_ASSERT(session, tombstone == NULL || !F_ISSET(tombstone, WT_UPDATE_HS));
 
     /*
