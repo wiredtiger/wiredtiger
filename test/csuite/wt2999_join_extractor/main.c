42d017194f965892da644f38ebab2fe220bf0edd
Alexey Anisimov <alexey.anisimov@mongodb.com>
2021-06-30 00:59:19 +0000

    Change key and value data type from int32_t to uint32_t.

--- test/csuite/wt2999_join_extractor/main.c
+++ test/csuite/wt2999_join_extractor/main.c
@@ -86,8 +86,8 @@ main(int argc, char *argv[])
     WT_CURSOR *cursor1, *cursor2, *jcursor;
     WT_ITEM k, v;
     WT_SESSION *session;
-    int32_t key, val[2];
-    int i, ret;
+    uint32_t i, key, val[2];
+    int ret;
 
     opts = &_opts;
     memset(opts, 0, sizeof(*opts));
