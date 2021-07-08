4b43e003091c5848c4b731d1f67e0661a33ad72c
Chenhao Qu <chenhao.qu@mongodb.com>
2021-07-08 03:50:13 +0000

    Revert "Revert "Revert "Modify tests to bypass wt-7771"""
    This reverts commit b234e1e5d5a39b88933eeb1462a15da0d08089b3.

--- test/suite/test_hs24.py
+++ test/suite/test_hs24.py
@@ -41,16 +41,18 @@ class test_hs24(wttest.WiredTigerTestCase):
         # Check the data store and the history store content is consistent.
         # If we have a value in the data store, we should see the older
         # version in the history store as well.
+        checkpoint_started = False
         for i in range(0, 2000):
             cursor.set_key(str(i))
             cursor2.set_key(str(i))
             ret = cursor.search()
             ret2 = cursor2.search()
-            if ret == 0:
+            if not checkpoint_started:
+                checkpoint_started = ret != wiredtiger.WT_NOTFOUND
+            if checkpoint_started:
                 self.assertEquals(cursor.get_value(), self.value2)
                 self.assertEquals(cursor2.get_value(), self.value1)
             else:
-                self.assertEquals(ret, wiredtiger.WT_NOTFOUND)
                 self.assertEquals(ret2, wiredtiger.WT_NOTFOUND)
         session2.rollback_transaction()
         self.session.rollback_transaction()
@@ -88,10 +90,15 @@ class test_hs24(wttest.WiredTigerTestCase):
         self.session.begin_transaction('read_timestamp=' + timestamp_str(4))
         # Check we can only see the version committed by the zero timestamp
         # commit thread before the checkpoint starts or value1.
+        checkpoint_started = False
         for i in range(0, 2000):
             value = cursor[str(i)]
-            if value != self.value3:
+            if not checkpoint_started:
+                checkpoint_started = value != self.value3
+            if checkpoint_started:
                 self.assertEquals(value, self.value1)
+            else:
+                self.assertEquals(value, self.value3)
         self.session.rollback_transaction()
 
     def zero_ts_commits(self):
@@ -131,10 +138,15 @@ class test_hs24(wttest.WiredTigerTestCase):
         # Check we can only see the version at timestamp 4, it's either
         # committed by the out of order timestamp commit thread before the
         # checkpoint starts or value1.
+        checkpoint_started = False
         for i in range(0, 2000):
             value = cursor[str(i)]
-            if value != self.value4:
+            if not checkpoint_started:
+                checkpoint_started = value != self.value4
+            if checkpoint_started:
                 self.assertEquals(value, self.value1)
+            else:
+                self.assertEquals(value, self.value4)
         self.session.rollback_transaction()
 
     def out_of_order_ts_commits(self):
