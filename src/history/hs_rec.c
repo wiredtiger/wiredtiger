3ec2871e8ce68887c336c9c6a581a09dbc110bb0
Luke Pearson <luke.pearson@mongodb.com>
2021-09-03 14:16:28 +1000

    Revert "WT-8032 Add failpoints to the history store update insert path (#6959)"
    This reverts commit f958af5d61791fbaab69f8ccfea64fd5064fa649.

--- src/history/hs_rec.c
+++ src/history/hs_rec.c
@@ -267,7 +267,8 @@ __hs_next_upd_full_value(WT_SESSION_IMPL *session, WT_UPDATE_VECTOR *updates,
  *     fails or succeeds, if there is a successful write to history, cache_write_hs is set to true.
  */
 int
-__wt_hs_insert_updates(WT_SESSION_IMPL *session, WT_RECONCILE *r, WT_MULTI *multi)
+__wt_hs_insert_updates(WT_SESSION_IMPL *session, WT_PAGE *page, WT_MULTI *multi,
+  bool *cache_write_hs, bool checkpoint_running)
 {
     WT_BTREE *btree, *hs_btree;
     WT_CURSOR *hs_cursor;
@@ -293,10 +294,9 @@ __wt_hs_insert_updates(WT_SESSION_IMPL *session, WT_RECONCILE *r, WT_MULTI *mult
     uint32_t i;
     uint8_t *p;
     int nentries;
-    bool checkpoint_running, enable_reverse_modify, hs_inserted, squashed;
+    bool enable_reverse_modify, hs_inserted, squashed;
 
-    checkpoint_running = F_ISSET(r, WT_REC_CHECKPOINT_RUNNING);
-    r->cache_write_hs = false;
+    *cache_write_hs = false;
     btree = S2BT(session);
     prev_upd = NULL;
     insert_cnt = 0;
@@ -347,7 +347,7 @@ __wt_hs_insert_updates(WT_SESSION_IMPL *session, WT_RECONCILE *r, WT_MULTI *mult
             continue;
 
         /* History store table key component: source key. */
-        switch (r->page->type) {
+        switch (page->type) {
         case WT_PAGE_COL_FIX:
         case WT_PAGE_COL_VAR:
             p = key->mem;
@@ -356,8 +356,8 @@ __wt_hs_insert_updates(WT_SESSION_IMPL *session, WT_RECONCILE *r, WT_MULTI *mult
             break;
         case WT_PAGE_ROW_LEAF:
             if (list->ins == NULL) {
-                WT_WITH_BTREE(session, btree,
-                  ret = __wt_row_leaf_key(session, r->page, list->ripcip, key, false));
+                WT_WITH_BTREE(
+                  session, btree, ret = __wt_row_leaf_key(session, page, list->ripcip, key, false));
                 WT_ERR(ret);
             } else {
                 key->data = WT_INSERT_KEY(list->ins);
@@ -365,7 +365,7 @@ __wt_hs_insert_updates(WT_SESSION_IMPL *session, WT_RECONCILE *r, WT_MULTI *mult
             }
             break;
         default:
-            WT_ERR(__wt_illegal_value(session, r->page->type));
+            WT_ERR(__wt_illegal_value(session, page->type));
         }
 
         first_globally_visible_upd = min_ts_upd = out_of_order_ts_upd = NULL;
@@ -626,11 +626,6 @@ __wt_hs_insert_updates(WT_SESSION_IMPL *session, WT_RECONCILE *r, WT_MULTI *mult
             /* Clear out the insert success flag prior to our insert attempt. */
             __wt_curhs_clear_insert_success(hs_cursor);
 
-            /* Fail here 0.05% of the time if we are in the eviction path. */
-            if (F_ISSET(r, WT_REC_EVICT) &&
-              __wt_failpoint(session, WT_TIMING_STRESS_FAILPOINT_HISTORY_STORE_INSERT_1, 0.05))
-                WT_ERR(EBUSY);
-
             /*
              * Calculate reverse modify and clear the history store records with timestamps when
              * inserting the first update. Always write on-disk data store updates to the history
@@ -703,11 +698,6 @@ __wt_hs_insert_updates(WT_SESSION_IMPL *session, WT_RECONCILE *r, WT_MULTI *mult
         __wt_update_vector_clear(&updates);
     }
 
-    /* Fail here 0.5% of the time if we are an eviction thread. */
-    if (F_ISSET(r, WT_REC_EVICT) &&
-      __wt_failpoint(session, WT_TIMING_STRESS_FAILPOINT_HISTORY_STORE_INSERT_2, 0.05))
-        WT_ERR(EBUSY);
-
     WT_ERR(__wt_block_manager_named_size(session, WT_HS_FILE, &hs_size));
     hs_btree = __wt_curhs_get_btree(hs_cursor);
     max_hs_size = hs_btree->file_max;
@@ -722,7 +712,7 @@ err:
 
     /* cache_write_hs is set to true as there was at least one successful write to history. */
     if (insert_cnt > 0)
-        r->cache_write_hs = true;
+        *cache_write_hs = true;
 
     __wt_scr_free(session, &key);
     /* modify_value is allocated in __wt_modify_pack. Free it if it is allocated. */
