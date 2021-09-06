78754fad5fdd0bcb6c8bbf7f41d6c299fc754dc4
Alexey Anisimov <alexey.anisimov@mongodb.com>
2021-09-06 19:01:37 +1000

    Addressed comments.

--- test/csuite/wt7989_compact_checkpoint/main.c
+++ test/csuite/wt7989_compact_checkpoint/main.c
@@ -132,20 +132,18 @@ run_test(bool stress_test, const char *home, const char *uri)
 
     file_sz_before = get_file_size(session, uri);
 
-    /* Create and initialize conditional variable. */
     td.conn = conn;
     td.uri = uri;
-    if (stress_test) {
-        td.cond = NULL;
-    } else {
-        testutil_check(__wt_cond_alloc((WT_SESSION_IMPL *)session, "compact operation", &td.cond));
-    }
+    td.cond = NULL;
 
-    /* Spawn checkpoint and compact threads. */
+    /* Spawn checkpoint and compact threads. Order is important! */
     if (stress_test) {
         testutil_check(pthread_create(&thread_compact, NULL, thread_func_compact, &td));
         testutil_check(pthread_create(&thread_checkpoint, NULL, thread_func_checkpoint, &td));
     } else {
+        /* Create and initialize conditional variable. */
+        testutil_check(__wt_cond_alloc((WT_SESSION_IMPL *)session, "compact operation", &td.cond));
+        
         testutil_check(pthread_create(&thread_checkpoint, NULL, thread_func_checkpoint, &td));
         testutil_check(pthread_create(&thread_compact, NULL, thread_func_compact, &td));
     }
