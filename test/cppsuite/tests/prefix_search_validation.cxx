8cfbddd4d810f950a502264d792de6d324f9cadf
Jie Chen <jie.chen@mongodb.com>
2021-09-13 05:31:59 +0000

    Simplified the test, to only validate number of entries traversed

--- test/cppsuite/tests/prefix_search_validation.cxx
+++ test/cppsuite/tests/prefix_search_validation.cxx
@@ -35,92 +35,105 @@
  * Class that defines operations that do nothing as an example. This shows how database operations
  * can be overriden and customized.
  */
+using namespace test_harness;
 class prefix_search_validation : public test_harness::test {
 
     wt_timestamp_t start_ts = UINT64_MAX;
     wt_timestamp_t end_ts = 0;
+    uint64_t keys_per_prefix = 0;
 
-    std::string ALPHABET_ARRAY = "abcdefghijklmnopqrstuvwxyz";
+    std::string alphabet{"abcdefghijklmnopqrstuvwxyz"};
     const int ALPHABET_SIZE = 26;
-    std::map<std::string, int> map;
     void
-    populate_worker(test_harness::thread_context *tc)
+    populate_worker(thread_context *tc)
     {
-        test_harness::logger::log_msg(LOG_ERROR, "Populate: prefix key: " + std::to_string(tc->id));
-        test_harness::collection &coll = tc->db.get_collection(0);
+        logger::log_msg(LOG_INFO, "Populate: prefix key: " + std::to_string(tc->id));
         std::string prefix_key;
+        int cmpp;
+        uint64_t collections_per_thread = tc->collection_count;
 
-
-        /*
-         * WiredTiger lets you open a cursor on a collection using the same pointer. When a session
-         * is closed, WiredTiger APIs close the cursors too.
-         */
-        test_harness::scoped_cursor cursor = tc->session.open_scoped_cursor(coll.name.c_str());
-        test_harness::scoped_cursor evict_cursor =
-          tc->session.open_scoped_cursor(coll.name.c_str(), "debug=(release_evict=true)");
-        /* Start a txn. */
-
-        tc->transaction.begin();
-        for (uint64_t i = 0; i < ALPHABET_SIZE; ++i) {
+        for (int64_t i = 0; i < collections_per_thread; ++i) {
+            collection &coll = tc->db.get_collection(i);
+            /*
+            * WiredTiger lets you open a cursor on a collection using the same pointer. When a session
+            * is closed, WiredTiger APIs close the cursors too.
+            */
+            scoped_cursor cursor = tc->session.open_scoped_cursor(coll.name.c_str());
+            scoped_cursor evict_cursor =
+            tc->session.open_scoped_cursor(coll.name.c_str(), "debug=(release_evict=true)");
+            /* Start a txn. */
+            tc->transaction.begin();
             for (uint64_t j = 0; j < ALPHABET_SIZE; ++j) {
-                prefix_key = generate_prefix_key(tc, i, j);
-                
-                if (tc->insert(cursor, coll.id, prefix_key)) {
-                    /* We failed to insert, rollback our transaction and retry. */
-                    tc->transaction.rollback();
-                    --i;
-                    continue;
+                for (uint64_t k = 0; k < ALPHABET_SIZE; ++k) {
+                    for (uint64_t count = 0; count < tc->key_count; ++count) {
+                        prefix_key = {alphabet.at(tc->id), alphabet.at(i), alphabet.at(j)};
+                        prefix_key += random_generator::instance().generate_string(tc->key_size);
+                        if (tc->insert(cursor, coll.id, prefix_key)) {
+                            /* We failed to insert, rollback our transaction and retry. */
+                            tc->transaction.rollback();
+                            --count;
+                            continue;
+                        }
+                    }
                 }
             }
-        }
-        wt_timestamp_t ts = tc->tsm->get_next_ts();
-        //std::cout << ts << std::endl;
-        tc->transaction.set_commit_timestamp(ts);
-        if (tc->transaction.commit() == true) {
-            testutil_die(0, "ERROR", "rollback");
-        }
+            tc->transaction.commit("commit_timestamp=" + timestamp_str(100));
 
-        for (uint64_t i = 0; i < ALPHABET_SIZE; ++i) {
             for (uint64_t j = 0; j < ALPHABET_SIZE; ++j) {
-                std::string key = generate_prefix_key(tc, i, j);
-                evict_cursor->set_key(evict_cursor.get(), key.c_str());
-                testutil_check(evict_cursor->search(evict_cursor.get()));
-                testutil_check(evict_cursor->reset(evict_cursor.get()));
+                for (uint64_t k = 0; k < ALPHABET_SIZE; ++k) {
+                    std::string key = {alphabet.at(tc->id), alphabet.at(i), alphabet.at(j)};
+                    evict_cursor->set_key(evict_cursor.get(), key.c_str());
+                    evict_cursor->search_near(evict_cursor.get(), &cmpp);
+                    testutil_check(evict_cursor->reset(evict_cursor.get()));
+                }
             }
         }
     }
 
-    std::string generate_prefix_key(test_harness::thread_context *tc, int i, int j) {
-        std::string prefix_key = "";
-        prefix_key += ALPHABET_ARRAY[tc->id];
-        prefix_key += ALPHABET_ARRAY[i];
-        prefix_key += ALPHABET_ARRAY[j];
-        return prefix_key;
+    std::string generate_random_search_key() {
+        char a = alphabet.at(random_generator::instance().generate_integer(0, ALPHABET_SIZE - 1));
+        char b = alphabet.at(random_generator::instance().generate_integer(0, ALPHABET_SIZE - 1));
+
+        return {a, b};
     }
 
     public:
     prefix_search_validation(const test_harness::test_args &args) : test(args) {}
 
     void
-    populate(test_harness::database &database, test_harness::timestamp_manager *_timestamp_manager,
-      test_harness::configuration *_config,
-      test_harness::workload_tracking *tracking) override final
+    populate(test_harness::database &database, test_harness::timestamp_manager *tsm,
+      test_harness::configuration *config, test_harness::workload_tracking *tracking) override final
     {
-        std::vector<test_harness::thread_context *> workers;
-        test_harness::thread_manager tm;
+        int64_t collection_count, key_count, key_size;
+        std::vector<thread_context *> workers;
+        thread_manager tm;
 
-        database.add_collection();
+        /* Validate our config. */
+        collection_count = config->get_int(COLLECTION_COUNT);
+        key_count = keys_per_prefix = config->get_int(KEY_COUNT_PER_COLLECTION);
+        key_size = config->get_int(KEY_SIZE);
+        testutil_assert(key_size > 0);
+        /* Keys must be unique. */
+        testutil_assert(key_count <= pow(10, key_size));
+
+
+        std::cout << key_size << " " << key_count << " " << collection_count << std::endl;
+        
+        /* Create n collections as per the configuration. */
+        for (int64_t i = 0; i < collection_count; ++i)
+            /*
+            * The database model will call into the API and create the collection, with its own
+            * session.
+            */
+            database.add_collection();
 
         /*
          * Spawn thread_count threads to populate the database, theoretically we should be IO bound
          * here.
          */
-        start_ts = _timestamp_manager->get_next_ts();
         for (int64_t i = 0; i < ALPHABET_SIZE; ++i) {
-            test_harness::thread_context *tc =
-              new test_harness::thread_context(i, test_harness::thread_type::INSERT, _config,
-                test_harness::connection_manager::instance().create_session(), _timestamp_manager,
-                tracking, database);
+            thread_context *tc = new thread_context(i, thread_type::INSERT, config,
+              connection_manager::instance().create_session(), tsm, tracking, database);
             workers.push_back(tc);
             tm.add_thread(
               std::bind(&prefix_search_validation::populate_worker, this, std::placeholders::_1),
@@ -128,7 +141,7 @@ class prefix_search_validation : public test_harness::test {
         }
 
         /* Wait for our populate threads to finish and then join them. */
-        test_harness::logger::log_msg(LOG_ERROR, "Populate: waiting for threads to complete.");
+        logger::log_msg(LOG_INFO, "Populate: waiting for threads to complete.");
         tm.join();
 
         /* Cleanup our workers. */
@@ -136,63 +149,57 @@ class prefix_search_validation : public test_harness::test {
             delete it;
             it = nullptr;
         }
-        end_ts =_timestamp_manager->get_next_ts();
-        std::cout << "start: " << start_ts << " end: " << end_ts << std::endl;
-        test_harness::logger::log_msg(LOG_ERROR, "Populate: finished.");
+        logger::log_msg(LOG_INFO, "Populate: finished.");
     }
 
     void
     read_operation(test_harness::thread_context *tc) override final
     {
 
-        test_harness::logger::log_msg(
+        logger::log_msg(
           LOG_INFO, type_string(tc->type) + " thread {" + std::to_string(tc->id) + "} commencing.");
 
-        test_harness::scoped_cursor cursor;
+        scoped_cursor cursor;
         int cmpp;
-        int64_t entries_stat, prefix_stat;
-        std::string prefix_key = "";
-        /* Open our statistic cursor. */
-        
-        char a = ALPHABET_ARRAY[test_harness::random_generator::instance().generate_integer(0, ALPHABET_SIZE)];
-        char b = ALPHABET_ARRAY[test_harness::random_generator::instance().generate_integer(0, ALPHABET_SIZE)];
-        prefix_key += a;
-        prefix_key += b;
+        int64_t entries_stat, prefix_stat, prev_entries_stat, prev_prefix_stat;
 
         cmpp = 0;
+        entries_stat = prev_entries_stat = get_stat(tc, WT_STAT_CONN_CURSOR_NEXT_SKIP_LT_100);;
+        prefix_stat = prev_prefix_stat = get_stat(tc, WT_STAT_CONN_CURSOR_SEARCH_NEAR_PREFIX_FAST_PATHS);
+
+        std::map<uint64_t, scoped_cursor> cursors;
         while (tc->running()) {
-            wt_timestamp_t read_ts = test_harness::random_generator::instance().generate_integer(start_ts, end_ts);
-            /* Get a collection. */
-            test_harness::collection &coll = tc->db.get_random_collection();
-            std::cout << read_ts << std::endl;
-            tc->transaction.begin("read_timestamp=" + timestamp_str(start_ts));
-            cursor = tc->session.open_scoped_cursor(coll.name.c_str());
-            if (tc->transaction.active()) {
+            tc->transaction.begin("read_timestamp=" + timestamp_str(10));
+
+            /* Get a collection and find a cached cursor. */
+            collection  &coll = tc->db.get_random_collection();
+            if (cursors.find(coll.id) == cursors.end()) {
+                scoped_cursor cursor = tc->session.open_scoped_cursor(coll.name.c_str());
                 cursor->reconfigure(cursor.get(), "prefix_key=true");
-                cursor->set_key(cursor.get(), "xt");
+                cursors.emplace(coll.id, std::move(cursor));
+            }
+
+            std::string srch_key = generate_random_search_key();  
+
+
+            logger::log_msg(LOG_ERROR, srch_key);
+            /* Do a second lookup now that we know it exists. */
+            auto &cursor = cursors[coll.id];
+            if (tc->transaction.active()) {
+
+                cursor->set_key(cursor.get(), "aa");
                 auto ret = cursor->search_near(cursor.get(), &cmpp);
+                testutil_assert(ret == WT_NOTFOUND);
+                entries_stat = get_stat(tc, WT_STAT_CONN_CURSOR_NEXT_SKIP_LT_100);
+                prefix_stat = get_stat(tc, WT_STAT_CONN_CURSOR_SEARCH_NEAR_PREFIX_FAST_PATHS);
 
-                const char *tracked_key;
-                cursor->get_key(cursor.get(), &tracked_key);
-                std::cout << prefix_key << std::endl;
-                test_harness::logger::log_msg(LOG_ERROR, "Ret? " + std::to_string(ret));
-                if (ret != 0) {
-                    if (ret == WT_NOTFOUND) {
-                        cursor->reset(cursor.get());
-                    } else if (ret == WT_ROLLBACK) {
-                        tc->transaction.rollback();
-                        tc->sleep();
-                        continue;
-                    } else
-                        testutil_die(ret, "Unexpected error returned from cursor->next()");
-                }
-                entries_stat = 0;
-                get_stat(tc, WT_STAT_CONN_CURSOR_NEXT_SKIP_LT_100, &entries_stat);
-                get_stat(tc, WT_STAT_CONN_CURSOR_SEARCH_NEAR_PREFIX_FAST_PATHS, &prefix_stat);
-
-                test_harness::logger::log_msg(LOG_ERROR,
+                logger::log_msg(LOG_ERROR,
                   "Read working: skipped entries " + std::to_string(entries_stat) +
                     " prefix fash path  " + std::to_string(prefix_stat));
+                testutil_assert(keys_per_prefix * ALPHABET_SIZE * 2 >= entries_stat - prev_entries_stat);
+                testutil_assert(prefix_stat > prev_prefix_stat);
+                prev_entries_stat = entries_stat;
+                prev_prefix_stat = prefix_stat;
                 tc->transaction.add_op();
                 // tc->transaction.try_rollback();
                 tc->sleep();
@@ -206,17 +213,20 @@ class prefix_search_validation : public test_harness::test {
             tc->transaction.rollback();
     }
 
-    void
-    get_stat(test_harness::thread_context *tc, int stat_field, int64_t *valuep)
+    int64_t
+    get_stat(test_harness::thread_context *tc, int stat_field)
     {
-        test_harness::scoped_cursor cursor =
-          tc->session.open_scoped_cursor(test_harness::STATISTICS_URI);
+        int64_t valuep;
+        /* Open our statistic cursor. */
+        scoped_cursor cursor =
+          tc->session.open_scoped_cursor(STATISTICS_URI);
 
         const char *desc, *pvalue;
         cursor->set_key(cursor.get(), stat_field);
         testutil_check(cursor->search(cursor.get()));
-        testutil_check(cursor->get_value(cursor.get(), &desc, &pvalue, valuep));
+        testutil_check(cursor->get_value(cursor.get(), &desc, &pvalue, &valuep));
         testutil_check(cursor->reset(cursor.get()));
+        return valuep;
     }
 
     std::string
