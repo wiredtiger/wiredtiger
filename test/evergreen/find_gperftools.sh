1c5f93b68eeae9078f9d45ab014d9c574dccc6f3
Jie Chen <jie.chen@mongodb.com>
2021-10-01 00:59:11 +0000

    Remove tcmalloc from ASAN and MSAN builds

--- test/evergreen/find_gperftools.sh
+++ test/evergreen/find_gperftools.sh
@@ -20,7 +20,7 @@ find_gperftools()
     echo tcmalloc_${build_variant}.tgz
     echo "-- FETCH GPERFTOOLS --"
     aws s3 ls "s3://build_external/jiechenbo_build/tcmalloc_${build_variant}.tgz"
-    if [[ true ]]; then
+    if [[ $? -ne 0 ]]; then
         echo "-- MAKE GPERFTOOLS --"
         curl --retry 5 -L https://github.com/gperftools/gperftools/releases/download/gperftools-2.9.1/gperftools-2.9.1.tar.gz -sS --max-time 120 --fail --output gperftools_${build_variant}.tgz
         tar xzf gperftools_${build_variant}.tgz
