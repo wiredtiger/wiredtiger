fd1ee9337e464e3a83f49a5128fe7ad3504f34bc
Etienne Petrel <etienne.petrel@mongodb.com>
2021-10-25 01:17:37 +0000

    Separate warning flags in different files depending on the compiler

--- cmake/strict/clang_strict.cmake
+++ cmake/strict/clang_strict.cmake
@@ -6,68 +6,14 @@
 # See the file LICENSE for redistribution information.
 #
 
-# List of common C/CXX flags.
-list(APPEND clang_base_flags "-Werror")
-list(APPEND clang_base_flags "-Wno-cast-align")
-list(APPEND clang_base_flags "-Wno-documentation-unknown-command")
-list(APPEND clang_base_flags "-Wno-format-nonliteral")
-list(APPEND clang_base_flags "-Wno-packed")
-list(APPEND clang_base_flags "-Wno-padded")
-list(APPEND clang_base_flags "-Wno-reserved-id-macro")
-list(APPEND clang_base_flags "-Wno-zero-length-array")
+include(cmake/strict/strict_flags_helpers.cmake)
 
-# We should turn on cast-qual, but not as a fatal error: see WT-2690.
-# For now, turn it off.
-list(APPEND clang_base_flags "-Wno-cast-qual")
+# Get common CLANG flags.
+set(clang_flags)
+get_clang_base_flags(clang_flags C)
 
-# Turn off clang thread-safety-analysis, it doesn't like some of the
-# code patterns in WiredTiger.
-list(APPEND clang_base_flags "-Wno-thread-safety-analysis")
+# Specific C flags:
+list(APPEND clang_flags "-Weverything")
 
-# On Centos 7.3.1611, system header files aren't compatible with
-# -Wdisabled-macro-expansion.
-list(APPEND clang_base_flags "-Wno-disabled-macro-expansion")
-
-# We occasionally use an extra semicolon to indicate an empty loop or
-# conditional body.
-list(APPEND clang_base_flags "-Wno-extra-semi-stmt")
-
-# Ignore unrecognized options.
-list(APPEND clang_base_flags "-Wno-unknown-warning-option")
-
-if(WT_DARWIN AND (CMAKE_C_COMPILER_VERSION VERSION_EQUAL 4.1))
-    # Apple clang has its own numbering system, and older OS X
-    # releases need some special love. Turn off some flags for
-    # Apple's clang 4.1:
-    #   Apple clang version 4.1
-    #   ((tags/Apple/clang-421.11.66) (based on LLVM 3.1svn)
-    list(APPEND clang_base_flags "-Wno-attributes")
-    list(APPEND clang_base_flags "-Wno-pedantic")
-    list(APPEND clang_base_flags "-Wno-unused-command-line-argument")
-endif()
-
-if(CMAKE_C_COMPILER_VERSION VERSION_GREATER_EQUAL 10)
-    # Clang 10+ has added additional on-by-default diagnostics that isn't
-    # compatible with some of the code patterns in WiredTiger.
-    list(APPEND clang_base_flags "-Wno-implicit-fallthrough")
-    list(APPEND clang_base_flags "-Wno-implicit-int-float-conversion")
-endif()
-
-if(WT_DARWIN AND NOT CMAKE_CROSSCOMPILING)
-    # If we are not cross-compiling, we can safely disable this diagnostic.
-    # Its incompatible with strict diagnostics when including external
-    # libraries that are not in the default linker path
-    # e.g. linking zlib/snappy/... from /usr/local/.
-    list(APPEND clang_base_flags "-Wno-poison-system-directories")
-endif()
-
-set(clang_c_flags ${clang_base_flags})
-set(clang_cxx_flags ${clang_base_flags})
-
-# Specific C flags.
-# The -Weverything flag is not recommended, we only keep it for C files as it was already enabled.
-list(APPEND clang_c_flags "-Weverything")
-
-# Set our compiler flags that can be used by the rest of our build.
-set(COMPILER_DIAGNOSTIC_C_FLAGS ${clang_c_flags})
-set(COMPILER_DIAGNOSTIC_CXX_FLAGS ${clang_cxx_flags})
+# Set our common compiler flags that can be used by the rest of our build.
+set(COMPILER_DIAGNOSTIC_C_FLAGS ${clang_flags})
--- cmake/strict/gcc_strict.cmake
+++ cmake/strict/gcc_strict.cmake
@@ -6,83 +6,28 @@
 # See the file LICENSE for redistribution information.
 #
 
-# List of common C/CXX flags.
-list(APPEND gcc_base_flags "-Wcast-align")
-list(APPEND gcc_base_flags "-Wdouble-promotion")
-list(APPEND gcc_base_flags "-Werror")
-list(APPEND gcc_base_flags "-Wfloat-equal")
-list(APPEND gcc_base_flags "-Wformat-nonliteral")
-list(APPEND gcc_base_flags "-Wformat-security")
-list(APPEND gcc_base_flags "-Wformat=2")
-list(APPEND gcc_base_flags "-Winit-self")
-list(APPEND gcc_base_flags "-Wmissing-declarations")
-list(APPEND gcc_base_flags "-Wmissing-field-initializers")
-list(APPEND gcc_base_flags "-Wpacked")
-list(APPEND gcc_base_flags "-Wpointer-arith")
-list(APPEND gcc_base_flags "-Wredundant-decls")
-list(APPEND gcc_base_flags "-Wswitch-enum")
-list(APPEND gcc_base_flags "-Wundef")
-list(APPEND gcc_base_flags "-Wuninitialized")
-list(APPEND gcc_base_flags "-Wunreachable-code")
-list(APPEND gcc_base_flags "-Wunused")
-list(APPEND gcc_base_flags "-Wwrite-strings")
+include(cmake/strict/strict_flags_helpers.cmake)
 
-# Non-fatal informational warnings.
-# We don't turn on the unsafe-loop-optimizations warning after gcc7,
-# it's too noisy to tolerate. Regardless, don't fail even when it's
-# configured.
-list(APPEND gcc_base_flags "-Wno-error=unsafe-loop-optimizations")
-if(${CMAKE_C_COMPILER_VERSION} VERSION_EQUAL 4.7)
-    list(APPEND gcc_base_flags "-Wno-c11-extensions")
-    list(APPEND gcc_base_flags "-Wunsafe-loop-optimizations")
-elseif(${CMAKE_C_COMPILER_VERSION} VERSION_EQUAL 5)
-    list(APPEND gcc_base_flags "-Wunsafe-loop-optimizations")
-elseif(${CMAKE_C_COMPILER_VERSION} VERSION_EQUAL 6)
-    list(APPEND gcc_base_flags "-Wunsafe-loop-optimizations")
-endif()
+# Get common GNU flags.
+set(gcc_flags)
+get_gnu_base_flags(gcc_flags C)
 
-if(${CMAKE_C_COMPILER_VERSION} VERSION_GREATER_EQUAL 5)
-    list(APPEND gcc_base_flags "-Wformat-signedness")
-    list(APPEND gcc_base_flags "-Wunused-macros")
-    list(APPEND gcc_base_flags "-Wvariadic-macros")
-endif()
-if(${CMAKE_C_COMPILER_VERSION} VERSION_GREATER_EQUAL 6)
-    list(APPEND gcc_base_flags "-Wduplicated-cond")
-    list(APPEND gcc_base_flags "-Wlogical-op")
-    list(APPEND gcc_base_flags "-Wunused-const-variable=2")
-endif()
-if(${CMAKE_C_COMPILER_VERSION} VERSION_GREATER_EQUAL 7)
-    list(APPEND gcc_base_flags "-Walloca")
-    list(APPEND gcc_base_flags "-Walloc-zero")
-    list(APPEND gcc_base_flags "-Wduplicated-branches")
-    list(APPEND gcc_base_flags "-Wformat-overflow=2")
-    list(APPEND gcc_base_flags "-Wformat-truncation=2")
-    list(APPEND gcc_base_flags "-Wrestrict")
-endif()
-if(${CMAKE_C_COMPILER_VERSION} VERSION_GREATER_EQUAL 8)
-    list(APPEND gcc_base_flags "-Wmultistatement-macros")
-endif()
+# FIX-ME-WT-8247: Add those flags to the common GNU flags if we want them for the compilation of the
+# c++ files too.
+list(APPEND gcc_flags "-Waggregate-return")
+list(APPEND gcc_flags "-Wall")
+list(APPEND gcc_flags "-Wextra")
+list(APPEND gcc_flags "-Wshadow")
+list(APPEND gcc_flags "-Wsign-conversion")
 
-set(gcc_c_flags ${gcc_base_flags})
-set(gcc_cxx_flags ${gcc_base_flags})
+# Specific C flags:
+list(APPEND gcc_flags "-Wbad-function-cast")
+list(APPEND gcc_flags "-Wdeclaration-after-statement")
+list(APPEND gcc_flags "-Wjump-misses-init")
+list(APPEND gcc_flags "-Wmissing-prototypes")
+list(APPEND gcc_flags "-Wnested-externs")
+list(APPEND gcc_flags "-Wold-style-definition")
+list(APPEND gcc_flags "-Wpointer-sign")
+list(APPEND gcc_flags "-Wstrict-prototypes")
 
-# FIX-ME-WT-8247: Add those flags to the common ones if we want them for the compilation of the c++ files too.
-list(APPEND gcc_c_flags "-Waggregate-return")
-list(APPEND gcc_c_flags "-Wall")
-list(APPEND gcc_c_flags "-Wextra")
-list(APPEND gcc_c_flags "-Wshadow")
-list(APPEND gcc_c_flags "-Wsign-conversion")
-
-# Specific C flags.
-list(APPEND gcc_c_flags "-Wbad-function-cast")
-list(APPEND gcc_c_flags "-Wdeclaration-after-statement")
-list(APPEND gcc_c_flags "-Wjump-misses-init")
-list(APPEND gcc_c_flags "-Wmissing-prototypes")
-list(APPEND gcc_c_flags "-Wnested-externs")
-list(APPEND gcc_c_flags "-Wold-style-definition")
-list(APPEND gcc_c_flags "-Wpointer-sign")
-list(APPEND gcc_c_flags "-Wstrict-prototypes")
-
-# Set our compiler flags that can be used by the rest of our build.
-set(COMPILER_DIAGNOSTIC_C_FLAGS ${gcc_c_flags})
-set(COMPILER_DIAGNOSTIC_CXX_FLAGS ${gcc_cxx_flags})
+set(COMPILER_DIAGNOSTIC_C_FLAGS ${gcc_flags})
