5bf2464fc4f6613f6717afa6b90785eb0b8fcad8
Alison Felizzi <alison.felizzi@mongodb.com>
2021-11-15 21:30:53 +0000

    Remove new line after JSON flag generation
    The flags.py scripts looks to validate the STOP value with the
    definition of the next line.

--- src/include/connection.h
+++ src/include/connection.h
@@ -559,7 +559,6 @@ struct __wt_connection_impl {
 #define WT_JSON_OUTPUT_ERROR 0x1u
 #define WT_JSON_OUTPUT_MESSAGE 0x2u
     /* AUTOMATIC FLAG VALUE GENERATION STOP 8 */
-
     uint8_t json_output; /* Output event handler messages in JSON format */
 
 /*
