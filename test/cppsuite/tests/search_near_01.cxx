2271c1faf891273548e9de9a8687530ec7af310c
Jie Chen <jie.chen@mongodb.com>
2021-11-17 00:20:41 +0000

    reworded last sentence in comment

--- test/cppsuite/tests/search_near_01.cxx
+++ test/cppsuite/tests/search_near_01.cxx
@@ -244,9 +244,10 @@ class search_near_01 : public test_harness::test {
                  * nears before another can finish one. Account for this problem by creating a
                  * buffer taking the maximum of either calculated 2 * expected entries or the
                  * minimum expected entries. The minimum expected entries is necessary in the case
-                 * that expected entries is a low number. Assert that the number of expected
-                 * entries is the upper limit which the prefix search near can traverse and the
-                 * prefix fast path is incremented.
+                 * that expected entries is a low number.
+                 *
+                 * Assert that the number of expected entries is the maximum allowed limit that the
+                 * prefix search nears can traverse and the prefix fast path is incremented.
                  */
                 buffer = std::max(2 * expected_entries, MINIMUM_EXPECTED_ENTRIES);
                 testutil_assert((expected_entries + buffer) >= entries_stat - prev_entries_stat);
