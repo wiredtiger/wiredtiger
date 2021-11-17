f602428d6f9e0111ae84d5f301a23acefdcdb52e
David A. Holland <dholland+wt@sauclovia.org>
2021-11-17 05:38:19 -0500

    Sort the arguments in the test list to put the common ones first.
    Particularly in the shell version (where they now all line up) this
    makes them a lot easier to read.
    
    No functional change.

--- test/checkpoint/smoke.sh
+++ test/checkpoint/smoke.sh
@@ -10,33 +10,33 @@ test "$TESTUTIL_BYPASS_VALGRIND" = "1" && exit 0
 # 1. Mixed tables cases. Use four (or eight) tables because there are four table types.
 
 echo "checkpoint: 4 mixed tables"
-$TEST_WRAPPER ./t -T 4 -t m
+$TEST_WRAPPER ./t -t m -T 4
 
 echo "checkpoint: 8 mixed tables"
-$TEST_WRAPPER ./t -T 8 -t m
+$TEST_WRAPPER ./t -t m -T 8
 
 echo "checkpoint: 4 mixed tables, with sweep"
-$TEST_WRAPPER ./t -T 4 -t m -W 3 -r 2 -s 1 -n 100000 -k 100000
+$TEST_WRAPPER ./t -t m -T 4 -W 3 -r 2 -n 100000 -k 100000 -s 1
 
 echo "checkpoint: 4 mixed tables, with timestamps"
-$TEST_WRAPPER ./t -T 4 -t m -W 3 -r 2 -x -n 100000 -k 100000
+$TEST_WRAPPER ./t -t m -T 4 -W 3 -r 2 -n 100000 -k 100000 -x
 
 # 2. FLCS cases.
 
 echo "checkpoint: 6 fixed-length column-store tables"
-$TEST_WRAPPER ./t -T 6 -t f
+$TEST_WRAPPER ./t -t f -T 6
 
 echo "checkpoint: 6 fixed-length column-store tables, named checkpoint"
-$TEST_WRAPPER ./t -c 'TeSt' -T 6 -t f
+$TEST_WRAPPER ./t -t f -T 6 -c 'TeSt'
 
 echo "checkpoint: 6 fixed-length column-store tables with prepare"
-$TEST_WRAPPER ./t -T 6 -t f -p
+$TEST_WRAPPER ./t -t f -T 6 -p
 
 echo "checkpoint: 6 fixed-length column-store tables, named checkpoint with prepare"
-$TEST_WRAPPER ./t -c 'TeSt' -T 6 -t f -p
+$TEST_WRAPPER ./t -t f -T 6  -c 'TeSt' -p
 
 echo "checkpoint: fixed-length column-store tables, stress history store. Sweep and timestamps"
-$TEST_WRAPPER ./t -t c -W 3 -r 2 -D -s 1 -x -n 100000 -k 100000 -C cache_size=100MB
+$TEST_WRAPPER ./t -t c -W 3 -r 2 -s 1 -x -n 100000 -k 100000 -C cache_size=100MB -D
 
 echo "checkpoint: fixed-length column-store tables, Sweep and timestamps"
 $TEST_WRAPPER ./t -t c -W 3 -r 2 -s 1 -x -n 100000 -k 100000 -C cache_size=100MB
@@ -44,19 +44,19 @@ $TEST_WRAPPER ./t -t c -W 3 -r 2 -s 1 -x -n 100000 -k 100000 -C cache_size=100MB
 # 3. VLCS cases.
 
 echo "checkpoint: 6 column-store tables"
-$TEST_WRAPPER ./t -T 6 -t c
+$TEST_WRAPPER ./t -t c -T 6
 
 echo "checkpoint: 6 column-store tables, named checkpoint"
-$TEST_WRAPPER ./t -c 'TeSt' -T 6 -t c
+$TEST_WRAPPER ./t -t c -T 6 -c 'TeSt'
 
 echo "checkpoint: 6 column-store tables with prepare"
-$TEST_WRAPPER ./t -T 6 -t c -p
+$TEST_WRAPPER ./t -t c -T 6 -p
 
 echo "checkpoint: 6 column-store tables, named checkpoint with prepare"
-$TEST_WRAPPER ./t -c 'TeSt' -T 6 -t c -p
+$TEST_WRAPPER ./t -t c -T 6 -c 'TeSt' -p
 
 echo "checkpoint: column-store tables, stress history store. Sweep and timestamps"
-$TEST_WRAPPER ./t -t c -W 3 -r 2 -D -s 1 -x -n 100000 -k 100000 -C cache_size=100MB
+$TEST_WRAPPER ./t -t c -W 3 -r 2 -s 1 -x -n 100000 -k 100000 -C cache_size=100MB -D
 
 echo "checkpoint: column-store tables, Sweep and timestamps"
 $TEST_WRAPPER ./t -t c -W 3 -r 2 -s 1 -x -n 100000 -k 100000 -C cache_size=100MB
@@ -64,19 +64,19 @@ $TEST_WRAPPER ./t -t c -W 3 -r 2 -s 1 -x -n 100000 -k 100000 -C cache_size=100MB
 # 4. Row-store cases.
 
 echo "checkpoint: 6 row-store tables"
-$TEST_WRAPPER ./t -T 6 -t r
+$TEST_WRAPPER ./t -t r -T 6
 
 echo "checkpoint: 6 row-store tables, named checkpoint"
-$TEST_WRAPPER ./t -c 'TeSt' -T 6 -t r
+$TEST_WRAPPER ./t -t r -T 6 -c 'TeSt'
 
 echo "checkpoint: 6 row-store tables with prepare"
-$TEST_WRAPPER ./t -T 6 -t r -p
+$TEST_WRAPPER ./t -t r -T 6 -p
 
 echo "checkpoint: 6 row-store tables, named checkpoint with prepare"
-$TEST_WRAPPER ./t -c 'TeSt' -T 6 -t r -p
+$TEST_WRAPPER ./t -t r -T 6 -c 'TeSt' -p
 
 echo "checkpoint: row-store tables, stress history store. Sweep and timestamps"
-$TEST_WRAPPER ./t -t r -W 3 -r 2 -D -s 1 -x -n 100000 -k 100000 -C cache_size=100MB
+$TEST_WRAPPER ./t -t r -W 3 -r 2 -s 1 -x -n 100000 -k 100000 -C cache_size=100MB -D
 
 echo "checkpoint: row-store tables, Sweep and timestamps"
 $TEST_WRAPPER ./t -t r -W 3 -r 2 -s 1 -x -n 100000 -k 100000 -C cache_size=100MB
@@ -84,4 +84,4 @@ $TEST_WRAPPER ./t -t r -W 3 -r 2 -s 1 -x -n 100000 -k 100000 -C cache_size=100MB
 # 5. LSM cases.
 
 echo "checkpoint: 6 LSM tables"
-$TEST_WRAPPER ./t -T 6 -t l
+$TEST_WRAPPER ./t -t l -T 6
