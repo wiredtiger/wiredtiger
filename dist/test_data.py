6dea6340d13029621010f4b0e2403cc91a350f81
Jie Chen <jie.chen@mongodb.com>
2021-11-24 22:40:14 +0000

    Restructure search near 01 assert testing

--- dist/test_data.py
+++ dist/test_data.py
@@ -220,7 +220,9 @@ methods = {
                             How long the insertions will occur for.''')]),
     'example_test' : Method(test_config),
     'hs_cleanup' : Method(test_config),
-    'search_near_01' : Method(test_config),
+    'search_near_01' : Method(test_config + [
+                        Config("search_near_threads", 10, r'''
+                            How long the insertions will occur for.''')]),
     'search_near_02' : Method(test_config),
     'search_near_03' : Method(test_config),
 }
