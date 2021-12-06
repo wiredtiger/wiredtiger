dd9f363e8870a4acfea8538fad677bee6f8f57c2
Andrew Morrow <acm@mongodb.com>
2021-12-06 11:28:45 -0500

    WT-8497 Initial riscv64 support

--- src/include/gcc.h
+++ src/include/gcc.h
@@ -280,6 +280,23 @@ WT_ATOMIC_FUNC(size, size_t, size_t *vp, size_t v)
         __asm__ volatile("" ::: "memory"); \
     } while (0)
 
+
+#elif defined(__riscv) && (__riscv_xlen == 64)
+#define WT_PAUSE() __asm__ volatile("nop" ::: "memory")
+#define WT_FULL_BARRIER()                               \
+    do {                                                \
+        __asm__ volatile("fence rw, rw" ::: "memory");  \
+    } while (0)
+#define WT_READ_BARRIER()                               \
+    do {                                                \
+        __asm__ volatile("fence r, r" ::: "memory");    \
+    } while (0)
+#define WT_WRITE_BARRIER()                              \
+    do {                                                \
+        __asm__ volatile("fence w, w" ::: "memory");    \
+    } while (0)
+
+
 #else
 #error "No write barrier implementation for this hardware"
 #endif
