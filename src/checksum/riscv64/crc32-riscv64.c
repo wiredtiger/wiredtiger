3393a97261ad466b0dcdd24142a4a886a5a0e87b
Andrew Morrow <acm@mongodb.com>
2021-12-08 13:28:11 -0500

    review changes and clang-format

--- src/checksum/riscv64/crc32-riscv64.c
+++ src/checksum/riscv64/crc32-riscv64.c
@@ -34,7 +34,7 @@ extern uint32_t __wt_checksum_sw(const void *chunk, size_t len);
 
 #if defined(__GNUC__)
 extern uint32_t (*wiredtiger_crc32c_func(void))(const void *, size_t)
-    __attribute__((visibility("default")));
+  __attribute__((visibility("default")));
 #else
 extern uint32_t (*wiredtiger_crc32c_func(void))(const void *, size_t);
 #endif
