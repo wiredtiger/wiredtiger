af23c8f8cc5070a2d9b2985b8f55e3affcd90eed
Haribabu Kommi <haribabu.kommi@mongodb.com>
2021-12-13 00:03:38 +0000

    WT-8534 Allow retrieving the checkpoint snapshot for backup restore
    WiredTiger backup doesn't contains the WiredTiger.turtle file to
    identify the source WiredTiger version of the backup. The saved
    checkpoint is retrieved as part of the backup restore recovery
    to fix any inconsistencies that exist.

--- src/txn/txn_recover.c
+++ src/txn/txn_recover.c
@@ -484,9 +484,10 @@ __recovery_set_checkpoint_snapshot(WT_SESSION_IMPL *session)
 
     /*
      * WiredTiger versions 10.0.1 onward have a valid checkpoint snapshot on-disk. Ignore reading
-     * the on-disk checkpoint snapshot from older versions.
+     * the on-disk checkpoint snapshot from older versions. WiredTiger backup doesn't contain the
+     * WiredTiger.turtle file. Allow retrieving the saved checkpoint snapshot if it exists.
      */
-    if (conn->recovery_major < 10 ||
+    if ((conn->recovery_major != 0 && conn->recovery_major < 10) ||
       (conn->recovery_major == 10 && conn->recovery_minor == 0 && conn->recovery_patch == 0))
         return (0);
 
