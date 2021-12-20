2e6e6ee2499217db39abc923523a249275e5afe2
bing.xu <bingxu.au@gmail.com>
2021-12-20 23:03:31 +0000

    add logic to perform writes and checkpoints to create .turtle file

--- test/suite/test_turtle.py
+++ test/suite/test_turtle.py
@@ -30,102 +30,34 @@ import wiredtiger, wttest
 from wtscenario import make_scenarios
 from helper import simulate_crash_restart
 
-# test_prepare17.py
-# The following test is to verify that if the out of order commit timestamp(for a transaction, say T2) lies between
-# previous commit and durable timestamps(for a transaction, say T1), the durable timestamp of T1 changes to
-# the commit timestamp of T2.
-class test_prepare17(wttest.WiredTigerTestCase):
-    session_config = 'isolation=snapshot'
-    uri = 'table:test_prepare17'
+# test_turtle.py
+# The following test is to validate the turtle file and to ensure it 
+# contains the correct key-value pairs.
+
+class test_turtle(wttest.WiredTigerTestCase):
+    # session_config = 'isolation=snapshot'
+    uri = 'table:test_turtle'
     nrows = 1000
 
-    format_values = [
-        ('integer-row', dict(key_format='i', value_format='S')),
-        ('column', dict(key_format='r', value_format='S')),
-        ('column-fix', dict(key_format='r', value_format='8t')),
-    ]
-    update = [
-        ('prepare', dict(prepare=True)),
-        ('non-prepare', dict(prepare=False)),
-    ]
-    scenarios = make_scenarios(format_values, update)
+    def init_values(self):
+            self.val = 'aaaa'
+            self.key_format = 'i'
+            self.value_format = 'S'
 
-    def moresetup(self):
-        if self.value_format == '8t':
-            self.value1 = 97
-            self.value2 = 98
-        else:
-            self.value1 = 'aaaaa'
-            self.value2 = 'bbbbb'
-
-    def test_prepare(self):
-        self.moresetup()
-        create_params = 'key_format={},value_format={}'.format(self.key_format, self.value_format)
-        self.session.create(self.uri, create_params)
-        cursor = self.session.open_cursor(self.uri)
-
-        # Transaction one
-        self.session.begin_transaction()
-        for i in range(1, self.nrows + 1):
-            cursor[i] = self.value1
-        self.session.prepare_transaction('prepare_timestamp=' + self.timestamp_str(2))
-        self.session.commit_transaction('commit_timestamp=' + self.timestamp_str(3)+ ',durable_timestamp=' + self.timestamp_str(6))
-
-        # In the case below, the commit timestamp lies between the previous commit and durable timestamps.
-        # Internally, WiredTiger changes the durable timestamp of Transaction one, i.e. 6 to the commit timestamp 
-        # of the transaction below, i.e, 4.
-        # As per the time window validation the commit timestamp cannot be in between any previous commit and 
-        # durable timestamps.
-        #
-        # Note: The scenario where commit timestamp lies between the previous commit and durable timestamps
-        # is not expected from MongoDB, but WiredTiger API can allow it.
-        if self.prepare:
-            self.session.begin_transaction()
-            for i in range(1, self.nrows + 1):
-                cursor[i] = self.value2
-            self.session.prepare_transaction('prepare_timestamp=' + self.timestamp_str(3))
-            self.session.commit_transaction('commit_timestamp=' + self.timestamp_str(4) + ',durable_timestamp=' + self.timestamp_str(7))
-        else:
-            self.session.begin_transaction()
-            for i in range(1, self.nrows + 1):
-                cursor[i] = self.value2
-            self.session.commit_transaction('commit_timestamp=' + self.timestamp_str(4))
-
-        # Time window validation occurs as part of checkpoint.
-        self.session.checkpoint()
-
-    def test_prepare_insert_remove(self):
-        self.moresetup()
-        if not self.prepare:
-            return
-
+    def test_turtle(self):
+        self.init_values()
         create_params = 'key_format={},value_format={}'.format(self.key_format, self.value_format)
         self.session.create(self.uri, create_params)
         cursor = self.session.open_cursor(self.uri)
 
         self.session.begin_transaction()
         for i in range(1, self.nrows + 1):
-            cursor[i] = self.value1
-        self.session.commit_transaction('commit_timestamp=' + self.timestamp_str(2))
-
-        # Transaction 2
-        self.session.begin_transaction()
-        for i in range(1, self.nrows + 1):
-            cursor[i] = self.value2
-            cursor.set_key(i)
-            cursor.remove()
-        self.session.prepare_transaction('prepare_timestamp=' + self.timestamp_str(3))
-        self.session.commit_transaction('commit_timestamp=' + self.timestamp_str(4) + ',durable_timestamp=' + self.timestamp_str(7))
-
-        self.conn.set_timestamp('stable_timestamp=' + self.timestamp_str(6))
+            cursor[i] = self.val
+        self.session.commit_transaction()
         self.session.checkpoint()
-        simulate_crash_restart(self, ".", "RESTART")
+        self.check_turle()
 
-        # Update in Transaction2 should be removed as the stable timestamp (6) is less than the durable timestamp (7).
-        cursor = self.session.open_cursor(self.uri)
-        self.session.begin_transaction()
-        for i in range(1, self.nrows + 1):
-            cursor.set_key(i)
-            cursor.search()
-            self.assertEqual(cursor.get_value(), self.value1)
-        self.session.rollback_transaction()
+    def check_turle(self):
+        with open('WiredTiger.turtle', 'r') as f:
+            lines = f.readlines()
+        return
