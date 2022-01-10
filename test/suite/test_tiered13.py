09952f6921acb0f41c6e623cb41d2228e8b60744
Susan LoVerso <sue@mongodb.com>
2022-01-10 10:45:31 -0500

    Whitespace

--- test/suite/test_tiered13.py
+++ test/suite/test_tiered13.py
@@ -142,6 +142,7 @@ class test_tiered13(test_import_base):
         # Try to import via the file:uri with file metadata.
         self.assertRaisesWithMessage(wiredtiger.WiredTigerError,
             lambda: self.session.create(self.file2uri, import_meta), msg)
+
         # Try to import via a renamed object. If we don't send in metadata,
         # we cannot tell it was a tiered table until we read in the root page.
         # Only test this in diagnostic mode which has an assertion.
@@ -149,9 +150,11 @@ class test_tiered13(test_import_base):
         # FIXME-8644 There is an error path bug in wt_bm_read preventing this from
         # working correctly although the code to return an error is in the code.
         # Uncomment these lines when that bug is fixed.
+
         #if wiredtiger.diagnostic_build():
         #    self.assertRaisesWithMessage(wiredtiger.WiredTigerError,
         #        lambda: self.session.create(self.otheruri, import_enabled), msg)
+
         # Try to import via a renamed object with metadata.
         self.assertRaisesWithMessage(wiredtiger.WiredTigerError,
             lambda: self.session.create(self.otheruri, import_meta), msg)
