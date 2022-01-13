83cf1266de8530ac82081e5dcc15fee534bd4440
Chenhao Qu <chenhao.qu@mongodb.com>
2022-01-13 11:16:05 +1100

    Add example of version cursor

--- examples/c/ex_cursor.c
+++ examples/c/ex_cursor.c
@@ -39,6 +39,7 @@ int cursor_insert(WT_CURSOR *cursor);
 int cursor_update(WT_CURSOR *cursor);
 int cursor_remove(WT_CURSOR *cursor);
 int cursor_largest_key(WT_CURSOR *cursor);
+int version_cursor_dump(WT_CURSOR *cursor);
 
 static const char *home;
 
@@ -164,6 +165,21 @@ cursor_largest_key(WT_CURSOR *cursor)
 }
 /*! [cursor largest key] */
 
+/*! [version cursor dump] */
+int
+version_cursor_dump(WT_CURSOR *cursor)
+{
+    wt_timestamp_t start_ts, start_durable_ts, stop_ts, stop_durable_ts;
+    uint64_t start_txnid, stop_txnid;
+    uint8_t flags, location, prepare, type;
+    const char *value;
+    cursor->set_key(cursor, "foo");
+    error_check(cursor->search(cursor));
+    return (cursor->get_value(cursor, &start_txnid, &start_ts, &start_durable_ts, &stop_txnid,
+      &stop_ts, &stop_durable_ts, &type, &prepare, &flags, &location, &value));
+}
+/*! [version cursor dump] */
+
 int
 main(int argc, char *argv[])
 {
@@ -206,7 +222,14 @@ main(int argc, char *argv[])
     error_check(cursor_search_near(cursor));
     error_check(cursor_update(cursor));
     error_check(cursor_remove(cursor));
+    error_check(cursor_insert(cursor));
     error_check(cursor_largest_key(cursor));
+    error_check(cursor->close(cursor));
+
+    /* Create a version cursor. */
+    error_check(session->create(
+      session, "table:map", "key_format=S,value_format=S,debug=(dump_version=true)"));
+    error_check(version_cursor_dump(cursor));
     error_check(cursor->close(cursor));
 
     /* Note: closing the connection implicitly closes open session(s). */
