e472078bf50277873ea2c499d35c94447eb5f328
Fiona O'Chee <xfionaochee@gmail.com>
2022-01-20 01:11:14 +0000

    Remove S3 dependency and remove autoconf Makefile

--- ext/storage_sources/s3_store/aws_bucket_conn.h
+++ ext/storage_sources/s3_store/aws_bucket_conn.h
@@ -16,6 +16,7 @@ class aws_bucket_conn {
     explicit aws_bucket_conn(const Aws::S3Crt::ClientConfiguration &config);
 
     bool list_buckets(Aws::Vector<Aws::S3Crt::Model::Bucket> *buckets) const;
+    
     bool list_bucket_objects(
       const std::string bucket_name, Aws::Vector<Aws::S3Crt::Model::Object> *bucket_objects) const;
     bool put_object(const Aws::String &bucket_name, const Aws::String &object_key,
@@ -26,7 +27,7 @@ class aws_bucket_conn {
 
     private:
     /* data */
-    Aws::S3Crt::S3CrtClient m_s3_crt_client;
+    const Aws::S3Crt::S3CrtClient m_s3_crt_client;
     // const Aws::String& bucket_name;
     // Aws::SDKOptions m_options;
 };
