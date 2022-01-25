641ecc61051efcd1304ff66fd63995cdf7ab9a18
Keith Bostic <keith.bostic@mongodb.com>
2022-01-25 17:38:36 +0000

    Rework tiered open name matching.

--- src/block_cache/block_tier.c
+++ src/block_cache/block_tier.c
@@ -9,6 +9,37 @@
 #include "wt_internal.h"
 
 /*
+ * __blkcache_tiered_match --
+ *     Check for an already open block matching the tiered name.
+ */
+static bool
+__blkcache_tiered_match(
+  WT_SESSION_IMPL *session, const char *name, uint32_t objectid, WT_BLOCK **blockp)
+{
+    WT_BLOCK *block;
+    WT_CONNECTION_IMPL *conn;
+
+    *blockp = NULL;
+
+    conn = S2C(session);
+
+    /*
+     * KEITH XXX: Assume the name and object ID pair is unique, and used consistently.
+     *
+     * Lock and search for the object.
+     */
+    __wt_spin_lock(session, &conn->block_lock);
+    TAILQ_FOREACH (block, &conn->blockqh, q)
+        if (block->objectid == objectid && strcmp(block->name, name) == 0) {
+            ++block->ref;
+            *blockp = block;
+            break;
+        }
+    __wt_spin_unlock(session, &conn->block_lock);
+    return (*blockp != NULL);
+}
+
+/*
  * __wt_blkcache_tiered_open --
  *     Open a tiered object.
  */
@@ -19,20 +50,17 @@ __wt_blkcache_tiered_open(
     WT_BLOCK *block;
     WT_BUCKET_STORAGE *bstorage;
     WT_CONFIG_ITEM pfx, v;
+    WT_DECL_ITEM(tmp);
     WT_DECL_RET;
     WT_TIERED *tiered;
-    size_t len;
     uint32_t allocsize;
-    char *tmp;
     const char *cfg[2], *object_name, *object_uri, *object_val;
     bool local_only, readonly;
 
     *blockp = NULL;
 
     tiered = (WT_TIERED *)session->dhandle;
-    object_uri = NULL;
-    object_val = NULL;
-    tmp = NULL;
+    object_uri = object_val = NULL;
 
     WT_ASSERT(session, object_id <= tiered->current_id);
     WT_ASSERT(session, uri == NULL || WT_PREFIX_MATCH(uri, "tiered:"));
@@ -40,7 +68,11 @@ __wt_blkcache_tiered_open(
 
     /*
      * First look for the local file. This will be the fastest access and we retain recent objects
-     * in the local database for awhile.
+     * in the local database for awhile. If we're passed a name to open, then by definition it's a
+     * local file.
+     *
+     * FIXME-WT-7590 we will need some kind of locking while we're looking at the tiered structure.
+     * This can be called at any time, because we are opening the objects lazily.
      */
     if (uri != NULL)
         object_id = tiered->current_id;
@@ -58,32 +90,44 @@ __wt_blkcache_tiered_open(
         local_only = false;
         readonly = true;
 
-        F_SET(session, WT_SESSION_QUIET_TIERED); /* KEITH: use exists call instead */
+        F_SET(session, WT_SESSION_QUIET_TIERED); /* KEITH XXX: use exists call instead */
     }
+
+    /* Check for an already opened block structure. */
+    if (__blkcache_tiered_match(session, object_name, object_id, blockp))
+        return (0);
+
+    /*
+     * KEITH XXX: We're doing a ton of work to get the allocation size; the block code can do that
+     * work, we don't need to do it here. Fix the btree code to pass any changed alloc size in the
+     * cfg[] and get rid of the argument and this code.
+     */
     WT_ERR(__wt_metadata_search(session, object_uri, (char **)&object_val));
     cfg[0] = object_val;
     cfg[1] = NULL;
     WT_ERR(__wt_config_gets(session, cfg, "allocation_size", &v));
     allocsize = (uint32_t)v.val;
+
     ret = __wt_block_open(session, object_name, cfg, false, readonly, false, allocsize, &block);
     F_CLR(session, WT_SESSION_QUIET_TIERED);
 
-    /*
-     * FIXME-WT-7590 we will need some kind of locking while we're looking at the tiered structure.
-     * This can be called at any time, because we are opening the objects lazily.
-     */
     if (!local_only && ret != 0) {
-        WT_ERR(__wt_config_gets(session, cfg, "tiered_storage.bucket_prefix", &pfx));
         /* We expect a prefix. */
+        WT_ERR(__wt_config_gets(session, cfg, "tiered_storage.bucket_prefix", &pfx));
         WT_ASSERT(session, pfx.len != 0);
-        len = strlen(object_name) + pfx.len + 1;
-        WT_ERR(__wt_calloc_def(session, len, &tmp)); /* KEITH XXX use a scratch buffer */
-        WT_ERR(__wt_snprintf(tmp, len, "%.*s%s", (int)pfx.len, pfx.str, object_name));
+
+        WT_ERR(__wt_scr_alloc(session, 0, &tmp));
+        WT_ERR(__wt_buf_fmt(session, tmp, "%.*s%s", (int)pfx.len, pfx.str, object_name));
+
+        /* Check for an already opened block structure. */
+        if (__blkcache_tiered_match(session, tmp->mem, object_id, blockp))
+            return (0);
+
         bstorage = tiered->bstorage;
         WT_WITH_BUCKET_STORAGE(bstorage, session,
-          ret = __wt_block_open(session, object_name, cfg, false, true, true, allocsize, &block));
-        WT_ERR(ret);
+          ret = __wt_block_open(session, tmp->mem, cfg, false, true, true, allocsize, &block));
     }
+    WT_ERR(ret);
 
     block->has_objects = true;
     block->objectid = object_id;
@@ -93,7 +137,7 @@ err:
     if (!local_only)
         __wt_free(session, object_uri);
     __wt_free(session, object_val);
-    __wt_free(session, tmp);
+    __wt_scr_free(session, &tmp);
     return (ret);
 }
 
@@ -106,19 +150,17 @@ int
 __wt_blkcache_get_handle(
   WT_SESSION_IMPL *session, WT_BLOCK *orig, uint32_t objectid, WT_BLOCK **blockp)
 {
-    WT_BLOCK *block;
-    WT_CONNECTION_IMPL *conn;
     u_int i;
-    const char *name;
 
     *blockp = NULL;
-
-    conn = S2C(session);
 
     /* We should never be looking for our own object. */
     WT_ASSERT(session, orig == NULL || orig->objectid != objectid);
 
-    /* Check the local cache for the object. */
+    /*
+     * Check the local cache for the object. We don't have to check the name because we can only
+     * reference objects in our name space.
+     */
     if (orig != NULL) {
         for (i = 0; i < orig->related_next; ++i)
             if (orig->related[i]->objectid == objectid) {
@@ -132,24 +174,13 @@ __wt_blkcache_get_handle(
     }
 
     /*
-     * Lock and search for the object.
+     * Get a reference to the object, opening it as necessary.
      *
-     * KEITH XXX: Assume the name and object ID pair is unique, and used consistently. I'm not sure
-     * that's the case, and taking the name from the session dhandle isn't right.
+     * KEITH XXX: could we search the list of block structures without doing the name mapping the
+     * tiered-open function implies? If so, that would be faster. I'm pretty sure the answer is
+     * "no", but I don't know that for a fact.
      */
-    name = orig == NULL ? session->dhandle->name : orig->name;
-    __wt_spin_lock(session, &conn->block_lock);
-    TAILQ_FOREACH (block, &conn->blockqh, q)
-        if (block->objectid == objectid && strcmp(block->name, name) == 0) {
-            ++block->ref;
-            *blockp = block;
-            break;
-        }
-    __wt_spin_unlock(session, &conn->block_lock);
-
-    /* Open if it wasn't there. */
-    if (block == NULL)
-        WT_RET(__wt_blkcache_tiered_open(session, NULL, objectid, blockp));
+    WT_RET(__wt_blkcache_tiered_open(session, NULL, objectid, blockp));
 
     /* Save a reference in the block in which we started for fast subsequent access. */
     if (orig != NULL)
