0bb5df78fd0020cee1605565a7bf0ff78a960ee4
Will Korteland <will.korteland@mongodb.com>
2022-01-28 15:03:42 +1100

    build simple failing test that uses __wt_block_extlist_read

--- test/unittest/tests/extent_list.cxx
+++ test/unittest/tests/extent_list.cxx
@@ -1,7 +1,15 @@
 #include <catch2/catch.hpp>
 
+#include "wt_internal.h"
+
 uint32_t factorial(uint32_t num) {
     return num > 1 ? factorial(num - 1) * num : 1;
+}
+
+TEST_CASE("Playing with extent list testing", "[extent_list]") {
+    int res = __wt_block_extlist_read(nullptr, nullptr, nullptr, 0);
+
+    REQUIRE(res == 1);
 }
 
 TEST_CASE("Factorials are computed", "[factorial]") {
