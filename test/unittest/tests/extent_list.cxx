df4daf49e83ee952f019b17e9bb23af99bf46dbf
Will Korteland <will.korteland@mongodb.com>
2022-01-31 06:04:34 +0000

    minor cleanup

--- test/unittest/tests/extent_list.cxx
+++ test/unittest/tests/extent_list.cxx
@@ -6,9 +6,9 @@
 
 TEST_CASE("block_off_srch_last", "[extent_list]") {
     std::vector<WT_EXT*> head(WT_SKIP_MAXDEPTH, nullptr);
-    WT_EXT **stack[WT_SKIP_MAXDEPTH];
+    std::vector<WT_EXT**> stack(WT_SKIP_MAXDEPTH, nullptr);
 
     SECTION("empty list has empty final element") {
-        REQUIRE(__ut_block_off_srch_last(&head[0], stack) == nullptr);
+        REQUIRE(__ut_block_off_srch_last(&head[0], &stack[0]) == nullptr);
     }
 }
