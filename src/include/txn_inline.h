aae764e016dc5b121fb6c16d319345eedfb475e4
Ravi Giri <ravi.giri@mongodb.com>
2022-01-31 10:22:09 +0000

    WT-8709 Assertion failure in reconcile_save_evict_state, 146: WT_TXNID_LE(mod-> last_oldest_id, oldest_id)

--- src/include/txn_inline.h
+++ src/include/txn_inline.h
@@ -476,7 +476,8 @@ __wt_txn_oldest_id(WT_SESSION_IMPL *session)
      */
     WT_READ_BARRIER();
 
-    if (!F_ISSET(conn, WT_CONN_RECOVERING)) {
+    if (!F_ISSET(conn, WT_CONN_RECOVERING) || session->dhandle == NULL ||
+      __wt_btree_immediately_durable(session)) {
         /*
          * Checkpoint transactions often fall behind ordinary application threads. If there is an
          * active checkpoint, keep changes until checkpoint is finished.
