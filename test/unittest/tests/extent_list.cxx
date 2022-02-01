6f8dc5c4767fdb644ae4721185317fa1609aa723
Will Korteland <will.korteland@mongodb.com>
2022-02-01 06:01:06 +0000

    add a couple more tests for block_off_srch_last

--- test/unittest/tests/extent_list.cxx
+++ test/unittest/tests/extent_list.cxx
@@ -4,11 +4,92 @@
 
 #include "wt_internal.h"
 
+WT_EXT* create_new_extent_list() {
+    // manually alloc enough extra space for the zero-length array at the end
+    auto sz = sizeof(WT_EXT) + WT_SKIP_MAXDEPTH * sizeof(WT_EXT*);
+
+    WT_EXT* ret = (WT_EXT*)malloc(sz);
+    memset(ret, 0, sz);
+
+    return ret;
+}
+
+void print_list(WT_EXT **head) {
+    WT_EXT* extp;
+    int i;
+
+    if (head == nullptr)
+        return;
+
+    for (i = 0; i < WT_SKIP_MAXDEPTH; i++) {
+        printf("L%d: ", i);
+
+        extp = head[i];
+        while (extp != nullptr) {
+            printf("%p -> ", extp);
+            extp = extp->next[i];
+        }
+
+        printf("X\n");
+    }
+}
+
 TEST_CASE("block_off_srch_last", "[extent_list]") {
     std::vector<WT_EXT*> head(WT_SKIP_MAXDEPTH, nullptr);
     std::vector<WT_EXT**> stack(WT_SKIP_MAXDEPTH, nullptr);
 
     SECTION("empty list has empty final element") {
         REQUIRE(__ut_block_off_srch_last(&head[0], &stack[0]) == nullptr);
+
+        for (int i = 0; i < WT_SKIP_MAXDEPTH; i++) {
+            REQUIRE(stack[i] == &head[i]);
+        }
+    }
+
+    SECTION("list with one element has non-empty final element", "[extent_list]") {
+        WT_EXT* first = create_new_extent_list();
+        head[0] = first;
+
+        REQUIRE(__ut_block_off_srch_last(&head[0], &stack[0]) == first);
+    }
+
+    SECTION("list with identical skip entries returns identical stack entries", "[extent_list]") {
+        WT_EXT* first = create_new_extent_list();
+        for (int i = 0; i < WT_SKIP_MAXDEPTH; i++)
+            head[i] = first;
+
+        __ut_block_off_srch_last(&head[0], &stack[0]);
+
+        for (int i = 0; i < WT_SKIP_MAXDEPTH; i++) {
+            REQUIRE(stack[i] == &head[i]->next[i]);
+        }
+    }
+
+    SECTION("list with differing skip entries returns differing stack entries", "[extent_list]") {
+        WT_EXT* first = create_new_extent_list();
+        WT_EXT* second = create_new_extent_list();
+        first->next[0] = second;
+
+        head[0] = first;
+        for (int i = 1; i < WT_SKIP_MAXDEPTH; i++)
+            head[i] = second;
+
+        __ut_block_off_srch_last(&head[0], &stack[0]);
+
+        for (int i = 0; i < WT_SKIP_MAXDEPTH; i++) {
+            REQUIRE(stack[i] == &second->next[i]);
+        }
+    }
+
+    SECTION("list with differing skip entries returns final entry", "[extent_list]") {
+        WT_EXT* first = create_new_extent_list();
+        WT_EXT* second = create_new_extent_list();
+        first->next[0] = second;
+
+        head[0] = first;
+        for (int i = 1; i < WT_SKIP_MAXDEPTH; i++)
+            head[i] = second;
+
+        REQUIRE(__ut_block_off_srch_last(&head[0], &stack[0]) == second);
     }
 }
