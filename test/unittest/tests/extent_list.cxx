2eb28b2ab7e558dfb17854cc031c08c3641f92a9
Will Korteland <will.korteland@mongodb.com>
2022-02-03 00:57:05 +0000

    convert manual memory management to smart pointer

--- test/unittest/tests/extent_list.cxx
+++ test/unittest/tests/extent_list.cxx
@@ -4,14 +4,13 @@
 
 #include "wt_internal.h"
 
-// can't use a unique_ptr here because of the zero-length array business
-WT_EXT* create_new_extent_list() {
+std::unique_ptr<WT_EXT> create_new_extent_list() {
     // manually alloc enough extra space for the zero-length array to encode two
     // skip lists.
     auto sz = sizeof(WT_EXT) + 2 * WT_SKIP_MAXDEPTH * sizeof(WT_EXT*);
 
-    WT_EXT* ret = (WT_EXT*)malloc(sz);
-    memset(ret, 0, sz);
+    auto ret = std::unique_ptr<WT_EXT>((WT_EXT*)malloc(sz));
+    memset(ret.get(), 0, sz);
 
     return ret;
 }
@@ -55,17 +54,17 @@ TEST_CASE("block_off_srch_last", "[extent_list]") {
         }
     }
 
-    SECTION("list with one element has non-empty final element", "[extent_list]") {
-        WT_EXT* first = create_new_extent_list();
-        head[0] = first;
+    SECTION("list with one element has non-empty final element") {
+        auto first = create_new_extent_list();
+        head[0] = first.get();
 
-        REQUIRE(__ut_block_off_srch_last(&head[0], &stack[0]) == first);
+        REQUIRE(__ut_block_off_srch_last(&head[0], &stack[0]) == head[0]);
     }
 
-    SECTION("list with identical skip entries returns identical stack entries", "[extent_list]") {
-        WT_EXT* first = create_new_extent_list();
+    SECTION("list with identical skip entries returns identical stack entries") {
+        auto first = create_new_extent_list();
         for (int i = 0; i < WT_SKIP_MAXDEPTH; i++)
-            head[i] = first;
+            head[i] = first.get();
 
         __ut_block_off_srch_last(&head[0], &stack[0]);
 
@@ -74,17 +73,17 @@ TEST_CASE("block_off_srch_last", "[extent_list]") {
         }
     }
 
-    SECTION("list with differing skip entries returns differing stack entries", "[extent_list]") {
-        WT_EXT* first = create_new_extent_list();
-        WT_EXT* second = create_new_extent_list();
-        WT_EXT* third = create_new_extent_list();
-        first->next[0] = second;
-        first->next[1] = third;
-        second->next[0] = third;
+    SECTION("list with differing skip entries returns differing stack entries") {
+        auto first = create_new_extent_list();
+        auto second = create_new_extent_list();
+        auto third = create_new_extent_list();
+        first->next[0] = second.get();
+        first->next[1] = third.get();
+        second->next[0] = third.get();
 
-        head[0] = first;
-        head[1] = second;
-        head[2] = third;
+        head[0] = first.get();
+        head[1] = second.get();
+        head[2] = third.get();
         for (int i = 3; i < WT_SKIP_MAXDEPTH; i++)
             head[i] = nullptr;
 
@@ -98,16 +97,16 @@ TEST_CASE("block_off_srch_last", "[extent_list]") {
         }
     }
 
-    SECTION("list with differing skip entries returns final entry", "[extent_list]") {
-        WT_EXT* first = create_new_extent_list();
-        WT_EXT* second = create_new_extent_list();
-        first->next[0] = second;
+    SECTION("list with differing skip entries returns final entry") {
+        auto first = create_new_extent_list();
+        auto second = create_new_extent_list();
+        first->next[0] = second.get();
 
-        head[0] = first;
+        head[0] = first.get();
         for (int i = 1; i < WT_SKIP_MAXDEPTH; i++)
-            head[i] = second;
+            head[i] = second.get();
 
-        REQUIRE(__ut_block_off_srch_last(&head[0], &stack[0]) == second);
+        REQUIRE(__ut_block_off_srch_last(&head[0], &stack[0]) == second.get());
     }
 }
 
@@ -115,7 +114,7 @@ TEST_CASE("block_off_srch", "[extent_list]") {
     std::vector<WT_EXT*> head(WT_SKIP_MAXDEPTH, nullptr);
     std::vector<WT_EXT**> stack(WT_SKIP_MAXDEPTH, nullptr);
 
-    SECTION("can't find offset in empty list", "[extent_list]") {
+    SECTION("can't find offset in empty list") {
         __ut_block_off_srch(&head[0], 0, &stack[0], false);
 
         for (int i = 0; i < WT_SKIP_MAXDEPTH; i++)
@@ -123,16 +122,16 @@ TEST_CASE("block_off_srch", "[extent_list]") {
     }
 
     SECTION("exact offset match returns matching list element") {
-        WT_EXT* first = create_new_extent_list();
-        WT_EXT* second = create_new_extent_list();
-        WT_EXT* third = create_new_extent_list();
-        first->next[0] = second;
-        first->next[1] = third;
-        second->next[0] = third;
+        auto first = create_new_extent_list();
+        auto second = create_new_extent_list();
+        auto third = create_new_extent_list();
+        first->next[0] = second.get();
+        first->next[1] = third.get();
+        second->next[0] = third.get();
 
-        head[0] = first;
-        head[1] = second;
-        head[2] = third;
+        head[0] = first.get();
+        head[1] = second.get();
+        head[2] = third.get();
         for (int i = 3; i < WT_SKIP_MAXDEPTH; i++)
             head[i] = nullptr;
 
@@ -151,16 +150,16 @@ TEST_CASE("block_off_srch", "[extent_list]") {
     }
 
     SECTION("search for item larger than maximum in list returns end of list") {
-        WT_EXT* first = create_new_extent_list();
-        WT_EXT* second = create_new_extent_list();
-        WT_EXT* third = create_new_extent_list();
-        first->next[0] = second;
-        first->next[1] = third;
-        second->next[0] = third;
+        auto first = create_new_extent_list();
+        auto second = create_new_extent_list();
+        auto third = create_new_extent_list();
+        first->next[0] = second.get();
+        first->next[1] = third.get();
+        second->next[0] = third.get();
 
-        head[0] = first;
-        head[1] = second;
-        head[2] = third;
+        head[0] = first.get();
+        head[1] = second.get();
+        head[2] = third.get();
         for (int i = 3; i < WT_SKIP_MAXDEPTH; i++)
             head[i] = nullptr;
 
@@ -180,17 +179,17 @@ TEST_CASE("block_off_srch", "[extent_list]") {
     SECTION("respect skip offset") {
         const int depth = 10;
 
-        WT_EXT* first = create_new_extent_list();
-        WT_EXT* second = create_new_extent_list();
-        WT_EXT* third = create_new_extent_list();
+        auto first = create_new_extent_list();
+        auto second = create_new_extent_list();
+        auto third = create_new_extent_list();
 
-        first->next[0 + depth] = second;
-        first->next[1 + depth] = third;
-        second->next[0 + depth] = third;
+        first->next[0 + depth] = second.get();
+        first->next[1 + depth] = third.get();
+        second->next[0 + depth] = third.get();
 
-        head[0] = first;
-        head[1] = second;
-        head[2] = third;
+        head[0] = first.get();
+        head[1] = second.get();
+        head[2] = third.get();
         for (int i = 3; i < WT_SKIP_MAXDEPTH; i++)
             head[i] = nullptr;
 
@@ -225,15 +224,15 @@ TEST_CASE("block_first_srch", "[extent_list]") {
     }
 
     SECTION("list with too-small chunks doesn't yield a larger chunk") {
-        WT_EXT* first = create_new_extent_list();
-        WT_EXT* second = create_new_extent_list();
-        WT_EXT* third = create_new_extent_list();
-        first->next[0] = second;
-        second->next[0] = third;
+        auto first = create_new_extent_list();
+        auto second = create_new_extent_list();
+        auto third = create_new_extent_list();
+        first->next[0] = second.get();
+        second->next[0] = third.get();
 
-        head[0] = first;
-        head[1] = second;
-        head[2] = third;
+        head[0] = first.get();
+        head[1] = second.get();
+        head[2] = third.get();
         for (int i = 3; i < WT_SKIP_MAXDEPTH; i++)
             head[i] = nullptr;
 
@@ -245,15 +244,15 @@ TEST_CASE("block_first_srch", "[extent_list]") {
     }
 
     SECTION("find an appropriate chunk") {
-        WT_EXT* first = create_new_extent_list();
-        WT_EXT* second = create_new_extent_list();
-        WT_EXT* third = create_new_extent_list();
-        first->next[0] = second;
-        second->next[0] = third;
+        auto first = create_new_extent_list();
+        auto second = create_new_extent_list();
+        auto third = create_new_extent_list();
+        first->next[0] = second.get();
+        second->next[0] = third.get();
 
-        head[0] = first;
-        head[1] = second;
-        head[2] = third;
+        head[0] = first.get();
+        head[1] = second.get();
+        head[2] = third.get();
         for (int i = 3; i < WT_SKIP_MAXDEPTH; i++)
             head[i] = nullptr;
 
