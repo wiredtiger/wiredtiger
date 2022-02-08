7440ae8d1218fbd3b1488125184af95efadfdeb1
Will Korteland <will.korteland@mongodb.com>
2022-02-08 03:09:37 +0000

    fix extern prototype generation script

--- src/include/extern.h
+++ src/include/extern.h
@@ -2353,8 +2353,11 @@ static inline void __wt_upd_value_assign(WT_UPDATE_VALUE *upd_value, WT_UPDATE *
 static inline void __wt_upd_value_clear(WT_UPDATE_VALUE *upd_value);
 
 #ifdef HAVE_UNITTEST
-extern WT_EXT *__ut_block_off_srch_last(WT_EXT**, WT_EXT***);
-extern void __ut_block_off_srch(WT_EXT**, wt_off_t, WT_EXT***, bool);
-extern bool __ut_block_first_srch(WT_EXT**, wt_off_t, WT_EXT***);
-extern void __ut_block_size_srch(WT_SIZE**, wt_off_t, WT_SIZE***);
+extern WT_EXT *__ut_block_off_srch_last(WT_EXT **head, WT_EXT ***stack)
+  WT_GCC_FUNC_DECL_ATTRIBUTE((warn_unused_result));
+extern bool __ut_block_first_srch(WT_EXT **head, wt_off_t size, WT_EXT ***stack)
+  WT_GCC_FUNC_DECL_ATTRIBUTE((warn_unused_result));
+extern void __ut_block_off_srch(WT_EXT **head, wt_off_t off, WT_EXT ***stack, bool skip_off);
+extern void __ut_block_size_srch(WT_SIZE **head, wt_off_t size, WT_SIZE ***stack);
+
 #endif
