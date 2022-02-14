8c7c03f84478a83998b1038c82b7d73eebebae11
Keith Bostic <keith.bostic@mongodb.com>
2022-02-14 16:36:43 +0000

    Comment on step behavior.

--- test/suite/test_tiered08.py
+++ test/suite/test_tiered08.py
@@ -105,6 +105,7 @@ class test_tiered08(wttest.WiredTigerTestCase):
     def verify(self, key_count):
         self.pr('Verifying tiered table: {}'.format(str(key_count)))
         c = self.session.open_cursor(self.uri, None, None)
+        # Speed up the test by not looking at every key/value pair.
         for i in range(1, key_count, 237):
             self.assertEqual(c[self.key_gen(i)], self.value_gen(i))
         c.close()
