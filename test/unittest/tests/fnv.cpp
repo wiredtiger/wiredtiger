b12761de648fcfea49815b16e3dc97073b604e91
Jeremy Thorp <jeremy.thorp@mongodb.com>
2022-02-15 23:48:57 +0000

    Changed formatting to make clang-format happy.

--- test/unittest/tests/fnv.cpp
+++ test/unittest/tests/fnv.cpp
@@ -2,7 +2,8 @@
 
 #include "wt_internal.h"
 
-// due credit: the cases that hash to zero came from http://www.isthe.com/chongo/tech/comp/fnv/#zero-hash
+// due credit: the cases that hash to zero came from
+// http://www.isthe.com/chongo/tech/comp/fnv/#zero-hash
 TEST_CASE("hash_fnv64", "[fnv]")
 {
     const uint64_t fnv1a_64_init = 0xcbf29ce484222325;
@@ -12,7 +13,7 @@ TEST_CASE("hash_fnv64", "[fnv]")
     REQUIRE(__wt_hash_fnv64("a", 1) == 0xaf63dc4c8601ec8c);
     REQUIRE(__wt_hash_fnv64("asdf", 4) == 0x90285684421f9857);
 
-    const std::vector<uint8_t> hash_to_zero = { 0xd5, 0x6b, 0xb9, 0x53, 0x42, 0x87, 0x08, 0x36 };
+    const std::vector<uint8_t> hash_to_zero = {0xd5, 0x6b, 0xb9, 0x53, 0x42, 0x87, 0x08, 0x36};
     REQUIRE(__wt_hash_fnv64(&hash_to_zero[0], hash_to_zero.size()) == 0x00);
 
     const std::string ascii_hash_to_zero = "!0IC=VloaY";
