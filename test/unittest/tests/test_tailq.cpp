5cc91d846d7824816a643937a6fe1684948f7c85
Jeremy Thorp <jeremy.thorp@mongodb.com>
2022-02-17 04:47:39 +0000

    Layout changes to keep s_all happy.

--- test/unittest/tests/test_tailq.cpp
+++ test/unittest/tests/test_tailq.cpp
@@ -1,61 +1,52 @@
 /*-
-* Copyright (c) 2014-present MongoDB, Inc.
-* Copyright (c) 2008-2014 WiredTiger, Inc.
-*	All rights reserved.
-*
-* See the file LICENSE for redistribution information.
-*/
+ * Copyright (c) 2014-present MongoDB, Inc.
+ * Copyright (c) 2008-2014 WiredTiger, Inc.
+ *	All rights reserved.
+ *
+ * See the file LICENSE for redistribution information.
+ */
 
 #include <list>
 #include <catch2/catch.hpp>
 #include "wt_internal.h"
 
-
-template <class T>
-struct tailq_entry {
-    explicit tailq_entry<T>(T const& value) : _value(value) {};
+template <class T> struct tailq_entry {
+    explicit tailq_entry<T>(T const &value) : _value(value){};
     TAILQ_ENTRY(tailq_entry<T>) _queue;
     T _value;
 };
 
-
-template <class T>
-class TestTailQWrapper
-{
+template <class T> class TestTailQWrapper {
     public:
     TestTailQWrapper();
     ~TestTailQWrapper();
-    void pushBack(const T& value);
-    void pushBack(std::list<T> const& values);
-    void removeValue(const T& value);
+    void pushBack(const T &value);
+    void pushBack(std::list<T> const &values);
+    void removeValue(const T &value);
     std::list<T> copyItemsFromTailQ();
 
     private:
     TAILQ_HEAD(, tailq_entry<T>) _tailq;
 };
 
-
-template <class T>
-TestTailQWrapper<T>::TestTailQWrapper()
+template <class T> TestTailQWrapper<T>::TestTailQWrapper()
 {
     _tailq = TAILQ_HEAD_INITIALIZER(_tailq);
 }
 
-
-template <class T>
-TestTailQWrapper<T>::~TestTailQWrapper()
+template <class T> TestTailQWrapper<T>::~TestTailQWrapper()
 {
-    tailq_entry<T>* item = TAILQ_FIRST(&_tailq);
+    tailq_entry<T> *item = TAILQ_FIRST(&_tailq);
     while (item) {
         TAILQ_REMOVE(&_tailq, item, _queue);
-        free(item); 
+        free(item);
         item = TAILQ_FIRST(&_tailq);
     }
 }
 
-
 template <class T>
-void TestTailQWrapper<T>::pushBack(const T& value)
+void
+TestTailQWrapper<T>::pushBack(const T &value)
 {
     auto item = static_cast<tailq_entry<T> *>(malloc(sizeof(tailq_entry<T>)));
     item->_value = value;
@@ -63,26 +54,21 @@ void TestTailQWrapper<T>::pushBack(const T& value)
     TAILQ_INSERT_TAIL(&_tailq, item, _queue);
 }
 
-
 template <class T>
-void TestTailQWrapper<T>::pushBack(std::list< T > const& values)
+void
+TestTailQWrapper<T>::pushBack(std::list<T> const &values)
 {
-    std::for_each(
-      values.begin(),
-      values.end(),
-      [this] (const int n) { this->pushBack(n); }
-    );
+    std::for_each(values.begin(), values.end(), [this](const int n) { this->pushBack(n); });
 }
 
-
 template <class T>
-void TestTailQWrapper<T>::removeValue(const T& value)
+void
+TestTailQWrapper<T>::removeValue(const T &value)
 {
-    tailq_entry<T>* item = TAILQ_FIRST(&_tailq);
+    tailq_entry<T> *item = TAILQ_FIRST(&_tailq);
     bool searching = true;
-    while ((item != nullptr) && searching)
-    {
-        tailq_entry<T>* nextItem = TAILQ_NEXT(item, _queue);
+    while ((item != nullptr) && searching) {
+        tailq_entry<T> *nextItem = TAILQ_NEXT(item, _queue);
         if (item->_value == value) {
             TAILQ_REMOVE(&_tailq, item, _queue);
             free(item);
@@ -93,24 +79,23 @@ void TestTailQWrapper<T>::removeValue(const T& value)
     }
 }
 
-
 template <class T>
-std::list< T > TestTailQWrapper<T>::copyItemsFromTailQ()
+std::list<T>
+TestTailQWrapper<T>::copyItemsFromTailQ()
 {
     std::list<T> items;
-    tailq_entry<T>* item = nullptr;
+    tailq_entry<T> *item = nullptr;
 
-    TAILQ_FOREACH(item, &_tailq, _queue) {
+    TAILQ_FOREACH (item, &_tailq, _queue) {
         items.push_back(item->_value);
     }
 
     return items;
 }
 
-
 TEST_CASE("Test TAILQ", "[TAILQ]")
 {
-    std::list<int> items {10, 20, 30, 40, 50, 60, 70};
+    std::list<int> items{10, 20, 30, 40, 50, 60, 70};
 
     TestTailQWrapper<int> testTailQWrapper;
     testTailQWrapper.pushBack(items);
@@ -135,6 +120,5 @@ TEST_CASE("Test TAILQ", "[TAILQ]")
         testTailQWrapper.removeValue(99);
         auto returnedItems3 = testTailQWrapper.copyItemsFromTailQ();
         CHECK(returnedItems3 == std::list<int>{10, 20, 40, 50, 70});
-
     }
 }
