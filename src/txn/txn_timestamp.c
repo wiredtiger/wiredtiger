58f2fa25af2ac998f5401209950160ffcc0b7045
Keith Bostic <keith.bostic@mongodb.com>
2022-02-27 18:55:52 +0000

    error: embedding a directive within macro arguments has undefined behavior [-Werror,-Wembedded-directive]

--- src/txn/txn_timestamp.c
+++ src/txn/txn_timestamp.c
@@ -566,12 +566,14 @@ __wt_txn_set_commit_timestamp(WT_SESSION_IMPL *session, wt_timestamp_t commit_ts
 #ifdef WT_STANDALONE_BUILD
         if (has_stable_ts && commit_ts <= stable_ts)
             WT_RET_MSG(session, EINVAL, "commit timestamp %s must be after the stable timestamp %s",
+              __wt_timestamp_to_string(commit_ts, ts_string[0]),
+              __wt_timestamp_to_string(stable_ts, ts_string[1]));
 #else
         if (has_stable_ts && commit_ts < stable_ts)
             WT_RET_MSG(session, EINVAL, "commit timestamp %s is before the stable timestamp %s",
-#endif
               __wt_timestamp_to_string(commit_ts, ts_string[0]),
               __wt_timestamp_to_string(stable_ts, ts_string[1]));
+#endif
 
         /*
          * Compare against the commit timestamp of the current transaction. Return an error if the
@@ -673,12 +675,14 @@ __wt_txn_set_durable_timestamp(WT_SESSION_IMPL *session, wt_timestamp_t durable_
 #ifdef WT_STANDALONE_BUILD
     if (has_stable_ts && durable_ts <= stable_ts)
         WT_RET_MSG(session, EINVAL, "durable timestamp %s must be after the stable timestamp %s",
+          __wt_timestamp_to_string(durable_ts, ts_string[0]),
+          __wt_timestamp_to_string(stable_ts, ts_string[1]));
 #else
     if (has_stable_ts && durable_ts < stable_ts)
         WT_RET_MSG(session, EINVAL, "durable timestamp %s is less than the stable timestamp %s",
-#endif
           __wt_timestamp_to_string(durable_ts, ts_string[0]),
           __wt_timestamp_to_string(stable_ts, ts_string[1]));
+#endif
 
     /* Check if the durable timestamp is less than the commit timestamp. */
     if (durable_ts < txn->commit_timestamp)
