6e358c100110e04cd4f9d6b56ca39e2a34c9f6e0
Jie Chen <jie.chen@mongodb.com>
2022-02-27 22:06:23 +0000

    Fixed up tests

--- test/suite/test_backup26.py
+++ test/suite/test_backup26.py
@@ -27,7 +27,7 @@
 # OTHER DEALINGS IN THE SOFTWARE.
 
 import wiredtiger, wttest
-import os, re
+import os, re, time
 from wtbackup import backup_base
 from wtscenario import make_scenarios
 from wtdataset import SimpleDataSet
@@ -39,48 +39,63 @@ from wtdataset import SimpleDataSet
 class test_backup26(backup_base):
     dir='backup.dir'                    # Backup directory name
     uri="table_backup"
-    ntables = 10000 if wttest.islongtest() else 50
+    ntables = 10000 if wttest.islongtest() else 500
 
     types = [
-        dict(uri='table:'),
-        dict(uri='file:'),
+        ["table", dict(uri='table:')],
+        ["file", dict(uri='file:')],
     ]
 
+    # Percentage of tables in table format, and inverse for file format.
+    percentage = [
+        ('hundred_precent', dict(percentage=1)),
+        ('fifty_percent', dict(percentage=0.5)),
+        ('ten_percent', dict(percentage=0.1)),
+        ('zero_percent', dict(percentage=0)),
+    ]
     remove_list = [
         ('empty remove list', dict(ext=None, type=None)),
         ('table remove list', dict(ext=".wt",type="table:")),
         ('file remove list', dict(ext="",type="file:")),
     ]
-    scenarios = make_scenarios(remove_list)
+    scenarios = make_scenarios(remove_list, percentage)
 
     def test_backup26(self):
         selective_remove_uri_list = []
         selective_file_list = []
-        for i in range(0, self.ntables):
-            dict_type = self.types[i % len(self.types)]
-            uri = "{0}{1}".format(dict_type["uri"], self.uri + str(i))
+        selective_remove_file_list = []
+        for i in range(0, int(self.ntables * self.percentage)):
+            uri = "file:{0}_file".format(self.uri + str(i))
             dataset = SimpleDataSet(self, uri, 100, key_format="S")
             dataset.populate()
-            if (self.type and self.type == dict_type["uri"]):
+            if (self.type and self.type == "file:"):
                 selective_remove_uri_list.append(uri)
+                selective_remove_file_list.append("{0}_file".format(self.uri + str(i)))
+            else:
+                selective_file_list.append(uri)
+
+        for i in range(0, int(self.ntables * (1 - self.percentage))):
+            uri = "table:{0}_table".format(self.uri + str(i))
+            dataset = SimpleDataSet(self, uri, 100, key_format="S")
+            dataset.populate()
+            if (self.type and self.type == "table:"):
+                selective_remove_uri_list.append(uri)
+                selective_remove_file_list.append("{0}_table.wt".format(self.uri + str(i)))
             else:
                 selective_file_list.append(uri)
         self.session.checkpoint()
 
         os.mkdir(self.dir)
-        file_list = os.listdir(".")
-        selective_remove_file_list = []
-        # Perform a regex match on all files and make sure that we match all the table name, 
-        # number and extension.
-        for file_uri in file_list:
-            if self.ext is not None and re.match('^{0}\d+{1}$'.format(self.uri, self.ext), file_uri):
-                selective_remove_file_list.append(file_uri)
 
         # Now copy the files using full backup. This should not include the tables inside the remove list.
         all_files = self.take_selective_backup(self.dir, selective_remove_file_list)
-        
+
+        starttime = time.time()        
         # After the full backup, open and recover the backup database.
         backup_conn = self.wiredtiger_open(self.dir, "backup_partial_restore=true")
+        elapsed = time.time() - starttime
+        self.pr("%s partial backup has taken %.2f seconds." % (str(self), elapsed))
+        
         bkup_session = backup_conn.open_session()
         # Open the cursor from uris that was part of the selective backup and expect failure
         # since file doesn't exist.
