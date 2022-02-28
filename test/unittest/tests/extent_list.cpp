269cb8cc4893bc1ca40637bc2d9143c1f41e690a
Jeremy Thorp <jeremy.thorp@mongodb.com>
2022-03-01 10:20:25 +1100

    Added missing copyright messages.

--- test/unittest/tests/extent_list.cpp
+++ test/unittest/tests/extent_list.cpp
@@ -1,3 +1,11 @@
+/*-
+* Copyright (c) 2014-present MongoDB, Inc.
+* Copyright (c) 2008-2014 WiredTiger, Inc.
+*	All rights reserved.
+*
+* See the file LICENSE for redistribution information.
+*/
+
 #include <memory>
 
 #include <catch2/catch.hpp>
--- test/unittest/tests/utils.h
+++ test/unittest/tests/utils.h
@@ -1,3 +1,11 @@
+/*-
+* Copyright (c) 2014-present MongoDB, Inc.
+* Copyright (c) 2008-2014 WiredTiger, Inc.
+*	All rights reserved.
+*
+* See the file LICENSE for redistribution information.
+*/
+
 namespace utils {
 void throwIfNonZero(int result);
 void wiredtigerCleanup();
