926a77a7abee6cd2962977284d22587bd7cfaa92
Jeremy Thorp <jeremy.thorp@mongodb.com>
2022-03-01 00:04:55 +0000

    Fixed layout of copyright message to make s_all happy.

--- test/unittest/tests/test_reconciliation_tracking.cpp
+++ test/unittest/tests/test_reconciliation_tracking.cpp
@@ -1,10 +1,10 @@
 /*-
-* Copyright (c) 2014-present MongoDB, Inc.
-* Copyright (c) 2008-2014 WiredTiger, Inc.
-*	All rights reserved.
-*
-* See the file LICENSE for redistribution information.
-*/
+ * Copyright (c) 2014-present MongoDB, Inc.
+ * Copyright (c) 2008-2014 WiredTiger, Inc.
+ *	All rights reserved.
+ *
+ * See the file LICENSE for redistribution information.
+ */
 
 #include <catch2/catch.hpp>
 
