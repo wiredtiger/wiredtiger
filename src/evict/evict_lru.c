dafa2199b09be4d3d6ab455302a8053516ab2a62
Clarisse Cheah <clarisse.cheah@mongodb.com>
2022-03-04 00:17:30 +0000

    Initialised cache_bytes_updates

--- src/evict/evict_lru.c
+++ src/evict/evict_lru.c
@@ -2699,7 +2699,7 @@ __wt_verbose_dump_cache(WT_SESSION_IMPL *session)
     conn = S2C(session);
     cache = conn->cache;
     total_bytes = total_dirty_bytes = total_updates_bytes = 0;
-    pct = 0.0; /* [-Werror=uninitialized] */
+    pct = cache_bytes_updates = 0.0; /* [-Werror=uninitialized] */
 
     WT_RET(__wt_msg(session, "%s", WT_DIVIDER));
     WT_RET(__wt_msg(session, "cache dump"));
