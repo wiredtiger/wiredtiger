c2d483ceeb34f9f18e862a16041dfc9357a7a091
Andrew Morton <andrew.morton@mongodb.com>
2022-03-09 03:02:12 +0000

    Align to 4.4

--- src/txn/txn.c
+++ src/txn/txn.c
@@ -1348,7 +1348,7 @@ __txn_resolve_prepared_op(WT_SESSION_IMPL *session, WT_TXN_OP *op, bool commit, 
          * rolling back a prepared reconciled update would result in only aborted updates on the
          * update chain.
          */
-        tw_found = __wt_read_cell_time_window(cbt, &tw);
+        __wt_read_cell_time_window(cbt, &tw, &tw_found);
         if (tw_found && tw.prepare == WT_PREPARE_INPROGRESS)
             WT_ERR(__txn_append_tombstone(session, op, cbt));
     }
