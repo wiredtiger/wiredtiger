d137505a2ee2b6ac15c6cc198051cbc60eecd946
Jie Chen <jie.chen@mongodb.com>
2022-03-16 03:39:00 +0000

    Revert "Added more testing hs"
    This reverts commit 95db4206b107e1830d7683cc8b08d78bdc143062.

--- test/suite/test_backup27.py
+++ test/suite/test_backup27.py
@@ -42,23 +42,20 @@ class test_backup27(backup_base):
     def add_timestamp_data(self, uri, key, val, timestamp):
         self.session.begin_transaction()
         c = self.session.open_cursor(uri, None, None)
-        for i in range(0, 10):
+        for i in range(0, 1000):
             k = key + str(i)
-            v = val
+            v = val + str(i)
             c[k] = v
         c.close()
         self.session.commit_transaction('commit_timestamp=' + self.timestamp_str(timestamp))
 
-    def validate_timestamp_data(self, session, uri, key, expected_val, expected_err, timestamp):
+    def validate_timestamp_data(self, session, uri, key, expected_err, timestamp):
         session.begin_transaction('read_timestamp=' + self.timestamp_str(timestamp))
         c = session.open_cursor(uri, None, None)
-        for i in range(0, 10):
+        for i in range(0, 1000):
             k = key + str(i)
             c.set_key(k)
             self.assertEqual(c.search(), expected_err)
-            if (expected_err == 0):
-                print(c.get_value(), timestamp)
-                self.assertEqual(c.get_value(), expected_val)
         c.close()
         session.commit_transaction()
 
@@ -88,15 +85,15 @@ class test_backup27(backup_base):
         bkup_session = backup_conn.open_session()
 
         # Test that the history store data still exists for the tables that got restored.
-        self.validate_timestamp_data(bkup_session, self.uri, "key", "val", 0, 1)
-        self.validate_timestamp_data(bkup_session, self.uri, "key", "val5", 0, 10)
+        self.validate_timestamp_data(bkup_session, self.uri, "key", 0, 1)
+        self.validate_timestamp_data(bkup_session, self.uri, "key", 0, 10)
 
         # Open the cursor and expect failure since file doesn't exist.
         self.assertRaisesException(
              wiredtiger.WiredTigerError,lambda: bkup_session.open_cursor(self.newuri, None, None))
         bkup_session.create(self.newuri, "key_format=S,value_format=S")
-        self.validate_timestamp_data(bkup_session, self.newuri, "key", None, wiredtiger.WT_NOTFOUND, 1)
-        self.validate_timestamp_data(bkup_session, self.newuri, "key", None, wiredtiger.WT_NOTFOUND, 10)
+        self.validate_timestamp_data(bkup_session, self.newuri, "key", wiredtiger.WT_NOTFOUND, 1)
+        self.validate_timestamp_data(bkup_session, self.newuri, "key", wiredtiger.WT_NOTFOUND, 5)
         backup_conn.close()
 
 if __name__ == '__main__':
