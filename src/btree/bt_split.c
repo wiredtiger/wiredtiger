2edae14cb62b4866b458d2e90462533016284090
Will Korteland <will.korteland@mongodb.com>
2022-03-22 05:23:06 +0000

    use a different approach

--- src/btree/bt_split.c
+++ src/btree/bt_split.c
@@ -1357,9 +1357,9 @@ __split_multi_inmem(WT_SESSION_IMPL *session, WT_PAGE *orig, WT_MULTI *multi, WT
     WT_PAGE *page;
     WT_PAGE_MODIFY *mod;
     WT_SAVE_UPD *supd;
-    WT_UPDATE *upd, *prev_upd, *upd_backup;
+    WT_UPDATE *prev_onpage, *upd, *tmp;
     uint64_t recno;
-    uint32_t i, slot, update_ds_counter;
+    uint32_t i, slot;
     bool prepare;
 
     /*
@@ -1409,8 +1409,6 @@ __split_multi_inmem(WT_SESSION_IMPL *session, WT_PAGE *orig, WT_MULTI *multi, WT
 
     /* Re-create each modification we couldn't write. */
     for (i = 0, supd = multi->supd; i < multi->supd_entries; ++i, ++supd) {
-        update_ds_counter = 0;
-
         /* Ignore update chains that don't need to be restored. */
         if (!supd->restore)
             continue;
@@ -1433,33 +1431,29 @@ __split_multi_inmem(WT_SESSION_IMPL *session, WT_PAGE *orig, WT_MULTI *multi, WT
          * size of the page is correct.
          */
         if (supd->onpage_upd != NULL && !F_ISSET(S2C(session), WT_CONN_IN_MEMORY)) {
+            tmp = supd->tombstone != NULL ? supd->tombstone : supd->onpage_upd;
+
             /*
              * We have decided to restore this update chain so it must have newer updates than the
              * onpage value on it.
              */
-            WT_ASSERT(session, upd != supd->onpage_upd);
+            WT_ASSERT(session, upd != tmp);
+            WT_ASSERT(session, F_ISSET(tmp, WT_UPDATE_DS));
 
             /*
              * Move the pointer to the position before the onpage value and truncate all the updates
              * starting from the onpage value.
              */
-            upd_backup = upd;
-            for (prev_upd = upd; prev_upd->next != NULL; prev_upd = prev_upd->next) {
-                if (F_ISSET(prev_upd->next, WT_UPDATE_DS)) {
-                    upd_backup = prev_upd->next;
-                    ++update_ds_counter;
-                    break;
-                }
-            }
-            prev_upd->next = NULL;
-
-            (void)upd_backup;
+            for (prev_onpage = upd;
+                 prev_onpage->next != NULL && prev_onpage->next != tmp;
+                 prev_onpage = prev_onpage->next)
+                 ;
+            WT_ASSERT(session, prev_onpage->next == tmp);
 #ifdef HAVE_DIAGNOSTIC
-            for (; upd_backup->next != NULL && upd_backup->next != supd->onpage_upd; upd_backup = upd_backup->next)
-                if (F_ISSET(upd_backup->next, WT_UPDATE_DS))
-                    ++update_ds_counter;
-            WT_ASSERT(session, update_ds_counter <= 1);
+            for (tmp = tmp->next;tmp != NULL && tmp != supd->onpage_upd; tmp = tmp->next)
+                WT_ASSERT(session, tmp->txnid == WT_TXN_ABORTED);
 #endif
+            prev_onpage->next = NULL;
         }
 
         switch (orig->type) {
