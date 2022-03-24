1f3c34c61142eef5494f883b422c863d724a7d29
Keith Bostic <keith.bostic@mongodb.com>
2022-03-24 01:30:58 +0000

    Add checks for WT_SESSION.timestamp_transaction.

--- test/suite/test_timestamp27.py
+++ test/suite/test_timestamp27.py
@@ -1,3 +1,5 @@
+
+
 #!/usr/bin/env python
 #
 # Public Domain 2014-present MongoDB, Inc.
@@ -59,6 +61,12 @@ class test_timestamp27_timestamp_notset(wttest.WiredTigerTestCase):
             self.session.begin_transaction()
             self.assertRaisesWithMessage(wiredtiger.WiredTigerError, lambda:
                 self.session.prepare_transaction('prepare_timestamp=0'), '/zero not permitted/')
+
+    def test_session_set_timestamp_zero(self):
+        self.session.begin_transaction()
+        for ts in ['commit', 'durable', 'prepare', 'read']:
+            self.assertRaisesWithMessage(wiredtiger.WiredTigerError, lambda:
+                self.session.timestamp_transaction(ts + '_timestamp=0'), '/zero not permitted/')
 
 if __name__ == '__main__':
     wttest.run()
