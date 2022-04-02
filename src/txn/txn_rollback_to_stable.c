f82c9db11b1074b7bd8c1f43687e44af497fb7e3
Keith Bostic <keith.bostic@mongodb.com>
2022-04-02 21:05:22 +0000

    More minor cleanup.

--- src/txn/txn_rollback_to_stable.c
+++ src/txn/txn_rollback_to_stable.c
@@ -1234,19 +1234,18 @@ __rollback_to_stable_page_skip(
     WT_PAGE_DELETED *page_del;
     wt_timestamp_t rollback_timestamp;
 
-    rollback_timestamp = *(wt_timestamp_t *)(context);
-    *skipp = false; /* Default to reading */
-
+    rollback_timestamp = *(wt_timestamp_t *)context;
     WT_UNUSED(visible_all);
+
+    *skipp = false; /* Default to reading */
 
     /* We can skip unprepared fast-delete operations durable at or before the RTS timestamp. */
     if (ref->state == WT_REF_DELETED) {
         page_del = ref->ft_info.del;
         if (page_del != NULL && page_del->prepare_state == 0 &&
-          page_del->durable_timestamp <= rollback_timestamp) {
+          page_del->durable_timestamp <= rollback_timestamp)
             *skipp = true;
-            return (0);
-        }
+        return (0);
     }
 
     /* Otherwise, if the page state is other than on disk, we want to look at it. */
@@ -1277,7 +1276,7 @@ __rollback_to_stable_btree_walk(WT_SESSION_IMPL *session, wt_timestamp_t rollbac
     /* Walk the tree, marking commits aborted where appropriate. */
     ref = NULL;
     while (
-      (ret = __wt_tree_walk_custom_skip(session, &ref, __rollback_to_stable__page_skip,
+      (ret = __wt_tree_walk_custom_skip(session, &ref, __rollback_to_stable_page_skip,
          &rollback_timestamp, WT_READ_NO_EVICT | WT_READ_WONT_NEED | WT_READ_VISIBLE_ALL)) == 0 &&
       ref != NULL)
         if (F_ISSET(ref, WT_REF_FLAG_LEAF))
