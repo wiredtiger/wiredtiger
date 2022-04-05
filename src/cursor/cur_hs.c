d5e1fef548f8b2f5bb32979403deb6e0457dc739
Chenhao Qu <chenhao.qu@mongodb.com>
2022-04-05 17:46:33 +1000

    No need to handle not found

--- src/cursor/cur_hs.c
+++ src/cursor/cur_hs.c
@@ -940,10 +940,8 @@ __curhs_insert(WT_CURSOR *cursor)
 #ifdef HAVE_DIAGNOSTIC
     /* Do a search again and call next to check the key order. */
     WT_WITH_TXN_ISOLATION(session, WT_ISO_READ_UNCOMMITTED, ret = file_cursor->search(file_cursor));
-    if (ret == 0)
-        WT_ERR_NOTFOUND_OK(__curhs_file_cursor_next(session, file_cursor), false);
-    else
-        WT_ASSERT(session, ret == WT_NOTFOUND);
+    WT_ASSERT(session, ret == 0);
+    WT_ERR_NOTFOUND_OK(__curhs_file_cursor_next(session, file_cursor), false);
 #endif
 
     /* Insert doesn't maintain a position across calls, clear resources. */
