b0d25586d4a017f10a2ab15c2282c3d5c35e6760
Etienne Petrel <etienne.petrel@mongodb.com>
2022-04-20 02:58:14 +0000

    Rename constants to avoid ambiguity

--- test/cppsuite/test_harness/workload_generator.cpp
+++ test/cppsuite/test_harness/workload_generator.cpp
@@ -88,13 +88,13 @@ workload_generator::run()
 
     /* Retrieve useful parameters from the test configuration. */
     operation_configs.push_back(
-      operation_config(_config->get_subconfig(CUSTOM_CONFIG), thread_type::CUSTOM));
+      operation_config(_config->get_subconfig(CUSTOM_OP_CONFIG), thread_type::CUSTOM));
     operation_configs.push_back(
-      operation_config(_config->get_subconfig(INSERT_CONFIG), thread_type::INSERT));
+      operation_config(_config->get_subconfig(INSERT_OP_CONFIG), thread_type::INSERT));
     operation_configs.push_back(
-      operation_config(_config->get_subconfig(READ_CONFIG), thread_type::READ));
+      operation_config(_config->get_subconfig(READ_OP_CONFIG), thread_type::READ));
     operation_configs.push_back(
-      operation_config(_config->get_subconfig(UPDATE_CONFIG), thread_type::UPDATE));
+      operation_config(_config->get_subconfig(UPDATE_OP_CONFIG), thread_type::UPDATE));
     populate_config = _config->get_subconfig(POPULATE_CONFIG);
 
     /* Populate the database. */
