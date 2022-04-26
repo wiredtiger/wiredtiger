9306917bd59ff7f8a3657dab5d002ffb8c7fdcec
Susan LoVerso <sue@mongodb.com>
2022-04-26 11:38:39 -0400

    S2C and use WT_ERR.

--- src/tiered/tiered_config.c
+++ src/tiered/tiered_config.c
@@ -174,8 +174,8 @@ __wt_tiered_conn_config(WT_SESSION_IMPL *session, const char **cfg, bool reconfi
     __wt_verbose(
       session, WT_VERB_TIERED, "TIERED_CONFIG: prefix %s", conn->bstorage->bucket_prefix);
 
-    WT_RET(__wt_config_gets(session, cfg, "tiered_storage.interval", &cval));
-    S2C(session)->tiered_interval = (uint64_t)cval.val;
+    WT_ERR(__wt_config_gets(session, cfg, "tiered_storage.interval", &cval));
+    conn->tiered_interval = (uint64_t)cval.val;
 
     WT_ASSERT(session, conn->bstorage != NULL);
     WT_STAT_CONN_SET(session, tiered_object_size, conn->bstorage->object_size);
