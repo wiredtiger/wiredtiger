4ff102d485eb4b260573116f0ba5602b7cc15da6
Etienne Petrel <etienne.petrel@mongodb.com>
2022-04-26 10:56:31 +0000

    Update function's header

--- test/cppsuite/test_harness/workload/thread_context.h
+++ test/cppsuite/test_harness/workload/thread_context.h
@@ -129,8 +129,7 @@ class thread_context {
     bool update(scoped_cursor &cursor, uint64_t collection_id, const std::string &key);
 
     /*
-     * Generic insert function, takes a collection_id and key_id, will generate the value. If a
-     * timestamp is not specified, the timestamp manager will generate one.
+     * Generic insert function, takes a collection_id and key_id, will generate the value.
      *
      * Return true if the operation was successful, a return value of false implies the transaction
      * needs to be rolled back.
