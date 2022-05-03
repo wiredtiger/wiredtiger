baf5f9a2889e05b41683e7c44905a4d4df25c6d3
Chenhao Qu <chenhao.qu@mongodb.com>
2022-05-03 13:39:41 +1000

    Enable column tests

--- test/suite/test_prepare21.py
+++ test/suite/test_prepare21.py
@@ -39,8 +39,8 @@ from wtthread import checkpoint_thread
 class test_prepare21(test_rollback_to_stable_base):
 
     format_values = [
-        #('column', dict(key_format='r', value_format='S')),
-        #('column_fix', dict(key_format='r', value_format='8t')),
+        ('column', dict(key_format='r', value_format='S')),
+        ('column_fix', dict(key_format='r', value_format='8t')),
         ('row_integer', dict(key_format='i', value_format='S')),
     ]
 
