665817562a37fa9050b20e55b48439f31428a377
Keith Bostic <keith.bostic@mongodb.com>
2022-05-18 03:10:38 +0000

    Rework a comment.

--- src/btree/bt_cursor.c
+++ src/btree/bt_cursor.c
@@ -1967,7 +1967,8 @@ __wt_btcur_range_truncate(WT_CURSOR_BTREE *start, WT_CURSOR_BTREE *stop)
     /*
      * All historical versions must be removed when a key is updated with no timestamp, but that
      * isn't possible in fast truncate operations. Disallow fast truncate in transactions configured
-     * to commit without a timestamp (excluding logged objects as timestamps will be ignored).
+     * to commit without a timestamp (excluding logged tables as timestamps cannot be relevant to
+     * them).
      */
     if (!F_ISSET(btree, WT_BTREE_LOGGED) && F_ISSET(session->txn, WT_TXN_TS_NOT_SET))
         WT_RET_MSG(session, EINVAL,
