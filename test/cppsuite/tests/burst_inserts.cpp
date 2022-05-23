c5e14957936f8e3039d4132c6c28b8ddaac0e61a
Etienne Petrel <etienne.petrel@mongodb.com>
2022-05-23 03:11:14 +0000

    Improve readibility

--- test/cppsuite/tests/burst_inserts.cpp
+++ test/cppsuite/tests/burst_inserts.cpp
@@ -96,16 +96,14 @@ class burst_inserts : public test {
               std::chrono::system_clock::now() - burst_start <
                 std::chrono::seconds(_burst_duration)) {
                 tc->transaction.try_begin();
-                cc.write_cursor->set_key(cc.write_cursor.get(),
-                  tc->pad_string(std::to_string(start_key + added_count), tc->key_size).c_str());
+                auto key = tc->pad_string(std::to_string(start_key + added_count), tc->key_size);
+                cc.write_cursor->set_key(cc.write_cursor.get(), key.c_str());
                 cc.write_cursor->search(cc.write_cursor.get());
 
                 /* A return value of true implies the insert was successful. */
-                std::string value =
+                auto value =
                   random_generator::instance().generate_pseudo_random_string(tc->value_size);
-                if (!tc->insert(cc.write_cursor, cc.coll.id,
-                      tc->pad_string(std::to_string(start_key + added_count), tc->key_size),
-                      value)) {
+                if (!tc->insert(cc.write_cursor, cc.coll.id, key, value)) {
                     tc->transaction.rollback();
                     added_count = 0;
                     continue;
