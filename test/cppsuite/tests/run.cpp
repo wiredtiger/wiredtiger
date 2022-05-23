5d7aade3d726390e3b662472475cf38d39ea50ea
Clarisse Cheah <clarisse.cheah@mongodb.com>
2022-05-23 02:00:44 +0000

    Resolve run.cpp merge conflicts

--- test/cppsuite/tests/run.cpp
+++ test/cppsuite/tests/run.cpp
@@ -157,7 +157,7 @@ main(int argc, char *argv[])
 {
     std::string cfg, config_filename, current_cfg, current_test_name, test_name, wt_open_config;
     int64_t error_code = 0;
-    const std::vector<std::string> all_tests = {"bounded_cursor_perf", "burst_inserts",
+    const std::vector<std::string> all_tests = {"bounded_cursor_perf", "burst_inserts", "cursor_bound_01",
       "hs_cleanup", "operations_test", "search_near_01", "search_near_02", "search_near_03",
       "test_template"};
 
