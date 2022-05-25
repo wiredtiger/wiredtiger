009d2c1b9f64ce11fda000c44f3ffe64f11e8cd9
Will Korteland <will.korteland@mongodb.com>
2022-05-25 05:15:52 +0000

    s_all

--- test/cppsuite/test_harness/workload/thread_context.cpp
+++ test/cppsuite/test_harness/workload/thread_context.cpp
@@ -149,9 +149,9 @@ transaction_context::try_rollback(const std::string &config)
 }
 
 /*
- * FIXME: WT-9198 We're concurrently doing a transaction that contains a bunch of operations
- * while moving the stable timestamp. Eat the occasional EINVAL from the transaction's first
- * commit timestamp being earlier than the stable timestamp.
+ * FIXME: WT-9198 We're concurrently doing a transaction that contains a bunch of operations while
+ * moving the stable timestamp. Eat the occasional EINVAL from the transaction's first commit
+ * timestamp being earlier than the stable timestamp.
  */
 int
 transaction_context::set_commit_timestamp(wt_timestamp_t ts)
