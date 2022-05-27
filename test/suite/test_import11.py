b55d35ef5500c0952e5fb77ecf8a3ac0255b9c1f
Monica Ng <monica.ng@mongodb.com>
2022-05-27 01:08:09 +0000

    Minor fixes in Python test

--- test/suite/test_import11.py
+++ test/suite/test_import11.py
@@ -116,11 +116,8 @@ class test_import11(test_import_base):
     def conn_config(self):
         return self.tiered_conn_config() + ',statistics=(all)'
 
-    def get_stat(self, stat, uri):
-        if uri == None:
-            stat_cursor = self.session.open_cursor('statistics:')
-        else:
-            stat_cursor = self.session.open_cursor('statistics:' + uri)
+    def get_stat(self, stat):
+        stat_cursor = self.session.open_cursor('statistics:')
         val = stat_cursor[stat][2]
         stat_cursor.close()
         return val
@@ -206,13 +203,14 @@ class test_import11(test_import_base):
         self.checkpoint_and_flush_tier()
 
         # Check the number of files imported after doing an import operation.
-        files_imported_prev = self.get_stat(stat.conn.tiered_files_imported, None)
+        files_imported_prev = self.get_stat(stat.conn.tiered_files_imported)
+        self.assertTrue(files_imported_prev == 1)
 
         self.session.create(self.uri_b, import_config)
         self.checkpoint_and_flush_tier()
 
         # Check the number of files imported has increased after doing another import operation.
-        files_imported = self.get_stat(stat.conn.tiered_files_imported, None)
+        files_imported = self.get_stat(stat.conn.tiered_files_imported)
         self.assertTrue(files_imported == files_imported_prev + 1)
 
         # Remove WiredTiger.export file.
