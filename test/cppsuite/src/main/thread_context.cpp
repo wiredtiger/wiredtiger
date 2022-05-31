629d6658e421d9a231f0549014ba00a414cdc2d1
Etienne Petrel <etienne.petrel@mongodb.com>
2022-05-31 09:15:57 +0000

    Move the transaction_context class to its own file

--- test/cppsuite/src/main/thread_context.cpp
+++ test/cppsuite/src/main/thread_context.cpp
@@ -31,6 +31,7 @@
 #include "src/common/api_const.h"
 #include "src/common/logger.h"
 #include "src/common/random_generator.h"
+#include "transaction.h"
 
 namespace test_harness {
 
@@ -51,132 +52,6 @@ type_string(thread_type type)
     default:
         testutil_die(EINVAL, "unexpected thread_type: %d", static_cast<int>(type));
     }
-}
-
-/* transaction_context class implementation */
-transaction_context::transaction_context(
-  configuration *config, timestamp_manager *timestamp_manager, WT_SESSION *session)
-    : _timestamp_manager(timestamp_manager), _session(session)
-{
-    /* Use optional here as our populate threads don't define this configuration. */
-    configuration *transaction_config = config->get_optional_subconfig(OPS_PER_TRANSACTION);
-    if (transaction_config != nullptr) {
-        _min_op_count = transaction_config->get_optional_int(MIN, 1);
-        _max_op_count = transaction_config->get_optional_int(MAX, 1);
-        delete transaction_config;
-    }
-}
-
-bool
-transaction_context::active() const
-{
-    return (_in_txn);
-}
-
-void
-transaction_context::add_op()
-{
-    _op_count++;
-}
-
-void
-transaction_context::begin(const std::string &config)
-{
-    testutil_assert(!_in_txn);
-    testutil_check(
-      _session->begin_transaction(_session, config.empty() ? nullptr : config.c_str()));
-    /* This randomizes the number of operations to be executed in one transaction. */
-    _target_op_count =
-      random_generator::instance().generate_integer<int64_t>(_min_op_count, _max_op_count);
-    _op_count = 0;
-    _in_txn = true;
-    _needs_rollback = false;
-}
-
-void
-transaction_context::try_begin(const std::string &config)
-{
-    if (!_in_txn)
-        begin(config);
-}
-
-/*
- * It's possible to receive rollback in commit, when this happens the API will rollback the
- * transaction internally.
- */
-bool
-transaction_context::commit(const std::string &config)
-{
-    WT_DECL_RET;
-    testutil_assert(_in_txn && !_needs_rollback);
-
-    ret = _session->commit_transaction(_session, config.empty() ? nullptr : config.c_str());
-    /*
-     * FIXME-WT-9198 Now we are accepting the error code EINVAL because of possible invalid
-     * timestamps as we know it can happen due to the nature of the framework. The framework may set
-     * the stable/oldest timestamps to a more recent date than the commit timestamp of the
-     * transaction which makes the transaction invalid. We only need to check against the stable
-     * timestamp as, by definition, the oldest timestamp is older than the stable one.
-     */
-    testutil_assert(ret == 0 || ret == EINVAL || ret == WT_ROLLBACK);
-
-    if (ret != 0)
-        logger::log_msg(LOG_WARN,
-          "Failed to commit transaction in commit, received error code: " + std::to_string(ret));
-    _op_count = 0;
-    _in_txn = false;
-    return (ret == 0);
-}
-
-void
-transaction_context::rollback(const std::string &config)
-{
-    testutil_assert(_in_txn);
-    testutil_check(
-      _session->rollback_transaction(_session, config.empty() ? nullptr : config.c_str()));
-    _needs_rollback = false;
-    _op_count = 0;
-    _in_txn = false;
-}
-
-void
-transaction_context::try_rollback(const std::string &config)
-{
-    if (can_rollback())
-        rollback(config);
-}
-
-/*
- * FIXME: WT-9198 We're concurrently doing a transaction that contains a bunch of operations while
- * moving the stable timestamp. Eat the occasional EINVAL from the transaction's first commit
- * timestamp being earlier than the stable timestamp.
- */
-int
-transaction_context::set_commit_timestamp(wt_timestamp_t ts)
-{
-    /* We don't want to set zero timestamps on transactions if we're not using timestamps. */
-    if (!_timestamp_manager->enabled())
-        return 0;
-    const std::string config = COMMIT_TS + "=" + timestamp_manager::decimal_to_hex(ts);
-    return _session->timestamp_transaction(_session, config.c_str());
-}
-
-void
-transaction_context::set_needs_rollback(bool rollback)
-{
-    _needs_rollback = rollback;
-}
-
-bool
-transaction_context::can_commit()
-{
-    return (!_needs_rollback && can_rollback());
-}
-
-bool
-transaction_context::can_rollback()
-{
-    return (_in_txn && _op_count >= _target_op_count);
 }
 
 /* thread_context class implementation */
--- test/cppsuite/src/main/thread_context.h
+++ test/cppsuite/src/main/thread_context.h
@@ -36,66 +36,12 @@
 #include "src/component/timestamp_manager.h"
 #include "src/main/configuration.h"
 #include "src/storage/scoped_types.h"
+#include "transaction.h"
 
 namespace test_harness {
 enum thread_type { CUSTOM, INSERT, READ, REMOVE, UPDATE };
 
 const std::string type_string(thread_type type);
-
-class transaction_context {
-    public:
-    explicit transaction_context(
-      configuration *config, timestamp_manager *timestamp_manager, WT_SESSION *session);
-
-    bool active() const;
-    void add_op();
-    void begin(const std::string &config = "");
-    /* Begin a transaction if we are not currently in one. */
-    void try_begin(const std::string &config = "");
-    /*
-     * Commit a transaction and return true if the commit was successful.
-     */
-    bool commit(const std::string &config = "");
-    /* Rollback a transaction, failure will abort the test. */
-    void rollback(const std::string &config = "");
-    /* Attempt to rollback the transaction given the requirements are met. */
-    void try_rollback(const std::string &config = "");
-    /* Set a commit timestamp. */
-    int set_commit_timestamp(wt_timestamp_t ts);
-    /* Set that the transaction needs to be rolled back. */
-    void set_needs_rollback(bool rollback);
-    /*
-     * Returns true if a transaction can be committed as determined by the op count and the state of
-     * the transaction.
-     */
-    bool can_commit();
-    /*
-     * Returns true if a transaction can be rolled back as determined by the op count and the state
-     * of the transaction.
-     */
-    bool can_rollback();
-
-    private:
-    bool _in_txn = false;
-    bool _needs_rollback = false;
-
-    /*
-     * _min_op_count and _max_op_count are the minimum and maximum number of operations within one
-     * transaction. is the current maximum number of operations that can be executed in the current
-     * transaction.
-     */
-    int64_t _max_op_count = INT64_MAX;
-    int64_t _min_op_count = 0;
-    /*
-     * op_count is the current number of operations that have been executed in the current
-     * transaction.
-     */
-    int64_t _op_count = 0;
-    int64_t _target_op_count = 0;
-
-    timestamp_manager *_timestamp_manager = nullptr;
-    WT_SESSION *_session = nullptr;
-};
 
 /* Container class for a thread and any data types it may need to interact with the database. */
 class thread_context {
