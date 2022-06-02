8ff3d837d996896fa7b17581280895d764a57517
siddhartha.mahajan <siddhartha.mahajan8899@gmail.com>
2022-06-02 01:41:09 +0000

    Remove do while from the macros

--- src/include/api.h
+++ src/include/api.h
@@ -308,16 +308,12 @@
  * Calling certain top level APIs allows for internal repositioning of cursors to facilitate
  * eviction of hot pages. These macros facilitate tracking when that is OK.
  */
-#define CURSOR_REPOSITION_ENTER(c, s)                                          \
-    do {                                                                       \
-        if (FLD_ISSET(S2C(s)->debug_flags, WT_CONN_DEBUG_CURSOR_REPOSITION) && \
-          (s)->api_call_counter == 1)                                          \
-            F_SET((c), WT_CURSTD_EVICT_REPOSITION);
+#define CURSOR_REPOSITION_ENTER(c, s)                                      \
+    if (FLD_ISSET(S2C(s)->debug_flags, WT_CONN_DEBUG_CURSOR_REPOSITION) && \
+      (s)->api_call_counter == 1)                                          \
+    F_SET((c), WT_CURSTD_EVICT_REPOSITION)
 
 #define CURSOR_REPOSITION_END(c, s)                                        \
     if (FLD_ISSET(S2C(s)->debug_flags, WT_CONN_DEBUG_CURSOR_REPOSITION) && \
       (s)->api_call_counter == 1)                                          \
-        F_CLR((c), WT_CURSTD_EVICT_REPOSITION);                            \
-    }                                                                      \
-    while (0)                                                              \
-        ;
+    F_CLR((c), WT_CURSTD_EVICT_REPOSITION)
