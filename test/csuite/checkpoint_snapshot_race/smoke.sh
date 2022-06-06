8e473126bdf7691384d7305970cb9e95356c5472
Alexander Gorrod <alexander.gorrod@mongodb.com>
2022-06-06 07:09:17 +0000

    Update stress test to be more stressy

--- test/csuite/checkpoint_snapshot_race/smoke.sh
+++ test/csuite/checkpoint_snapshot_race/smoke.sh
@@ -43,9 +43,9 @@ run_set_of_tests () {
 }
 
 # Run the program for an increasingly long time period
-for i in `seq 50`
+for i in `seq 100`
 do
-    runtime=`echo "$i*$i+1" | bc`
+    runtime=`echo "$i+1" | bc`
     run_set_of_tests $i 20 $runtime
 done
 
