5d9524077acea124c3f4c2116188d42d976c979b
Ruby Chen <ruby.chen@mongodb.com>
2022-06-07 23:55:58 +0000

    Addressing PR comments.

--- dist/api_data.py
+++ dist/api_data.py
@@ -948,9 +948,6 @@ tiered_storage_configuration_common = [
         time in seconds to retain data on tiered storage on the local tier for faster read
         access''',
         min='0', max='10000'),
-    Config('object_target_size', '0', r'''
-        This option is no longer supported, retained for backward compatibility''',
-        min='0', undoc=True),
 ]
 connection_reconfigure_tiered_storage_configuration = [
     Config('tiered_storage', '', r'''
