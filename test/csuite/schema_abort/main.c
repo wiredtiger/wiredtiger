e617d7cb8231ddb6a40b7152d73e93f1b7887eca
Susan LoVerso <sue@mongodb.com>
2022-06-08 15:23:21 -0400

    Don't run upgrade with tiered storage yet.

--- test/csuite/schema_abort/main.c
+++ test/csuite/schema_abort/main.c
@@ -419,6 +419,8 @@ test_upgrade(THREAD_DATA *td)
     WT_DECL_RET;
     WT_SESSION *session;
 
+    /* FIXME-WT-9423 Remove this return when tiered storage supports upgrade. */
+    if (tiered)
     testutil_check(td->conn->open_session(td->conn, NULL, NULL, &session));
 
     if ((ret = session->upgrade(session, uri, NULL)) != 0)
