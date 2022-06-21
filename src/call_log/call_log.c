8505d6ab5456f2b5f34d95ac67f2098a02b050ec
Sean04 <sean.watt@mongodb.com>
2022-06-21 03:48:54 +0000

    Fixed error message.

--- src/call_log/call_log.c
+++ src/call_log/call_log.c
@@ -25,14 +25,13 @@ __wt_conn_call_log_setup(WT_SESSION_IMPL *session)
     WT_RET(__wt_scr_alloc(session, 0, &file_name));
     WT_ERR(__wt_filename_construct(
       session, "", "wt_call_log", __wt_process_id(), UINT32_MAX, file_name));
-    WT_ERR(__wt_fopen(session, (const char *)file_name->data, WT_FS_OPEN_CREATE, WT_STREAM_APPEND,
-      &conn->call_log_fst));
+    if ((ret = __wt_fopen(session, (const char *)file_name->data, WT_FS_OPEN_CREATE,
+           WT_STREAM_APPEND, &conn->call_log_fst)) != 0)
+        WT_ERR_MSG(session, ret, "Failed to open call log.");
 
-    F_SET(session, WT_CONN_CALL_LOG_ENABLED);
+    F_SET(conn, WT_CONN_CALL_LOG_ENABLED);
 
 err:
-    if (!F_ISSET(session, WT_CONN_CALL_LOG_ENABLED))
-        WT_ERR_MSG(session, WT_ERROR, "Failed to open call log.");
     __wt_scr_free(session, &file_name);
     return (ret);
 }
@@ -48,7 +47,7 @@ __wt_conn_call_log_teardown(WT_SESSION_IMPL *session)
 
     conn = S2C(session);
 
-    if (!F_ISSET(session, WT_CONN_CALL_LOG_ENABLED))
+    if (!F_ISSET(conn, WT_CONN_CALL_LOG_ENABLED))
         return (0);
 
     return (__wt_fclose(session, &conn->call_log_fst));
