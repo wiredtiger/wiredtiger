3deed739fbd6d184d9306a909a6e1f21b525c900
Etienne Petrel <etienne.petrel@mongodb.com>
2022-06-24 03:28:38 +0000

    Avoid updating the tests that depend on the string by leaving the case unmodified

--- src/block/block_read.c
+++ src/block/block_read.c
@@ -214,13 +214,13 @@ __wt_block_read_off(WT_SESSION_IMPL *session, WT_BLOCK *block, WT_ITEM *buf, uin
 
         if (!F_ISSET(session, WT_SESSION_QUIET_CORRUPT_FILE))
             __wt_errx(session,
-              "%s: potential hardware corruption. Read checksum error for %" PRIu32
+              "%s: potential hardware corruption, read checksum error for %" PRIu32
               "B block at offset %" PRIuMAX
               ": calculated block checksum doesn't match expected checksum",
               block->name, size, (uintmax_t)offset);
     } else if (!F_ISSET(session, WT_SESSION_QUIET_CORRUPT_FILE))
         __wt_errx(session,
-          "%s: potential hardware corruption. Read checksum error for %" PRIu32
+          "%s: potential hardware corruption, read checksum error for %" PRIu32
           "B block at offset %" PRIuMAX ": block header checksum of %#" PRIx32
           " doesn't match expected checksum of %#" PRIx32,
           block->name, size, (uintmax_t)offset, swap.checksum, checksum);
