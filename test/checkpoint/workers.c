e69d133fefc0b2cb394f5741557afe714d9f42fc
Susan LoVerso <sue@mongodb.com>
2022-06-27 15:21:02 -0400

    Add periodic worker progress output.

--- test/checkpoint/workers.c
+++ test/checkpoint/workers.c
@@ -392,6 +392,8 @@ real_worker(void)
         }
 
     for (i = 0; i < g.nops && g.running; ++i, __wt_yield()) {
+        if (i > 0 && i % 5000 == 0)
+            printf("Worker %u of %u ops\n", i, g.nops);
         if (start_txn) {
             if ((ret = session->begin_transaction(session, begin_cfg)) != 0) {
                 (void)log_print_err("real_worker:begin_transaction", ret, 1);
