30cad1bc0c73ce98b48db10e367b76b1df89fcd7
Siddhartha Mahajan <55784503+Siddhartha8899@users.noreply.github.com>
2022-07-07 13:26:13 +1000

    WT-9214 Process the call log for wiredtiger_open (#8084)
    Co-authored-by: Sean04 <sean.watt@mongodb.com>

---
+++ test/simulator/timestamp/src/connection_simulator.cpp
@@ -0,0 +1,67 @@
+/*-
+ * Public Domain 2014-present MongoDB, Inc.
+ * Public Domain 2008-2014 WiredTiger, Inc.
+ *
+ * This is free and unencumbered software released into the public domain.
+ *
+ * Anyone is free to copy, modify, publish, use, compile, sell, or
+ * distribute this software, either in source code form or as a compiled
+ * binary, for any purpose, commercial or non-commercial, and by any
+ * means.
+ *
+ * In jurisdictions that recognize copyright laws, the author or authors
+ * of this software dedicate any and all copyright interest in the
+ * software to the public domain. We make this dedication for the benefit
+ * of the public at large and to the detriment of our heirs and
+ * successors. We intend this dedication to be an overt act of
+ * relinquishment in perpetuity of all present and future rights to this
+ * software under copyright law.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#include "connection_simulator.h"
+
+/* Get an instance of connection_simulator class. */
+connection_simulator &
+connection_simulator::get_connection()
+{
+    static connection_simulator _connection_instance;
+    return (_connection_instance);
+}
+
+session_simulator *
+connection_simulator::open_session()
+{
+    session_simulator *session = new session_simulator();
+
+    _session_list.push_back(session);
+
+    return (session);
+}
+
+int
+connection_simulator::query_timestamp() const
+{
+    return (0);
+}
+
+int
+connection_simulator::set_timestamp() const
+{
+    return (0);
+}
+
+connection_simulator::connection_simulator() {}
+
+connection_simulator::~connection_simulator()
+{
+    for (auto session : _session_list)
+        delete session;
+}
--- test/simulator/timestamp/src/session_simulator.cpp
+++ test/simulator/timestamp/src/session_simulator.cpp
@@ -26,16 +26,6 @@
  * OTHER DEALINGS IN THE SOFTWARE.
  */
 
-#include <iostream>
-#include "json.hpp"
+#include "session_simulator.h"
 
-using json = nlohmann::json;
-
-int
-main()
-{
-    json j = {{"pi", 3.141}, {"happy", true}};
-    std::cout << j.dump(4) << std::endl;
-
-    return (0);
-}
+session_simulator::session_simulator() {}
