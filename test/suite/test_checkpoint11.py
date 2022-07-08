4cd390dd84a0ccf72ab64b33e113c7cf9fa10672
Chenhao Qu <chenhao.qu@mongodb.com>
2022-07-08 10:48:07 +1000

    revert test change

--- test/suite/test_checkpoint11.py
+++ test/suite/test_checkpoint11.py
@@ -43,10 +43,10 @@ class test_checkpoint(wttest.WiredTigerTestCase):
     session_config = 'isolation=snapshot'
 
     format_values = [
-        #('column-fix', dict(key_format='r', value_format='8t',
-        #    extraconfig=',allocation_size=512,leaf_page_max=512')),
+        ('column-fix', dict(key_format='r', value_format='8t',
+            extraconfig=',allocation_size=512,leaf_page_max=512')),
         ('column', dict(key_format='r', value_format='S', extraconfig='')),
-        #('string_row', dict(key_format='S', value_format='S', extraconfig='')),
+        ('string_row', dict(key_format='S', value_format='S', extraconfig='')),
     ]
     overlap_values = [
         ('overlap', dict(do_overlap=True)),
