ccbd57479a7a880efebae99fd9b4d7e2f07b0a16
Chenhao Qu <chenhao.qu@mongodb.com>
2022-07-08 10:50:26 +1000

    Fix setting ins_head

--- src/btree/col_srch.c
+++ src/btree/col_srch.c
@@ -255,10 +255,12 @@ leaf_only:
             cbt->recno = __col_var_last_recno(current);
             cbt->slot = page->entries == 0 ? 0 : page->entries - 1;
             if (cbt->recno != WT_RECNO_OOB) {
-                cbt->ins_head = WT_COL_UPDATE_SLOT(page, cbt->slot);
-                ins = __col_insert_search(cbt->ins_head, cbt->ins_stack, cbt->next_stack, cbt->recno);
-                if (ins != NULL && cbt->recno == WT_INSERT_RECNO(ins))
+                ins_head = WT_COL_UPDATE_SLOT(page, cbt->slot);
+                ins = __col_insert_search(ins_head, cbt->ins_stack, cbt->next_stack, cbt->recno);
+                if (ins != NULL && cbt->recno == WT_INSERT_RECNO(ins)) {
                     cbt->ins = ins;
+                    cbt->ins_head = ins_head;
+                }
             }
             goto past_end;
         } else {
