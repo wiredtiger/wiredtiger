4d1d22606bb1a10e36c5c87b5c613440a866f0b5
Alexey Anisimov <alexey.anisimov@mongodb.com>
2022-07-11 16:44:53 +1000

    Use FLD_ISSET instead of manually checking flags.

--- src/session/session_dhandle.c
+++ src/session/session_dhandle.c
@@ -776,8 +776,7 @@ void
 __wt_session_dhandle_readunlock(WT_SESSION_IMPL *session)
 {
     WT_ASSERT(session, session->dhandle != NULL);
-    WT_ASSERT(
-      session, (session->dhandle->lock_flags | WT_DHANDLE_LOCK_READ) == WT_DHANDLE_LOCK_READ);
+    WT_ASSERT(session, FLD_ISSET(session->dhandle->lock_flags, WT_DHANDLE_LOCK_READ));
     FLD_CLR(session->dhandle->lock_flags, WT_DHANDLE_LOCK_READ);
     __wt_readunlock(session, &session->dhandle->rwlock);
 }
