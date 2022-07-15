acaae7fe56c204eb2f4b3ac74e6d7ae3728c8b20
Will Korteland <will.korteland@mongodb.com>
2022-07-15 00:49:50 +0000

    die on failure when last exit was non-zero

--- test/evergreen/build_windows.ps1
+++ test/evergreen/build_windows.ps1
@@ -4,6 +4,19 @@ param (
     [string]$vcvars_bat = "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\VC\Auxiliary\Build\vcvars64.bat"
 )
 
+function Die-On-Failure {
+    param (
+        $Result
+    )
+
+    if ($Result -ne 0) {
+        throw("Task failed: " + $Result)
+    }
+}
+
+# This script should fail when a cmdlet fails.
+$ErrorActionPreference = "Stop"
+
 # Source the vcvars to ensure we have access to the Visual Studio toolchain
 cmd /c "`"$vcvars_bat`"&set" |
 foreach {
@@ -29,9 +42,11 @@ cd cmake_build
 if( $configure -eq $true) {
     # Note that ${args} are all the command line options that are not automatically parsed by the param function.
     C:\cmake\bin\cmake --no-warn-unused-cli -DSWIG_DIR='C:\swigwin-3.0.2' -DSWIG_EXECUTABLE='C:\swigwin-3.0.2\swig.exe' -DCMAKE_BUILD_TYPE='None' -DENABLE_PYTHON=1 -DENABLE_STRICT=1 -DCMAKE_TOOLCHAIN_FILE='..\cmake\toolchains\cl.cmake' ${args} -G "Ninja" ..\.
+    Die-On-Failure($LastExitCode)
 }
 
 # Execute Ninja build.
 if( $build -eq $true) {
     ninja
+    Die-On-Failure($LastExitCode)
 }
