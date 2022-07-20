cd6a12f3d754a5f038fb18a35bfb2da21be25ec8
Mick Graham <mick.graham@mongodb.com>
2022-07-20 07:19:24 +0000

    Updating final code review comments

--- ext/storage_sources/s3_store/s3_log_system.h
+++ ext/storage_sources/s3_store/s3_log_system.h
@@ -82,7 +82,7 @@ class S3LogSystem : public Aws::Utils::Logging::LogSystemInterface {
 
     // Inherited from AWS LogSystemInterface and is not used.
     void
-    Flush() override
+    Flush() override final
     {
     }
 
