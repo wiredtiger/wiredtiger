0ff5679a41e6d8682de2e936475fef84aa2b5780
Mick Graham <mick.graham@mongodb.com>
2022-07-21 05:15:23 +0000

    Fixing spacing issue on comment

--- test/evergreen/compatibility_test_for_releases.sh
+++ test/evergreen/compatibility_test_for_releases.sh
@@ -83,7 +83,7 @@ build_branch()
         . ./test/evergreen/find_cmake.sh
         config=""
         config+="-DENABLE_SNAPPY=1 "
-	# Need to disable configs since WT-9455
+        # Need to disable configs since WT-9455
 	config+="-DENABLE_LZ4=0 -DENABLE_ZLIB=0 -DENABLE_ZSTD=0 "
         config+="-DWT_STANDALONE_BUILD=0 "
         (mkdir -p build && cd build &&
