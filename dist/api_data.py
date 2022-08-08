454d410bcdba43a0a852edaa8316bd83c8259cd7
Ruby Chen <ruby.chen@mongodb.com>
2022-08-08 01:05:43 +0000

    Adding new config flag for checkpoint race.

--- dist/api_data.py
+++ dist/api_data.py
@@ -781,8 +781,8 @@ connection_runtime_config = [
         type='list', undoc=True,
         choices=[
         'aggressive_sweep', 'backup_rename', 'checkpoint_evict_page',
-        'checkpoint_reserved_txnid_delay', 'checkpoint_slow', 'checkpoint_stop', 'compact_slow',
-        'evict_reposition', 'failpoint_eviction_fail_after_reconciliation', 
+        'checkpoint_reserved_txnid_delay', 'checkpoint_slow', 'checkpoint_stop', 'compact_slow', 
+        'checkpoint_race', 'evict_reposition', 'failpoint_eviction_fail_after_reconciliation', 
         'failpoint_history_store_delete_key_from_ts', 'history_store_checkpoint_delay',
         'history_store_search', 'history_store_sweep_race', 'prepare_checkpoint_delay', 'split_1',
         'split_2', 'split_3', 'split_4', 'split_5', 'split_6', 'split_7', 'tiered_flush_finish']),
--- src/include/btree_inline.h
+++ src/include/btree_inline.h
@@ -727,6 +727,7 @@ __wt_page_only_modify_set(WT_SESSION_IMPL *session, WT_PAGE *page)
 static inline void
 __wt_tree_modify_set(WT_SESSION_IMPL *session)
 {
+    struct timespec tsp;
     /*
      * Test before setting the dirty flag, it's a hot cache line.
      *
@@ -741,6 +742,13 @@ __wt_tree_modify_set(WT_SESSION_IMPL *session)
 
         S2BT(session)->modified = true;
         WT_FULL_BARRIER();
+        /*
+         *  Simululate a delay after marking a tree dirty - there is a race where checkpoint
+         *  can clear the dirty state before a page is marked clean.
+         */
+        tsp.tv_sec = 0;
+        tsp.tv_nsec = 1000000;
+        __checkpoint_timing_stress(session, WT_TIMING_STRESS_CHECKPOINT_RACE, &tsp);
     }
 
     /*
