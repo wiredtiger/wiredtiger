bff6e35ff103f9bf851603ca878747a78322787a
Ruby Chen <ruby.chen@mongodb.com>
2022-08-26 01:19:22 +0000

    Remove column-fix option for python test.

--- test/suite/test_rollback_to_stable37.py
+++ test/suite/test_rollback_to_stable37.py
@@ -40,7 +40,6 @@ class test_rollback_to_stable37(test_rollback_to_stable_base):
 
     format_values = [
         ('column', dict(key_format='r', value_format='S')),
-        ('column_fix', dict(key_format='r', value_format='8t')),
         ('row_integer', dict(key_format='i', value_format='S')),
     ]
 
