9c62b28de08cbc259e55cec296ef9b0a7063fb37
Chenhao Qu <chenhao.qu@mongodb.com>
2022-08-29 15:55:03 +1000

    Fix compile

--- src/evict/evict_page.c
+++ src/evict/evict_page.c
@@ -185,9 +185,8 @@ __wt_evict(WT_SESSION_IMPL *session, WT_REF *ref, uint8_t previous_state, uint32
      * Fail 0.1% of the time after we have done reconciliation. We should always evict the page of a
      * dead tree.
      */
-    if (!closing &&
-      !tree_dead __wt_failpoint(
-        session, WT_TIMING_STRESS_FAILPOINT_EVICTION_FAIL_AFTER_RECONCILIATION, 10))
+    if (!closing && !tree_dead &&
+      __wt_failpoint(session, WT_TIMING_STRESS_FAILPOINT_EVICTION_FAIL_AFTER_RECONCILIATION, 10))
         return (EBUSY);
 
     /* Check we are not evicting an accessible internal page with an active split generation. */
