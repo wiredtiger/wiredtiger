be9afa9607c72bc5ff817eca71f97aa7c516aaaa
Alexander Gorrod <alexander.gorrod@mongodb.com>
2022-09-28 06:14:08 +0000

    Add more comments and steps to the test program to get it closer to reproducing. It now exits at line 106, the following is the state of things:
    $ ./wt -h WT_TEST/test_rollback_to_stable50.0/ -r verify -d dump_pages file:aaa.wt
    =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
    file:aaa.wt: checkpoint WiredTigerCheckpoint.2
    Root: [0: 24576-28672, 4096, 3377926738] row-store internal
    0x1d7b210: row-store internal
        0x1d7b210, memory, internal
        disk 0x1df50e0, dsk_mem_size 52, write_gen: 7, entries 1, clean, disk-alloc, memory-size 324
        K {}
        0x1d7d750, disk, leaf
    0x1d7d750: row-store leaf
        0x1d7d750, memory, leaf
        disk 0x1df60f0, dsk_mem_size 381, write_gen: 6, entries 2, clean, keys-built, disk-alloc, memory-size 493
        K {AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA}
        value: len 201
        V {A2A2A2A2A2A2A2A2A2A2A2A2A2A2A2A2A2A2A2A2A2A2A2A2A2A2A2A2A2A2A2A2A2A2A2A2A2A2A2A2A2A2A2A2A2A2A2A2A2A2A2A2A2A2A2A2A2A2A2A2A2A2A2A2A2A2A2A2A2A2A2A2A2A2A2A2A2A2A2A2A2A2A2A2A2A2A2A2A2A2A2A2A2A2A2A2A2A2A2A2}
        K {BBBBBBBBBB}
        value/short: len 21
        V {B2B2B2B2B2B2B2B2B2B2}
    
    That is, the data file contains record AAA with no timestamp information.
    
    The history store still contains the older version of A:
    $ ./wt -h WT_TEST/test_rollback_to_stable50.0/ -r verify -d dump_pages file:WiredTigerHS.wt
    =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
    file:WiredTigerHS.wt: checkpoint WiredTigerCheckpoint.3
    Root: [0: 8192-12288, 4096, 3211426801] row-store internal
    0x7f5e30000e00: row-store internal
        0x7f5e30000e00, memory, internal
        disk 0x25a2440, dsk_mem_size 56, write_gen: 7, entries 1, clean, disk-alloc, memory-size 328
        K {\00}
        0x2543c40, disk, leaf, newest durable: (0, 0)/(0, 20) oldest start: (0, 0)/0 newest stop (0, 20)/0, [0: 4096-8192, 4096, 389301789]
    0x2543c40: row-store leaf
        0x2543c40, memory, leaf, newest durable: (0, 0)/(0, 20) oldest start: (0, 0)/0 newest stop (0, 20)/0, [0: 4096-8192, 4096, 389301789]
        disk 0x25a13b0, dsk_mem_size 359, write_gen: 2, entries 1, clean, keys-built, disk-alloc, memory-size 463
        K {2,AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\00,15,0}
        value: len 204, start: (0, 0)/(0, 0)/0 stop: (0, 20)/(0, 20)/0
        V {20,15,3,A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1\00}

--- test/suite/test_rollback_to_stable50.py
+++ test/suite/test_rollback_to_stable50.py
@@ -36,7 +36,7 @@ class test_rollback_to_stable50(test_rollback_to_stable_base):
 
     def conn_config(self):
         # TODO - Is it required?
-        config = 'cache_size=1MB,statistics=(all),log=(enabled=true)'
+        config = 'cache_size=1MB,statistics=(all),log=(enabled=true),statistics_log=(wait=1,on_close=1)'
         return config
 
     def insert_kv(self, uri, key, value, ts):
@@ -65,8 +65,9 @@ class test_rollback_to_stable50(test_rollback_to_stable_base):
         # Setup global oldest and stable
         stable_ts = 10
         oldest_ts = 10
-        self.conn.set_timestamp('oldest_timestamp=' + self.timestamp_str(stable_ts) +
-                                ',stable_timestamp=' + self.timestamp_str(oldest_ts))
+        self.conn.set_timestamp('oldest_timestamp=' + self.timestamp_str(oldest_ts) +
+                                ',stable_timestamp=' + self.timestamp_str(stable_ts))
+
         # Insert value A1 @ 15.
         key = 'A' * 100
         value = 'A1' * 100
@@ -78,29 +79,40 @@ class test_rollback_to_stable50(test_rollback_to_stable_base):
         commit_ts = 20
         self.insert_kv(uri, key, value, commit_ts)
 
-        # Persist to disk, there should be A2 on the DS and A1 in the HS.
-        #self.session.checkpoint()
+        # Create a checkpoint that will force A1 to be written to the history store
+        stable_ts = 22
+        oldest_ts = 17
+        self.conn.set_timestamp('oldest_timestamp=' + self.timestamp_str(oldest_ts) +
+                                ',stable_timestamp=' + self.timestamp_str(stable_ts))
 
-        # TODO - Is it necessary?
-        # Perform eviction, there should be A2 on the DS and A1 in the HS.
+        # Perform eviction, there should be A2 on the DS and A1 in the HS (in cache).
         self.evict(uri, key)
+        # Get the history store page reconciled, oldest needs to be held back here, or the
+        # reconciliation would remove the content from the history store.
+        self.session.checkpoint()
+
+        # Eviction isn't enough to force a rewrite here, since the page is clean. Add another
+        # record on the page so a reconciliation happens
+        self.insert_kv(uri, 'B' * 10, 'B2' * 10, 23)
 
         # Move the oldest and stable timestamps @ 25 to make A1 and A2 globally visible.
         stable_ts = 25
         oldest_ts = 25
-        self.conn.set_timestamp('oldest_timestamp=' + self.timestamp_str(stable_ts) +
-                                ',stable_timestamp=' + self.timestamp_str(oldest_ts))
-
-        # Perform eviction, this should reset the time window now the update is globally visible.
+        self.conn.set_timestamp('oldest_timestamp=' + self.timestamp_str(oldest_ts) +
+                                ',stable_timestamp=' + self.timestamp_str(stable_ts))
+        # Evict the page with the now globally visible key
         self.evict(uri, key)
+        self.session.checkpoint()
+        exit(1)
 
         # Update value A2 -> A3 @ 30.
         value = 'A3' * 100
         commit_ts = 30
         self.insert_kv(uri, key, value, commit_ts)
 
-        # Persist A3 to disk - this should move A2 to the histor store, with a 0 timestamp
+        # Persist A3 to disk - this should move A2 to the history store, with a 0 timestamp
         self.evict(uri, key)
+        exit(1)
 
         # Perform a checkpoint so that rollback to stable will try to replace A3 with A2 on
         # recovery
