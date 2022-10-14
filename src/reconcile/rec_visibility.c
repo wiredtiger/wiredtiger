d4d62d5cbe61fe1bb9ad0a80c8d43e433ff4685c
Chenhao Qu <chenhao.qu@mongodb.com>
2022-10-14 17:52:44 +1100

    Fix crash

--- src/reconcile/rec_visibility.c
+++ src/reconcile/rec_visibility.c
@@ -177,10 +177,11 @@ __rec_append_orig_value(
         WT_ERR(__wt_scr_alloc(session, 0, &tmp));
         WT_ERR(__wt_page_cell_data_ref_kv(session, page, unpack, tmp));
         /*
-         * We should never see an overflow removed value because we haven't freed the overflown
+         * We should never see an overflow removed value because we haven't freed the overflow
          * blocks.
          */
-        WT_ASSERT(session, __wt_cell_type_raw(unpack->cell) != WT_CELL_VALUE_OVFL_RM);
+        WT_ASSERT(session,
+          unpack->cell == NULL || __wt_cell_type_raw(unpack->cell) != WT_CELL_VALUE_OVFL_RM);
         WT_ERR(__wt_upd_alloc(session, tmp, WT_UPDATE_STANDARD, &append, &size));
         total_size += size;
         append->txnid = unpack->tw.start_txn;
