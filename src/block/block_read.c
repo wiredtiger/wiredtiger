5c7b6a5e5afa63f128c236c70a1c497f552c5e54
Will Korteland <will.korteland@mongodb.com>
2022-10-26 00:47:07 +0000

    review verbose log levels for block manager

--- src/block/block_read.c
+++ src/block/block_read.c
@@ -156,7 +156,7 @@ __wt_block_read_off(WT_SESSION_IMPL *session, WT_BLOCK *block, WT_ITEM *buf, uin
     WT_BLOCK_HEADER *blk, swap;
     size_t bufsize;
 
-    __wt_verbose(session, WT_VERB_READ, "off %" PRIuMAX ", size %" PRIu32 ", checksum %#" PRIx32,
+    __wt_verbose_debug2(session, WT_VERB_READ, "off %" PRIuMAX ", size %" PRIu32 ", checksum %#" PRIx32,
       (uintmax_t)offset, size, checksum);
 
     WT_STAT_CONN_INCR(session, block_read);
