c9d23cd2db8785dd993eb82ef9ee64d6b88d4a13
Ravi Giri <ravi.giri@mongodb.com>
2022-10-27 11:34:33 +0000

    Fix buffer overflow

--- test/utility/misc.c
+++ test/utility/misc.c
@@ -366,17 +366,17 @@ void
 testutil_wiredtiger_open(TEST_OPTS *opts, const char *config, WT_EVENT_HANDLER *event_handler,
   WT_CONNECTION **connectionp, bool recovery)
 {
-    char buf[512];
+    char buf[1024];
 
     strncpy(buf, config, sizeof(buf));
     if (recovery)
-        strncat(buf, TESTUTIL_ENV_CONFIG_REC, sizeof(buf) - strlen(buf));
+        strncat(buf, TESTUTIL_ENV_CONFIG_REC, sizeof(buf) - strlen(buf) - 1);
     else if (opts->compat)
-        strncat(buf, TESTUTIL_ENV_CONFIG_COMPAT, sizeof(buf) - strlen(buf));
+        strncat(buf, TESTUTIL_ENV_CONFIG_COMPAT, sizeof(buf) - strlen(buf) - 1);
 
     if (opts->tiered_storage) {
-        strncat(buf, TESTUTIL_ENV_CONFIG_TIERED_EXT, sizeof(buf) - strlen(buf));
-        strncat(buf, TESTUTIL_ENV_CONFIG_TIERED, sizeof(buf) - strlen(buf));
+        strncat(buf, TESTUTIL_ENV_CONFIG_TIERED_EXT, sizeof(buf) - strlen(buf) - 1);
+        strncat(buf, TESTUTIL_ENV_CONFIG_TIERED, sizeof(buf) - strlen(buf) - 1);
     }
     testutil_check(wiredtiger_open(NULL, event_handler, buf, connectionp));
 }
