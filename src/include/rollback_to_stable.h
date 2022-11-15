fce7255d8c1588d450fd2bc9151eb678259eb5a8
Alexander Gorrod <alexander.gorrod@mongodb.com>
2022-11-15 04:14:00 +0000

    Add missing header file

---
+++ src/include/rollback_to_stable.h
@@ -0,0 +1,17 @@
+/*-
+ * Copyright (c) 2014-present MongoDB, Inc.
+ * Copyright (c) 2008-2014 WiredTiger, Inc.
+ *	All rights reserved.
+ *
+ * See the file LICENSE for redistribution information.
+ */
+
+#define WT_CHECK_RECOVERY_FLAG_TXNID(session, txnid)                                           \
+    (F_ISSET(S2C(session), WT_CONN_RECOVERING) && S2C(session)->recovery_ckpt_snap_min != 0 && \
+      (txnid) >= S2C(session)->recovery_ckpt_snap_min)
+
+/* Enable rollback to stable verbose messaging during recovery. */
+#define WT_VERB_RECOVERY_RTS(session)                                                              \
+    (F_ISSET(S2C(session), WT_CONN_RECOVERING) ?                                                   \
+        WT_DECL_VERBOSE_MULTI_CATEGORY(((WT_VERBOSE_CATEGORY[]){WT_VERB_RECOVERY, WT_VERB_RTS})) : \
+        WT_DECL_VERBOSE_MULTI_CATEGORY(((WT_VERBOSE_CATEGORY[]){WT_VERB_RTS})))
