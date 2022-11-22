c0caf5f8a390b96fbc2e1b0f4819bcbf81d95862
Monica Ng <monica.ng@mongodb.com>
2022-11-22 01:17:59 +0000

    Fix initialisation logic from addr path

--- src/include/cell_inline.h
+++ src/include/cell_inline.h
@@ -935,9 +935,11 @@ copy_cell_restart:
     case WT_CELL_ADDR_LEAF:
     case WT_CELL_ADDR_LEAF_NO:
         /* Unpack the row and/or byte counts if the chunk of data includes it. */
-        if (ps != NULL && F_ISSET(dsk, WT_PAGE_STAT_ROWBYTE) && __wt_process.page_stats_2022) {
-            WT_RET(__wt_vunpack_int(&p, end == NULL ? 0 : WT_PTRDIFF(end, p), &ps->byte_count));
-            WT_RET(__wt_vunpack_int(&p, end == NULL ? 0 : WT_PTRDIFF(end, p), &ps->row_count));
+        if (ps != NULL && __wt_process.page_stats_2022) {
+            if (F_ISSET(dsk, WT_PAGE_STAT_BYTE_COUNT))
+                WT_RET(__wt_vunpack_int(&p, end == NULL ? 0 : WT_PTRDIFF(end, p), &ps->byte_count));
+            if (F_ISSET(dsk, WT_PAGE_STAT_ROW_COUNT))
+                WT_RET(__wt_vunpack_int(&p, end == NULL ? 0 : WT_PTRDIFF(end, p), &ps->row_count));
         }
         /* FALLTHROUGH */
     case WT_CELL_KEY:
