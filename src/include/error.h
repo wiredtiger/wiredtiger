610b7943b3de63d56ad476e9ea7371a1590174e0
Andrew Morton <andrew.morton@mongodb.com>
2022-11-24 05:35:41 +0000

    WT-10043 Update assertion code to consider diagnostic_asserts flag

--- src/include/error.h
+++ src/include/error.h
@@ -123,82 +123,124 @@
     __wt_panic(session, EINVAL, "%s: 0x%" PRIxMAX, \
       "encountered an illegal file format or internal value", (uintmax_t)(v))
 
-/*
- * WT_ERR_ASSERT, WT_RET_ASSERT, WT_ASSERT
- *	Assert an expression, aborting in diagnostic mode and otherwise exiting
- * the function with an error. WT_ASSERT is deprecated, and should be used only
- * where required for performance.
- */
-#ifdef HAVE_DIAGNOSTIC
-#define WT_ASSERT(session, exp)             \
-    do {                                    \
-        if (!(exp)) {                       \
-            __wt_errx(session, "%s", #exp); \
-            __wt_abort(session);            \
-        }                                   \
-    } while (0)
-#define WT_ERR_ASSERT(session, exp, v, ...)    \
-    do {                                       \
-        if (!(exp)) {                          \
-            __wt_err(session, v, __VA_ARGS__); \
-            __wt_abort(session);               \
-        }                                      \
-    } while (0)
-#define WT_RET_ASSERT(session, exp, v, ...)    \
-    do {                                       \
-        if (!(exp)) {                          \
-            __wt_err(session, v, __VA_ARGS__); \
-            __wt_abort(session);               \
-        }                                      \
-    } while (0)
-#define WT_RET_PANIC_ASSERT(session, exp, v, ...) \
-    do {                                          \
-        if (!(exp)) {                             \
-            __wt_err(session, v, __VA_ARGS__);    \
-            __wt_abort(session);                  \
-        }                                         \
-    } while (0)
-#else
-#define WT_ASSERT(session, exp) WT_UNUSED(session)
-#define WT_ERR_ASSERT(session, exp, v, ...)      \
-    do {                                         \
-        if (!(exp))                              \
-            WT_ERR_MSG(session, v, __VA_ARGS__); \
-    } while (0)
-#define WT_RET_ASSERT(session, exp, v, ...)      \
-    do {                                         \
-        if (!(exp))                              \
-            WT_RET_MSG(session, v, __VA_ARGS__); \
-    } while (0)
-#define WT_RET_PANIC_ASSERT(session, exp, v, ...)  \
-    do {                                           \
-        if (!(exp))                                \
-            WT_RET_PANIC(session, v, __VA_ARGS__); \
-    } while (0)
-#endif
+/* Branch prediction hints. likely. */
+#define likely(x) __builtin_expect(!!(x), 1)
+#define unlikely(x) __builtin_expect(!!(x), 0)
 
 /*
- * WT_ASSERT_ALWAYS
- *  Assert an expression, abort in both diagnostic and release mode if it fails.
+ * TRIGGER_ABORT --
+ *  Abort the program.
  *
  * When unit testing assertions we don't want to call __wt_abort, but we do want to track that we
  * should have done so.
  */
 #ifdef HAVE_UNITTEST_ASSERTS
-#define WT_ASSERT_ALWAYS(session, exp, failure_reason)                                          \
-    do {                                                                                        \
-        if (!(exp)) {                                                                           \
-            WT_IGNORE_RET(__wt_snprintf((session)->unittest_assert_msg,                         \
-              WT_SESSION_UNITTEST_BUF_LEN, "Assertion '%s' failed: %s", #exp, failure_reason)); \
-            (session)->unittest_assert_hit = true;                                              \
-        }                                                                                       \
+#define TRIGGER_ABORT(session, exp, ...)                                                       \
+    do {                                                                                       \
+        size_t _offset;                                                                        \
+        if ((session) == NULL) {                                                               \
+            __wt_errx(                                                                         \
+              session, "A non-NULL session must be provided when unit testing assertions");    \
+            __wt_abort(session);                                                               \
+        }                                                                                      \
+                                                                                               \
+        /*                                                                                     \
+         * Normally these are two errx messages, but we need to stitch them both together into \
+         * unittest_assert_msg. To do so insert the second message immediately after the first \
+         */                                                                                    \
+        _offset = (size_t)__wt_snprintf((session)->unittest_assert_msg,                        \
+          WT_SESSION_UNITTEST_BUF_LEN, "Assertion '%s' failed: ", #exp);                       \
+        WT_IGNORE_RET(__wt_snprintf((session)->unittest_assert_msg + _offset,                  \
+          WT_SESSION_UNITTEST_BUF_LEN - _offset, __VA_ARGS__));                                \
+                                                                                               \
+        (session)->unittest_assert_hit = true;                                                 \
     } while (0)
 #else
-#define WT_ASSERT_ALWAYS(session, exp, failure_reason)                             \
-    do {                                                                           \
-        if (!(exp)) {                                                              \
-            __wt_errx(session, "Assertion '%s' failed: %s", #exp, failure_reason); \
-            __wt_abort(session);                                                   \
-        }                                                                          \
+#define TRIGGER_ABORT(session, exp, ...)                     \
+    do {                                                     \
+        __wt_errx(session, "Assertion '%s' failed: ", #exp); \
+        __wt_errx(session, __VA_ARGS__);                     \
+        __wt_abort(session);                                 \
     } while (0)
 #endif
+
+/*
+ * DIAGNOSTIC_ASSERTS_ENABLED --
+ *  Fetch whether diagnostic asserts are enabled for this session. In most
+ * cases the session inherits this value from the connection.
+ */
+#define DIAGNOSTIC_ASSERTS_ENABLED(session)                                        \
+    (session != NULL) &&                                                           \
+      (likely((session)->diagnostic_asserts_level == DIAG_ASSERTS_CONN) ?          \
+          FLD_ISSET(S2C(session)->debug_flags, WT_CONN_DEBUG_DIAGNOSTIC_ASSERTS) : \
+          (session)->diagnostic_asserts_level == DIAG_ASSERTS_ON)
+
+/*
+ * WT_ASSERT --
+ *  Assert an expression provided that diagnostic asserts are enabled.
+ *
+ * FIXME-WT-10045 - WT_ASSERT should now always take an error message as an argument.
+ */
+#define WT_ASSERT(session, exp)                            \
+    do {                                                   \
+        if (unlikely(DIAGNOSTIC_ASSERTS_ENABLED(session))) \
+            if (unlikely(!(exp))) {                        \
+                TRIGGER_ABORT(session, exp, "");           \
+            }                                              \
+    } while (0)
+
+/*
+ * WT_ASSERT_ALWAYS --
+ *  Assert an expression regardless of whether diagnostic asserts are enabled.
+ */
+#define WT_ASSERT_ALWAYS(session, exp, ...)           \
+    do {                                              \
+        if (unlikely(!(exp))) {                       \
+            TRIGGER_ABORT(session, exp, __VA_ARGS__); \
+        }                                             \
+    } while (0)
+
+/*
+ * WT_ERR_ASSERT --
+ *  Assert an expression. If diagnostic asserts are enabled then abort the program,
+ * otherwise return an error.
+ */
+#define WT_ERR_ASSERT(session, exp, v, ...)               \
+    do {                                                  \
+        if (unlikely(!(exp))) {                           \
+            if (DIAGNOSTIC_ASSERTS_ENABLED(session)) {    \
+                TRIGGER_ABORT(session, exp, __VA_ARGS__); \
+            } else                                        \
+                WT_ERR_MSG(session, v, __VA_ARGS__);      \
+        }                                                 \
+    } while (0)
+
+/*
+ * WT_RET_ASSERT --
+ *  Assert an expression. If diagnostic asserts are enabled then abort the program,
+ * otherwise print a message and early return from the function.
+ */
+#define WT_RET_ASSERT(session, exp, v, ...)               \
+    do {                                                  \
+        if (unlikely(!(exp))) {                           \
+            if (DIAGNOSTIC_ASSERTS_ENABLED(session)) {    \
+                TRIGGER_ABORT(session, exp, __VA_ARGS__); \
+            } else                                        \
+                WT_RET_MSG(session, v, __VA_ARGS__);      \
+        }                                                 \
+    } while (0)
+
+/*
+ * WT_RET_PANIC_ASSERT --
+ *  Assert an expression. If diagnostic asserts are enabled then abort the
+ * program, otherwise return WT_PANIC from the function.
+ */
+#define WT_RET_PANIC_ASSERT(session, exp, v, ...)         \
+    do {                                                  \
+        if (unlikely(!(exp))) {                           \
+            if (DIAGNOSTIC_ASSERTS_ENABLED(session)) {    \
+                TRIGGER_ABORT(session, exp, __VA_ARGS__); \
+            } else                                        \
+                WT_RET_PANIC(session, v, __VA_ARGS__);    \
+        }                                                 \
+    } while (0)
