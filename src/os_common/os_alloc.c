8146b2d2a5c79e672bd36e52024304dfc95e3939
Susan LoVerso <sue@mongodb.com>
2022-11-30 14:50:35 -0500

    Use explicit overwrite so it is available in release builds.

--- src/os_common/os_alloc.c
+++ src/os_common/os_alloc.c
@@ -144,11 +144,8 @@ __realloc_func(WT_SESSION_IMPL *session, size_t *bytes_allocated_ret, size_t byt
             WT_RET_MSG(session, __wt_errno(), "memory allocation of %" WT_SIZET_FMT " bytes failed",
               bytes_to_allocate);
         memcpy(p, tmpp, *bytes_allocated_ret);
-        /*
-         * This call will only overwrite the old memory in diagnostic mode. But it would be very
-         * unusual for anyone to want this mode in a production system.
-         */
-        __wt_overwrite_and_free_len(session, tmpp, bytes_allocated);
+        __wt_explicit_overwrite(tmpp, bytes_allocated);
+        __wt_free(session, tmpp);
     } else {
         if ((p = realloc(p, bytes_to_allocate)) == NULL)
             WT_RET_MSG(session, __wt_errno(), "memory allocation of %" WT_SIZET_FMT " bytes failed",
