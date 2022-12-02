df27ba0f6bfb9699b17f383b529bf62c2d385ec1
Etienne Petrel <etienne.petrel@mongodb.com>
2022-12-02 14:23:10 +0000

    Create a new Operation constructor to operate on random tables

--- bench/workgen/workgen.h
+++ bench/workgen/workgen.h
@@ -307,6 +307,8 @@ struct Operation {
     Operation(OpType optype, Table table, Key key, Value value);
     Operation(OpType optype, Table table, Key key);
     Operation(OpType optype, Table table);
+    // Constructor with no table implies the use of random tables.
+    Operation(OpType optype, Key key, Value value);
     // Constructor with string applies to NOOP, SLEEP, CHECKPOINT
     Operation(OpType optype, const std::string& config);
     Operation(const Operation &other);
