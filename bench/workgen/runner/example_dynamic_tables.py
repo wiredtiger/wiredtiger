945261a65b24e903208eec6990edfc8a054c591c
Etienne Petrel <etienne.petrel@mongodb.com>
2022-12-05 21:05:38 +0000

    Avoid silent errors

--- bench/workgen/runner/example_dynamic_tables.py
+++ bench/workgen/runner/example_dynamic_tables.py
@@ -46,8 +46,7 @@ class ThreadWithReturnValue(pythread.Thread):
         self._return = None
 
     def run(self):
-        if self._target is not None:
-            self._return = self._target(*self._args, **self._kwargs)
+        self._return = self._target(*self._args, **self._kwargs)
             
     def join(self, *args):
         pythread.Thread.join(self, *args)
