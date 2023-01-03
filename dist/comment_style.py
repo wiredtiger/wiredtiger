b1fe8fdeb95502f412bc8099cbd93e317a7a1a54
Will Korteland <will.korteland@mongodb.com>
2023-01-03 23:44:12 +0000

    code cleanup

--- dist/comment_style.py
+++ dist/comment_style.py
@@ -1,6 +1,6 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-import os, subprocess, re, sys
+import argparse, os, re, subprocess, sys
 
 def print_msg(file_name, line_num, line, multiline):
     print("Illegal " + multiline + "comment in " + file_name + ":" + str(line_num - 1) + " " +
@@ -19,15 +19,19 @@ def check_c_comments(file_name):
     return count
 
 def match(stripped_line):
+    # We don't worry about whitespace in matches as we strip the line of whitespace.
+    url = re.compile(r'https?:\/\/')
+
+    # // Style comments
+    single_line_comment = re.compile(
+        r'^(?:[^"/\\]|\"(?:[^\"\\]|\\.)*\"|/(?:[^/"\\]|\\.)|/\"(?:[^\"\\]|\\.)*\"|\\.)*//(.*)$')
+
     return single_line_comment.match(stripped_line) and not url.search(stripped_line)
 
-def validate(line, expect_match):
-    if (expect_match and not match(line.strip())) or (not expect_match and match(line.strip())):
-        print("Test failed:" + line)
-    else:
-        print("Test success")
-
 def check_cpp_comments(file_name):
+    # Text before comments
+    text_check = re.compile(r'^[^\/\n]+')
+
     with open(file_name) as f:
         count = 0
         line_num = 0
@@ -58,21 +62,6 @@ def check_cpp_comments(file_name):
             line_num += 1
         return count
 
-# A collection of cases we handle.
-tests = [
-    (r'// hello world', True),
-    (r'     // hello world', True),
-    (r'hello world', False),
-    (r'printf("Hello, World!\n"); // prints hello world', True),
-    (r'String url = "http://www.example.com"', False),
-    (r'// hello world', True),
-    (r'//\\', True),
-    (r'// "some comment"', True),
-    (r'new URI("http://www.google.com")', False),
-    (r'printf("Escaped quote\""); // Comment', True),
-    (r' * http://www.google.com', False)
-]
-
 def file_is_cpp(name):
     if re.search('(.cpp|.cxx|.hpp)$', name) is not None:
         return True
@@ -89,56 +78,71 @@ def file_is_cpp(name):
                             text=True).stdout.strip('\n')
     return "C++" in result
 
-# Move up to root dir.
-os.chdir("..")
-
-# We don't worry about whitespace in matches as we strip the line of whitespace.
-url = re.compile(r'https?:\/\/')
-
-# // Style comments
-single_line_comment = re.compile(
-    r'^(?:[^"/\\]|\"(?:[^\"\\]|\\.)*\"|/(?:[^/"\\]|\\.)|/\"(?:[^\"\\]|\\.)*\"|\\.)*//(.*)$')
-# Text before comments
-text_check = re.compile(r'^[^\/\n]+')
-
-# Some directories aren't expected to comply with WiredTiger style. Ignore them.
-ignore_directories=[
-    'src/checksum',
-    'src/os_win',
-    'src/support',
+# A collection of cases we handle.
+tests = [
+    (r'// hello world', True),
+    (r'     // hello world', True),
+    (r'hello world', False),
+    (r'printf("Hello, World!\n"); // prints hello world', True),
+    (r'String url = "http://www.example.com"', False),
+    (r'// hello world', True),
+    (r'//\\', True),
+    (r'// "some comment"', True),
+    (r'new URI("http://www.google.com")', False),
+    (r'printf("Escaped quote\""); // Comment', True),
+    (r' * http://www.google.com', False)
 ]
 
-fast=False
+def validate(line, expect_match):
+    if (expect_match and not match(line.strip())) or (not expect_match and match(line.strip())):
+        print("Test failed:" + line)
+    else:
+        print("Test success")
 
-if len(sys.argv) > 1:
-    if (sys.argv[1] == '-t'):
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-t', '--test', default=False,
+                        help='Run self tests', action='store_true')
+    parser.add_argument('-f', '--fast', default=False,
+                        help='Use fast mode by only checking files changed in git',
+                        action='store_true')
+    args = parser.parse_args()
+
+    if args.test:
         for test in tests:
             validate(test[0], test[1])
-        sys.exit(0)
-    elif (sys.argv[1] == '-F'):
-        fast=True
+            sys.exit(0)
 
-command = " find bench examples ext  src test -name \"*.[ch]\" -o -name \"*.in\" -o -name \
-    \"*.cxx\" -o -name \"*.cpp\" -o -name \"*.i\" "
-if fast:
-    command = "git diff --name-only $(git merge-base --fork-point develop) bench examples ext src \
-        test | grep -E '(.c|.h|.cpp|.in|.cxx|.i)$'"
+    # Move up to root dir.
+    os.chdir("..")
 
-result = subprocess.run(command, shell=True, capture_output=True, text=True).stdout.strip('\n')
-count = 0
-for file_name in result.split('\n'):
-    cpp_file=False
-    skip=False
-    for directory in ignore_directories:
-        if directory in file_name:
-            skip=True
-    if skip:
-        continue
-    if file_is_cpp(file_name):
-        count += check_cpp_comments(file_name)
-    else:
-        count += check_c_comments(file_name)
+    # Some directories aren't expected to comply with WiredTiger style. Ignore them.
+    ignore_directories=[
+        'src/checksum',
+        'src/os_win',
+        'src/support',
+    ]
 
-if (count != 0):
-    print('Detected ' + str(count) +' comment format issues!')
+    command = "find bench examples ext src test -name \"*.[ch]\" -o -name \"*.in\" -o -name \
+        \"*.cxx\" -o -name \"*.cpp\" -o -name \"*.i\" "
+    if args.fast:
+        command = "git diff --name-only $(git merge-base --fork-point develop) bench examples ext src \
+            test | grep -E '(.c|.h|.cpp|.in|.cxx|.i)$'"
+
+    result = subprocess.run(command, shell=True, capture_output=True, text=True).stdout.strip('\n')
+    count = 0
+    for file_name in result.split('\n'):
+        skip=False
+        for directory in ignore_directories:
+            if directory in file_name:
+                skip=True
+        if skip:
+            continue
+        if file_is_cpp(file_name):
+            count += check_cpp_comments(file_name)
+        else:
+            count += check_c_comments(file_name)
+
+    if (count != 0):
+        print('Detected ' + str(count) +' comment format issues!')
 
