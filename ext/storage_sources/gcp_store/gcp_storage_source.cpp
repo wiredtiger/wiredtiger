f69fedd321cdeed9b13237e54ae34afaaa002214
Sona Reddy <sona.reddy@mongodb.com>
2023-01-12 23:25:27 +0000

    finished cmake and fixed struct definition errors

--- ext/storage_sources/gcp_store/gcp_storage_source.cpp
+++ ext/storage_sources/gcp_store/gcp_storage_source.cpp
@@ -8,43 +8,45 @@
 
 #include "gcp_connection.h"
 #include "wt_internal.h"
+struct gcp_file_system;
+struct gcp_file_handle;
 
-struct gcp_storage {
+struct gcp_store {
     WT_STORAGE_SOURCE store;
     std::list<gcp_file_system *> gcp_fs;
-}
+};
 
 struct gcp_file_system {
     WT_FILE_SYSTEM fs;
     std::list<gcp_file_handle *> gcp_fh;
     gcp_connection *gcp_conn;
-}
+};
 
 struct gcp_file_handle {
     WT_FILE_HANDLE fh;
     gcp_store *store;
-}
+};
 
 static int
 gcp_customize_file_system(
-  WT_STORAGE_SOURCE *, WT_SESSION *, const char *, const char *, const char *, WT_FILE_SYSTEM **);
-static int gcp_add_reference(WT_STORAGE_SOURCE *);
-static int gcp_file_system_terminate(WT_FILE_SYSTEM *, WT_SESSION *);
+  WT_STORAGE_SOURCE *, WT_SESSION *, const char *, const char *, const char *, WT_FILE_SYSTEM **) __attribute__((__unused__));
+static int gcp_add_reference(WT_STORAGE_SOURCE *) __attribute__((__unused__));
+static int gcp_file_system_terminate(WT_FILE_SYSTEM *, WT_SESSION *) __attribute__((__unused__));
 static int gcp_flush(
-  WT_STORAGE_SOURCE *, WT_SESSION *, WT_FILE_SYSTEM *, const char *, const char *, const char *);
+  WT_STORAGE_SOURCE *, WT_SESSION *, WT_FILE_SYSTEM *, const char *, const char *, const char *) __attribute__((__unused__));
 static int gcp_flush_finish(
-  WT_STORAGE_SOURCE *, WT_SESSION *, WT_FILE_SYSTEM *, const char *, const char *, const char *);
-static int gcp_file_exists(WT_FILE_SYSTEM *, WT_SESSION *, const char *, bool *);
+  WT_STORAGE_SOURCE *, WT_SESSION *, WT_FILE_SYSTEM *, const char *, const char *, const char *) __attribute__((__unused__));
+static int gcp_file_exists(WT_FILE_SYSTEM *, WT_SESSION *, const char *, bool *) __attribute__((__unused__));
 static int gcp_file_open(
-  WT_FILE_SYSTEM *, WT_SESSION *, const char *, WT_FS_OPEN_FILE_TYPE, uint32_t, WT_FILE_HANDLE **);
-static int gcp_remove(WT_FILE_SYSTEM *, WT_SESSION *, const char *, uint32_t);
-static int gcp_rename(WT_FILE_SYSTEM *, WT_SESSION *, const char *, const char *, uint32_t);
-static int gcp_file_size(WT_FILE_HANDLE *, WT_SESSION *, wt_off_t *);
+  WT_FILE_SYSTEM *, WT_SESSION *, const char *, WT_FS_OPEN_FILE_TYPE, uint32_t, WT_FILE_HANDLE **) __attribute__((__unused__));
+static int gcp_remove(WT_FILE_SYSTEM *, WT_SESSION *, const char *, uint32_t) __attribute__((__unused__));
+static int gcp_rename(WT_FILE_SYSTEM *, WT_SESSION *, const char *, const char *, uint32_t) __attribute__((__unused__));
+static int gcp_file_size(WT_FILE_HANDLE *, WT_SESSION *, wt_off_t *) __attribute__((__unused__));
 static int gcp_object_list_add(
-  const gcp_storage &, char ***, const std::vector<std::string> &, const uint32_t);
+  const gcp_store &, char ***, const std::vector<std::string> &, const uint32_t) __attribute__((__unused__));
 static int gcp_object_list_single(
-  WT_FILE_SYSTEM *, WT_SESSION *, const char *, const char *, char ***, uint32_t *);
-static int gcp_object_list_free(WT_FILE_SYSTEM *, WT_SESSION *, char **, uint32_t);
+  WT_FILE_SYSTEM *, WT_SESSION *, const char *, const char *, char ***, uint32_t *) __attribute__((__unused__));
+static int gcp_object_list_free(WT_FILE_SYSTEM *, WT_SESSION *, char **, uint32_t) __attribute__((__unused__));
 
 static int
 gcp_customize_file_system(WT_STORAGE_SOURCE *store, WT_SESSION *session, const char *bucket,
@@ -53,7 +55,7 @@ gcp_customize_file_system(WT_STORAGE_SOURCE *store, WT_SESSION *session, const c
     WT_UNUSED(store);
     WT_UNUSED(session);
     WT_UNUSED(bucket);
-    WT_UNUSED(authToken);
+    WT_UNUSED(auth_token);
     WT_UNUSED(config);
     WT_UNUSED(file_system);
 
@@ -166,7 +168,7 @@ gcp_file_size(WT_FILE_HANDLE *file_handle, WT_SESSION *session, wt_off_t *sizep)
 }
 
 static int
-gcp_object_list_add(const gcp_storage &gcp_, char ***object_list,
+gcp_object_list_add(const gcp_store &gcp_, char ***object_list,
   const std::vector<std::string> &objects, const uint32_t count)
 {
     WT_UNUSED(gcp_);
