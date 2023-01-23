a41b78b9d91449c05561a7f1cb5838b798fc44bb
Lachlan Ting <lachlan.kl.ting@gmail.com>
2023-01-23 04:24:06 +0000

    update cmake to work with external flag, still not working with path

--- cmake/third_party/azure_sdk.cmake
+++ cmake/third_party/azure_sdk.cmake
@@ -1,4 +1,5 @@
-azure_include(ExternalProject)
+include(ExternalProject)
+include(FetchContent)
 include(GNUInstallDirs)
 include(${CMAKE_SOURCE_DIR}/cmake/helpers.cmake)
 
@@ -22,16 +23,16 @@ endif()
 
 set(azure_storage_lib_location)
 set(azure_core_lib_location)
+set(azure_storage_common_lib_location)
 set(azure_sdk_include_location)
 
 if(IMPORT_AZURE_SDK_PACKAGE)
-    find_package()
-    # use the Azure provided variables to set the paths for the Azure targets.
-    set(azure_storage_lib_location ${INSTALL_DIR}/${CMAKE_INSTALL_LIBDIR}/libazure-storage-blobs${CMAKE_SHARED_LIBRARY_SUFFIX})
-    set(azure_core_lib_location ${INSTALL_DIR}/${CMAKE_INSTALL_LIBDIR}/libazure-core${CMAKE_SHARED_LIBRARY_SUFFIX})
-    set(azure_sdk_include_location ${INSTALL_DIR}/${CMAKE_INSTALL_INCLUDEDIR})
-
-else if (IMPORT_AZURE_SDK_EXTERNAL)
+    # find_package()
+    # # use the Azure provided variables to set the paths for the Azure targets.
+    # set(azure_storage_lib_location ${INSTALL_DIR}/${CMAKE_INSTALL_LIBDIR}/libazure-storage-blobs${CMAKE_SHARED_LIBRARY_SUFFIX})
+    # set(azure_core_lib_location ${INSTALL_DIR}/${CMAKE_INSTALL_LIBDIR}/libazure-core${CMAKE_SHARED_LIBRARY_SUFFIX})
+    # set(azure_sdk_include_location ${INSTALL_DIR}/${CMAKE_INSTALL_INCLUDEDIR})
+elseif (IMPORT_AZURE_SDK_EXTERNAL)
     ExternalProject_Add(
         azure-sdk
         PREFIX azure-sdk-cpp
@@ -43,10 +44,10 @@ else if (IMPORT_AZURE_SDK_EXTERNAL)
             -DCMAKE_POSITION_INDEPENDENT_CODE=ON
             -DCMAKE_INSTALL_PREFIX=${CMAKE_CURRENT_BINARY_DIR}/azure-sdk-cpp/install
         BUILD_ALWAYS FALSE
-        INSTALL_DIR ${CMAKE_CURRENT_BINARY_DIR}/azure-sdk-cpp/install
         BUILD_BYPRODUCTS
             ${CMAKE_CURRENT_BINARY_DIR}/azure-sdk-cpp/install/${CMAKE_INSTALL_LIBDIR}/libazure-storage-blobs${CMAKE_SHARED_LIBRARY_SUFFIX}
             ${CMAKE_CURRENT_BINARY_DIR}/azure-sdk-cpp/install/${CMAKE_INSTALL_LIBDIR}/libazure-core${CMAKE_SHARED_LIBRARY_SUFFIX}
+        INSTALL_DIR ${CMAKE_CURRENT_BINARY_DIR}/azure-sdk-cpp/install
         TEST_COMMAND ""
         UPDATE_COMMAND ""
     )
@@ -54,13 +55,15 @@ else if (IMPORT_AZURE_SDK_EXTERNAL)
     file(MAKE_DIRECTORY ${INSTALL_DIR}/${CMAKE_INSTALL_INCLUDEDIR})
 
     # Set the path variables to be used for the AZURE targets.
+    set(azure_sdk_include_location ${INSTALL_DIR}/${CMAKE_INSTALL_INCLUDEDIR})
     set(azure_storage_lib_location ${INSTALL_DIR}/${CMAKE_INSTALL_LIBDIR}/libazure-storage-blobs${CMAKE_SHARED_LIBRARY_SUFFIX})
     set(azure_core_lib_location ${INSTALL_DIR}/${CMAKE_INSTALL_LIBDIR}/libazure-core${CMAKE_SHARED_LIBRARY_SUFFIX})
-    set(azure_sdk_include_location ${INSTALL_DIR}/${CMAKE_INSTALL_INCLUDEDIR})
+    set(azure_storage_common_lib_location ${INSTALL_DIR}/${CMAKE_INSTALL_LIBDIR}/libazure-storage-common${CMAKE_SHARED_LIBRARY_SUFFIX})
 endif()
 
 add_library(azure_storage_lib SHARED IMPORTED)
 add_library(azure_core_lib SHARED IMPORTED)
+add_library(azure_storage_common_lib SHARED IMPORTED)
 
 # Small workaround to declare the include directory under INTERFACE_INCLUDE_DIRECTORIES during the configuration phase.
 set_target_properties(azure_storage_lib PROPERTIES
@@ -73,3 +76,12 @@ set_target_properties(azure_core_lib PROPERTIES
     IMPORTED_LOCATION ${azure_core_lib_location}
     INTERFACE_INCLUDE_DIRECTORIES ${azure_sdk_include_location}
 )
+
+set_target_properties(azure_storage_common_lib PROPERTIES
+    IMPORTED_LOCATION ${azure_storage_common_lib_location}
+    INTERFACE_INCLUDE_DIRECTORIES ${azure_sdk_include_location}
+)
+
+add_dependencies(azure_storage_lib azure_core_lib)
+add_dependencies(azure_storage_lib azure_storage_common_lib)
+target_link_libraries(azure_core_lib INTERFACE azure_storage_lib)
