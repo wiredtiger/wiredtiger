7d69aff1f968f828515748bc539b1991f6c35b5c
Luke Pearson <luke.pearson@mongodb.com>
2023-01-24 10:33:29 +1100

    Revert "Additional repro changes from will's branch."
    This reverts commit 07d8b7a4b12f1d7263bcfb98c15853eef88e1032.

--- src/include/btmem.h
+++ src/include/btmem.h
@@ -1431,15 +1431,6 @@ struct __wt_update_vector {
  * records. Berkeley DB did support mutable records, but it won't scale and it isn't useful enough
  * to re-implement, IMNSHO.)
  */
-
-#define WT_INSERT_HIST_ENTRIES 128
-
-struct __wt_insert_hist {
-    WT_INSERT *old_ins;
-    WT_INSERT *new_ins;
-    u_int idx;
-};
-
 struct __wt_insert {
     WT_UPDATE *upd; /* value */
 
@@ -1456,20 +1447,6 @@ struct __wt_insert {
 #define WT_INSERT_RECNO(ins) (((WT_INSERT *)(ins))->u.recno)
 
     WT_INSERT *next[0]; /* forward-linked skip list */
-
-#define WT_INS_PTR_SWAP(ins, old, i)                        \
-    do {                                                    \
-        struct __wt_insert_hist hist = {                    \
-            .old_ins = old,                                 \
-            .new_ins = ins,                                 \
-            .idx = i                                        \
-        };                                                  \
-        ins->insert_hist[ins->insert_hist_ptr++] = hist;    \
-        ins->insert_hist_ptr %= WT_INSERT_HIST_ENTRIES;     \
-    } while (0)
-
-    struct __wt_insert_hist insert_hist[WT_INSERT_HIST_ENTRIES];
-    uint8_t insert_hist_ptr;
 };
 
 /*
