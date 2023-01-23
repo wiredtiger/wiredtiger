9912d0e8fb507f24ee9656144fc9fe6e6009ce95
Lachlan Ting <lachlan.kl.ting@gmail.com>
2023-01-23 05:17:46 +0000

    WT-10510 add test cases for list put delete with prefixes

--- ext/storage_sources/azure_store/test/test_azure_connection.cpp
+++ ext/storage_sources/azure_store/test/test_azure_connection.cpp
@@ -31,4 +31,106 @@
 
 #include "azure_connection.h"
 
-TEST_CASE("testing class azure_connection", "azure_connection") {}
+TEST_CASE("Testing Azure Connection Class", "azure_connection") {
+    
+    SECTION("Testing prefix functionality works for list, put, and delete.") 
+    {
+        std::vector<std::string> objects;
+        azure_connection conn = azure_connection("myblobcontainer1", "");
+        azure_connection pfx_test = azure_connection("myblobcontainer1", "pfx_test_");
+        
+        // There is nothing in the container so there should be 0 objects
+        objects.clear();
+        REQUIRE(pfx_test.list_objects("", objects, false) == 0);
+        REQUIRE(objects.size() == 0);
+        objects.clear();
+        REQUIRE(conn.list_objects("", objects, true) == 0); 
+        REQUIRE(objects.size() == 0);
+        // The container is still empty so doing a list object with a prefix should 
+        // result in 0 objects
+        objects.clear(); 
+        REQUIRE(conn.list_objects("test", objects, false) == 0); 
+        REQUIRE(objects.size() == 0);
+        
+        objects.clear(); 
+        REQUIRE(conn.list_objects("test", objects, true) == 0); 
+        REQUIRE(objects.size() == 0);
+        // Add an object into the empty container resulting in 1 object now
+        objects.clear(); 
+        REQUIRE(pfx_test.put_object("test.txt", "/home/ubuntu/wiredtiger/ext/storage_sources/azure_store/test.txt") == 0);
+        REQUIRE(pfx_test.list_objects("", objects, false) == 0); 
+        REQUIRE(objects.size() == 1);
+        objects.clear(); 
+        REQUIRE(conn.list_objects("", objects, true) == 0); 
+        REQUIRE(objects.size() == 1);
+        // Calling list objects with a prefix that doesn't exist should result in 0 objects 
+        objects.clear(); 
+        REQUIRE(pfx_test.list_objects("bad_pfx_", objects, false) == 0); 
+        REQUIRE(objects.size() == 0);
+        objects.clear(); 
+        REQUIRE(pfx_test.list_objects("bad_pfx_", objects, true) == 0); 
+        REQUIRE(objects.size() == 0);
+        // Calling list objects with the prefix given should result in 1 object
+        objects.clear(); 
+        REQUIRE(pfx_test.list_objects("pfx_test_", objects, false) == 0); 
+        REQUIRE(objects.size() == 1);
+        
+        objects.clear(); 
+        REQUIRE(pfx_test.list_objects("pfx_test_", objects, true) == 0); 
+        REQUIRE(objects.size() == 1);
+        // Adding another object into the container to test the prefix functionality 
+        // and the list single functionality
+        objects.clear(); 
+        REQUIRE(pfx_test.put_object("test1.txt", "/home/ubuntu/wiredtiger/ext/storage_sources/azure_store/test.txt") == 0);
+        REQUIRE(pfx_test.list_objects("pfx_test_", objects, false) == 0); 
+        REQUIRE(objects.size() == 2);
+        objects.clear();
+        REQUIRE(pfx_test.list_objects("pfx_test_", objects, true) == 0); 
+        REQUIRE(objects.size() == 1);
+        // Creation of another connection class to test prefix functionality works with 
+        // multiple prefixes
+        azure_connection pfx_check = azure_connection("myblobcontainer1", "pfx_check_");
+        // There is are 2 objects in the container so there should be 2 objects
+        objects.clear();
+        REQUIRE(pfx_check.list_objects("", objects, false) == 0);
+        REQUIRE(objects.size() == 2);
+        // Now test that there should be no objects with the prefix pfx_check_ in the 
+        // container 
+        objects.clear();
+        REQUIRE(pfx_check.list_objects("pfx_check_", objects, false) == 0);
+        REQUIRE(objects.size() == 0);
+        // Now let's add an object with the prefix pfx_check_ to the container
+        objects.clear();
+        REQUIRE(pfx_check.put_object("test.txt", "/home/ubuntu/wiredtiger/ext/storage_sources/azure_store/test.txt") == 0);
+        REQUIRE(pfx_check.list_objects("pfx_check_", objects, false) == 0);
+        REQUIRE(objects.size() == 1);
+        // Testing 2 occurances of pfx_check_ in the same container results in 2 objects 
+        objects.clear();
+        REQUIRE(pfx_check.put_object("test1.txt", "/home/ubuntu/wiredtiger/ext/storage_sources/azure_store/test.txt") == 0);
+        REQUIRE(pfx_check.list_objects("pfx_check_", objects, false) == 0);
+        REQUIRE(objects.size() == 2);
+        // There should be 4 objects in the container let's check that 
+        objects.clear();
+        REQUIRE(pfx_check.list_objects("", objects, false) == 0);
+        REQUIRE(objects.size() == 4);
+        // Test deletion of a pfx_check_ object results in only 1 left and 3 overall 
+        objects.clear();
+        REQUIRE(pfx_check.delete_object("test.txt") == 0);
+        REQUIRE(pfx_check.list_objects("pfx_check_", objects, false) == 0);
+        REQUIRE(objects.size() == 1);
+        objects.clear();
+        REQUIRE(pfx_check.list_objects("", objects, false) == 0);
+        REQUIRE(objects.size() == 3);
+        // Delete remaining objects in the container
+        REQUIRE(pfx_check.delete_object("test1.txt") == 0);
+        REQUIRE(pfx_test.delete_object("test.txt") == 0);
+        REQUIRE(pfx_test.delete_object("test1.txt") == 0);
+        // Check there are no objects in the container 
+        objects.clear();
+        REQUIRE(pfx_check.list_objects("", objects, false) == 0);
+        REQUIRE(objects.size() == 0);
+        // Check that deletion of an object that doesn't exist results in a -1 
+        REQUIRE(pfx_check.delete_object("test.txt") == -1);
+        REQUIRE(pfx_test.delete_object("test.txt") == -1);
+    }
+}
