5f06583e7cd368cb304a1533c0022df2cdf798ac
Will Korteland <will.korteland@mongodb.com>
2023-01-24 01:20:15 +0000

    fix build

--- src/include/serial_inline.h
+++ src/include/serial_inline.h
@@ -31,11 +31,8 @@ __insert_simple_func(
      */
     for (i = 0; i < skipdepth; i++) {
         WT_ORDERED_READ(old_ins, *ins_stack[i]);
-        if (old_ins != new_ins->next[i] || !__wt_atomic_cas_ptr(ins_stack[i], old_ins, new_ins)) {
-            WT_INS_PTR_SWAP(new_ins, NULL, 0xffffffff - i);
+        if (old_ins != new_ins->next[i] || !__wt_atomic_cas_ptr(ins_stack[i], old_ins, new_ins))
             return (i == 0 ? WT_RESTART : 0);
-        }
-        WT_INS_PTR_SWAP(new_ins, old_ins, i);
     }
 
     return (0);
