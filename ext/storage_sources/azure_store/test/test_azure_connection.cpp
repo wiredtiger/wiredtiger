195842c01a12929f8a1e30bbf9f0f6522bf03cff
Lachlan Ting <lachlan.kl.ting@gmail.com>
2023-02-01 23:02:54 +0000

    added function to create file

--- ext/storage_sources/azure_store/test/test_azure_connection.cpp
+++ ext/storage_sources/azure_store/test/test_azure_connection.cpp
@@ -32,53 +32,62 @@
 #include "azure_connection.h"
 #include <fstream>
 
+static std::string
+create_file(const std::string &file_name, const std::string &payload)
+{
+    std::ofstream file(file_name);
+    file << payload;
+    file.close();
+    return file_name;
+}
+
 TEST_CASE("Testing Azure Connection Class", "azure-connection")
 {
-
     auto azure_client = Azure::Storage::Blobs::BlobContainerClient::CreateFromConnectionString(
       std::getenv("AZURE_STORAGE_CONNECTION_STRING"), "myblobcontainer1");
     bool exists = false;
 
-    azure_connection conn = azure_connection("myblobcontainer1", "unit_testing_");
-    azure_connection conn_bad = azure_connection("myblobcontainer1", "bad_prefix_");
     // Prefix for objects in this test.
     const std::string prefix = "unit_testing_";
+
+    azure_connection conn = azure_connection("myblobcontainer1", "unit_testing_");
+    azure_connection conn_bad = azure_connection("myblobcontainer1", "bad_prefix_");
+
     const std::string object_name = "test_object";
     const std::string file_name = "test_object.txt";
     const std::string path = "./" + file_name;
+    const std::string prefix_file_name = prefix + file_name;
+
     const std::string object_name_2 = "test_object_2";
     const std::string file_name_2 = "test_object_2.txt";
     const std::string path_2 = "./" + file_name_2;
-    const std::string non_exi_object_key = "test_non_exist";
-    const std::string non_exi_file_name = "test_non_exist.txt";
-    const std::string prefix_file_name = prefix + file_name;
     const std::string prefix_file_name_2 = prefix + file_name_2;
 
-    std::ofstream file(file_name);
+    const std::string non_exi_object_key = "test_non_exist";
+    const std::string non_exi_file_name = "test_non_exist.txt";
+
+    void *buffer = calloc(1024, sizeof(char));
+    std::vector<std::string> objects;
+
+    // Payloads for blobs uploaded to container.
     const std::string payload = "Test payload";
     const std::string payload_substr = "payload";
-    file << payload;
-    file.close();
-
-    std::ofstream file_2(file_name_2);
     const std::string payload_2 = "Testing offset and substring";
     const std::string payload_2_substr = "offset";
-    file_2 << payload_2;
-    file_2.close();
 
-    SECTION("Checks azure connection constructor.", "[azure-connection]")
+    SECTION("Check Azure connection constructor.", "[azure-connection]")
     {
         REQUIRE_THROWS_WITH(azure_connection("Bad_bucket", ""), "Bad_bucket : No such bucket.");
     }
 
-    SECTION("Check if an object exists in Azure.", "[azure-connection]")
+    SECTION("Check object exists in Azure.", "[azure-connection]")
     {
         // Object does not exist yet so there should be 0 objects.
         REQUIRE(conn.object_exists(file_name, exists) == 0);
         REQUIRE(exists == false);
 
-        auto blob_client = azure_client.GetBlockBlobClient(prefix_file_name);
-        blob_client.UploadFrom(file_name);
+        auto blob_client = azure_client.GetBlockBlobClient(prefix + file_name);
+        blob_client.UploadFrom(create_file(file_name, payload));
 
         // Object exists now in the container.
         REQUIRE(conn.object_exists(file_name, exists) == 0);
@@ -87,55 +96,54 @@ TEST_CASE("Testing Azure Connection Class", "azure-connection")
         blob_client.Delete();
     }
 
-    SECTION("Lists Azure objects under the text bucket.", "[azure-connection]")
+    SECTION("List Azure objects under the test bucket.", "[azure-connection]")
     {
-        std::vector<std::string> objects;
-
         // Total objects to insert in the test.
         const int32_t num_objects = 11;
 
         // No matching objects. Object size should be 0.
-        REQUIRE(conn.list_objects(prefix_file_name, objects, false) == 0);
+        objects.clear();
+        REQUIRE(conn.list_objects(prefix + "test_object", objects, false) == 0);
         REQUIRE(objects.size() == 0);
 
         // No matching objects with listSingle. Object size should be 0.
-        REQUIRE(conn.list_objects(prefix_file_name, objects, true) == 0);
+        REQUIRE(conn.list_objects(prefix + "test_object", objects, true) == 0);
         REQUIRE(objects.size() == 0);
 
         // Put objects to prepare for test.
         for (int i = 0; i < num_objects; i++) {
             auto blob_client =
-              azure_client.GetBlockBlobClient(prefix_file_name + std::to_string(i) + ".txt");
-            blob_client.UploadFrom(file_name);
+              azure_client.GetBlockBlobClient(prefix + "test_object" + std::to_string(i) + ".txt");
+            blob_client.UploadFrom(create_file(file_name, payload));
         }
 
         // List all objects. This is the size of num_objects.
-        REQUIRE(conn.list_objects(prefix_file_name, objects, false) == 0);
+        REQUIRE(conn.list_objects(prefix + "test_object", objects, false) == 0);
         REQUIRE(objects.size() == num_objects);
 
         // List single. Object size should be 1.
         objects.clear();
-        REQUIRE(conn.list_objects(prefix_file_name, objects, true) == 0);
+        REQUIRE(conn.list_objects(prefix + "test_object", objects, true) == 0);
         REQUIRE(objects.size() == 1);
 
-        // There should be 2 matches with 'list_obj_1' prefix pattern.
+        // There should be 2 matches with 'unit_testing_test_object1' prefix pattern.
         objects.clear();
-        REQUIRE(conn.list_objects(prefix_file_name + "1", objects, false) == 0);
+        REQUIRE(conn.list_objects(prefix + "test_object1", objects, false) == 0);
         REQUIRE(objects.size() == 2);
 
         // Clean up.
         for (int i = 0; i < num_objects; i++) {
             auto blob_client =
-              azure_client.GetBlockBlobClient(prefix_file_name + std::to_string(i) + ".txt");
+              azure_client.GetBlockBlobClient(prefix + "test_object" + std::to_string(i) + ".txt");
             blob_client.Delete();
         }
     }
 
-    SECTION("Testing delete functionality for Azure.", "[azure-connection]")
+    SECTION("Test delete functionality for Azure.", "[azure-connection]")
     {
-        auto blob_client = azure_client.GetBlockBlobClient(prefix_file_name);
+        auto blob_client = azure_client.GetBlockBlobClient(prefix + file_name);
 
-        blob_client.UploadFrom(file_name);
+        blob_client.UploadFrom(create_file(file_name, payload));
 
         // Test that an object can be deleted.
         REQUIRE(conn.delete_object(file_name) == 0);
@@ -144,26 +152,28 @@ TEST_CASE("Testing Azure Connection Class", "azure-connection")
         REQUIRE(conn.delete_object(non_exi_object_key) == ENOENT);
     }
 
-    SECTION("Checking put functionality in Azure.", "[azure-connection]")
+    SECTION("Check put functionality in Azure.", "[azure-connection]")
     {
         REQUIRE(conn.put_object(file_name, path) == 0);
 
         auto blob_client = azure_client.GetBlockBlobClient(prefix_file_name);
-        blob_client.Delete();
 
         // Test that putting an object that doesn't exist locally returns -1.
         REQUIRE(conn.put_object(non_exi_object_key, non_exi_file_name) == -1);
 
         // Test that deleting an object that exists but prefix is wrong returns a ENOENT.
-        REQUIRE(conn_bad.delete_object(object_name + std::to_string(0) + ".txt") == ENOENT);
+        REQUIRE(conn_bad.delete_object(file_name) == ENOENT);
+
+        blob_client.Delete();
     }
 
-    SECTION("Checking read functionality in Azure.", "[azure-connection]")
+    SECTION("Check read functionality in Azure.", "[azure-connection]")
     {
+        // Create two blob clients for our two blobs.
         auto blob_client = azure_client.GetBlockBlobClient(prefix_file_name);
-
-        blob_client.UploadFrom(file_name);
-        void *buffer = calloc(1024, sizeof(char));
+        auto blob_client_2 = azure_client.GetBlockBlobClient(prefix_file_name_2);
+        blob_client.UploadFrom(create_file(file_name, payload));
+        blob_client_2.UploadFrom(create_file(file_name_2, payload_2));
 
         // Test reading whole file.
         REQUIRE(conn.read_object(file_name, 0, payload.length(), buffer) == 0);
@@ -171,11 +181,11 @@ TEST_CASE("Testing Azure Connection Class", "azure-connection")
         memset(buffer, 0, 1024);
 
         // Check that read works from the middle to the end.
-        REQUIRE(conn.read_object(file_name, payload.find(payload_substr),
-                  payload.length() - payload.find(payload_substr), buffer) == 0);
+        const int str_len = payload_2.length() - payload_2.find(payload_2_substr);
+        REQUIRE(
+          conn.read_object(file_name_2, payload_2.find(payload_2_substr), str_len, buffer) == 0);
         REQUIRE(static_cast<char *>(buffer) ==
-          payload.substr(
-            payload.find(payload_substr), payload.length() - payload.find(payload_substr)));
+          payload_2.substr(payload_2.find(payload_2_substr), str_len));
         memset(buffer, 0, 1024);
 
         // Test overflow on positive offset but past EOF.
@@ -188,14 +198,20 @@ TEST_CASE("Testing Azure Connection Class", "azure-connection")
 
         // Test overflow with negative offset.
         REQUIRE(conn.read_object(file_name, -1, 12, buffer) == -1);
-        free(buffer);
+
+        // Tests that reading a existing object but wrong prefix returns a ENOENT.
+        REQUIRE(conn_bad.read_object(file_name, 0, 1, buffer) == ENOENT);
 
         blob_client.Delete();
+        blob_client_2.Delete();
 
         // Test that reading a non existent object returns a ENOENT.
         REQUIRE(conn.read_object(non_exi_object_key, 0, 1, buffer) == ENOENT);
-
-        // Tests that reading a non existant object returns a ENOENT.
-        REQUIRE(conn.read_object(non_exi_object_key, 0, 1, buffer) == ENOENT);
     }
+
+    // Sanity check that nothing exists and clean up.
+    free(buffer);
+    objects.clear();
+    REQUIRE(conn.list_objects(prefix, objects, false) == 0);
+    REQUIRE(objects.size() == 0);
 }
