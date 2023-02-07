6c08fb26a6d282d0614612c7e00a0e64ab4732df
Hari Babu Kommi <haribabu.kommi@mongodb.com>
2023-02-07 12:33:36 +1100

    WT-10575 Fix checkpoint cursor to return only visible stable data
    Returning only visible data can lead to inconsisten results when
    a prepared transaction gets committed in parallel to the checkpoint
    with a commit timestamp less than or equal to the checkpoint stable
    timestamp.
    
    To avoid producing torn transaction results, along with the commit
    timestamp visibility, the data must be durable according to the
    checkpoint stable timestamp.

--- src/btree/bt_delete.c
+++ src/btree/bt_delete.c
@@ -169,6 +169,7 @@ __wt_delete_page(WT_SESSION_IMPL *session, WT_REF *ref, bool *skipp)
     /* History store data are always visible. No need to check visibility. */
     if (!WT_IS_HS(session->dhandle) &&
       !__wt_txn_visible(session, addr.ta.newest_txn,
+        WT_MAX(addr.ta.newest_start_durable_ts, addr.ta.newest_stop_durable_ts),
         WT_MAX(addr.ta.newest_start_durable_ts, addr.ta.newest_stop_durable_ts)))
         goto err;
 
