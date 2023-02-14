1e04eba48332df4638aa1e57cb75620c664eaefd
Sean04 <sean.watt@mongodb.com>
2023-02-14 02:31:43 +0000

    WT-10563 add checksum value in checksum mismatch error message.

--- src/block/block_read.c
+++ src/block/block_read.c
@@ -215,9 +215,12 @@ __wt_block_read_off(WT_SESSION_IMPL *session, WT_BLOCK *block, WT_ITEM *buf, uin
         if (!F_ISSET(session, WT_SESSION_QUIET_CORRUPT_FILE))
             __wt_errx(session,
               "%s: potential hardware corruption, read checksum error for %" PRIu32
-              "B block at offset %" PRIuMAX
-              ": calculated block checksum doesn't match expected checksum",
-              block->name, size, (uintmax_t)offset);
+              "B block at offset %" PRIuMAX ": calculated block checksum of %#" PRIx32
+              " doesn't match expected checksum of %#" PRIx32,
+              block->name, size, (uintmax_t)offset,
+              __wt_checksum(
+                buf->mem, F_ISSET(&swap, WT_BLOCK_DATA_CKSUM) ? size : WT_BLOCK_COMPRESS_SKIP),
+              checksum);
     } else if (!F_ISSET(session, WT_SESSION_QUIET_CORRUPT_FILE))
         __wt_errx(session,
           "%s: potential hardware corruption, read checksum error for %" PRIu32
