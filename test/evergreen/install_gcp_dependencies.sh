2e2990e7155df2efad06da11745edb45d29cb666
Jie Chen <jie.chen@mongodb.com>
2023-02-22 06:55:55 +0000

    cleaned up gcp script

--- test/evergreen/install_gcp_dependencies.sh
+++ test/evergreen/install_gcp_dependencies.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 set -o errexit  # Exit the script with error if any of the commands fail
 
-# Install dependencies for the GCP SDK. This method is being used instead of having them installed on system
-# to reduce any versioning problems in the future if the GCP SDK requirements were to change.
-
+# Install dependencies for the GCP SDK. This method is being used instead of having them installed 
+# on system to reduce any versioning problems in the future if the GCP SDK requirements were to
+# change. The installation of the libraries were followed from GCP SDK's documentation:
+# https://github.com/googleapis/google-cloud-cpp/blob/main/doc/packaging.md
+#
 install_abseil ()
 {
     mkdir abseil-cpp && cd abseil-cpp
@@ -11,11 +13,11 @@ install_abseil ()
     tar --strip-components=1 -xzf abseil-cpp.tar.gz
     sed -i 's/^#define ABSL_OPTION_USE_\(.*\) 2/#define ABSL_OPTION_USE_\1 0/' "absl/base/options.h"
     mkdir cmake-out && cd cmake-out
-    /opt/cmake/bin/cmake \
+    cmake \
         -DCMAKE_BUILD_TYPE=Release \
         -DABSL_BUILD_TESTING=OFF \
         -DBUILD_SHARED_LIBS=yes ../.
-    make -j4
+    make -j 4
     cd ..
     sudo /opt/cmake/bin/cmake --build cmake-out --target install
     cd ..
@@ -27,12 +29,12 @@ install_nlohmann ()
     curl -sSL https://github.com/nlohmann/json/archive/v3.11.2.tar.gz -o nlohmann-json.tar.gz
     tar --strip-components=1 -xzf nlohmann-json.tar.gz
     mkdir cmake-out && cd cmake-out
-    /opt/cmake/bin/cmake \
+    cmake \
         -DCMAKE_BUILD_TYPE=Release \
         -DBUILD_SHARED_LIBS=yes \
         -DBUILD_TESTING=OFF \
         -DJSON_BuildTests=OFF ../.
-    make -j4
+    make -j 4
     cd ..
     sudo /opt/cmake/bin/cmake --build cmake-out --target install
     cd ..
@@ -44,13 +46,12 @@ install_crc32c ()
     curl -sSL https://github.com/google/crc32c/archive/1.1.2.tar.gz -o crc32c.tar.gz
     tar --strip-components=1  -xzf crc32c.tar.gz
     mkdir cmake-out && cd cmake-out
-    /opt/cmake/bin/cmake \
-        -DCMAKE_BUILD_TYPE=Release \
+    cmake -DCMAKE_BUILD_TYPE=Release \
         -DBUILD_SHARED_LIBS=yes \
         -DCRC32C_BUILD_TESTS=OFF \
         -DCRC32C_BUILD_BENCHMARKS=OFF \
         -DCRC32C_USE_GLOG=OFF ../.
-    make -j4
+    make -j 4
     cd ..
     sudo /opt/cmake/bin/cmake --build cmake-out --target install
     cd ..
