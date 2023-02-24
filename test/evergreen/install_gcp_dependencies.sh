492234a983dda8e759a03ba293998922490a8259
Jie Chen <jie.chen@mongodb.com>
2023-02-24 00:49:37 +0000

    Address PR changes

--- test/evergreen/install_gcp_dependencies.sh
+++ test/evergreen/install_gcp_dependencies.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-set -o errexit  # Exit the script with error if any of the commands fail
+set -o errexit
 
 # Install dependencies for the GCP SDK. This method is being used instead of having them installed 
 # on system to reduce any versioning problems in the future if the GCP SDK requirements were to
@@ -11,19 +11,18 @@ install_abseil ()
     mkdir abseil-cpp && cd abseil-cpp
     curl -sSL https://github.com/abseil/abseil-cpp/archive/20230125.0.tar.gz -o abseil-cpp.tar.gz
     tar --strip-components=1 -xzf abseil-cpp.tar.gz
-    sed -i 's/^#define ABSL_OPTION_USE_\(.*\) 2/#define ABSL_OPTION_USE_\1 0/' "absl/base/options.h"
     mkdir cmake-out && cd cmake-out
     $CMAKE \
         -DCMAKE_BUILD_TYPE=Release \
         -DABSL_BUILD_TESTING=OFF \
         -DBUILD_SHARED_LIBS=yes ../.
-    make -j 4
+    make -j $(nproc)
     cd ..
     sudo /opt/cmake/bin/cmake --build cmake-out --target install
     cd ..
 }
 
-install_nlohmann ()
+install_nlohmann_json ()
 {
     mkdir json && cd json
     curl -sSL https://github.com/nlohmann/json/archive/v3.11.2.tar.gz -o nlohmann-json.tar.gz
@@ -34,7 +33,7 @@ install_nlohmann ()
         -DBUILD_SHARED_LIBS=yes \
         -DBUILD_TESTING=OFF \
         -DJSON_BuildTests=OFF ../.
-    make -j 4
+    make -j $(nproc)
     cd ..
     sudo /opt/cmake/bin/cmake --build cmake-out --target install
     cd ..
@@ -51,7 +50,7 @@ install_crc32c ()
         -DCRC32C_BUILD_TESTS=OFF \
         -DCRC32C_BUILD_BENCHMARKS=OFF \
         -DCRC32C_USE_GLOG=OFF ../.
-    make -j 4
+    make -j $(nproc)
     cd ..
     sudo /opt/cmake/bin/cmake --build cmake-out --target install
     cd ..
@@ -64,5 +63,5 @@ fi
 
 CMAKE=$1
 install_abseil
-install_nlohmann
+install_nlohmann_json
 install_crc32c
