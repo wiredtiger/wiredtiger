a660e6f578edb87cea4101891f57a00facad00fe
Will Korteland <will.korteland@mongodb.com>
2023-02-27 02:42:08 +0000

    use a tmpfile for type_to_str

--- dist/type_to_str.py
+++ dist/type_to_str.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
-import re
+import os, re
+
+from dist import compare_srcfile, format_srcfile
 
 """
 * docstring: The documentation to attach to the generated function.
@@ -42,17 +44,23 @@ def generate_string_conversion(docstring, name, define_regex, srcfile, out):
     out.write('}\n')
 
 if __name__ == '__main__':
-    with open('../src/include/str_inline.h', 'w+') as out:
+    src = '../src/include/str_inline.h'
+    tmp = '__tmp' + str(os.getpid())
+
+    with open(tmp, 'w') as tfile:
         generate_string_conversion('Convert a prepare state to its string representation.',
                                    'prepare_state',
                                    r'^#define\s+(WT_PREPARE_[A-Z0-9_]+)\s+[0-9]+',
                                    'src/include/btmem.h',
-                                   out)
-        out.write('\n')
+                                   tfile)
+        tfile.write('\n')
 
         generate_string_conversion('Convert an update type to its string representation.',
                                    'update_type',
                                    # Also match comments/space at end to avoid e.g. WT_UPDATE_SIZE.
                                    r'^#define\s+(WT_UPDATE_[A-Z0-9_]+)\s+[0-9]+\s+/\*',
                                    'src/include/btmem.h',
-                                   out)
+                                   tfile)
+
+    format_srcfile(tmp)
+    compare_srcfile(tmp, src)
