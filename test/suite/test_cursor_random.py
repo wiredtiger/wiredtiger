3520177e5696510a6274e974de25f94c5d9a1949
Sean04 <sean.watt@mongodb.com>
2023-02-28 00:05:56 +0000

    Extract warning message into variable.

--- test/suite/test_cursor_random.py
+++ test/suite/test_cursor_random.py
@@ -42,6 +42,7 @@ class test_cursor_random(wttest.WiredTigerTestCase):
         ('not-sample', dict(config='next_random=true'))
     ]
     scenarios = make_scenarios(types, config)
+    expected_warning_msg = 'Eviction took more than 1 minute'
 
     # Check that opening a random cursor on a row-store returns not-supported
     # for methods other than next, reconfigure and reset, and next returns
@@ -114,7 +115,7 @@ class test_cursor_random(wttest.WiredTigerTestCase):
         # take longer to evict pages due to overflow items on the page.
         self.conn.close()
         if (sys.platform.startswith('darwin')):
-            self.ignoreStdoutPatternIfExists('Eviction took more than 1 minute')
+            self.ignoreStdoutPatternIfExists(self.expected_warning_msg)
 
     def test_cursor_random_multiple_insert_records_small(self):
         self.cursor_random_multiple_insert_records(2000)
@@ -147,7 +148,7 @@ class test_cursor_random(wttest.WiredTigerTestCase):
         # take longer to evict pages due to overflow items on the page.
         self.conn.close()
         if (sys.platform.startswith('darwin')):
-            self.ignoreStdoutPatternIfExists('Eviction took more than 1 minute')
+            self.ignoreStdoutPatternIfExists(self.expected_warning_msg)
 
     def test_cursor_random_multiple_page_records_reopen_small(self):
         self.cursor_random_multiple_page_records(2000, True)
@@ -185,7 +186,7 @@ class test_cursor_random(wttest.WiredTigerTestCase):
         # take longer to evict pages due to overflow items on the page.
         self.conn.close()
         if (sys.platform.startswith('darwin')):
-            self.ignoreStdoutPatternIfExists('Eviction took more than 1 minute')
+            self.ignoreStdoutPatternIfExists(self.expected_warning_msg)
 
     # Check that next_random fails in the presence of a set of values, all of
     # which are deleted.
@@ -208,7 +209,7 @@ class test_cursor_random(wttest.WiredTigerTestCase):
         # take longer to evict pages due to overflow items on the page.
         self.conn.close()
         if (sys.platform.startswith('darwin')):
-            self.ignoreStdoutPatternIfExists('Eviction took more than 1 minute')
+            self.ignoreStdoutPatternIfExists(self.expected_warning_msg)
 
 # Check that opening a random cursor on column-store returns not-supported.
 class test_cursor_random_column(wttest.WiredTigerTestCase):
