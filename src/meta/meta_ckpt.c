2513f2a086affe8469f3c57866f7cc7885ba73cd
Susan LoVerso <sue@mongodb.com>
2023-04-06 09:06:23 -0400

    Add in the actual call for the fix.

--- src/meta/meta_ckpt.c
+++ src/meta/meta_ckpt.c
@@ -893,6 +893,11 @@ __wt_meta_ckptlist_get_from_config(WT_SESSION_IMPL *session, bool update, WT_CKP
 
             WT_ERR(__ckpt_load(session, &k, &v, ckpt));
             WT_ERR(__wt_meta_block_metadata(session, config, ckpt));
+            /*
+             * Load any incremental information from the configuration string into the existing base
+             * checkpoints.
+             */
+            WT_ERR(__meta_blk_mods_load(session, config, NULL, ckpt, false, true));
         }
     }
     WT_ERR_NOTFOUND_OK(ret, false);
