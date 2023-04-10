cdf70663da6622a31f851a25dc6629141fe87580
Meilianggang <lianggang.mei@intel.com>
2023-04-10 21:21:20 +0800

    Update ext/compressors/iaa/iaacodec/include/iaaInterface-c.h
    Co-authored-by: Etienne Petrel <77254506+etienneptl@users.noreply.github.com>

--- ext/compressors/iaa/iaacodec/include/iaaInterface-c.h
+++ ext/compressors/iaa/iaacodec/include/iaaInterface-c.h
@@ -15,6 +15,15 @@ extern "C"
 uint32_t getMaxCompressedDataSize(uint32_t srcLen);
 uint32_t doCompressData(WT_COMPRESSOR *compressor, WT_SESSION *session, uint8_t *source, uint32_t source_size, uint8_t *dest, uint32_t dest_size);
 void doDecompressData(WT_COMPRESSOR *compressor, WT_SESSION *session, uint8_t *source, uint32_t source_size, uint8_t* dest, uint32_t uncompressed_size, uint32_t* result_lenp);
+}
+#endif
+
+extern "C"
+{
+#endif
+uint32_t getMaxCompressedDataSize(uint32_t srcLen);
+uint32_t doCompressData(WT_COMPRESSOR *compressor, WT_SESSION *session, uint8_t *source, uint32_t source_size, uint8_t *dest, uint32_t dest_size);
+void doDecompressData(WT_COMPRESSOR *compressor, WT_SESSION *session, uint8_t *source, uint32_t source_size, uint8_t* dest, uint32_t uncompressed_size, uint32_t* result_lenp);
 
 #ifdef __cplusplus
 }
