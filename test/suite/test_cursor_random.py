6c98b677c587459547f2f6a5d2edf74dc3d293f3
Etienne Petrel <etienne.petrel@mongodb.com>
2023-04-11 01:25:08 +0000

    Revert "WT-10380 Fix expected warning output in test_cursor_random (#8850)"
    This reverts commit b615ce15ba2f1ad10f106817a7a70b39e214041f.

--- test/suite/test_cursor_random.py
+++ test/suite/test_cursor_random.py
@@ -26,7 +26,7 @@
 # ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 # OTHER DEALINGS IN THE SOFTWARE.
 
-import wiredtiger, wttest, sys
+import wiredtiger, wttest
 from wtdataset import SimpleDataSet, ComplexDataSet, simple_key, simple_value
 from wtscenario import make_scenarios
 
@@ -42,7 +42,6 @@ class test_cursor_random(wttest.WiredTigerTestCase):
         ('not-sample', dict(config='next_random=true'))
     ]
     scenarios = make_scenarios(types, config)
-    expected_warning_msg = 'Eviction took more than 1 minute'
 
     # Check that opening a random cursor on a row-store returns not-supported
     # for methods other than next, reconfigure and reset, and next returns
@@ -114,8 +113,7 @@ class test_cursor_random(wttest.WiredTigerTestCase):
         # Ignore the eviction generation drain warning as it is possible for eviction to
         # take longer to evict pages due to overflow items on the page.
         self.conn.close()
-        if (sys.platform.startswith('darwin')):
-            self.ignoreStdoutPatternIfExists(self.expected_warning_msg)
+        self.ignoreStdoutPatternIfExists('evict generation drain waited')
 
     def test_cursor_random_multiple_insert_records_small(self):
         self.cursor_random_multiple_insert_records(2000)
@@ -147,8 +145,7 @@ class test_cursor_random(wttest.WiredTigerTestCase):
         # Ignore the eviction generation drain warning as it is possible for eviction to
         # take longer to evict pages due to overflow items on the page.
         self.conn.close()
-        if (sys.platform.startswith('darwin')):
-            self.ignoreStdoutPatternIfExists(self.expected_warning_msg)
+        self.ignoreStdoutPatternIfExists('evict generation drain waited')
 
     def test_cursor_random_multiple_page_records_reopen_small(self):
         self.cursor_random_multiple_page_records(2000, True)
@@ -185,8 +182,7 @@ class test_cursor_random(wttest.WiredTigerTestCase):
         # Ignore the eviction generation drain warning as it is possible for eviction to
         # take longer to evict pages due to overflow items on the page.
         self.conn.close()
-        if (sys.platform.startswith('darwin')):
-            self.ignoreStdoutPatternIfExists(self.expected_warning_msg)
+        self.ignoreStdoutPatternIfExists('evict generation drain waited')
 
     # Check that next_random fails in the presence of a set of values, all of
     # which are deleted.
@@ -208,8 +204,7 @@ class test_cursor_random(wttest.WiredTigerTestCase):
         # Ignore the eviction generation drain warning as it is possible for eviction to
         # take longer to evict pages due to overflow items on the page.
         self.conn.close()
-        if (sys.platform.startswith('darwin')):
-            self.ignoreStdoutPatternIfExists(self.expected_warning_msg)
+        self.ignoreStdoutPatternIfExists('evict generation drain waited')
 
 # Check that opening a random cursor on column-store returns not-supported.
 class test_cursor_random_column(wttest.WiredTigerTestCase):
