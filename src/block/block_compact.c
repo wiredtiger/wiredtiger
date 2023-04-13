26f56fe61364ebf06778b682b4f03df9113ea60e
Peter Macko <peter.macko@mongodb.com>
2023-04-12 23:20:04 -0400

    Small fix.

--- src/block/block_compact.c
+++ src/block/block_compact.c
@@ -382,26 +382,26 @@ __wt_block_compact_progress(WT_SESSION_IMPL *session, WT_BLOCK *block, u_int *ms
     if (time_diff / WT_PROGRESS_MSG_PERIOD > *msg_countp) {
         ++*msg_countp;
 
-        if (block->compact_pages_rewritten_expected == 0)
-            __block_compact_estimate_remaining_work(session, block);
-
         /*
-         * If we don't have the estimate at this point, it means that there wasn't enough work done
-         * to justify having an estimate, which should be extremely rare at this point. So it should
-         * be okay for us to skip the message in that case.
+         * If we don't have the estimate at this point, it means that we haven't reviewed even
+         * enough pages. This should almost never happen.
          */
-        if (block->compact_pages_rewritten_expected == 0)
+        if (block->compact_pages_rewritten_expected == 0) {
+            __wt_verbose_debug1(session, WT_VERB_COMPACT_PROGRESS,
+              "compacting %s for %" PRIu64 " seconds; reviewed %" PRIu64
+              " pages, rewritten %" PRIu64 " pages (%" PRIu64 "MB)",
+              block->name, time_diff, block->compact_pages_reviewed, block->compact_pages_rewritten,
+              block->compact_bytes_rewritten / WT_MEGABYTE);
             __wt_verbose_debug1(session, WT_VERB_COMPACT,
-              "%s: still collecting information for estimating the total runtime", block->name);
-        else {
+              "%s: still collecting information for estimating the progress", block->name);
+        } else {
             progress = WT_MIN(
               (int)(100 * block->compact_pages_rewritten / block->compact_pages_rewritten_expected),
               100);
             __wt_verbose_debug1(session, WT_VERB_COMPACT_PROGRESS,
-              "compacting %s for %" PRIu64 " seconds; reviewed %" PRIu64 " pages (%" PRIu64
-              "MB), rewritten %" PRIu64 " pages (%" PRIu64 "MB), approx. %d%% done",
-              block->name, time_diff, block->compact_pages_reviewed,
-              block->compact_bytes_reviewed / WT_MEGABYTE, block->compact_pages_rewritten,
+              "compacting %s for %" PRIu64 " seconds; reviewed %" PRIu64
+              " pages, rewritten %" PRIu64 " pages (%" PRIu64 "MB), approx. %d%% done",
+              block->name, time_diff, block->compact_pages_reviewed, block->compact_pages_rewritten,
               block->compact_bytes_rewritten / WT_MEGABYTE, progress);
         }
     }
