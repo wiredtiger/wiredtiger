07a0be746803804f00856fb19b57680bba824969
Monica Ng <monica.ng@mongodb.com>
2023-04-19 03:18:02 +0000

    Minor improvements to Python test

--- test/suite/test_checkpoint_snapshot05.py
+++ test/suite/test_checkpoint_snapshot05.py
@@ -148,12 +148,12 @@ class test_checkpoint_snapshot05(wttest.WiredTigerTestCase):
             ckpt.start()
             
             # Wait for checkpoint to start and acquire its snapshot before committing.
-            ckpt_started = 0
-            while not ckpt_started:
-                stat_cursor = self.session.open_cursor('statistics:', None, None)
-                ckpt_started = stat_cursor[stat.conn.txn_checkpoint_snapshot_acquired][2]
-                stat_cursor.close()
+            ckpt_snapshot = 0
+            while not ckpt_snapshot:
                 time.sleep(1)
+                stat_cursor = self.session.open_cursor('statistics:', None, None)
+                ckpt_snapshot = stat_cursor[stat.conn.txn_checkpoint_snapshot_acquired][2]
+                stat_cursor.close()
 
             session1.commit_transaction()
             self.evict(self.uri, ds, self.nrows)
