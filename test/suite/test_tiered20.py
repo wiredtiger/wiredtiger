2385a7b2570baa5a56b4f93b2175ea00067a9ea7
Don Anderson <dda@ddanderson.com>
2023-05-01 14:14:30 -0400

    Use early return to make easier to read. Put code that's only need for dirstore in an 'if'. Removed duplicated line. Removed commented out debug code. Fix capitalization and errors in "main" function.

--- test/suite/test_tiered20.py
+++ test/suite/test_tiered20.py
@@ -80,109 +80,108 @@ class test_tiered20(TieredConfigMixin, wttest.WiredTigerTestCase):
 
         # For tiered scenarios only, we have specific tests that deal with files that
         # have been shared to the bucket.
-        if self.is_tiered_scenario():
-            # we should be able to drop locally (not removing shared), and then
-            # we should get an error if we create again.  Having an error protects us
-            # from clobbering objects in a bucket that might apply to a previous test
-            # or run.
+        if not self.is_tiered_scenario():
+            return
 
-            # For this test, we don't clean up what's in the cloud, only clean up locally.
-            self.create_flush_drop(uri_c, False)
+        # We should be able to drop locally (not removing shared), and then
+        # we should get an error if we create again.  Having an error protects us
+        # from clobbering objects in a bucket that might apply to a previous test
+        # or run.
 
-            # Now, we're creating the same URI.  Even though there should be no local
-            # files or metadata, it should detect existing items in the cloud.
-            # We expect the tiered create will return an EEXIST, with no output message.
-            expected_errno = os.strerror(errno.EEXIST)
-            self.assertRaisesException(wiredtiger.WiredTigerError,
-                lambda: self.create_flush_drop(uri_c, False), expected_errno)
+        # For this test, we don't clean up what's in the cloud, only clean up locally.
+        self.create_flush_drop(uri_c, False)
 
-            # Now, create another WT home directory and link the buckets together.
-            # This emulates multiple systems sharing the same AWS bucket.
-            # We want to make sure that there is no way to have a collision when
-            # tiered tables are created, even when they are of the same name and prefix.
-            #
-            # In a production setup, multiple systems should never have the same prefix,
-            # but we don't want to rely on that.  Once an object is written to a bucket
-            # it should never be overwritten.  It can only be removed (and yes, then
-            # the name would be available in the storage)
-            os.mkdir('SECOND')
-            session1 = self.session  # save the first session
-            conn2 = self.setUpConnectionOpen('SECOND')
-            session2 = self.setUpSessionOpen(conn2)
+        # Now, we're creating the same URI.  Even though there should be no local
+        # files or metadata, it should detect existing items in the cloud.
+        # We expect the tiered create will return an EEXIST, with no output message.
+        expected_errno = os.strerror(errno.EEXIST)
+        self.assertRaisesException(wiredtiger.WiredTigerError,
+            lambda: self.create_flush_drop(uri_c, False), expected_errno)
+
+        # Now, create another WT home directory and link the buckets together.
+        # This emulates multiple systems sharing the same AWS bucket.
+        # We want to make sure that there is no way to have a collision when
+        # tiered tables are created, even when they are of the same name and prefix.
+        #
+        # In a production setup, multiple systems should never have the same prefix,
+        # but we don't want to rely on that.  Once an object is written to a bucket
+        # it should never be overwritten.  It can only be removed (and yes, then
+        # the name would be available in the storage)
+        os.mkdir('SECOND')
+        session1 = self.session  # save the first session
+        conn2 = self.setUpConnectionOpen('SECOND')
+        session2 = self.setUpSessionOpen(conn2)
+
+        if self.is_local_storage:   # dir_store
             os.rmdir('SECOND/bucket1')
             os.symlink('../bucket1', 'SECOND/bucket1')
 
-            # Create URIs on each connection (as if on two systems).
-            # This should not conflict, as nothing has been pushed to the shared storage.
-            session1.create(uri_d, "key_format=S,value_format=S")
-            session2.create(uri_d, "key_format=S,value_format=S")
-            c1 = session1.open_cursor(uri_d)
-            c1["a"] = "a"
-            c1.close()
-            c2 = session2.open_cursor(uri_d)
-            c2["a"] = "SOMETHING_VERY_DIFFERENT!" * 1000
-            c2.close()
-            session1.checkpoint()
-            session2.checkpoint()
+        # Create URIs on each connection (as if on two systems).
+        # This should not conflict, as nothing has been pushed to the shared storage.
+        session1.create(uri_d, "key_format=S,value_format=S")
+        session2.create(uri_d, "key_format=S,value_format=S")
+        c1 = session1.open_cursor(uri_d)
+        c1["a"] = "a"
+        c1.close()
+        c2 = session2.open_cursor(uri_d)
+        c2["a"] = "SOMETHING_VERY_DIFFERENT!" * 1000
+        c2.close()
+        session1.checkpoint()
+        session2.checkpoint()
 
-            # Make sure the file systems in the first connection.
-            self.assertTrue(os.path.exists(uri_d_local_file1))
+        # Make sure the file systems in the first connection.
+        self.assertTrue(os.path.exists(uri_d_local_file1))
 
-            # The first flush from the first "system" should succeed
-            session1.checkpoint('flush_tier=(enabled,force=true)')
-            if self.is_local_storage:   # dir_store
-                uri_d_shared_file1 = os.path.join('bucket1', 'pfx_' + uri_d_local_file1)
-                uri_d_second_file1 = os.path.join('SECOND', uri_d_local_file1)
+        # The first flush from the first "system" should succeed
+        session1.checkpoint('flush_tier=(enabled,force=true)')
+        if self.is_local_storage:   # dir_store
+            uri_d_shared_file1 = os.path.join('bucket1', 'pfx_' + uri_d_local_file1)
+            uri_d_second_file1 = os.path.join('SECOND', uri_d_local_file1)
 
-                #os.system('ls -l bucket1')
-                uri_d_shared_file1 = os.path.join('bucket1', 'pfx_' + uri_d_local_file1)
+            self.assertTrue(os.path.exists(uri_d_shared_file1))
+            self.assertTrue(filecmp.cmp(uri_d_local_file1, uri_d_shared_file1))
 
-                self.assertTrue(os.path.exists(uri_d_shared_file1))
-                self.assertTrue(filecmp.cmp(uri_d_local_file1, uri_d_shared_file1))
+        # We should be able to do this part of the test for any tiered scenario,
+        # not just dir_store.  Remove this 'if' and comment when FIXME-WT-11004 is finished.
+        if self.is_local_storage:
+            # The second flush from the other "system" should detect the conflict.
+            # Normally such a failure would crash Python, but we've changed our
+            # configuration such that we continue after the fail to write.
+            # We'll check for the error message we expect in the error output.
+            with self.expectedStderrPattern(expected_errno):
+                session2.checkpoint('flush_tier=(enabled,force=true)')
 
-            # We should be able to do this part of the test for any tiered scenario,
-            # not just dir_store.  Remove this 'if' and comment when FIXME-WT-11004 is finished.
-            if self.is_local_storage:
-                # The second flush from the other "system" should detect the conflict.
-                # Normally such a failure would crash Python, but we've changed our
-                # configuration such that we continue after the fail to write.
-                # We'll check for the error message we expect in the error output.
-                with self.expectedStderrPattern(expected_errno):
-                    session2.checkpoint('flush_tier=(enabled,force=true)')
+        # At this point, in dir_store, we can verify that the original copy
+        # is still in place, and that the version from the second directory
+        # did not overwrite it.
+        if self.is_local_storage:   # dir_store
+            self.assertTrue(os.path.exists(uri_d_shared_file1))
+            self.assertFalse(filecmp.cmp(uri_d_second_file1, uri_d_shared_file1))
+            self.assertTrue(filecmp.cmp(uri_d_local_file1, uri_d_shared_file1))
 
-            # At this point, in dir_store, we can verify that the original copy
-            # is still in place, and that the version from the second directory
-            # did not overwrite it.
-            if self.is_local_storage:   # dir_store
-                #os.system('ls -l bucket1')
-                #os.system('ls -l {} SECOND/{} bucket1/{}'.format(uri_d_local_file1, uri_d_local_file1, 'pfx_' + uri_d_local_file1))
-                self.assertTrue(os.path.exists(uri_d_shared_file1))
-                self.assertFalse(filecmp.cmp(uri_d_second_file1, uri_d_shared_file1))
-                self.assertTrue(filecmp.cmp(uri_d_local_file1, uri_d_shared_file1))
+        # We're done with the second connection.
+        session2.close()
+        conn2.close()
 
-            # We're done with the second connection.
-            session2.close()
-            conn2.close()
+        # We'd like to enable the following code to make sure the local file gets removed.
+        # Removing the local file guarantees that we'd go to the cloud to get the data.
+        # However, this loop appears to last forever, it seems to keep the
+        # file around because the file handle is in use.  Even inserting a
+        # self.reopen_conn() into the loop does not fix it.  FIXME-WT-10995.
+        #
+        #    # Make sure the local file is removed, so we actually will go to the
+        #    # cloud the next time this URI is accessed.
+        #    while os.path.exists(uri_d_local_file1):
+        #        self.pr('sleeping...')
+        #        time.sleep(1)
+        #    self.pr('{}: file is removed, continuing'.format(uri_d_local_file1))
 
-            # We'd like to enable the following code to make sure the local file gets removed.
-            # Removing the local file guarantees that we'd go to the cloud to get the data.
-            # However, this loop appears to last forever, it seems to keep the
-            # file around because the file handle is in use.  Even inserting a
-            # self.reopen_conn() into the loop does not fix it.  FIXME-WT-10995.
-            #
-            #    # Make sure the local file is removed, so we actually will go to the
-            #    # cloud the next time this URI is accessed.
-            #    while os.path.exists(uri_d_local_file1):
-            #        self.pr('sleeping...')
-            #        time.sleep(1)
-            #    self.pr('{}: file is removed, continuing'.format(uri_d_local_file1))
-
-            # Meanwhile, the first system should not have any trouble accessing
-            # the data via the cloud.
-            self.reopen_conn()
-            c1 = self.session.open_cursor(uri_d)
-            self.assertEqual(c1["a"], "a")
-            c1.close()
+        # Meanwhile, the first system should not have any trouble accessing
+        # the data via the cloud.
+        self.reopen_conn()
+        c1 = self.session.open_cursor(uri_d)
+        self.assertEqual(c1["a"], "a")
+        c1.close()
 
 if __name__ == '__main__':
-    wttest.run
+    wttest.run()
