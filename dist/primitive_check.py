719db21d903f034c2cb8651af81b4272ad4cfbae
Luke Pearson <luke.pearson@mongodb.com>
2023-05-03 15:33:45 +1000

    Update script

--- dist/primitive_check.py
+++ dist/primitive_check.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # This is a temporary script to detect code changes to WiredTiger primitives.
-# FIXME-WT-10861 Delete this once completed.
+# FIXME-WT-10861 That ticket will introduce a script to replace this one, delete this script when
+# complete.
 import subprocess, re
 primitives = [
     "WT_BARRIER",
@@ -29,14 +30,14 @@ primitives = [
     "FLD_CLR_ATOMIC"
 ]
 
-root = subprocess.run(['git', 'rev-parse', '--show-toplevel'],
-        capture_output=True, text=True).stdout
+command = "git rev-parse --show-toplevel"
+root = subprocess.run(command, capture_output=True, text=True, shell=True).stdout
 
-diff_command="git diff $(git merge-base --fork-point develop) -- src/"
-diff = subprocess.run(diff_command, capture_output=True, shell=True, text=True, cwd=root.strip()).stdout
+command="git diff $(git merge-base --fork-point develop) -- src/"
+diff = subprocess.run(command, capture_output=True, shell=True, text=True, cwd=root.strip()).stdout
 found = False
 found_primitives = []
-start_regex = "(\+|-).*"
+start_regex = "^(\+|-).*"
 for primitive in primitives:
     if (re.search(start_regex + primitive, diff)):
         found_primitives.append(primitive)
@@ -45,5 +46,5 @@ for primitive in primitives:
 if (found):
     print("Code changes made since this branch diverged from develop include the following"
         " concurrency control primitives: " + str(found_primitives))
-    print("If this is correct, this will impact the in progress shared variable review project."
-          " Please reach out accordingly.")
+    print("If you have introduced or removed a primite it will impact the in progress shared"
+          " variable review project. Please reach out accordingly.")
