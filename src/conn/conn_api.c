894aace6d9aa0007f52eb2db7b80c713e5f4ee80
Marc Butler <135883+marcbutler@users.noreply.github.com>
2023-05-10 03:20:22 +0000

    Add configuration to disable skipping internal pages
    Loading of internal pages when the aggrated stop durable timestamp is zero
    may now be enabled through configuration.
    
    Default to false as though in rare circumstances disk space may not be
    recovered, preventing this edge case does not justify the increase in
    startup time.

--- src/conn/conn_api.c
+++ src/conn/conn_api.c
@@ -2955,6 +2955,10 @@ wiredtiger_open(const char *home, WT_EVENT_HANDLER *event_handler, const char *c
     if (cval.val)
         F_SET(conn, WT_CONN_CACHE_CURSORS);
 
+    WT_ERR(__wt_config_gets(session, cfg, "checkpoint_cleanup_skip_internal", &cval));
+    if (cval.val)
+        F_SET(conn, WT_CONN_CKPT_CLEANUP_SKIP_INT);
+
     WT_ERR(__wt_config_gets(session, cfg, "checkpoint_sync", &cval));
     if (cval.val)
         F_SET(conn, WT_CONN_CKPT_SYNC);
