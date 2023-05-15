d8bab49b3d4a31a395bd70f01ac45a97fee33314
Ruby Chen <ruby.chen@mongodb.com>
2023-05-15 01:16:31 +0000

    Adding in fixes for test_backup29

--- test/suite/test_backup29.py
+++ test/suite/test_backup29.py
@@ -41,12 +41,6 @@ class test_backup29(backup_base):
               'close_idle_time=3,close_scan_interval=1),' + \
               'statistics=(fast)'
     create_config = 'allocation_size=512,key_format=i,value_format=S'
-<<<<<<< HEAD
-    # Backup directory name
-    dir='backup.dir'
-    uri = 'test_backup29'
-    uri2 = 'test_other'
-=======
     # Backup directory name. Uncomment if actually taking a backup.
     # dir='backup.dir'
     uri1 = 'test_first'
@@ -57,7 +51,6 @@ class test_backup29(backup_base):
     table2_uri = 'table:' + uri2
     active_uri = 'table:active.wt'
 
->>>>>>> 0dc273cfc... WT-11036 Add test of incremental backup after files are closed (#9132)
     value_base = '-abcdefghijkl'
 
     few = 100
@@ -82,20 +75,6 @@ class test_backup29(backup_base):
         self.pr("block bitmap: " + blocks)
         return blocks
 
-<<<<<<< HEAD
-    def test_backup29(self):
-        os.mkdir(self.dir)
-
-        # Create and populate the table.
-        file_uri = 'file:' + self.uri + '.wt'
-        file2_uri = 'file:' + self.uri2 + '.wt'
-        table_uri = 'table:' + self.uri
-        table2_uri = 'table:' + self.uri2
-        self.session.create(table_uri, self.create_config)
-        self.session.create(table2_uri, self.create_config)
-        c = self.session.open_cursor(table_uri)
-        c2 = self.session.open_cursor(table2_uri)
-=======
     def compare_bitmap(self, orig, new):
         # Compare the bitmaps from the metadata. Once a bit is set, it should never
         # be cleared. But new bits could be set. So the check is only: if the original
@@ -117,19 +96,20 @@ class test_backup29(backup_base):
         self.session.create(self.table2_uri, self.create_config)
         c1 = self.session.open_cursor(self.table1_uri)
         c2 = self.session.open_cursor(self.table2_uri)
->>>>>>> 0dc273cfc... WT-11036 Add test of incremental backup after files are closed (#9132)
         # Only add a few entries.
         self.pr("Write: " + str(self.few) + " initial data items")
         for i in range(1, self.few):
             val = str(i) + self.value_base
-            c[i] = val
+            c1[i] = val
             c2[i] = val
         self.session.checkpoint()
 
         # Take the initial full backup for incremental.
         config = 'incremental=(enabled,granularity=4k,this_id="ID1")'
         bkup_c = self.session.open_cursor('backup:', None, config)
-        self.take_full_backup(self.dir, bkup_c)
+        # Uncomment these lines if actually taking the full backup is helpful for debugging.
+        # os.mkdir(self.dir)
+        # self.take_full_backup(self.dir, bkup_c)
         bkup_c.close()
 
         # Add a lot more data to both tables to generate a filled in block mod bitmap.
@@ -137,19 +117,8 @@ class test_backup29(backup_base):
         self.pr("Write: " + str(self.nentries) + " additional data items")
         for i in range(self.few, self.nentries):
             val = str(i) + self.value_base
-            c[i] = val
+            c1[i] = val
             c2[i] = val
-<<<<<<< HEAD
-        last_i = self.nentries
-        c.close()
-        c2.close()
-        self.session.checkpoint()
-        # Get the block mod bitmap from the file URI.
-        orig_bitmap = self.parse_blkmods(file2_uri)
-        self.pr("CLOSE and REOPEN conn")
-        self.reopen_conn()
-        self.pr("Reopened conn")
-=======
         c1.close()
         c2.close()
         self.session.checkpoint()
@@ -157,59 +126,31 @@ class test_backup29(backup_base):
         self.orig1_bitmap = self.parse_blkmods(self.file1_uri)
         self.orig2_bitmap = self.parse_blkmods(self.file2_uri)
 
->>>>>>> 0dc273cfc... WT-11036 Add test of incremental backup after files are closed (#9132)
 
     def incr_backup_and_validate(self):
         # After reopening we want to open both tables, but only modify one of them for
         # the first checkpoint. Then modify the other table, checkpoint, and then check the
         # that the block mod bitmap remains correct for the other table.
-<<<<<<< HEAD
-        c = self.session.open_cursor(table_uri)
-        c2 = self.session.open_cursor(table2_uri)
-=======
         c1 = self.session.open_cursor(self.table1_uri)
         c2 = self.session.open_cursor(self.table2_uri)
         last_i = self.nentries
->>>>>>> 0dc273cfc... WT-11036 Add test of incremental backup after files are closed (#9132)
 
         # Change one table and checkpoint. Keep the other table clean.
         self.pr("Update only table 1: " + str(last_i))
         val = str(last_i) + self.value_base
-        c[last_i] = val
+        c1[last_i] = val
         self.session.checkpoint()
-<<<<<<< HEAD
-=======
         new1_bitmap = self.parse_blkmods(self.file1_uri)
->>>>>>> 0dc273cfc... WT-11036 Add test of incremental backup after files are closed (#9132)
 
         # Now change the other table and checkpoint again.
         self.pr("Update second table: " + str(last_i))
         c2[last_i] = val
         self.session.checkpoint()
-<<<<<<< HEAD
-        new_bitmap = self.parse_blkmods(file2_uri)
-=======
         new2_bitmap = self.parse_blkmods(self.file2_uri)
->>>>>>> 0dc273cfc... WT-11036 Add test of incremental backup after files are closed (#9132)
 
-        c.close()
+        c1.close()
         c2.close()
 
-<<<<<<< HEAD
-        # Compare the bitmaps from the metadata. Once a bit is set, it should never
-        # be cleared. But new bits could be set. So the check is only: if the original
-        # bitmap has a bit set then the current bitmap must be set for that bit also. 
-        #
-        # First convert both bitmaps to a binary string, accounting for any possible leading
-        # zeroes (that would be truncated off). Then compare bit by bit.
-        orig_bits = bin(int('1'+orig_bitmap, 16))[3:]
-        new_bits = bin(int('1'+new_bitmap, 16))[3:]
-        self.pr("Original bitmap in binary: " + orig_bits)
-        self.pr("Reopened bitmap in binary: " + new_bits)
-        for orig, new in zip(orig_bits, new_bits):
-            if orig != '0':
-                self.assertTrue(new != '0')
-=======
         self.compare_bitmap(self.orig1_bitmap, new1_bitmap)
         self.compare_bitmap(self.orig2_bitmap, new2_bitmap)
 
@@ -258,7 +199,6 @@ class test_backup29(backup_base):
         self.pr("Sweep done")
 
         self.incr_backup_and_validate()
->>>>>>> 0dc273cfc... WT-11036 Add test of incremental backup after files are closed (#9132)
 
 if __name__ == '__main__':
     wttest.run()
