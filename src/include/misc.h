9d19078a69aded4ea3df9abfccd9788888da2150
Monica Ng <monica.ng@mongodb.com>
2023-06-05 05:22:22 +0000

    Remove NULL check

--- src/include/misc.h
+++ src/include/misc.h
@@ -275,22 +275,17 @@
 #define WT_STREQ(s, cs) (sizeof(cs) == 2 ? (s)[0] == (cs)[0] && (s)[1] == '\0' : strcmp(s, cs) == 0)
 
 /*
- * Check if a non-empty string matches a byte sequence of len bytes. Since the macro expects both
- * strings to be of non-zero length in order to be a match, it will return false if len == 0. The
- * bytes argument does not need to be null-terminated.
- *
- * Use an inline function as that tends to be safer and we can check for NULL values.
+ * Check if a non-empty string matches a byte sequence of len bytes. The macro expects both strings
+ * to be of non-zero length in order to be a match. The bytes argument does not need to be
+ * null-terminated. Similar to other string related functions, this is not expected to work on NULL
+ * values.
  */
 #define WT_STRING_MATCH(str, bytes, len) __wt_string_match(str, bytes, len)
 
 static inline bool
 __wt_string_match(const char *str, const char *bytes, size_t len)
 {
-    if (str != NULL && bytes != NULL && len > 0 && (strlen(str) == len) &&
-      (strncmp(str, bytes, len) == 0))
-        return (true);
-    else
-        return (false);
+    return (len > 0 && (strlen(str) == len) && (strncmp(str, bytes, len) == 0));
 }
 
 /*
