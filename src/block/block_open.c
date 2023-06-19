3ecafc2901d3f1b0da44fac105f4fa4a8cf49ed6
Marc Butler <135883+marcbutler@users.noreply.github.com>
2023-06-19 03:37:06 +0000

    Restored accidentally removed ref init

--- src/block/block_open.c
+++ src/block/block_open.c
@@ -196,6 +196,7 @@ __wt_block_open(WT_SESSION_IMPL *session, const char *filename, uint32_t objecti
     WT_ERR(__wt_calloc_one(session, &block));
     WT_ERR(__wt_strdup(session, filename, &block->name));
     block->objectid = objectid;
+    block->ref = 1;
 
     /* If not passed an allocation size, get one from the configuration. */
     if (allocsize == 0) {
