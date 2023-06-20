f5f6cb46244bed933d0a4030137abee6d0682d34
Siddhartha Mahajan <siddhartha.mahajan8899@mongodb.com>
2023-06-20 00:47:53 +0000

    s_all

--- src/btree/bt_page.c
+++ src/btree/bt_page.c
@@ -413,8 +413,10 @@ __wt_page_inmem(WT_SESSION_IMPL *session, WT_REF *ref, const void *image, size_t
      * single call. If the disk image is in allocated memory, start with that.
      */
     if (LF_ISSET(WT_PAGE_DISK_ALLOC)) {
-        /* The page-header's in-memory disk size should not be greater than the buffer memory
-         * passed. */
+        /*
+         * The page-header's in-memory disk size should not be greater than the buffer memory
+         * passed.
+         */
         WT_ASSERT(session, page->dsk->mem_size <= image_alloc_size);
         size = page->dsk_alloc_size = image_alloc_size;
     }
