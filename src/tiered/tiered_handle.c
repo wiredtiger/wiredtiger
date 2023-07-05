6d122ca49f03f6fe2c961ba2ccbb1f66bf7312c3
Susan LoVerso <sue@mongodb.com>
2023-07-05 16:15:15 -0400

    Only cleanup the tiers on close, not free the entire structure.

--- src/tiered/tiered_handle.c
+++ src/tiered/tiered_handle.c
@@ -853,7 +853,7 @@ __wt_tiered_close(WT_SESSION_IMPL *session, WT_TIERED *tiered, bool final)
 {
     __wt_verbose(session, WT_VERB_TIERED, "TIERED_CLOSE: tiered %p called final %d", (void *)tiered,
       (int) final);
-    __tiered_cleanup(session, tiered, final);
+    __tiered_cleanup_tiers(session, tiered, final);
 
     return (__wt_btree_close(session));
 }
