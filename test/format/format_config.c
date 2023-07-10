7e440e2987f2d6a2c9006d8a7fcb3292d5101364
Mick Graham <mick.graham@mongodb.com>
2023-07-10 05:39:51 +0000

    WT-11281 Preventing combinations of configs that can easily cause out of disk space issues

--- test/format/format_config.c
+++ test/format/format_config.c
@@ -51,6 +51,7 @@ static void config_mirrors(void);
 static void config_off(TABLE *, const char *);
 static void config_off_all(const char *);
 static void config_pct(TABLE *);
+static void config_run_length(void);
 static void config_statistics(void);
 static void config_tiered_storage(void);
 static void config_transaction(void);
@@ -500,6 +501,8 @@ config_run(void)
     /* Configure the cache last, cache size depends on everything else. */
     config_cache();
 
+    /* Adjust run length if needed. */
+    config_run_length();
     /*
      * Run-length is configured by a number of operations and a timer.
      *
@@ -1392,6 +1395,47 @@ config_pct(TABLE *table)
     testutil_assert(TV(OPS_PCT_DELETE) + TV(OPS_PCT_INSERT) + TV(OPS_PCT_MODIFY) +
         TV(OPS_PCT_READ) + TV(OPS_PCT_WRITE) ==
       100);
+}
+
+/*
+ * config_run_length --
+ *     Run length configuration.
+ */
+static void
+config_run_length(void)
+{
+    /*
+     * Run-length is configured by a number of operations and a timer.
+     *
+     * If the operation count and the timer are both configured, do nothing. If only the timer is
+     * configured, clear the operations count. If only the operation count is configured, limit the
+     * run to 6 hours. If neither is configured, leave the operations count alone and limit the run
+     * to 30 minutes.
+     *
+     * In other words, if we rolled the dice on everything, do a short run. If we chose a number of
+     * operations but the rest of the configuration means operations take a long time to complete
+     * (for example, a small cache and many worker threads), don't let it run forever.
+     */
+    if (config_explicit(NULL, "runs.timer")) {
+        if (!config_explicit(NULL, "runs.ops"))
+            config_single(NULL, "runs.ops=0", false);
+    } else {
+        if (!config_explicit(NULL, "runs.ops"))
+            config_single(NULL, "runs.timer=30", false);
+        else
+            config_single(NULL, "runs.timer=360", false);
+    }
+
+    /* There are combinations that can cause out of disk space issues. Prevent that if possible.
+     * Due to CONFIG.stress, runs.timer is often considered explicit so override if absolutely
+     * needed.
+     */
+    if (GV(RUNS_TIMER) > 10 && GV(LOGGING) && !GV(LOGGING_REMOVE) && GV(BACKUP) &&
+      GV(OPS_SALVAGE)) {
+        config_single(NULL, "runs.timer=10", true);
+        WARN(
+          "limiting runs.timer=%d as logging=1, backup=1, ops.salvage=1, and logging.remove=0", 10);
+    }
 }
 
 /*
