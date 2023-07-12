d3accdd3498cbbb4a66150ea120a6a479d05c2b5
Chenhao Qu <chenhao.qu@mongodb.com>
2023-07-12 15:09:13 +1000

    Fix compile

--- src/support/hazard.c
+++ src/support/hazard.c
@@ -106,7 +106,7 @@ __wt_hazard_set_func(WT_SESSION_IMPL *session, WT_REF *ref, bool *busyp
         WT_ASSERT(session, session->nhazard == hazard_inuse && hazard_inuse < session->hazard_size);
         hp = &head[hazard_inuse];
         ++hazard_inuse;
-        WT_C_MEMMODEL_ATOMIC_STORE(&session->hazard_inuse, hazrd_inuse, WT_ATOMIC_SEQ_CST);
+        WT_C_MEMMODEL_ATOMIC_STORE(&session->hazard_inuse, hazard_inuse, WT_ATOMIC_SEQ_CST);
     } else {
         WT_ASSERT(session, session->nhazard < hazard_inuse && hazard_inuse <= session->hazard_size);
 
