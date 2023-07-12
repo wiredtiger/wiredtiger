032e8b0258401e8f33a6f519232e8ac84408b1b1
Hari Babu Kommi <haribabu.kommi@mongodb.com>
2023-07-12 13:14:03 +1000

    Do not evict the page if we do not skip anything from that page

--- src/btree/bt_curnext.c
+++ src/btree/bt_curnext.c
@@ -957,7 +957,7 @@ __wt_btcur_next(WT_CURSOR_BTREE *cbt, bool truncating)
               (newpage && cbt->page_obsolete_deleted_count > 0)) {
                 WT_ERR(__wt_page_dirty_and_evict_soon(session, cbt->ref));
                 WT_STAT_CONN_INCR(session, cache_eviction_force_obsolete_delete);
-            } else if (page->type != WT_PAGE_COL_FIX && !cbt->valid_data) {
+            } else if (page->type != WT_PAGE_COL_FIX && total_skipped != 0 && !cbt->valid_data) {
                 __wt_page_evict_soon(session, cbt->ref);
                 WT_STAT_CONN_INCR(session, cache_eviction_force_delete);
             }
