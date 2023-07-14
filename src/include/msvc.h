bd3aa0afc4f568f6e8eec29f60aca89cbb6ec4bc
Chenhao Qu <chenhao.qu@mongodb.com>
2023-07-14 14:27:45 +1000

    Use a full barrier instead of interlocked exchange

--- src/include/msvc.h
+++ src/include/msvc.h
@@ -59,20 +59,19 @@
         }                                             \
     } while (0)
 
-#define WT_C_MEMMODEL_ATOMIC_STORE(ptr, value, memorder)                     \
-    do {                                                                     \
-        switch (memorder) {                                                  \
-        case WT_ATOMIC_RELEASE:                                              \
-        case WT_ATOMIC_ACQ_REL:                                              \
-            WT_BARRIER();                                                    \
-        case WT_ATOMIC_RELAXED:                                              \
-        case WT_ATOMIC_ACQUIRE:                                              \
-            *(ptr) = (value);                                                \
-            break;                                                           \
-        case WT_ATOMIC_SEQ_CST:                                              \
-            _InterlockedExchange64((volatile __int64 *)ptr, (int64_t)value); \
-            break;                                                           \
-        }                                                                    \
+#define WT_C_MEMMODEL_ATOMIC_STORE(ptr, value, memorder) \
+    do {                                                 \
+        switch (memorder) {                              \
+        case WT_ATOMIC_RELEASE:                          \
+        case WT_ATOMIC_ACQ_REL:                          \
+            WT_BARRIER();                                \
+        case WT_ATOMIC_RELAXED:                          \
+        case WT_ATOMIC_ACQUIRE:                          \
+            *(ptr) = (value);                            \
+        case WT_ATOMIC_SEQ_CST:                          \
+            WT_FULL_BARRIER();                           \
+            break;                                       \
+        }                                                \
     } while (0)
 
 #define WT_ATOMIC_FUNC(name, ret, type, s, t)                                                     \
