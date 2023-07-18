c406025694f6dffb982400fb081a89fbf7e05fa3
Will Korteland <will.korteland@mongodb.com>
2023-07-18 21:24:34 +0000

    add comment explaining qsort_r generation

--- src/support/qsort_r.c
+++ src/support/qsort_r.c
@@ -6,5 +6,10 @@
  * See the file LICENSE for redistribution information.
  */
 
+/*
+ * The two variants are almost identical modulo function signatures. Define the macro that causes
+ * the normal version to use signatures compatible with the _r variant, and include the source file
+ * directly. This doesn't hide the "regular" quicksort since this is a different compilation unit.
+ */
 #define WT_QSORT_R_DEF
 #include "qsort.c"
