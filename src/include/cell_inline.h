6fd91f728d0948f3f2d813509abbb9be6cf235da
Monica Ng <monica.ng@mongodb.com>
2023-07-25 05:03:03 +0000

    Add prints in fast-truncate pack 7.0

--- src/include/cell_inline.h
+++ src/include/cell_inline.h
@@ -215,6 +215,10 @@ __wt_cell_pack_addr(WT_SESSION_IMPL *session, WT_CELL *cell, u_int cell_type, ui
         WT_IGNORE_RET(__wt_vpack_uint(&p, 0, page_del->txnid));
         WT_IGNORE_RET(__wt_vpack_uint(&p, 0, page_del->timestamp));
         WT_IGNORE_RET(__wt_vpack_uint(&p, 0, page_del->durable_timestamp));
+
+        printf("7.0 packed fast-truncate page_del->txnid %lu\n", page_del->txnid);
+        printf("7.0 packed fast-truncate page_del->timestamp %lu\n", page_del->timestamp);
+        printf("7.0 packed fast-truncate page_del->durable_timestamp %lu\n", page_del->durable_timestamp);
     }
 
     if (recno == WT_RECNO_OOB)
