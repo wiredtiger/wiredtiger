325b7387deec9415a5ff48df2742975733224b1f
Etienne Petrel <etienne.petrel@mongodb.com>
2023-07-27 04:20:52 +0000

    A->Z

--- src/include/compact.h
+++ src/include/compact.h
@@ -8,11 +8,11 @@
 
 struct __wt_compact_state {
     bool first_pass;            /* First compact iteration */
-    uint32_t lsm_count;         /* Number of LSM trees seen */
     uint32_t file_count;        /* Number of files seen */
+    uint32_t lsm_count;         /* Number of LSM trees seen */
     uint64_t free_space_target; /* Configured minimum space that should be recovered */
     uint64_t max_time;          /* Configured timeout */
-    uint64_t prev_file_size;    /* File size during compaction in B */
+    uint64_t prev_file_size;    /* File size during compaction */
 
     struct timespec begin; /* Starting time */
 };
