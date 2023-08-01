41330b58a4b6c4a3d6d3a638454bd2556a64d1a9
Etienne Petrel <etienne.petrel@mongodb.com>
2023-08-01 13:26:21 +0000

    Revert some changes

--- src/btree/bt_compact.c
+++ src/btree/bt_compact.c
@@ -309,7 +309,6 @@ int
 __wt_compact(WT_SESSION_IMPL *session)
 {
     WT_BM *bm;
-    WT_CONNECTION_IMPL *conn;
     WT_DECL_RET;
     WT_REF *ref;
     u_int i, msg_count;
@@ -321,7 +320,6 @@ __wt_compact(WT_SESSION_IMPL *session)
     uint64_t stats_pages_skipped;            /* Pages skipped */
 
     bm = S2BT(session)->bm;
-    conn = S2C(session);
     msg_count = 0;
     ref = NULL;
 
@@ -371,8 +369,8 @@ __wt_compact(WT_SESSION_IMPL *session)
                 bm->compact_progress(bm, session, &msg_count);
             WT_ERR(__wt_session_compact_check_timeout(session));
             if (session->event_handler->handle_general != NULL) {
-                ret = session->event_handler->handle_general(session->event_handler, &conn->iface,
-                  &session->iface, WT_EVENT_COMPACT_CHECK, NULL);
+                ret = session->event_handler->handle_general(session->event_handler,
+                  &(S2C(session))->iface, &session->iface, WT_EVENT_COMPACT_CHECK, NULL);
                 /* If the user's handler returned non-zero we return WT_ERROR to the caller. */
                 if (ret != 0)
                     WT_ERR_MSG(session, WT_ERROR, "compact interrupted by application");
