b3ef3a5fa9f737d21cfe30d0a4e1962363c1eb90
Ruby Chen <ruby.chen@mongodb.com>
2023-08-03 04:30:55 +0000

    comment changes.

--- src/block_cache/block_chunkcache.c
+++ src/block_cache/block_chunkcache.c
@@ -697,9 +697,9 @@ __wt_chunkcache_setup(WT_SESSION_IMPL *session, const char *cfg[], bool reconfig
 
         WT_RET(chunkcache->fh->handle->fh_map(chunkcache->fh->handle, &session->iface,
           (void **)&chunkcache->memory, &mapped_size, NULL));
-        if (mapped_size == chunkcache->capacity)
+        if (mapped_size != chunkcache->capacity)
             WT_RET_MSG(session, EINVAL,
-              "Storage size %lu does not equal capacity of chunk cache %lu", mapped_size,
+              "Storage size mapping %lu does not equal capacity of chunk cache %lu", mapped_size,
               chunkcache->capacity);
 
         WT_RET(__wt_calloc(session, 1, ((chunkcache->capacity / chunkcache->chunk_size) / 8),
