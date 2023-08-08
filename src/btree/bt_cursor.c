2790e85a1d89861c92bc3d0cb5acf01fffec3f8b
Luke Pearson <luke.pearson@mongodb.com>
2023-08-08 10:28:57 +1000

    Removing braces

--- src/btree/bt_cursor.c
+++ src/btree/bt_cursor.c
@@ -164,9 +164,8 @@ __cursor_page_pinned(WT_CURSOR_BTREE *cbt, bool search_operation)
      * whether we correctly resolved the transaction becomes hard. It is easier to skip this check
      * in that instance.
      */
-    if (cbt->ref->page->read_gen == WT_READGEN_OLDEST && !F_ISSET(session->txn, WT_TXN_PREPARE)) {
+    if (cbt->ref->page->read_gen == WT_READGEN_OLDEST && !F_ISSET(session->txn, WT_TXN_PREPARE))
         return (false);
-    }
 
     return (true);
 }
