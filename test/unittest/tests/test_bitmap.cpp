74bd28804e795a5a0dcc749ef773844b75dd236d
Siddhartha Mahajan <siddhartha.mahajan8899@mongodb.com>
2023-08-10 05:54:36 +0000

    Create free and alloc functions

--- test/unittest/tests/test_bitmap.cpp
+++ test/unittest/tests/test_bitmap.cpp
@@ -11,33 +11,49 @@
 #include "wiredtiger.h"
 #include "wrappers/mock_session.h"
 
+// Mark the free chunk in the bitmap as in use.
+void
+alloc_bitmap(WT_CHUNKCACHE* chunkcache, size_t bit_index)
+{
+    uint8_t *map_byte = &chunkcache->free_bitmap[bit_index / 8];
+    *map_byte = *map_byte | (uint8_t)(0x01 << (bit_index % 8));
+}
+
+// Mark the used chunk in the bitmap as free.
+void
+free_bitmap(WT_CHUNKCACHE* chunkcache, size_t bit_index)
+{
+    uint8_t *map_byte = &chunkcache->free_bitmap[bit_index / 8];
+    *map_byte = *map_byte & ~(0x01 << (bit_index % 8));
+}
+
 TEST_CASE("Chunkcache bitmap: __chunkcache_bitmap_find_free", "[bitmap]")
 {
     // Build Mock session, this will automatically create a mock connection.
     std::shared_ptr<MockSession> session = MockSession::buildTestMockSession();
 
     WT_SESSION_IMPL *session_impl = session->getWtSessionImpl();
-    uint64_t capacity = 1000;
+    uint64_t capacity = 100;
     size_t chunk_size = 10;
+    WT_CHUNKCACHE *chunkcache;
 
     // Setup chunk cache.
     REQUIRE(
-      (session->getMockConnection()->setupChunkCache(session_impl, capacity, chunk_size)) == 0);
+      (session->getMockConnection()->setupChunkCache(session_impl, capacity, chunk_size, chunkcache)) == 0);
 
     SECTION("Free index found in bitmap")
     {
         size_t bit_index;
+
+        // We have 10 chunks so there are 10 bits.
+        for (int i = 0; i < 10; i++)
+            alloc_bitmap(chunkcache, i);
+        REQUIRE(__ut_chunkcache_bitmap_find_free(session_impl, &bit_index) == ENOSPC);
+
+        free_bitmap(chunkcache, 3);
         REQUIRE(__ut_chunkcache_bitmap_find_free(session_impl, &bit_index) == 0);
-        // REQUIRE(bit_index == 0);
+        REQUIRE(bit_index == 3);
 
-        // __chunkcache_bitmap_find_free(session, &bit_index);
-        // REQUIRE(bit_index == 0);
-
-        // map_byte = &chunkcache->free_bitmap[bit_index / 8];
-        // __wt_atomic_cas8(map_byte, *map_byte, *map_byte | (uint8_t)(0x01 << (bit_index % 8)))
-
-        // __chunkcache_bitmap_find_free(session, &bit_index);
-        // REQUIRE(bit_index == 1);
     }
 
     // Edge case of full bitmap - getting correct error
