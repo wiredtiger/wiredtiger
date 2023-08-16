a4f93daa4de6e0e2dd609b352c978dcbba6c0b73
Jie Chen <jie.chen@mongodb.com>
2023-08-16 06:31:25 +0000

    Fix the mirroring problem

--- test/format/format.h
+++ test/format/format.h
@@ -242,8 +242,8 @@ typedef struct {
 
     char *config_open; /* Command-line configuration */
 
-    TABLE *base_mirror;  /* First mirrored table */
-    bool mirror_fix_var; /* Special case if mirroring both FIX and VAR tables */
+    TABLE *base_mirror;    /* First mirrored table */
+    bool mirror_col_store; /* Special case if mirroring both FIX and VAR tables */
 
     RWLOCK backup_lock; /* Backup running */
     uint64_t backup_id; /* Block incremental id */
