f760c7f9fdcf0526124889c552b262eabd0d1ed1
y123456yz <3058078489@qq.com>
2023-08-17 14:20:30 +0800

    Code presentation perfection

--- src/btree/bt_handle.c
+++ src/btree/bt_handle.c
@@ -951,9 +951,10 @@ __btree_page_sizes(WT_SESSION_IMPL *session)
      */
     WT_RET(__wt_config_gets(session, cfg, "memory_page_max", &cval));
     btree->maxmempage = (uint64_t)cval.val;
+    int min_pages_num = 10;
     if (!F_ISSET(conn, WT_CONN_CACHE_POOL) && (cache_size = conn->cache_size) > 0)
         btree->maxmempage = (uint64_t)WT_MIN(
-          btree->maxmempage, (conn->cache->eviction_dirty_trigger * cache_size) / 100);
+          btree->maxmempage, ((conn->cache->eviction_dirty_trigger * cache_size) / 100) / min_pages_num);
 
     /* Enforce a lower bound of a single disk leaf page */
     btree->maxmempage = WT_MAX(btree->maxmempage, btree->maxleafpage);
