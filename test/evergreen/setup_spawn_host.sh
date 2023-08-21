c4b0d64ab6c762f5ce182e11ce456195fe2a997a
Jie Chen <jie.chen@mongodb.com>
2023-08-21 05:30:58 +0000

    Updated issue

--- test/evergreen/setup_spawn_host.sh
+++ test/evergreen/setup_spawn_host.sh
@@ -24,18 +24,18 @@ if [[ -n $WT_ARCHIVE ]]; then
 fi
 
 # Find any avaliable core dumps in build directory.
-GDB_CORE_DUMP=$(ls ${HOME}/wiredtiger/cmake_build/*.core | head -n 1 2>/dev/null)
+GDB_CORE_DUMP=$(find ${HOME}/wiredtiger/cmake_build -name "*.core" | head -n 1 2>/dev/null)
 if [[ -n $GDB_CORE_DUMP ]]; then
     # Grab the source directory that the test was run in, and fix up the expected path to the
     # correct path on the new machine.
     OLD_WT_PATH=readelf -n GDB_CORE_DUMP | grep "wiredtiger" | head -n 1 | sed -e 's/wiredtiger.*/wiredtiger/' -e 's/^[ \t]*//'
-    cat >> ~/.gdbinit <<EOF
+    cat >> ~/.gdbinit << EOF
     set solib-search-path ${HOME}/wiredtiger/cmake_build:${HOME}/wiredtiger/TCMALLOC_LIB/lib
     set substitution-path ${OLD_WT_PATH} ${HOME}/wiredtiger
     set pagination off
     set print pretty on
+EOF
 
-    EOF
 fi
 
 # Install CMake into the machine.
