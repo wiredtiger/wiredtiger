cc3a26f654d90c7455798fc812576382988ba10d
Ravi Giri <ravi.giri@mongodb.com>
2023-09-13 19:15:11 +0000

    Updated code comments

--- src/include/cell_inline.h
+++ src/include/cell_inline.h
@@ -1228,7 +1228,7 @@ __wt_cell_unpack_kv(WT_SESSION_IMPL *session, const WT_PAGE_HEADER *dsk, WT_CELL
 
 /*
  * __wt_cell_get_ta --
- *     Set the time aggregate from an unpacked address cell.
+ *     Get the underlying time aggregate from an unpacked address cell.
  */
 static inline void
 __wt_cell_get_ta(WT_CELL_UNPACK_ADDR *unpack_addr, WT_TIME_AGGREGATE **tap)
@@ -1236,9 +1236,9 @@ __wt_cell_get_ta(WT_CELL_UNPACK_ADDR *unpack_addr, WT_TIME_AGGREGATE **tap)
     *tap = &unpack_addr->ta;
 }
 
-/*
+ /*
  * __wt_cell_get_tw --
- *     Set the time window from an unpacked value cell.
+ *     Get the underlying time window from an unpacked value cell.
  */
 static inline void
 __wt_cell_get_tw(WT_CELL_UNPACK_KV *unpack_value, WT_TIME_WINDOW **twp)
