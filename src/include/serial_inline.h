7c05c22efbcae72f93acf420283a7409e84f8b62
Ruby Chen <ruby.chen@mongodb.com>
2023-09-22 04:42:40 +0000

    Addressing PR comments.

--- src/include/serial_inline.h
+++ src/include/serial_inline.h
@@ -101,7 +101,7 @@ __col_append_serial_func(WT_SESSION_IMPL *session, WT_INSERT_HEAD *ins_head, WT_
   WT_INSERT *new_ins, uint64_t *recnop, u_int skipdepth)
 {
     WT_BTREE *btree;
-    uint64_t recno, local_recno;
+    uint64_t recno, last_recno;
     u_int i;
 
     btree = S2BT(session);
@@ -128,8 +128,9 @@ __col_append_serial_func(WT_SESSION_IMPL *session, WT_INSERT_HEAD *ins_head, WT_
      */
     *recnop = recno;
     do {
-        local_recno = *((volatile uint64_t *)(&btree->last_recno));
-    } while ((recno > local_recno) && !__wt_atomic_cas64(&btree->last_recno, local_recno, recno));
+        /* Ensure we only read the value once. */
+        last_recno = *((volatile uint64_t *)(&btree->last_recno));
+    } while ((recno > last_recno) && !__wt_atomic_cas64(&btree->last_recno, last_recno, recno));
 
     return (0);
 }
