89280f5d7c5bbd1ee0b2413506a65d33a2316416
Andrew Morton <ajmorton2@gmail.com>
2023-09-26 14:55:08 +1000

    Revert "Add note to WT_ORDERED_READ"
    This reverts commit 3a783132ad2ac9657c3fe472a2ab3537d3f88233.

--- src/include/hardware.h
+++ src/include/hardware.h
@@ -75,9 +75,7 @@
     } while (0)
 
 /*
- * Read a shared location and guarantee that subsequent reads do not see any earlier state. This
- * macro will be gradually replaced by WT_READ_ONCE and WT_READ_ORDERED. If you are looking to use
- * this macro please consider the above two first.
+ * Read a shared location and guarantee that subsequent reads do not see any earlier state.
  */
 #define WT_ORDERED_READ(v, val) \
     do {                        \
