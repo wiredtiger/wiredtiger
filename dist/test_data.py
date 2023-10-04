ab2efa124ba37dc882f26edc3413e0ceb9732361
Etienne Petrel <etienne.petrel@mongodb.com>
2023-10-04 22:26:25 +0000

    Disable the debug mode for bg compaction by default

--- dist/test_data.py
+++ dist/test_data.py
@@ -228,7 +228,7 @@ test_config = [
         type='category', subconfig=operation_tracker),
 
 # Non component top level configuration.
-    Config('background_compact_debug_mode', 'true', r'''
+    Config('background_compact_debug_mode', 'false', r'''
         If true, background compact aggressively removes compact statistics for a file and decreases
         the max amount of time a file can be skipped for.''', type='boolean'),
     Config('cache_max_wait_ms', 0, r'''
