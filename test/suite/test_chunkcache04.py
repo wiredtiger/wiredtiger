53646ffa88c7670c4c1bb895ea6c590ef6d0b6a2
Ruby Chen <ruby.chen@mongodb.com>
2023-10-11 22:46:08 +0000

    Creating new test for ingest functionality and pinning.

---
+++ test/suite/test_chunkcache04.py
@@ -0,0 +1,139 @@
+#!/usr/bin/env python3
+#
+# Public Domain 2014-present MongoDB, Inc.
+# Public Domain 2008-2014 WiredTiger, Inc.
+#
+# This is free and unencumbered software released into the public domain.
+#
+# Anyone is free to copy, modify, publish, use, compile, sell, or
+# distribute this software, either in source code form or as a compiled
+# binary, for any purpose, commercial or non-commercial, and by any
+# means.
+#
+# In jurisdictions that recognize copyright laws, the author or authors
+# of this software dedicate any and all copyright interest in the
+# software to the public domain. We make this dedication for the benefit
+# of the public at large and to the detriment of our heirs and
+# successors. We intend this dedication to be an overt act of
+# relinquishment in perpetuity of all present and future rights to this
+# software under copyright law.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+
+import os, sys
+import wiredtiger, wttest
+
+from wtdataset import SimpleDataSet
+from wtscenario import make_scenarios
+from time import sleep
+
+
+'''
+- Functional testing for the ingesting. Verify ingests are taking place with both pinned and unpinned chunks.
+- Verify that when ingesting new chunks with old pinned objects, we are releasing the pin on the old objects. 
+'''
+class test_chunkcache4(wttest.WiredTigerTestCase):
+    rows = 10000
+
+    format_values = [
+        ('column', dict(key_format='r', value_format='S')),
+        ('row_string', dict(key_format='S', value_format='S')),
+    ]
+
+    cache_types = [('in-memory', dict(chunk_cache_type='DRAM'))]
+    if sys.byteorder == 'little':
+        # WT's filesystem layer doesn't support mmap on big-endian platforms.
+        cache_types.append(('on-disk', dict(chunk_cache_type='FILE')))
+
+    scenarios = make_scenarios(format_values, cache_types)
+
+    def conn_config(self):
+        if not os.path.exists('bucket2'):
+            os.mkdir('bucket2')
+
+        return 'tiered_storage=(auth_token=Secret,bucket=bucket2,bucket_prefix=pfx_,name=dir_store),' \
+            'chunk_cache=[enabled=true,chunk_size=512KB,capacity=20MB,pinned=("table:chunkcache01", "table:chunkcache02"),type=DRAM,storage_path=WiredTigerChunkCache]'
+
+    def conn_extensions(self, extlist):
+        if os.name == 'nt':
+            extlist.skip_if_missing = True
+        extlist.extension('storage_sources', 'dir_store')
+
+    def get_stat(self, stat):
+        stat_cursor = self.session.open_cursor('statistics:')
+        val = stat_cursor[stat][2]
+        stat_cursor.close()
+        return val
+
+    def insert(self, uri, ds):
+        cursor = self.session.open_cursor(uri)
+        for i in range(1, self.rows):
+            cursor[ds.key(i)] = str(i) * 100
+    
+    def read_and_verify(self, uri, ds):
+        cursor = self.session.open_cursor(uri)
+        for i in range(1, self.rows):
+            cursor.set_key(ds.key(i))
+            cursor.search()
+            self.assertEqual(cursor.get_value(), str(i) * 100)
+
+    def test_chunkcache04(self):
+        uris = ["table:chunkcache03", "table:chunkcache04"]
+        ds = [SimpleDataSet(self, uri, 0, key_format=self.key_format, value_format=self.value_format) for uri in uris]
+
+        # Insert unpinned data into two tables. 
+        for i, dataset in enumerate(ds):
+            dataset.populate()
+            self.insert(uris[i], dataset)
+
+        # As we have not flushed yet, assert we have no newly inserted chunks. 
+        self.assertEqual(self.get_stat(wiredtiger.stat.conn.chunk_cache_newly_inserted), 0)
+        
+        self.session.checkpoint()
+        self.session.checkpoint('flush_tier=(enabled)')
+
+        # Assert that chunks are not pinned. 
+        self.assertEqual(self.get_stat(wiredtiger.stat.conn.chunk_cache_chunks_pinned), 0)
+
+        # Assert the new chunks are ingested. 
+        unpinned_ingest = self.get_stat(wiredtiger.stat.conn.chunk_cache_newly_inserted)
+        self.assertGreater(unpinned_ingest, 0)
+        
+        uris = ["table:chunkcache01", "table:chunkcache02"]
+        ds = [SimpleDataSet(self, uri, 0, key_format=self.key_format, value_format=self.value_format) for uri in uris]
+
+        # Insert pinned data into two tables.
+        for i, dataset in enumerate(ds):
+            dataset.populate()
+            self.insert(uris[i], dataset)
+
+        self.session.checkpoint()
+        self.session.checkpoint('flush_tier=(enabled)')
+
+        # Assert the new chunks are ingested.
+        pinned_ingest = self.get_stat(wiredtiger.stat.conn.chunk_cache_newly_inserted)
+        self.assertGreater(pinned_ingest, unpinned_ingest)
+
+        # Assert that chunks are pinned.
+        old_pinned = self.get_stat(wiredtiger.stat.conn.chunk_cache_chunks_pinned)
+        self.assertGreater(old_pinned, 0)
+
+        # Flush the pinned chunks
+        self.session.checkpoint()
+        self.session.checkpoint('flush_tier=(enabled)')
+
+        # Assert the pinned chunks were unset after flush.
+        self.assertGreaterEqual(old_pinned, self.get_stat(wiredtiger.stat.conn.chunk_cache_chunks_pinned))
+        
+        # Reopen wiredtiger to migrate all data to disk.
+        self.reopen_conn()
+
+        # Assert none of the chunks are pinned and newly ingested chunks are cleared. 
+        self.assertEqual(self.get_stat(wiredtiger.stat.conn.chunk_cache_chunks_pinned), 0)
+        self.assertEqual(self.get_stat(wiredtiger.stat.conn.chunk_cache_newly_inserted), 0)
