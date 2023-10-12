f80d1c5e1af29d9c3f38c6c398616071c10edc02
Clarisse Cheah <clarisse.cheah@mongodb.com>
2023-10-12 02:28:46 +0000

    Initial changes

--- src/session/session_prefetch.c
+++ src/session/session_prefetch.c
@@ -10,54 +10,26 @@
 
 /*
  * __wt_session_prefetch_check --
- *     Check to see whether cursors owned by this session might benefit from doing pre-fetch
- *
- * FIXME-WT-11758 Change this function so that when pre-fetching is enabled on the session level,
- *     all cursors associated with that session with automatically perform pre-fetching. Use the
- *     session-level configuration once it is introduced.
+ *     Turn on prefetching for all cursors within a session if pre-fetching is configured on the
+ *     session.
  */
 bool
 __wt_session_prefetch_check(WT_SESSION_IMPL *session, WT_REF *ref)
 {
-    /* Internal threads should not be configured to do pre-fetching. */
-    if (!S2C(session)->prefetch_auto_on || F_ISSET(session, WT_SESSION_INTERNAL))
-        return (false);
-
-    if (S2C(session)->prefetch_queue_count > WT_MAX_PREFETCH_QUEUE)
-        return (false);
-
     /*
-     * Don't deal with internal pages at the moment - finding the right content to preload based on
-     * internal pages is hard.
+     * Check if pre-fetching is enabled on the session level. If not, don't perform pre-fetching on
+     * any of the associated cursors. We don't perform pre-fetching on internal threads or internal
+     * pages either (finding the right content to preload based on internal page is hard), so check
+     * for those here too.
      */
-    if (F_ISSET(ref, WT_REF_FLAG_INTERNAL))
+    if (!F_ISSET(session, WT_SESSION_PREFETCH) || F_ISSET(session, WT_SESSION_INTERNAL) ||
+      F_ISSET(ref, WT_REF_FLAG_INTERNAL)) {
+        WT_STAT_CONN_INCR(session, block_prefetch_skipped);
         return (false);
+    }
 
     if (session->pf.prefetch_disk_read_count == 1)
         WT_STAT_CONN_INCR(session, block_prefetch_disk_one);
-
-    /* A single read from disk is common - don't use it to guide pre-fetch behavior. */
-    if (session->pf.prefetch_disk_read_count < 2) {
-        WT_STAT_CONN_INCR(session, block_prefetch_skipped);
-        return (false);
-    }
-
-    if (session->pf.prefetch_prev_ref == NULL) {
-        WT_STAT_CONN_INCR(session, block_prefetch_attempts);
-        return (true);
-    }
-
-    /*
-     * If the previous pre-fetch was using the same home ref, pre-fetch for approximately the number
-     * of pages that were added to the queue.
-     */
-    if (session->pf.prefetch_prev_ref->page == ref->home &&
-      session->pf.prefetch_skipped_with_parent < WT_PREFETCH_QUEUE_PER_TRIGGER) {
-        ++session->pf.prefetch_skipped_with_parent;
-        WT_STAT_CONN_INCR(session, block_prefetch_skipped);
-        return (false);
-    }
-    session->pf.prefetch_skipped_with_parent = 0;
 
     WT_STAT_CONN_INCR(session, block_prefetch_attempts);
     return (true);
