dc10a666d95dccee008a991916cc91266ace129e
y123456yz <3058078489@qq.com>
2023-10-17 22:50:15 +0800

    remove unuse code

--- examples/c/ex_debug_tree.c
+++ examples/c/ex_debug_tree.c
@@ -41,9 +41,6 @@ debug_tree_example(void)
     WT_CONNECTION *conn;
     WT_CURSOR *cursor;
     WT_SESSION *session;
-//    const char *key, *value;
-//    int ret;
-    char buf[1024];
     int i;
     WT_ITEM value_item;
     WT_BTREE *btree;
@@ -69,9 +66,7 @@ debug_tree_example(void)
     for (i = 0; i < MAX_TEST_KV_NUM; i++) {
         cursor->set_key(cursor, i);
 
-        memset(buf, 0, sizeof(buf));
-        snprintf(buf, sizeof(buf), "old value%d #####################################################", i);
-        value_item.data = buf;
+        value_item.data = "old value #####################################################";
         value_item.size = strlen(value_item.data);
         
         cursor->set_value(cursor, &value_item);
@@ -82,8 +77,7 @@ debug_tree_example(void)
     for (i = 0; i < MAX_TEST_KV_NUM; i++) {
         cursor->set_key(cursor, i);
 
-        snprintf(buf, sizeof(buf), "new value%d #####################################################", i);
-        value_item.data = buf;
+        value_item.data = "new value #####################################################";
         value_item.size = strlen(value_item.data);
         cursor->set_value(cursor, &value_item);
         error_check(cursor->update(cursor));
