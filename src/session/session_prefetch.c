04084f09d4f39f5cd8d6d2d1b4cb4f537d24b9b5
Clarisse Cheah <clarisse.cheah@mongodb.com>
2023-10-17 03:59:13 +0000

    Address review comments

--- src/session/session_prefetch.c
+++ src/session/session_prefetch.c
@@ -10,8 +10,7 @@
 
 /*
  * __wt_session_prefetch_check --
- *     Turn on prefetching for all cursors within a session if pre-fetching is configured on the
- *     session.
+ *     Check if pre-fetching is configured on the session.
  */
 bool
 __wt_session_prefetch_check(WT_SESSION_IMPL *session, WT_REF *ref)
@@ -19,10 +18,10 @@ __wt_session_prefetch_check(WT_SESSION_IMPL *session, WT_REF *ref)
     if (S2C(session)->prefetch_queue_count > WT_MAX_PREFETCH_QUEUE)
         return (false);
     /*
-     * Check if pre-fetching is enabled on the session level. If not, don't perform pre-fetching on
-     * any of the associated cursors. We don't perform pre-fetching on internal threads or internal
-     * pages either (finding the right content to preload based on internal page is hard), so check
-     * for those here too.
+     * Check if pre-fetching is enabled on the session level. We don't perform pre-fetching on
+     * internal threads or internal pages (finding the right content to preload based on internal
+     * pages is hard), so check for that too. The result of this check will be used by cursor logic
+     * to determine if pre-fetching will be performed.
      */
     if (!F_ISSET(session, WT_SESSION_PREFETCH) || F_ISSET(session, WT_SESSION_INTERNAL) ||
       F_ISSET(ref, WT_REF_FLAG_INTERNAL)) {
@@ -30,6 +29,7 @@ __wt_session_prefetch_check(WT_SESSION_IMPL *session, WT_REF *ref)
         return (false);
     }
 
+    session->pf.prefetch_skipped_with_parent = 0;
     if (session->pf.prefetch_disk_read_count == 1)
         WT_STAT_CONN_INCR(session, block_prefetch_disk_one);
 
