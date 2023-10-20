0c539e670364d75b4ddbcd8c7c74a6f0a58e160e
Etienne Petrel <etienne.petrel@mongodb.com>
2023-10-20 03:31:35 +0000

    Remove unused packages

--- test/suite/test_compact10.py
+++ test/suite/test_compact10.py
@@ -26,7 +26,7 @@
 # ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 # OTHER DEALINGS IN THE SOFTWARE.
 
-import os, shutil, time, wiredtiger, wttest
+import os, time, wttest
 from wiredtiger import stat
 from wtbackup import backup_base
 
