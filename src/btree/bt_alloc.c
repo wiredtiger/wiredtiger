26828793bb22ca8f1332617990c6d493f22351cf
Marc Butler <135883+marcbutler@users.noreply.github.com>
2023-10-23 16:53:24 +1100

    interim push for share

--- src/btree/bt_alloc.c
+++ src/btree/bt_alloc.c
@@ -122,21 +122,27 @@ __wt_upd_free(WT_SESSION_IMPL *session, WT_PAGE *page, WT_UPDATE **updp)
 
 /* ---------------------------------------------------------------------------- */
 
-/* potentially overkill consider winding down to 1024 */
+/* use a single vm page -- though potentially overkill consider winding down to 1024 */
 #define BT_ARENA_BITMAP_SIZE (4096u * 8u)
 #define BT_ARENA_BITMAP_SIZE_BYTES ((BT_ARENA_BITMAP_SIZE) >> 3)
+
+#define BT_ARENA_PAGE_SIZE (4096u)
 
 /* This will need to hang off the btree somewhere. */
 struct bt_arena {
     /* virtual memory */
     size_t vsize;
-    void  *vmem;
+    uintptr_t vmem;
 
     /* concurrency protection? */
 
-    uint32_t region_count;                          /* used to infer maximal region size */
-    size_t largest;                                 /* size of largest region in bytes */
-    uint8_t region_map[BT_ARENA_BITMAP_SIZE_BYTES]; /* bitmap of utilized regions */
+    size_t region_max;          /* maximal region size */
+    size_t region_largest;      /* size of largest region in bytes */
+    uint32_t region_count;      /* used to infer maximal region size */
+    uint32_t region_highest;    /* highest bit offset for a region */
+
+    /* bitmap of utilized regions 0 = used, 1 = unused */
+    _Alignas(size_t) uint8_t region_map[BT_ARENA_BITMAP_SIZE_BYTES];
 };
 
 /*
@@ -151,7 +157,8 @@ struct bt_region_header {
     uint32_t pad1_;
     uint32_t offset;
     uint32_t pad2_;
-};
+} __attribute__((packed));
+
 
 int bt_arena_ctor(struct bt_arena *arena, size_t vmem_size);
 int bt_arena_dtor(struct bt_arena *arena);
@@ -163,15 +170,116 @@ int bt_arena_page_free(struct bt_arena *arena, WT_PAGE *page);
 int bt_arena_zalloc(struct bt_arena *arena, size_t alloc_size, WT_PAGE *page, void **mem_pp);
 int bt_arena_free(struct bt_arena *arena, size_t alloc_size, WT_PAGE *page, void *mem_p);
 
+
+static uint32_t next_highest_pow2(uint32_t v)
+{
+    v--;
+    v |= v >> 1;
+    v |= v >> 2;
+    v |= v >> 4;
+    v |= v >> 8;
+    v |= v >> 16;
+    v++;
+    v += (v == 0);
+    return v;
+}
+
+
 int bt_arena_ctor(struct bt_arena *arena, size_t vmem_size)
 {
+    size_t actual_size;
+    void *vmem;
+
     if (arena == NULL || vmem_size == 0) {
         return EINVAL;
     }
+
     /* reserve virtual memory */
+    actual_size = (vmem_size + BT_ARENA_PAGE_SIZE) & (BT_ARENA_PAGE_SIZE - 1);
+    vmem = mmap(NULL, actual_size, PROT_READ|PROT_WRITE, MAP_ANONYMOUS|MAP_PRIVATE, -1, 0);
+    if (vmem == MAP_FAILED) {
+        return errno;
+    }
+
     /* initialize bitmap */
+    memset(arena->region_map, 0xff, sizeof(arena->region_map));
+
     /* initialize arena */
+    arena->vsize = actual_size;
+    arena->vmem = (uintptr_t)vmem;
+    arena->region_max = actual_size;
+    arena->region_largest = 0;
+    arena->region_highest = 0;
+    arena->region_count = 0;
+    return 0;
+}
+
+
+int bt_arena_dtor(struct bt_arena *arena)
+{
+    int retval;
+
+    if (arena == NULL) {
+        return EINVAL;
+    }
+    /* iteratively free threaded out of region allocations */
+    /* decommit virtual memory */
+    retval = munmap((void*)arena->vmem, arena->vsize);
+    return (retval) ? errno : retval;
+}
+
+
+int bt_arena_page_alloc(struct bt_arena *arena, size_t alloc_size, void **page_pp)
+{
+    uint64_t *map_iter;
+    uint32_t r_count;
+    size_t new_max;
+
+    if (arena == NULL || alloc_size == 0 || page_pp == NULL) {
+        return EINVAL;
+    }
+
+    if (arena->region_count >= BT_ARENA_BITMAP_SIZE) {
+        /* how?: need warning log message as to why */
+        return ENOMEM;
+    }
+
+    WT_ASSERT(NULL, arena->region_highest <= arena->region_count);
+    /* which branch is unlikely??? */
+    if (arena->region_highest == arena->region_count) {
+        /*
+         * append a new region
+         *
+         * potentially sub-divides the available space for a region
+         */
+        r_count = arena->region_count + 1;
+        new_max = arena->vsize / next_highest_pow2(r_count);
+        if (new_max < arena->region_largest) {
+            /* how?: need warning log message as to why */
+            return ENOMEM;
+        }
+        arena->region_max = new_max;
+        WT_ASSERT(NULL, 0 && "not implemented");
+    } else {
+        /*
+         * scan to find the first free region
+         *
+         * guaranteed to exist by earlier region count check
+         */
+        for (map_iter = (void *)&arena->region_map; *map_iter != 0; ++map_iter) {
+            WT_ASSERT(NULL, ((uintptr_t)map_iter - (uintptr_t)&arena->region_map) < BT_ARENA_BITMAP_SIZE_BYTES);
+        }
+        WT_ASSERT(NULL, 0 && "not implemented");
+    }
+
     return ENOTSUP;
 }
 
-int bt_arena_dtor(struct bt_arena *arena);
+int bt_arena_page_free(struct bt_arena *arena, WT_PAGE *page)
+{
+    if (arena == NULL || page == NULL) {
+        return EINVAL;
+    }
+    WT_ASSERT(NULL, 0 && "not implemented");
+    return ENOTSUP;
+}
