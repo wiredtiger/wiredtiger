35a62c126d33dc34794b2b7319e9db8ff2774a1d
Mick Graham <mick.graham@mongodb.com>
2023-11-01 01:51:10 +0000

    WT-11778 Restricting eviction of metadata files by threads that have active updates on them. Restricting dirty eviction to sessions with snapshot isolation

--- src/evict/evict_page.c
+++ src/evict/evict_page.c
@@ -734,6 +734,10 @@ __evict_review(WT_SESSION_IMPL *session, WT_REF *ref, uint32_t evict_flags, bool
     if (F_ISSET(conn, WT_CONN_IN_MEMORY) && !modified && !closing)
         return (__wt_set_return(session, EBUSY));
 
+    /* Check the current session has the correct isolation level for dirty content. */
+    if (modified && session->txn->isolation != WT_ISO_SNAPSHOT)
+        return (__wt_set_return(session, EBUSY));
+
     /* Check if the page can be evicted. */
     if (!closing) {
         /*
