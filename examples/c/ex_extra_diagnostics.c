e6f82a8bb78fd1761c289c82c45a023af0aafa83
Andrew Morton <andrew.morton@mongodb.com>
2023-11-05 22:53:52 +0000

    s_all

--- examples/c/ex_extra_diagnostics.c
+++ examples/c/ex_extra_diagnostics.c
@@ -41,8 +41,12 @@ main(int argc, char *argv[])
     home = example_setup(argc, argv);
 
 #ifdef HAVE_DIAGNOSTIC
-    /* In diagnostic mode diagnostics are always enabled. Attempting to configure them will return an error. */
-    testutil_assert(wiredtiger_open(home, NULL, "create,extra_diagnostics=[key_out_of_order]", &conn) == EINVAL);
+    /*
+     * In diagnostic mode diagnostics are always enabled. Attempting to configure them will return
+     * an error.
+     */
+    testutil_assert(
+      wiredtiger_open(home, NULL, "create,extra_diagnostics=[key_out_of_order]", &conn) == EINVAL);
 #else
     /*! [Configure extra_diagnostics] */
     /* Open a connection to the database, enabling key ordering checks. */
