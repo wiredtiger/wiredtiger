c2767e9678757f787714869de2133435491d4ea1
Sean <41533874+Sean04@users.noreply.github.com>
2023-11-08 16:23:35 +1100

    WT-9607 Fix timing of compact progress messages (#9836)
    (cherry picked from commit f9183b017d6e455c6f97dcf7244b2ec0040dc1eb)

--- src/block/block_compact.c
+++ src/block/block_compact.c
@@ -72,7 +72,7 @@ __wt_block_compact_get_progress_stats(WT_SESSION_IMPL *session, WT_BM *bm,
  *     Output compact progress message.
  */
 void
-__wt_block_compact_progress(WT_SESSION_IMPL *session, WT_BLOCK *block, u_int *msg_countp)
+__wt_block_compact_progress(WT_SESSION_IMPL *session, WT_BLOCK *block)
 {
     struct timespec cur_time;
     uint64_t time_diff;
@@ -83,9 +83,9 @@ __wt_block_compact_progress(WT_SESSION_IMPL *session, WT_BLOCK *block, u_int *ms
     __wt_epoch(session, &cur_time);
 
     /* Log one progress message every twenty seconds. */
-    time_diff = WT_TIMEDIFF_SEC(cur_time, session->compact->begin);
-    if (time_diff / WT_PROGRESS_MSG_PERIOD > *msg_countp) {
-        ++*msg_countp;
+    time_diff = WT_TIMEDIFF_SEC(cur_time, session->compact->last_progress);
+    if (time_diff > WT_PROGRESS_MSG_PERIOD) {
+        session->compact->last_progress = cur_time;
         __wt_verbose_debug1(session, WT_VERB_COMPACT_PROGRESS,
           " compacting %s for %" PRIu64 " seconds; reviewed %" PRIu64 " pages, rewritten %" PRIu64
           " pages",
