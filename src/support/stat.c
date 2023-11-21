73131e60a4ddb0746eafbd9ed158ad262779ae08
y123456yz <3058078489@qq.com>
2023-11-21 16:13:09 +0800

    change recoment and run s_all

--- src/support/stat.c
+++ src/support/stat.c
@@ -1567,8 +1567,8 @@ static const char *const __stats_connection_desc[] = {
   "checkpoint: scrub dirty target",
   "checkpoint: scrub time (msecs)",
   "checkpoint: stop timing stress active",
+  "checkpoint: time spent on all-tree checkpoint work (usecs)",
   "checkpoint: total time (msecs)",
-  "checkpoint: transaction checkpoint tree duration (usecs)",
   "checkpoint: transaction checkpoints due to obsolete pages",
   "checkpoint: wait cycles while cache dirty level is decreasing",
   "chunk-cache: aggregate number of spanned chunks on read",
@@ -2257,8 +2257,8 @@ __wt_stat_connection_clear_single(WT_CONNECTION_STATS *stats)
     /* not clearing checkpoint_scrub_target */
     /* not clearing checkpoint_scrub_time */
     /* not clearing checkpoint_stop_stress_active */
-    /* not clearing checkpoint_time_total */
     stats->checkpoint_tree_duration = 0;
+    /* not clearing checkpoint_time_total */
     stats->checkpoint_obsolete_applied = 0;
     stats->checkpoint_wait_reduce_dirty = 0;
     stats->chunkcache_spans_chunks_read = 0;
@@ -2958,8 +2958,8 @@ __wt_stat_connection_aggregate(WT_CONNECTION_STATS **from, WT_CONNECTION_STATS *
     to->checkpoint_scrub_target += WT_STAT_READ(from, checkpoint_scrub_target);
     to->checkpoint_scrub_time += WT_STAT_READ(from, checkpoint_scrub_time);
     to->checkpoint_stop_stress_active += WT_STAT_READ(from, checkpoint_stop_stress_active);
-    to->checkpoint_time_total += WT_STAT_READ(from, checkpoint_time_total);
     to->checkpoint_tree_duration += WT_STAT_READ(from, checkpoint_tree_duration);
+    to->checkpoint_time_total += WT_STAT_READ(from, checkpoint_time_total);
     to->checkpoint_obsolete_applied += WT_STAT_READ(from, checkpoint_obsolete_applied);
     to->checkpoint_wait_reduce_dirty += WT_STAT_READ(from, checkpoint_wait_reduce_dirty);
     to->chunkcache_spans_chunks_read += WT_STAT_READ(from, chunkcache_spans_chunks_read);
