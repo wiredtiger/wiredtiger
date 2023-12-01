690ae2f6b41a06da91cfa8217fc456e47e9783ad
Peter Macko <peter.macko@mongodb.com>
2023-12-01 12:26:54 -0500

    WT-12069 Fix Coverity bugs in the model

--- test/model/src/driver/debug_log_parser.cpp
+++ test/model/src/driver/debug_log_parser.cpp
@@ -492,7 +492,7 @@ debug_log_parser::commit_transaction(kv_transaction_ptr txn)
     /* Process the checkpoint metadata, if there are any associated with the transaction. */
     auto i = _txn_ckpt_metadata.find(txn->id());
     if (i != _txn_ckpt_metadata.end()) {
-        for (auto p : i->second)
+        for (auto &p : i->second)
             metadata_checkpoint_apply(p.first, p.second);
         _txn_ckpt_metadata.erase(i);
     }
@@ -505,6 +505,15 @@ debug_log_parser::commit_transaction(kv_transaction_ptr txn)
 struct from_debug_log_helper_args {
     debug_log_parser &parser;
     kv_database &database;
+
+    /*
+     * from_debug_log_helper_args::from_debug_log_helper_args --
+     *     Create an instance of this struct.
+     */
+    inline from_debug_log_helper_args(debug_log_parser &parser_arg, kv_database &database_arg)
+        : parser(parser_arg), database(database_arg)
+    {
+    }
 };
 
 /*
@@ -743,6 +752,12 @@ debug_log_parser::from_json(kv_database &database, const char *path)
                     parser.apply(op_entry.get<prev_lsn>());
                     continue;
                 }
+
+                /* Backup IDs. */
+                if (op_type == "backup_id")
+                    continue; /* Nothing to do. */
+
+                /* Silently ignore the other operation types. */
             }
 
             continue;
