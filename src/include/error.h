ba2e68dec53e828e8609741745c66e8c4d815834
Jeremy Thorp <jeremy.thorp@mongodb.com>
2023-12-06 10:28:00 +1100

    Changed WT_TRET and WT_TRET_ERROR_OK to use inline functions.

--- src/include/error.h
+++ src/include/error.h
@@ -98,22 +98,28 @@
 #define WT_RET_BUSY_OK(a) WT_RET_ERROR_OK(a, EBUSY)
 #define WT_RET_NOTFOUND_OK(a) WT_RET_ERROR_OK(a, WT_NOTFOUND)
 /* Set "ret" if not already set. */
-#define WT_TRET(a)                                                                           \
-    do {                                                                                     \
-        int __ret;                                                                           \
-        if ((__ret = (a)) != 0 &&                                                            \
-          (__ret == WT_PANIC || ret == 0 || ret == WT_DUPLICATE_KEY || ret == WT_NOTFOUND || \
-            ret == WT_RESTART))                                                              \
-            ret = __ret;                                                                     \
-    } while (0)
-#define WT_TRET_ERROR_OK(a, e)                                                               \
-    do {                                                                                     \
-        int __ret;                                                                           \
-        if ((__ret = (a)) != 0 && __ret != (e) &&                                            \
-          (__ret == WT_PANIC || ret == 0 || ret == WT_DUPLICATE_KEY || ret == WT_NOTFOUND || \
-            ret == WT_RESTART))                                                              \
-            ret = __ret;                                                                     \
-    } while (0)
+__inline static void __wt_tret(int *pret, int a)
+{
+    int __ret, ret;
+    ret = *pret;
+    if ((__ret = (a)) != 0 &&
+      (__ret == WT_PANIC || ret == 0 || ret == WT_DUPLICATE_KEY || ret == WT_NOTFOUND ||
+        ret == WT_RESTART))
+        *pret = __ret;
+}
+#define WT_TRET(a) __wt_tret(&ret, a)
+
+__inline static void __wt_tret_error_ok(int *pret, int a, int e)
+{
+    int __ret, ret;
+    ret = *pret;
+    if ((__ret = (a)) != 0 && __ret != (e) &&
+      (__ret == WT_PANIC || ret == 0 || ret == WT_DUPLICATE_KEY || ret == WT_NOTFOUND ||
+        ret == WT_RESTART))
+        *pret = __ret;
+}
+#define WT_TRET_ERROR_OK(a, e) __wt_tret_error_ok(&ret, a, e)
+
 #define WT_TRET_BUSY_OK(a) WT_TRET_ERROR_OK(a, EBUSY)
 #define WT_TRET_NOTFOUND_OK(a) WT_TRET_ERROR_OK(a, WT_NOTFOUND)
 
