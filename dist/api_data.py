b9c413261c3966f8f656d4e7c218c6caf1869114
Etienne Petrel <etienne.petrel@mongodb.com>
2023-12-06 01:51:33 +0000

    Update description of the run_once field

--- dist/api_data.py
+++ dist/api_data.py
@@ -1392,7 +1392,7 @@ methods = {
         enable/disabled the background compaction server.''',
         type='boolean'),
     Config('exclude', '', r'''
-        A list of table objects to be excluded from background compaction. The list is immutable and
+        list of table objects to be excluded from background compaction. The list is immutable and
         only applied when the background compaction gets enabled. The list is not saved between the
         calls and needs to be reapplied each time the service is enabled. The individual objects in
         the list can only be of the \c table: URI type''',
@@ -1401,7 +1401,8 @@ methods = {
         minimum amount of space recoverable for compaction to proceed''',
         min='1MB'),
     Config('run_once', 'false', r'''
-        configure background compaction server to run once''',
+        configure background compaction server to run once. In this mode, compaction is always
+        attempted on each table unless explicitly excluded''',
         type='boolean'),
     Config('timeout', '1200', r'''
         maximum amount of time to allow for compact in seconds. The actual amount of time spent
