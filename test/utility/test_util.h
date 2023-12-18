c7cc0e46aa5f861764ff432cf89406257b570870
Adeline Chen <adelinechen13@gmail.com>
2023-12-18 05:12:01 +0000

    Add full stop to comment

--- test/utility/test_util.h
+++ test/utility/test_util.h
@@ -333,7 +333,7 @@ typedef struct {
 
 /*
  * testutil_system --
- *     A convenience macro for testutil_system_internal. Accepts line number as an argument
+ *     A convenience macro for testutil_system_internal. Accepts line number as an argument.
  */
 #define testutil_system(fmt, ...)                                                  \
     WT_GCC_FUNC_ATTRIBUTE((format(printf, 1, 2)))                                  \
