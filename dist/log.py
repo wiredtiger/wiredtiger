6b83f4b0865fa359a61cb6af25ad977e9539e2af
Yury Ershov <yuriy.ershov@gmail.com>
2023-12-21 01:26:34 +1100

    WT-12195 Improve s_all parallel execution

--- dist/log.py
+++ dist/log.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 
+import os
 from dist import compare_srcfile, format_srcfile
 import log_data
 
 # Temporary file.
-tmp_file = '__tmp'
+tmp_file = '__tmp_log' + str(os.getpid())
 
 # Map log record types to:
 #   0. C type
