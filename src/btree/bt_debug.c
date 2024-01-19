37c2eeeab97ba2208ce06535ea4f5b00b6bac034
Mariam Mojid <mariam.mojid@mongodb.com>
2024-01-19 03:20:20 +0000

    remove square brackets around entry meta data

--- src/btree/bt_debug.c
+++ src/btree/bt_debug.c
@@ -780,7 +780,7 @@ __wt_debug_disk(
     flags = dump_app_data ? WT_DEBUG_UNREDACT : 0;
     WT_RET(__debug_config(session, ds, ofile, flags));
 
-    WT_ERR(ds->f(ds, "- %s page\n\t[", __wt_page_type_string(dsk->type)));
+    WT_ERR(ds->f(ds, "- %s page\n\t", __wt_page_type_string(dsk->type)));
     switch (dsk->type) {
     case WT_PAGE_BLOCK_MANAGER:
         break;
@@ -809,7 +809,7 @@ __wt_debug_disk(
     if (F_ISSET(dsk, WT_PAGE_EMPTY_V_NONE))
         WT_ERR(ds->f(ds, "empty-none, "));
 
-    WT_ERR(ds->f(ds, "generation: %" PRIu64 "]\n", dsk->write_gen));
+    WT_ERR(ds->f(ds, "generation: %" PRIu64 "\n", dsk->write_gen));
 
     switch (dsk->type) {
     case WT_PAGE_BLOCK_MANAGER:
