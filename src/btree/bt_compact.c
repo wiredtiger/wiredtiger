3db2dec7f21469fee8688e67dd8a36d454998958
Etienne Petrel <etienne.petrel@mongodb.com>
2024-01-22 23:52:45 +0000

    Always check the compact_state value when walking internal pages

--- src/btree/bt_compact.c
+++ src/btree/bt_compact.c
@@ -256,10 +256,10 @@ __compact_walk_internal(WT_SESSION_IMPL *session, WT_REF *parent)
     WT_INTL_FOREACH_BEGIN (session, parent->page, ref) {
         if (F_ISSET(ref, WT_REF_FLAG_LEAF)) {
             WT_ERR(__compact_page(session, ref, &skipp));
+            if (session->compact_state == WT_COMPACT_EXITING)
+                break;
             if (!skipp)
                 overall_progress = true;
-            else if (session->compact_state == WT_COMPACT_EXITING)
-                break;
         }
     }
     WT_INTL_FOREACH_END;
