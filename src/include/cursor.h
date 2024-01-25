a5bc3a7cce7761859c2e3a33af888faa8d79b61f
Jeremy Thorp <jeremy.thorp@mongodb.com>
2024-01-25 16:16:16 +1100

    Drafted unit tests for WT_TRET and WT_TRET_ERROR_OK.

--- src/include/cursor.h
+++ src/include/cursor.h
@@ -274,7 +274,7 @@ struct __wt_cursor_btree {
  * __wt_curbt2bt --
  *     Safely return the WT_BTREE pointed to by the cursor_btree's dhandle.
  */
-static WT_BTREE *NO_INLINE_FOR_CODE_COVERAGE
+static NO_INLINE_FOR_CODE_COVERAGE WT_BTREE *
 __wt_curbt2bt(WT_CURSOR_BTREE *cursor_btree)
 {
     WT_DATA_HANDLE *dhandle;
@@ -289,7 +289,7 @@ __wt_curbt2bt(WT_CURSOR_BTREE *cursor_btree)
  *     Safely return the WT_BTREE pointed to by the cursor's dhandle.
  */
 
-static WT_BTREE *NO_INLINE_FOR_CODE_COVERAGE
+static NO_INLINE_FOR_CODE_COVERAGE WT_BTREE *
 __wt_cur2bt(WT_CURSOR *cursor)
 {
     WT_CURSOR_BTREE *cursor_btree;
