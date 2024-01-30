b443753aa8ffa94e3b7c9803ad69de5fefd12089
Yury Ershov <yuriy.ershov@gmail.com>
2024-01-30 18:54:14 +1100

    Bring pygrep closer to gnu grep

--- dist/pygrep.py
+++ dist/pygrep.py
@@ -1,29 +1,52 @@
 #!/usr/bin/env python3
 
 # This a simple limited replacement for GNU grep
-#  - All options must go in the first arg
-#  - Order of options matters! - see source
+# Caveats:
+# * Accepts only stdin
+# * Unknown options must go as a separate arg
 
-import re, sys
+import re, sys, argparse
 
 def matchLine(regex, line):
+    global args
     match = regex.search(line)
-    if bool(match) != bool(inverse): print(line, end="")
+    if bool(match) != bool(args.inverse): print(line, end='')
 
 def matchSearch(regex, line):
     for match in regex.finditer(line):
         print(match if isinstance(match, str) else match[0])
 
-inverse, match, opts, args = False, matchLine, "", sys.argv[1:]
-if args[0][0] == "-": opts, args = args[0], args[1:]
-regex = "|".join(args)
+argparser = argparse.ArgumentParser(description='Simple Grep Replacement.', add_help=False)
+argparser.add_argument('-F', '--fgrep', dest='is_regex', action='store_false', default=True,
+        help='Interpret pattern as a set of fixed strings (behave like fgrep).')
+argparser.add_argument('-E', '--egrep', dest='is_regex', action='store_true',
+        help='Interpret pattern as an extended regular expression (behave like egrep).')
+argparser.add_argument('-e', '--regexp', dest='pattern', action='append',
+        help='Specify multiple patterns on the command line.')
+argparser.add_argument('-w', '--word-regexp', dest='word', action='store_true',
+        help='The expression is searched for as a word.')
+argparser.add_argument('-v', '--invert-match', dest='inverse', action='store_true',
+        help='Selected lines are those not matching any of the specified patterns.')
+argparser.add_argument('-o', '--only-matching', dest='only_matching', action='store_true',
+        help='Prints only the matching part of the lines.')
+argparser.add_argument('pattern', action='append', nargs='?',
+        help='Search pattern.')
+#argparser.add_argument('files', action='append', nargs='*',
+#        help='Search pattern.')
 
-for opt in [c for c in opts]:
-    if opt == 'e': regex = "|".join(args)
-    elif opt == 'f': regex = "|".join([re.escape(arg) for arg in args])
-    elif opt == 'w': regex = '\\b(?:' + regex + ')\\b'
-    elif opt == 'v': inverse = True; match = matchLine;
-    elif opt == 'o': match = matchSearch
+#args = argparser.parse_args(sys.argv[1:])
+args = argparser.parse_known_args(sys.argv[1:])[0]  # Ignore unknown options rathern than complain
+args.pattern = [line
+                for pat in args.pattern if pat is not None
+                for line in pat.splitlines() if line != '']
+if not args.pattern:
+    argparser.print_help()
+    exit()
+
+match = matchSearch if args.only_matching else matchLine
+if not args.is_regex: args.pattern = [re.escape(pat) for pat in args.pattern]
+regex = '|'.join(args.pattern)
+if args.word: regex = '\\b(?:' + regex + ')\\b'
 
 regex = re.compile(regex)
 
