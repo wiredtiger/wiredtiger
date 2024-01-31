bba44df40bc569610d6ed6fd69cfd1736603da2a
Adeline Chen <adelinechen13@gmail.com>
2024-01-31 00:21:37 +0000

    add helper function, finish formatting

--- tools/wt.py
+++ tools/wt.py
@@ -1,72 +1,104 @@
-import pprint
-def parse_dump_pages(file_path):
+import json
+import sys
+
+def print_output(output):
+    str = "{"
+    for count, key in enumerate(output):
+        value = output[key]
+        if count:
+            str += ", "
+        str += "\n\t" + key + ": {\n\t\t\"metadata\": " + json.dumps(value["metadata"]) 
+        if "data" in value:
+            str += "\n\t\t\"data\": " + json.dumps(value["data"]) 
+        if "hs_modify" in value:
+            str += "\n\t\t\"hs_modify\": " + json.dumps(value["hs_modify"]) 
+        if "hs_update" in value:
+            str += "\n\t\t\"hs_update\": " + json.dumps(value["hs_update"]) 
+        str += "\n\t}"
+    str += "\n}"
+    print(str)
+
+
+def string_to_array(line):
+    dict = {}
+    for x in line.split(" | "):
+        kv_pair = x.split(": ", 1)
+        if kv_pair[1][0] == "[" and kv_pair[1][-1] == "]":
+            kv_pair[1] = kv_pair[1][1:-1]
+            kv_pair[1] = kv_pair[1].split(", ")
+        if kv_pair[0] == "addr": 
+            temp = kv_pair[1][0].split(": ")
+            dict.update({"object_id": temp[0], "offset_range": temp[1], "size": kv_pair[1][1], 
+                         "checksum": kv_pair[1][2]})
+        else:
+            dict[kv_pair[0]] = kv_pair[1]
+    return dict
+
+
+def parse_dump_pages(file_path, allow_data, allow_hs):
     """
     Parse the output file of dump_pages
     """
     f = open(file_path, "r")
-    f.readline() # separator 
-    f.readline() # test name
-    line = f.readline()
+    line = f.readline() # separator
     output = {}
     cur_node = {"metadata": {}}
     cur_node_id = None
     page_type = None
+    time_stamp = None
+
+    while line and line[0:2] != "- ": # checkpoints
+        line = f.readline()
     while line:
-        if line[0:2] == "- ":
-            page_type = line.split()[-1]
+        if line[0:2] == "- ": # start of a new node
             line = line[len("- "):-1]
-            if page_type == "Root":
-                cur_node_id = line
+            page_info = [line.split(": ")[0]] + line.split(": ")[1].split()
+            page_type = page_info[-1]
+            if page_info[0] == "Root":
+                cur_node_id = page_info[0]
+                cur_node["metadata"]["store_type"] = page_info[1]
             elif page_type == "internal":
                 output[cur_node_id] = cur_node 
                 cur_node_id = line.split(": ")[0]
-                cur_node = {"metadata": {}, "data": {}}
+                cur_node = {"metadata": {}}
             elif page_type == "leaf":
                 output[cur_node_id] = cur_node 
                 cur_node_id = line.split(": ")[0]
-                cur_node = {"metadata": {}, "data": {}}
+                cur_node = {"metadata": {}}
             else:
                 pass
-        elif line[0:3] == "\t> ":
-            line = line[len("\t> "):-1]
-            for x in line.split(" | "):
-                kv_pair = x.split(": ", 1)
-                if kv_pair[1][0] == "[" and kv_pair[1][-1] == "]":
-                    kv_pair[1] = kv_pair[1][1:-1]
-                    kv_pair[1] = kv_pair[1].split(", ")
-                cur_node["metadata"][kv_pair[0]] = kv_pair[1]
-        elif line.startswith("\tK: ") or line.startswith("\trecno: "):
+        elif line[0:3] == "\t> ": # metadata for new node
+            cur_node["metadata"].update(string_to_array(line[len("\t> "):-1]))
+        elif allow_data and (line.startswith("\tK: ") or line.startswith("\trecno: ")): # actual data
+            if "data" not in cur_node:
+                cur_node["data"] = {}
+            key = line[1:-1].split(": ")[1][1:-1]
+            metadata = string_to_array(f.readline()[1:-1])
             if page_type == "leaf": 
-                line = line[1:-1]
-                key = line.split(": ")[1]
-                line = f.readline()[1:-1]
-                metadata = {}
-                for x in line.split(" | "):
-                    kv_pair = x.split(": ", 1)
-                    if kv_pair[1][0] == "[" and kv_pair[1][-1] == "]":
-                        kv_pair[1] = kv_pair[1][1:-1]
-                        kv_pair[1] = kv_pair[1].split(", ")
-                    metadata[kv_pair[0]] = kv_pair[1]
-                value = f.readline()[4:-2]
+                value = f.readline()[4:-1][1:-1]
                 cur_node["data"][key] = {"value": value, "metadata": metadata}    
             elif page_type == "internal":
-                line = line[1:-1]
-                key = line.split(": ")[1]
-                line = f.readline()[1:-1]
-                value = {}
-                for x in line.split(" | "):
-                    kv_pair = x.split(": ", 1)
-                    if kv_pair[1][0] == "[" and kv_pair[1][-1] == "]":
-                        kv_pair[1] = kv_pair[1][1:-1]
-                        kv_pair[1] = kv_pair[1].split(", ")
-                    value[kv_pair[0]] = kv_pair[1]
-                cur_node["data"][key] = value
-        else:
+                metadata["K"] = key
+                cur_node["data"][metadata.pop("ref")] = metadata
+        elif allow_hs and line.startswith("\ths_"): # hs info
+            [hs_key, hs_value] = line[1:-1].split(": ")
+            if hs_key not in cur_node:
+                cur_node[hs_key] = []
+            cur_node[hs_key].append({hs_value[1:-1]: time_stamp})
             pass
+        else: # timestamp 
+            time_stamp = string_to_array(line[1:-1])
         line = f.readline() 
     if cur_node_id is not None:
         output[cur_node_id] = cur_node
-    pprint.pprint(output)
+    # pprint.pprint(output)
+    # print(json.dumps(output, indent=4))
+    print_output(output)
     f.close()
 
-parse_dump_pages("../WT_TEST/testing/test_row.txt")
+def main():
+    file = sys.argv[1]
+    parse_dump_pages(file, True, True)
+
+if __name__ == '__main__':
+    main()
