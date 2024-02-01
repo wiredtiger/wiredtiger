43987800c51563980a6af691abf2a16676afa2ae
Mariam Mojid <mariam.mojid@mongodb.com>
2024-02-01 02:45:30 +0000

    change find wt exec path to use subprocess instead of os.walk to add max depth to dir tree search + address pr comments

--- tools/wt_verify.py
+++ tools/wt_verify.py
@@ -27,12 +27,14 @@
 # OTHER DEALINGS IN THE SOFTWARE.
 
 # A python script to run the WT tool verify command.
+# This script wraps the wt util tool and takes in arguments for the tool,
+# handles the output and provides additional processing options on the output
+# such as pretty printed output and visualisation options.
 
-# This script only handles Row store and Variable Length Column Store (VLCS),
-# not Fixed Length Column Store (FLCS)
+# This script only supports Row store and Variable Length Column Store (VLCS).
+# Fixed Length Column Store (FLCS) is not supported.
 
 import argparse
-import fnmatch
 import os
 import subprocess
 import sys
@@ -43,13 +45,14 @@ def visualize(data, visualization_type):
     """
     Visualizing data
     """
+    pass
 
 
-def parse_output(file_path):
+def parse_output(output):
     """
     Parse the output file
     """
-    return ''
+    return output
 
 
 def execute_command(command):
@@ -64,23 +67,27 @@ def execute_command(command):
         print(f"Error executing command: {e}", file=sys.stderr)
         sys.exit(1)
     
-    return result
+    return result.stdout
 
 
 def find_wt_exec_path():
     """
     Find the path of the WT tool executable. 
     
-    Prompt user to provide path (if none or many paths found)
+    We expect to find exactly one wt binary. Otherwise exit and prompt the user to provide an explicit path to the binary
     """
-    wt_verify_dir_path = os.path.dirname(os.path.abspath(__file__))
-    path = f"{wt_verify_dir_path}/../."
-    result = []
+    path = f"{os.path.dirname(os.path.abspath(__file__))}/../."
 
-    for (root, _, files) in os.walk(path):
-        for name in files:
-            if fnmatch.fnmatch(name, "wt"):
-                result.append(os.path.join(root, name))
+    try:
+        result = subprocess.run(['find', path, '-maxdepth', '2', '-name', 'wt'],
+                                stdout=subprocess.PIPE, text=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Error executing command: {e}", file=sys.stderr)
+        sys.exit(1)
+
+    list_of_paths = result.stdout.split('\n')
+    # Sanity check to remove empty new lines from find output
+    result = [path for path in list_of_paths if path != '']
 
     if len(result) > 1:
         print("Error: multiple wt executables found. Please provide wt executable path using the -wt flag:")
@@ -91,7 +98,7 @@ def find_wt_exec_path():
         print("Error: wt executable not found. Please provide path using the -wt flag.")
         exit(1)
 
-    return result
+    return result[0]
 
 
 def construct_command(args):
@@ -101,7 +108,7 @@ def construct_command(args):
     if args.wt_exec_path:
         command = f"{args.wt_exec_path} -h {args.home_dir} verify"
     else:
-        command = "{} -h {} verify".format(''.join(find_wt_exec_path()), args.home_dir)
+        command = f"{find_wt_exec_path()} -h {args.home_dir} verify"
 
     if args.unredacted:
         command += " -u"
@@ -119,42 +126,32 @@ def main():
     parser.add_argument('-hd', '--home_dir', required=True, help='Path to the WiredTiger database home directory.')
     parser.add_argument('-f', '--file_name', required=True, help='Name of the WiredTiger file to verify.')
     parser.add_argument('-wt', '--wt_exec_path', help='Path of the WT tool executable.')
-    parser.add_argument('-o', '--output_path', default='.', help='Directory path where the output file will be saved (default: current directory).')
-    parser.add_argument('-n', '--output_file_name', help='Name of the output file (default wt_verify_output.txt)')
+    parser.add_argument('-o', '--output_file', help='Option to save output in given output file.')
     parser.add_argument('-d', '--dump_config', choices=['dump_pages', 'dump_blocks'], help='Option to specify dump_pages or dump_blocks configuration.')
     parser.add_argument('-t', '--keep_tx_ids', action='store_true', help='Option to keep transaction IDs during verification.')
     parser.add_argument('-u', '--unredacted', action='store_true', help='Option to display unredacted output.')  
-    parser.add_argument('-p', '--print_output', action='store_true', default=True, help='Print the output (default is on)')
-    parser.add_argument('-v', '--visualize', choices=['page_sizes', 'num_entries', 'dsk_image_sizes'], help='Type of visualization')
+    parser.add_argument('-p', '--print_output', action='store_true', default=False, help='Print the output (default is on)')
+    parser.add_argument('-v', '--visualize', choices=['page_sizes', 'entries', 'dsk_image_sizes'], help='Type of visualization')
 
     args = parser.parse_args()
 
     try:
         command = construct_command(args)
         output = execute_command(command)
-    except RuntimeError as e:
-        print(str(e), file=sys.stderr)
-        sys.exit(1)
-    except ValueError as e:
-        print(str(e), file=sys.stderr)
-        sys.exit(1)
-    except TypeError as e:
+    except (RuntimeError, ValueError, TypeError) as e:
         print(str(e), file=sys.stderr)
         sys.exit(1)
 
     parsed_data = parse_output(output)
-    if args.output_file_name:
-        output_file_path = Path(args.output_path) / f"{args.output_file_name}"
-    else:
-        output_file_path = Path(args.output_path) / "wt_verify_output.txt"
 
-    try:
-        with open(output_file_path, 'w') as file:
-            file.write(parsed_data)
-        print(f"Output written to {output_file_path}")
-    except IOError as e:
-        print(f"Failed to write output to file: {e}", file=sys.stderr)
-        sys.exit(1)
+    if args.output_file:
+        try:
+            with open(args.output_file, 'w') as file:
+                file.write(parsed_data)
+            print(f"Output written to {args.output_file}")
+        except IOError as e:
+            print(f"Failed to write output to file: {e}", file=sys.stderr)
+            sys.exit(1)
 
     if args.print_output:
         print(parsed_data)
