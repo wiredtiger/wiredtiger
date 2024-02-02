b99e47332a0ceeb70552af897b35966c8ac95d52
Andrew Morton <andrew.morton@mongodb.com>
2024-02-02 05:18:00 +0000

    Disable FLCS test variant of recovery01

--- test/suite/test_recovery01.py
+++ test/suite/test_recovery01.py
@@ -38,7 +38,7 @@ class test_recovery01(wttest.WiredTigerTestCase):
 
     format_values = [
         ('column', dict(key_format='r', value_format='S')),
-        ('column_fix', dict(key_format='r', value_format='8t')),
+        # ('column_fix', dict(key_format='r', value_format='8t')),
         ('row_integer', dict(key_format='i', value_format='S')),
     ]
 
