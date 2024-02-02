69fde12469cd226d5e983fe12fffeea3e84970d0
Hari Babu Kommi <haribabu.kommi@mongodb.com>
2024-02-02 12:06:45 +1100

    Improve the comment

--- src/btree/bt_prefetch.c
+++ src/btree/bt_prefetch.c
@@ -25,7 +25,9 @@ __wt_btree_prefetch(WT_SESSION_IMPL *session, WT_REF *ref)
     conn = S2C(session);
     block_preload = 0;
 
-    /* Don't add refs from trees that have eviction disabled since they are probably being closed */
+    /*
+     * Do not add refs from trees that have eviction disabled since they are probably being closed.
+     */
     if (!(F_ISSET(ref, WT_REF_FLAG_LEAF)) || S2BT(session)->evict_disabled > 0 ||
       (__wt_session_gen(session, WT_GEN_SPLIT) == 0)) {
         WT_STAT_CONN_INCR(session, block_prefetch_failed_start);
