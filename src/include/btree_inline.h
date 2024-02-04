739b3e484d49f2f4a043d098209622faf6ee0a0f
Hari Babu Kommi <haribabu.kommi@mongodb.com>
2024-02-05 10:52:36 +1100

    use ordered read

--- src/include/btree_inline.h
+++ src/include/btree_inline.h
@@ -1609,8 +1609,8 @@ __wt_get_page_modify_ta(WT_SESSION_IMPL *session, WT_PAGE *page, WT_TIME_AGGREGA
     if (page->modify->stop_ta == NULL)
         return (false);
 
-    *ta = page->modify->stop_ta;
-    return (true);
+    WT_ORDERED_READ(*ta, page->modify->stop_ta);
+    return (*ta != NULL);
 }
 
 /*
