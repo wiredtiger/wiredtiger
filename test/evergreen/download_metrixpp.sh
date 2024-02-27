e23ea24c9010ef3edb43dfb97c7f6b4eb929ca6e
Jeremy Thorp <jeremy.thorp@mongodb.com>
2024-02-28 09:12:08 +1100

    Created a script to download Mextrix++

---
+++ test/evergreen/download_metrixpp.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -o errexit  # Exit the script with error if any of the commands fail
+
+# This script clones Metrix++ and checks out a specific commit
+git clone https://github.com/metrixplusplus/metrixplusplus metrixplusplus
+
+cd metrixplusplus
+
+# Check out the version from 18th Dec 2022, which is the latest commit as of 28th Feb 2024.
+git checkout 78dc5380de9aaa3d615f8be6c84e90cb2ae0d90b
+
+cd ..
