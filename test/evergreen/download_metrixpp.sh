7365ba9eef304d2a46598b973f2b876fd6d82965
Jeremy Thorp <jeremy.thorp@mongodb.com>
2024-02-28 09:41:32 +1100

    Change error checking in download_metrixpp.sh to avoid issues with the complexity task.

--- test/evergreen/download_metrixpp.sh
+++ test/evergreen/download_metrixpp.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
-set -o errexit  # Exit the script with error if any of the commands fail
-
 # This script clones Metrix++ and checks out a specific commit
-git clone https://github.com/metrixplusplus/metrixplusplus metrixplusplus
+git clone https://github.com/metrixplusplus/metrixplusplus metrixplusplus || exit
 
-cd metrixplusplus
+cd metrixplusplus || exit
 
 # Check out the version from 18th Dec 2022, which is the latest commit as of 28th Feb 2024.
-git checkout 78dc5380de9aaa3d615f8be6c84e90cb2ae0d90b
+git checkout 78dc5380de9aaa3d615f8be6c84e90cb2ae0d90b || exit
 
 cd ..
