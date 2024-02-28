395014b7f81af2980b727259bf7d1cd7831edee3
y123456yz <3058078489@qq.com>
2024-02-28 21:00:59 +0800

    bug fix

--- src/cursor/cur_file.c
+++ src/cursor/cur_file.c
@@ -169,7 +169,7 @@ __curfile_equals(WT_CURSOR *a, WT_CURSOR *b, int *equalp)
     ret = __wt_btcur_equals((WT_CURSOR_BTREE *)a, (WT_CURSOR_BTREE *)b, equalp);
 
 err:
-    API_END_RET(session, ret);
+    API_END_RET_STAT(session, ret, cursor_equals);
 }
 
 /*
@@ -694,10 +694,14 @@ __curfile_cache(WT_CURSOR *cursor)
     WT_SESSION_IMPL *session;
 
     cbt = (WT_CURSOR_BTREE *)cursor;
+    CURSOR_API_CALL(cursor, session, ret, cache, CUR2BT(cbt));
     session = CUR2S(cursor);
 
     WT_TRET(__wt_cursor_cache(cursor, cbt->dhandle));
     WT_TRET(__wt_session_release_dhandle(session));
+
+err:
+    API_END_RET_STAT(session, ret, cursor_cache);
     return (ret);
 }
 
