bfee12df50801d432028f8ea3acb40da4bd495ca
Luke Pearson <luke.pearson@mongodb.com>
2024-03-04 18:30:09 +0000

    Fix whitespace in cmake ile

--- cmake/configs/aarch64/linux/rcpc_test.cmake
+++ cmake/configs/aarch64/linux/rcpc_test.cmake
@@ -8,18 +8,18 @@ include(CheckCSourceCompiles)
 # Do this in a function to avoid the required flags variable leaving this scope.
 function(rcpc_test)
 set(CMAKE_REQUIRED_FLAGS -march=armv8.2-a+rcpc+crc)
-check_c_source_compiles("
-    int main(void) {
-        int a;
-        int *b;
-        int c;
-        b = &a;
-        __asm__ volatile(\"ldapr %0, %1\" : \"=r\"(c) : \"Q\"(b));
-        return 0;
-    }"
-    HAVE_RCPC
-    FAIL_REGEX "switch.*conflicts with.*switch"
-)
+    check_c_source_compiles("
+        int main(void) {
+            int a;
+            int *b;
+            int c;
+            b = &a;
+            __asm__ volatile(\"ldapr %0, %1\" : \"=r\"(c) : \"Q\"(b));
+            return 0;
+        }"
+        HAVE_RCPC
+        FAIL_REGEX "switch.*conflicts with.*switch"
+    )
 endfunction()
 
 cmake_language(CALL rcpc_test)
