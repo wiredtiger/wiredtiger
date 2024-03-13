26f95a888c344cfa7e8ecc5a501dc98bf081c329
Chenhao Qu <chenhao.qu@mongodb.com>
2024-03-13 03:15:16 +0000

    Change compare function

--- src/include/btree_cmp_inline.h
+++ src/include/btree_cmp_inline.h
@@ -22,6 +22,110 @@
 #define WT_VECTOR_SIZE 16 /* chunk size */
 
 /*
+ * __lex_compare_ge_16 --
+ *     Lexicographic comparison routine for data greater than or equal to 16 bytes. Returns: < 0 if
+ *     user_item is lexicographically < tree_item, = 0 if user_item is lexicographically =
+ *     tree_item, > 0 if user_item is lexicographically > tree_item. We use the names "user" and
+ *     "tree" so it's clear in the btree code which the application is looking at when we call its
+ *     comparison function.
+ */
+static WT_INLINE int
+__lex_compare_ge_16(const uint8_t *ustartp, const uint8_t *tstartp, size_t len, int lencmp)
+{
+    struct {
+        uint64_t a, b;
+    } udata, tdata;
+    uint64_t u64, t64;
+    const uint8_t *uendp, *userp, *tendp, *treep;
+    bool firsteq;
+
+    uendp = ustartp + len;
+    tendp = tstartp + len;
+
+    for (userp = ustartp, treep = tstartp; uendp - userp > WT_VECTOR_SIZE;
+         userp += WT_VECTOR_SIZE, treep += WT_VECTOR_SIZE) {
+        memcpy(&udata, userp, WT_VECTOR_SIZE);
+        memcpy(&tdata, treep, WT_VECTOR_SIZE);
+        if (udata.a != tdata.a || udata.b != tdata.b)
+            goto final128;
+    }
+
+    memcpy(&udata, uendp - WT_VECTOR_SIZE, WT_VECTOR_SIZE);
+    memcpy(&tdata, tendp - WT_VECTOR_SIZE, WT_VECTOR_SIZE);
+
+final128:
+    firsteq = udata.a == tdata.a;
+    u64 = firsteq ? udata.b : udata.a;
+    t64 = firsteq ? tdata.b : tdata.a;
+
+#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+    u64 = __builtin_bswap64(u64);
+    t64 = __builtin_bswap64(t64);
+#endif
+
+    return (u64 < t64 ? -1 : u64 > t64 ? 1 : lencmp);
+}
+
+/*
+ * __lex_compare_lt_16 --
+ *     Lexicographic comparison routine for data less than 16 bytes. Returns: < 0 if user_item is
+ *     lexicographically < tree_item, = 0 if user_item is lexicographically = tree_item, > 0 if
+ *     user_item is lexicographically > tree_item. We use the names "user" and "tree" so it's clear
+ *     in the btree code which the application is looking at when we call its comparison function.
+ */
+static WT_INLINE int
+__lex_compare_lt_16(const uint8_t *ustartp, const uint8_t *tstartp, size_t len, int lencmp)
+{
+    uint64_t ta, tb, ua, ub, u64, t64;
+    const uint8_t *tendp, *uendp;
+
+    uendp = ustartp + len;
+    tendp = tstartp + len;
+    if (len & sizeof(uint64_t)) {
+        memcpy(&ua, ustartp, sizeof(uint64_t));
+        memcpy(&ta, tstartp, sizeof(uint64_t));
+        memcpy(&ub, uendp - sizeof(uint64_t), sizeof(uint64_t));
+        memcpy(&tb, tendp - sizeof(uint64_t), sizeof(uint64_t));
+    } else if (len & sizeof(uint32_t)) {
+        uint32_t UA, TA, UB, TB;
+        memcpy(&UA, ustartp, sizeof(uint32_t));
+        memcpy(&TA, tstartp, sizeof(uint32_t));
+        memcpy(&UB, uendp - sizeof(uint32_t), sizeof(uint32_t));
+        memcpy(&TB, tendp - sizeof(uint32_t), sizeof(uint32_t));
+        ua = UA;
+        ta = TA;
+        ub = UB;
+        tb = TB;
+    } else if (len & sizeof(uint16_t)) {
+        uint16_t UA, TA, UB, TB;
+        memcpy(&UA, ustartp, sizeof(uint16_t));
+        memcpy(&TA, tstartp, sizeof(uint16_t));
+        memcpy(&UB, uendp - sizeof(uint16_t), sizeof(uint16_t));
+        memcpy(&TB, tendp - sizeof(uint16_t), sizeof(uint16_t));
+        ua = UA;
+        ta = TA;
+        ub = UB;
+        tb = TB;
+    } else if (len & sizeof(uint8_t)) {
+        uint8_t UA, TA;
+        memcpy(&UA, ustartp, sizeof(uint8_t));
+        memcpy(&TA, tstartp, sizeof(uint8_t));
+        return (UA < TA ? -1 : UA > TA ? 1 : lencmp);
+    } else
+        return (lencmp);
+
+    u64 = ua == ta ? ub : ua;
+    t64 = ua == ta ? tb : ta;
+
+#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+    u64 = __builtin_bswap64(u64);
+    t64 = __builtin_bswap64(t64);
+#endif
+
+    return (u64 < t64 ? -1 : u64 > t64 ? 1 : lencmp);
+}
+
+/*
  * __wt_lex_compare --
  *     Lexicographic comparison routine. Returns: < 0 if user_item is lexicographically < tree_item,
  *     = 0 if user_item is lexicographically = tree_item, > 0 if user_item is lexicographically >
@@ -32,69 +136,29 @@ static WT_INLINE int
 __wt_lex_compare(const WT_ITEM *user_item, const WT_ITEM *tree_item)
 {
     size_t len, usz, tsz;
-    const uint8_t *userp, *treep;
+    int lencmp, ret_val;
 
     usz = user_item->size;
     tsz = tree_item->size;
-    len = WT_MIN(usz, tsz);
-
-    userp = (const uint8_t *)user_item->data;
-    treep = (const uint8_t *)tree_item->data;
-
-#ifdef HAVE_X86INTRIN_H
-    /* Use vector instructions if we'll execute at least 2 of them. */
-    if (len >= WT_VECTOR_SIZE * 2) {
-        size_t remain;
-        __m128i res_eq, u, t;
-
-        remain = len % WT_VECTOR_SIZE;
-        len -= remain;
-        if (WT_ALIGNED_16(userp) && WT_ALIGNED_16(treep))
-            for (; len > 0;
-                 len -= WT_VECTOR_SIZE, userp += WT_VECTOR_SIZE, treep += WT_VECTOR_SIZE) {
-                u = _mm_load_si128((const __m128i *)userp);
-                t = _mm_load_si128((const __m128i *)treep);
-                res_eq = _mm_cmpeq_epi8(u, t);
-                if (_mm_movemask_epi8(res_eq) != 65535)
-                    break;
-            }
-        else
-            for (; len > 0;
-                 len -= WT_VECTOR_SIZE, userp += WT_VECTOR_SIZE, treep += WT_VECTOR_SIZE) {
-                u = _mm_loadu_si128((const __m128i *)userp);
-                t = _mm_loadu_si128((const __m128i *)treep);
-                res_eq = _mm_cmpeq_epi8(u, t);
-                if (_mm_movemask_epi8(res_eq) != 65535)
-                    break;
-            }
-        len += remain;
+    if (usz < tsz) {
+        len = usz;
+        lencmp = -1;
+    } else if (usz > tsz) {
+        len = tsz;
+        lencmp = 1;
+    } else {
+        len = usz;
+        lencmp = 0;
     }
-#elif defined(HAVE_ARM_NEON_INTRIN_H)
-    /* Use vector instructions if we'll execute at least 1 of them. */
-    if (len >= WT_VECTOR_SIZE) {
-        size_t remain;
-        uint8x16_t res_eq, u, t;
-        remain = len % WT_VECTOR_SIZE;
-        len -= remain;
-        for (; len > 0; len -= WT_VECTOR_SIZE, userp += WT_VECTOR_SIZE, treep += WT_VECTOR_SIZE) {
-            u = vld1q_u8(userp);
-            t = vld1q_u8(treep);
-            res_eq = vceqq_u8(u, t);
-            if (vminvq_u8(res_eq) != 255)
-                break;
-        }
-        len += remain;
-    }
-#endif
-    /*
-     * Use the non-vectorized version for the remaining bytes and for the small key sizes.
-     */
-    for (; len > 0; --len, ++userp, ++treep)
-        if (*userp != *treep)
-            return (*userp < *treep ? -1 : 1);
 
-    /* Contents are equal up to the smallest length. */
-    return ((usz == tsz) ? 0 : (usz < tsz) ? -1 : 1);
+    if (len >= WT_VECTOR_SIZE)
+        ret_val = __lex_compare_ge_16(
+          (const uint8_t *)user_item->data, (const uint8_t *)tree_item->data, len, lencmp);
+    else
+        ret_val = __lex_compare_lt_16(
+          (const uint8_t *)user_item->data, (const uint8_t *)tree_item->data, len, lencmp);
+
+    return (ret_val);
 }
 
 /*
@@ -217,65 +281,6 @@ final128:
 
     match += (size_t)__builtin_clzll(u64 ^ t64) / 8;
     *matchp = match;
-
-    return (u64 < t64 ? -1 : u64 > t64 ? 1 : lencmp);
-}
-
-/*
- * __lex_compare_lt_16 --
- *     Lexicographic comparison routine for data less than 16 bytes. Returns: < 0 if user_item is
- *     lexicographically < tree_item, = 0 if user_item is lexicographically = tree_item, > 0 if
- *     user_item is lexicographically > tree_item. We use the names "user" and "tree" so it's clear
- *     in the btree code which the application is looking at when we call its comparison function.
- */
-static WT_INLINE int
-__lex_compare_lt_16(const uint8_t *ustartp, const uint8_t *tstartp, size_t len, int lencmp)
-{
-    uint64_t ta, tb, ua, ub, u64, t64;
-    const uint8_t *tendp, *uendp;
-
-    uendp = ustartp + len;
-    tendp = tstartp + len;
-    if (len & sizeof(uint64_t)) {
-        memcpy(&ua, ustartp, sizeof(uint64_t));
-        memcpy(&ta, tstartp, sizeof(uint64_t));
-        memcpy(&ub, uendp - sizeof(uint64_t), sizeof(uint64_t));
-        memcpy(&tb, tendp - sizeof(uint64_t), sizeof(uint64_t));
-    } else if (len & sizeof(uint32_t)) {
-        uint32_t UA, TA, UB, TB;
-        memcpy(&UA, ustartp, sizeof(uint32_t));
-        memcpy(&TA, tstartp, sizeof(uint32_t));
-        memcpy(&UB, uendp - sizeof(uint32_t), sizeof(uint32_t));
-        memcpy(&TB, tendp - sizeof(uint32_t), sizeof(uint32_t));
-        ua = UA;
-        ta = TA;
-        ub = UB;
-        tb = TB;
-    } else if (len & sizeof(uint16_t)) {
-        uint16_t UA, TA, UB, TB;
-        memcpy(&UA, ustartp, sizeof(uint16_t));
-        memcpy(&TA, tstartp, sizeof(uint16_t));
-        memcpy(&UB, uendp - sizeof(uint16_t), sizeof(uint16_t));
-        memcpy(&TB, tendp - sizeof(uint16_t), sizeof(uint16_t));
-        ua = UA;
-        ta = TA;
-        ub = UB;
-        tb = TB;
-    } else if (len & sizeof(uint8_t)) {
-        uint8_t UA, TA;
-        memcpy(&UA, ustartp, sizeof(uint8_t));
-        memcpy(&TA, tstartp, sizeof(uint8_t));
-        return (UA < TA ? -1 : UA > TA ? 1 : lencmp);
-    } else
-        return (lencmp);
-
-    u64 = ua == ta ? ub : ua;
-    t64 = ua == ta ? tb : ta;
-
-#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
-    u64 = __builtin_bswap64(u64);
-    t64 = __builtin_bswap64(t64);
-#endif
 
     return (u64 < t64 ? -1 : u64 > t64 ? 1 : lencmp);
 }
