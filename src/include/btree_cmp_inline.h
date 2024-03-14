4be8b13dc0ae3c928f4ff1a8640950401d5a5ee8
Chenhao Qu <chenhao.qu@mongodb.com>
2024-03-14 02:59:43 +0000

    Add separate intel implementation

--- src/include/btree_cmp_inline.h
+++ src/include/btree_cmp_inline.h
@@ -21,6 +21,7 @@
 #define WT_ALIGNED_16(p) (((uintptr_t)(p)&0x0f) == 0)
 #define WT_VECTOR_SIZE 16 /* chunk size */
 
+#ifndef HAVE_X86INTRIN_H
 /*
  * __lex_compare_ge_16 --
  *     Lexicographic comparison routine for data greater than or equal to 16 bytes. Returns: < 0 if
@@ -160,6 +161,54 @@ __wt_lex_compare(const WT_ITEM *user_item, const WT_ITEM *tree_item)
 
     return (ret_val);
 }
+#else
+/*
+ * __wt_lex_compare --
+ *     Lexicographic comparison routine. Returns: < 0 if user_item is lexicographically < tree_item,
+ *     = 0 if user_item is lexicographically = tree_item, > 0 if user_item is lexicographically >
+ *     tree_item. We use the names "user" and "tree" so it's clear in the btree code which the
+ *     application is looking at when we call its comparison function.
+ */
+static WT_INLINE int
+__wt_lex_compare(const WT_ITEM *user_item, const WT_ITEM *tree_item)
+{
+    size_t len, usz, tsz;
+    const uint8_t *userp, *treep;
+
+    usz = user_item->size;
+    tsz = tree_item->size;
+    len = WT_MIN(usz, tsz);
+
+    userp = (const uint8_t *)user_item->data;
+    treep = (const uint8_t *)tree_item->data;
+
+    /* Use vector instructions if we'll execute at least 2 of them. */
+    if (len >= WT_VECTOR_SIZE * 2) {
+        size_t remain;
+        __m128i res_eq, u, t;
+
+        remain = len % WT_VECTOR_SIZE;
+        len -= remain;
+        for (; len > 0; len -= WT_VECTOR_SIZE, userp += WT_VECTOR_SIZE, treep += WT_VECTOR_SIZE) {
+            u = _mm_load_si128((const __m128i *)userp);
+            t = _mm_load_si128((const __m128i *)treep);
+            res_eq = _mm_cmpeq_epi8(u, t);
+            if (_mm_movemask_epi8(res_eq) != 65535)
+                break;
+        }
+        len += remain;
+    }
+    /*
+     * Use the non-vectorized version for the remaining bytes and for the small key sizes.
+     */
+    for (; len > 0; --len, ++userp, ++treep)
+        if (*userp != *treep)
+            return (*userp < *treep ? -1 : 1);
+
+    /* Contents are equal up to the smallest length. */
+    return ((usz == tsz) ? 0 : (usz < tsz) ? -1 : 1);
+}
+#endif
 
 /*
  * __wt_compare --
@@ -230,6 +279,7 @@ __wt_compare_bounds(WT_SESSION_IMPL *session, WT_CURSOR *cursor, WT_ITEM *key, u
     return (0);
 }
 
+#ifndef HAVE_X86INTRIN_H
 /*
  * __lex_compare_skip_ge_16 --
  *     Lexicographic comparison routine for data greater than or equal to 16 bytes, skipping leading
@@ -336,6 +386,78 @@ __wt_lex_compare_skip(
 
     return (ret_val);
 }
+#else
+/*
+ * __wt_lex_compare_skip --
+ *     Lexicographic comparison routine, skipping leading bytes. Returns: < 0 if user_item is
+ *     lexicographically < tree_item = 0 if user_item is lexicographically = tree_item > 0 if
+ *     user_item is lexicographically > tree_item We use the names "user" and "tree" so it's clear
+ *     in the btree code which the application is looking at when we call its comparison function.
+ */
+static WT_INLINE int
+__wt_lex_compare_skip(
+  WT_SESSION_IMPL *session, const WT_ITEM *user_item, const WT_ITEM *tree_item, size_t *matchp)
+{
+    size_t len, usz, tsz;
+    const uint8_t *userp, *treep;
+    int ret_val;
+
+    usz = user_item->size;
+    tsz = tree_item->size;
+    len = WT_MIN(usz, tsz) - *matchp;
+
+    userp = (const uint8_t *)user_item->data + *matchp;
+    treep = (const uint8_t *)tree_item->data + *matchp;
+
+    /* Use vector instructions if we'll execute at least 2 of them. */
+    if (len >= WT_VECTOR_SIZE * 2) {
+        size_t remain;
+        __m128i res_eq, u, t;
+
+        remain = len % WT_VECTOR_SIZE;
+        len -= remain;
+        for (; len > 0; len -= WT_VECTOR_SIZE, userp += WT_VECTOR_SIZE, treep += WT_VECTOR_SIZE,
+             *matchp += WT_VECTOR_SIZE) {
+            u = _mm_load_si128((const __m128i *)userp);
+            t = _mm_load_si128((const __m128i *)treep);
+            res_eq = _mm_cmpeq_epi8(u, t);
+            if (_mm_movemask_epi8(res_eq) != 65535)
+                break;
+        }
+        len += remain;
+    }
+    ret_val = 0;
+    /*
+     * Use the non-vectorized version for the remaining bytes and for the small key sizes.
+     */
+    for (; len > 0; --len, ++userp, ++treep, ++*matchp)
+        if (*userp != *treep) {
+            ret_val = *userp < *treep ? -1 : 1;
+            break;
+        }
+
+    /* Contents are equal up to the smallest length. */
+    if (ret_val == 0)
+        ret_val = ((usz == tsz) ? 0 : (usz < tsz) ? -1 : 1);
+
+#ifdef HAVE_DIAGNOSTIC
+    /*
+     * There are various optimizations in the code to skip comparing prefixes that are known to be
+     * the same. If configured, check that the prefixes actually match.
+     */
+    if (FLD_ISSET(S2C(session)->timing_stress_flags, WT_TIMING_STRESS_PREFIX_COMPARE)) {
+        int full_cmp_ret;
+        full_cmp_ret = __wt_lex_compare(user_item, tree_item);
+        WT_ASSERT_ALWAYS(NULL, full_cmp_ret == ret_val,
+          "Comparison that skipped prefix returned different result than a full comparison");
+    }
+#else
+    WT_UNUSED(session);
+#endif
+
+    return (ret_val);
+}
+#endif
 
 /*
  * __wt_compare_skip --
