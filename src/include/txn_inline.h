e86711db75bc0362e405706a544b303a28b768da
Chenhao Qu <chenhao.qu@mongodb.com>
2024-03-18 02:51:03 +0000

    Optimize code

--- src/include/txn_inline.h
+++ src/include/txn_inline.h
@@ -373,8 +373,13 @@ __wt_txn_op_delete_commit_apply_timestamps(WT_SESSION_IMPL *session, WT_REF *ref
         WT_ASSERT(session, previous_state == WT_REF_MEM);
         WT_ASSERT(session, ref->page != NULL && ref->page->modify != NULL);
         if ((updp = ref->page->modify->inst_updates) != NULL)
-            for (; *updp != NULL; ++updp) {
-                if ((*updp)->start_ts == WT_TS_NONE) {
+            /*
+             * If we have already set the timestamp, no need to set the timestamp again. We have
+             * either set the timestamp on all the updates, or we have set the timestamp on none of
+             * the updates.
+             */
+            if ((*updp) != NULL && (*updp)->start_ts == WT_TS_NONE) {
+                for (; *updp != NULL; ++updp) {
                     (*updp)->start_ts = commit_timestamp;
                     (*updp)->durable_ts = commit_timestamp;
                 }
