7e8e97dfb370f57ead8c29954a55ed91f5deb1c4
Etienne Petrel <etienne.petrel@mongodb.com>
2024-03-20 00:24:34 +0000

    WT-12680 Add a log when parallel compaction is executed on the same table

--- src/block/block_compact.c
+++ src/block/block_compact.c
@@ -18,8 +18,12 @@ int
 __wt_block_compact_start(WT_SESSION_IMPL *session, WT_BLOCK *block)
 {
 
-    if (block->compact_session_id != WT_SESSION_ID_INVALID)
+    if (block->compact_session_id != WT_SESSION_ID_INVALID) {
+        __wt_verbose_info(session, WT_VERB_COMPACT,
+          "Compaction already happening on data handle %s by session %p, returning EBUSY.",
+          block->name, (void *)session);
         return (EBUSY);
+    }
 
     /* Switch to first-fit allocation. */
     __wt_block_configure_first_fit(block, true);
