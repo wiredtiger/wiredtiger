9397d28c927a3972c98eff876e639ec64597bf10
Chenhao Qu <chenhao.qu@mongodb.com>
2024-03-23 04:27:05 +0000

    Fix compile

--- src/include/btree_cmp_inline.h
+++ src/include/btree_cmp_inline.h
@@ -29,7 +29,6 @@ static WT_INLINE int
 __lex_compare_gt_16(const uint8_t *ustartp, const uint8_t *tstartp, size_t len, int lencmp)
 {
     __m128i res_eq, t, u;
-    uint64_t t64, u64;
     int32_t eq_bits;
     size_t i, final_bytes;
 
@@ -307,7 +306,6 @@ __lex_compare_skip_gt_16(
   const uint8_t *ustartp, const uint8_t *tstartp, size_t len, int lencmp, size_t *matchp)
 {
     __m128i res_eq, t, u;
-    uint64_t t64, u64;
     int32_t eq_bits;
     size_t match, final_bytes;
 
