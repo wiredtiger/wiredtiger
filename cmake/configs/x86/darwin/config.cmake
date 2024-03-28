7e7d5372504a2581536fde6ad68ba367e60a737a
Jasmine Bi <jasmine.bi@mongodb.com>
2024-03-28 05:25:28 +0000

    used add_cmake_flag for all to check before appending

--- cmake/configs/x86/darwin/config.cmake
+++ cmake/configs/x86/darwin/config.cmake
@@ -5,6 +5,6 @@ set(WT_POSIX ON CACHE BOOL "")
 # Enable x86 SIMD instrinsics when available.
 CHECK_INCLUDE_FILE("x86intrin.h" has_x86intrin)
 if(has_x86intrin)
-    add_compile_options($<$<COMPILE_LANGUAGE:C>:-DHAVE_X86INTRIN_H>)
+    add_cmake_flag(CMAKE_C_FLAGS -DHAVE_X86INTRIN_H)
 endif()
 unset(has_x86intrin CACHE)
