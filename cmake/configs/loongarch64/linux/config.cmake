8eadf402467dc78036b4572744e3578c01ff2aca
Jasmine Bi <jasmine.bi@mongodb.com>
2024-03-28 05:25:28 +0000

    Used add_cmake_flag for all to check before appending

--- cmake/configs/loongarch64/linux/config.cmake
+++ cmake/configs/loongarch64/linux/config.cmake
@@ -7,7 +7,7 @@ set(WT_POSIX ON CACHE BOOL "")
 # Linux requires '_GNU_SOURCE' to be defined for access to GNU/Linux extension functions
 # e.g. Access to O_DIRECT on Linux. Append this macro to our compiler flags for Linux-based
 # builds.
-add_compile_options($<$<COMPILE_LANGUAGE:C>:-D_GNU_SOURCE>)
+add_cmake_flag(CMAKE_C_FLAGS -D_GNU_SOURCE)
 
 # Linux requires buffers aligned to 4KB boundaries for O_DIRECT to work.
 set(WT_BUFFER_ALIGNMENT_DEFAULT "4096" CACHE STRING "")
