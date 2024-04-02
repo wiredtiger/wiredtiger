b85b81e090e49e8d2ba9e9a3aa497478f64cf4b4
Hari Babu Kommi <haribabu.kommi@mongodb.com>
2024-04-03 09:57:44 +1100

    use divide instead of multiply for better compiler optimization

--- src/include/serial_inline.h
+++ src/include/serial_inline.h
@@ -303,7 +303,7 @@ __wt_update_serial(WT_SESSION_IMPL *session, WT_CURSOR_BTREE *cbt, WT_PAGE *page
      * allowed by btree. This is to prevent unnecessary page splits due to the obsolete updates
      * present on the page.
      */
-    if (page->memory_footprint > (S2BT(session)->splitmempage * 0.5))
+    if (page->memory_footprint > (S2BT(session)->splitmempage / 2))
         __wt_update_obsolete_check(session, cbt->ref, upd->next, false);
 
     return (0);
