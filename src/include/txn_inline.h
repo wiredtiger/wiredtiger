c4d49b7ca446bcd32d1a8b2d46af745dfa78d06c
Luke Pearson <luke.pearson@mongodb.com>
2024-04-08 22:06:40 +0000

    Improve the function comment

--- src/include/txn_inline.h
+++ src/include/txn_inline.h
@@ -100,7 +100,8 @@ __wt_txn_op_set_recno(WT_SESSION_IMPL *session, uint64_t recno)
 
 /*
  * __wt_txn_op_set_key --
- *     Set the latest transaction operation with the given key.
+ *     Copy the given key onto the most recent transaction operation. This function early exits if
+ *     the transaction cannot prepare.
  */
 static WT_INLINE int
 __wt_txn_op_set_key(WT_SESSION_IMPL *session, const WT_ITEM *key)
