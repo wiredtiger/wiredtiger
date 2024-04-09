71a49acc13d2dc48cc4de9aa0c62fe4db4099262
Yury Ershov <yuriy.ershov@gmail.com>
2024-04-09 10:05:31 +1000

    Add comment

--- src/evict/evict_lru.c
+++ src/evict/evict_lru.c
@@ -1885,10 +1885,12 @@ __evict_walk_tree(WT_SESSION_IMPL *session, WT_EVICT_QUEUE *queue, u_int max_ent
      */
     switch (btree->evict_start_type) {
     case WT_EVICT_WALK_NEXT:
+        /* Each time when evict_ref is null, alternate between linear and random walk */
         if (btree->evict_ref == NULL && (++btree->linear_walk_restarts) & 1)
             goto rand_next;
         break;
     case WT_EVICT_WALK_PREV:
+        /* Each time when evict_ref is null, alternate between linear and random walk */
         if (btree->evict_ref == NULL && (++btree->linear_walk_restarts) & 1)
             goto rand_prev;
         FLD_SET(walk_flags, WT_READ_PREV);
