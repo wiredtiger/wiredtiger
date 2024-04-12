1573f8bc392127631887b5f167d61d332d84263a
Don Anderson <donald.anderson@mongodb.com>
2024-04-12 02:48:15 +0000

    Added comment.

--- src/include/cursor_inline.h
+++ src/include/cursor_inline.h
@@ -52,6 +52,10 @@ __cursor_set_recno(WT_CURSOR_BTREE *cbt, uint64_t v)
 static WT_INLINE int
 __cursor_copy_release(WT_CURSOR *cursor)
 {
+    /*
+     * Make a redundant test first, since it should always fail for high performance situations and
+     * we can exit the function quickly.
+     */
     if (F_ISSET(cursor, WT_CURSTD_DEBUG_COPY_KEY | WT_CURSTD_DEBUG_COPY_VALUE) &&
       FLD_ISSET(S2C(CUR2S(cursor))->debug_flags, WT_CONN_DEBUG_CURSOR_COPY)) {
         if (F_ISSET(cursor, WT_CURSTD_DEBUG_COPY_KEY)) {
