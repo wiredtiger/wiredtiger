77e589afb7b8f446bd32c23ecc8e30a71e1425ff
Don Anderson <donald.anderson@mongodb.com>
2024-04-12 01:11:45 +0000

    Change copy_release so code mods are equivalent to original.

--- src/include/cursor_inline.h
+++ src/include/cursor_inline.h
@@ -52,7 +52,8 @@ __cursor_set_recno(WT_CURSOR_BTREE *cbt, uint64_t v)
 static WT_INLINE int
 __cursor_copy_release(WT_CURSOR *cursor)
 {
-    if (F_ISSET(cursor, WT_CURSTD_DEBUG_COPY_KEY | WT_CURSTD_DEBUG_COPY_VALUE)) {
+    if (F_ISSET(cursor, WT_CURSTD_DEBUG_COPY_KEY | WT_CURSTD_DEBUG_COPY_VALUE) &&
+      FLD_ISSET(S2C(CUR2S(cursor))->debug_flags, WT_CONN_DEBUG_CURSOR_COPY)) {
         if (F_ISSET(cursor, WT_CURSTD_DEBUG_COPY_KEY)) {
             WT_RET(__wt_cursor_copy_release_item(cursor, &cursor->key));
             F_CLR(cursor, WT_CURSTD_DEBUG_COPY_KEY);
