3e48199f1133c9868bacf278620702bcc874a2f0
y123456yz <3058078489@qq.com>
2024-04-14 00:32:05 +0800

    fix test_txn28.py bug

--- test/suite/test_txn28.py
+++ test/suite/test_txn28.py
@@ -26,18 +26,13 @@
 # ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 # OTHER DEALINGS IN THE SOFTWARE.
 
-import wiredtiger, wttest, time
+import wiredtiger, wttest, time, os, re
 from wtdataset import SimpleDataSet
 
 # test_txn28.py
 #   Test that checking the snapshot array is correctly outputted.
 class test_txn28(wttest.WiredTigerTestCase):
-    conn_config = 'cache_size=1MB'
-
-    def get_lines_containing_substring(self, text, substring):
-        lines = text.split('\n')
-        return [line for line in lines if substring in line]
-
+    # Get the number after a substring in the string
     def get_numbers_after_substring(self, text, sub_str):
         pattern = rf'{re.escape(sub_str)}\s*(\d+)'
         match = re.search(pattern, text)
@@ -46,13 +41,24 @@ class test_txn28(wttest.WiredTigerTestCase):
         else:
             return None
 
-    # Get the content in the first square bracket.
-    def count_numbers_in_string(self, s):
-        return len(re.findall(r'\d+', s))
+    # Get the number of digits between two strings
+    def count_integers_between_substrings(self, text, sub1, sub2):
+        # Regular expression matching integers
+        pattern = re.compile(r'\d+')
+        # Find the position of the substring
+        start_index = text.find(sub1)
+        end_index = text.find(sub2, start_index + len(sub1))
+        # If the substring does not exist, return -1
+        if start_index == -1 or end_index == -1:
+            return -1
+        # Extract text between substrings
+        substring = text[start_index + len(sub1):end_index]
+        # Use regular expressions to match integers and calculate quantities
+        return len(pattern.findall(substring))
     
     def test_snapshot_array_dump(self):
         uri = "table:txn28"
-        # Create a very basic table.
+        # Create a basic table.
         ds = SimpleDataSet(self, uri, 10, key_format='S', value_format='S')
         ds.populate()
 
@@ -66,21 +72,34 @@ class test_txn28(wttest.WiredTigerTestCase):
         session2 = self.conn.open_session()
         cursor2 = session2.open_cursor(uri)
         session2.begin_transaction()
-        cursor2.set_key(ds.key(6))
-        cursor2.set_value("bbb")
+        cursor2[ds.key(6)] = "bbb"
 
+        # The third session.
+        session3 = self.conn.open_session()
+        cursor3 = session2.open_cursor(uri)
+        session3.begin_transaction()
+        cursor3[ds.key(7)] = "ccc"
+
+        # Get the stdout.txt file directory
+        stdout_path = os.path.join(os.getcwd(), 'stdout.txt')
         max_snapshot_list_item_count = 0         
-        # Get the "transaction state dump" info, get content of " snapshot count: " and "snapshot: [xxx]".
+        # Get the "transaction state dump" info, get content of " snapshot count: " and "snapshot: [xxx]"'s digital num.
         with self.expectedStdoutPattern('transaction state dump'):
-            txn_debug_info=self.conn.debug_info('txn')
-            snapshot_lines = self.get_lines_containing_substring(txn_debug_info, " snapshot count: ")
-            for line in snapshot_lines:
-                snapshot_count = self.get_numbers_after_substring(line, ", snapshot count: ")
-                snapshot_list_item_count = self.count_numbers_in_string(line)
-                self.assertEquals(snapshot_count, snapshot_list_item_count)        
-                if max_snapshot_list_item_count < snapshot_list_item_count:
-                    max_snapshot_list_item_count = snapshot_list_item_count
-        self.assertEquals(max_snapshot_list_item_count, 1)
- 
+            self.conn.debug_info('txn')
+
+        with open(stdout_path, 'r') as file:
+            for line in file:
+                snapshot_count_str = ", snapshot count: "
+                if snapshot_count_str in line:
+                    snapshot_count = int(self.get_numbers_after_substring(line, snapshot_count_str))
+                    sub1 = ", snapshot: ["
+                    sub2 = "], commit_timestamp:"
+                    snapshot_list_item_count = self.count_integers_between_substrings(line, sub1, sub2)
+                    self.assertEquals(snapshot_count, snapshot_list_item_count)
+                    if max_snapshot_list_item_count < snapshot_list_item_count:
+                        max_snapshot_list_item_count = snapshot_list_item_count
+
+        self.assertEquals(max_snapshot_list_item_count, 2)
+        session3.commit_transaction()
         session2.commit_transaction()
         session1.commit_transaction()
