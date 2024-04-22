5d32419a4ebdc0e1e895ca1a6b1376ce0803a4a1
Chenhao Qu <chenhao.qu@mongodb.com>
2024-04-22 00:00:39 +0000

    Fix inverted bound

--- src/cursor/cur_hs.c
+++ src/cursor/cur_hs.c
@@ -633,7 +633,7 @@ __wt_curhs_search_near_before(WT_CURSOR *cursor)
     session = CUR2S(cursor);
     conn = S2C(session);
 
-    WT_RET(cursor->bound(cursor, conn->hs_compiled_conf.compiled_lower_bound_conf));
+    WT_RET(cursor->bound(cursor, conn->hs_compiled_conf.compiled_upper_bound_conf));
     WT_RET(cursor->prev(cursor));
     WT_RET(cursor->bound(cursor, conn->hs_compiled_conf.compiled_clear_bound_conf));
 
@@ -653,7 +653,7 @@ __wt_curhs_search_near_after(WT_CURSOR *cursor)
     session = CUR2S(cursor);
     conn = S2C(session);
 
-    WT_RET(cursor->bound(cursor, conn->hs_compiled_conf.compiled_upper_bound_conf));
+    WT_RET(cursor->bound(cursor, conn->hs_compiled_conf.compiled_lower_bound_conf));
     WT_RET(cursor->next(cursor));
     WT_RET(cursor->bound(cursor, conn->hs_compiled_conf.compiled_clear_bound_conf));
 
