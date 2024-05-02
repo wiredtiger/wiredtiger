6d7073fcb38e6b54c83b3f00b17a3d6fe6d8d7c3
Etienne Petrel <etienne.petrel@mongodb.com>
2024-05-02 06:02:43 +0000

    Add test tags

--- test/suite/test_dictionary02.py
+++ test/suite/test_dictionary02.py
@@ -26,6 +26,10 @@
 # ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 # OTHER DEALINGS IN THE SOFTWARE.
 #
+# [TEST_TAGS]
+# compression
+# [END_TAGS]
+#
 # test_dictionary02.py
 #       Test cells with same values are reused through the dictionary despite RLE information.
 
