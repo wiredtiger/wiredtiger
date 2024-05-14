ed51b5dbdc5dd51e40f28a45f63191a31e9804bb
sueloverso <sue@mongodb.com>
2024-05-14 14:05:29 -0400

    WT-12649 Always send in and check remap boolean. (#10591)
    In WT-11555 I didn't add in the remap boolean for this code path because
    I thought it couldn't race. I was wrong and it too needs the fix from
    that ticket. @keitharnoldsmith because you reviewed the other PR.
    @raviprakashgiri29 because you've been involved in this ticket. I'm
    going to let it run a while, but I was readily reproducing.
    
    (cherry picked from commit 9b8e54f886832840c62954653969df0c013b2c5c)

--- src/os_posix/os_fs.c
+++ src/os_posix/os_fs.c
@@ -744,7 +744,7 @@ __posix_file_write_mmap(
     static int remap_opportunities;
     WT_FILE_HANDLE_POSIX *pfh;
     WT_SESSION_IMPL *session;
-    bool mmap_success;
+    bool mmap_success, remap;
 
     session = (WT_SESSION_IMPL *)wt_session;
     pfh = (WT_FILE_HANDLE_POSIX *)file_handle;
@@ -796,8 +796,9 @@ use_syscall:
         /* If we are actively extending the file, don't remap it on every write. */
         if ((remap_opportunities++) % WT_REMAP_SKIP == 0) {
             __wti_posix_prepare_remap_resize_file(
-              file_handle, wt_session, offset + (wt_off_t)len, NULL);
-            __wti_posix_remap_resize_file(file_handle, wt_session);
+              file_handle, wt_session, offset + (wt_off_t)len, &remap);
+            if (remap)
+                __wti_posix_remap_resize_file(file_handle, wt_session);
             WT_STAT_CONN_INCRV(session, block_remap_file_write, 1);
         }
     return (0);
@@ -1107,9 +1108,9 @@ __wti_posix_prepare_remap_resize_file(
     sleep_usec = 10;
     yield_count = 0;
 
+    WT_ASSERT(session, remap != NULL);
     /* If there is no mapped region or it is already the right size, there is nothing to do. */
-    if (remap != NULL)
-        *remap = false;
+    *remap = false;
     if (pfh->mmap_buf == NULL || pfh->mmap_size == len)
         return;
 
@@ -1124,8 +1125,7 @@ wait:
     if (__wt_atomic_casv32(&pfh->mmap_resizing, 0, 1) == false)
         goto wait;
 
-    if (remap != NULL)
-        *remap = true;
+    *remap = true;
     /*
      * Wait for any sessions using the region for I/O to finish. Now that we have set the resizing
      * flag, new sessions will not use the region, defaulting to system calls instead.
