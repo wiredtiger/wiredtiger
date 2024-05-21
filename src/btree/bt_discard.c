8fcbb2ec726dc487be0eba1f4313a19c29213a2f
sueloverso <sue@mongodb.com>
2024-05-21 15:27:48 -0400

    WT-12986 External subsystem functions wt -> wti. (#10589)

--- src/btree/bt_discard.c
+++ src/btree/bt_discard.c
@@ -242,13 +242,13 @@ __free_page_modify(WT_SESSION_IMPL *session, WT_PAGE *page)
 }
 
 /*
- * __wt_ref_addr_safe_free --
+ * __wti_ref_addr_safe_free --
  *     Any thread that is reviewing the address in a WT_REF, must also be holding a split generation
  *     to ensure that the page index they are using remains valid. Utilize the same generation type
  *     to safely free the address once all users of it have left the generation.
  */
 void
-__wt_ref_addr_safe_free(WT_SESSION_IMPL *session, void *p, size_t len)
+__wti_ref_addr_safe_free(WT_SESSION_IMPL *session, void *p, size_t len)
 {
     WT_DECL_RET;
     uint64_t split_gen;
@@ -306,17 +306,17 @@ __wt_ref_addr_free(WT_SESSION_IMPL *session, WT_REF *ref)
     }
 
     if (home == NULL || __wt_off_page(home, ref_addr)) {
-        __wt_ref_addr_safe_free(session, ((WT_ADDR *)ref_addr)->addr, ((WT_ADDR *)ref_addr)->size);
-        __wt_ref_addr_safe_free(session, ref_addr, sizeof(WT_ADDR));
+        __wti_ref_addr_safe_free(session, ((WT_ADDR *)ref_addr)->addr, ((WT_ADDR *)ref_addr)->size);
+        __wti_ref_addr_safe_free(session, ref_addr, sizeof(WT_ADDR));
     }
 }
 
 /*
- * __wt_free_ref --
+ * __wti_free_ref --
  *     Discard the contents of a WT_REF structure (optionally including the pages it references).
  */
 void
-__wt_free_ref(WT_SESSION_IMPL *session, WT_REF *ref, int page_type, bool free_pages)
+__wti_free_ref(WT_SESSION_IMPL *session, WT_REF *ref, int page_type, bool free_pages)
 {
     WT_IKEY *ikey;
 
@@ -377,17 +377,18 @@ __free_page_int(WT_SESSION_IMPL *session, WT_PAGE *page)
     uint32_t i;
 
     for (pindex = WT_INTL_INDEX_GET_SAFE(page), i = 0; i < pindex->entries; ++i)
-        __wt_free_ref(session, pindex->index[i], page->type, false);
+        __wti_free_ref(session, pindex->index[i], page->type, false);
 
     __wt_free(session, pindex);
 }
 
 /*
- * __wt_free_ref_index --
+ * __wti_free_ref_index --
  *     Discard a page index and its references.
  */
 void
-__wt_free_ref_index(WT_SESSION_IMPL *session, WT_PAGE *page, WT_PAGE_INDEX *pindex, bool free_pages)
+__wti_free_ref_index(
+  WT_SESSION_IMPL *session, WT_PAGE *page, WT_PAGE_INDEX *pindex, bool free_pages)
 {
     WT_REF *ref;
     uint32_t i;
@@ -409,7 +410,7 @@ __wt_free_ref_index(WT_SESSION_IMPL *session, WT_PAGE *page, WT_PAGE_INDEX *pind
           __wt_hazard_check_assert(session, ref, false),
           "Attempting to discard ref to a page with hazard pointers");
 
-        __wt_free_ref(session, ref, page->type, free_pages);
+        __wti_free_ref(session, ref, page->type, free_pages);
     }
     __wt_free(session, pindex);
 }
