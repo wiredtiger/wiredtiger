fc1d15835b7e367acbb29e76b205d7b7cc66b21c
Hari Babu Kommi <haribabu.kommi@mongodb.com>
2024-05-28 10:08:43 +1000

    simplify the test

--- test/suite/test_cc07.py
+++ test/suite/test_cc07.py
@@ -1,14 +1,10 @@
 #!/usr/bin/env python
 
 import wttest
+from test_cc01 import test_cc_base
 
 # test_cc07.py
-class test_cc07(wttest.WiredTigerTestCase):
-    num_tables = 1
-    create_params = 'key_format=i,value_format=S'
-    table_numkv = 100 * 1000
-    uri_prefix = 'table:cc07'
-    value_size = 1024
+class test_cc07(test_cc_base):
 
     def populate(self, uri, start_key, num_keys, value_size=1024):
         c = self.session.open_cursor(uri, None)
@@ -19,37 +15,22 @@ class test_cc07(wttest.WiredTigerTestCase):
         c.close()
 
     def test_cc(self):
+        create_params = 'key_format=i,value_format=S'
+        nrows = 10000
+        uri = 'table:cc07'
+        value_size = 1024
 
-        # Create N tables.
-        uris = []
-        for i in range(self.num_tables):
-            uri = self.uri_prefix + f'_{i}'
-            uris.append(uri)
-            self.session.create(uri, self.create_params)
-
-        # Append some data.
-        for uri in uris:
-            self.populate(uri, 0, self.table_numkv)
-
-        session2 = self.conn.open_session()
-        long_txn_running = False
+        self.session.create(uri, create_params)
 
         for i in range(10):
-            # Stop and start a long running transaction.
-            if long_txn_running:
-                session2.rollback_transaction()
-            # Update the oldest ts to make content obsolete.
-            self.conn.set_timestamp('oldest_timestamp=' + self.timestamp_str(self.table_numkv * (i + 1) // 2))
-            session2.begin_transaction()
-            long_txn_running = True
-
             # Append some data.
-            for uri in uris:
-                self.populate(uri, self.table_numkv * (i + 1), self.table_numkv * (i + 2))
+            self.populate(uri, nrows * (i), nrows * (i + 1))
 
             # Checkpoint with cleanup.
-            self.session.checkpoint("debug=(checkpoint_cleanup=true)")
-            self.conn.set_timestamp('stable_timestamp=' + self.timestamp_str(self.table_numkv * (i + 2)))
+            self.conn.set_timestamp('stable_timestamp=' + self.timestamp_str(nrows * (i + 1)))
+            self.session.checkpoint()
+            self.conn.set_timestamp('oldest_timestamp=' + self.timestamp_str(nrows * (i + 1)))
 
-        session2.rollback_transaction()
+        self.session.checkpoint("debug=(checkpoint_cleanup=true)")
+        self.wait_for_cc_to_run()
 
