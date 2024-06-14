7a7d4705be7b015481570c54003d2fc4df7f1fdc
Andrew Morton <andrew.morton@mongodb.com>
2024-06-14 04:41:28 +0000

    Change type of retry to int

--- src/btree/bt_random.c
+++ src/btree/bt_random.c
@@ -388,7 +388,8 @@ __wt_random_descent(WT_SESSION_IMPL *session, WT_REF **refp, uint32_t flags, WT_
     WT_PAGE *page;
     WT_PAGE_INDEX *pindex;
     WT_REF *current, *descent;
-    uint32_t i, entries, retry;
+    uint32_t i, entries;
+    int retry;
     bool eviction, sample_inmem_page;
 
     *refp = NULL;
