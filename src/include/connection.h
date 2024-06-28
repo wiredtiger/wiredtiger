1b24c3b662dc313562b963a2abaacb707af6edce
Will Korteland <will.korteland@mongodb.com>
2024-06-28 04:19:50 +0000

    s_all

--- src/include/connection.h
+++ src/include/connection.h
@@ -295,10 +295,10 @@ struct __wt_name_flag {
  * Set all flags related to incremental backup in one macro. The flags do get individually cleared
  * at different times so there is no corresponding macro for clearing.
  */
-#define WT_CONN_SET_INCR_BACKUP(conn)                        \
-    do {                                                     \
-        F_SET((conn), WT_CONN_INCR_BACKUP);                  \
-        WT_STAT_CONN_SET(session, backup_incremental, 1);    \
+#define WT_CONN_SET_INCR_BACKUP(conn)                     \
+    do {                                                  \
+        F_SET((conn), WT_CONN_INCR_BACKUP);               \
+        WT_STAT_CONN_SET(session, backup_incremental, 1); \
     } while (0)
 
 /*
@@ -621,12 +621,12 @@ struct __wt_connection_impl {
 #define WT_CONN_LOG_ENABLED 0x004u         /* Logging is enabled */
 #define WT_CONN_LOG_EXISTED 0x008u         /* Log files found */
 #define WT_CONN_LOG_FORCE_DOWNGRADE 0x010u /* Force downgrade */
-#define WT_CONN_LOG_RECOVER_DIRTY 0x040u   /* Recovering unclean */
-#define WT_CONN_LOG_RECOVER_DONE 0x080u    /* Recovery completed */
-#define WT_CONN_LOG_RECOVER_ERR 0x100u     /* Error if recovery required */
-#define WT_CONN_LOG_RECOVER_FAILED 0x200u  /* Recovery failed */
-#define WT_CONN_LOG_REMOVE 0x400u          /* Removal is enabled */
-#define WT_CONN_LOG_ZERO_FILL 0x800u       /* Manually zero files */
+#define WT_CONN_LOG_RECOVER_DIRTY 0x020u   /* Recovering unclean */
+#define WT_CONN_LOG_RECOVER_DONE 0x040u    /* Recovery completed */
+#define WT_CONN_LOG_RECOVER_ERR 0x080u     /* Error if recovery required */
+#define WT_CONN_LOG_RECOVER_FAILED 0x100u  /* Recovery failed */
+#define WT_CONN_LOG_REMOVE 0x200u          /* Removal is enabled */
+#define WT_CONN_LOG_ZERO_FILL 0x400u       /* Manually zero files */
                                            /* AUTOMATIC FLAG VALUE GENERATION STOP 32 */
     uint32_t log_flags;                    /* Global logging configuration */
     WT_CONDVAR *log_cond;                  /* Log server wait mutex */
