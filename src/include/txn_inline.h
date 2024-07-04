7b4d6ce9a4971eca2d071eb020124a0eb0e9dcf3
Etienne Petrel <77254506+etienneptl@users.noreply.github.com>
2024-07-04 14:53:52 +1000

    Revert "WT-13092 Perform dirty eviction when the page has obsolete time windo…"
    This reverts commit 4db9ff290ab64e899debadba38f7e6a03b26150a.

--- src/include/txn_inline.h
+++ src/include/txn_inline.h
@@ -720,21 +720,6 @@ __txn_visible_all_id(WT_SESSION_IMPL *session, uint64_t id)
 }
 
 /*
- * __wt_txn_timestamp_visible_all --
- *     Check whether a given timestamp is either globally visible or obsolete.
- */
-static WT_INLINE bool
-__wt_txn_timestamp_visible_all(WT_SESSION_IMPL *session, wt_timestamp_t timestamp)
-{
-    wt_timestamp_t pinned_ts;
-
-    /* Compare the given timestamp to the pinned timestamp, if it exists. */
-    __wt_txn_pinned_timestamp(session, &pinned_ts);
-
-    return (pinned_ts != WT_TS_NONE && timestamp <= pinned_ts);
-}
-
-/*
  * __wt_txn_visible_all --
  *     Check whether a given time window is either globally visible or obsolete. For global
  *     visibility checks, the commit times are checked against the oldest possible readers in the
@@ -749,6 +734,8 @@ __wt_txn_timestamp_visible_all(WT_SESSION_IMPL *session, wt_timestamp_t timestam
 static WT_INLINE bool
 __wt_txn_visible_all(WT_SESSION_IMPL *session, uint64_t id, wt_timestamp_t timestamp)
 {
+    wt_timestamp_t pinned_ts;
+
     /*
      * When shutting down, the transactional system has finished running and all we care about is
      * eviction, make everything visible.
@@ -773,7 +760,10 @@ __wt_txn_visible_all(WT_SESSION_IMPL *session, uint64_t id, wt_timestamp_t times
         return (session->txn->checkpoint_oldest_timestamp != WT_TS_NONE &&
           timestamp <= session->txn->checkpoint_oldest_timestamp);
 
-    return (__wt_txn_timestamp_visible_all(session, timestamp));
+    /* If no oldest timestamp has been supplied, updates have to stay in cache. */
+    __wt_txn_pinned_timestamp(session, &pinned_ts);
+
+    return (pinned_ts != WT_TS_NONE && timestamp <= pinned_ts);
 }
 
 /*
