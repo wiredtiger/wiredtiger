feabde6d20e85a8a2b58349dae3f9edc47707758
Etienne Petrel <etienne.petrel@mongodb.com>
2024-07-13 15:32:11 +1000

    Update comments in struct __wt_heuristic_controls

--- src/include/connection.h
+++ src/include/connection.h
@@ -152,15 +152,17 @@ struct __wt_bucket_storage {
  */
 struct __wt_heuristic_controls {
     /*
-     * Number of btrees processed in the current checkpoint due to obsolete time window information.
+     * The controls below deal with the cleanup of obsolete time window information. This process
+     * can be configured on two levels:
+     *   - The maximum number of btrees to process in a single checkpoint,
+     *   - The maximum number of pages per btree to process in a single checkpoint.
      */
+
+    /* Number of btrees processed in the current checkpoint. */
     wt_shared uint32_t obsolete_tw_btree_count;
-    /* Maximum number of btrees that can be processed due to obsolete time window information. */
+    /* Maximum number of btrees that can be processed per checkpoint. */
     uint32_t obsolete_tw_btree_max;
-    /*
-     * Maximum number of pages that can be processed due to obsolete time window information per
-     * btree.
-     */
+    /* Maximum number of pages that can be processed per btree. */
     uint32_t obsolete_tw_pages_dirty_max;
 };
 
