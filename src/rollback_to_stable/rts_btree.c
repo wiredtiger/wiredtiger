d18d9dfbf9873dd28f0fa5ebebe9d409ef4f0cf7
Etienne Petrel <etienne.petrel@mongodb.com>
2024-07-18 16:43:10 +1000

    WT-13290 Update assertions in __rts_btree_abort_update to take into account scenarios with fast truncate

--- src/rollback_to_stable/rts_btree.c
+++ src/rollback_to_stable/rts_btree.c
@@ -95,11 +95,11 @@ __rts_btree_abort_update(WT_SESSION_IMPL *session, WT_ITEM *key, WT_UPDATE *firs
          */
         if (F_ISSET(S2C(session), WT_CONN_RECOVERING) && !WT_IS_HS(session->dhandle)) {
             WT_ASSERT(session, first_upd->type == WT_UPDATE_TOMBSTONE);
-            WT_ASSERT(session, first_upd->prepare_state == WT_PREPARE_INPROGRESS);
-            WT_ASSERT(session, F_ISSET(first_upd, WT_UPDATE_PREPARE_RESTORED_FROM_DS));
+            WT_ASSERT(session,
+              F_ISSET(
+                first_upd, WT_UPDATE_PREPARE_RESTORED_FROM_DS | WT_UPDATE_RESTORED_FAST_TRUNCATE));
             WT_ASSERT(session, !hs_update);
             WT_ASSERT(session, stable_upd->next == NULL);
-            WT_ASSERT(session, F_ISSET(upd, WT_UPDATE_RESTORED_FROM_DS));
             stable_upd->txnid = WT_TXN_NONE;
         }
 
