ea86c24336ac9e72babd524d94c793e5c8a72295
Etienne Petrel <etienne.petrel@mongodb.com>
2024-07-30 19:43:16 +1000

    Skip test with tiered enabled

--- test/suite/test_cc08.py
+++ test/suite/test_cc08.py
@@ -26,12 +26,14 @@
 # ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 # OTHER DEALINGS IN THE SOFTWARE.
 
+import wttest
 from test_cc01 import test_cc_base
 from wiredtiger import stat
 from wtscenario import make_scenarios
 
 # test_cc08.py
 # Verify checkpoint cleanup cleans up logged tables when configured in aggressive mode.
+@wttest.skip_for_hook("tiered", "FIXME-WT-9809 - Checkpoint cleanup does not support tiered tables")
 class test_cc08(test_cc_base):
     conn_config = 'statistics=(all),statistics_log=(json,wait=1,on_close=true),log=(enabled=true)'
 
