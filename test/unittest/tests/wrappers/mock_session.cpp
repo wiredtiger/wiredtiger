612f9226ebd09d451be97038fd4ee0e530581391
Luke Pearson <luke.pearson@mongodb.com>
2024-08-16 19:40:59 +0000

    WT-13402 Add a WT_EVENT_HANDLER to the mock session to avoid crashes from WT*_MSG

--- test/unittest/tests/wrappers/mock_session.cpp
+++ test/unittest/tests/wrappers/mock_session.cpp
@@ -15,6 +15,13 @@
 MockSession::MockSession(WT_SESSION_IMPL *session, std::shared_ptr<MockConnection> mockConnection)
     : _sessionImpl(session), _mockConnection(std::move(mockConnection))
 {
+    // We could initialize this in the list but order is important and this seems easier.
+    WT_CLEAR(_handler_wrap.handler);
+    _handler_wrap.handler.handle_error = handleWiredTigerError;
+    _handler_wrap.handler.handle_message = handleWiredTigerMessage;
+    _handler_wrap.mock_session = this;
+
+    _sessionImpl->event_handler = &_handler_wrap.handler;
 }
 
 MockSession::~MockSession()
@@ -33,4 +40,14 @@ MockSession::buildTestMockSession()
 
     // Construct an object that will now own the two pointers passed in.
     return std::shared_ptr<MockSession>(new MockSession(sessionImpl, mockConnection));
+}
+
+int handleWiredTigerError(WT_EVENT_HANDLER *handler, WT_SESSION *session, int error, const char *message) {
+    ((handler_wrap*)handler)->mock_session->add_callback_message(message);
+    return (0);
+}
+
+int handleWiredTigerMessage(WT_EVENT_HANDLER *handler, WT_SESSION *session, const char *message) {
+    ((handler_wrap*)handler)->mock_session->add_callback_message(message);
+    return (0);
 }
--- test/unittest/tests/wrappers/mock_session.h
+++ test/unittest/tests/wrappers/mock_session.h
@@ -10,8 +10,23 @@
 #define WT_MOCK_SESSION_H
 
 #include <memory>
+#include <vector>
 #include "wt_internal.h"
 #include "mock_connection.h"
+
+
+int handleWiredTigerError(WT_EVENT_HANDLER *handler, WT_SESSION *session, int error, const char *message);
+int handleWiredTigerMessage(WT_EVENT_HANDLER *handler, WT_SESSION *session, const char *message);
+
+/* Forward declare the class so we can include it in our wrapper. */
+class MockSession;
+
+/* This is a convenience type that lets us get back to our MockSession from the handler callback. */
+struct event_handler_wrapper {
+    WT_EVENT_HANDLER handler;
+    MockSession *mock_session;
+};
+typedef struct event_handler_wrapper handler_wrap;
 
 class MockSession {
 public:
@@ -27,6 +42,15 @@ public:
         return _mockConnection;
     };
 
+    void
+    add_callback_message(const char *message) {
+        _messages.push_back(message);
+    }
+
+    const std::string&
+    get_last_message() {
+        return _messages.back();
+    }
     static std::shared_ptr<MockSession> buildTestMockSession();
 
 private:
@@ -37,6 +61,8 @@ private:
 
     // This class is implemented such that it owns, and is responsible for freeing, this pointer
     WT_SESSION_IMPL *_sessionImpl;
+    handler_wrap _handler_wrap;
+    std::vector<std::string> _messages;
 };
 
 #endif // WT_MOCK_SESSION_H
