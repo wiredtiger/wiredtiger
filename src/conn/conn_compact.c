91536617fc8f8fd82ded03e86e4c25faaf5bc900
Zunyi Liu <43444919+zunyiliu@users.noreply.github.com>
2024-08-16 16:06:24 +1000

    Update src/conn/conn_compact.c
    Co-authored-by: Etienne Petrel <77254506+etienneptl@users.noreply.github.com>

--- src/conn/conn_compact.c
+++ src/conn/conn_compact.c
@@ -729,7 +729,7 @@ __wti_background_compact_server_create(WT_SESSION_IMPL *session)
     /*
      * Compaction does enough I/O it may be called upon to perform slow operations for the block
      * manager. Don't let the background compaction thread be pulled into eviction to limit
-     * performance impact, when the cache is under pressure, compaction should halt straight away.
+     * performance impacts.
      */
     session_flags = WT_SESSION_CAN_WAIT | WT_SESSION_IGNORE_CACHE_SIZE;
     WT_RET(__wt_open_internal_session(
