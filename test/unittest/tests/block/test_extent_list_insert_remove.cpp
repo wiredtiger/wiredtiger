97201a9a13cf74956b6b393eee7597b2eee69819
Tod Johnson <tod.johnson@mongodb.com>
2024-08-16 16:31:44 +0000

    WT-13388 Add comments to insert and verify lists giving the values or intervals.

--- test/unittest/tests/block/test_extent_list_insert_remove.cpp
+++ test/unittest/tests/block/test_extent_list_insert_remove.cpp
@@ -165,9 +165,9 @@ TEST_CASE("Extent Lists: block_ext_insert", "[extent_list2]")
         BREAK;
         /* Extents to insert */
         std::vector<off_size> insert_list{
-          off_size(3 * 4096, 4096),
-          off_size(4096, 4096),
-          off_size(5 * 4096, 4096),
+          off_size(3 * 4096, 4096), // [12,288, 16,383]
+          off_size(4096, 4096),     // [4,096, 8,191]
+          off_size(5 * 4096, 4096), // [20,480, 24,575]
         };
 
         /* Empty extent list */
@@ -230,9 +230,9 @@ TEST_CASE("Extent Lists: block_off_insert", "[extent_list2]")
         BREAK;
         /* Extents to insert */
         std::vector<off_size> insert_list{
-          off_size(3 * 4096, 4096),
-          off_size(4096, 4096),
-          off_size(5 * 4096, 4096),
+          off_size(3 * 4096, 4096), // [12,288, 16,383]
+          off_size(4096, 4096),     // [4,096, 8,191]
+          off_size(5 * 4096, 4096), // [20,480, 24,575]
         };
 
         /* Empty extent list */
@@ -290,7 +290,7 @@ TEST_CASE("Extent Lists: block_off_srch_pair", "[extent_list2]")
         verify_empty_extent_list(&extlist.off[0], &stack[0]);
 
         /* Verify */
-        std::vector<wt_off_t> expected_list{0, 4096, 3 * 4096};
+        std::vector<wt_off_t> expected_list{0, 4096, 3 * 4096}; // 3, 4096, 12,288
         WT_EXT dummy;
         for (const wt_off_t &expected : expected_list) {
 #ifdef DEBUG
@@ -310,21 +310,21 @@ TEST_CASE("Extent Lists: block_off_srch_pair", "[extent_list2]")
         BREAK;
         /* Extents to insert */
         std::vector<off_size> insert_list{
-          off_size(3 * 4096, 4096), // Second
-          off_size(4096, 4096),     // First
-          off_size(5 * 4096, 4096), // Third
+          off_size(3 * 4096, 4096), // Second [12,288, 16,383]
+          off_size(4096, 4096),     // First [4,096, 8,191]
+          off_size(5 * 4096, 4096), // Third [20,480, 24,575]
         };
 
         std::vector<search_before_after> expected_before_after{
-          search_before_after(0, nullptr, &insert_list[1]),    // Before first
-          search_before_after(4096, nullptr, &insert_list[1]), // At first
+          search_before_after(0, nullptr, &insert_list[1]),    // Before first 0
+          search_before_after(4096, nullptr, &insert_list[1]), // At first 4,096
           search_before_after(
-            2 * 4096, &insert_list[1], &insert_list[0]), // Between first and second
-          search_before_after(3 * 4096, &insert_list[1], &insert_list[0]), // At second
+            2 * 4096, &insert_list[1], &insert_list[0]), // Between first and second 8,192
+          search_before_after(3 * 4096, &insert_list[1], &insert_list[0]), // At second 12,288
           search_before_after(
-            4 * 4096, &insert_list[0], &insert_list[2]), // Between second and third
-          search_before_after(5 * 4096, &insert_list[0], &insert_list[2]), // At third
-          search_before_after(6 * 4096, &insert_list[2], nullptr),         // After third
+            4 * 4096, &insert_list[0], &insert_list[2]), // Between second and third 16,384
+          search_before_after(5 * 4096, &insert_list[0], &insert_list[2]), // At third 20,480
+          search_before_after(6 * 4096, &insert_list[2], nullptr),         // After third 24,576
         };
 
         /* Empty extent list */
@@ -414,33 +414,33 @@ TEST_CASE("Extent Lists: block_off_match", "[extent_list2]")
 
     /* Extents to insert */
     std::vector<off_size> insert_list{
-      off_size(3 * 4096, 4096), // Second
-      off_size(4096, 4096),     // First
-      off_size(5 * 4096, 4096), // Third
+      off_size(3 * 4096, 4096), // Second [12,288, 16,383]
+      off_size(4096, 4096),     // First [4,096, 8,191]
+      off_size(5 * 4096, 4096), // Third [20,480, 24,575]
     };
 
     std::vector<search_match> expected_match{
-      search_match(0, 0, false),                 // Empty: Before first
-      search_match(4095, 0, false),              // Empty: Just before first
-      search_match(4096, 0, false),              // Empty: At the start of first
-      search_match(4096 + 4095, 0, false),       // Empty: At end first
-      search_match(2 * 4096, 0, false),          // Empty: Just after first
-      search_match(2 * 4096 + 64, 0, false),     // Empty: Between first and second
-      search_match(3 * 4096, 0, false),          // Empty: At the start of second
-      search_match(4 * 4096 + 64, 0, false),     // Empty: Between second and third
-      search_match(5 * 4096, 0, false),          // Empty: At the start of third
-      search_match(6 * 4096, 0, false),          // Empty: Just after third
-      search_match(4096 - 128, 64, false),       // Before first
-      search_match(4095, 1, false),              // Just before first, i.e. touching.
-      search_match(4096 - 64, 128, true),        // Overlapping the start of first
-      search_match(4096, 1, true),               // Just the start of first
-      search_match(4096, 64, true),              // At the start of first
-      search_match(4096 + 64, 64, true),         // Within first
-      search_match(2 * 4096 - 64, 64, true),     // At the end of first
-      search_match(2 * 4096 - 1, 1, true),       // Just the end of first
-      search_match(2 * 4096 - 64, 128, true),    // Overlapping the end of first
-      search_match(4096, 4096, true),            // The same as first
-      search_match(4096 - 64, 4096 + 128, true), // Completely overlapping first
+      search_match(0, 0, false),                 // Empty: Before first 0
+      search_match(4095, 0, false),              // Empty: Just before first 4,095
+      search_match(4096, 0, false),              // Empty: At the start of first 4,096
+      search_match(4096 + 4095, 0, false),       // Empty: At end first 8,191
+      search_match(2 * 4096, 0, false),          // Empty: Just after first 8,192
+      search_match(2 * 4096 + 64, 0, false),     // Empty: Between first and second 8,256
+      search_match(3 * 4096, 0, false),          // Empty: At the start of second 12,288
+      search_match(4 * 4096 + 64, 0, false),     // Empty: Between second and third 16,448
+      search_match(5 * 4096, 0, false),          // Empty: At the start of third 20,480
+      search_match(6 * 4096, 0, false),          // Empty: Just after third 24,576
+      search_match(4096 - 128, 64, false),       // Before first [3,968, 4,031]
+      search_match(4095, 1, false),              // Just before first, i.e. touching. [4,095, 4,095]
+      search_match(4096 - 64, 128, true),        // Overlapping the start of first [4,032, 4,160]
+      search_match(4096, 1, true),               // Just the start of first [4,096, 4,096]
+      search_match(4096, 64, true),              // At the start of first [4,096, 4,159]
+      search_match(4096 + 64, 64, true),         // Within first [4,160, 4,223]
+      search_match(2 * 4096 - 64, 64, true),     // At the end of first [8,128, 8,191]
+      search_match(2 * 4096 - 1, 1, true),       // Just the end of first [8,191, 8,191]
+      search_match(2 * 4096 - 64, 128, true),    // Overlapping the end of first [8,128, 8,255]
+      search_match(4096, 4096, true),            // The same as first [4,096, 8191]
+      search_match(4096 - 64, 4096 + 128, true), // Completely overlapping first [4,032, 8,255]
     };
 
     std::vector<WT_EXT **> stack(WT_SKIP_MAXDEPTH, nullptr);
