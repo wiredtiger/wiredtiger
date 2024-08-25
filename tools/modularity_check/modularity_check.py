ab4faf4df48d9824c71a7feb87653215cb66e864
Andrew Morton <andrew.morton@mongodb.com>
2024-08-25 08:13:36 +0000

    Add command to explain a cycle

--- tools/modularity_check/modularity_check.py
+++ tools/modularity_check/modularity_check.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 
 import argparse
+import ast
+
+import networkx as nx
 
 from parse_wt_ast import parse_wiredtiger_files
 from build_dependency_graph import build_graph
-from query_dependency_graph import who_uses, who_is_used_by
+from query_dependency_graph import who_uses, who_is_used_by, explain_cycle
 
 def parse_args():
     parser = argparse.ArgumentParser(description="TODO")
@@ -17,6 +20,13 @@ def parse_args():
     who_is_used_by_parser = subparsers.add_parser(
         'who_is_used_by', help='Who this module is used by')
     who_is_used_by_parser.add_argument('module', type=str, help='module name')
+
+    list_cycles_parser = subparsers.add_parser('list_cycles', help='List cycles present in the dependency graph')
+    list_cycles_parser.add_argument('module', type=str, help='module name')
+
+    explain_cycle_parser = subparsers.add_parser('explain_cycle', help='Describe the links that create a given cycle')
+    explain_cycle_parser.add_argument('cycle', type=str, 
+        help="The cycle to explain. It must be in the format \"['meta', 'conn', 'log']\"")
 
     return parser.parse_args()
 
@@ -31,6 +41,14 @@ def main():
         who_uses(args.module, graph)
     elif args.command == "who_is_used_by":
         who_is_used_by(args.module, graph)
+    elif args.command == "list_cycles":
+        for c in nx.simple_cycles(graph, length_bound=3):
+            if args.module in c:
+                print(c)
+    elif args.command == "explain_cycle":
+        cycle = ast.literal_eval(args.cycle)
+        explain_cycle(cycle, graph)
+
     else:
         print(f"Unrecognised command {args.command}!")
         exit(1)
--- tools/modularity_check/query_dependency_graph.py
+++ tools/modularity_check/query_dependency_graph.py
@@ -1,24 +1,48 @@
 import networkx as nx
 
+from typing import List
+
+# Print the links that form the dependency of `from_node` on `to_node`
+# This function assumes that the dependency exists
+def print_edge(graph, from_node, to_node) -> bool:
+    edge = (from_node, to_node)
+    link_data = nx.get_edge_attributes(graph, 'link_data')
+    link_data[edge].print_type_uses()
+    link_data[edge].print_struct_accesses()
+    link_data[edge].print_func_calls()
+
 def who_uses(module: str, graph: nx.DiGraph):
     incoming_edges = graph.in_edges(module)
-    link_data = nx.get_edge_attributes(graph, 'link_data')
 
     for edge in sorted(incoming_edges):
         (caller, _) = edge
         print(f"\n{caller}/")
-        link_data[edge].print_type_uses()
-        link_data[edge].print_struct_accesses()
-        link_data[edge].print_func_calls()
+        print_edge(graph, caller, module)
 
 def who_is_used_by(module: str, graph: nx.DiGraph):
     incoming_edges = graph.out_edges(module)
-    link_data = nx.get_edge_attributes(graph, 'link_data')
 
     for edge in sorted(incoming_edges):
         (_, callee) = edge
         print(f"\n{callee}/")
-        link_data[edge].print_type_uses()
-        link_data[edge].print_struct_accesses()
-        link_data[edge].print_func_calls()
+        print_edge(graph, module, callee)
 
+# Given a list of nodes that describe a cycle explain the path to reach it
+def explain_cycle(cycle: List[str], graph: nx.DiGraph):
+
+    link_data = nx.get_edge_attributes(graph, 'link_data')
+    
+    # First check the cycle exists
+    for i in range(0, len(cycle)):
+        cur_node = cycle[i]
+        next_node = cycle[(i + 1) % len(cycle)]
+        if not graph.has_edge(cur_node, next_node):
+            print(f"No cycle! There is no dependency from '{cur_node}' to '{next_node}'")
+            return
+
+    # If it does then dump the path
+    for i in range(0, len(cycle)):
+        cur_node = cycle[i]
+        next_node = cycle[(i + 1) % len(cycle)]
+        print(f"From {cur_node} to {next_node}:")
+        print_edge(graph, cur_node, next_node)
