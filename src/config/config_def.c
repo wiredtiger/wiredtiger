15d0fc42f5b887faed60e46e5dcf65371fd615a4
Ravi Prakash Giri <ravi.giri@mongodb.com>
2024-08-26 00:16:42 +0000

    WT-13416 Remove obsolete eviction logging components.

--- src/config/config_def.c
+++ src/config/config_def.c
@@ -546,10 +546,7 @@ const char __WT_CONFIG_CHOICE_compact[] = "compact";
 const char __WT_CONFIG_CHOICE_compact_progress[] = "compact_progress";
 const char __WT_CONFIG_CHOICE_configuration[] = "configuration";
 const char __WT_CONFIG_CHOICE_error_returns[] = "error_returns";
-const char __WT_CONFIG_CHOICE_evict[] = "evict";
 const char __WT_CONFIG_CHOICE_eviction[] = "eviction";
-const char __WT_CONFIG_CHOICE_evict_stuck[] = "evict_stuck";
-const char __WT_CONFIG_CHOICE_evictserver[] = "evictserver";
 const char __WT_CONFIG_CHOICE_fileops[] = "fileops";
 const char __WT_CONFIG_CHOICE_generation[] = "generation";
 const char __WT_CONFIG_CHOICE_handleops[] = "handleops";
@@ -584,8 +581,7 @@ static const char *confchk_verbose_choices[] = {__WT_CONFIG_CHOICE_all, __WT_CON
   __WT_CONFIG_CHOICE_checkpoint, __WT_CONFIG_CHOICE_checkpoint_cleanup,
   __WT_CONFIG_CHOICE_checkpoint_progress, __WT_CONFIG_CHOICE_chunkcache, __WT_CONFIG_CHOICE_compact,
   __WT_CONFIG_CHOICE_compact_progress, __WT_CONFIG_CHOICE_configuration,
-  __WT_CONFIG_CHOICE_error_returns, __WT_CONFIG_CHOICE_evict, __WT_CONFIG_CHOICE_eviction,
-  __WT_CONFIG_CHOICE_evict_stuck, __WT_CONFIG_CHOICE_evictserver, __WT_CONFIG_CHOICE_fileops,
+  __WT_CONFIG_CHOICE_error_returns, __WT_CONFIG_CHOICE_eviction, __WT_CONFIG_CHOICE_fileops,
   __WT_CONFIG_CHOICE_generation, __WT_CONFIG_CHOICE_handleops, __WT_CONFIG_CHOICE_history_store,
   __WT_CONFIG_CHOICE_history_store_activity, __WT_CONFIG_CHOICE_log, __WT_CONFIG_CHOICE_lsm,
   __WT_CONFIG_CHOICE_lsm_manager, __WT_CONFIG_CHOICE_metadata, __WT_CONFIG_CHOICE_mutex,
@@ -718,11 +714,10 @@ static const WT_CONFIG_CHECK confchk_WT_CONNECTION_reconfigure[] = {
     "\"block_cache\",\"checkpoint\",\"checkpoint_cleanup\","
     "\"checkpoint_progress\",\"chunkcache\",\"compact\","
     "\"compact_progress\",\"configuration\",\"error_returns\","
-    "\"evict\",\"eviction\",\"evict_stuck\",\"evictserver\","
-    "\"fileops\",\"generation\",\"handleops\",\"history_store\","
-    "\"history_store_activity\",\"log\",\"lsm\",\"lsm_manager\","
-    "\"metadata\",\"mutex\",\"out_of_order\",\"overflow\","
-    "\"prefetch\",\"read\",\"reconcile\",\"recovery\","
+    "\"eviction\",\"fileops\",\"generation\",\"handleops\","
+    "\"history_store\",\"history_store_activity\",\"log\",\"lsm\","
+    "\"lsm_manager\",\"metadata\",\"mutex\",\"out_of_order\","
+    "\"overflow\",\"prefetch\",\"read\",\"reconcile\",\"recovery\","
     "\"recovery_progress\",\"rts\",\"salvage\",\"shared_cache\","
     "\"split\",\"temporary\",\"thread_group\",\"tiered\","
     "\"timestamp\",\"transaction\",\"verify\",\"version\",\"write\"]",
@@ -2766,8 +2761,7 @@ static const char *confchk_verbose13_choices[] = {__WT_CONFIG_CHOICE_all, __WT_C
   __WT_CONFIG_CHOICE_checkpoint, __WT_CONFIG_CHOICE_checkpoint_cleanup,
   __WT_CONFIG_CHOICE_checkpoint_progress, __WT_CONFIG_CHOICE_chunkcache, __WT_CONFIG_CHOICE_compact,
   __WT_CONFIG_CHOICE_compact_progress, __WT_CONFIG_CHOICE_configuration,
-  __WT_CONFIG_CHOICE_error_returns, __WT_CONFIG_CHOICE_evict, __WT_CONFIG_CHOICE_eviction,
-  __WT_CONFIG_CHOICE_evict_stuck, __WT_CONFIG_CHOICE_evictserver, __WT_CONFIG_CHOICE_fileops,
+  __WT_CONFIG_CHOICE_error_returns, __WT_CONFIG_CHOICE_eviction, __WT_CONFIG_CHOICE_fileops,
   __WT_CONFIG_CHOICE_generation, __WT_CONFIG_CHOICE_handleops, __WT_CONFIG_CHOICE_history_store,
   __WT_CONFIG_CHOICE_history_store_activity, __WT_CONFIG_CHOICE_log, __WT_CONFIG_CHOICE_lsm,
   __WT_CONFIG_CHOICE_lsm_manager, __WT_CONFIG_CHOICE_metadata, __WT_CONFIG_CHOICE_mutex,
@@ -2960,11 +2954,10 @@ static const WT_CONFIG_CHECK confchk_wiredtiger_open[] = {
     "\"block_cache\",\"checkpoint\",\"checkpoint_cleanup\","
     "\"checkpoint_progress\",\"chunkcache\",\"compact\","
     "\"compact_progress\",\"configuration\",\"error_returns\","
-    "\"evict\",\"eviction\",\"evict_stuck\",\"evictserver\","
-    "\"fileops\",\"generation\",\"handleops\",\"history_store\","
-    "\"history_store_activity\",\"log\",\"lsm\",\"lsm_manager\","
-    "\"metadata\",\"mutex\",\"out_of_order\",\"overflow\","
-    "\"prefetch\",\"read\",\"reconcile\",\"recovery\","
+    "\"eviction\",\"fileops\",\"generation\",\"handleops\","
+    "\"history_store\",\"history_store_activity\",\"log\",\"lsm\","
+    "\"lsm_manager\",\"metadata\",\"mutex\",\"out_of_order\","
+    "\"overflow\",\"prefetch\",\"read\",\"reconcile\",\"recovery\","
     "\"recovery_progress\",\"rts\",\"salvage\",\"shared_cache\","
     "\"split\",\"temporary\",\"thread_group\",\"tiered\","
     "\"timestamp\",\"transaction\",\"verify\",\"version\",\"write\"]",
@@ -3025,8 +3018,7 @@ static const char *confchk_verbose14_choices[] = {__WT_CONFIG_CHOICE_all, __WT_C
   __WT_CONFIG_CHOICE_checkpoint, __WT_CONFIG_CHOICE_checkpoint_cleanup,
   __WT_CONFIG_CHOICE_checkpoint_progress, __WT_CONFIG_CHOICE_chunkcache, __WT_CONFIG_CHOICE_compact,
   __WT_CONFIG_CHOICE_compact_progress, __WT_CONFIG_CHOICE_configuration,
-  __WT_CONFIG_CHOICE_error_returns, __WT_CONFIG_CHOICE_evict, __WT_CONFIG_CHOICE_eviction,
-  __WT_CONFIG_CHOICE_evict_stuck, __WT_CONFIG_CHOICE_evictserver, __WT_CONFIG_CHOICE_fileops,
+  __WT_CONFIG_CHOICE_error_returns, __WT_CONFIG_CHOICE_eviction, __WT_CONFIG_CHOICE_fileops,
   __WT_CONFIG_CHOICE_generation, __WT_CONFIG_CHOICE_handleops, __WT_CONFIG_CHOICE_history_store,
   __WT_CONFIG_CHOICE_history_store_activity, __WT_CONFIG_CHOICE_log, __WT_CONFIG_CHOICE_lsm,
   __WT_CONFIG_CHOICE_lsm_manager, __WT_CONFIG_CHOICE_metadata, __WT_CONFIG_CHOICE_mutex,
@@ -3219,11 +3211,10 @@ static const WT_CONFIG_CHECK confchk_wiredtiger_open_all[] = {
     "\"block_cache\",\"checkpoint\",\"checkpoint_cleanup\","
     "\"checkpoint_progress\",\"chunkcache\",\"compact\","
     "\"compact_progress\",\"configuration\",\"error_returns\","
-    "\"evict\",\"eviction\",\"evict_stuck\",\"evictserver\","
-    "\"fileops\",\"generation\",\"handleops\",\"history_store\","
-    "\"history_store_activity\",\"log\",\"lsm\",\"lsm_manager\","
-    "\"metadata\",\"mutex\",\"out_of_order\",\"overflow\","
-    "\"prefetch\",\"read\",\"reconcile\",\"recovery\","
+    "\"eviction\",\"fileops\",\"generation\",\"handleops\","
+    "\"history_store\",\"history_store_activity\",\"log\",\"lsm\","
+    "\"lsm_manager\",\"metadata\",\"mutex\",\"out_of_order\","
+    "\"overflow\",\"prefetch\",\"read\",\"reconcile\",\"recovery\","
     "\"recovery_progress\",\"rts\",\"salvage\",\"shared_cache\","
     "\"split\",\"temporary\",\"thread_group\",\"tiered\","
     "\"timestamp\",\"transaction\",\"verify\",\"version\",\"write\"]",
@@ -3287,8 +3278,7 @@ static const char *confchk_verbose15_choices[] = {__WT_CONFIG_CHOICE_all, __WT_C
   __WT_CONFIG_CHOICE_checkpoint, __WT_CONFIG_CHOICE_checkpoint_cleanup,
   __WT_CONFIG_CHOICE_checkpoint_progress, __WT_CONFIG_CHOICE_chunkcache, __WT_CONFIG_CHOICE_compact,
   __WT_CONFIG_CHOICE_compact_progress, __WT_CONFIG_CHOICE_configuration,
-  __WT_CONFIG_CHOICE_error_returns, __WT_CONFIG_CHOICE_evict, __WT_CONFIG_CHOICE_eviction,
-  __WT_CONFIG_CHOICE_evict_stuck, __WT_CONFIG_CHOICE_evictserver, __WT_CONFIG_CHOICE_fileops,
+  __WT_CONFIG_CHOICE_error_returns, __WT_CONFIG_CHOICE_eviction, __WT_CONFIG_CHOICE_fileops,
   __WT_CONFIG_CHOICE_generation, __WT_CONFIG_CHOICE_handleops, __WT_CONFIG_CHOICE_history_store,
   __WT_CONFIG_CHOICE_history_store_activity, __WT_CONFIG_CHOICE_log, __WT_CONFIG_CHOICE_lsm,
   __WT_CONFIG_CHOICE_lsm_manager, __WT_CONFIG_CHOICE_metadata, __WT_CONFIG_CHOICE_mutex,
@@ -3469,11 +3459,10 @@ static const WT_CONFIG_CHECK confchk_wiredtiger_open_basecfg[] = {
     "\"block_cache\",\"checkpoint\",\"checkpoint_cleanup\","
     "\"checkpoint_progress\",\"chunkcache\",\"compact\","
     "\"compact_progress\",\"configuration\",\"error_returns\","
-    "\"evict\",\"eviction\",\"evict_stuck\",\"evictserver\","
-    "\"fileops\",\"generation\",\"handleops\",\"history_store\","
-    "\"history_store_activity\",\"log\",\"lsm\",\"lsm_manager\","
-    "\"metadata\",\"mutex\",\"out_of_order\",\"overflow\","
-    "\"prefetch\",\"read\",\"reconcile\",\"recovery\","
+    "\"eviction\",\"fileops\",\"generation\",\"handleops\","
+    "\"history_store\",\"history_store_activity\",\"log\",\"lsm\","
+    "\"lsm_manager\",\"metadata\",\"mutex\",\"out_of_order\","
+    "\"overflow\",\"prefetch\",\"read\",\"reconcile\",\"recovery\","
     "\"recovery_progress\",\"rts\",\"salvage\",\"shared_cache\","
     "\"split\",\"temporary\",\"thread_group\",\"tiered\","
     "\"timestamp\",\"transaction\",\"verify\",\"version\",\"write\"]",
@@ -3537,8 +3526,7 @@ static const char *confchk_verbose16_choices[] = {__WT_CONFIG_CHOICE_all, __WT_C
   __WT_CONFIG_CHOICE_checkpoint, __WT_CONFIG_CHOICE_checkpoint_cleanup,
   __WT_CONFIG_CHOICE_checkpoint_progress, __WT_CONFIG_CHOICE_chunkcache, __WT_CONFIG_CHOICE_compact,
   __WT_CONFIG_CHOICE_compact_progress, __WT_CONFIG_CHOICE_configuration,
-  __WT_CONFIG_CHOICE_error_returns, __WT_CONFIG_CHOICE_evict, __WT_CONFIG_CHOICE_eviction,
-  __WT_CONFIG_CHOICE_evict_stuck, __WT_CONFIG_CHOICE_evictserver, __WT_CONFIG_CHOICE_fileops,
+  __WT_CONFIG_CHOICE_error_returns, __WT_CONFIG_CHOICE_eviction, __WT_CONFIG_CHOICE_fileops,
   __WT_CONFIG_CHOICE_generation, __WT_CONFIG_CHOICE_handleops, __WT_CONFIG_CHOICE_history_store,
   __WT_CONFIG_CHOICE_history_store_activity, __WT_CONFIG_CHOICE_log, __WT_CONFIG_CHOICE_lsm,
   __WT_CONFIG_CHOICE_lsm_manager, __WT_CONFIG_CHOICE_metadata, __WT_CONFIG_CHOICE_mutex,
@@ -3719,11 +3707,10 @@ static const WT_CONFIG_CHECK confchk_wiredtiger_open_usercfg[] = {
     "\"block_cache\",\"checkpoint\",\"checkpoint_cleanup\","
     "\"checkpoint_progress\",\"chunkcache\",\"compact\","
     "\"compact_progress\",\"configuration\",\"error_returns\","
-    "\"evict\",\"eviction\",\"evict_stuck\",\"evictserver\","
-    "\"fileops\",\"generation\",\"handleops\",\"history_store\","
-    "\"history_store_activity\",\"log\",\"lsm\",\"lsm_manager\","
-    "\"metadata\",\"mutex\",\"out_of_order\",\"overflow\","
-    "\"prefetch\",\"read\",\"reconcile\",\"recovery\","
+    "\"eviction\",\"fileops\",\"generation\",\"handleops\","
+    "\"history_store\",\"history_store_activity\",\"log\",\"lsm\","
+    "\"lsm_manager\",\"metadata\",\"mutex\",\"out_of_order\","
+    "\"overflow\",\"prefetch\",\"read\",\"reconcile\",\"recovery\","
     "\"recovery_progress\",\"rts\",\"salvage\",\"shared_cache\","
     "\"split\",\"temporary\",\"thread_group\",\"tiered\","
     "\"timestamp\",\"transaction\",\"verify\",\"version\",\"write\"]",
