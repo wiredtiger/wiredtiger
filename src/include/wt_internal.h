879fb2c652db832cbf6a34b95c692df5db933095
Marc Butler <135883+marcbutler@users.noreply.github.com>
2024-08-29 03:17:04 +0000

    Remove merge re-intro of LOG_DESC in wt_internal.h

--- src/include/wt_internal.h
+++ src/include/wt_internal.h
@@ -277,8 +277,6 @@ struct __wt_keyed_encryptor;
 typedef struct __wt_keyed_encryptor WT_KEYED_ENCRYPTOR;
 struct __wt_log;
 typedef struct __wt_log WT_LOG;
-struct __wt_log_desc;
-typedef struct __wt_log_desc WT_LOG_DESC;
 struct __wt_log_record;
 typedef struct __wt_log_record WT_LOG_RECORD;
 struct __wt_logslot;
