5c77a6dc0a2a1a48ca5aa3e845103ef8e9c9185f
Tod Johnson <tod.johnson@mongodb.com>
2024-09-04 17:16:23 +0000

    WT-13456 Add tests that __block_append() crashes if it is called for a non-append.  These tests are disabled as committed.

--- test/unittest/tests/block/test_extent_list_insert_block.cpp
+++ test/unittest/tests/block/test_extent_list_insert_block.cpp
@@ -247,6 +247,20 @@ TEST_CASE("Extent Lists: block_append", "[extent_list]")
               off_size(3 * 4096, 4096), // Second [12,288, 16,383].
               off_size(5 * 4096, 4096), // Third [20,480, 24,575].
             }},
+#if 0                           // Tests that are not appends and should crash.
+          {off_size(4 * 4096, 4096), // Below last extent, nonoverlapping
+            {
+              off_size(4096, 4096),     // First [4,096, 8,191].
+              off_size(3 * 4096, 3*4096), // Second' [12,288, 24,575].
+            }},
+#elif 0                         // Tests that are not appends and should crash.
+          {off_size(5 * 4096 + 1024, 4096), // Overlapping last extent
+            {
+              off_size(4096, 4096),     // First [4,096, 8,191].
+              off_size(3 * 4096, 4096), // Second [12,288, 16,383].
+              off_size(5 * 4096, 4096 + 1024), // Third' [20,480, 25,559].
+            }},
+#endif
         };
 
         /* Setup. */
