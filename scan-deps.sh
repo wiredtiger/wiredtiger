8c5ac8f086a06d8508c5530582faa494022c24a6
Yury Ershov <yuriy.ershov@gmail.com>
2024-09-05 13:54:16 +1000

    Updates

--- scan-deps.sh
+++ scan-deps.sh
@@ -22,21 +22,38 @@ perl -nE '
             $func = undef;
         }
     }
-    $h{"$file: -: DEF($1): $2"} = 1 if /-RecordDecl .*? (struct|union) (\w+) definition/;
-    $h{"$file: -: DEF(type): $1 => ".(($type=$2) =~ s/\x27:\x27/ => /r =~ s/\s++\*++( =|$)/$1/gr)} = 1 if /-TypedefDecl .*? referenced (\w+) \x27(.*?)\x27[^\x27]*$/;
+    if (@structstackindent) {
+        my $i = index($_, "-");
+        if ($i < 0) {
+            @structstackindent = (); @structstack = ();
+        } else {
+            while (@structstackindent && $structstackindent[-1] >= $i) {
+                pop @structstackindent; pop @structstack;
+            }
+        }
+    }
+    if (/^(.*?)-RecordDecl .*? (struct|union) (?:(\w+) )?definition/) {
+        $h{"$file: -: DEF($2): $3"} = 1 if $3;
+        push @structstackindent, length $1;
+        push @structstack, ($3 || ($structstack[-1] || "-")."-unnamed".(++$unnamed));
+    }
+    if (@structstack && /-FieldDecl .* referenced (\w++)/) {
+        $h{"$file: ".($func || "-").": FIELD: $structstack[-1]->$1"} = 1;
+    }
+    $h{"$file: -: DEF(type): $1 => ".(($type=$2) =~ s/\x27:\x27/ => /r =~ s/\s++\*++( =|$)/$1/gr)} = 1 if /-TypedefDecl .*? referenced (\w++) \x27(.*?)\x27[^\x27]*$/;
 
     next if !$func;
 
     # $h{"$file: $func: TYPE: ".(($type=$1) =~ s/\x27:\x27/ => /r =~ s/\s++\*++( =|$)/$1/gr)}=1 if /ImplicitCastExpr.*?\x27(.*?)\x27[^\x27]*$/;
     $h{"$file: $func: FUNC: $1"}=1 if /DeclRefExpr .*? Function 0x\w++ \x27([^\x27]++)\x27/;
-    if (/MemberExpr.*?(->\w++)/) {
-        $member = $1;
+    if (/MemberExpr.*? (?:->|\.)(\w++) 0x/) {
+        $member = $member ? "$1.$member" : $1;
         next;
     }
     if ($member) {
-        if (/(?:ImplicitCastExpr|ParenExpr).*?\x27(.*?)\x27/) {
+        if (/(?:ImplicitCastExpr|ParenExpr|DeclRefExpr).*?\x27(.*?)\x27/) {
             $type = $1;
-            $h{"$file: $func: MEMBER: ".($type =~ s/(struct|union|const|\*)//gr =~ s/(^\s++)|(\s++$)//gr).$member} = 1;
+            $h{"$file: $func: MEMBER: ".($type =~ s/(struct|union|const|\*)//gr =~ s/(^\s++)|(\s++$)//gr)."->$member"} = 1;
         }
         $member = undef;
     }
