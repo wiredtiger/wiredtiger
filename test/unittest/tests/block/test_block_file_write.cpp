221c6249f1cb19c0258042f60ac8ed43871c6b8a
Jie Chen <jie.chen@mongodb.com>
2024-09-11 02:08:55 +0000

    Fixed up some comments

--- test/unittest/tests/block/test_block_file_write.cpp
+++ test/unittest/tests/block/test_block_file_write.cpp
@@ -14,12 +14,13 @@
  * The block session manages an internal caching mechanism for both block and size blocks that are
  * created or discarded.
  */
-#include "wt_internal.h"
 #include <catch2/catch.hpp>
+#include <iostream>
+
+#include "wt_internal.h"
 #include "../wrappers/mock_session.h"
 #include "../wrappers/config_parser.h"
 #include "../wrappers/item_wrapper.h"
-#include <iostream>
 
 const int DEFAULT_BLOCK_SIZE = 512;
 const std::string ALLOCATION_SIZE = "512";
@@ -33,17 +34,20 @@ void
 validate_block_write(WT_SESSION_IMPL *session, WT_BLOCK *block, wt_off_t offset, uint32_t size,
   uint32_t checksum, const std::string &expected_str, uint32_t expected_size,
   wt_off_t &expected_offset)
-{
-    WT_BLOCK_MGR_SESSION *bms = reinterpret_cast<WT_BLOCK_MGR_SESSION *>(session->block_manager);
+{ 
+    // Test offset, size and checksum.
     expected_offset += expected_size;
     REQUIRE(offset == expected_offset);
     REQUIRE(size == expected_size);
     REQUIRE(checksum != 0);
 
+    // Test size and extent cache is created after block write.
     REQUIRE(session->block_manager_cleanup != nullptr);
+    WT_BLOCK_MGR_SESSION *bms = reinterpret_cast<WT_BLOCK_MGR_SESSION *>(session->block_manager);
     REQUIRE(bms->ext_cache != nullptr);
     REQUIRE(bms->sz_cache != nullptr);
 
+    // Test that the block was correctly written.
     std::string buf(expected_str.length(), ' ');
     block->fh->handle->fh_read(block->fh->handle, (WT_SESSION *)session, offset,
       expected_str.length(), static_cast<void *>(buf.data()));
@@ -58,6 +62,7 @@ create_block(std::shared_ptr<mock_session> session, config_parser &cp)
       (__wt_block_open(session->get_wt_session_impl(), DEFAULT_FILE_NAME.c_str(),
         WT_TIERED_OBJECTID_NONE, cp.get_config_array(), false, false, false, 0, &block)) == 0);
 
+    // Create required extent list for block writes.
     REQUIRE(__wti_block_ckpt_init(session->get_wt_session_impl(), &block->live, "live") == 0);
     block->size = DEFAULT_BLOCK_SIZE;
     return block;
@@ -65,7 +70,7 @@ create_block(std::shared_ptr<mock_session> session, config_parser &cp)
 
 TEST_CASE("Block: __wti_block_write_off", "[block_write]")
 {
-    /* Build Mock session, this will automatically create a mock connection. */
+    // Build Mock session, this will automatically create a mock connection.
     std::shared_ptr<mock_session> session = mock_session::build_test_mock_session();
     config_parser cp({{"allocation_size", ALLOCATION_SIZE}, {"block_allocation", BLOCK_ALLOCATION},
       {"os_cache_max", OS_CACHE_MAX}, {"os_cache_dirty_max", OS_CACHE_DIRTY_MAX},
@@ -73,12 +78,13 @@ TEST_CASE("Block: __wti_block_write_off", "[block_write]")
 
     REQUIRE((session->get_mock_connection()->setup_block_manager(session->get_wt_session_impl())) == 0);
 
+    // Initialize the correct checksum function.
     __wt_process.checksum = wiredtiger_crc32c_func();
+
     // Create WT_ITEM buffer and copy a string into it.
     WT_ITEM *buf;
     REQUIRE(__wt_scr_alloc(session->get_wt_session_impl(), 0, &buf) == 0);
     REQUIRE(__wt_buf_initsize(session->get_wt_session_impl(), buf, DEFAULT_BLOCK_SIZE) == 0);
-
     std::string expected_str("hello");
     expected_str.copy(
       reinterpret_cast<char *>(const_cast<void *>(buf->data)), expected_str.length(), 0);
@@ -88,7 +94,7 @@ TEST_CASE("Block: __wti_block_write_off", "[block_write]")
     uint32_t size, checksum;
     SECTION("Test that the arguments checksum, size and offset is correct")
     {
-        // Test normal write case.
+        // Test default write case.
         REQUIRE((__ut_block_write_off(session->get_wt_session_impl(), block, buf, &offset, &size,
                   &checksum, false, false, false)) == 0);
         validate_block_write(session->get_wt_session_impl(), block, offset, size, checksum,
@@ -100,7 +106,6 @@ TEST_CASE("Block: __wti_block_write_off", "[block_write]")
                   &checksum2, false, false, false)) == 0);
         validate_block_write(session->get_wt_session_impl(), block, offset, size, checksum,
           expected_str, buf->size, expected_offset);
-
         REQUIRE(checksum2 == checksum);
 
         std::string str2("1234567");
