524526205076f72ce123642a9895755cc6010db7
Siddhartha Mahajan <siddhartha.mahajan8899@gmail.com>
2024-09-24 05:16:17 +0000

    WT-13561 Remove __wt_page_release_evict and __wt_page_dirty_and_evict_soon from evict/

--- src/evict/evict_inline.h
+++ src/evict/evict_inline.h
@@ -107,20 +107,6 @@ __wt_page_evict_soon(WT_SESSION_IMPL *session, WT_REF *ref)
 }
 
 /*
- * __wt_page_dirty_and_evict_soon --
- *     Mark a page dirty and set it to be evicted as soon as possible.
- */
-static WT_INLINE int
-__wt_page_dirty_and_evict_soon(WT_SESSION_IMPL *session, WT_REF *ref)
-{
-    WT_RET(__wt_page_modify_init(session, ref->page));
-    __wt_page_modify_set(session, ref->page);
-    __wt_page_evict_soon(session, ref);
-
-    return (0);
-}
-
-/*
  * __wt_eviction_clean_pressure --
  *     Return true if clean cache is stressed and will soon require application threads to evict
  *     content.
