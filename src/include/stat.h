b3d036ecd91d149b1c3077770156fee5fd236d0e
Etienne Petrel <77254506+etienneptl@users.noreply.github.com>
2024-10-03 16:41:57 +1000

    WT-13241 Mark obsolete time window pages dirty during checkpoint cleanup (v8.0) (#10883) (#11072)
    The time window written to the pages to support MVCC can become obsolete
    and needs to be cleaned to reclaim the disk space occupied by the
    obsolete time window.
    
    As part of the checkpoint cleanup that goes through the obsolete time
    window pages in memory, mark them dirty to let the reconciliation remove
    the obsolete time window from the pages.
    
    (cherry picked from commit 52da2f987e56b6c09aca806187d7f034a661f210)
    
    Co-authored-by: Hari Babu Kommi <haribabu.kommi@mongodb.com>

--- src/include/stat.h
+++ src/include/stat.h
@@ -649,6 +649,7 @@ struct __wt_connection_stats {
     int64_t checkpoint_pages_visited_leaf;
     int64_t checkpoint_pages_reconciled;
     int64_t checkpoint_cleanup_pages_evict;
+    int64_t checkpoint_cleanup_pages_obsolete_tw;
     int64_t checkpoint_cleanup_pages_removed;
     int64_t checkpoint_cleanup_pages_walk_skipped;
     int64_t checkpoint_cleanup_pages_visited;
@@ -1221,6 +1222,7 @@ struct __wt_dsrc_stats {
     int64_t cache_state_pages;
     int64_t checkpoint_snapshot_acquired;
     int64_t checkpoint_cleanup_pages_evict;
+    int64_t checkpoint_cleanup_pages_obsolete_tw;
     int64_t checkpoint_cleanup_pages_removed;
     int64_t checkpoint_cleanup_pages_walk_skipped;
     int64_t checkpoint_cleanup_pages_visited;
