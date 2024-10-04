8753bc8e8176f5e1d87f62d57d9c51ef66a93df3
Chenhao Qu <chenhao.qu@mongodb.com>
2024-10-04 04:54:44 +0000

    Add a comment

--- src/btree/bt_split.c
+++ src/btree/bt_split.c
@@ -1414,6 +1414,10 @@ __split_multi_inmem(WT_SESSION_IMPL *session, WT_PAGE *orig, WT_MULTI *multi, WT
     if (!__wt_readgen_evict_soon(&orig_read_gen))
         __wt_atomic_store64(&page->read_gen, orig_read_gen);
 
+    /*
+     * Mark the page as dirty for future garbage collection through reconciliation. We only end here
+     * if we have content to clean up in the future.
+     */
     if (F_ISSET(S2BT(session), WT_BTREE_GARBAGE_COLLECT))
         __wt_page_modify_set(session, ref->page);
 
