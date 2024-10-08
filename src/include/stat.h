93dce2eab43591315facf72d70b547fcd4d1836b
Etienne Petrel <77254506+etienneptl@users.noreply.github.com>
2024-10-08 14:17:07 +1100

    WT-13333 Process logged tables only when checkpoint cleanup is configured to be aggressive (v8.0) (#10918) (#11078)
    The connection configuration string
    `checkpoint_cleanup=[method=reclaim_space]` can be used to process
    logged tables during checkpoint cleanup. The associated connection flag
    has been renamed from `WT_CONN_CKPT_CLEANUP_SKIP_INT` to
    `WT_CONN_CKPT_CLEANUP_RECLAIM_SPACE`.
    
    (cherry picked from commit 395866cec4676ce735450180a5dc992ed8e0b1dd)

--- src/include/stat.h
+++ src/include/stat.h
@@ -650,6 +650,7 @@ struct __wt_connection_stats {
     int64_t checkpoint_pages_reconciled;
     int64_t checkpoint_cleanup_pages_evict;
     int64_t checkpoint_cleanup_pages_obsolete_tw;
+    int64_t checkpoint_cleanup_pages_read_reclaim_space;
     int64_t checkpoint_cleanup_pages_removed;
     int64_t checkpoint_cleanup_pages_walk_skipped;
     int64_t checkpoint_cleanup_pages_visited;
@@ -1223,6 +1224,7 @@ struct __wt_dsrc_stats {
     int64_t checkpoint_snapshot_acquired;
     int64_t checkpoint_cleanup_pages_evict;
     int64_t checkpoint_cleanup_pages_obsolete_tw;
+    int64_t checkpoint_cleanup_pages_read_reclaim_space;
     int64_t checkpoint_cleanup_pages_removed;
     int64_t checkpoint_cleanup_pages_walk_skipped;
     int64_t checkpoint_cleanup_pages_visited;
