343049db6a1b67fd10db513d9b17532e01a18634
Chenhao Qu <chenhao.qu@mongodb.com>
2024-10-09 00:32:42 +0000

    Fix another place

--- src/log/log_auto.c
+++ src/log/log_auto.c
@@ -129,7 +129,7 @@ __wt_logrec_alloc(WT_SESSION_IMPL *session, size_t size, WT_ITEM **logrecp)
     WT_LOG *log;
     size_t allocsize;
 
-    log = S2C(session)->log;
+    log = S2C(session)->log_info.log;
     allocsize = log == NULL ? WT_LOG_ALIGN : log->allocsize;
     WT_RET(__wt_scr_alloc(session, WT_ALIGN(size + 1, allocsize), &logrec));
     WT_CLEAR(*(WT_LOG_RECORD *)logrec->data);
