8f54331f61658e001532324472528a7115e84bcb
Jie Chen <jie.chen@mongodb.com>
2024-10-11 01:12:27 +0000

    Fixed

--- src/conn/conn_api.c
+++ src/conn/conn_api.c
@@ -2607,6 +2607,8 @@ __conn_write_base_config(WT_SESSION_IMPL *session, const char *cfg[])
       "timing_stress_for_test=,"
       "use_environment_priv=,"
       "verbose=,"
+      "per_session_control_points=,"
+      "per_connection_control_points=,"
       "verify_metadata=,",
       &base_config));
     __wt_config_init(session, &parser, base_config);
