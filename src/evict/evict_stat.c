f637267813f944871f21068b64b0e053f42f186e
Siddhartha Mahajan <siddhartha.mahajan8899@gmail.com>
2024-10-23 08:12:33 +0000

    s_all

--- src/evict/evict_stat.c
+++ src/evict/evict_stat.c
@@ -118,11 +118,11 @@ __evict_stat_walk(WT_SESSION_IMPL *session)
 
 /*
  * __wt_evict_cache_stat_walk --
- *     This function gathers tree-level eviction statistics. It helps in understanding how 
- *     eviction is functioning for a specific tree, providing insights into eviction activity that 
- *     may assist in diagnosing issues. This function is not for connection-level statistics, and 
- *     requires a special `cache_walk` configuration defined in `api_data.py` to enable eviction 
- *     statistics for a specific tree.
+ *     This function gathers tree-level eviction statistics. It helps in understanding how eviction
+ *     is functioning for a specific tree, providing insights into eviction activity that may assist
+ *     in diagnosing issues. This function is not for connection-level statistics, and requires a
+ *     special `cache_walk` configuration defined in `api_data.py` to enable eviction statistics for
+ *     a specific tree.
  */
 void
 __wt_evict_cache_stat_walk(WT_SESSION_IMPL *session)
