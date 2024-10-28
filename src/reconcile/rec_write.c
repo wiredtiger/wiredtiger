d7d6b5da8d53215b4819e1cb666eabe10ce85e72
Chenhao Qu <chenhao.qu@mongodb.com>
2024-10-28 21:15:48 +0000

    Set aligned buffer

--- src/reconcile/rec_write.c
+++ src/reconcile/rec_write.c
@@ -589,6 +589,7 @@ __rec_init(WT_SESSION_IMPL *session, WT_REF *ref, uint32_t flags, WT_SALVAGE_COO
         /* Disk buffers need to be aligned for writing. */
         F_SET(&r->chunk_A.image, WT_ITEM_ALIGNED);
         F_SET(&r->chunk_B.image, WT_ITEM_ALIGNED);
+        F_SET(&r->delta, WT_ITEM_ALIGNED);
     }
 
     /* Remember the configuration. */
