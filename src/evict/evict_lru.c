77dc4737d2dbc01aa48a5fdedf5570e3a4a65d14
Alexandra (Sasha) Fedorova <sasha.fedorova@mongodb.com>
2024-10-30 14:40:56 -0700

    Got rid of undefined refs

--- src/evict/evict_lru.c
+++ src/evict/evict_lru.c
@@ -183,3 +183,355 @@ __wt_evict_threads_destroy(WT_SESSION_IMPL *session)
     return (0);
 }
 
+
+/* !!!
+ * __wt_evict_file_exclusive_on --
+ *     Acquire exclusive access to a file/tree making it possible to evict the entire file using
+ *     `__wt_evict_file`. It does this by incrementing the `evict_disabled` counter for a
+ *     tree, which disables all other means of eviction (except file eviction).
+ *
+ *     For the incremented `evict_disabled` value, the eviction server skips walking this tree for
+ *     eviction candidates, and force-evicting or queuing pages from this tree is not allowed.
+ *
+ *     It is called from multiple places in the code base, such as when initiating file eviction
+ *     `__wt_evict_file` or when opening or closing trees.
+ *
+ *     Return an error code if unable to acquire necessary locks or clear the eviction queues.
+ */
+int
+__wt_evict_file_exclusive_on(WT_SESSION_IMPL *session) {
+
+	(void)session;
+	return 0;
+}
+
+/* !!!
+ * __wt_evict_file_exclusive_off --
+ *     Release exclusive access to a file/tree by decrementing the `evict_disabled` count
+ *     back to zero, allowing eviction to proceed for the tree.
+ *
+ *     It is called from multiple places in the code where exclusive eviction access is no longer
+ *     needed.
+ */
+void
+__wt_evict_file_exclusive_off(WT_SESSION_IMPL *session)
+{
+	(void)session;
+
+}
+
+/* !!!
+ * __wt_evict_page_urgent --
+ *     Push a page into the urgent eviction queue.
+ *
+ *     It is called by the eviction server if pages require immediate eviction or by the application
+ *     threads as part of forced eviction when directly evicting pages is not feasible.
+ *
+ *     Input parameters:
+ *       `ref`: A reference to the page that is being added to the urgent eviction queue.
+ *
+ *     Return `true` if the page has been successfully added to the urgent queue, or `false` is
+ *     already marked for eviction.
+ */
+bool
+__wt_evict_page_urgent(WT_SESSION_IMPL *session, WT_REF *ref) {
+
+	(void)session;
+	(void)ref;
+
+	return false;
+}
+
+/* !!!
+ * __wt_evict_priority_set --
+ *     Set a tree's eviction priority. A higher priority indicates less likelihood for the tree to
+ *     be considered for eviction. The eviction server skips the eviction of trees with a non-zero
+ *     priority unless eviction is in an aggressive state and the Btree is significantly utilizing
+ *     the cache.
+ *
+ *     At present, it is exclusively called for metadata and bloom filter files, as these are meant
+ *     to be retained in the cache.
+ *
+ *     Input parameter:
+ *       `v`: An integer that denotes the priority level.
+ */
+void
+__wt_evict_priority_set(WT_SESSION_IMPL *session, uint64_t v)
+{
+    S2BT(session)->evict_priority = v;
+}
+
+/*
+ * __wt_evict_priority_clear --
+ *     Clear a tree's eviction priority to zero. It is called during the closure of the
+ *     dhandle/btree.
+ */
+void
+__wt_evict_priority_clear(WT_SESSION_IMPL *session)
+{
+    S2BT(session)->evict_priority = 0;
+}
+
+/* !!!
+ * __wt_evict_server_wake --
+ *     Wake up the eviction server thread. The eviction server typically sleeps for some time when
+ *     cache usage is below the target thresholds. When the cache is expected to exceed these
+ *     thresholds, callers can nudge the eviction server to wake up and resume its work.
+ *
+ *     This function is called in situations where pages are queued for urgent eviction or when
+ *     application threads request eviction assistance.
+ */
+void
+__wt_evict_server_wake(WT_SESSION_IMPL *session)
+{
+    WT_CACHE *cache;
+    WT_CONNECTION_IMPL *conn;
+
+    conn = S2C(session);
+    cache = conn->cache;
+
+    if (WT_VERBOSE_LEVEL_ISSET(session, WT_VERB_EVICTION, WT_VERBOSE_DEBUG_2)) {
+        uint64_t bytes_dirty, bytes_inuse, bytes_max, bytes_updates;
+
+        bytes_inuse = __wt_cache_bytes_inuse(cache);
+        bytes_max = conn->cache_size;
+        bytes_dirty = __wt_cache_dirty_inuse(cache);
+        bytes_updates = __wt_cache_bytes_updates(cache);
+        __wt_verbose_debug2(session, WT_VERB_EVICTION,
+          "waking, bytes inuse %s max (%" PRIu64 "MB %s %" PRIu64 "MB), bytes dirty %" PRIu64
+          "(bytes), bytes updates %" PRIu64 "(bytes)",
+          bytes_inuse <= bytes_max ? "<=" : ">", bytes_inuse / WT_MEGABYTE,
+          bytes_inuse <= bytes_max ? "<=" : ">", bytes_max / WT_MEGABYTE, bytes_dirty,
+          bytes_updates);
+    }
+
+    __wt_cond_signal(session, conn->evict->evict_cond);
+}
+
+/*
+ * __wti_evict_app_assist_worker --
+ *     Worker function for __wt_evict_app_assist_worker_check: evict pages if the cache crosses
+ *     eviction trigger thresholds.
+ */
+int
+__wti_evict_app_assist_worker(WT_SESSION_IMPL *session, bool busy, bool readonly, double pct_full)
+{
+	(void)session;
+	(void)busy;
+	(void)readonly;
+	(void)pct_full;
+
+	return 0;
+}
+
+/*
+ * __wti_evict_list_clear_page --
+ *     Check whether a page is present in the LRU eviction list. If the page is found in the list,
+ *     remove it. This is called from the page eviction code to make sure there is no attempt to
+ *     evict a child page multiple times.
+ */
+void
+__wti_evict_list_clear_page(WT_SESSION_IMPL *session, WT_REF *ref)
+{
+	(void)session;
+	(void)ref;
+}
+
+/*
+ * __verbose_dump_cache_single --
+ *     Output diagnostic information about a single file in the cache.
+ */
+static int
+__verbose_dump_cache_single(WT_SESSION_IMPL *session, uint64_t *total_bytesp,
+  uint64_t *total_dirty_bytesp, uint64_t *total_updates_bytesp)
+{
+    WT_BTREE *btree;
+    WT_DATA_HANDLE *dhandle;
+    WT_PAGE *page;
+    WT_REF *next_walk;
+    size_t size;
+    uint64_t intl_bytes, intl_bytes_max, intl_dirty_bytes;
+    uint64_t intl_dirty_bytes_max, intl_dirty_pages, intl_pages;
+    uint64_t leaf_bytes, leaf_bytes_max, leaf_dirty_bytes;
+    uint64_t leaf_dirty_bytes_max, leaf_dirty_pages, leaf_pages, updates_bytes;
+
+    intl_bytes = intl_bytes_max = intl_dirty_bytes = 0;
+    intl_dirty_bytes_max = intl_dirty_pages = intl_pages = 0;
+    leaf_bytes = leaf_bytes_max = leaf_dirty_bytes = 0;
+    leaf_dirty_bytes_max = leaf_dirty_pages = leaf_pages = 0;
+    updates_bytes = 0;
+
+    dhandle = session->dhandle;
+    btree = dhandle->handle;
+    WT_RET(__wt_msg(session, "%s(%s%s)%s%s:", dhandle->name,
+      WT_DHANDLE_IS_CHECKPOINT(dhandle) ? "checkpoint=" : "",
+      WT_DHANDLE_IS_CHECKPOINT(dhandle) ? dhandle->checkpoint : "<live>",
+      btree->evict_disabled != 0 ? " eviction disabled" : "",
+      btree->evict_disabled_open ? " at open" : ""));
+
+    /*
+     * We cannot walk the tree of a dhandle held exclusively because the owning thread could be
+     * manipulating it in a way that causes us to dump core. So print out that we visited and
+     * skipped it.
+     */
+    if (F_ISSET(dhandle, WT_DHANDLE_EXCLUSIVE))
+        return (__wt_msg(session, " handle opened exclusively, cannot walk tree, skipping"));
+
+    next_walk = NULL;
+    while (__wt_tree_walk(session, &next_walk,
+             WT_READ_CACHE | WT_READ_NO_EVICT | WT_READ_NO_WAIT | WT_READ_VISIBLE_ALL) == 0 &&
+      next_walk != NULL) {
+        page = next_walk->page;
+        size = __wt_atomic_loadsize(&page->memory_footprint);
+
+        if (F_ISSET(next_walk, WT_REF_FLAG_INTERNAL)) {
+            ++intl_pages;
+            intl_bytes += size;
+            intl_bytes_max = WT_MAX(intl_bytes_max, size);
+            if (__wt_page_is_modified(page)) {
+                ++intl_dirty_pages;
+                intl_dirty_bytes += size;
+                intl_dirty_bytes_max = WT_MAX(intl_dirty_bytes_max, size);
+            }
+        } else {
+            ++leaf_pages;
+            leaf_bytes += size;
+            leaf_bytes_max = WT_MAX(leaf_bytes_max, size);
+            if (__wt_page_is_modified(page)) {
+                ++leaf_dirty_pages;
+                leaf_dirty_bytes += size;
+                leaf_dirty_bytes_max = WT_MAX(leaf_dirty_bytes_max, size);
+            }
+            if (page->modify != NULL)
+                updates_bytes += page->modify->bytes_updates;
+        }
+    }
+
+    if (intl_pages == 0)
+        WT_RET(__wt_msg(session, "internal: 0 pages"));
+    else
+        WT_RET(
+          __wt_msg(session,
+            "internal: "
+            "%" PRIu64 " pages, %.2f KB, "
+            "%" PRIu64 "/%" PRIu64 " clean/dirty pages, "
+            "%.2f/%.2f clean / dirty KB, "
+            "%.2f KB max page, "
+            "%.2f KB max dirty page ",
+            intl_pages, (double)intl_bytes / WT_KILOBYTE, intl_pages - intl_dirty_pages,
+            intl_dirty_pages, (double)(intl_bytes - intl_dirty_bytes) / WT_KILOBYTE,
+            (double)intl_dirty_bytes / WT_KILOBYTE, (double)intl_bytes_max / WT_KILOBYTE,
+            (double)intl_dirty_bytes_max / WT_KILOBYTE));
+    if (leaf_pages == 0)
+        WT_RET(__wt_msg(session, "leaf: 0 pages"));
+    else
+        WT_RET(
+          __wt_msg(session,
+            "leaf: "
+            "%" PRIu64 " pages, %.2f KB, "
+            "%" PRIu64 "/%" PRIu64 " clean/dirty pages, "
+            "%.2f /%.2f /%.2f clean/dirty/updates KB, "
+            "%.2f KB max page, "
+            "%.2f KB max dirty page",
+            leaf_pages, (double)leaf_bytes / WT_KILOBYTE, leaf_pages - leaf_dirty_pages,
+            leaf_dirty_pages, (double)(leaf_bytes - leaf_dirty_bytes) / WT_KILOBYTE,
+            (double)leaf_dirty_bytes / WT_KILOBYTE, (double)updates_bytes / WT_KILOBYTE,
+            (double)leaf_bytes_max / WT_KILOBYTE, (double)leaf_dirty_bytes_max / WT_KILOBYTE));
+
+    *total_bytesp += intl_bytes + leaf_bytes;
+    *total_dirty_bytesp += intl_dirty_bytes + leaf_dirty_bytes;
+    *total_updates_bytesp += updates_bytes;
+
+    return (0);
+}
+
+/*
+ * __verbose_dump_cache_apply --
+ *     Apply dumping cache for all the dhandles.
+ */
+static int
+__verbose_dump_cache_apply(WT_SESSION_IMPL *session, uint64_t *total_bytesp,
+  uint64_t *total_dirty_bytesp, uint64_t *total_updates_bytesp)
+{
+    WT_CONNECTION_IMPL *conn;
+    WT_DATA_HANDLE *dhandle;
+    WT_DECL_RET;
+
+    conn = S2C(session);
+    for (dhandle = NULL;;) {
+        WT_DHANDLE_NEXT(session, dhandle, &conn->dhqh, q);
+        if (dhandle == NULL)
+            break;
+
+        /* Skip if the tree is marked discarded by another thread. */
+        if (!WT_DHANDLE_BTREE(dhandle) || !F_ISSET(dhandle, WT_DHANDLE_OPEN) ||
+          F_ISSET(dhandle, WT_DHANDLE_DISCARD))
+            continue;
+
+        WT_WITH_DHANDLE(session, dhandle,
+          ret = __verbose_dump_cache_single(
+            session, total_bytesp, total_dirty_bytesp, total_updates_bytesp));
+        if (ret != 0)
+            WT_RET(ret);
+    }
+    return (0);
+}
+
+/*
+ * __wt_verbose_dump_cache --
+ *     Output diagnostic information about the cache.
+ */
+int
+__wt_verbose_dump_cache(WT_SESSION_IMPL *session)
+{
+    WT_CACHE *cache;
+    WT_CONNECTION_IMPL *conn;
+    WT_DECL_RET;
+    double pct;
+    uint64_t bytes_dirty_intl, bytes_dirty_leaf, bytes_inmem;
+    uint64_t cache_bytes_updates, total_bytes, total_dirty_bytes, total_updates_bytes;
+    bool needed;
+
+    conn = S2C(session);
+    cache = conn->cache;
+    total_bytes = total_dirty_bytes = total_updates_bytes = 0;
+    pct = 0.0; /* [-Werror=uninitialized] */
+    WT_NOT_READ(cache_bytes_updates, 0);
+
+    WT_RET(__wt_msg(session, "%s", WT_DIVIDER));
+    WT_RET(__wt_msg(session, "cache dump"));
+
+    WT_RET(__wt_msg(session, "cache full: %s", __wt_cache_full(session) ? "yes" : "no"));
+    needed = __wt_evict_clean_needed(session, &pct);
+    WT_RET(__wt_msg(session, "cache clean check: %s (%2.3f%%)", needed ? "yes" : "no", pct));
+    needed = __wt_evict_dirty_needed(session, &pct);
+    WT_RET(__wt_msg(session, "cache dirty check: %s (%2.3f%%)", needed ? "yes" : "no", pct));
+    needed = __wti_evict_updates_needed(session, &pct);
+    WT_RET(__wt_msg(session, "cache updates check: %s (%2.3f%%)", needed ? "yes" : "no", pct));
+
+    WT_WITH_HANDLE_LIST_READ_LOCK(session,
+      ret = __verbose_dump_cache_apply(
+        session, &total_bytes, &total_dirty_bytes, &total_updates_bytes));
+    WT_RET(ret);
+
+    /*
+     * Apply the overhead percentage so our total bytes are comparable with the tracked value.
+     */
+    total_bytes = __wt_cache_bytes_plus_overhead(conn->cache, total_bytes);
+    cache_bytes_updates = __wt_cache_bytes_updates(cache);
+
+    bytes_inmem = __wt_atomic_load64(&cache->bytes_inmem);
+    bytes_dirty_intl = __wt_atomic_load64(&cache->bytes_dirty_intl);
+    bytes_dirty_leaf = __wt_atomic_load64(&cache->bytes_dirty_leaf);
+
+    WT_RET(__wt_msg(session, "cache dump: total found: %.2f MB vs tracked inuse %.2f MB",
+      (double)total_bytes / WT_MEGABYTE, (double)bytes_inmem / WT_MEGABYTE));
+    WT_RET(__wt_msg(session, "total dirty bytes: %.2f MB vs tracked dirty %.2f MB",
+      (double)total_dirty_bytes / WT_MEGABYTE,
+      (double)(bytes_dirty_intl + bytes_dirty_leaf) / WT_MEGABYTE));
+    WT_RET(__wt_msg(session, "total updates bytes: %.2f MB vs tracked updates %.2f MB",
+      (double)total_updates_bytes / WT_MEGABYTE, (double)cache_bytes_updates / WT_MEGABYTE));
+
+    return (0);
+}
+
