a9ed3400c1696558c6be7821ed8ee690dca23d3f
Etienne Petrel <etienne.petrel@mongodb.com>
2024-10-30 12:49:56 +1100

    Create a main function and refactor

--- dist/ruff_check.py
+++ dist/ruff_check.py
@@ -8,7 +8,6 @@ import sys
 
 # Always use this script's folder as the working directory.
 CURRENT_DIR = pathlib.Path(__file__).parent.resolve()
-RUFF_TOML_FILE = f"{CURRENT_DIR}/ruff.toml"
 
 
 def check_ruff_version(expected_version: str) -> bool:
@@ -35,7 +34,7 @@ def get_expected_ruff_version(config_file: str) -> str:
             if m:
                 ruff_version = m.group(1)
                 break
-    assert ruff_version, f"No ruff version has been found in {RUFF_TOML_FILE}"
+    assert ruff_version, f"No ruff version has been found in {config_file}"
     return ruff_version
 
 
@@ -63,21 +62,21 @@ def show_ruff_installation_steps(expected_version: str) -> None:
     print(f"python3 -m pip install ruff=={expected_version}\n")
 
 
-ruff_expected_version = get_expected_ruff_version(RUFF_TOML_FILE)
+def main():
+    ruff_toml_file = f"{CURRENT_DIR}/ruff.toml"
+    ruff_expected_version = get_expected_ruff_version(ruff_toml_file)
 
-# If Ruff is not installed, show how to install it.
-if not which("ruff"):
-    print("Ruff is not installed!")
-    show_ruff_installation_steps(ruff_expected_version)
-    sys.exit(1)
-# Otherwise, check which version is installed.
-elif not check_ruff_version(ruff_expected_version):
-    print("Ruff version check failed.")
-    show_ruff_installation_steps(ruff_expected_version)
-    sys.exit(1)
+    # Check the correct version of Ruff is installed.
+    if not which("ruff") or not check_ruff_version(ruff_expected_version):
+        print("Please check the right version of Ruff is installed.")
+        show_ruff_installation_steps(ruff_expected_version)
+        sys.exit(1)
+
+    cmd = ["ruff", "check", "--fix", "../.", ".", "--config", ruff_toml_file]
+
+    if not run_ruff_checks(cmd):
+        sys.exit(1)
 
 
-cmd = ["ruff", "check", "--fix", "../.", ".", "--config", RUFF_TOML_FILE]
-
-if not run_ruff_checks(cmd):
-    sys.exit(1)
+if __name__ == "__main__":
+    main()
