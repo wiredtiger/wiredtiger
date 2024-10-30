bfe200ef1737701b6adb151b34f5d0227e1a07a7
Etienne Petrel <etienne.petrel@mongodb.com>
2024-10-30 15:49:52 +1100

    More refactoring and comments

--- dist/ruff_check.py
+++ dist/ruff_check.py
@@ -10,7 +10,7 @@ import sys
 CURRENT_DIR = pathlib.Path(__file__).parent.resolve()
 
 
-def check_ruff_version(expected_version: str) -> bool:
+def __check_ruff_version(expected_version: str) -> bool:
     cmd = ["ruff", "--version"]
     try:
         output = subprocess.check_output(cmd, cwd=CURRENT_DIR).decode().strip()
@@ -26,19 +26,29 @@ def check_ruff_version(expected_version: str) -> bool:
     return True
 
 
-def get_expected_ruff_version(config_file: str) -> str:
+# Check Ruff is installed with the expected version.
+def check_ruff_installation(expected_version: str) -> bool:
+    if not which("ruff") or not __check_ruff_version(expected_version):
+        return False
+    return True
+
+
+# Retrieve the expected Ruff version from a toml file.
+def get_expected_ruff_version(ruff_toml_file: str) -> str:
     ruff_version = None
-    with open(config_file) as file:
+    with open(ruff_toml_file) as file:
         for line in file:
             m = re.search(r'required-version = "==(\d.\d.\d)"', line.strip())
             if m:
                 ruff_version = m.group(1)
                 break
-    assert ruff_version, f"No ruff version has been found in {config_file}"
+    assert ruff_version, f"No ruff version has been found in {ruff_toml_file}"
     return ruff_version
 
 
-def run_ruff_checks(cmd: list[str]) -> bool:
+# Execute the Ruff check command.
+def run_ruff_checks(ruff_toml_file: str) -> bool:
+    cmd = ["ruff", "check", "--fix", "../.", ".", "--config", ruff_toml_file]
     try:
         output = subprocess.check_output(cmd, cwd=CURRENT_DIR).decode().strip()
         if output == "All checks passed!":
@@ -53,6 +63,7 @@ def run_ruff_checks(cmd: list[str]) -> bool:
         return False
 
 
+# Show how to install Ruff.
 def show_ruff_installation_steps(expected_version: str) -> None:
     doc_link = "https://docs.astral.sh/ruff/installation/"
     print(f"Please install Ruff using the official documentation: {doc_link}\n")
@@ -66,15 +77,14 @@ def main():
     ruff_toml_file = f"{CURRENT_DIR}/ruff.toml"
     ruff_expected_version = get_expected_ruff_version(ruff_toml_file)
 
-    # Check the correct version of Ruff is installed.
-    if not which("ruff") or not check_ruff_version(ruff_expected_version):
+    # Check Ruff is present on the system.
+    if not check_ruff_installation(ruff_expected_version):
         print("Please check the right version of Ruff is installed.")
         show_ruff_installation_steps(ruff_expected_version)
         sys.exit(1)
 
-    cmd = ["ruff", "check", "--fix", "../.", ".", "--config", ruff_toml_file]
-
-    if not run_ruff_checks(cmd):
+    # Run Ruff checks.
+    if not run_ruff_checks(ruff_toml_file):
         sys.exit(1)
 
 
