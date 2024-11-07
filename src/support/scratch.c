023224a387f1a3225c76059604d53098aa174eb5
Ravi Prakash Giri <ravi.giri@mongodb.com>
2024-11-07 21:36:01 +0000

    WT-13640 Implement Locking Mechanism for default session scratch buffer operations

--- src/support/scratch.c
+++ src/support/scratch.c
@@ -274,6 +274,13 @@ __wt_scr_alloc_func(WT_SESSION_IMPL *session, size_t size, WT_ITEM **scratchp
     size_t allocated;
     u_int i;
 
+    /*
+     * To prevent concurrent access to the scratch buffer when multiple connection APIs use the same
+     * default session, a lock should be acquired.
+     */
+    if (F_ISSET(session, WT_SESSION_INTERNAL))
+        __wt_spin_lock(session, &session->session_scratch_lock);
+
     /* Don't risk the caller not catching the error. */
     *scratchp = NULL;
 
@@ -349,9 +356,15 @@ __wt_scr_alloc_func(WT_SESSION_IMPL *session, size_t size, WT_ITEM **scratchp
 #endif
 
     *scratchp = *best;
+    if (F_ISSET(session, WT_SESSION_INTERNAL))
+        __wt_spin_unlock(session, &session->session_scratch_lock);
+
     return (0);
 
 err:
+    if (F_ISSET(session, WT_SESSION_INTERNAL))
+        __wt_spin_unlock(session, &session->session_scratch_lock);
+
     WT_RET_MSG(session, ret, "session unable to allocate a scratch buffer");
 }
 
