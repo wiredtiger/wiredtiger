05ebc3b62cbcd4f833f1d98575125167a4fdf166
Will Korteland <korteland@users.noreply.github.com>
2024-11-07 11:14:18 +1100

    SLS-8 Compression and encryption for deltas (#11180)
    This adds support for both encryption and compression for both deltas
    and full pages, as well as a reasonable amount of testing.
    
    I've made fairly liberal use of TODO comments here, but I don't see any
    of them as M3 blockers.
    
    ---------
    
    Co-authored-by: Chenhao Qu <chenhao.qu@mongodb.com>
    Co-authored-by: Etienne Petrel <77254506+etienneptl@users.noreply.github.com>

--- src/block_pantry/block_mgr.c
+++ src/block_pantry/block_mgr.c
@@ -109,6 +109,20 @@ __bmp_write_size(WT_BM *bm, WT_SESSION_IMPL *session, size_t *sizep)
 }
 
 /*
+ * __bm_encrypt_skip_size --
+ *     Return the skip size for encryption
+ */
+static size_t
+__bm_encrypt_skip_size(WT_BM *bm, WT_SESSION_IMPL *session, bool is_delta)
+{
+    WT_UNUSED(bm);
+    WT_UNUSED(session);
+    WT_UNUSED(is_delta);
+
+    return (WT_BLOCK_PANTRY_HEADER_BYTE_SIZE);
+}
+
+/*
  * __bmp_method_set --
  *     Set up the legal methods.
  */
@@ -147,6 +161,7 @@ __bmp_method_set(WT_BM *bm, bool readonly)
     bm->verify_start = __wt_bmp_verify_start;
     bm->write = __bmp_write;
     bm->write_size = __bmp_write_size;
+    bm->encrypt_skip = __bm_encrypt_skip_size;
 }
 
 /*
