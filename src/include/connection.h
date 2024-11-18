9519687df5111ecf27bdc134d4c68797b4f22ddd
Chenhao Qu <chenhao.qu@mongodb.com>
2024-11-18 00:36:52 +0000

    SLS-588 Fix advancing checkpoint in the failure case

--- src/include/connection.h
+++ src/include/connection.h
@@ -185,11 +185,7 @@ struct __wt_disaggregated_storage {
     WT_BUCKET_STORAGE *bstorage;
     WT_NAMED_STORAGE_SOURCE *nstorage;
     WT_FILE_HANDLE *bstorage_meta;
-
-    wt_shared uint64_t num_meta_put; /* The number metadata puts since connection open. */
-
-    uint64_t num_meta_put_at_ckpt_begin; /* The number metadata puts at checkpoint begin. */
-                                         /* Updates are protected by the checkpoint lock. */
+    /* Updates are protected by the checkpoint lock. */
 };
 
 /*
