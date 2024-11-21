712cd4cd5846769dab2ba54bca6c787c24377465
Yury Ershov <yuriy.ershov@gmail.com>
2024-11-21 19:12:31 +1100

    Add eviction.evict_use_npos config

--- dist/api_data.py
+++ dist/api_data.py
@@ -636,7 +636,7 @@ connection_runtime_config = [
             if true, control all dirty page evictions through forcing update restore eviction.''',
             type='boolean'),
         Config('eviction_checkpoint_ts_ordering', 'false', r'''
-            if true, act as if eviction is being run in parallel to checkpoint. We should return 
+            if true, act as if eviction is being run in parallel to checkpoint. We should return
             EBUSY in eviction if we detect any timestamp ordering issue.''',
             type='boolean'),
         ]),
@@ -658,6 +658,12 @@ connection_runtime_config = [
             Config('evict_sample_inmem', 'true', r'''
                 If no in-memory ref is found on the root page, attempt to locate a random
                 in-memory page by examining all entries on the root page.''',
+                type='boolean'),
+            Config('evict_use_npos', 'false', r'''
+                Experimental: Use "soft pointers" instead of hard hazard pointers in eviction server
+                to remember its walking position in the tree. This might be preferable to set to
+                "true" if there are many collections. It can improve or degrade performance
+                depending on the workload.''',
                 type='boolean'),
             ]),
     Config('eviction_checkpoint_target', '1', r'''
