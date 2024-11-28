c7d733940640cdc819212e849191411849e08aa6
Chenhao Qu <chenhao.qu@mongodb.com>
2024-11-28 23:58:33 +0000

    Fix compile

--- src/conn/conn_layered.c
+++ src/conn/conn_layered.c
@@ -1420,7 +1420,7 @@ err:
 }
 
 /* TODO: use this function to drain the inges table */
-static void __layered_drain_ingest_tables(void) __attribute__((unused));
+static int __layered_drain_ingest_tables(void) __attribute__((unused));
 
 /*
  * __layered_drain_ingest_tables --
