88e4e1d6002f741ff5334babc73bfcada0945c08
Andrew Morton <andrew.morton@mongodb.com>
2024-12-01 20:26:00 +0000

    Split live_restore compatibility checks from file system creation

--- src/conn/conn_api.c
+++ src/conn/conn_api.c
@@ -2823,15 +2823,26 @@ wiredtiger_open(const char *home, WT_EVENT_HANDLER *event_handler, const char *c
      * ensure we have a valid file system.
      *
      * Check the "aux_path" config. If it is provided validate that a custom file system has not
-     * been provided, and that the connection is not in memory.
+     * been provided, and that the connection is not in memory or Windows.
      */
     WT_ERR(__wt_config_gets(session, cfg, "aux_path", &cval));
+
+    if (cval.len != 0) {
+        /* Live restore compatibility checks. */
+        if (conn->file_system != NULL)
+            WT_ERR_MSG(session, EINVAL, "Live restore is not compatible with custom file systems.");
+        if (F_ISSET(conn, WT_CONN_IN_MEMORY))
+            WT_ERR_MSG(
+              session, EINVAL, "Live restore is not compatible with in-memory connections.");
+#ifdef _MSC_VER
+        WT_ERR_MSG(session, EINVAL, "Live restore is not supported on Windows.");
+#endif
+    }
+
     if (conn->file_system == NULL) {
-        if (F_ISSET(conn, WT_CONN_IN_MEMORY)) {
-            WT_ASSERT_ALWAYS(session, cval.len == 0,
-              "An in memory connection is not compatible with a live restore.");
+        if (F_ISSET(conn, WT_CONN_IN_MEMORY))
             WT_ERR(__wt_os_inmemory(session));
-        } else {
+        else {
 #if defined(_MSC_VER)
             WT_ERR(__wt_os_win(session));
 #else
@@ -2843,9 +2854,7 @@ wiredtiger_open(const char *home, WT_EVENT_HANDLER *event_handler, const char *c
                 WT_ERR(__wt_os_posix(session, &conn->file_system));
 #endif
         }
-    } else
-        WT_ASSERT_ALWAYS(
-          session, cval.len == 0, "A custom file system is not compatible with a live restore.");
+    }
     WT_ERR(__conn_chk_file_system(session, F_ISSET(conn, WT_CONN_READONLY)));
 
     /* Make sure no other thread of control already owns this database. */
