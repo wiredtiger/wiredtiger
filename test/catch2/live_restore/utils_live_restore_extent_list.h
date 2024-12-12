d3580f9352c31ff18e0f748e3200fc1d2db87246
Andrew Morton <andrew.morton@mongodb.com>
2024-12-12 03:38:15 +0000

    Move helper functions into utils file

--- test/catch2/live_restore/utils_live_restore_extent_list.h
+++ test/catch2/live_restore/utils_live_restore_extent_list.h
@@ -8,7 +8,15 @@
 
 #pragma once
 
+#include <string>
+
 #include "wt_internal.h"
+#include "../../../src/live_restore/live_restore_private.h"
 
 namespace utils {
+
+std::string build_str_from_extent(WT_LIVE_RESTORE_HOLE_NODE *ext);
+bool extent_list_in_order(WT_LIVE_RESTORE_HOLE_NODE *head);
+void write_to_file(std::string filepath, int len);
+
 } // namespace utils.
