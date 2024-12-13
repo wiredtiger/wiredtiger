06dae5fef33e1c1adce65ed2052662eacaa5d78a
Sean04 <sean.watt@mongodb.com>
2024-12-13 00:21:33 +0000

    WT-13858 Add early return.

--- src/live_restore/live_restore_fs.c
+++ src/live_restore/live_restore_fs.c
@@ -769,7 +769,8 @@ __live_restore_fh_find_holes_in_dest_file(
 
     data_end_offset = 0;
     WT_SYSCALL(((fd = open(filename, O_RDONLY)) == -1 ? -1 : 0), ret);
-    WT_ERR(ret);
+    if (ret != 0)
+        return (ret);
 
     /* Check that we opened a valid file descriptor. */
     WT_ASSERT(session, fcntl(fd, F_GETFD) != -1 || errno != EBADF);
