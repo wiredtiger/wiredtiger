032235bdbfca37396e0b27efdba71fb13308b7b3
Andrew Morton <andrew.morton@mongodb.com>
2024-12-15 21:17:00 +0000

    Add undoc, replace debug_fill_holes_on_close with a generic debug_flags

--- src/live_restore/live_restore_fs.c
+++ src/live_restore/live_restore_fs.c
@@ -632,7 +632,8 @@ __live_restore_fh_close(WT_FILE_HANDLE *fh, WT_SESSION *wt_session)
     session = (WT_SESSION_IMPL *)wt_session;
     __wt_verbose_debug1(session, WT_VERB_FILEOPS, "LIVE_RESTORE_FS: Closing file: %s\n", fh->name);
 
-    if (lr_fh->destination.back_pointer->debug_fill_holes_on_close)
+    if (FLD_ISSET(
+          lr_fh->destination.back_pointer->debug_flags, WT_LIVE_RESTORE_DEBUG_FILL_HOLES_ON_CLOSE))
         WT_RET(__live_restore_fs_fill_holes_on_file_close(fh, wt_session));
 
     lr_fh->destination.fh->close(lr_fh->destination.fh, wt_session);
@@ -1169,7 +1170,8 @@ __wt_os_live_restore_fs(
 
     WT_ERR_NOTFOUND_OK(
       __wt_config_gets(session, cfg, "live_restore.debug.fill_holes_on_close", &cval), false);
-    lr_fs->debug_fill_holes_on_close = cval.val != 0;
+    if (cval.val != 0)
+        FLD_SET(lr_fs->debug_flags, WT_LIVE_RESTORE_DEBUG_FILL_HOLES_ON_CLOSE);
 
     __wt_verbose_debug1(session, WT_VERB_FILEOPS,
       "WiredTiger started in live restore mode! Source path is: %s, Destination path is %s",
