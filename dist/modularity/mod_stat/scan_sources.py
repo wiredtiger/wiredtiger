80636f159f58b5b6c47fa46ca6130180762c207a
Siddhartha Mahajan <siddhartha.mahajan8899@gmail.com>
2024-12-16 08:35:27 +0000

    read wt_feds instead of importing it

--- dist/modularity/mod_stat/scan_sources.py
+++ dist/modularity/mod_stat/scan_sources.py
@@ -10,15 +10,25 @@ import sys, os
 
 # layercparse is a library written and maintained by the WiredTiger team.
 import layercparse as lcp
-from layercparse.scan_sources_tool import scan_sources_main
-import wt_defs
+from layercparse.scan_sources_tool import commandline, scan_sources_main
+
+def load_wt_defs(rootPath):
+    wt_defs_path = os.path.join(rootPath, 'dist', 'access_check', 'wt_defs.py')
+    with open(wt_defs_path, "r") as f:
+        code = f.read()
+    wt_defs = {}
+    exec(code, {}, wt_defs)
+    return wt_defs
 
 def main():
+    args = commandline()
+    wt_defs = load_wt_defs(args.home)
+
     lcp.Log.module_name_mismatch.enabled = False
 
-    return scan_sources_main(extraFiles=wt_defs.extraFiles,
-                             modules=wt_defs.modules,
-                             extraMacros=wt_defs.extraMacros)
+    return scan_sources_main(extraFiles=wt_defs["extraFiles"],
+                             modules=wt_defs["modules"],
+                             extraMacros=wt_defs["extraMacros"])
 
 if __name__ == "__main__":
     try:
