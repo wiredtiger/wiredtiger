65baeb2801a80fd98279c654e1864c529b0c9df1
Siddhartha Mahajan <siddhartha.mahajan8899@gmail.com>
2024-12-18 02:04:27 +0000

    Allow WT to use layercpare latest commit from github

--- dist/modularity/init.sh
+++ dist/modularity/init.sh
@@ -6,5 +6,13 @@ set -ueo pipefail
 chmod 755 .venv/bin/activate
 
 . .venv/bin/activate
-pip3 -q --disable-pip-version-check install -r requirements.txt
 
+REPO_URL="https://github.com/wiredtiger/layercparse.git"
+BRANCH="main"
+HASH_FILE=".layercparse_commit_hash"
+LATEST_HASH=$(git ls-remote "$REPO_URL" "$BRANCH" | awk '{print $1}')
+
+if [[ ! -f "$HASH_FILE" || "$LATEST_HASH" != "$(cat $HASH_FILE)" ]]; then
+    pip3 -q --disable-pip-version-check install git+"$REPO_URL@$BRANCH"
+    echo "$LATEST_HASH" > "$HASH_FILE"
+fi
