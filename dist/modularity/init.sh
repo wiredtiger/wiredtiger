9d19393a426e6a48b2fa8f0c7eed8dc5570f63fd
Siddhartha Mahajan <siddhartha.mahajan8899@gmail.com>
2024-12-20 07:17:49 +0000

    last update

--- dist/modularity/init.sh
+++ dist/modularity/init.sh
@@ -10,20 +10,16 @@ REPO_URL="https://github.com/wiredtiger/layercparse.git"
 BRANCH="main"
 HASH_FILE=".venv/layercparse_hash"
 
-is_cached() {
+is_layercparse_cached() {
   pip3 -qq show layercparse > /dev/null 2>&1
 }
 
-needs_update() {
-    if is_cached && [[ -f "$HASH_FILE" ]]; then
-        if find "$HASH_FILE" -mtime -1 | grep -q "$HASH_FILE"; then
-            return 1
-        fi
-    fi
-    return 0
+# Function to check if layercparse cache is older than 24 hours
+is_layercparse_cache_outdated() {
+  [[ -n $(find "$HASH_FILE" -mtime +0 2>/dev/null) ]]
 }
 
-if needs_update; then
+if ! is_layercparse_cached || is_layercparse_cache_outdated; then
     latest_hash=$(git ls-remote $REPO_URL $BRANCH | awk '{print $1}')
     if [[ ! -f "$HASH_FILE" || "$latest_hash" != "$(cat $HASH_FILE)" || ! is_cached ]]; then
         pip3 -q --disable-pip-version-check install git+"$REPO_URL@$BRANCH"
