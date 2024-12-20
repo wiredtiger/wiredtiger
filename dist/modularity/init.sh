439586de978870ce4e17bd65fcafe9f71614ea17
Siddhartha Mahajan <siddhartha.mahajan8899@gmail.com>
2024-12-20 05:37:09 +0000

    update init.sh

--- dist/modularity/init.sh
+++ dist/modularity/init.sh
@@ -2,28 +2,30 @@
 
 set -ueo pipefail
 
-VENV_PATH=".venv"
 [[ -f .venv/bin/activate ]] || virtualenv -q -p python3 .venv
 chmod 755 .venv/bin/activate
-. "$VENV_PATH"/bin/activate
+. .venv/bin/activate
 
 REPO_URL="https://github.com/wiredtiger/layercparse.git"
 BRANCH="main"
-LAYERCPARSE_METADATA_FILE="$VENV_PATH/layercparse_metadata"
+HASH_FILE=".venv/layercparse_hash"
 
-IS_CACHED=$(pip3 show layercparse > /dev/null 2>&1 && echo true || echo false)
-if ($IS_CACHED && [[ -f "$LAYERCPARSE_METADATA_FILE" ]]); then
-    LAST_TIME_CHECKED=$(grep 'last_time_checked' "$LAYERCPARSE_METADATA_FILE" | cut -d ':' -f 2 | xargs)
+is_cached() {
+  pip3 -qq show layercparse > /dev/null 2>&1
+}
+
+if is_cached && [[ -f "$HASH_FILE" ]]; then
+    LAST_TIME_CHECKED=$(stat -c %Y "$HASH_FILE")
     CURRENT_TIME=$(date +%s)
     DIFF=$((CURRENT_TIME - LAST_TIME_CHECKED))
     if [ "$DIFF" -le 86400 ]; then
-            exit 0
+        return 0
     fi
 fi
 
 LATEST_HASH=$(git ls-remote $REPO_URL $BRANCH | awk '{print $1}')
-if [[ ! -f "$LAYERCPARSE_METADATA_FILE" || "$LATEST_HASH" != "$(grep 'hash' "$LAYERCPARSE_METADATA_FILE" | cut -d ':' -f 2 | xargs)" || $IS_CACHED == false ]]; then
+if [[ ! -f "$HASH_FILE" || "$LATEST_HASH" != "$(cat $HASH_FILE)" || ! is_cached ]]; then
     pip3 -q --disable-pip-version-check install git+"$REPO_URL@$BRANCH"
+    echo "$LATEST_HASH" > "$HASH_FILE"
 fi
-echo "last_time_checked: $(date +%s)" > "$LAYERCPARSE_METADATA_FILE"
-echo "hash: $LATEST_HASH" >> "$LAYERCPARSE_METADATA_FILE"
+touch "$HASH_FILE"
