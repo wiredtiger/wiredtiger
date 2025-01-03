1ff5225ffbc42d77c6336cae25939b04b7087ba9
Alex Pullen <alex@pullen.net.nz>
2025-01-03 03:25:17 +0000

    Update from WT-13864

--- src/support/err.c
+++ src/support/err.c
@@ -344,7 +344,6 @@ __eventv(WT_SESSION_IMPL *session, bool is_json, int error, const char *func, in
             if (len >= tmp->memsize)
                 goto err;
         }
-        WT_ERR(__wt_session_set_last_error(session, error, WT_NONE, tmp->mem));
 
         /* Allocate a scratch buffer (known to be large enough), and JSON encode the message. */
         WT_ERR(__wt_scr_alloc(session, tmp->size * WT_MAX_JSON_ENCODE + 256, &json_msg));
@@ -394,7 +393,6 @@ __eventv(WT_SESSION_IMPL *session, bool is_json, int error, const char *func, in
         prefix_len = sizeof(msg) - remain;
         WT_ERR(__wt_vsnprintf_len_set(p, remain, &len, fmt, ap));
         if (len < remain) {
-            WT_ERR(__wt_session_set_last_error(session, error, WT_NONE, p));
             remain -= len;
             p += len;
             if (err != NULL)
@@ -418,7 +416,6 @@ __eventv(WT_SESSION_IMPL *session, bool is_json, int error, const char *func, in
             no_stderr = true;
             WT_ERR(__wt_vsnprintf_len_set(p, remain, &len, fmt, ap_copy));
             if (len < remain) {
-                WT_ERR(__wt_session_set_last_error(session, error, WT_NONE, p));
                 remain -= len;
                 p += len;
                 if (err != NULL)
