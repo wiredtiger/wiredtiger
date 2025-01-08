dd1d2ad07bef5ab3e22a002d52b74e7afb87717c
Chenhao Qu <chenhao.qu@mongodb.com>
2025-01-08 23:30:58 +0000

    Add a todo

--- src/conn/conn_ckpt.c
+++ src/conn/conn_ckpt.c
@@ -26,6 +26,10 @@ __ckpt_server_config(WT_SESSION_IMPL *session, const char **cfg, bool *startp)
     conn = S2C(session);
 
     WT_RET(__wt_config_gets(session, cfg, "checkpoint.precise", &cval));
+    /*
+     * TODO: Disaggregated storage should only supports precise checkpoint but mongod is not ready
+     * for that yet. Enable precise checkpoint automatically for disagg in the future.
+     */
     if (cval.val)
         F_SET(conn, WT_CONN_PRECISE_CHECKPOINT);
     else
