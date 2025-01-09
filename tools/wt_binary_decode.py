204e8771982912db560524947d5aab82a07a62e9
Don Anderson <donald.anderson@mongodb.com>
2025-01-09 16:19:51 +0000

    Revert "SLS-910 Added support for disagg checkpoint cookies."
    This reverts commit ec92f597778bb448545c1f92f562a95d452b4296.

--- tools/wt_binary_decode.py
+++ tools/wt_binary_decode.py
@@ -33,7 +33,6 @@
 # libraries), and may be useful as 1) a learning tool 2) quick way to hack/extend dumping.
 
 import binascii, codecs, io, os, string, sys, traceback
-from wt_tools_common import unpack_int
 
 # Optional dependency: crc32c
 have_crc32c = False
@@ -60,6 +59,95 @@ class PageHeader:
 # A container for fields in the WT_BLOCK_HEADER
 class BlockHeader:
     pass
+
+################################################################
+# Borrowed from intpacking.py, with small adjustments.
+# Variable-length integer packing
+# need: up to 64 bits, both signed and unsigned
+#
+# Try hard for small values (up to ~2 bytes), after that, just encode the
+# length in the first byte.
+#
+#  First byte | Next |                        |
+#  byte       | bytes| Min Value              | Max Value
+# ------------+------+------------------------+--------------------------------
+# [00 00xxxx] | free | N/A                    | N/A
+# [00 01llll] | 8-l  | -2^64                  | -2^13 - 2^6
+# [00 1xxxxx] | 1    | -2^13 - 2^6            | -2^6 - 1 
+# [01 xxxxxx] | 0    | -2^6                   | -1
+# [10 xxxxxx] | 0    | 0                      | 2^6 - 1 
+# [11 0xxxxx] | 1    | 2^6                    | 2^13 + 2^6 - 1
+# [11 10llll] | l    | 2^13 + 2^6             | 2^64 - 1
+# [11 11xxxx] | free | N/A                    | N/A
+
+NEG_MULTI_MARKER = 0x10
+NEG_2BYTE_MARKER = 0x20
+NEG_1BYTE_MARKER = 0x40
+POS_1BYTE_MARKER = 0x80
+POS_2BYTE_MARKER = 0xc0
+POS_MULTI_MARKER = 0xe0
+
+NEG_1BYTE_MIN = -2**6
+NEG_2BYTE_MIN = -2**13 + NEG_1BYTE_MIN
+POS_1BYTE_MAX = 2**6 - 1
+POS_2BYTE_MAX = 2**13 + POS_1BYTE_MAX
+
+_python3 = (sys.version_info >= (3, 0, 0))
+
+if not _python3:
+    raise Exception('This script requires Python 3')
+
+def _ord(b):
+    return b
+
+def _chr(x, y=None):
+    a = [x]
+    if y != None:
+        a.append(y)
+    return bytes(a)
+
+def _is_string(s):
+    return type(s) is str
+
+def _string_result(s):
+    return s.decode()
+
+def getbits(x, start, end=0):
+    '''return the least significant bits of x, from start to end'''
+    return (x & ((1 << start) - 1)) >> (end)
+
+def get_int(b, size):
+    r = 0
+    for i in range(size):
+        r = (r << 8) | _ord(b[i])
+    return r
+
+def unpack_int(b):
+    marker = _ord(b[0])
+    if marker < NEG_MULTI_MARKER or marker >= 0xf0:
+        raise Exception('Not a packed integer')
+    elif marker < NEG_2BYTE_MARKER:
+        sz = 8 - getbits(marker, 4)
+        if sz < 0:
+            raise Exception('Not a valid packed integer')
+        part1 = (-1 << (sz << 3))
+        part2 = get_int(b[1:], sz)
+        part3 = b[sz+1:]
+        return (part1 | part2, part3)
+    elif marker < NEG_1BYTE_MARKER:
+        return (NEG_2BYTE_MIN + ((getbits(marker, 5) << 8) | _ord(b[1])), b[2:])
+    elif marker < POS_1BYTE_MARKER:
+        return (NEG_1BYTE_MIN + getbits(marker, 6), b[1:])
+    elif marker < POS_2BYTE_MARKER:
+        return (getbits(marker, 6), b[1:])
+    elif marker < POS_MULTI_MARKER:
+        return (POS_1BYTE_MAX + 1 +
+               ((getbits(marker, 5) << 8) | _ord(b[1])), b[2:])
+    else:
+        sz = getbits(marker, 4)
+        return (POS_2BYTE_MAX + 1 + get_int(b[1:], sz), b[sz+1:])
+
+################################################################
 
 # page types from btmem.h:
 WT_PAGE_INVALID = 0
--- tools/wt_tools_common.py
+++ tools/wt_tools_common.py
@@ -28,81 +28,34 @@
 
 # Common functions used by python scripts in this directory.
 
-import sys
+import os, sys
 
-################################################################
-# Borrowed from intpacking.py, with small adjustments.
-# Variable-length integer packing
-# need: up to 64 bits, both signed and unsigned
-#
-# Try hard for small values (up to ~2 bytes), after that, just encode the
-# length in the first byte.
-#
-#  First byte | Next |                        |
-#  byte       | bytes| Min Value              | Max Value
-# ------------+------+------------------------+--------------------------------
-# [00 00xxxx] | free | N/A                    | N/A
-# [00 01llll] | 8-l  | -2^64                  | -2^13 - 2^6
-# [00 1xxxxx] | 1    | -2^13 - 2^6            | -2^6 - 1
-# [01 xxxxxx] | 0    | -2^6                   | -1
-# [10 xxxxxx] | 0    | 0                      | 2^6 - 1
-# [11 0xxxxx] | 1    | 2^6                    | 2^13 + 2^6 - 1
-# [11 10llll] | l    | 2^13 + 2^6             | 2^64 - 1
-# [11 11xxxx] | free | N/A                    | N/A
-
-NEG_MULTI_MARKER = 0x10
-NEG_2BYTE_MARKER = 0x20
-NEG_1BYTE_MARKER = 0x40
-POS_1BYTE_MARKER = 0x80
-POS_2BYTE_MARKER = 0xc0
-POS_MULTI_MARKER = 0xe0
-
-NEG_1BYTE_MIN = -2**6
-NEG_2BYTE_MIN = -2**13 + NEG_1BYTE_MIN
-POS_1BYTE_MAX = 2**6 - 1
-POS_2BYTE_MAX = 2**13 + POS_1BYTE_MAX
-
-_python3 = (sys.version_info >= (3, 0, 0))
-
-if not _python3:
-    raise Exception('This script requires Python 3')
-
-def _ord(b):
-    return b
-
-def _getbits(x, start, end=0):
-    '''return the least significant bits of x, from start to end'''
-    return (x & ((1 << start) - 1)) >> (end)
-
-def _get_int(b, size):
-    r = 0
-    for i in range(size):
-        r = (r << 8) | _ord(b[i])
-    return r
-
-def unpack_int(b):
-    marker = _ord(b[0])
-    if marker < NEG_MULTI_MARKER or marker >= 0xf0:
-        raise Exception('Not a packed integer')
-    elif marker < NEG_2BYTE_MARKER:
-        sz = 8 - _getbits(marker, 4)
-        if sz < 0:
-            raise Exception('Not a valid packed integer')
-        part1 = (-1 << (sz << 3))
-        part2 = _get_int(b[1:], sz)
-        part3 = b[sz+1:]
-        return (part1 | part2, part3)
-    elif marker < NEG_1BYTE_MARKER:
-        return (NEG_2BYTE_MIN + ((_getbits(marker, 5) << 8) | _ord(b[1])), b[2:])
-    elif marker < POS_1BYTE_MARKER:
-        return (NEG_1BYTE_MIN + _getbits(marker, 6), b[1:])
-    elif marker < POS_2BYTE_MARKER:
-        return (_getbits(marker, 6), b[1:])
-    elif marker < POS_MULTI_MARKER:
-        return (POS_1BYTE_MAX + 1 +
-               ((_getbits(marker, 5) << 8) | _ord(b[1])), b[2:])
+# Set the system path to include the python build directory if we can find it.
+def setup_python_path():
+    # Assuming we're somewhere in a build directory, walk the tree up
+    # looking for the wt program.
+    curdir = os.getcwd()
+    d = curdir
+    found = False
+    while d != '/':
+        if os.path.isfile(os.path.join(d, 'wt')) or os.path.isfile(os.path.join(d, 'wt.exe')):
+            found = True
+            break
+        d = os.path.dirname(d)
+    if found:
+        sys.path.insert(1, os.path.join(d, 'lang', 'python'))
     else:
-        sz = _getbits(marker, 4)
-        return (POS_2BYTE_MAX + 1 + _get_int(b[1:], sz), b[sz+1:])
+        print('Cannot find wt, must run this from a build directory')
+        sys.exit(1)
 
-################################################################
+# Import the wiredtiger directory and return the wiredtiger_open function.
+def import_wiredtiger():
+    try:
+        from wiredtiger import wiredtiger_open
+    except:
+        setup_python_path()
+        from wiredtiger import wiredtiger_open
+    return wiredtiger_open
+
+# This name will be exported
+wiredtiger_open = import_wiredtiger()
