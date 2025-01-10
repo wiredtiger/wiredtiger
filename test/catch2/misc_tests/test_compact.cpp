d5bbd5bb4ed976898edff4e7866a64a1162860e8
Dylan Liang <dylanthekid350@gmail.com>
2025-01-10 04:36:25 +0000

    implemented working unit tests for compact and rollback workflows

--- test/catch2/misc_tests/test_compact.cpp
+++ test/catch2/misc_tests/test_compact.cpp
@@ -9,6 +9,8 @@
 #include <catch2/catch.hpp>
 #include "wt_internal.h"
 #include "../wrappers/connection_wrapper.h"
+#include <thread>
+#include <chrono>
 
 /*
  * [wt_compact]: test_compact.cpp
@@ -17,22 +19,27 @@
 
 TEST_CASE("Test functions for compaction", "[compact]")
 {
-    WT_CONNECTION *conn;
     WT_SESSION *session;
-    WT_SESSION_IMPL *session_impl;
-    const char *err_msg_content;
 
     connection_wrapper conn_wrapper = connection_wrapper(".", "create");
-    conn = conn_wrapper.get_wt_connection();
+    WT_CONNECTION *conn = conn_wrapper.get_wt_connection();
     REQUIRE(conn->open_session(conn, NULL, NULL, &session) == 0);
-    session_impl = (WT_SESSION_IMPL *)session;
+    WT_SESSION_IMPL *session_impl = (WT_SESSION_IMPL *)session;
+    WT_CONNECTION_IMPL *conn_impl = (WT_CONNECTION_IMPL *)conn;
 
     SECTION("Test __wt_background_compact_signal")
     {
-        CHECK(__wt_background_compact_signal(session_impl, "") == EINVAL);
-        err_msg_content = "Cannot reconfigure background compaction while it's already running.";
+        conn_impl->background_compact.running = true;
+        conn_impl->background_compact.config = "";
+
+        CHECK(__wt_background_compact_signal(session_impl, "background=true") == EINVAL);
+        const char *err_msg_content =
+          "Cannot reconfigure background compaction while it's already running.";
         CHECK(session_impl->err_info.err == EINVAL);
         CHECK(session_impl->err_info.sub_level_err == WT_BACKGROUND_COMPACT_ALREADY_RUNNING);
         CHECK(strcmp(session_impl->err_info.err_msg, err_msg_content) == 0);
+
+        conn_impl->background_compact.running = false;
+        conn_impl->background_compact.config = NULL;
     }
 }
--- test/catch2/misc_tests/test_rollback.cpp
+++ test/catch2/misc_tests/test_rollback.cpp
@@ -25,32 +25,91 @@ check_error(WT_SESSION_IMPL *session, int error, int sub_level_error, std::strin
 
 TEST_CASE("Test functions for rollback workflows", "[rollback]")
 {
-    WT_CONNECTION *conn;
     WT_SESSION *session;
-    WT_SESSION_IMPL *session_impl;
 
     connection_wrapper conn_wrapper = connection_wrapper(".", "create");
-    conn = conn_wrapper.get_wt_connection();
+    WT_CONNECTION *conn = conn_wrapper.get_wt_connection();
+    WT_CONNECTION_IMPL *conn_impl = (WT_CONNECTION_IMPL *)conn;
     REQUIRE(conn->open_session(conn, NULL, NULL, &session) == 0);
-    session_impl = (WT_SESSION_IMPL *)session;
+    WT_SESSION_IMPL *session_impl = (WT_SESSION_IMPL *)session;
 
     SECTION("Test __wti_evict_app_assist_worker")
     {
-        CHECK(__wti_evict_app_assist_worker(session_impl, true, true, 1.0) == WT_ROLLBACK);
-        check_error(session_impl, WT_ROLLBACK, WT_CACHE_OVERFLOW, "Cache capacity has overflown");
-    }
+        WT_CURSOR *cursor;
 
-    SECTION("Test __wt_txn_is_blocking")
-    {
-        CHECK(__wt_txn_is_blocking(session_impl) == WT_ROLLBACK);
-        check_error(session_impl, WT_ROLLBACK, WT_WRITE_CONFLICT,
-          "Write conflict between concurrent operations");
+        // Set eviction trigger, cache max wait and cache size to low values.
+        conn_impl->evict_server_running = true;
+        conn_impl->evict->cache_max_wait_us = 1;
+        conn_impl->evict->eviction_trigger = 1;
+        conn_impl->cache_size = 1;
+
+        // Create a table and insert key and value to create a page to evict.
+        REQUIRE(session->create(session, "table:rollback", "key_format=S,value_format=S") == 0);
+        session->open_cursor(session, "table:rollback", NULL, NULL, &cursor);
+        session->begin_transaction(session, NULL);
+        cursor->set_key(cursor, "key");
+        cursor->set_value(cursor, "value");
+        cursor->update(cursor);
+        session->commit_transaction(session, NULL);
+        cursor->close(cursor);
+
+        CHECK(__wti_evict_app_assist_worker(session_impl, false, false, 100) == WT_ROLLBACK);
+        check_error(session_impl, WT_ROLLBACK, WT_CACHE_OVERFLOW, "Cache capacity has overflown");
+
+        // Drop the table.
+        session->drop(session, "table:rollback", NULL);
     }
 
     SECTION("Test __txn_modify_block")
     {
-        // CHECK(__txn_modify_block(session_impl,))
+        // Create a table so session can have a set dhandle.
+        REQUIRE(session->create(session, "table:rollback", "key_format=S,value_format=S") == 0);
+        FLD_SET(session_impl->lock_flags, WT_SESSION_LOCKED_HANDLE_LIST);
+        REQUIRE(__wt_conn_dhandle_alloc(session_impl, "table:rollback", NULL) == 0);
+
+        // Allocate update.
+        WT_UPDATE *upd;
+        REQUIRE(__wt_upd_alloc(session_impl, NULL, 2, &upd, NULL) == 0);
+
+        // Transaction must be invisible, so we say that the session have a transaction snapshot
+        // and the transaction id is greater than the max snap transaction id.
+        F_SET(session_impl->txn, WT_TXN_HAS_SNAPSHOT);
+        session_impl->txn->snapshot_data.snap_max = 0;
+        upd->txnid = 1;
+        upd->type = 1;
+        CHECK(__txn_modify_block(session_impl, NULL, upd, NULL));
+        check_error(session_impl, WT_ROLLBACK, WT_WRITE_CONFLICT,
+          "Write conflict between concurrent operations");
+
+        // Clear lock so the table can be dropped.
+        FLD_CLR(session_impl->lock_flags, WT_SESSION_LOCKED_HANDLE_LIST);
+        session->drop(session, "table:rollback", NULL);
+    }
+
+    SECTION("Test __wt_txn_is_blocking")
+    {
+        // Allow rollbacks.
+        session_impl->txn->mod_count = 1;
+        session_impl->operation_start_us = 1;
+        session_impl->operation_timeout_us = 1;
+        F_SET(session_impl, WT_TXN_RUNNING);
+
+        // Set transaction id and oldest pinned id to be the same.
+        WT_TXN_SHARED *txn_shared = WT_SESSION_TXN_SHARED(session_impl);
+        txn_shared->id = 2;
+        txn_shared->pinned_id = 2;
+
+        CHECK(__wt_txn_is_blocking(session_impl) == WT_ROLLBACK);
         check_error(session_impl, WT_ROLLBACK, WT_OLDEST_FOR_EVICTION,
           "Transaction has the oldest pinned transaction ID");
+
+        // Reset back to the initial values.
+        session_impl->txn->mod_count = 0;
+        session_impl->operation_start_us = 0;
+        session_impl->operation_timeout_us = 0;
+        F_CLR(session_impl, WT_TXN_RUNNING);
+
+        txn_shared->id = 0;
+        txn_shared->pinned_id = 0;
     }
 }
