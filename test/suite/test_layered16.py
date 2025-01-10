2d054ae77bfbd56556409312a56df83de58b0d98
Don Anderson <donald.anderson@mongodb.com>
2025-01-10 18:22:22 +0000

    SLS-873 A layered cursor reset should force next usage to reopen at the latest checkpoint

---
+++ test/suite/test_layered16.py
@@ -0,0 +1,222 @@
+#!/usr/bin/env python3
+#
+# Public Domain 2014-present MongoDB, Inc.
+# Public Domain 2008-2014 WiredTiger, Inc.
+#
+# This is free and unencumbered software released into the public domain.
+#
+# Anyone is free to copy, modify, publish, use, compile, sell, or
+# distribute this software, either in source code form or as a compiled
+# binary, for any purpose, commercial or non-commercial, and by any
+# means.
+#
+# In jurisdictions that recognize copyright laws, the author or authors
+# of this software dedicate any and all copyright interest in the
+# software to the public domain. We make this dedication for the benefit
+# of the public at large and to the detriment of our heirs and
+# successors. We intend this dedication to be an overt act of
+# relinquishment in perpetuity of all present and future rights to this
+# software under copyright law.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+
+import os, time, wiredtiger, wttest
+from helper_disagg import DisaggConfigMixin, gen_disagg_storages
+from wtscenario import make_scenarios
+
+# test_layered16.py
+#    Extra tests for follower picking up new checkpoints.
+class test_layered16(wttest.WiredTigerTestCase, DisaggConfigMixin):
+    nitems = 500
+
+    conn_base_config = 'layered_table_log=(enabled),statistics=(all),statistics_log=(wait=1,json=true,on_close=true),' \
+                     + 'disaggregated=(stable_prefix=.,page_log=palm),'
+    conn_config = conn_base_config + 'disaggregated=(role="leader")'
+
+    create_session_config = 'key_format=S,value_format=S'
+
+    layered_uris = ["layered:test_layered16a", "layered:test_layered16b"]
+    #layered_uris = ["layered:test_layered16a"]
+    other_uris = ["file:test_layered16c", "table:test_layered16d"]
+    other_uris = []
+    all_uris = layered_uris + other_uris
+
+    disagg_storages = gen_disagg_storages('test_layered16', disagg_only = True)
+    scenarios = make_scenarios(disagg_storages)
+
+    # Load the page log extension, which has object storage support
+    def conn_extensions(self, extlist):
+        if os.name == 'nt':
+            extlist.skip_if_missing = True
+        DisaggConfigMixin.conn_extensions(self, extlist)
+
+    # Custom test case setup
+    def early_setup(self):
+        os.mkdir('follower')
+        # Create the home directory for the PALM k/v store, and share it with the follower.
+        os.mkdir('kv_home')
+        os.symlink('../kv_home', 'follower/kv_home', target_is_directory=True)
+
+    # Test more than one table.
+    def test_layered16(self):
+        # Create all tables in the leader
+        for uri in self.all_uris:
+            cfg = self.create_session_config
+            if not uri.startswith('layered'):
+                cfg += ',block_manager=disagg,layered_table_log=(enabled=false),log=(enabled=false)'
+            self.session.create(uri, cfg)
+
+        # Create the follower
+        conn_follow = self.wiredtiger_open('follower', self.extensionsConfig() + ',create,' + self.conn_base_config + 'disaggregated=(role="follower")')
+        session_follow = conn_follow.open_session('')
+        follower_cursors = dict()
+
+        #
+        # Setup: Check data in the follower normally.
+        #
+
+        # Put data to all tables, version 0
+        value_prefix0 = '---'
+        for uri in self.all_uris:
+            cursor = self.session.open_cursor(uri, None, None)
+            for i in range(self.nitems):
+                cursor[str(i)] = value_prefix0 + str(i)
+                if i % 250 == 0:
+                    time.sleep(1)
+            cursor.close()
+
+        time.sleep(1)
+        self.session.checkpoint()
+        time.sleep(1)
+
+        # Check data in the follower
+        self.disagg_advance_checkpoint(conn_follow)
+        for uri in self.all_uris:
+            cursor = session_follow.open_cursor(uri, None, None)
+            for i in range(self.nitems):
+                self.assertEquals(cursor[str(i)], value_prefix0 + str(i))
+            cursor.close()
+
+        #
+        # Part 1: Check data in the follower, but keep the cursors open.
+        #
+
+        # Put data to all tables, version 1
+        value_prefix1 = 'aaa'
+        for uri in self.all_uris:
+            cursor = self.session.open_cursor(uri, None, None)
+            for i in range(self.nitems):
+                cursor[str(i)] = value_prefix1 + str(i)
+                if i % 250 == 0:
+                    time.sleep(1)
+            cursor.close()
+
+        time.sleep(1)
+        self.session.checkpoint()
+        time.sleep(1)
+
+        mcur = session_follow.open_cursor("metadata:")
+        oldmeta = dict()
+        for (k, v) in mcur:
+            self.tty(f'Metadata: {k} => {v}')
+            oldmeta[k] = v
+        mcur.close()
+
+        # Check data in the follower
+        self.disagg_advance_checkpoint(conn_follow)
+        self.tty(f'\n\n*****  ADVANCE CHECKPOiNT TO {self.disagg_get_complete_checkpoint(None)}\n\n')
+        self.tty('')
+        mcur = session_follow.open_cursor("metadata:")
+        newmeta = dict()
+        for (k, v) in mcur:
+            self.tty(f'Metadata: {k} => {v}')
+            newmeta[k] = v
+        mcur.close()
+        for k in oldmeta:
+            if newmeta[k] != oldmeta[k]:
+                self.tty(f' Diff: {k} => {oldmeta[k]}, {newmeta[k]}')
+
+        for uri in self.all_uris:
+            self.pr(f'{uri}: Open a cursor')
+            cursor = session_follow.open_cursor(uri, None, None)
+            follower_cursors[uri] = cursor
+            for i in range(self.nitems):
+                self.assertEquals(cursor[str(i)], value_prefix1 + str(i))
+
+        #
+        # Part 2: Close and reopen the cursor after picking up a checkpoint.
+        #
+
+        # Put data to all tables, version 2
+        value_prefix2 = 'bbb'
+        for uri in self.all_uris:
+            cursor = self.session.open_cursor(uri, None, None)
+            for i in range(self.nitems):
+                cursor[str(i)] = value_prefix2 + str(i)
+                if i % 250 == 0:
+                    time.sleep(1)
+            cursor.close()
+
+        time.sleep(1)
+        self.session.checkpoint()
+        time.sleep(1)
+
+        # Check data in the follower
+        self.tty(f'\n\n*****  ADVANCE CHECKPOiNT TO {self.disagg_get_complete_checkpoint(None)}\n\n')
+        self.tty('')
+        self.disagg_advance_checkpoint(conn_follow)
+        for uri in self.all_uris:
+            self.pr(f'{uri}: Close and reopen the cursor')
+            if uri in follower_cursors:
+                follower_cursors[uri].close()
+            cursor = session_follow.open_cursor(uri, None, None)
+            follower_cursors[uri] = cursor
+            for i in range(self.nitems):
+                self.assertEquals(cursor[str(i)], value_prefix2 + str(i))
+
+        #
+        # Part 3: Reset the cursor after picking up a checkpoint.
+        #
+
+        # Put data to all tables, version 3
+        value_prefix3 = 'ccc'
+        for uri in self.all_uris:
+            cursor = self.session.open_cursor(uri, None, None)
+            for i in range(self.nitems):
+                cursor[str(i)] = value_prefix3 + str(i)
+                if i % 250 == 0:
+                    time.sleep(1)
+            cursor.close()
+
+        time.sleep(1)
+        self.session.checkpoint()
+        time.sleep(1)
+
+        # Check data in the follower
+        self.disagg_advance_checkpoint(conn_follow)
+        for uri in self.all_uris:
+            self.pr(f'{uri}: Reset and reuse the cursor')
+            cursor = follower_cursors[uri]
+
+            #####
+            #cursor.close()
+            #cursor = self.session.open_cursor(uri, None, None)
+            #follower_cursors[uri] = cursor
+            #####
+
+            cursor.reset()
+            for i in range(self.nitems):
+                self.assertEquals(cursor[str(i)], value_prefix3 + str(i))
+
+        # Clean up
+        for cursor in follower_cursors.values():
+            cursor.close()
+
+        session_follow.close()
+        conn_follow.close()
