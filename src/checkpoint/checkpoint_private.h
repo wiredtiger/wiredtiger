b995372a84980fc81c14afd5d345f5bb77762941
Etienne Petrel <etienne.petrel@mongodb.com>
2025-01-14 11:26:38 +1100

    Move WT_CHECKPOINT_STATE checkpoint_private.h

--- src/checkpoint/checkpoint_private.h
+++ src/checkpoint/checkpoint_private.h
@@ -12,6 +12,33 @@
 #define WTI_CKPT_FOREACH_NAME_OR_ORDER(ckptbase, ckpt) \
     for ((ckpt) = (ckptbase); (ckpt)->name != NULL || (ckpt)->order != 0; ++(ckpt))
 
+/*
+ * Inactive should always be 0. Other states are roughly ordered by appearance in the checkpoint
+ * life cycle.
+ */
+typedef enum {
+    WT_CHECKPOINT_STATE_INACTIVE,
+    WT_CHECKPOINT_STATE_APPLY_META,
+    WT_CHECKPOINT_STATE_APPLY_BTREE,
+    WT_CHECKPOINT_STATE_UPDATE_OLDEST,
+    WT_CHECKPOINT_STATE_SYNC_FILE,
+    WT_CHECKPOINT_STATE_EVICT_FILE,
+    WT_CHECKPOINT_STATE_BM_SYNC,
+    WT_CHECKPOINT_STATE_RESOLVE,
+    WT_CHECKPOINT_STATE_POSTPROCESS,
+    WT_CHECKPOINT_STATE_HS,
+    WT_CHECKPOINT_STATE_HS_SYNC,
+    WT_CHECKPOINT_STATE_COMMIT,
+    WT_CHECKPOINT_STATE_META_CKPT,
+    WT_CHECKPOINT_STATE_META_SYNC,
+    WT_CHECKPOINT_STATE_ROLLBACK,
+    WT_CHECKPOINT_STATE_LOG,
+    WT_CHECKPOINT_STATE_CKPT_TREE,
+    WT_CHECKPOINT_STATE_ACTIVE,
+    WT_CHECKPOINT_STATE_ESTABLISH,
+    WT_CHECKPOINT_STATE_START_TXN
+} WT_CHECKPOINT_STATE;
+
 /* DO NOT EDIT: automatically built by prototypes.py: BEGIN */
 
 /* DO NOT EDIT: automatically built by prototypes.py: END */
