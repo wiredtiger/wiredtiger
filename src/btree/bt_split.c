7a5350bdacb989d740f93427509a82730712f87d
Monica Ng <30430618+mm-ng@users.noreply.github.com>
2025-01-16 09:01:38 +1100

    SLS-894 Only discard a ref when its change counter is 0 (#11425)

--- src/btree/bt_split.c
+++ src/btree/bt_split.c
@@ -699,7 +699,8 @@ __split_parent(WT_SESSION_IMPL *session, WT_REF *ref, WT_REF **ref_new, uint32_t
              * which seems like asking for trouble.) Don't discard any ref has the prefetch flag,
              * the prefetch thread would crash if it sees a freed ref.
              */
-            if (next_ref != ref && WT_REF_GET_STATE(next_ref) == WT_REF_DELETED &&
+            if (next_ref->ref_changes == 0 && next_ref != ref &&
+              WT_REF_GET_STATE(next_ref) == WT_REF_DELETED &&
               (btree->type != BTREE_COL_VAR || i != 0) &&
               !F_ISSET_ATOMIC_8(next_ref, WT_REF_FLAG_PREFETCH) &&
               __wti_delete_page_skip(session, next_ref, true) &&
