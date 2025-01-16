1804cf0cb6e355039edad336bf88aa8ee3068aa4
Dylan Liang <dylanthekid350@gmail.com>
2025-01-16 06:43:32 +0000

    implemented new config option for timing_stress_for_test and added python test

--- src/schema/schema_alter.c
+++ src/schema/schema_alter.c
@@ -437,12 +437,18 @@ __schema_alter(WT_SESSION_IMPL *session, const char *uri, const char *newcfg[])
 int
 __wt_schema_alter(WT_SESSION_IMPL *session, const char *uri, const char *newcfg[])
 {
+    struct timespec tsp;
     WT_DECL_RET;
     WT_SESSION_IMPL *int_session;
 
     WT_ASSERT_SPINLOCK_OWNED(session, &S2C(session)->checkpoint_lock);
     WT_ASSERT_SPINLOCK_OWNED(session, &S2C(session)->schema_lock);
 
+    /* Add a 2 second wait to simulate session alter slowness. */
+    tsp.tv_sec = 2;
+    tsp.tv_nsec = 0;
+    printf("stressing\n");
+    __wt_timing_stress(session, WT_TIMING_STRESS_SESSION_ALTER_SLOW, &tsp);
     WT_RET(__wti_schema_internal_session(session, &int_session));
     WT_ERR(__wt_meta_track_on(int_session));
     ret = __schema_alter(int_session, uri, newcfg);
---
+++ test/suite/test_timing_stress_for_test01.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+#
+# Public Domain 2014-present MongoDB, Inc.
+# Public Domain 2008-2014 WiredTiger, Inc.
+#
+# This is free and unencumbered software released into the public domain.
+#
+# Anyone is free to copy, modify, publish, use, compile, sell, or
+# distribute this software, either in source code form or as a compiled
+# binary, for any purpose, commercial or non-commercial, and by any
+# means.
+#
+# In jurisdictions that recognize copyright laws, the author or authors
+# of this software dedicate any and all copyright interest in the
+# software to the public domain. We make this dedication for the benefit
+# of the public at large and to the detriment of our heirs and
+# successors. We intend this dedication to be an overt act of
+# relinquishment in perpetuity of all present and future rights to this
+# software under copyright law.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+#
+# [TEST_TAGS]
+# connection_api
+# [END_TAGS]
+
+import errno
+import time
+import wttest
+import wiredtiger
+from threading import Thread
+
+# test_timing_stress_for_test01.py
+#   Test that the config for timing_stress_for_test delays the relase of the schema lock.
+class test_timing_stress_for_test01(wttest.WiredTigerTestCase):
+    uri = 'table:timing_stress_for_test'
+    conn_config = 'timing_stress_for_test=[session_alter_slow]'
+    expected_delay = 2
+
+    def alter_table(self):
+        # Alter the table with any configuration.
+        self.session.alter(self.uri, 'access_pattern_hint=random')
+
+    def drop_table(self):
+        start_time = time.time()
+        # Start a second session.
+        session2 = self.conn.open_session()
+        # Drop the table.
+        self.assertEqual(session2.drop(self.uri, None), 0)
+        end_time = time.time()
+
+        # Expect delay to be 2 seconds.
+        self.assertGreaterEqual(end_time - start_time, self.expected_delay)
+
+    def test_release_schema_lock_slow_no_wait(self):
+        # Create a simple table in the first session.
+        session1 = self.session
+        session1.create(self.uri, 'key_format=S,value_format=S')
+
+        # Create two threads to alter the table and drop the table.
+        thread1 = Thread(target = self.alter_table)
+        thread2 = Thread(target = self.drop_table)
+
+        thread1.start()
+        thread2.start()
+
+        thread1.join()
+        thread2.join()
+
+
+
