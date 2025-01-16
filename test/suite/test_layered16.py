0499171b79b09cc341a4219af169072176cf632b
Chenhao Qu <chenhao.qu@mongodb.com>
2025-01-16 04:19:37 +0000

    Fix test_layered16

--- test/suite/test_layered16.py
+++ test/suite/test_layered16.py
@@ -43,6 +43,10 @@ class test_layered16(wttest.WiredTigerTestCase, DisaggConfigMixin):
 
     num_restarts = 0
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.ignoreStdoutPattern('WT_VERB_RTS')
+
     # Load the page log extension, which has object storage support
     def conn_extensions(self, extlist):
         if os.name == 'nt':
