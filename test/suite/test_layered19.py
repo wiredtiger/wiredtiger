de8cf868e8a0d56096db54ec64f7a7b3b15314e9
Don Anderson <donald.anderson@mongodb.com>
2025-01-17 21:15:58 +0000

    Fix test case after merge.

--- test/suite/test_layered19.py
+++ test/suite/test_layered19.py
@@ -27,7 +27,7 @@
 # OTHER DEALINGS IN THE SOFTWARE.
 
 import os, time, wiredtiger, wttest
-from helper_disagg import DisaggConfigMixin, gen_disagg_storages
+from helper_disagg import DisaggConfigMixin, disagg_test_class, gen_disagg_storages
 from wtscenario import make_scenarios
 
 # We support using the reset operation to pick up the latest checkpoint for layered: URIs.
@@ -38,10 +38,11 @@ test_milestone4 = True
 
 # test_layered19.py
 #    Extra tests for follower picking up new checkpoints.
+@disagg_test_class
 class test_layered19(wttest.WiredTigerTestCase, DisaggConfigMixin):
     nitems = 500
 
-    conn_base_config = 'layered_table_log=(enabled),statistics=(all),statistics_log=(wait=1,json=true,on_close=true),' \
+    conn_base_config = 'statistics=(all),statistics_log=(wait=1,json=true,on_close=true),' \
                      + 'disaggregated=(stable_prefix=.,page_log=palm),'
     conn_config = conn_base_config + 'disaggregated=(role="leader")'
 
@@ -102,7 +103,7 @@ class test_layered19(wttest.WiredTigerTestCase, DisaggConfigMixin):
         for uri in self.all_uris:
             cfg = self.create_session_config
             if not uri.startswith('layered'):
-                cfg += ',block_manager=disagg,layered_table_log=(enabled=false),log=(enabled=false)'
+                cfg += ',block_manager=disagg,log=(enabled=false)'
             self.session.create(uri, cfg)
 
         # Create the follower
