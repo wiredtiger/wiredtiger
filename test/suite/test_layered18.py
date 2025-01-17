1c517b89cbeb01a5e87c37eecf0fbe6fdf6773b9
Peter Macko <peter.macko@mongodb.com>
2025-01-17 11:42:24 -0500

    Removed the unnecessary init

--- test/suite/test_layered18.py
+++ test/suite/test_layered18.py
@@ -53,10 +53,6 @@ class test_layered18(wttest.WiredTigerTestCase, DisaggConfigMixin):
         ('shared', dict(prefix='table:')),
     ])
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.ignoreStdoutPattern('WT_VERB_RTS')
-
     # Load the page log extension, which has object storage support
     def conn_extensions(self, extlist):
         if os.name == 'nt':
