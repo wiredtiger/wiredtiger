8054042745b839be8f98e455dc239f5cb34edd5d
Will Korteland <will.korteland@mongodb.com>
2025-01-30 03:51:54 +0000

    forgot to add test

---
+++ test/suite/test_layered23.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python3
+#
+# Public Domain 2014-present MongoDB, Inc.
+# Public Domain 2008-2014 WiredTiger, Inc.
+#
+# This is free and unencumbered software released into the public domain.
+#
+# Anyone is free to copy, modify, publish, use, compile, sell, or
+# distribute this software, either in source code form or as a compiled
+# binary, for any purpose, commercial or non-commercial, and by any
+# means.
+#
+# In jurisdictions that recognize copyright laws, the author or authors
+# of this software dedicate any and all copyright interest in the
+# software to the public domain. We make this dedication for the benefit
+# of the public at large and to the detriment of our heirs and
+# successors. We intend this dedication to be an overt act of
+# relinquishment in perpetuity of all present and future rights to this
+# software under copyright law.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+
+import os
+import wttest
+from helper_disagg import DisaggConfigMixin, disagg_test_class
+
+# test_layered23.py
+#    Ensure a secondary that drops a table does not fall back to reading
+#    the stable table.
+@disagg_test_class
+class test_layered23(wttest.WiredTigerTestCase, DisaggConfigMixin):
+    uri = "layered:test_layered23"
+
+    conn_base_config = "disaggregated=(stable_prefix=.,page_log=palm),"
+    conn_config = conn_base_config + 'disaggregated=(role="leader")'
+
+    nitems = 10000
+
+    def conn_extensions(self, extlist):
+        if os.name == 'nt':
+            extlist.skip_if_missing = True
+        extlist.extension('page_log', 'palm')
+
+    def early_setup(self):
+        os.mkdir("follower")
+        # Create the home directory for the PALM k/v store, and share it with the follower.
+        os.mkdir('kv_home')
+        os.symlink('../kv_home', 'follower/kv_home', target_is_directory=True)
+
+    def test_layered23(self):
+        session_config = 'key_format=S,value_format=S'
+
+        #
+        # Part 1: Create a layered table and a follower that also has that data.
+        #
+        self.session.create(self.uri, session_config)
+        # TODO figure out self.extensionsConfig()
+        conn_follow = self.wiredtiger_open('follower', self.extensionsConfig() + ',create,' + self.conn_base_config + 'disaggregated=(role="follower")')
+        session_follow = conn_follow.open_session('')
+        session_follow.create(self.uri, session_config)
+
+        cursor = self.session.open_cursor(self.uri, None, None)
+        for i in range(self.nitems):
+            cursor["Hello " + str(i)] = "World"
+            cursor["Hi " + str(i)] = "There"
+            cursor["OK " + str(i)] = "Go"
+        cursor.close()
+
+        self.session.checkpoint()
+        self.disagg_advance_checkpoint(conn_follow)
+
+        # Sanity-check all data made it to the follower.
+        cursor_follow2 = session_follow.open_cursor(self.uri, None, None)
+        item_count = 0
+        while cursor_follow2.next() == 0:
+            item_count += 1
+        self.assertEqual(item_count, self.nitems * 3)
+        cursor_follow2.close()
+
+        #
+        # Part 2: drop the table on the secondary and check it has no data.
+        #
+        session_follow.drop(self.uri)
+
+        # TODO rest of test
