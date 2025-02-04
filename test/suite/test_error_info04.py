e9d00b536651f7e6620a917e1e2c8b0b064d44f5
Dylan Liang <dylanthekid350@gmail.com>
2025-02-04 03:35:03 +0000

    removed unused import

--- test/suite/test_error_info04.py
+++ test/suite/test_error_info04.py
@@ -26,7 +26,7 @@
 # ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 # OTHER DEALINGS IN THE SOFTWARE.
 
-import wiredtiger, time
+import wiredtiger
 from error_info_util import error_info_util
 
 # test_error_info04.py
