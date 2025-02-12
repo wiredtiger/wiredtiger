d54cb8f90fb3602494d0fd947bae0db666cb4648
Chenhao Qu <chenhao.qu@mongodb.com>
2025-02-12 02:38:07 +0000

    clang analyzer

--- src/cursor/cur_layered.c
+++ src/cursor/cur_layered.c
@@ -240,7 +240,6 @@ __clayered_open_cursors(WT_CURSOR_LAYERED *clayered, bool update)
     session = CUR2S(clayered);
     conn = S2C(session);
     layered = (WT_LAYERED_TABLE *)session->dhandle;
-    checkpoint_timestamp = WT_TS_NONE;
     ingest_btree = NULL;
 
     WT_ASSERT_SPINLOCK_OWNED(session, &conn->schema_lock);
