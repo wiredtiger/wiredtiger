d24d8619fec547096f7b37ba66060ab2504e58e8
luke-pearson <luke.pearson@mongodb.com>
2025-02-14 18:04:15 +0000

    WT-14080 Add the required metadata config

--- dist/api_data.py
+++ dist/api_data.py
@@ -337,6 +337,10 @@ file_meta = file_config + [
         LSN of the last checkpoint'''),
     Config('id', '', r'''
         the file's ID number'''),
+    Config('live_restore', '', r'''
+        live restore metadata for a file''', type='category', subconfig=[
+            Config('bitmap', '', r'''bitmap representation of a file'''),
+            Config('bitmap_size', 0, r'''the size of the bitmap as an integer''', type='int')]),
     Config('readonly', 'false', r'''
         the file is read-only. All methods that modify a file are disabled. See @ref
         readonly for more information''',
