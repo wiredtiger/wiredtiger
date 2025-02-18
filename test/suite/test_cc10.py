c8b43bde8557f00b7e6341c845a2c1c1ae50ec49
Etienne Petrel <etienne.petrel@mongodb.com>
2025-02-19 06:55:17 +1100

    Rework test

--- test/suite/test_cc10.py
+++ test/suite/test_cc10.py
@@ -28,55 +28,63 @@
 
 import time
 from test_cc01 import test_cc_base
-from wtdataset import SimpleDataSet
+from wiredtiger import stat
 from wtscenario import make_scenarios
 
 # test_cc10.py
-# Test that checkpoint cleans the obsolete history store pages.
+# Test that the obsolete cleanup thread can be configured to run at different intervals.
 class test_cc10(test_cc_base):
-    # Force a small cache.
-    conn_config_common = 'cache_size=50MB,eviction_updates_trigger=95,eviction_updates_target=80,statistics=(all)'
-
     waiting_time = [
-        ('1sec', dict(conn_config=f'{conn_config_common},checkpoint_cleanup=[wait=1]')),
-        ('2sec', dict(conn_config=f'{conn_config_common},checkpoint_cleanup=[wait=2]')),
-        ('3sec', dict(conn_config=f'{conn_config_common},checkpoint_cleanup=[wait=3]')),
+        ('1sec', dict(conn_config=f'checkpoint_cleanup=[wait=1]')),
+        ('2sec', dict(conn_config=f'checkpoint_cleanup=[wait=2]')),
+        ('3sec', dict(conn_config=f'checkpoint_cleanup=[wait=3]')),
     ]
     scenarios = make_scenarios(waiting_time)
 
     def test_cc10(self):
-        nrows = 10000
+        # Create a table.
+        create_params = 'key_format=i,value_format=S'
+        nrows = 1000
+        uri = "table:cc02"
 
-        # Create a table without logging.
-        uri = "table:cc10"
-        ds = SimpleDataSet(self, uri, 0, key_format="i", value_format="S")
-        ds.populate()
+        # Create and populate a table.
+        self.session.create(uri, create_params)
+
+        old_value = "a"
+        old_ts = 1
+        self.populate(uri, 0, nrows, old_value, old_ts)
 
         # Pin oldest and stable to timestamp 1.
-        self.conn.set_timestamp('oldest_timestamp=' + self.timestamp_str(1) +
-            ',stable_timestamp=' + self.timestamp_str(1))
+        self.conn.set_timestamp(f'oldest_timestamp={self.timestamp_str(old_ts)},stable_timestamp={self.timestamp_str(old_ts)}')
 
-        bigvalue = "aaaaa" * 100
-        bigvalue2 = "ddddd" * 100
-        self.large_updates(uri, bigvalue, ds, nrows, 10)
+        # Update each record with a newer timestamp.
+        new_value = "b"
+        new_ts = 10
+        self.populate(uri, 0, nrows, new_value, new_ts)
 
-        # Check that all updates are seen.
-        self.check(bigvalue, uri, nrows, 10)
+        # Make the updates stable and checkpoint to write everything in the DS and HS.
+        # The most recent updates with the ts 10 should be in the DS while the ones with ts 1 should
+        # be in the HS.
+        self.conn.set_timestamp(f'stable_timestamp={new_ts}')
+        self.session.checkpoint()
 
-        self.large_updates(uri, bigvalue2, ds, nrows, 100)
-
-        # Check that the new updates are only seen after the update timestamp.
-        self.check(bigvalue2, uri, nrows, 100)
-
-        # Check that old updates are seen.
-        self.check(bigvalue, uri, nrows, 10)
-
-        # Pin oldest and stable to timestamp 100.
-        self.conn.set_timestamp('oldest_timestamp=' + self.timestamp_str(100) +
-            ',stable_timestamp=' + self.timestamp_str(100))
+        # Make the updates in the HS obsolete.
+        self.conn.set_timestamp(f'oldest_timestamp={new_ts}')
 
         # Wait for obsolete cleanup to occur, this should clean the history store.
         # We don't use the debug option to force cleanup as the thread should be running in the
-        # background. Wait some time to make sure thread could run.
+        # background.
         time.sleep(5)
         self.check_cc_stats(force_cleanup=False)
+
+        c = self.session.open_cursor('statistics:')
+        visited = c[stat.conn.checkpoint_cleanup_pages_visited][2]
+        obsolete_evicted = c[stat.conn.checkpoint_cleanup_pages_evict][2]
+        obsolete_on_disk = c[stat.conn.checkpoint_cleanup_pages_removed][2]
+        c.close()
+
+        # We should always visit pages for cleanup.
+        self.assertGreater(visited, 0)
+
+        # We should have performed some cleanup.
+        self.assertGreater(obsolete_evicted + obsolete_on_disk, 0)
