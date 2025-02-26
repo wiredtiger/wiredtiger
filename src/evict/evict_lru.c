0292fca57d95f8386af1670499ab152f2028c017
Zunyi Liu <43444919+zunyiliu@users.noreply.github.com>
2025-02-26 20:04:08 +1100

    WT-13283 Cache aggressive mode shows garbage value on 6.0 FTDC (#11622)
    We currently do check before decrementing `evict_aggressive_score` to
    ensure it won't drop below zero. However the check then decrement
    operations are not implemented atomically which causes race condition.
    This PR is to apply the compare-and-swap operation to the code to avoid
    the value drops to -1(which is `int_max` as `evict_aggressive_score` is
    an unsigned int).
    
    (cherry picked from commit 4a0a6109f05468cdfb1a7964e98bcfb4c351aa88)

--- src/evict/evict_lru.c
+++ src/evict/evict_lru.c
@@ -828,8 +828,7 @@ __evict_pass(WT_SESSION_IMPL *session)
             __wt_verbose(session, WT_VERB_EVICTSERVER, "%s", "unable to reach eviction goal");
             break;
         }
-        if (__wt_atomic_load32(&cache->evict_aggressive_score) > 0)
-            (void)__wt_atomic_subv32(&cache->evict_aggressive_score, 1);
+        __wt_atomic_decrement_if_positive(&cache->evict_aggressive_score);
         loop = 0;
         eviction_progress = cache->eviction_progress;
     }
@@ -2524,8 +2523,8 @@ __wt_cache_eviction_worker(WT_SESSION_IMPL *session, bool busy, bool readonly, d
         if (!F_ISSET(conn, WT_CONN_RECOVERING) && __wt_cache_stuck(session)) {
             ret = __wt_txn_is_blocking(session);
             if (ret == WT_ROLLBACK) {
-                if (__wt_atomic_load32(&cache->evict_aggressive_score) > 0)
-                    (void)__wt_atomic_subv32(&cache->evict_aggressive_score, 1);
+                __wt_atomic_decrement_if_positive(&cache->evict_aggressive_score);
+
                 WT_STAT_CONN_INCR(session, txn_rollback_oldest_pinned);
                 __wt_verbose_debug1(session, WT_VERB_TRANSACTION, "rollback reason: %s",
                   session->txn->rollback_reason);
@@ -2600,8 +2599,7 @@ err:
          */
         if (ret == 0 && cache_max_wait_us != 0 && session->cache_wait_us > cache_max_wait_us) {
             ret = __wt_txn_rollback_required(session, WT_TXN_ROLLBACK_REASON_CACHE_OVERFLOW);
-            if (__wt_atomic_load32(&cache->evict_aggressive_score) > 0)
-                (void)__wt_atomic_subv32(&cache->evict_aggressive_score, 1);
+            __wt_atomic_decrement_if_positive(&cache->evict_aggressive_score);
             WT_STAT_CONN_INCR(session, cache_timed_out_ops);
             __wt_verbose_notice(
               session, WT_VERB_TRANSACTION, "rollback reason: %s", session->txn->rollback_reason);
