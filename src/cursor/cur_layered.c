0125981153457eae55f7d31fb0b6a6e30b017be0
Don Anderson <donald.anderson@mongodb.com>
2025-02-27 15:32:07 +0000

    SLS-873 Update layered cursor on follower when checkpoint picked up

--- src/cursor/cur_layered.c
+++ src/cursor/cur_layered.c
@@ -12,7 +12,8 @@ static int __clayered_copy_bounds(WT_CURSOR_LAYERED *);
 static int __clayered_lookup(WT_CURSOR_LAYERED *, WT_ITEM *);
 static int __clayered_open_cursors(WT_CURSOR_LAYERED *, bool);
 static int __clayered_reset_cursors(WT_CURSOR_LAYERED *, bool);
-static int __clayered_search_near(WT_CURSOR *cursor, int *exactp);
+static int __clayered_search_near(WT_CURSOR *, int *);
+static int __clayered_update_state(WT_CURSOR_LAYERED *, bool *);
 
 /*
  * We need a tombstone to mark deleted records, and we use the special value below for that purpose.
@@ -115,6 +116,7 @@ __clayered_enter(WT_CURSOR_LAYERED *clayered, bool reset, bool update)
 {
     WT_DECL_RET;
     WT_SESSION_IMPL *session;
+    bool external_state_change;
 
     session = CUR2S(clayered);
 
@@ -123,17 +125,28 @@ __clayered_enter(WT_CURSOR_LAYERED *clayered, bool reset, bool update)
         WT_RET(__clayered_reset_cursors(clayered, false));
     }
 
+    WT_RET(__clayered_update_state(clayered, &external_state_change));
+
     for (;;) {
         /*
-         * Stop when we are up-to-date, as long as this is:
+         * Continue if the state of the world just got updated.
+         * Otherwise stop when we are ptherwise up-to-date, as long as this is:
          *   - an update operation with a ingest cursor, or
          *   - a read operation and the cursor is open for reading.
          */
-        if ((update && clayered->ingest_cursor != NULL) ||
-          (!update && F_ISSET(clayered, WT_CLAYERED_OPEN_READ)))
+        if (!external_state_change &&
+            ((update && clayered->ingest_cursor != NULL) ||
+              (!update && F_ISSET(clayered, WT_CLAYERED_OPEN_READ))))
             break;
 
         WT_WITH_SCHEMA_LOCK(session, ret = __clayered_open_cursors(clayered, update));
+
+        /*
+         * We only check the external state once. There will always be a race where
+         * the state changes after we check and when we start using the cursor again.
+         * Checking multiple times may narrow the race a bit, but adds expense.
+         */
+        external_state_change = false;
         WT_RET(ret);
     }
 
@@ -173,12 +186,12 @@ __clayered_leave(WT_CURSOR_LAYERED *clayered)
  *     Close any btree cursors that are not needed.
  */
 static int
-__clayered_close_cursors(WT_CURSOR_LAYERED *clayered)
+__clayered_close_cursors(WT_CURSOR_LAYERED *clayered, bool include_ingest)
 {
     WT_CURSOR *c;
 
     clayered->current_cursor = NULL;
-    if ((c = clayered->ingest_cursor) != NULL) {
+    if (include_ingest && (c = clayered->ingest_cursor) != NULL) {
         WT_RET(c->close(c));
         clayered->ingest_cursor = NULL;
     }
@@ -188,6 +201,41 @@ __clayered_close_cursors(WT_CURSOR_LAYERED *clayered)
     }
 
     clayered->flags = 0;
+    return (0);
+}
+
+/*
+ * __clayered_update_state --
+ *     Return true if the state of the system has changed relative
+ * to this cursor's state.
+ */
+static int
+__clayered_update_state(WT_CURSOR_LAYERED *clayered, bool *state_updated)
+{
+    WT_CONNECTION_IMPL *conn;
+    uint64_t current_checkpoint_id;
+    bool current_leader;
+
+    conn = S2C(CUR2S(clayered));
+    current_leader = conn->layered_table_manager.leader;
+    *state_updated = false;
+
+    if (!current_leader)
+        WT_ACQUIRE_READ(current_checkpoint_id, conn->disaggregated_storage.global_checkpoint_id);
+    else
+        current_checkpoint_id = 0;
+
+    if (current_leader != clayered->leader) {
+        WT_RET(__clayered_close_cursors(clayered, true));
+        clayered->leader = current_leader;
+        clayered->checkpoint_id = current_checkpoint_id;
+        *state_updated = true;
+    } else if (!current_leader && current_checkpoint_id != clayered->checkpoint_id) {
+        /* Only close the stable cursor. */
+        WT_RET(__clayered_close_cursors(clayered, false));
+        clayered->checkpoint_id = current_checkpoint_id;
+        *state_updated = true;
+    }
     return (0);
 }
 
@@ -1472,7 +1520,7 @@ __clayered_close_int(WT_CURSOR *cursor)
      * cursors in the session. It might be better to keep them out of the session cursor list, but I
      * don't know how to do that? Probably opening a file cursor directly instead of a table cursor?
      */
-    WT_TRET(__clayered_close_cursors(clayered));
+    WT_TRET(__clayered_close_cursors(clayered, true));
 
     /* In case we were somehow left positioned, clear that. */
     __clayered_leave(clayered);
---
+++ test/suite/test_layered31.py
@@ -0,0 +1,205 @@
+#!/usr/bin/env python3
+#
+# Public Domain 2014-present MongoDB, Inc.
+# Public Domain 2008-2014 WiredTiger, Inc.
+#
+# This is free and unencumbered software released into the public domain.
+#
+# Anyone is free to copy, modify, publish, use, compile, sell, or
+# distribute this software, either in source code form or as a compiled
+# binary, for any purpose, commercial or non-commercial, and by any
+# means.
+#
+# In jurisdictions that recognize copyright laws, the author or authors
+# of this software dedicate any and all copyright interest in the
+# software to the public domain. We make this dedication for the benefit
+# of the public at large and to the detriment of our heirs and
+# successors. We intend this dedication to be an overt act of
+# relinquishment in perpetuity of all present and future rights to this
+# software under copyright law.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+
+import os, time, wiredtiger, wttest
+from helper_disagg import DisaggConfigMixin, disagg_test_class, gen_disagg_storages
+from wtscenario import make_scenarios
+
+# We support using the reset operation to pick up the latest checkpoint for layered: URIs.
+# In milestone 4, however, layered URIs are not used, and the way we pick up the latest checkpoint
+# is a bit hacky.  We still want to verify that this technique works, at least for a while.
+# The milestone 4 specific code is marked by this variable:
+test_milestone4 = True
+
+# test_layered31.py
+#    Extra tests for follower picking up new checkpoints.
+@disagg_test_class
+class test_layered31(wttest.WiredTigerTestCase, DisaggConfigMixin):
+    nitems = 500
+
+    conn_base_config = 'statistics=(all),statistics_log=(wait=1,json=true,on_close=true),' \
+                     + 'disaggregated=(page_log=palm),'
+    conn_config = conn_base_config + 'disaggregated=(role="leader")'
+
+    create_session_config = 'key_format=S,value_format=S'
+
+    layered_uris = ["layered:test_layered31a", "layered:test_layered31b"]
+    all_uris = layered_uris
+    if test_milestone4:
+        other_uris = ["file:test_layered31c", "table:test_layered31d"]
+        all_uris += other_uris
+
+    disagg_storages = gen_disagg_storages('test_layered31', disagg_only = True)
+    scenarios = make_scenarios(disagg_storages)
+
+    # Load the page log extension, which has object storage support
+    def conn_extensions(self, extlist):
+        if os.name == 'nt':
+            extlist.skip_if_missing = True
+        DisaggConfigMixin.conn_extensions(self, extlist)
+
+    # Open a cursor on the follower.  Generally, the test will open a layered: uri.
+    # But we want to temporarily support a world in M4 where we must directly open the
+    # stable table, using a file: or table: . This is needed because in this world we don't
+    # pre-create the ingest tables needed to use a layered: uri.
+    def open_follow_cursor(self, session, uri):
+        config = None
+        if test_milestone4:
+            if uri.startswith('table:') or uri.startswith('file:'):
+                #self.tty(f'\n\n**** OPENING FOLLOW CURSOR {uri} with force=true ****\n')
+                config = 'force=true'
+        return session.open_cursor(uri, None, config)
+
+    # Reset a cursor on the follower.  Generally, the test will open a layered: uri,
+    # and a reset is a signal have the cursor move to the next checkpoint. This works
+    # for layered cursors but not cursors in general.  In the m4 milestone where we don't
+    # use a layered cursor, to get similar behavior, we need to reopen the cursor.
+    def reset_follow_cursor(self, cursor):
+        if test_milestone4:
+            uri = cursor.uri
+            if uri.startswith('table:') or uri.startswith('file:'):
+                #self.tty(f'\n\n**** INSTEAD OF RESET, REOPEN FOLLOW CURSOR {uri} with force=true ****\n')
+                session = cursor.session
+                cursor = session.open_cursor(uri, None, 'force=true')
+                return cursor
+        cursor.reset()
+        return cursor
+
+    # Test more than one table.
+    def test_layered31(self):
+        # Create all tables in the leader
+        for uri in self.all_uris:
+            cfg = self.create_session_config
+            if not uri.startswith('layered'):
+                cfg += ',block_manager=disagg,log=(enabled=false)'
+            self.session.create(uri, cfg)
+
+        # Create the follower
+        conn_follow = self.wiredtiger_open('follower', self.extensionsConfig() + ',create,' + self.conn_base_config + 'disaggregated=(role="follower")')
+        session_follow = conn_follow.open_session('')
+        follower_cursors = dict()
+
+        #
+        # Setup: Check data in the follower normally.
+        #
+
+        # Put data to all tables, version 0
+        value_prefix0 = '---'
+        for uri in self.all_uris:
+            cursor = self.session.open_cursor(uri, None, None)
+            for i in range(self.nitems):
+                cursor[str(i)] = value_prefix0 + str(i)
+            cursor.close()
+
+        self.session.checkpoint()
+
+        # Check data in the follower
+        self.disagg_advance_checkpoint(conn_follow)
+        for uri in self.all_uris:
+            cursor = self.open_follow_cursor(session_follow, uri)
+            for i in range(self.nitems):
+                self.assertEquals(cursor[str(i)], value_prefix0 + str(i))
+            cursor.close()
+
+        #
+        # Part 1: Check data in the follower, but keep the cursors open.
+        #
+
+        # Put data to all tables, version 1
+        value_prefix1 = 'aaa'
+        for uri in self.all_uris:
+            cursor = self.session.open_cursor(uri, None, None)
+            for i in range(self.nitems):
+                cursor[str(i)] = value_prefix1 + str(i)
+            cursor.close()
+
+        self.session.checkpoint()
+
+        # Check data in the follower
+        self.disagg_advance_checkpoint(conn_follow)
+        for uri in self.all_uris:
+            self.pr(f'{uri}: Open a cursor')
+            cursor = self.open_follow_cursor(session_follow, uri)
+            follower_cursors[uri] = cursor
+            for i in range(self.nitems):
+                self.assertEquals(cursor[str(i)], value_prefix1 + str(i))
+
+        #
+        # Part 2: Close and reopen the cursor after picking up a checkpoint.
+        #
+
+        # Put data to all tables, version 2
+        value_prefix2 = 'bbb'
+        for uri in self.all_uris:
+            cursor = self.session.open_cursor(uri, None, None)
+            for i in range(self.nitems):
+                cursor[str(i)] = value_prefix2 + str(i)
+            cursor.close()
+
+        self.session.checkpoint()
+
+        # Check data in the follower
+        self.disagg_advance_checkpoint(conn_follow)
+        for uri in self.all_uris:
+            self.pr(f'{uri}: Close and reopen the cursor')
+            if uri in follower_cursors:
+                follower_cursors[uri].close()
+            cursor = self.open_follow_cursor(session_follow, uri)
+            follower_cursors[uri] = cursor
+            for i in range(self.nitems):
+                self.assertEquals(cursor[str(i)], value_prefix2 + str(i))
+
+        #
+        # Part 3: Reset the cursor after picking up a checkpoint.
+        #
+
+        # Put data to all tables, version 3
+        value_prefix3 = 'ccc'
+        for uri in self.all_uris:
+            cursor = self.session.open_cursor(uri, None, None)
+            for i in range(self.nitems):
+                cursor[str(i)] = value_prefix3 + str(i)
+            cursor.close()
+
+        self.session.checkpoint()
+
+        # Check data in the follower
+        self.disagg_advance_checkpoint(conn_follow)
+        for uri in self.all_uris:
+            self.pr(f'{uri}: Reset and reuse the cursor')
+            cursor = follower_cursors[uri]
+            cursor = self.reset_follow_cursor(cursor)
+            for i in range(self.nitems):
+                self.assertEquals(cursor[str(i)], value_prefix3 + str(i))
+
+        # Clean up
+        for cursor in follower_cursors.values():
+            cursor.close()
+
+        session_follow.close()
+        conn_follow.close()
