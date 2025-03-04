fbf2a3bd4e9693208d01ac6eb540409f81e5e244
Siddhartha8899 <siddhartha.mahajan8899@gmail.com>
2025-03-04 23:06:53 +0000

    Add an assertion in__split_internal

--- src/btree/bt_split.c
+++ src/btree/bt_split.c
@@ -1074,6 +1074,7 @@ __split_internal(WT_SESSION_IMPL *session, WT_PAGE *parent, WT_PAGE *page)
 
     /* The split is complete and verified, ignore benign errors. */
     complete = WT_ERR_IGNORE;
+    WT_ASSERT(session, F_ISSET_ATOMIC_16(page, WT_PAGE_INTL_PINDEX_UPDATE));
 
     /*
      * We don't care about the page-index we allocated, all we needed was the array of WT_REF
