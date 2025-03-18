ce576f167749039c8b80987db57392bff49e3cbb
Peter Macko <peter.macko@mongodb.com>
2025-03-18 14:51:58 -0400

    Test changes.

--- test/suite/test_layered36.py
+++ test/suite/test_layered36.py
@@ -43,9 +43,8 @@ class test_layered36(wttest.WiredTigerTestCase, DisaggConfigMixin):
 
     create_session_config = 'key_format=S,value_format=S'
 
-    table_name_preexisting = "test_layered36a"
-    table_name_new_empty = "test_layered36b"
-    table_name_new_filled = "test_layered36c"
+    table_name_empty = "test_layered36a"
+    table_name_filled = "test_layered36b"
 
     disagg_storages = gen_disagg_storages('test_layered36', disagg_only = True)
     scenarios = make_scenarios(disagg_storages, [
@@ -93,24 +92,25 @@ class test_layered36(wttest.WiredTigerTestCase, DisaggConfigMixin):
     def test_layered36(self):
 
         # Create an empty table.
-        uri_new_empty = self.prefix + self.table_name_new_empty
+        uri_empty = self.prefix + self.table_name_empty
         config = self.create_session_config + ',' + self.table_config
-        self.session.create(uri_new_empty, config)
+        self.session.create(uri_empty, config)
 
         # Create a table with some data.
-        uri_new_filled = self.prefix + self.table_name_new_filled
+        uri_filled = self.prefix + self.table_name_filled
         config = self.create_session_config + ',' + self.table_config
-        self.session.create(uri_new_filled, config)
+        self.session.create(uri_filled, config)
 
-        # FIXME-SLS-760
+        # FIXME-SLS-760: This test triggers SLS-760.
         if False:
             self.session.begin_transaction() # Draining requires timestamps
-            cursor = self.session.open_cursor(uri_new_filled, None, None)
+            cursor = self.session.open_cursor(uri_filled, None, None)
             cursor['a'] = 'b'
             cursor.close()
             self.session.commit_transaction('commit_timestamp=' + self.timestamp_str(10))
 
         # Step up and checkpoint.
+        self.conn.set_timestamp('stable_timestamp=' + self.timestamp_str(10))
         self.conn.reconfigure('disaggregated=(role="leader")')
         self.session.checkpoint()
 
@@ -120,110 +120,15 @@ class test_layered36(wttest.WiredTigerTestCase, DisaggConfigMixin):
         self.conn.reconfigure(f'disaggregated=(checkpoint_meta="{checkpoint_meta}")')
 
         # Check the tables
-        cursor = self.session.open_cursor(uri_new_empty, None, None)
+        cursor = self.session.open_cursor(uri_empty, None, None)
         item_count = 0
         while cursor.next() == 0:
             item_count += 1
         cursor.close()
         self.assertEqual(item_count, 0)
 
-        cursor = self.session.open_cursor(uri_new_filled, None, None)
-        # FIXME-SLS-760
+        cursor = self.session.open_cursor(uri_filled, None, None)
+        # FIXME-SLS-760: This test triggers SLS-760.
         if False:
             self.assertEqual(cursor['a'], 'b')
         cursor.close()
-
-    # A more complicated test with preexisting tables and switching node roles.
-    def test_layered36_ext(self):
-        # FIXME-SLS-760 This test triggers SLS-760.
-        self.skipTest('Skipped due to SLS-760.')
-
-        # The node started as a follower, so step it up as the leader
-        self.conn.reconfigure('disaggregated=(role="leader")')
-
-        # Create the first table, with some data
-        uri_preexisting = self.prefix + self.table_name_preexisting
-        config = self.create_session_config + ',' + self.table_config
-        self.session.create(uri_preexisting, config)
-        cursor = self.session.open_cursor(uri_preexisting, None, None)
-        cursor['a'] = 'b'
-        cursor.close()
-
-        # Create a checkpoint
-        self.session.checkpoint()
-
-        # Start the follower
-        conn_follow = self.wiredtiger_open('follower', self.extensionsConfig() + ',create,' + self.conn_config)
-        self.disagg_advance_checkpoint(conn_follow)
-        session_follow = conn_follow.open_session('')
-
-        # Check the new table in the follower
-        cursor = session_follow.open_cursor(uri_preexisting, None, None)
-        self.assertEqual(cursor['a'], 'b')
-        cursor.close()
-
-        #
-        # Part 1: Create new tables in the follower (e.g., due to oplog application)
-        #
-
-        # An empty table
-        uri_new_empty = self.prefix + self.table_name_new_empty
-        config = self.create_session_config + ',' + self.table_config
-        session_follow.create(uri_new_empty, config)
-
-        # A table with some data
-        uri_new_filled = self.prefix + self.table_name_new_filled
-        config = self.create_session_config + ',' + self.table_config
-        session_follow.create(uri_new_filled, config)
-
-        session_follow.begin_transaction() # Draining requires timestamps
-        cursor = session_follow.open_cursor(uri_new_filled, None, None)
-        cursor['a'] = 'b'
-        cursor.close()
-        session_follow.commit_transaction('commit_timestamp=' + self.timestamp_str(10))
-
-        #
-        # Part 2: Close the original connection, and step up the follower
-        #
-
-        # We didn't create tables in the leader, because we're using clean shutdown here.
-        self.close_conn()
-
-        # Step up the follower, which should result in the tables being created in SLS.
-        conn_follow.reconfigure('disaggregated=(role="leader")')
-
-        # Create a checkpoint, to ensure the data is committed to SLS.
-        session_follow.set_timestamp('stable_timestamp=' + self.timestamp_str(10))
-        session_follow.checkpoint()
-
-        #
-        # Part 3: Open a new follower and check that the tables are there
-        #
-
-        # Start the a follower
-        self.restart_without_local_files()
-        self.open_conn()
-        self.disagg_advance_checkpoint(self.conn, conn_follow)
-
-        # Check the tables in the follower
-        cursor = self.session.open_cursor(uri_preexisting, None, None)
-        self.assertEqual(cursor['a'], 'b')
-        cursor.close()
-
-        cursor = self.session.open_cursor(uri_new_empty, None, None)
-        item_count = 0
-        while cursor.next() == 0:
-            item_count += 1
-        cursor.close()
-        self.assertEqual(item_count, 0)
-
-        cursor = self.session.open_cursor(uri_new_filled, None, None)
-        self.assertEqual(cursor['a'], 'b')
-        cursor.close()
-
-        #
-        # Clean up
-        #
-
-        session_follow.close()
-        conn_follow.close()
