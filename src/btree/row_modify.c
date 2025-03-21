02c4a6e9e186b431708d1c9612a5a5cdb6daf066
Chenhao Qu <chenhao.qu@mongodb.com>
2025-03-21 03:55:59 +0000

    Fix obsolete check

--- src/btree/row_modify.c
+++ src/btree/row_modify.c
@@ -355,7 +355,7 @@ __wt_update_obsolete_check(
     WT_PAGE *page;
     WT_TXN_GLOBAL *txn_global;
     WT_UPDATE *first, *next;
-    wt_timestamp_t last_ckpt_timestamp;
+    wt_timestamp_t prune_timestamp;
     size_t size;
     uint64_t oldest_id;
     u_int count;
@@ -369,8 +369,7 @@ __wt_update_obsolete_check(
     if (WT_PAGE_TRYLOCK(session, page) != 0)
         return;
 
-    WT_ACQUIRE_READ(
-      last_ckpt_timestamp, S2C(session)->disaggregated_storage.last_checkpoint_timestamp);
+    WT_ACQUIRE_READ(prune_timestamp, session->dhandle->prune_timestamp);
 
     oldest_id = __wt_txn_oldest_id(session);
 
@@ -407,8 +406,8 @@ __wt_update_obsolete_check(
          */
         if (__wt_txn_upd_visible_all(session, upd) ||
           (F_ISSET(CUR2BT(cbt), WT_BTREE_GARBAGE_COLLECT) &&
-            (WT_TXNID_LT(upd->txnid, oldest_id) && last_ckpt_timestamp != WT_TS_NONE &&
-              upd->durable_ts <= last_ckpt_timestamp))) {
+            (WT_TXNID_LT(upd->txnid, oldest_id) && prune_timestamp != WT_TS_NONE &&
+              upd->durable_ts <= prune_timestamp))) {
             if (first == NULL && WT_UPDATE_DATA_VALUE(upd))
                 first = upd;
         } else
