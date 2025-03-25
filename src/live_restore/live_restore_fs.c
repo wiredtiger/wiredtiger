ecdace3a24858c9ffab517b272fde70d07f84365
Sean <41533874+Sean04@users.noreply.github.com>
2025-03-25 17:42:24 +1100

    Update src/live_restore/live_restore_fs.c
    Co-authored-by: Andrew Morton <andrew.morton@mongodb.com>

--- src/live_restore/live_restore_fs.c
+++ src/live_restore/live_restore_fs.c
@@ -1013,9 +1013,9 @@ __wti_live_restore_fs_restore_file(WT_FILE_HANDLE *fh, WT_SESSION *wt_session)
         WT_ERR(__live_restore_fh_close_source(session, lr_fh, true));
 
         /*
-         * Dirty the tree again to ensure the live restore metadata is written out with checkpoint.
-         * Avoid original files as they take "fake" checkpoints and cannot be original and modified
-         * at the same time.
+         * Dirty the tree again to ensure the live restore metadata is written out by the next checkpoint.
+         * Avoid original files as they take "fake" checkpoints and setting both the original and modified flags
+         * at the same time is an illegal state.
          */
         if (!S2BT(session)->original)
             __wt_tree_modify_set(session);
