2440a730420ff03e4adca8599b46aa04219845db
Monica Ng <monica.ng@mongodb.com>
2025-04-07 04:18:32 +0000

    Address review comments

--- src/reconcile/rec_write.c
+++ src/reconcile/rec_write.c
@@ -2607,7 +2607,7 @@ __rec_split_write(WT_SESSION_IMPL *session, WT_RECONCILE *r, WT_REC_CHUNK *chunk
              *
              * Also track how large the delta is compared to the full page image.
              */
-            WT_STAT_CONN_INCRV(session, block_byte_write_saved_delta_intl, chunk->image.size);
+            WT_STAT_CONN_INCRV(session, block_byte_write_saved_delta_intl, chunk->image.size - r->delta.size);
 
             if (delta_pct <= 20)
                 WT_STAT_CONN_INCR(session, block_byte_write_intl_delta_lt20);
@@ -2632,7 +2632,7 @@ __rec_split_write(WT_SESSION_IMPL *session, WT_RECONCILE *r, WT_REC_CHUNK *chunk
                   multi->block_meta.delta_count);
         } else if (F_ISSET(r->ref, WT_REF_FLAG_LEAF)) {
             WT_STAT_CONN_DSRC_INCR(session, rec_page_delta_leaf);
-            WT_STAT_CONN_INCRV(session, block_byte_write_saved_delta_leaf, chunk->image.size);
+            WT_STAT_CONN_INCRV(session, block_byte_write_saved_delta_leaf, chunk->image.size - r->delta.size);
 
             if (delta_pct <= 20)
                 WT_STAT_CONN_INCR(session, block_byte_write_leaf_delta_lt20);
@@ -2686,15 +2686,15 @@ __rec_split_write(WT_SESSION_IMPL *session, WT_RECONCILE *r, WT_REC_CHUNK *chunk
                 ++multi->block_meta.reconciliation_id;
         } else
             __wt_page_block_meta_assign(session, &multi->block_meta);
+        WT_RET(__rec_write(session, compressed_image == NULL ? &chunk->image : compressed_image,
+          &multi->block_meta, addr, &addr_size, &compressed_size, false,
+          F_ISSET(r, WT_REC_CHECKPOINT), compressed_image != NULL));
 
         if (F_ISSET(r->ref, WT_REF_FLAG_INTERNAL))
             WT_STAT_CONN_INCR(session, rec_page_full_image_internal);
         else if (F_ISSET(r->ref, WT_REF_FLAG_LEAF))
             WT_STAT_CONN_INCR(session, rec_page_full_image_leaf);
 
-        WT_RET(__rec_write(session, compressed_image == NULL ? &chunk->image : compressed_image,
-          &multi->block_meta, addr, &addr_size, &compressed_size, false,
-          F_ISSET(r, WT_REC_CHECKPOINT), compressed_image != NULL));
 #ifdef HAVE_DIAGNOSTIC
         verify_image = true;
 #endif
