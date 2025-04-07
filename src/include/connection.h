52f1f816ef9361924e593b045a1b83e4ef4a432b
jchen <jie.chen@mongodb.com>
2025-04-07 04:04:03 +0000

    ran s_all

--- src/include/connection.h
+++ src/include/connection.h
@@ -846,6 +846,6 @@ struct __wt_sweep_cookie {
  *      Whenever conn->close encounters a non-zero return code, abort the process to track where it
  * came from. This is strictly to be used for debugging purposes.
  */
-#define WT_CONN_CLOSE_ABORT(s, ret)                     \
+#define WT_CONN_CLOSE_ABORT(s, ret)                                          \
     if (F_ISSET(S2C(s), WT_CONN_CLOSING) && (ret != 0) && (ret != WT_PANIC)) \
         __wt_abort(s);
