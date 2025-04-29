629093c4d76c55ec14ad5a1aff9325811216e566
Hari Babu Kommi <haribabu.kommi@mongodb.com>
2025-04-30 07:42:02 +1000

    Add a comment

--- src/conn/conn_dhandle.c
+++ src/conn/conn_dhandle.c
@@ -324,6 +324,7 @@ __wt_conn_dhandle_close(WT_SESSION_IMPL *session, bool final, bool mark_dead, bo
         WT_ASSERT_ALWAYS(session, btree->max_upd_txn != WT_TXN_ABORTED,
           "Assert failure: session: %s: btree->max_upd_txn == WT_TXN_ABORTED", session->name);
         if (check_visibility) {
+            /* Bump the oldest ID, we're about to do some visibility checks. */
             WT_RET(__wt_txn_update_oldest(session, WT_TXN_OLDEST_STRICT | WT_TXN_OLDEST_WAIT));
             if (!__wt_txn_visible_all(session, btree->max_upd_txn, WT_TS_NONE))
                 WT_RET_SUB(session, EBUSY, WT_UNCOMMITTED_DATA,
