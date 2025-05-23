664d4b65891a2c03092e378bb032b59af85646a4
Alana Huang <alana.huang@mongodb.com>
2025-05-23 01:12:26 +0000

    split the if condition to be more readable

--- src/evict/evict_page.c
+++ src/evict/evict_page.c
@@ -915,17 +915,23 @@ __evict_reconcile(WT_SESSION_IMPL *session, WT_REF *ref, uint32_t evict_flags)
          * Scrub if we're supposed to or toss it in sometimes if we are in debugging mode.
          *
          * Note that don't scrub if checkpoint is running on the tree.
-         *
-         * Scrub only cache is under clean eviction target or the page has high read generation.
          */
-        if (!WT_SESSION_BTREE_SYNC(session) &&
-          (F_ISSET(evict, WT_EVICT_CACHE_SCRUB) ||
-            (FLD_ISSET(conn->debug_flags, WT_CONN_DEBUG_EVICT_AGGRESSIVE_MODE) &&
-              __wt_random(&session->rnd_random) % 3 == 0))) {
-            if (!(__wt_evict_clean_needed(session, NULL)) ||
-              ref->page->read_gen > __evict_read_gen(session)) {
-                LF_SET(WT_REC_SCRUB);
-            }
+        const bool debug_aggressive_scrub_enabled =
+          FLD_ISSET(conn->debug_flags, WT_CONN_DEBUG_EVICT_AGGRESSIVE_MODE) &&
+          __wt_random(&session->rnd_random) % 3 == 0;
+
+        const bool can_scrub = !WT_SESSION_BTREE_SYNC(session) &&
+          (F_ISSET(evict, WT_EVICT_CACHE_SCRUB) || debug_aggressive_scrub_enabled);
+
+        /*
+         * Scrub only if cache is under the clean eviction target or the page has high read
+         * generation.
+         */
+        const bool is_scrub_candidate = !__wt_evict_clean_needed(session, NULL) ||
+          ref->page->read_gen > __evict_read_gen(session);
+
+        if (can_scrub && is_scrub_candidate) {
+            LF_SET(WT_REC_SCRUB);
         }
     }
 
