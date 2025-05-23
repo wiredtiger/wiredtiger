9ca11f9d13fbaf202c28049ec22f410bdb7a3423
zunyi.liu <zunyi.liu@mongodb.com>
2025-05-23 07:07:35 +0000

    Add logic to allow truncate only when connection is ready for hs operation and allow the truncate when remove_files is false

--- src/schema/schema_drop.c
+++ src/schema/schema_drop.c
@@ -50,21 +50,21 @@ __drop_file(
 
     /* Remove the metadata entry (ignore missing items). */
     WT_TRET(__wt_metadata_remove(session, uri));
-    if (!remove_files)
-        return (ret);
-
-    /*
-     * Schedule the remove of the underlying physical file when the drop completes.
-     */
-    WT_TRET(__wt_meta_track_drop(session, filename));
+    if (remove_files)
+        /*
+         * Schedule the remove of the underlying physical file when the drop completes.
+         */
+        WT_TRET(__wt_meta_track_drop(session, filename));
 
     /*
      * Truncate history store for the dropped file, this is a best-effort operation, as we don't
      * fail drop if truncate returns an error. There is no history store to truncate for in-memory
-     * database.
+     * database, and we should not call history store truncate if connection is not minimal or
+     * ready.
      */
     WT_ERR(ret);
-    if (!F_ISSET_ATOMIC_32(S2C(session), WT_CONN_IN_MEMORY))
+    if (!F_ISSET_ATOMIC_32(S2C(session), WT_CONN_IN_MEMORY) &&
+      F_ISSET_ATOMIC_32(S2C(session), WT_CONN_MINIMAL | WT_CONN_READY))
         if (__wt_hs_btree_truncate(session, id) != 0)
             __wt_verbose_warning(
               session, WT_VERB_HS, "Failed to truncate history store for the file: %s", uri);
