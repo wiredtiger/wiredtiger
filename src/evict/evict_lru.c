30e873719d8d162ae87e6d3bb30a40e120139eca
Linh Tran <30374401+tdlinh2712@users.noreply.github.com>
2025-06-04 13:32:19 +1000

    WT-14657 Track stats for oldest id rollback due to cache stuck (#11986)
    Currently there are two reasons why an oldest transaction is rolled
    back:
    - Its transaction id is the global oldest in the system
    - It's transaction snapshot id is the global oldest
    
    This PR separate the stats to track the number of occurrences of each
    cause
    
    (cherry picked from commit 1bf57370a44236f9a9cbc8936c57d15736228e83)

--- src/evict/evict_lru.c
+++ src/evict/evict_lru.c
@@ -2862,10 +2862,9 @@ __wti_evict_app_assist_worker(
             ret = __wt_txn_is_blocking(session);
             if (ret == WT_ROLLBACK) {
                 __wt_atomic_decrement_if_positive(&evict->evict_aggressive_score);
-
-                WT_STAT_CONN_INCR(session, txn_rollback_oldest_pinned);
-                __wt_verbose_debug1(session, WT_VERB_TRANSACTION, "rollback reason: %s",
-                  session->txn->rollback_reason);
+                if (F_ISSET(session, WT_SESSION_SAVE_ERRORS))
+                    __wt_verbose_debug1(session, WT_VERB_TRANSACTION, "rollback reason: %s",
+                      session->err_info.err_msg);
             }
             WT_ERR(ret);
         }
