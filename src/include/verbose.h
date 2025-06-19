5e947231d9cac367c1985ca518b4f0483fded259
Linh Tran <linh.tran@mongodb.com>
2025-06-19 05:15:27 +0000

    fix test

--- src/include/verbose.h
+++ src/include/verbose.h
@@ -50,6 +50,7 @@
     "WT_VERB_SALVAGE", \
     "WT_VERB_SHARED_CACHE", \
     "WT_VERB_SPLIT", \
+    "WT_VERB_SWEEP", \
     "WT_VERB_TEMPORARY", \
     "WT_VERB_THREAD_GROUP", \
     "WT_VERB_TIERED", \
