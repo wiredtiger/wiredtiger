wiredtiger/wiredtiger

commit aa8b5a7c0c7cd13d5c08ce5c3566ee1ee9078864

WT-14140 Unnecessary schema lock taken for active file: dhandles that are not swept (#11934)
Summarize the reason behind this change (this might be the problem
you're solving, or the context around the request) and the solution you
have chosen.

src/conn/conn_sweep.c
@@ -14,6 +14,41 @@
       __wt_atomic_load32(&(dhandle)->references) == 0)
 
 /*
+ * __sweep_file_dhandle_check_and_reset_tod --
+ *     Check if the file dhandle exists for the table dhandle and resets its time-of-death if it
+ *     does.
+ */
+static int
+__sweep_file_dhandle_check_and_reset_tod(WT_SESSION_IMPL *session, WT_DATA_HANDLE *dhandle)
+{
+    WT_DECL_RET;
+    WT_TABLE *table;
+
+    ret = WT_NOTFOUND;
+    /*
+     * The sweep server's algorithm is altered to prevent unnecessary table dhandle closures. This
+     * is done by checking for associated file dhandles before marking table dhandles for sweeping.
+     * It resolves schema lock contention caused by repetitive table dhandle operations during
+     * MongoDB cursor activity on simple tables, and ensures table dhandles are retained for active
+     * file dhandles, which is required for file dhandle access.
+     */
+    table = (WT_TABLE *)dhandle;
+    if (table->is_simple && table->cg_complete) {
+        ret = __wt_conn_dhandle_find(session, table->cgroups[0]->source, NULL);
+
+        /*
+         * Reset the time of death if the file dhandle exists for the associated table dhandle.
+         */
+        if (ret == 0) {
+            dhandle->timeofdeath = 0;
+            return (ret);
+        }
+    }
+
+    return (ret);
+}
+
+/*
  * __sweep_mark --
  *     Mark idle handles with a time of death, and note if we see dead handles.
  */
@@ -22,6 +57,7 @@ __sweep_mark(WT_SESSION_IMPL *session, uint64_t now)
 {
     WT_CONNECTION_IMPL *conn;
     WT_DATA_HANDLE *dhandle;
+    WT_DECL_RET;
 
     conn = S2C(session);
 
@@ -44,6 +80,20 @@ __sweep_mark(WT_SESSION_IMPL *session, uint64_t now)
         if (F_ISSET(dhandle, WT_DHANDLE_EXCLUSIVE) ||
           __wt_atomic_loadi32(&dhandle->session_inuse) > 0 || dhandle->timeofdeath != 0)
             continue;
+
+        /* For table dhandles, skip expiration if associated file dhandles exist. */
+        if (dhandle->type == WT_DHANDLE_TYPE_TABLE) {
+            WT_WITH_TABLE_READ_LOCK(session,
+              WT_WITH_HANDLE_LIST_READ_LOCK(
+                session, (ret = __sweep_file_dhandle_check_and_reset_tod(session, dhandle))));
+
+            /* Continue if the file dhandle exists for the associated table dhandle. */
+            if (ret == 0)
+                continue;
+
+            WT_ASSERT_ALWAYS(
+              session, ret == WT_NOTFOUND, "Connection dhandle find has returned an error.");
+        }
 
         /*
          * Never close out the history store handle via sweep. It can cause a deadlock if eviction

test/suite/test_sweep06.py
@@ -1,0 +1,91 @@
+#!/usr/bin/env python
+#
+# Public Domain 2014-present MongoDB, Inc.
+# Public Domain 2008-2014 WiredTiger, Inc.
+#
+# This is free and unencumbered software released into the public domain.
+#
+# Anyone is free to copy, modify, publish, use, compile, sell, or
+# distribute this software, either in source code form or as a compiled
+# binary, for any purpose, commercial or non-commercial, and by any
+# means.
+#
+# In jurisdictions that recognize copyright laws, the author or authors
+# of this software dedicate any and all copyright interest in the
+# software to the public domain. We make this dedication for the benefit
+# of the public at large and to the detriment of our heirs and
+# successors. We intend this dedication to be an overt act of
+# relinquishment in perpetuity of all present and future rights to this
+# software under copyright law.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+#
+# test_sweep06.py
+# This test confirms that table dhandles are correctly retained and not
+# marked for sweep server expiry when file data dhandles are present.
+
+from suite_subprocess import suite_subprocess
+import wttest, threading
+from wiredtiger import stat
+from wtscenario import make_scenarios
+
+class test_sweep06(wttest.WiredTigerTestCase, suite_subprocess):
+    dhandles = 200
+    format='key_format=i,value_format=S'
+    tablebase = 'test_sweep06'
+    uri = 'table:' + tablebase
+    conn_config = 'file_manager=(close_handle_minimum=0,' + \
+                  'close_idle_time=60,close_scan_interval=30),session_max=530'
+
+    cursor_caching = [
+        ('cursor_caching_disabled', dict(cursor_caching=False)),
+        ('cursor_caching_enabled', dict(cursor_caching=True)),
+    ]
+
+    scenarios = make_scenarios(cursor_caching)
+
+    def insert(self, i, start, rows):
+        session = self.conn.open_session()
+        uri = self.uri + str(i)
+        cursor = session.open_cursor(uri)
+
+        session.begin_transaction()
+        for i in range(start, rows):
+            cursor.set_key(i)
+            cursor.set_value(str(i))
+            cursor.insert()
+        session.commit_transaction()
+        cursor.close()
+        session.close()
+
+    def test_dhandles(self):
+        if self.cursor_caching:
+            self.session.reconfigure('cache_cursors=true')
+        for i in range(1,self.dhandles):
+            uri = self.uri + str(i)
+            self.session.create(uri, self.format)
+
+        for i in range(1,100):
+            threads = []
+            for i in range(1,self.dhandles):
+                thread = threading.Thread(target=self.insert, args=(i, 0, 100))
+                thread.start()
+                threads.append(thread)
+
+            for thread in threads:
+                thread.join()
+
+        stat_cursor = self.session.open_cursor('statistics:', None, None)
+        close1 = stat_cursor[stat.conn.dh_sweep_dead_close][2]
+        close2 = stat_cursor[stat.conn.dh_sweep_expired_close][2]
+        stat_cursor.close()
+
+        # The expectation is that no dhandles have been closed.
+        self.assertEqual(close1, 0)
+        self.assertEqual(close2, 0)

dist/s_string.ok
@@ -1343,6 +1343,7 @@ timespec
 tinfo
 tlb
 tmp
+tod
 tokenizer
 toklen
 tokname

commit 1d974de6d085e32006f2eb546c5ea19c1bef7e09

WT-14804 Fix crash when printing an invalid dhandle's name on sweep walk (#12038)
A bug was introduced in commit
https://github.com/wiredtiger/wiredtiger/commit/aa8b5a7c0c7cd13d5c08ce5c3566ee1ee9078864
, in `__sweep_file_dhandle_check_and_reset_tod` function which calls
`__wt_conn_dhandle_find`. Inside `__wt_conn_dhandle_find()` we find a
dhandle and assign the `session->dhandle` to it.
Later inside the sweep walk, `__sweep_check_session_callback` is called,
but the dhandle might already been deleted but `session->dhandle` still
points to that handle, when it tries to print the session's dhandle
name, it will point to an invalid pointer.
This PR fixes it by assigning back the session's dhandle to null after
calling `__wt_conn_dhandle_find()` to avoid this bug from happening.

dist/api_data.py
@@ -863,6 +863,7 @@ connection_runtime_config = [
             'salvage',
             'shared_cache',
             'split',
+            'sweep',
             'temporary',
             'thread_group',
             'tiered',

src/config/config_def.c
@@ -589,6 +589,7 @@ const char __WT_CONFIG_CHOICE_rts[] = "rts";
 const char __WT_CONFIG_CHOICE_salvage[] = "salvage";
 const char __WT_CONFIG_CHOICE_shared_cache[] = "shared_cache";
 const char __WT_CONFIG_CHOICE_split[] = "split";
+const char __WT_CONFIG_CHOICE_sweep[] = "sweep";
 const char __WT_CONFIG_CHOICE_temporary[] = "temporary";
 const char __WT_CONFIG_CHOICE_thread_group[] = "thread_group";
 const char __WT_CONFIG_CHOICE_tiered[] = "tiered";
@@ -611,9 +612,9 @@ static const char *confchk_verbose_choices[] = {__WT_CONFIG_CHOICE_all, __WT_CON
   __WT_CONFIG_CHOICE_prefetch, __WT_CONFIG_CHOICE_read, __WT_CONFIG_CHOICE_reconcile,
   __WT_CONFIG_CHOICE_recovery, __WT_CONFIG_CHOICE_recovery_progress, __WT_CONFIG_CHOICE_rts,
   __WT_CONFIG_CHOICE_salvage, __WT_CONFIG_CHOICE_shared_cache, __WT_CONFIG_CHOICE_split,
-  __WT_CONFIG_CHOICE_temporary, __WT_CONFIG_CHOICE_thread_group, __WT_CONFIG_CHOICE_tiered,
-  __WT_CONFIG_CHOICE_timestamp, __WT_CONFIG_CHOICE_transaction, __WT_CONFIG_CHOICE_verify,
-  __WT_CONFIG_CHOICE_version, __WT_CONFIG_CHOICE_write, NULL};
+  __WT_CONFIG_CHOICE_sweep, __WT_CONFIG_CHOICE_temporary, __WT_CONFIG_CHOICE_thread_group,
+  __WT_CONFIG_CHOICE_tiered, __WT_CONFIG_CHOICE_timestamp, __WT_CONFIG_CHOICE_transaction,
+  __WT_CONFIG_CHOICE_verify, __WT_CONFIG_CHOICE_version, __WT_CONFIG_CHOICE_write, NULL};
 
 static const WT_CONFIG_CHECK confchk_WT_CONNECTION_reconfigure[] = {
   {"block_cache", "category", NULL, NULL, confchk_wiredtiger_open_block_cache_subconfigs, 12,
@@ -747,7 +748,7 @@ static const WT_CONFIG_CHECK confchk_WT_CONNECTION_reconfigure[] = {
     "\"live_restore_progress\",\"log\",\"metadata\",\"mutex\","
     "\"out_of_order\",\"overflow\",\"prefetch\",\"read\","
     "\"reconcile\",\"recovery\",\"recovery_progress\",\"rts\","
-    "\"salvage\",\"shared_cache\",\"split\",\"temporary\","
+    "\"salvage\",\"shared_cache\",\"split\",\"sweep\",\"temporary\","
     "\"thread_group\",\"tiered\",\"timestamp\",\"transaction\","
     "\"verify\",\"version\",\"write\"]",
     NULL, 0, NULL, WT_CONFIG_COMPILED_TYPE_LIST, 10, INT64_MIN, INT64_MAX, confchk_verbose_choices},
@@ -2630,9 +2631,9 @@ static const char *confchk_verbose12_choices[] = {__WT_CONFIG_CHOICE_all, __WT_C
   __WT_CONFIG_CHOICE_prefetch, __WT_CONFIG_CHOICE_read, __WT_CONFIG_CHOICE_reconcile,
   __WT_CONFIG_CHOICE_recovery, __WT_CONFIG_CHOICE_recovery_progress, __WT_CONFIG_CHOICE_rts,
   __WT_CONFIG_CHOICE_salvage, __WT_CONFIG_CHOICE_shared_cache, __WT_CONFIG_CHOICE_split,
-  __WT_CONFIG_CHOICE_temporary, __WT_CONFIG_CHOICE_thread_group, __WT_CONFIG_CHOICE_tiered,
-  __WT_CONFIG_CHOICE_timestamp, __WT_CONFIG_CHOICE_transaction, __WT_CONFIG_CHOICE_verify,
-  __WT_CONFIG_CHOICE_version, __WT_CONFIG_CHOICE_write, NULL};
+  __WT_CONFIG_CHOICE_sweep, __WT_CONFIG_CHOICE_temporary, __WT_CONFIG_CHOICE_thread_group,
+  __WT_CONFIG_CHOICE_tiered, __WT_CONFIG_CHOICE_timestamp, __WT_CONFIG_CHOICE_transaction,
+  __WT_CONFIG_CHOICE_verify, __WT_CONFIG_CHOICE_version, __WT_CONFIG_CHOICE_write, NULL};
 
 static const char *confchk_write_through_choices[] = {
   __WT_CONFIG_CHOICE_data, __WT_CONFIG_CHOICE_log, NULL};
@@ -2830,7 +2831,7 @@ static const WT_CONFIG_CHECK confchk_wiredtiger_open[] = {
     "\"live_restore_progress\",\"log\",\"metadata\",\"mutex\","
     "\"out_of_order\",\"overflow\",\"prefetch\",\"read\","
     "\"reconcile\",\"recovery\",\"recovery_progress\",\"rts\","
-    "\"salvage\",\"shared_cache\",\"split\",\"temporary\","
+    "\"salvage\",\"shared_cache\",\"split\",\"sweep\",\"temporary\","
     "\"thread_group\",\"tiered\",\"timestamp\",\"transaction\","
     "\"verify\",\"version\",\"write\"]",
     NULL, 0, NULL, WT_CONFIG_COMPILED_TYPE_LIST, 10, INT64_MIN, INT64_MAX,
@@ -2896,9 +2897,9 @@ static const char *confchk_verbose13_choices[] = {__WT_CONFIG_CHOICE_all, __WT_C
   __WT_CONFIG_CHOICE_prefetch, __WT_CONFIG_CHOICE_read, __WT_CONFIG_CHOICE_reconcile,
   __WT_CONFIG_CHOICE_recovery, __WT_CONFIG_CHOICE_recovery_progress, __WT_CONFIG_CHOICE_rts,
   __WT_CONFIG_CHOICE_salvage, __WT_CONFIG_CHOICE_shared_cache, __WT_CONFIG_CHOICE_split,
-  __WT_CONFIG_CHOICE_temporary, __WT_CONFIG_CHOICE_thread_group, __WT_CONFIG_CHOICE_tiered,
-  __WT_CONFIG_CHOICE_timestamp, __WT_CONFIG_CHOICE_transaction, __WT_CONFIG_CHOICE_verify,
-  __WT_CONFIG_CHOICE_version, __WT_CONFIG_CHOICE_write, NULL};
+  __WT_CONFIG_CHOICE_sweep, __WT_CONFIG_CHOICE_temporary, __WT_CONFIG_CHOICE_thread_group,
+  __WT_CONFIG_CHOICE_tiered, __WT_CONFIG_CHOICE_timestamp, __WT_CONFIG_CHOICE_transaction,
+  __WT_CONFIG_CHOICE_verify, __WT_CONFIG_CHOICE_version, __WT_CONFIG_CHOICE_write, NULL};
 
 static const char *confchk_write_through2_choices[] = {
   __WT_CONFIG_CHOICE_data, __WT_CONFIG_CHOICE_log, NULL};
@@ -3096,7 +3097,7 @@ static const WT_CONFIG_CHECK confchk_wiredtiger_open_all[] = {
     "\"live_restore_progress\",\"log\",\"metadata\",\"mutex\","
     "\"out_of_order\",\"overflow\",\"prefetch\",\"read\","
     "\"reconcile\",\"recovery\",\"recovery_progress\",\"rts\","
-    "\"salvage\",\"shared_cache\",\"split\",\"temporary\","
+    "\"salvage\",\"shared_cache\",\"split\",\"sweep\",\"temporary\","
     "\"thread_group\",\"tiered\",\"timestamp\",\"transaction\","
     "\"verify\",\"version\",\"write\"]",
     NULL, 0, NULL, WT_CONFIG_COMPILED_TYPE_LIST, 10, INT64_MIN, INT64_MAX,
@@ -3165,9 +3166,9 @@ static const char *confchk_verbose14_choices[] = {__WT_CONFIG_CHOICE_all, __WT_C
   __WT_CONFIG_CHOICE_prefetch, __WT_CONFIG_CHOICE_read, __WT_CONFIG_CHOICE_reconcile,
   __WT_CONFIG_CHOICE_recovery, __WT_CONFIG_CHOICE_recovery_progress, __WT_CONFIG_CHOICE_rts,
   __WT_CONFIG_CHOICE_salvage, __WT_CONFIG_CHOICE_shared_cache, __WT_CONFIG_CHOICE_split,
-  __WT_CONFIG_CHOICE_temporary, __WT_CONFIG_CHOICE_thread_group, __WT_CONFIG_CHOICE_tiered,
-  __WT_CONFIG_CHOICE_timestamp, __WT_CONFIG_CHOICE_transaction, __WT_CONFIG_CHOICE_verify,
-  __WT_CONFIG_CHOICE_version, __WT_CONFIG_CHOICE_write, NULL};
+  __WT_CONFIG_CHOICE_sweep, __WT_CONFIG_CHOICE_temporary, __WT_CONFIG_CHOICE_thread_group,
+  __WT_CONFIG_CHOICE_tiered, __WT_CONFIG_CHOICE_timestamp, __WT_CONFIG_CHOICE_transaction,
+  __WT_CONFIG_CHOICE_verify, __WT_CONFIG_CHOICE_version, __WT_CONFIG_CHOICE_write, NULL};
 
 static const char *confchk_write_through3_choices[] = {
   __WT_CONFIG_CHOICE_data, __WT_CONFIG_CHOICE_log, NULL};
@@ -3353,7 +3354,7 @@ static const WT_CONFIG_CHECK confchk_wiredtiger_open_basecfg[] = {
     "\"live_restore_progress\",\"log\",\"metadata\",\"mutex\","
     "\"out_of_order\",\"overflow\",\"prefetch\",\"read\","
     "\"reconcile\",\"recovery\",\"recovery_progress\",\"rts\","
-    "\"salvage\",\"shared_cache\",\"split\",\"temporary\","
+    "\"salvage\",\"shared_cache\",\"split\",\"sweep\",\"temporary\","
     "\"thread_group\",\"tiered\",\"timestamp\",\"transaction\","
     "\"verify\",\"version\",\"write\"]",
     NULL, 0, NULL, WT_CONFIG_COMPILED_TYPE_LIST, 10, INT64_MIN, INT64_MAX,
@@ -3422,9 +3423,9 @@ static const char *confchk_verbose15_choices[] = {__WT_CONFIG_CHOICE_all, __WT_C
   __WT_CONFIG_CHOICE_prefetch, __WT_CONFIG_CHOICE_read, __WT_CONFIG_CHOICE_reconcile,
   __WT_CONFIG_CHOICE_recovery, __WT_CONFIG_CHOICE_recovery_progress, __WT_CONFIG_CHOICE_rts,
   __WT_CONFIG_CHOICE_salvage, __WT_CONFIG_CHOICE_shared_cache, __WT_CONFIG_CHOICE_split,
-  __WT_CONFIG_CHOICE_temporary, __WT_CONFIG_CHOICE_thread_group, __WT_CONFIG_CHOICE_tiered,
-  __WT_CONFIG_CHOICE_timestamp, __WT_CONFIG_CHOICE_transaction, __WT_CONFIG_CHOICE_verify,
-  __WT_CONFIG_CHOICE_version, __WT_CONFIG_CHOICE_write, NULL};
+  __WT_CONFIG_CHOICE_sweep, __WT_CONFIG_CHOICE_temporary, __WT_CONFIG_CHOICE_thread_group,
+  __WT_CONFIG_CHOICE_tiered, __WT_CONFIG_CHOICE_timestamp, __WT_CONFIG_CHOICE_transaction,
+  __WT_CONFIG_CHOICE_verify, __WT_CONFIG_CHOICE_version, __WT_CONFIG_CHOICE_write, NULL};
 
 static const char *confchk_write_through4_choices[] = {
   __WT_CONFIG_CHOICE_data, __WT_CONFIG_CHOICE_log, NULL};
@@ -3610,7 +3611,7 @@ static const WT_CONFIG_CHECK confchk_wiredtiger_open_usercfg[] = {
     "\"live_restore_progress\",\"log\",\"metadata\",\"mutex\","
     "\"out_of_order\",\"overflow\",\"prefetch\",\"read\","
     "\"reconcile\",\"recovery\",\"recovery_progress\",\"rts\","
-    "\"salvage\",\"shared_cache\",\"split\",\"temporary\","
+    "\"salvage\",\"shared_cache\",\"split\",\"sweep\",\"temporary\","
     "\"thread_group\",\"tiered\",\"timestamp\",\"transaction\","
     "\"verify\",\"version\",\"write\"]",
     NULL, 0, NULL, WT_CONFIG_COMPILED_TYPE_LIST, 10, INT64_MIN, INT64_MAX,

src/conn/conn_api.c
@@ -2341,7 +2341,7 @@ __wt_verbose_config(WT_SESSION_IMPL *session, const char *cfg[], bool reconfig)
       {"read", WT_VERB_READ}, {"reconcile", WT_VERB_RECONCILE}, {"recovery", WT_VERB_RECOVERY},
       {"recovery_progress", WT_VERB_RECOVERY_PROGRESS}, {"rts", WT_VERB_RTS},
       {"salvage", WT_VERB_SALVAGE}, {"shared_cache", WT_VERB_SHARED_CACHE},
-      {"split", WT_VERB_SPLIT}, {"temporary", WT_VERB_TEMPORARY},
+      {"split", WT_VERB_SPLIT}, {"sweep", WT_VERB_SWEEP}, {"temporary", WT_VERB_TEMPORARY},
       {"thread_group", WT_VERB_THREAD_GROUP}, {"timestamp", WT_VERB_TIMESTAMP},
       {"tiered", WT_VERB_TIERED}, {"transaction", WT_VERB_TRANSACTION}, {"verify", WT_VERB_VERIFY},
       {"version", WT_VERB_VERSION}, {"write", WT_VERB_WRITE}, {NULL, 0}};

src/conn/conn_sweep.c
@@ -41,6 +41,7 @@ __sweep_file_dhandle_check_and_reset_tod(WT_SESSION_IMPL *session, WT_DATA_HANDL
          */
         if (ret == 0) {
             dhandle->timeofdeath = 0;
+            session->dhandle = NULL;
             return (ret);
         }
     }
@@ -102,6 +103,9 @@ __sweep_mark(WT_SESSION_IMPL *session, uint64_t now)
         if (WT_IS_HS(dhandle))
             continue;
 
+        __wt_verbose_level(session, WT_VERB_SWEEP, WT_VERBOSE_DEBUG_3,
+          "Sweep server setting the time of death for dhandle %s", dhandle->name);
+
         dhandle->timeofdeath = now;
         WT_STAT_CONN_INCR(session, dh_sweep_tod);
     }
@@ -144,7 +148,6 @@ static int
 __sweep_expire_one(WT_SESSION_IMPL *session)
 {
     WT_DECL_RET;
-
     /*
      * Acquire an exclusive lock on the handle and mark it dead.
      *
@@ -229,7 +232,6 @@ __sweep_discard_trees(WT_SESSION_IMPL *session, u_int *dead_handlesp)
 
         if (!F_ISSET(dhandle, WT_DHANDLE_OPEN) || !F_ISSET(dhandle, WT_DHANDLE_DEAD))
             continue;
-
         /* If the handle is marked dead, flush it from cache. */
         WT_WITH_DHANDLE(
           session, dhandle, ret = __wt_conn_dhandle_close(session, false, false, false));
@@ -372,7 +374,7 @@ __sweep_check_session_callback(
         if (!array_session->sweep_warning_60min) {
             array_session->sweep_warning_60min = 1;
             WT_STAT_CONN_INCR(session, no_session_sweep_60min);
-            __wt_verbose_warning(session, WT_VERB_DEFAULT,
+            __wt_verbose_warning(session, WT_VERB_SWEEP,
               "Session %" PRIu32 " (@: 0x%p name: %s) did not run a sweep for 60 minutes.",
               array_session->id, (void *)array_session,
               array_session->name == NULL ? "EMPTY" : array_session->name);
@@ -474,6 +476,9 @@ __sweep_server(void *arg)
         /*
          * Check for any "rogue" sessions, which did not run a session sweep in a long time.
          */
+        __wt_verbose_level(session, WT_VERB_SWEEP, WT_VERBOSE_DEBUG_3,
+          "Sweep server performing a session check after removing %u dead handles", dead_handles);
+
         __sweep_check_session_sweep(session, now);
 
         /* Remember the last sweep time. */

src/include/config.h
@@ -288,6 +288,7 @@ extern const char __WT_CONFIG_CHOICE_split_7[];
 extern const char __WT_CONFIG_CHOICE_split_8[];
 extern const char __WT_CONFIG_CHOICE_stable[];
 extern const char __WT_CONFIG_CHOICE_stable_timestamp[];
+extern const char __WT_CONFIG_CHOICE_sweep[];
 extern const char __WT_CONFIG_CHOICE_temporary[];
 extern const char __WT_CONFIG_CHOICE_thread_group[];
 extern const char __WT_CONFIG_CHOICE_tiered[];

src/include/verbose.h
@@ -48,6 +48,7 @@
     "WT_VERB_RECOVERY_PROGRESS", \
     "WT_VERB_RTS", \
     "WT_VERB_SALVAGE", \
+    "WT_VERB_SWEEP", \
     "WT_VERB_SHARED_CACHE", \
     "WT_VERB_SPLIT", \
     "WT_VERB_TEMPORARY", \

src/include/wiredtiger.in
@@ -2418,8 +2418,8 @@ struct __wt_connection {
      * "history_store_activity"\, \c "live_restore"\, \c "live_restore_progress"\, \c "log"\, \c
      * "metadata"\, \c "mutex"\, \c "out_of_order"\, \c "overflow"\, \c "prefetch"\, \c "read"\, \c
      * "reconcile"\, \c "recovery"\, \c "recovery_progress"\, \c "rts"\, \c "salvage"\, \c
-     * "shared_cache"\, \c "split"\, \c "temporary"\, \c "thread_group"\, \c "tiered"\, \c
-     * "timestamp"\, \c "transaction"\, \c "verify"\, \c "version"\, \c "write"; default \c [].}
+     * "shared_cache"\, \c "split"\, \c "sweep"\, \c "temporary"\, \c "thread_group"\, \c "tiered"\,
+     * \c "timestamp"\, \c "transaction"\, \c "verify"\, \c "version"\, \c "write"; default \c [].}
      * @configend
      * @errors
      */
@@ -3379,9 +3379,9 @@ struct __wt_connection {
  * "generation"\, \c "handleops"\, \c "history_store"\, \c "history_store_activity"\, \c
  * "live_restore"\, \c "live_restore_progress"\, \c "log"\, \c "metadata"\, \c "mutex"\, \c
  * "out_of_order"\, \c "overflow"\, \c "prefetch"\, \c "read"\, \c "reconcile"\, \c "recovery"\, \c
- * "recovery_progress"\, \c "rts"\, \c "salvage"\, \c "shared_cache"\, \c "split"\, \c "temporary"\,
- * \c "thread_group"\, \c "tiered"\, \c "timestamp"\, \c "transaction"\, \c "verify"\, \c
- * "version"\, \c "write"; default \c [].}
+ * "recovery_progress"\, \c "rts"\, \c "salvage"\, \c "shared_cache"\, \c "split"\, \c "sweep"\, \c
+ * "temporary"\, \c "thread_group"\, \c "tiered"\, \c "timestamp"\, \c "transaction"\, \c "verify"\,
+ * \c "version"\, \c "write"; default \c [].}
  * @config{verify_metadata, open connection and verify any WiredTiger metadata.  Not supported when
  * opening a connection from a backup.  This API allows verification and detection of corruption in
  * WiredTiger metadata., a boolean flag; default \c false.}
@@ -8717,6 +8717,7 @@ typedef enum {
     WT_VERB_RECOVERY_PROGRESS,    /*!< Recovery progress messages. */
     WT_VERB_RTS,                  /*!< RTS messages. */
     WT_VERB_SALVAGE,              /*!< Salvage messages. */
+    WT_VERB_SWEEP,
     WT_VERB_SHARED_CACHE,
     WT_VERB_SPLIT,
     WT_VERB_TEMPORARY,

test/suite/test_sweep01.py
@@ -44,7 +44,8 @@ class test_sweep01(wttest.WiredTigerTestCase, suite_subprocess):
     numkv = 1000
     conn_config = 'file_manager=(close_handle_minimum=0,' + \
                   'close_idle_time=3,close_scan_interval=1),' + \
-                  'statistics=(fast),operation_tracking=(enabled=false),'
+                  'statistics=(fast),operation_tracking=(enabled=false),' + \
+                  'verbose=(sweep:2)'
 
     types = [
         ('row', dict(tabletype='row',

test/suite/test_sweep03.py
@@ -44,7 +44,8 @@ class test_sweep03(wttest.WiredTigerTestCase, suite_subprocess):
     numkv = 100
     conn_config = 'file_manager=(close_handle_minimum=10,' + \
                   'close_idle_time=0,close_scan_interval=1),' + \
-                  'statistics=(fast),'
+                  'statistics=(fast),' + \
+                  'verbose=(sweep:3)'
 
     types = [
         ('row', dict(tabletype='row',
@@ -99,7 +100,7 @@ class test_sweep03(wttest.WiredTigerTestCase, suite_subprocess):
         stat_cursor = self.session.open_cursor('statistics:', None, None)
         close1 = stat_cursor[stat.conn.dh_sweep_dead_close][2]
         stat_cursor.close()
-
+        self.ignoreStdoutPatternIfExists('sweep-server')
         # We expect nothing to have been closed.
         self.assertEqual(close1, 0)
 
@@ -137,6 +138,7 @@ class test_sweep03(wttest.WiredTigerTestCase, suite_subprocess):
         self.assertEqual(close2, 1)
         # Ensure that any space was reclaimed from cache.
         self.assertLess(cache2, cache1)
+        self.ignoreStdoutPatternIfExists('sweep-server')
 
     @wttest.skip_for_hook("tiered", "Fails with tiered storage")
     def test_disable_idle_timeout_drop(self):
@@ -170,6 +172,7 @@ class test_sweep03(wttest.WiredTigerTestCase, suite_subprocess):
         self.assertEqual(close2, close1)
         # Ensure that any space was reclaimed from cache.
         self.assertLess(cache2, cache1)
+        self.ignoreStdoutPatternIfExists('sweep-server')
 
     # FIXME - WT11133 Uncomment and re-enable this test after fixing this ticket.
     # def test_force_drop_and_sweep(self):

test/suite/test_sweep04.py
@@ -80,7 +80,8 @@ class test_sweep04(wttest.WiredTigerTestCase):
 
     conn_config = 'file_manager=(close_handle_minimum=0,' + \
                   'close_idle_time=3,close_scan_interval=1),' + \
-                  'statistics=(fast),operation_tracking=(enabled=false),'
+                  'statistics=(fast),operation_tracking=(enabled=false),' + \
+                  'verbose=(sweep:2)'
 
     create_params = 'key_format=i,value_format=i'
 

test/suite/test_sweep05.py
@@ -37,7 +37,8 @@ class test_sweep05(wttest.WiredTigerTestCase):
     '''
     Test detection of sessions without recent session sweep.
     '''
-    conn_config = 'statistics=(all)'
+    conn_config = 'statistics=(all),' + \
+                  'verbose=(sweep:3)'
     create_params = 'key_format=i,value_format=i'
     table_numkv = 10
     table_uri_format = 'table:test_sweep05_%s'

test/suite/test_sweep06.py
@@ -41,7 +41,8 @@ class test_sweep06(wttest.WiredTigerTestCase, suite_subprocess):
     tablebase = 'test_sweep06'
     uri = 'table:' + tablebase
     conn_config = 'file_manager=(close_handle_minimum=0,' + \
-                  'close_idle_time=60,close_scan_interval=30),session_max=530'
+                  'close_idle_time=60,close_scan_interval=30),session_max=530,' + \
+                  'verbose=(sweep:3)'
 
     cursor_caching = [
         ('cursor_caching_disabled', dict(cursor_caching=False)),
@@ -89,3 +90,7 @@ class test_sweep06(wttest.WiredTigerTestCase, suite_subprocess):
         # The expectation is that no dhandles have been closed.
         self.assertEqual(close1, 0)
         self.assertEqual(close2, 0)
+
+        # We enabled verbose log level DEBUG_3 in this test to catch an invalid pointer in dhandle.
+        # However, this also causes the log line 'session dhandle name' to appear, which we want to ignore.
+        self.ignoreStdoutPatternIfExists('sweep-server')

test/suite/test_verbose04.py
@@ -83,6 +83,7 @@ class test_verbose04(test_verbose_base):
       'WT_VERB_SALVAGE',
       'WT_VERB_SHARED_CACHE',
       'WT_VERB_SPLIT',
+      'WT_VERB_SWEEP',
       'WT_VERB_TEMPORARY',
       'WT_VERB_THREAD_GROUP',
       'WT_VERB_TIMESTAMP',