26f67a6b39aa6038bbcf68de90099e92c2388ea0
Linh Tran <linh.tran@mongodb.com>
2025-06-23 02:00:19 +0000

    update WT_UPDATE_SIZE

--- src/include/btmem.h
+++ src/include/btmem.h
@@ -1537,12 +1537,12 @@ struct __wt_update {
     wt_timestamp_t durable_ts; /* timestamps */
     wt_timestamp_t start_ts;
 
-    /* 
+    /*
      * Prepared transaction fields
      */
     uint64_t prepared_id;
     wt_timestamp_t prepare_ts;
-    
+
     /*
      * The durable timestamp of the previous update in the update chain. This timestamp is used for
      * diagnostic checks only, and could be removed to reduce the size of the structure should that
@@ -1619,7 +1619,7 @@ struct __wt_update {
  * WT_UPDATE_SIZE is the expected structure size excluding the payload data -- we verify the build
  * to ensure the compiler hasn't inserted padding.
  */
-#define WT_UPDATE_SIZE 47
+#define WT_UPDATE_SIZE 63
 
 /*
  * If there is no value, ensure that the memory allocation size matches that returned by sizeof().
