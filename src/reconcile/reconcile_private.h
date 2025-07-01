f0b6e92ff325c66bec2f345341d5ce35d4387bc8
Chenhao Qu <chenhao.qu@mongodb.com>
2025-07-01 06:43:55 +0000

    Address unticketed comments

--- src/reconcile/reconcile_private.h
+++ src/reconcile/reconcile_private.h
@@ -176,9 +176,6 @@ struct __wti_reconcile {
     /* Track the pinned timestamp at the time reconciliation started. */
     wt_timestamp_t rec_start_pinned_ts;
 
-    /* Track the pinned stable timestamp at the time reconciliation started. */
-    wt_timestamp_t rec_start_pinned_stable_ts;
-
     /* Track the prune timestamp at the time reconciliation started. */
     wt_timestamp_t rec_prune_timestamp;
 
@@ -455,8 +452,10 @@ typedef struct {
       F_ISSET(&S2C(session)->disaggregated_storage, WT_DISAGG_LEAF_PAGE_DELTA) && \
       (r)->multi_next == 1 && !r->ovfl_items && WT_REC_RESULT_SINGLE_PAGE((session), (r))
 
-/* Called when building the internal page image to indicate should we start to build a delta for the
- * page. We are still building so multi_next should still be 0 instead of 1. */
+/*
+ * Called when building the internal page image to indicate should we start to build a delta for the
+ * page. We are still building so multi_next should still be 0 instead of 1.
+ */
 #define WT_BUILD_DELTA_INT(session, r)                                                   \
     F_ISSET(S2BT(session), WT_BTREE_DISAGGREGATED) &&                                    \
       F_ISSET(&S2C(session)->disaggregated_storage, WT_DISAGG_INTERNAL_PAGE_DELTA) &&    \
