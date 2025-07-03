ff6ac0b8237032b3a19b38cf0a9e66f2bb6224cb
Chenhao Qu <chenhao.qu@mongodb.com>
2025-07-03 01:39:31 +0000

    Uncomment a test

--- test/suite/test_layered38.py
+++ test/suite/test_layered38.py
@@ -77,63 +77,63 @@ class test_layered38(wttest.WiredTigerTestCase, DisaggConfigMixin):
             session.rollback_transaction()
         return count
 
-    # def test_gc_ingest_table(self):
-    #     # Create the oplog
-    #     oplog = Oplog(value_size=500)
+    def test_gc_ingest_table(self):
+        # Create the oplog
+        oplog = Oplog(value_size=500)
 
-    #     # Create the table on leader and tell oplog about it
-    #     self.session.create(self.uri, "allocation_size=512,leaf_page_max=512,key_format=S,value_format=S")
-    #     t = oplog.add_uri(self.uri)
+        # Create the table on leader and tell oplog about it
+        self.session.create(self.uri, "allocation_size=512,leaf_page_max=512,key_format=S,value_format=S")
+        t = oplog.add_uri(self.uri)
 
-    #     # Create the follower and create its table
-    #     # To keep this test relatively easy, we're only using a single URI.
-    #     conn_follow = self.wiredtiger_open('follower', self.extensionsConfig() + self.conn_base_config + 'disaggregated=(role="follower")')
-    #     session_follow = conn_follow.open_session('')
-    #     session_follow.create(self.uri, "allocation_size=512,leaf_page_max=512,key_format=S,value_format=S")
+        # Create the follower and create its table
+        # To keep this test relatively easy, we're only using a single URI.
+        conn_follow = self.wiredtiger_open('follower', self.extensionsConfig() + self.conn_base_config + 'disaggregated=(role="follower")')
+        session_follow = conn_follow.open_session('')
+        session_follow.create(self.uri, "allocation_size=512,leaf_page_max=512,key_format=S,value_format=S")
 
-    #     # Load the data for oplog
-    #     oplog.insert(t, self.nitems)
+        # Load the data for oplog
+        oplog.insert(t, self.nitems)
 
-    #     # Apply it to the leader WT.
-    #     oplog.apply(self, self.session, 0, self.nitems)
-    #     oplog.check(self, self.session, 0, self.nitems)
+        # Apply it to the leader WT.
+        oplog.apply(self, self.session, 0, self.nitems)
+        oplog.check(self, self.session, 0, self.nitems)
 
-    #     ts = oplog.last_timestamp()
-    #     self.conn.set_timestamp(f'stable_timestamp={self.timestamp_str(ts)}')
+        ts = oplog.last_timestamp()
+        self.conn.set_timestamp(f'stable_timestamp={self.timestamp_str(ts)}')
 
-    #     # On the follower -
-    #     # Apply all the entries to follower
-    #     oplog.apply(self, session_follow, 0, self.nitems)
-    #     oplog.check(self, session_follow, 0, self.nitems)
+        # On the follower -
+        # Apply all the entries to follower
+        oplog.apply(self, session_follow, 0, self.nitems)
+        oplog.check(self, session_follow, 0, self.nitems)
 
-    #     # Ensure everything is in the ingest table
-    #     count = self.count_ingest(session_follow)
-    #     self.assertEqual(count, self.nitems)
+        # Ensure everything is in the ingest table
+        count = self.count_ingest(session_follow)
+        self.assertEqual(count, self.nitems)
 
-    #     # Hold a cursor open on the layered table, and on the ingest as well.
-    #     session_follow2 = conn_follow.open_session('')
-    #     hold_cursor = session_follow2.open_cursor(self.uri)
-    #     hold_cursor.next()
+        # Hold a cursor open on the layered table, and on the ingest as well.
+        session_follow2 = conn_follow.open_session('')
+        hold_cursor = session_follow2.open_cursor(self.uri)
+        hold_cursor.next()
 
-    #     # Take a checkpoint and advance it, make sure everything is still good
-    #     self.session.checkpoint()
-    #     self.disagg_advance_checkpoint(conn_follow)
-    #     oplog.check(self, session_follow, 0, self.nitems)
+        # Take a checkpoint and advance it, make sure everything is still good
+        self.session.checkpoint()
+        self.disagg_advance_checkpoint(conn_follow)
+        oplog.check(self, session_follow, 0, self.nitems)
 
-    #     # At this point, everything in the ingest table is redundant, as it's
-    #     # also in the stable table on the follower. However, it cannot be removed
-    #     # as there is a cursor open.
-    #     self.evict_ingest(session_follow, ts)
-    #     count = self.count_ingest(session_follow)
-    #     self.assertEqual(count, self.nitems)
+        # At this point, everything in the ingest table is redundant, as it's
+        # also in the stable table on the follower. However, it cannot be removed
+        # as there is a cursor open.
+        self.evict_ingest(session_follow, ts)
+        count = self.count_ingest(session_follow)
+        self.assertEqual(count, self.nitems)
 
-    #     # Close the cursor held open.
-    #     hold_cursor.close()
+        # Close the cursor held open.
+        hold_cursor.close()
 
-    #     # Now eviction should remove all the items from the ingest table.
-    #     self.evict_ingest(session_follow, ts)
-    #     count = self.count_ingest(session_follow)
-    #     self.assertEqual(count, 0)
+        # Now eviction should remove all the items from the ingest table.
+        self.evict_ingest(session_follow, ts)
+        count = self.count_ingest(session_follow)
+        self.assertEqual(count, 0)
 
     def test_gc_ingest_table_with_remove(self):
         # Create the oplog
