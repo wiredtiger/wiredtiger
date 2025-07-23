5b11dd85c265a4d8ce1c9a8063068d685331bdcc
Mariam Mojid <mariammojid@gmail.com>
2025-07-23 22:32:51 +0000

    crash prior to log flush when logging enabled and add FIXME

--- test/model/src/driver/kv_workload_generator.cpp
+++ test/model/src/driver/kv_workload_generator.cpp
@@ -60,13 +60,7 @@ kv_workload_generator_spec::kv_workload_generator_spec()
     truncate = 0.005;
 
     checkpoint = 0.02;
-    /*
-     * FIXME-WT-14395 - The last crash point in the checkpoint code (used by test/model's
-     * checkpoint_crash) triggers after the checkpoint txn commits but before the turtle file is
-     * updated. This causes inconsistent behavior when logging is enabled. For now checkpoint_crash
-     * has been disabled. Once resolved, return checkpoint_crash probability value to 0.002.
-     */
-    checkpoint_crash = 0;
+    checkpoint_crash = 0.002;
     crash = 0.002;
     evict = 0.1;
     restart = 0.002;
