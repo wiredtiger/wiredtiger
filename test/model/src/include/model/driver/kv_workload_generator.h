215a2a740f90ca055dfbc30192b8a8e1d21f382a
Will Korteland <will.korteland@mongodb.com>
2025-07-24 13:28:46 +1000

    merge test/model

--- test/model/src/include/model/driver/kv_workload_generator.h
+++ test/model/src/include/model/driver/kv_workload_generator.h
@@ -47,6 +47,9 @@ namespace model {
  */
 struct kv_workload_generator_spec {
 
+    /* Top-level database configuration. */
+    float disaggregated;
+
     /* The minimum and maximum number of tables. */
     size_t min_tables;
     size_t max_tables;
@@ -68,9 +71,6 @@ struct kv_workload_generator_spec {
 
     /* The probability of allowing the use of "set commit timestamp" in a transaction. */
     float use_set_commit_timestamp;
-
-    /* The probability of running with connection level logging */
-    float conn_logging;
 
     /* Probabilities of operations within a transaction. */
     float finish_transaction; /* Commit, prepare, or rollback. */
@@ -450,17 +450,10 @@ public:
     }
 
     static std::string
-    generate_stress_configurations(uint64_t seed = 0)
+    generate_configurations(uint64_t seed = 0)
     {
         kv_workload_generator generator(_default_spec, seed);
-        return generator.generate_connection_stress_config();
-    }
-
-    static std::string
-    generate_log_configurations(uint64_t seed = 0)
-    {
-        kv_workload_generator generator(_default_spec, seed);
-        return generator.generate_connection_log_config();
+        return generator.generate_connection_config();
     }
 
 protected:
@@ -506,16 +499,10 @@ protected:
     void create_table();
 
     /*
-     * kv_workload_generator::generate_connection_stress_config --
-     *     Generate random time stress configurations.
+     * kv_workload_generator::generate_connection_config --
+     *     Generate random WiredTiger timing stress configurations.
      */
-    std::string generate_connection_stress_config();
-
-    /*
-     * kv_workload_generator::generate_connection_log_config --
-     *     Generate random WiredTiger log configurations.
-     */
-    std::string generate_connection_log_config();
+    std::string generate_connection_config();
 
     /*
      * kv_workload_generator::generate_key --
@@ -552,6 +539,7 @@ private:
     kv_workload &_workload;
 
     kv_workload_generator_spec _spec;
+    kv_database_config _database_config;
     random _random;
 
     table_id_t _last_table_id;
