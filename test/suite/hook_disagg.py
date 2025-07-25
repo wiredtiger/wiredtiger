455ec01d201e0f3f6d905441d849e90541c27c18
jchen <29520270+jiechenbo@users.noreply.github.com>
2025-07-25 12:17:19 +1000

    WT-14542 Enable caching for layered cursors and their constituents in WT (#12158)
    The PR adds code to track how long applications and internal threads are
    spending opening cursors. It also enables cursor caching for layered
    cursors and it's constituent cursors (ingest and shared cursors).
    
    Here are the technical change to support cursor layered cursors:
    - Update the cursor->close() to accomodate for new cursor cache
    - Implement cursor->reopen() and cursor->cache() functions.
    - Deal with session dhandle referencing
    
    ---------
    
    Co-authored-by: Alexander Gorrod <alexander.gorrod@mongodb.com>

--- test/suite/hook_disagg.py
+++ test/suite/hook_disagg.py
@@ -267,6 +267,11 @@ def session_create_replace(orig_session_create, session_self, uri, config):
     ret = orig_session_create(session_self, uri, config_str)
     return ret
 
+# Called to replace Session.drop
+def session_drop_replace(orig_session_drop, session_self, uri, config):
+    uri = replace_uri(uri)
+    return orig_session_drop(session_self, uri, config)
+
 # Called to replace Session.open_cursor.  We skip calls that do backup
 # as that is not yet supported in disaggregated storage.
 def session_open_cursor_replace(orig_session_open_cursor, session_self, uri, dupcursor, config):
@@ -363,6 +368,10 @@ class DisaggHookCreator(wthooks.WiredTigerHookCreator):
         orig_session_create = self.Session['create']
         self.Session['create'] =  (wthooks.HOOK_REPLACE, lambda s, uri, config=None:
           session_create_replace(orig_session_create, s, uri, config))
+
+        orig_session_drop = self.Session['drop']
+        self.Session['drop'] =  (wthooks.HOOK_REPLACE, lambda s, uri, config=None:
+          session_drop_replace(orig_session_drop, s, uri, config))
 
         orig_session_open_cursor = self.Session['open_cursor']
         self.Session['open_cursor'] = (wthooks.HOOK_REPLACE, lambda s, uri, todup=None, config=None:
