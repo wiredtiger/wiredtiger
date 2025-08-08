94428154ab780a0d9945d0f6a0a99b3aef8f9a74
Yury Ershov <yuriy.ershov@gmail.com>
2025-08-08 15:35:16 +1000

    WT-14517 Check for turtle and metadata files on startup in disagg mode (#12218)
    When starting up, the turtle and metadata files can be read before WT
    switches to Disagg mode. This PR ensures that doesn't happen by either
    failing to start or automatically deleting those files.

--- src/include/extern.h
+++ src/include/extern.h
@@ -1470,6 +1470,8 @@ extern int __wti_delete_page_instantiate(WT_SESSION_IMPL *session, WT_REF *ref)
   WT_GCC_FUNC_DECL_ATTRIBUTE((warn_unused_result));
 extern int __wti_desc_write(WT_SESSION_IMPL *session, WT_FH *fh, uint32_t allocsize)
   WT_GCC_FUNC_DECL_ATTRIBUTE((warn_unused_result));
+extern int __wti_disagg_check_local_files(WT_SESSION_IMPL *session, const char *cfg[])
+  WT_GCC_FUNC_DECL_ATTRIBUTE((warn_unused_result));
 extern int __wti_disagg_conn_config(WT_SESSION_IMPL *session, const char **cfg, bool reconfig)
   WT_GCC_FUNC_DECL_ATTRIBUTE((warn_unused_result));
 extern int __wti_disagg_destroy(WT_SESSION_IMPL *session)
