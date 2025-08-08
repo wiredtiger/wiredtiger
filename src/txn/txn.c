f67c6841ed27f142e32e45ba2b08b4d73e073547
zunyi.liu <zunyi.liu@mongodb.com>
2025-08-08 00:41:00 +0000

    Bug fix

--- src/txn/txn.c
+++ src/txn/txn.c
@@ -2270,7 +2270,7 @@ __wt_txn_rollback(WT_SESSION_IMPL *session, const char *cfg[], bool api_call)
             }
             break;
         case WT_TXN_OP_REF_DELETE:
-            WT_TRET(__wt_delete_page_rollback(session, op->u.ref));
+            WT_TRET(__wt_delete_page_rollback(session, op));
             break;
         case WT_TXN_OP_TRUNCATE_COL:
         case WT_TXN_OP_TRUNCATE_ROW:
