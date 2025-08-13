31553ae54fd255a15ca8f4e8ff597f4cd9253bb3
Monica Ng <monica.ng@mongodb.com>
2025-08-13 00:39:03 +0000

    Update flag name

--- src/include/cell.h
+++ src/include/cell.h
@@ -257,7 +257,7 @@ struct __wt_cell_unpack_delta_leaf_kv {
     WT_ITEM delta_value_data;
 
 /* AUTOMATIC FLAG VALUE GENERATION START 0 */
-#define WT_VALUE_IS_DELETE 0x1u
+#define WT_DELTA_LEAF_IS_DELETE 0x1u
     /* AUTOMATIC FLAG VALUE GENERATION STOP 8 */
     uint8_t flags;
 };
