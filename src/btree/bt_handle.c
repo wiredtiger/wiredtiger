42083104be11495adf96f7eb50c146ca3509d160
Ivan Kochin <ivan.kochin@mongodb.com>
2025-08-14 11:08:06 +1000

    WT-15158 Avoid setting ingest_btree->prune_timestamp during initialization (#12242)
    Currently during the ingest table initialization we set
    `prune_timestamp` to the last known checkpoint timestamp. According to
    the current logic it could cause the following assertion failure:
    ```c
    __layered_update_gc_ingest_tables_prune_timestamps(...) {
         WT_ASSERT(session, prune_timestamp >= btree->prune_timestamp);
    ``` 
    
    It happens because we always update timestamp to the last checkpoint's
    timestamp for this table which is in use and it could be newer than the
    last known checkpoint timestamp.
    
    This logic does require a redesign which is the main scope for WT-15192
    Since this issue blocks other activities it was decided to merge it
    without a regression test, the regression test would be provided as a
    separate PR as well (WT-15191)

--- src/btree/bt_handle.c
+++ src/btree/bt_handle.c
@@ -680,9 +680,9 @@ __btree_conf(WT_SESSION_IMPL *session, WT_CKPT *ckpt, bool is_ckpt)
     if (F_ISSET(session, WT_SESSION_IMPORT))
         btree->modified = true;
 
+    /* FIXME-WT-15192: Consider setting `prune_timestamp` to `last_checkpoint_timestamp` */
     if (F_ISSET(btree, WT_BTREE_GARBAGE_COLLECT))
-        WT_ACQUIRE_READ(
-          btree->prune_timestamp, conn->disaggregated_storage.last_checkpoint_timestamp);
+        btree->prune_timestamp = WT_TS_NONE;
 
     return (0);
 }
