fc082cfb6eeeda0e99219c47eafc1dd5c9f5e452
Linh Tran <linh.tran@mongodb.com>
2025-08-18 01:37:48 +0000

    remove printing config

--- src/log/log_mgr.c
+++ src/log/log_mgr.c
@@ -245,7 +245,7 @@ __wt_logmgr_config(WT_SESSION_IMPL *session, const char **cfg, bool reconfig)
               session, EINVAL, "In-memory configuration incompatible with log=(enabled=true)");
         if (__wt_config_gets(session, cfg, "disaggregated.page_log", &cval) == 0 && cval.len != 0)
             WT_RET_MSG(session, EINVAL,
-              "Disaggregated storage is incompatible with log=(enabled=true), cfg=%s", *cfg);
+              "Disaggregated storage is incompatible with log=(enabled=true)");
     }
 
     if (enabled)
