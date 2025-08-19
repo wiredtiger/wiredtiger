a7f267c800089afa96685dd47b52fa37b2745ac1
jchen <jie.chen@mongodb.com>
2025-08-19 23:22:06 +0000

    Fix drop issue

---
+++ test/suite/test_drop04.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+#
+# Public Domain 2014-present MongoDB, Inc.
+# Public Domain 2008-2014 WiredTiger, Inc.
+#
+# This is free and unencumbered software released into the public domain.
+#
+# Anyone is free to copy, modify, publish, use, compile, sell, or
+# distribute this software, either in source code form or as a compiled
+# binary, for any purpose, commercial or non-commercial, and by any
+# means.
+#
+# In jurisdictions that recognize copyright laws, the author or authors
+# of this software dedicate any and all copyright interest in the
+# software to the public domain. We make this dedication for the benefit
+# of the public at large and to the detriment of our heirs and
+# successors. We intend this dedication to be an overt act of
+# relinquishment in perpetuity of all present and future rights to this
+# software under copyright law.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+#
+#!/usr/bin/env python
+#
+# Public Domain 2014-present MongoDB, Inc.
+# Public Domain 2008-2014 WiredTiger, Inc.
+#
+# This is free and unencumbered software released into the public domain.
+#
+# Anyone is free to copy, modify, publish, use, compile, sell, or
+# distribute this software, either in source code form or as a compiled
+# binary, for any purpose, commercial or non-commercial, and by any
+# means.
+#
+# In jurisdictions that recognize copyright laws, the author or authors
+# of this software dedicate any and all copyright interest in the
+# software to the public domain. We make this dedication for the benefit
+# of the public at large and to the detriment of our heirs and
+# successors. We intend this dedication to be an overt act of
+# relinquishment in perpetuity of all present and future rights to this
+# software under copyright law.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+
+from test_cc01 import test_cc_base
+import wttest
+
+# test_drop04.py
+# Test dropping a collection on empty logged table.
+class test_drop04(test_cc_base):
+    conn_config = 'log=(enabled=true)'
+    uri = 'table:test_drop04'
+
+    def test_drop_after_bulk_load(self):
+        self.session.create(self.uri, 'key_format=i,value_format=S')
+
+        # Checkpoint the database and wait for the checkpoint cleanup.
+        self.wait_for_cc_to_run()
+        self.prout("Finished checkpoint.")
+
+        for i in range(100):
+            self.prout(f"Iteration {i+1}: drop with force=false should fail.")
+            self.assertTrue(self.raisesBusy(lambda: self.session.drop(self.uri, "force=false")),
+                            "was expecting drop call to fail with EBUSY")
+            self.prout(f"Iteration {i+1}: Exists after unsuccessful drop.")
+            self.session.checkpoint()
+            self.prout(f"Finished checkpoint {i+1}.")
+
+        # Perform an insert, following by checkpoint to let the drop succeed.
+        cursor = self.session.open_cursor(self.uri, None, None)
+        self.session.begin_transaction()
+        cursor[1] = "value1"
+        self.session.commit_transaction()
+        self.session.checkpoint()
+        cursor.close()
+        self.assertEqual(self.session.drop(self.uri), 0)
+
+        self.prout("Done.")
+
+if __name__ == '__main__':
+    wttest.run()
