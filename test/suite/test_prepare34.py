e1bf78a965afba68dfd8ad4c3f6369b2c5be0636
Monica Ng <30430618+mm-ng@users.noreply.github.com>
2025-08-25 18:36:40 +1000

    Revert "WT-15246 Re-enable prepared testing that depends on new delta data fo…"
    This reverts commit b8426de0d46bfad8cbf75cca40aa34cd87c116d7.

--- test/suite/test_prepare34.py
+++ test/suite/test_prepare34.py
@@ -37,6 +37,7 @@ from prepare_util import test_prepare_preserve_prepare_base
 class test_prepare34(test_prepare_preserve_prepare_base):
     uri = 'table:test_prepare34'
 
+    @wttest.skip_for_hook("disagg", "Skip test until cell packing/unpacking is supported for page delta")
     def test_rollback_prepare_modify(self):
         """
         Test that prepared transactions containing modify operations that are rolled back
@@ -120,6 +121,7 @@ class test_prepare34(test_prepare_preserve_prepare_base):
             self.assertEqual(value, cursor[i])
         self.session.rollback_transaction()
 
+    @wttest.skip_for_hook("disagg", "Skip test until cell packing/unpacking is supported for page delta")
     def test_commit_prepare_modify(self):
         """
         Test that prepared transactions containing modify operations that are rolled back
