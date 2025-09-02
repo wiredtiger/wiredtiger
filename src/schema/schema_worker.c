8741e4286b5f23ef6c9f3f7cb67bdbef8de520b6
Mariam Mojid <98868895+mariammojid@users.noreply.github.com>
2025-09-02 11:20:33 +1000

    Update src/schema/schema_worker.c
    Co-authored-by: Etienne Petrel <77254506+etienneptl@users.noreply.github.com>

--- src/schema/schema_worker.c
+++ src/schema/schema_worker.c
@@ -110,8 +110,8 @@ __schema_layered_worker_verify(WT_SESSION_IMPL *session, const char *uri,
               "Verify (layered): %s ingest table on leader cannot be verified. "
               "Ingest contains dirty content or open cursors, which is an invalid state.",
               ingest_uri);
-    }
     WT_ERR(ret);
+    }
 
     WT_WITHOUT_DHANDLE(session,
       ret = __wt_schema_worker(session, stable_uri, file_func, name_func, cfg, open_flags));
