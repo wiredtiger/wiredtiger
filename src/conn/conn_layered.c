3c20e1885fa9e3b0bc746d51792963d8249371a1
Peter Macko <peter.macko@mongodb.com>
2025-09-11 21:38:32 -0400

    Added a FIXME for checking meta_lsn == current_meta_lsn

--- src/conn/conn_layered.c
+++ src/conn/conn_layered.c
@@ -361,6 +361,8 @@ __disagg_pick_up_checkpoint(WT_SESSION_IMPL *session, uint64_t meta_lsn)
           ", new metadata LSN = %" PRIu64,
           current_meta_lsn, meta_lsn);
 
+    /* FIXME-WT-15448 We might also want to add a check for picking up the same checkpoint again. */
+
     /*
      * Part 1: Get the metadata of the shared metadata table and insert it into our metadata table.
      */
