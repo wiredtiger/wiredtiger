bee0dffe1fe61a6bfaa88666126e606e4c5abccf
Chenhao Qu <chenhao.qu@mongodb.com>
2025-09-18 23:34:52 +0000

    Mark the col page clean

--- src/btree/bt_delete.c
+++ src/btree/bt_delete.c
@@ -585,6 +585,8 @@ __instantiate_col_var(WT_SESSION_IMPL *session, WT_REF *ref, WT_PAGE_DELETED *pa
     /* We just read the page and it's still locked. The append list should be empty. */
     WT_ASSERT(session, WT_COL_APPEND(page) == NULL);
 
+    __wt_page_modify_clear(session, page);
+
 err:
     __wt_free(session, upd);
 
@@ -699,8 +701,9 @@ __wti_delete_page_instantiate(WT_SESSION_IMPL *session, WT_REF *ref)
      * We do not need to mark the page dirty here. (It used to be necessary because evicting a clean
      * instantiated page would lose the delete information; but that is no longer the case.) Note
      * though that because VLCS instantiation goes through col_modify it will mark the page dirty
-     * regardless, except in read-only trees where attempts to mark things dirty are ignored. (Row-
-     * store instantiation adds the tombstones by hand and so does not need to mark the page dirty.)
+     * regardless, except in read-only trees where attempts to mark things dirty are ignored.
+     * Therefore, we explicitly mark it as clean. (Row- store instantiation adds the tombstones by
+     * hand and so does not need to mark the page dirty.)
      *
      * Note that partially visible truncates that may need instantiation can appear in read-only
      * trees (whether a read-only open of the live database or via a checkpoint cursor) if they were
