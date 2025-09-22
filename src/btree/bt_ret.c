2b2d1b029a1c73806ca11af1a15ef13c3d401cc7
Chenhao Qu <chenhao.qu@mongodb.com>
2025-09-22 12:46:25 +0000

    Fix col fix case

--- src/btree/bt_ret.c
+++ src/btree/bt_ret.c
@@ -184,6 +184,9 @@ __wt_read_cell_time_window(WT_CURSOR_BTREE *cbt, WT_TIME_WINDOW *tw)
         __read_col_time_window(session, page, WT_COL_PTR(page, &page->pg_var[cbt->slot]), tw);
         break;
     case WT_PAGE_COL_FIX:
+        if (cbt->recno >= cbt->ref->ref_recno + cbt->ref->page->entries)
+            return (false);
+
         return (__col_fix_get_time_window(session, cbt->ref, cbt->recno, tw));
     }
     return (true);
