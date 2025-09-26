a8013aee19f8e3b7f144dd88337ceb29598e6e74
zunyi.liu <zunyi.liu@mongodb.com>
2025-09-26 06:29:34 +0000

    rename layered53 to 54

--- test/suite/test_layered53.py
+++ test/suite/test_layered53.py
@@ -33,11 +33,11 @@ from wiredtiger import stat
 import time
 
 
-# test_layered53.py
+# test_layered54.py
 # Tests that prefix and suffix compression are working properly for page deltas.
 
 @disagg_test_class
-class test_layered53(wttest.WiredTigerTestCase, DisaggConfigMixin):
+class test_layered54(wttest.WiredTigerTestCase, DisaggConfigMixin):
 
     delta = [
         ('write_leaf_only', dict(delta_config='page_delta=(internal_page_delta=false,leaf_page_delta=true)', delta_type='leaf_only')),
@@ -47,10 +47,10 @@ class test_layered53(wttest.WiredTigerTestCase, DisaggConfigMixin):
 
     conn_base_config = 'cache_size=32MB,transaction_sync=(enabled,method=fsync),statistics=(all),statistics_log=(wait=1,json=true,on_close=true),' \
                      + 'disaggregated=(page_log=palm),page_delta=(delta_pct=100),'
-    disagg_storages = gen_disagg_storages('test_layered53', disagg_only = True)
+    disagg_storages = gen_disagg_storages('test_layered54', disagg_only = True)
 
     nrows = 1000
-    uri='file:test_layered53'
+    uri='file:test_layered54'
 
     # Make scenarios for different cloud service providers
     scenarios = make_scenarios(disagg_storages, delta)
