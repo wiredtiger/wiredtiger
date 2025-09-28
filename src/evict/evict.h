be6da931fd1a3f85bd120ab37e9a451022c39dc4
Chenhao Qu <chenhao.qu@mongodb.com>
2025-09-28 23:47:03 +0000

    Log a warning instead of panic

--- src/evict/evict.h
+++ src/evict/evict.h
@@ -178,8 +178,6 @@ static WT_INLINE bool __wt_evict_clean_pressure(WT_SESSION_IMPL *session)
   WT_GCC_FUNC_DECL_ATTRIBUTE((warn_unused_result));
 static WT_INLINE bool __wt_evict_dirty_needed(WT_SESSION_IMPL *session, double *pct_fullp)
   WT_GCC_FUNC_DECL_ATTRIBUTE((warn_unused_result));
-static WT_INLINE bool __wt_evict_needed(WT_SESSION_IMPL *session, bool busy, bool readonly,
-  double *pct_fullp) WT_GCC_FUNC_DECL_ATTRIBUTE((warn_unused_result));
 static WT_INLINE bool __wt_evict_page_is_soon(WT_PAGE *page)
   WT_GCC_FUNC_DECL_ATTRIBUTE((warn_unused_result));
 static WT_INLINE bool __wt_evict_page_is_soon_or_wont_need(WT_PAGE *page)
@@ -187,6 +185,8 @@ static WT_INLINE bool __wt_evict_page_is_soon_or_wont_need(WT_PAGE *page)
 static WT_INLINE int __wt_evict_app_assist_worker_check(
   WT_SESSION_IMPL *session, bool busy, bool readonly, bool interruptible, bool *didworkp)
   WT_GCC_FUNC_DECL_ATTRIBUTE((warn_unused_result));
+static WT_INLINE int __wt_evict_needed(WT_SESSION_IMPL *session, bool busy, bool readonly,
+  bool ignore_updates_dirty, double *pct_fullp) WT_GCC_FUNC_DECL_ATTRIBUTE((warn_unused_result));
 static WT_INLINE void __wt_evict_clear_npos(WT_BTREE *btree);
 static WT_INLINE void __wt_evict_favor_clearing_dirty_cache(WT_SESSION_IMPL *session);
 static WT_INLINE void __wt_evict_inherit_page_state(WT_PAGE *orig_page, WT_PAGE *new_page);
--- src/evict/evict_lru.c
+++ src/evict/evict_lru.c
@@ -3075,7 +3075,7 @@ __wti_evict_app_assist_worker(
 
         /* See if eviction is still needed. */
         double pct_full;
-        if (!__wt_evict_needed(session, busy, readonly, &pct_full) ||
+        if (!__wt_evict_needed(session, busy, readonly, true, &pct_full) ||
           (pct_full < 100.0 &&
             (__wt_atomic_loadv64(&evict->eviction_progress) > initial_progress + max_progress)))
             break;
