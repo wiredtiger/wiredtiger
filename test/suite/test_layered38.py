3e0d709ef432e9eb5414c23b5c98d2205a3ef2e9
Albert Song <albert.song@mongodb.com>
2025-09-30 03:54:39 +0000

    add extra case to verify repeat picking up same checkpoint

--- test/suite/test_layered38.py
+++ test/suite/test_layered38.py
@@ -222,6 +222,10 @@ class test_layered38(wttest.WiredTigerTestCase, DisaggConfigMixin):
         # Close the cursor held open.
         hold_cursor.close()
 
+        # Picking up the same checkpoint should log a message about picking up the same checkpoint.
+        with self.expectedStdoutPattern(".*Picking up the same checkpoint again.*"):
+            self.disagg_advance_checkpoint(conn_follow)
+
         # Push forward the checkpoint again to avoid picking up the same checkpoint twice.
         self.session.checkpoint()
 
