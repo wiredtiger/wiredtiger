ba4c226bf2850d5d4f734b9dc136aeac0382dfe9
Siddhartha8899 <siddhartha.mahajan8899@gmail.com>
2025-10-01 05:20:48 +0000

    WT-15634 Remove closed  tickets from the code

--- test/model/src/driver/kv_workload_generator.cpp
+++ test/model/src/driver/kv_workload_generator.cpp
@@ -39,7 +39,7 @@ namespace model {
  */
 kv_workload_generator_spec::kv_workload_generator_spec()
 {
-    disaggregated = 0; /* FIXME-WT-15042 Enable this when ready. */
+    disaggregated = 0;
 
     min_tables = 3;
     max_tables = 10;
