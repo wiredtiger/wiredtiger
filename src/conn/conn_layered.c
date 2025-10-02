5b7e1c1694b0baae3a00ffeafb52309fb8467325
Peter Macko <peter.macko@mongodb.com>
2025-10-01 21:12:41 -0400

    WT-15644 Add verbose logging for disagg checkpoints

--- src/conn/conn_layered.c
+++ src/conn/conn_layered.c
@@ -84,6 +84,9 @@ __layered_create_missing_ingest_table(
 
     WT_WITH_SCHEMA_LOCK(session, ret = __wt_schema_create(session, uri, ingest_config->data));
 
+    __wt_verbose_debug2(session, WT_VERB_DISAGGREGATED_STORAGE,
+      "Created missing ingest table \"%s\" from \"%s\"", uri, layered_cfg);
+
 err:
     __wt_scr_free(session, &ingest_config);
     return (ret);
@@ -161,6 +164,8 @@ __layered_create_missing_stable_tables(WT_SESSION_IMPL *session)
             /* Ensure that we properly handle empty tables. */
             WT_ERR(__wt_disagg_copy_metadata_later(
               internal_session, stable_uri, layered_uri + strlen("layered:")));
+            __wt_verbose_debug2(session, WT_VERB_DISAGGREGATED_STORAGE,
+              "Created missing stable table \"%s\" from \"%s\"", stable_uri, layered_uri);
         }
 
         __wt_free(session, stable_uri);
@@ -276,7 +281,7 @@ __wt_disagg_put_checkpoint_meta(WT_SESSION_IMPL *session, const char *checkpoint
     WT_DECL_RET;
     WT_DISAGGREGATED_STORAGE *disagg;
     uint64_t lsn;
-    char *checkpoint_root_copy;
+    char *checkpoint_root_copy, ts_string[WT_TS_INT_STRING_SIZE];
 
     buf = NULL;
     checkpoint_root_copy = NULL;
@@ -311,6 +316,13 @@ __wt_disagg_put_checkpoint_meta(WT_SESSION_IMPL *session, const char *checkpoint
     WT_RELEASE_WRITE(disagg->last_checkpoint_meta_lsn, lsn);
     WT_RELEASE_WRITE(disagg->last_checkpoint_timestamp, checkpoint_timestamp);
 
+    __wt_verbose_debug2(session, WT_VERB_DISAGGREGATED_STORAGE,
+      "Wrote disaggregated checkpoint metadata: lsn=%" PRIu64 ", timestamp=%" PRIu64
+      " %s"
+      ", root=\"%s\"",
+      lsn, checkpoint_timestamp, __wt_timestamp_to_string(checkpoint_timestamp, ts_string),
+      checkpoint_root_copy);
+
     __wt_free(session, disagg->last_checkpoint_root);
     disagg->last_checkpoint_root = checkpoint_root_copy;
     checkpoint_root_copy = NULL;
@@ -337,6 +349,7 @@ __disagg_pick_up_checkpoint(WT_SESSION_IMPL *session, uint64_t meta_lsn)
     size_t len, metadata_value_cfg_len;
     uint64_t checkpoint_timestamp, current_meta_lsn;
     char *buf, *cfg_ret, *checkpoint_config, *root, *metadata_value_cfg, *layered_ingest_uri;
+    char ts_string[WT_TS_INT_STRING_SIZE];
     const char *cfg[3], *current_value, *metadata_key, *metadata_value;
 
     conn = S2C(session);
@@ -370,6 +383,9 @@ __disagg_pick_up_checkpoint(WT_SESSION_IMPL *session, uint64_t meta_lsn)
      * Part 1: Get the metadata of the shared metadata table and insert it into our metadata table.
      */
 
+    __wt_verbose_debug1(session, WT_VERB_DISAGGREGATED_STORAGE,
+      "Picking up disaggregated storage checkpoint: metadata_lsn=%" PRIu64, meta_lsn);
+
     /* Read the checkpoint metadata of the shared metadata table from the special metadata page. */
     WT_ERR_MSG_CHK(session,
       __disagg_get_meta(session, WT_DISAGG_METADATA_MAIN_PAGE_ID, meta_lsn, &item),
@@ -399,6 +415,13 @@ __disagg_pick_up_checkpoint(WT_SESSION_IMPL *session, uint64_t meta_lsn)
     metadata_key = WT_DISAGG_METADATA_URI;
     metadata_value = root = buf;
 
+    __wt_verbose_debug2(session, WT_VERB_DISAGGREGATED_STORAGE,
+      "Picking up disaggregated storage checkpoint: metadata_lsn=%" PRIu64 ", timestamp=%" PRIu64
+      " %s"
+      ", root=\"%s\"",
+      meta_lsn, checkpoint_timestamp, __wt_timestamp_to_string(checkpoint_timestamp, ts_string),
+      root);
+
     /* We need an internal session when modifying metadata. */
     WT_ERR(__wt_open_internal_session(conn, "checkpoint-pick-up", false, 0, 0, &internal_session));
 
@@ -423,6 +446,10 @@ __disagg_pick_up_checkpoint(WT_SESSION_IMPL *session, uint64_t meta_lsn)
     /* Put our new config in */
     WT_ERR(__wt_metadata_insert(internal_session, metadata_key, cfg_ret));
     __wt_free(session, cfg_ret);
+
+    __wt_verbose_debug2(session, WT_VERB_DISAGGREGATED_STORAGE,
+      "Updated the local metadata for key \"%s\" to include the new checkpoint: \"%s\"",
+      metadata_key, metadata_value);
 
     /*
      * Part 2: Get the metadata for other tables from the shared metadata table.
@@ -476,6 +503,10 @@ __disagg_pick_up_checkpoint(WT_SESSION_IMPL *session, uint64_t meta_lsn)
             WT_ERR_MSG_CHK(session, md_cursor->insert(md_cursor),
               "Failed to insert metadata for key \"%s\"", metadata_key);
 
+            __wt_verbose_debug2(session, WT_VERB_DISAGGREGATED_STORAGE,
+              "Updated the local metadata for key \"%s\" to include new checkpoint: \"%.*s\"",
+              metadata_key, (int)cval.len, cval.str);
+
             /*
              * Mark any matching data handles to be out of date. Any new opens will get the new
              * metadata.
@@ -511,6 +542,10 @@ __disagg_pick_up_checkpoint(WT_SESSION_IMPL *session, uint64_t meta_lsn)
             md_cursor->set_value(md_cursor, metadata_value);
             WT_ERR_MSG_CHK(session, md_cursor->insert(md_cursor),
               "Failed to insert metadata for key \"%s\"", metadata_key);
+
+            __wt_verbose_debug2(session, WT_VERB_DISAGGREGATED_STORAGE,
+              "Inserted new key to the local metadata \"%s\": \"%s\"", metadata_key,
+              metadata_value);
         }
     }
     WT_ERR_NOTFOUND_OK(ret, false);
@@ -545,6 +580,10 @@ __disagg_pick_up_checkpoint(WT_SESSION_IMPL *session, uint64_t meta_lsn)
     /* Update ingest tables' prune timestamps. */
     WT_ERR_MSG_CHK(session, __layered_update_gc_ingest_tables_prune_timestamps(internal_session),
       "Updating prune timestamp failed");
+
+    /* Log the completion of the checkpoint pick-up. */
+    __wt_verbose_debug1(session, WT_VERB_DISAGGREGATED_STORAGE,
+      "Finished picking up disaggregated storage checkpoint: metadata_lsn=%" PRIu64, meta_lsn);
 
 err:
     if (ret == 0)
@@ -1084,6 +1123,9 @@ __disagg_begin_checkpoint(WT_SESSION_IMPL *session)
     WT_RET(disagg->npage_log->page_log->pl_begin_checkpoint(
       disagg->npage_log->page_log, &session->iface, 0));
 
+    __wt_verbose_debug2(session, WT_VERB_DISAGGREGATED_STORAGE,
+      "Begin next disaggregated storage checkpoint: num_meta_put=%" PRIu64, disagg->num_meta_put);
+
     /* Store is sufficient because updates are protected by the checkpoint lock. */
     disagg->num_meta_put_at_ckpt_begin = disagg->num_meta_put;
     return (0);
@@ -1525,6 +1567,7 @@ __wt_disagg_advance_checkpoint(WT_SESSION_IMPL *session, bool ckpt_success)
     WT_DISAGGREGATED_STORAGE *disagg;
     wt_timestamp_t checkpoint_timestamp;
     uint64_t meta_lsn;
+    char ts_string[WT_TS_INT_STRING_SIZE];
 
     conn = S2C(session);
     disagg = &conn->disaggregated_storage;
@@ -1550,6 +1593,11 @@ __wt_disagg_advance_checkpoint(WT_SESSION_IMPL *session, bool ckpt_success)
           &session->iface, 0, (uint64_t)checkpoint_timestamp, meta, NULL));
         WT_RELEASE_WRITE(
           conn->disaggregated_storage.last_checkpoint_timestamp, checkpoint_timestamp);
+
+        __wt_verbose_debug1(session, WT_VERB_DISAGGREGATED_STORAGE,
+          "Completed disaggregated storage checkpoint: lsn=%" PRIu64 ", timestamp=%" PRIu64 " %s",
+          meta_lsn, checkpoint_timestamp,
+          __wt_timestamp_to_string(checkpoint_timestamp, ts_string));
     }
 
     WT_ERR(__disagg_begin_checkpoint(session));
@@ -2102,6 +2150,9 @@ __wt_disagg_update_shared_metadata(WT_SESSION_IMPL *session, const char *key, co
     cursor->set_key(cursor, key);
     cursor->set_value(cursor, value);
     WT_ERR(cursor->insert(cursor));
+
+    __wt_verbose_debug2(session, WT_VERB_DISAGGREGATED_STORAGE,
+      "Updated disaggregated shared metadata: key=\"%s\" value=\"%s\"", key, value);
 
 err:
     if (cursor != NULL)
