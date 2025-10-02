04d9b6c1efdc320c7a17129ccb63ba2239d03b4c
Mariam Mojid <98868895+mariammojid@users.noreply.github.com>
2025-10-02 16:01:16 +1000

    Update test/suite/test_verify2.py
    Co-authored-by: Etienne Petrel <77254506+etienneptl@users.noreply.github.com>

--- test/suite/test_verify2.py
+++ test/suite/test_verify2.py
@@ -73,7 +73,7 @@ class test_verify2(wttest.WiredTigerTestCase):
         self.assertEqual(self.session.verify(self.uri, None), 0)
 
     def test_verify_empty(self):
-        #  Verifying a non existant table should return ENOENT
+        #  Verifying a non-existent table should return ENOENT.
         self.assertRaisesException(wiredtiger.WiredTigerError,
                               lambda: self.session.verify(self.uri, None),
                               os.strerror(errno.ENOENT))
