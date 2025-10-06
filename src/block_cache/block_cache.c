095056148af4f92f84bac8605926818c6ab257d1
Albert <whoisong@gmail.com>
2025-10-07 08:38:58 +1100

    Update src/block_cache/block_cache.c
    Co-authored-by: Etienne Petrel <77254506+etienneptl@users.noreply.github.com>

--- src/block_cache/block_cache.c
+++ src/block_cache/block_cache.c
@@ -871,7 +871,7 @@ __wt_blkcache_setup(WT_SESSION_IMPL *session, const char *cfg[], bool reconfig)
     if (cval.val == 0)
         return (0);
 
-    /* FIXME-WT-15663 Disable block cache until it is stable */
+    /* FIXME-WT-15663 Disable block cache until it is stable. */
     if (cval.val == 1) {
         __wt_verbose_warning(session, WT_VERB_BLKCACHE,
           "Block cache is currently disabled. Continuing without block cache "
