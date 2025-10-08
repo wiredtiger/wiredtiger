1d3ccde54343e3f9b69ac19edbe4a8c34a35cb8d
Vamsi Krishna <bvpvamsikrishna@gmail.com>
2025-10-08 04:37:33 +0000

    wt-15210-backport-to-8.2

--- dist/api_data.py
+++ dist/api_data.py
@@ -622,6 +622,10 @@ connection_runtime_config = [
                 Use legacy page visit strategy for eviction. Using this option is highly discouraged
                 as it will re-introduce the bug described in WT-9121.''',
                 type='boolean'),
+            Config('scrub_evict_under_target_limit', 'false',
+                r'''Change the eviction strategy to scrub eviction when the cache usage is under
+                half way between the target limit to the trigger limit.''',
+                type='boolean'),
             ]),
     Config('eviction_checkpoint_target', '1', r'''
         perform eviction at the beginning of checkpoints to bring the dirty content in cache
