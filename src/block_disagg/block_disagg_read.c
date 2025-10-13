5e0c7069a912901160ab9fef14d7ada0685c945a
Yury Ershov <yuriy.ershov@gmail.com>
2025-10-14 09:32:26 +1100

    Update

--- src/block_disagg/block_disagg_read.c
+++ src/block_disagg/block_disagg_read.c
@@ -64,7 +64,7 @@ __block_disagg_check_lsn_frontier(WT_SESSION_IMPL *session, uint64_t lsn)
     WT_ACQUIRE_READ(
       last_materialized_lsn, S2C(session)->disaggregated_storage.last_materialized_lsn);
     /* last_materialized_lsn = 0 means it's not initialized yet. */
-    if (last_materialized_lsn != 0 && lsn > last_materialized_lsn) {
+    if (last_materialized_lsn != WT_DISAGG_LSN_NONE && lsn > last_materialized_lsn) {
         WT_RET_SUB_MSG(session, EINVAL, WT_DISAGG_BAD_LSN,
           "Attempted to read page with LSN %" PRIu64
           " ahead of the materialization frontier at "
