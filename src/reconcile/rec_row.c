7a393609b35d8f2d9352b0edff2dd561461c2118
Chenhao Qu <chenhao.qu@mongodb.com>
2025-10-14 07:48:36 +0000

    Typo

--- src/reconcile/rec_row.c
+++ src/reconcile/rec_row.c
@@ -247,7 +247,7 @@ __rec_pack_delta_row_int(
 
     WT_CLEAR(t_kv_struct);
 
-    /* Ensure we never build a delta for the first key. */
+    /* Ensure that we never build a delta for the first key. */
     WT_ASSERT(session, !r->cell_zero);
 
 #define WT_CELL_ADDR_DEL_VISIBLE_ALL_LEN (2)
