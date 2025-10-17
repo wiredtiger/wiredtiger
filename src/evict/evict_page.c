3247edc43288d3709d9b683e6cfaf07abc643819
svc-bot-sebb <82952146+svc-bot-sebb@users.noreply.github.com>
2025-10-17 13:12:53 +1300

    WT-15477 Track eviction max stats per database run (v8.2 backport) (#12586)
    Cherry-pick of 724a2e54e3b621902b03e859943c91b9b246bd23 for
    BACKPORT-26202
    
    Co-authored-by: Hari Babu Kommi <haribabu.kommi@mongodb.com>
    (cherry picked from commit f7b89baf4c3cee98eb3752323c609f0fca73514f)

--- src/evict/evict_page.c
+++ src/evict/evict_page.c
@@ -154,8 +154,12 @@ __evict_stats_update(WT_SESSION_IMPL *session, uint8_t flags)
     }
     if (!session->evict_timeline.reentry_hs_eviction) {
         eviction_time_milliseconds = eviction_time / WT_THOUSAND;
-        if (eviction_time_milliseconds > conn->cache->evict_max_ms)
-            conn->cache->evict_max_ms = eviction_time_milliseconds;
+        if (eviction_time_milliseconds >
+          __wt_atomic_load64(&conn->cache->evict_max_ms_per_checkpoint))
+            __wt_atomic_store64(
+              &conn->cache->evict_max_ms_per_checkpoint, eviction_time_milliseconds);
+        if (eviction_time_milliseconds > __wt_atomic_load64(&conn->cache->evict_max_ms))
+            __wt_atomic_store64(&conn->cache->evict_max_ms, eviction_time_milliseconds);
         if (eviction_time_milliseconds > WT_MINUTE * WT_THOUSAND)
             __wt_verbose_warning(session, WT_VERB_EVICT,
               "Eviction took more than 1 minute (%" PRIu64 "us). Building disk image took %" PRIu64
@@ -292,8 +296,15 @@ __wt_evict(WT_SESSION_IMPL *session, WT_REF *ref, uint8_t previous_state, uint32
      * force pages out before they're larger than the cache. We don't care about races, it's just a
      * statistic.
      */
-    if (page->memory_footprint > conn->cache->evict_max_page_size)
-        conn->cache->evict_max_page_size = page->memory_footprint;
+    if (__wt_atomic_loadsize(&page->memory_footprint) >
+      __wt_atomic_load64(&conn->cache->evict_max_page_size_per_checkpoint))
+        __wt_atomic_store64(&conn->cache->evict_max_page_size_per_checkpoint,
+          __wt_atomic_loadsize(&page->memory_footprint));
+
+    if (__wt_atomic_loadsize(&page->memory_footprint) >
+      __wt_atomic_load64(&conn->cache->evict_max_page_size))
+        __wt_atomic_store64(
+          &conn->cache->evict_max_page_size, __wt_atomic_loadsize(&page->memory_footprint));
 
     /* Figure out whether reconciliation was done on the page */
     if (__wt_page_evict_clean(page)) {
