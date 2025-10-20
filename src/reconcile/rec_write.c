4fd053ce3f710061b9cf1da2df9770df1a6b8461
Chenhao Qu <chenhao.qu@mongodb.com>
2025-10-20 05:20:13 +0000

    Reverse op order

--- src/reconcile/rec_write.c
+++ src/reconcile/rec_write.c
@@ -522,8 +522,8 @@ __rec_write_page_status(WT_SESSION_IMPL *session, WTI_RECONCILE *r)
          * remains dirty (that can't happen when evicting, the page is exclusively locked).
          */
         if (__wt_atomic_cas32(&mod->page_state, WT_PAGE_DIRTY_FIRST, WT_PAGE_CLEAN)) {
-            __wt_cache_dirty_decr(session, page);
             WT_RELEASE_WRITE(mod->dirty, (bool)false);
+            __wt_cache_dirty_decr(session, page);
         } else
             WT_ASSERT_ALWAYS(
               session, !F_ISSET(r, WT_REC_EVICT), "Page state has been modified during eviction");
