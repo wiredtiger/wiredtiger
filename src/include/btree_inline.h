e3a9ca05fad12597fad83c58d6f997ec8643503e
Chenhao Qu <chenhao.qu@mongodb.com>
2025-10-21 00:23:27 +0000

    Add comment

--- src/include/btree_inline.h
+++ src/include/btree_inline.h
@@ -764,6 +764,19 @@ __wt_page_only_modify_set(WT_SESSION_IMPL *session, WT_PAGE *page)
         if (last_running != WT_TXN_NONE)
             page->modify->first_dirty_txn = last_running;
 
+        /*
+         * Mark the page as dirty after increasing the dirty cache size. This ensures that the
+         * checkpoint does not visit and reconcile the page before the dirty cache size is
+         * increased. If the page is reconciled and cleaned before that, it could result in a
+         * decrease to the dirty cache size that hasn't been accounted for in the increase,
+         * potentially causing the dirty cache size to go negative. Since this is a newly modified
+         * page, reconciliation can only clean the page if it belongs to the metadata or the history
+         * store. It is acceptable for the checkpoint to skip these pages as:
+         *      1. Metadata is non-transactional, so consistency isn't a concern.
+         *      2. History store writes only contain data that has already been skipped in the data
+         * store checkpoint.
+         *      3. Updates to the data store remain invisible to the checkpoint.
+         */
         WT_RELEASE_WRITE(page->modify->modified, (bool)true);
     }
 
