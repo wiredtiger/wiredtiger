adfe3caf514236095df58eb90e631f1e92138f7a
Alana Huang <alana.huang@mongodb.com>
2025-10-21 05:44:17 +0000

    fix var declaration

--- src/evict/evict_lru.c
+++ src/evict/evict_lru.c
@@ -2127,15 +2127,13 @@ rand_next:
          * reset here.
          */
         if (page->evict_pass_gen == 0) {
-            const uint64_t gen_gap =
-              __wt_atomic_load64(&cache->evict_pass_gen) - page->cache_create_gen;
+            gen_gap = __wt_atomic_load64(&cache->evict_pass_gen) - page->cache_create_gen;
             if (gen_gap > __wt_atomic_load64(&cache->evict_max_unvisited_gen_gap))
                 __wt_atomic_store64(&cache->evict_max_unvisited_gen_gap, gen_gap);
             if (gen_gap > __wt_atomic_load64(&cache->evict_max_unvisited_gen_gap_per_checkpoint))
                 __wt_atomic_store64(&cache->evict_max_unvisited_gen_gap_per_checkpoint, gen_gap);
         } else {
-            const uint64_t gen_gap =
-              __wt_atomic_load64(&cache->evict_pass_gen) - page->evict_pass_gen;
+            gen_gap = __wt_atomic_load64(&cache->evict_pass_gen) - page->evict_pass_gen;
             if (gen_gap > __wt_atomic_load64(&cache->evict_max_visited_gen_gap))
                 __wt_atomic_store64(&cache->evict_max_visited_gen_gap, gen_gap);
             if (gen_gap > __wt_atomic_load64(&cache->evict_max_visited_gen_gap_per_checkpoint))
