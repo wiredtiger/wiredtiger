811664f44ddae6f8a672f69b2774bcbe048a3b16
Ivan Kochin <ivan.kochin@mongodb.com>
2025-10-22 01:06:40 +0000

    Manually revert changes for aggressive_score + Bring back __wt_atomic_stats_max as it was removed as a part of reverting manual changes

--- src/evict/evict_lru.c
+++ src/evict/evict_lru.c
@@ -934,12 +934,11 @@ __evict_pass(WT_SESSION_IMPL *session)
          * If there is still no progress after 2s, we will treat the cache as stuck and start
          * rolling back transactions and writing updates to the history store table.
          */
-        uint32_t aggressive_score = __wt_atomic_load_uint32_relaxed(&evict->evict_aggressive_score);
         if (eviction_progress == __wt_atomic_load_uint64_v_relaxed(&evict->eviction_progress)) {
             if (WT_CLOCKDIFF_MS(time_now, time_prev) >= 20 && F_ISSET(evict, WT_EVICT_CACHE_HARD)) {
-                if (aggressive_score < WT_EVICT_SCORE_MAX)
+                if (__wt_atomic_load_uint32_relaxed(&evict->evict_aggressive_score) <
+                  WT_EVICT_SCORE_MAX)
                     (void)__wt_atomic_add_uint32_v(&evict->evict_aggressive_score, 1);
-
                 oldest_id = __wt_atomic_load_uint64_v_relaxed(&txn_global->oldest_id);
                 if (prev_oldest_id == oldest_id &&
                   __wt_atomic_load_uint64_v_relaxed(&txn_global->current) != oldest_id &&
@@ -2653,21 +2652,13 @@ __evict_walk_tree(WT_SESSION_IMPL *session, WTI_EVICT_QUEUE *queue, u_int max_en
         if (page->evict_pass_gen == 0) {
             const uint64_t gen_gap =
               __wt_atomic_load_uint64_relaxed(&evict->evict_pass_gen) - page->cache_create_gen;
-            if (gen_gap > __wt_atomic_load_uint64_relaxed(&evict->evict_max_unvisited_gen_gap))
-                __wt_atomic_store_uint64_relaxed(&evict->evict_max_unvisited_gen_gap, gen_gap);
-            if (gen_gap >
-              __wt_atomic_load_uint64_relaxed(&evict->evict_max_unvisited_gen_gap_per_checkpoint))
-                __wt_atomic_store_uint64_relaxed(
-                  &evict->evict_max_unvisited_gen_gap_per_checkpoint, gen_gap);
+            __wt_atomic_stats_max(&evict->evict_max_unvisited_gen_gap, gen_gap);
+            __wt_atomic_stats_max(&evict->evict_max_unvisited_gen_gap_per_checkpoint, gen_gap);
         } else {
             const uint64_t gen_gap =
               __wt_atomic_load_uint64_relaxed(&evict->evict_pass_gen) - page->evict_pass_gen;
-            if (gen_gap > __wt_atomic_load_uint64_relaxed(&evict->evict_max_visited_gen_gap))
-                __wt_atomic_store_uint64_relaxed(&evict->evict_max_visited_gen_gap, gen_gap);
-            if (gen_gap >
-              __wt_atomic_load_uint64_relaxed(&evict->evict_max_visited_gen_gap_per_checkpoint))
-                __wt_atomic_store_uint64_relaxed(
-                  &evict->evict_max_visited_gen_gap_per_checkpoint, gen_gap);
+            __wt_atomic_stats_max(&evict->evict_max_visited_gen_gap, gen_gap);
+            __wt_atomic_stats_max(&evict->evict_max_visited_gen_gap_per_checkpoint, gen_gap);
         }
 
         page->evict_pass_gen = __wt_atomic_load_uint64_relaxed(&evict->evict_pass_gen);
