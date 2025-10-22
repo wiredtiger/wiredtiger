2437b0da39ee9b82e030eb57267914d032ec1cae
Hari Babu Kommi <haribabu.kommi@mongodb.com>
2025-10-23 10:22:20 +1100

    WT-15736 Split "maximum page size seen at eviction" to 3 seperate statistics (v8.0 backport) (#12621)
    Cherry-pick of ea289bfd5c6923aa7dfa5068877356f654e7da95 for
    BACKPORT-26204
    
    Co-authored-by: Alana Huang <alana.huang@mongodb.com>
    (cherry picked from commit 88ae30810857ef059d0b8526dc1280983f66e99f)
    
    ---------
    
    Co-authored-by: svc-bot-sebb <82952146+svc-bot-sebb@users.noreply.github.com>
    Co-authored-by: Alana Huang <alana.huang@mongodb.com>

--- src/conn/conn_cache.c
+++ src/conn/conn_cache.c
@@ -353,9 +353,12 @@ __wti_cache_stats_update(WT_SESSION_IMPL *session)
     WT_STAT_SET(session, stats, cache_bytes_leaf, leaf);
     WT_STAT_SET(session, stats, cache_bytes_other, __wt_cache_bytes_other(cache));
     WT_STAT_SET(session, stats, cache_bytes_updates, __wt_cache_bytes_updates(cache));
-
-    WT_STAT_SET(session, stats, cache_eviction_maximum_page_size_per_checkpoint,
-      cache->evict_max_page_size_per_checkpoint);
+    WT_STAT_SET(session, stats, cache_eviction_maximum_clean_page_size_per_checkpoint,
+      cache->evict_max_clean_page_size_per_checkpoint);
+    WT_STAT_SET(session, stats, cache_eviction_maximum_dirty_page_size_per_checkpoint,
+      cache->evict_max_dirty_page_size_per_checkpoint);
+    WT_STAT_SET(session, stats, cache_eviction_maximum_updates_page_size_per_checkpoint,
+      cache->evict_max_updates_page_size_per_checkpoint);
     WT_STAT_SET(session, stats, cache_eviction_maximum_page_size, cache->evict_max_page_size);
     WT_STAT_SET(session, stats, cache_eviction_maximum_milliseconds, cache->evict_max_ms);
     WT_STAT_SET(session, stats, cache_eviction_maximum_milliseconds_per_checkpoint,
--- src/txn/txn_ckpt.c
+++ src/txn/txn_ckpt.c
@@ -1158,10 +1158,11 @@ __txn_checkpoint(WT_SESSION_IMPL *session, const char *cfg[])
     /* Reset the statistics tracked per checkpoint. */
     __wt_atomic_store64(&cache->evict_max_unvisited_gen_gap_per_checkpoint, 0);
     __wt_atomic_store64(&cache->evict_max_visited_gen_gap_per_checkpoint, 0);
-    __wt_atomic_store64(&cache->evict_max_page_size_per_checkpoint, 0);
+    __wt_atomic_store64(&cache->evict_max_clean_page_size_per_checkpoint, 0);
+    __wt_atomic_store64(&cache->evict_max_dirty_page_size_per_checkpoint, 0);
+    __wt_atomic_store64(&cache->evict_max_updates_page_size_per_checkpoint, 0);
     __wt_atomic_store64(&cache->evict_max_ms_per_checkpoint, 0);
     cache->reentry_hs_eviction_ms = 0;
-
     __wt_atomic_store32(&conn->heuristic_controls.obsolete_tw_btree_count, 0);
     conn->rec_maximum_hs_wrapup_milliseconds = 0;
     conn->rec_maximum_image_build_milliseconds = 0;
