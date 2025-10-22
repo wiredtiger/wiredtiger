c8478cbe86ccbb747bde61e0fd83347256027d4e
Hari Babu Kommi <haribabu.kommi@mongodb.com>
2025-10-23 09:04:47 +1100

    fix the test

--- test/suite/test_eviction05.py
+++ test/suite/test_eviction05.py
@@ -91,7 +91,6 @@ class test_eviction05(wttest.WiredTigerTestCase):
         self.assertGreater(self.get_stat(stat.conn.cache_eviction_maximum_clean_page_size_per_checkpoint), 0)
 
         self.assertGreater(self.get_stat(stat.conn.cache_eviction_maximum_page_size), 0)
-        self.assertEqual(self.get_stat(stat.conn.cache_eviction_maximum_page_size_per_checkpoint), 0)
 
         # Run a checkpoint and verify that eviction max stats per database run are not reset.
         self.session.checkpoint()
