57805f5bf8e3cb3e4029eeb3f68eeab7097c1bb9
Albert Song <albert.song@mongodb.com>
2025-10-23 02:19:11 +0000

    Add test case for wt verify reducted

--- test/suite/test_verify.py
+++ test/suite/test_verify.py
@@ -29,6 +29,7 @@
 import os, re, struct
 from suite_subprocess import suite_subprocess
 import wiredtiger, wttest
+from helper import WiredTigerCursor
 
 # test_verify.py
 #    Utilities: wt verify
@@ -78,6 +79,9 @@ class test_verify(wttest.WiredTigerTestCase, suite_subprocess):
                     count += 1
             f.close()
         return count
+
+    def check_file_contains(self, filename, content):
+        return super().check_file_contains(filename, content)
 
     # FIXME-WT-15064:
     @wttest.skip_for_hook("disagg", " We cannot access shared tables data directly")
@@ -384,6 +388,43 @@ class test_verify(wttest.WiredTigerTestCase, suite_subprocess):
         # The test may output the following error message while opening a file that
         # does not exist. Ignore that.
         self.ignoreStderrPatternIfExists('No such file or directory')
+
+    def test_verify_reducted(self):
+        """
+        Test verify in a 'wt' process on a table with reducted.
+        """
+        self.skip_disagg_wt_verify_test()
+        if not wiredtiger.diagnostic_build():
+            self.skipTest('requires a diagnostic build as the test uses verify -d dump_pages')
+
+        params = 'key_format=S,value_format=S'
+        self.session.create('table:' + self.tablename, params)
+        self.nentries = 10
+        self.populate(self.tablename)
+        """
+        Insert some secret entries into the table
+        """
+        with WiredTigerCursor(self.session, 'table:' + self.tablename, None, None) as cursor:
+            cursor['secret_key'] = "#hidden#"
+
+        # stabilize the table with a checkpoint
+        self.session.checkpoint()
+
+        # Check the reducted output
+        self.runWt(["-p", "verify", '-d', 'dump_pages', f"file:{self.tablename}.wt"],
+            outfilename='verify_reducted.out', errfilename="verify_reducted.err", failure=False)
+
+        self.check_empty_file('verify_reducted.err')
+        self.check_file_not_contains('verify_reducted.out', 'secret_key')
+        self.check_file_not_contains('verify_reducted.out', '#hidden#')
+
+        # Check the unreducted output
+        self.runWt(["-p", "verify", '-d', 'dump_pages', '-u', f"file:{self.tablename}.wt"],
+            outfilename='verify_reducted.out', errfilename="verify_reducted.err", failure=False)
+
+        self.check_empty_file('verify_reducted.err')
+        self.check_file_contains('verify_reducted.out', 'secret_key')
+        self.check_file_contains('verify_reducted.out', '#hidden#')
 
     def test_verify_all(self):
         """
