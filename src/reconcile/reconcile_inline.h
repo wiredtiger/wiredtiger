17dfb30fb2e42b5f2e5334afcbf0f822c0695441
Chenhao Qu <chenhao.qu@mongodb.com>
2025-10-27 04:00:17 +0000

    Improve comment

--- src/reconcile/reconcile_inline.h
+++ src/reconcile/reconcile_inline.h
@@ -511,8 +511,8 @@ __wti_rec_time_window_clear_obsolete(WT_SESSION_IMPL *session, WTI_UPDATE_SELECT
     btree = S2BT(session);
 
     /*
-     * Never clear the timestamps on the ingest tables. They are needed for step-up even they are
-     * globally visible.
+     * Never clear the timestamps on the ingest tables. They are needed for step-up even when they
+     * are globally visible.
      */
     if (F_ISSET(btree, WT_BTREE_GARBAGE_COLLECT))
         return;
