752f5047cbdcf5da2e28606b1f177328544b0e20
Ravi Prakash Giri <ravi.giri@mongodb.com>
2025-10-27 22:31:20 +0000

    WT-15617 Design framework to rewrite pages with deltas directly into full pages

--- src/btree/bt_read.c
+++ src/btree/bt_read.c
@@ -147,6 +147,7 @@ __page_read(WT_SESSION_IMPL *session, WT_REF *ref, uint32_t flags)
 {
     WT_ADDR_COPY addr;
     WT_DECL_RET;
+    WT_ITEM *base_image;
     WT_ITEM *deltas;
     WT_ITEM *tmp;
     WT_PAGE *page;
@@ -246,6 +247,14 @@ __page_read(WT_SESSION_IMPL *session, WT_REF *ref, uint32_t flags)
     else
         deltas = NULL;
 
+    /* Build a full disk image of the page after reading from disk. */
+    if (/* DISABLES CODE */ (0) && count > 1) {
+        ret = __wti_build_full_disk_image_on_read(session, ref, deltas, count - 1, base_image);
+        for (i = 0; i < count - 1; ++i)
+            __wt_buf_free(session, &deltas[i]);
+        __wt_buf_free(session, base_image);
+        WT_ERR(ret);
+    }
     /*
      * Build the in-memory version of the page. Clear our local reference to the allocated copy of
      * the disk image on return, the in-memory object steals it.
