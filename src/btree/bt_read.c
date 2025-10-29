842c5d8685089c680459a15e6aa427adf32684a1
Chenhao Qu <chenhao.qu@mongodb.com>
2025-10-29 23:32:49 +0000

    WT-15892 Fix __wt_page_in_func segfault after use after free of the page

--- src/btree/bt_read.c
+++ src/btree/bt_read.c
@@ -277,6 +277,7 @@ __page_read(WT_SESSION_IMPL *session, WT_REF *ref, uint32_t flags)
         for (i = 0; i < count - 1; ++i)
             __wt_buf_free(session, &deltas[i]);
         WT_ERR(ret);
+        page = ref->page;
     }
 
     __wt_free(session, tmp);
