98b0087c76a93ac463ca9e40edeaff6aa343e807
Chenhao Qu <chenhao.qu@mongodb.com>
2025-10-30 11:25:08 +1100

    WT-15892 Fix __wt_page_in_func segfault because use after free of the page (#12682)
    The delta rewrite code changes the page inside ref underneath. While the
    caller still has a pointer to the old page, which causes this issue.
    Refresh the pointer in the caller to fix this.
    
    This should all go away after we can generate a full page directly from
    the base page and the deltas.

--- src/btree/bt_read.c
+++ src/btree/bt_read.c
@@ -277,6 +277,8 @@ __page_read(WT_SESSION_IMPL *session, WT_REF *ref, uint32_t flags)
         for (i = 0; i < count - 1; ++i)
             __wt_buf_free(session, &deltas[i]);
         WT_ERR(ret);
+        /* The page may be changed if we consolidate the deltas to a new page. */
+        page = ref->page;
     }
 
     __wt_free(session, tmp);
