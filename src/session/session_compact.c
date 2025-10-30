821f9f09bde9e4015953d3db8e7db1a941914fa2
Albert Song <albert.song@mongodb.com>
2025-10-30 23:09:59 +0000

    Add err of compact call in session compact readonly path

--- src/session/session_compact.c
+++ src/session/session_compact.c
@@ -565,6 +565,9 @@ __wti_session_compact_readonly(WT_SESSION *wt_session, const char *uri, const ch
     session = (WT_SESSION_IMPL *)wt_session;
     SESSION_API_CALL_NOCONF(session, compact);
 
+    if (__wt_conn_is_disagg(session))
+        WT_ERR_MSG(session, ENOTSUP, "Compaction does not work with disaggregated storage.");
+
     WT_STAT_CONN_INCR(session, session_table_compact_fail);
     ret = __wti_session_notsup(session);
 err:
