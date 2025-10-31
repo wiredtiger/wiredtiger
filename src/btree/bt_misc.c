bd4085d710934a07498c0fab9ff4dca3ff8f1d9c
Albert Song <albert.song@mongodb.com>
2025-10-31 04:38:51 +0000

    WT-15898 Free local temp item after use

--- src/btree/bt_misc.c
+++ src/btree/bt_misc.c
@@ -88,10 +88,13 @@ __wt_key_string(
   WT_SESSION_IMPL *session, const void *data_arg, size_t size, const char *key_format, WT_ITEM *buf)
 {
     WT_ITEM tmp;
+    const char *ret_str = "";
 
 #ifdef HAVE_DIAGNOSTIC
-    if (session->dump_raw)
-        return (__wt_buf_set_printable(session, data_arg, size, false, buf));
+    if (session->dump_raw) {
+        ret_str = __wt_buf_set_printable(session, data_arg, size, false, buf);
+        goto cleanup;
+    }
 #endif
 
     /*
@@ -107,7 +110,14 @@ __wt_key_string(
             size = sizeof(WT_ERR_STRING);
         }
     }
-    return (__wt_buf_set_printable_format(session, data_arg, size, key_format, false, buf));
+
+    ret_str = __wt_buf_set_printable_format(session, data_arg, size, key_format, false, buf);
+
+cleanup:
+
+    /* Always clean up local wt_item */
+    __wt_buf_free(session, &tmp);
+    return (ret_str);
 }
 
 /*
