227283588eb794172e1129aca2ab88471e810411
Hari Babu Kommi <haribabu.kommi@mongodb.com>
2025-10-31 11:01:46 +1100

    fix the condition

--- src/btree/bt_read.c
+++ src/btree/bt_read.c
@@ -572,7 +572,7 @@ skip_evict:
         }
         __wt_spin_backoff(&yield_cnt, &sleep_usecs);
         ++sleep_count;
-        if (sleep_count > 10 * WT_THOUSAND && sleep_count % 10 * WT_THOUSAND == 0)
+        if (sleep_count > 10 * WT_THOUSAND && sleep_count % (10 * WT_THOUSAND) == 0)
             __wt_verbose_warning(session, WT_VERB_READ,
               "sleep to wait the page %p for %" WT_SIZET_FMT " times", (void *)ref, sleep_count);
         WT_STAT_CONN_INCRV(session, page_sleep, sleep_usecs);
