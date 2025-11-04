9193855bd4b301ed987f1587ff2ff53ea33f0e7b
Ivan Kochin <ivan.kochin@mongodb.com>
2025-11-04 01:24:45 +0000

    Fix the rest of warnings

--- src/block/block_ext.c
+++ src/block/block_ext.c
@@ -225,7 +225,7 @@ __block_ext_insert(WT_SESSION_IMPL *session, WT_EXTLIST *el, WT_EXT *ext)
     }
 
     ++el->entries;
-    el->bytes += (uint64_t)ext->size;
+    __wt_atomic_add_uint64_relaxed(&el->bytes, (uint64_t)ext->size);
 
     /* Update the cached end-of-list. */
     if (ext->next[0] == NULL)
