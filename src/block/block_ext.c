cab1cac43c6867ae3d2a2b71f0c82189b987d86c
Albert <whoisong@gmail.com>
2025-11-04 11:34:20 +1100

    Update src/block/block_ext.c
    Co-authored-by: Etienne Petrel <77254506+etienneptl@users.noreply.github.com>

--- src/block/block_ext.c
+++ src/block/block_ext.c
@@ -1502,7 +1502,7 @@ err:
 
 /*
  * __wti_block_extlist_dump_all --
- *     Dump all extent lists as verbose messages including living checkpoint and historical ones. As
+ *     Dump all extent lists as verbose messages including the live checkpoint and historical ones. As
  *     this is a debug function, errors are ignored.
  */
 void
