f9f267e58f0747b5d2562afd0c4fcd9faef2df1c
Jasmine Bi <jasmine.bi@mongodb.com>
2025-11-12 23:02:33 +0000

    hardcode valformat

--- test/suite/test_cursor_random.py
+++ test/suite/test_cursor_random.py
@@ -217,10 +217,7 @@ class test_cursor_random_column(wttest.WiredTigerTestCase):
         ('file', dict(uri='file:random')),
         ('table', dict(uri='table:random'))
     ]
-    valfmt_values = [
-        ('string', dict(valfmt='S')),
-    ]
-    scenarios = make_scenarios(type_values, valfmt_values)
+    scenarios = make_scenarios(type_values, [('string', dict(valfmt='S'))])
 
     def test_cursor_random_column(self):
         self.session.create(self.uri, 'key_format=r,value_format={}'.format(self.valfmt))
