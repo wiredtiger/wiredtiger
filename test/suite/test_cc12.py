0ec685381fbb8069ef46ef846177f41633d582ef
Albert Song <albert.song@mongodb.com>
2025-11-17 02:38:23 +0000

    Fix repro about WT-16002

--- test/suite/test_cc12.py
+++ test/suite/test_cc12.py
@@ -40,7 +40,7 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 # Verify checkpoint cleanup cleans up logged tables when configured in aggressive mode.
 @wttest.skip_for_hook("tiered", "Checkpoint cleanup does not support tiered tables")
 class test_cc12(test_cc_base):
-    conn_config = 'statistics=(all),statistics_log=(json,wait=1,on_close=true),log=(enabled=true)'
+    conn_config = 'cache_size=8M,statistics=(all),statistics_log=(json,wait=1,on_close=true,sources=[file:]),log=(enabled=true)'
 
     checkpoint_cleanup_methods = [
         ('cc_method_reclaim_space', dict()),
@@ -49,34 +49,55 @@ class test_cc12(test_cc_base):
     scenarios = make_scenarios(checkpoint_cleanup_methods)
 
     lock = threading.Lock()
-    g_ts = 1
+    g_ts = 3
+    opened_session = 1
+    partitions = 100
+
+    op_map = {}
 
     def random_populate(self, uri, seed, ts):
+        tid = threading.get_ident()
+        key_partition = 0
+        with self.lock:
+            key_partition = len(self.op_map)
+            self.op_map[tid] = key_partition
         session = self.conn.open_session()
-        for r in range(1000000):
-            session.begin_transaction()
-            cur = session.open_cursor(uri)
-            for sr in range(1000):
-                key = random.randint(0, 10000)
-                cur[key] = f"upd_{r}_{sr}_{seed}"
-            cur.close()
+        try:
+            for r in range(1000):
+                session.begin_transaction()
+                cur = session.open_cursor(uri)
+                for sr in range(10):
+                    key = random.randint(0, 1000/self.partitions) * self.partitions + key_partition
+                    cur[key] = f"upd_{r}_{sr}_{seed}"
+                cur.close()
+                with self.lock:
+                    ts = self.g_ts
+                    self.g_ts += 1
+                session.commit_transaction(f'commit_timestamp=' + self.timestamp_str(ts))
             with self.lock:
-                ts = self.g_ts
-                self.g_ts += 1
-            session.commit_transaction(f'commit_timestamp=' + self.timestamp_str(ts))
+                session.close()
+                self.opened_session -= 1
+        except Exception as e:
+            session.close()
+            print("Random populate failed : " + str(e))
+        with self.lock:
+            del self.op_map[tid]
 
     def long_transaction_round(self, uri, ts):
-        self.session.begin_transaction()#'read_timestamp=' + self.timestamp_str(1))
+        self.session.begin_transaction('read_timestamp=' + self.timestamp_str(2))
         long_cur = self.session.open_cursor(uri)
         long_cur[-1] = "halted"
         long_cur.close()
-        with ThreadPoolExecutor(max_workers=20) as executor:
-            for seed in range(10000):
+        with ThreadPoolExecutor(max_workers=self.partitions) as executor:
+            for seed in range(100):
                 # self.random_populate(uri, seed, ts)
                 executor.submit(self.random_populate, uri, seed, ts)
         self.session.commit_transaction()
-        self.session.checkpoint()
+        self.session.checkpoint('debug=(checkpoint_cleanup=true)')
 
+    def get_hs_size(self):
+        with WiredTigerStat(self.session) as stat_cursor:
+            return stat_cursor[stat.conn.cache_hs_ondisk][2]
 
     def test_cc12(self):
         # Increase the likelihood of having internal pages since they are targeted by checkpoint
@@ -89,12 +110,29 @@ class test_cc12(test_cc_base):
         self.conn.set_timestamp('oldest_timestamp=' + self.timestamp_str(1))
         history_size = []
 
-        for ts in range(100):
-            hs_path = os.path.join(self.home, 'WiredTigerHS.wt')
-            history_size.append(os.stat(hs_path).st_size)
+        populate_cur = self.session.open_cursor(uri)
+        for key in range(10000):
+            populate_cur[key] = "init"
+        populate_cur.close()
+
+        for ts in range(4):
             # with WiredTigerStat(self.session, 'history-store') as stat_cursor:
             #     history_size.append(stat_cursor[stat.dsrc.block_size][2])
             self.long_transaction_round(uri, ts)
+            size_before = self.get_hs_size()
             self.wait_for_cc_to_run()
+            history_size.append((size_before, self.get_hs_size()))
+        
+        self.prout("First rounds test: " + str(history_size))
+        history_size.clear()
 
-        print(history_size)
+        for _ in range(2):
+            self.reopen_conn()
+            size_before = self.get_hs_size()
+            self.wait_for_cc_to_run()
+            history_size.append((size_before, self.get_hs_size()))
+            size_before = self.get_hs_size()
+            self.wait_for_cc_to_run()
+            history_size.append((size_before, self.get_hs_size()))
+        
+        self.prout("Reopen rounds test: " + str(history_size))
