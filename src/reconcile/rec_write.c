015b60346c3c2548148a9a1843b96bf3bda8e0d4
Albert Song <albert.song@mongodb.com>
2025-11-20 23:46:00 +0000

    separate the varaibles

--- src/reconcile/rec_write.c
+++ src/reconcile/rec_write.c
@@ -3032,7 +3032,8 @@ __rec_write_wrapup(WT_SESSION_IMPL *session, WTI_RECONCILE *r)
     WT_REF_STATE previous_ref_state;
     WT_TIME_AGGREGATE stop_ta, *stop_tap, ta;
     uint32_t i;
-    bool disagg_block_free_required;
+    bool disagg_page_free_required;
+    bool disagg_page_is_valid;
 
     btree = S2BT(session);
     bm = btree->bm;
@@ -3041,7 +3042,8 @@ __rec_write_wrapup(WT_SESSION_IMPL *session, WTI_RECONCILE *r)
     mod = page->modify;
     WT_TIME_AGGREGATE_INIT(&ta);
     previous_ref_state = 0;
-    disagg_block_free_required = false;
+    disagg_page_is_valid = false;
+    disagg_page_free_required = false;
 
     /*
      * If using the history store table eviction path and we found updates that weren't globally
@@ -3058,7 +3060,7 @@ __rec_write_wrapup(WT_SESSION_IMPL *session, WTI_RECONCILE *r)
     if (page->disagg_info != NULL)
         if (page->disagg_info->block_meta.page_id != WT_BLOCK_INVALID_PAGE_ID)
             /* As an initial condition, the page must have a valid page_id. */
-            disagg_block_free_required = true;
+            disagg_page_is_valid = true;
 
     /*
      * Wrap up overflow tracking. If we are about to create a checkpoint, the system must be
@@ -3097,12 +3099,14 @@ __rec_write_wrapup(WT_SESSION_IMPL *session, WTI_RECONCILE *r)
             break;
         }
 
-        /* Free the disaggregated block if reconciliation results in zero pages, multiple pages, or
-         * a single empty page. */
-        if (disagg_block_free_required)
-            disagg_block_free_required =
+        /*
+         * Free the disaggregated block if reconciliation results in zero pages, multiple pages, or
+         * a single empty page.
+         */
+        if (disagg_page_is_valid)
+            disagg_page_free_required =
               (r->multi_next != 1 || r->multi->block_meta->page_id == WT_BLOCK_INVALID_PAGE_ID);
-        WT_RET(__wt_ref_block_free(session, ref, disagg_block_free_required));
+        WT_RET(__wt_ref_block_free(session, ref, disagg_page_free_required));
         break;
     case WT_PM_REC_EMPTY: /* Page deleted */
         break;
@@ -3124,10 +3128,12 @@ __rec_write_wrapup(WT_SESSION_IMPL *session, WTI_RECONCILE *r)
              * We have skipped writing a delta in the first reconciliation after the page is read
              * from disk.
              */
-            /* Free the disaggregated block if reconciliation results in zero pages, multiple pages,
-             * or a single empty page. */
-            if (disagg_block_free_required)
-                disagg_block_free_required =
+            /*
+             * Free the disaggregated block if reconciliation results in zero pages, multiple pages,
+             * or a single empty page.
+             */
+            if (disagg_page_is_valid)
+                disagg_page_free_required =
                   (r->multi_next != 1 || r->multi->block_meta->page_id == WT_BLOCK_INVALID_PAGE_ID);
             if (mod->mod_replace.block_cookie == NULL) {
                 WT_ASSERT(session, WT_DELTA_ENABLED_FOR_PAGE(session, page->type));
@@ -3138,9 +3144,11 @@ __rec_write_wrapup(WT_SESSION_IMPL *session, WTI_RECONCILE *r)
                 if (ref->addr != NULL) {
                     if (page->disagg_info == NULL)
                         WT_RET(__wt_ref_block_free(session, ref, false));
-                    else if (disagg_block_free_required && !F_ISSET(r, WT_REC_EMPTY_DELTA))
-                        /* Only free a disagg page if it's not empty delta. */
+                    else if (r->multi->block_meta->page_id == WT_BLOCK_INVALID_PAGE_ID)
                         WT_RET(__wt_ref_block_free(session, ref, true));
+                    else if (!F_ISSET(r, WT_REC_EMPTY_DELTA))
+                        /* Only free a disagg page if it's not empty delta. */
+                        WT_RET(__wt_ref_block_free(session, ref, disagg_page_free_required));
                 }
             } else {
                 /* Free the disaggregated block if reconciliation results in zero pages, multiple
@@ -3148,7 +3156,7 @@ __rec_write_wrapup(WT_SESSION_IMPL *session, WTI_RECONCILE *r)
                 if (page->disagg_info == NULL)
                     WT_RET(__wt_btree_block_free(
                       session, mod->mod_replace.block_cookie, mod->mod_replace.block_cookie_size));
-                else if (disagg_block_free_required) {
+                else if (disagg_page_free_required) {
                     WT_ASSERT(session,
                       r->multi == NULL ||
                         r->multi->block_meta->page_id != WT_BLOCK_INVALID_PAGE_ID);
