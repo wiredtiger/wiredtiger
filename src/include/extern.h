491453b62be20e4ec8389452967f583a0608cb72
Peter Macko <peter.macko@mongodb.com>
2025-11-25 14:59:37 -0500

    WT-15985 Test upgrade/downgrade of disagg address cookies (#12801)
    Add two debug modes for testing upgrade/downgrade for disagg address
    cookies, which make up new fields, so that we can test that our
    upgrade/downgrade framework works.

--- src/include/extern.h
+++ src/include/extern.h
@@ -1491,6 +1491,8 @@ extern int __wti_desc_write(WT_SESSION_IMPL *session, WT_FH *fh, uint32_t allocs
   WT_GCC_FUNC_DECL_ATTRIBUTE((warn_unused_result));
 extern int __wti_disagg_conn_config(WT_SESSION_IMPL *session, const char **cfg, bool reconfig)
   WT_GCC_FUNC_DECL_ATTRIBUTE((warn_unused_result));
+extern int __wti_disagg_debug_mode_config(WT_SESSION_IMPL *session, const char *cfg[])
+  WT_GCC_FUNC_DECL_ATTRIBUTE((warn_unused_result));
 extern int __wti_disagg_destroy(WT_SESSION_IMPL *session)
   WT_GCC_FUNC_DECL_ATTRIBUTE((warn_unused_result));
 extern int __wti_disagg_set_last_materialized_lsn(WT_SESSION_IMPL *session, uint64_t lsn)
