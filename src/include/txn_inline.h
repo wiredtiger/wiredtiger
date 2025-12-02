5f2d1c72ee80e1ac79d394be81ec5c2f482b7088
Linh Tran <30374401+tdlinh2712@users.noreply.github.com>
2025-12-02 12:25:05 +1100

    WT-15967 Create the prepared update on the ingest table in standby when claiming the prepared update (#12815)
    In disaggregated storage, follower nodes can only modify the ingest
    table, not the stable table. However, with precise checkpoints, prepared
    transactions are stored in the stable table. This creates a problem:
    when follower nodes pick up checkpoints containing prepared
    transactions, they cannot resolve these transactions because they lack
    write access to the stable table where the prepared updates are.
    
    This PR resolves the issue by restoring prepared updates from the stable
    table to the ingest table during the prepared transaction discovery
    process. The implementation:
    
    - Find prepared transactions in stable table checkpoints during
    discovery
    - Restores prepared updates from stable table to ingest table and adds
    it to the prepared discover resulting hash map for claiming.
    - After the prepared transactions are claimed and committed/rolled back,
    transaction resolution are now accessible in the ingest table.

--- src/include/txn_inline.h
+++ src/include/txn_inline.h
@@ -695,9 +695,13 @@ __wt_txn_op_set_timestamp(WT_SESSION_IMPL *session, WT_TXN_OP *op, bool validate
             __wt_txn_op_delete_apply_prepare_state(session, op, true);
         else {
             upd = op->u.op_upd;
-
-            /* Resolve prepared update to be committed update. */
-            __txn_apply_prepare_state_update(session, upd, true);
+            /*
+             * upd can be NULL when called from prepared discover module to initialize a new op, in
+             * which case we won't need to resolve the prepare state here.
+             */
+            if (upd != NULL)
+                /* Resolve prepared update to be committed update. */
+                __txn_apply_prepare_state_update(session, upd, true);
         }
     } else {
         if (op->type == WT_TXN_OP_REF_DELETE)
