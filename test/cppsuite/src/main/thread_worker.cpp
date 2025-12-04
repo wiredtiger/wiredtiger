45c63f11170394e2dde77d2b7b043cae382b9630
luke.pearson <luke.pearson@mongodb.com>
2025-12-04 22:06:03 +0000

    Dont forget to initialize the op counting

--- test/cppsuite/src/main/thread_worker.cpp
+++ test/cppsuite/src/main/thread_worker.cpp
@@ -84,6 +84,13 @@ thread_worker::thread_worker(uint64_t id, thread_type type, configuration *confi
     if (op_tracker->enabled())
         op_track_cursor = session.open_scoped_cursor(op_tracker->get_operation_table_name());
 
+    /* Use optional here as our populate threads don't define this configuration. */
+    configuration *ops_config = config->get_optional_subconfig(OPS_PER_TRANSACTION);
+    if (ops_config != nullptr) {
+        _min_op_count = ops_config->get_optional_int(MIN, 1);
+        _max_op_count = ops_config->get_optional_int(MAX, 1);
+        delete ops_config;
+    }
     testutil_assert(key_size > 0 && value_size > 0);
 }
 
