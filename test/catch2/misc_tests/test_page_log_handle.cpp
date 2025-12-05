c63e56b04b2c6d39915bd608625a76f1c21a5f9c
jchen <jie.chen@mongodb.com>
2025-12-05 04:49:45 +0000

    Fix asan failure

--- test/catch2/misc_tests/test_page_log_handle.cpp
+++ test/catch2/misc_tests/test_page_log_handle.cpp
@@ -11,31 +11,37 @@
 #include "wiredtiger.h"
 #include "../wrappers/mock_session.h"
 
-int mock_plh_open_handle(WT_PAGE_LOG *page_log, WT_SESSION *session,
-  uint64_t table_id, WT_PAGE_LOG_HANDLE **plh)
+int
+mock_plh_open_handle(
+  WT_PAGE_LOG *page_log, WT_SESSION *session, uint64_t table_id, WT_PAGE_LOG_HANDLE **plh)
 {
     WT_PAGE_LOG_HANDLE *mock_log_handle;
     WT_UNUSED(page_log);
     WT_UNUSED(table_id);
 
-    WT_RET(__wt_calloc_one((WT_SESSION_IMPL*)session, &mock_log_handle));
+    WT_RET(__wt_calloc_one((WT_SESSION_IMPL *)session, &mock_log_handle));
     *plh = mock_log_handle;
     return (0);
 }
 
-int mock_terminate(WT_PAGE_LOG *page_log, WT_SESSION *session) {
-    __wt_free((WT_SESSION_IMPL*)session, page_log);
+int
+mock_terminate(WT_PAGE_LOG *page_log, WT_SESSION *session)
+{
+    __wt_free((WT_SESSION_IMPL *)session, page_log);
     return (0);
 }
 
-int mock_plh_close(WT_PAGE_LOG_HANDLE *plh, WT_SESSION *session)
+int
+mock_plh_close(WT_PAGE_LOG_HANDLE *plh, WT_SESSION *session)
 {
     WT_UNUSED(plh);
     WT_UNUSED(session);
     return (0);
 }
 
-void setup_page_log_queue(WT_SESSION_IMPL *session) {
+void
+setup_page_log_queue(WT_SESSION_IMPL *session)
+{
     WT_CONNECTION_IMPL *conn_impl = S2C(session);
     WT_NAMED_PAGE_LOG *npl;
 
@@ -51,22 +57,18 @@ void setup_page_log_queue(WT_SESSION_IMPL *session) {
     REQUIRE(__wt_strdup(session, "mock", &npl->name) == 0);
 
     /* Insert mock page log into mock connection. */
-    TAILQ_INIT(&conn_impl->pagelogqh); 
+    TAILQ_INIT(&conn_impl->pagelogqh);
     TAILQ_INSERT_TAIL(&conn_impl->pagelogqh, npl, q);
 }
 
-TEST_CASE("Test Disaggregate configuration logic", "[disagg_config]")
-{ 
+TEST_CASE("Test disaggregated configuration logic", "[disagg_config]")
+{
     std::shared_ptr<mock_session> session_mock = mock_session::build_test_mock_session();
 
     WT_CONNECTION_IMPL *conn_impl = session_mock->get_mock_connection()->get_wt_connection_impl();
     WT_SESSION_IMPL *session = session_mock->get_wt_session_impl();
-    
 
-    const char *disagg_cfg[] = {
-        "disaggregated=(role=follower,page_log=mock)",
-        NULL
-    };
+    const char *disagg_cfg[] = {"disaggregated=(role=follower,page_log=mock)", NULL};
 
     /* Setup the page log queue. */
     setup_page_log_queue(session);
@@ -78,7 +80,9 @@ TEST_CASE("Test Disaggregate configuration logic", "[disagg_config]")
         REQUIRE(conn_impl->disaggregated_storage.npage_log != nullptr);
         REQUIRE(conn_impl->disaggregated_storage.page_log_meta != nullptr);
 
-        /* The key provider log handle should not be constructed if no custom key provider is set. */
+        /*
+         * The key provider log handle should not be constructed if no custom key provider is set.
+         */
         REQUIRE(conn_impl->disaggregated_storage.page_log_key_provider == nullptr);
 
         free(conn_impl->disaggregated_storage.page_log);
@@ -89,7 +93,7 @@ TEST_CASE("Test Disaggregate configuration logic", "[disagg_config]")
     SECTION("Test key provider handle is constructed")
     {
         WT_KEY_PROVIDER kp;
-        conn_impl->key_provider = &kp; // Dummy non-nullptr value
+        conn_impl->key_provider = &kp; /* Dummy non-nullptr value. */
 
         REQUIRE(__wti_disagg_conn_config(session, disagg_cfg, false) == WT_ERROR);
         REQUIRE(conn_impl->disaggregated_storage.page_log != nullptr);
@@ -109,7 +113,7 @@ TEST_CASE("Test Disaggregate configuration logic", "[disagg_config]")
         conn_impl->disaggregated_storage.page_log_meta = &mock_page_log_handle;
         conn_impl->disaggregated_storage.page_log_key_provider = &mock_page_log_handle;
         REQUIRE(__wti_disagg_destroy(session) == 0);
-        
+
         REQUIRE(conn_impl->disaggregated_storage.page_log_meta == nullptr);
         REQUIRE(conn_impl->disaggregated_storage.page_log_key_provider == nullptr);
     }
