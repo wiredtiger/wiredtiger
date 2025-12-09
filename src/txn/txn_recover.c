554475d338b13a62563c79bbcb59014caae1983a
jchen <jie.chen@mongodb.com>
2025-12-09 00:49:34 +0000

    Fixed segfault

--- src/txn/txn_recover.c
+++ src/txn/txn_recover.c
@@ -842,6 +842,7 @@ __metadata_clean_incomplete_table(WT_RECOVERY *r, const char *uri, const char *c
     const char *cfg[] = {config, NULL};
     const char *drop_cfg[] = {WT_CONFIG_BASE(r->session, WT_SESSION_drop), "force=true", NULL};
     const char *name;
+    u_int i; 
 
     /*
      * We are only interested in the meta information of the table handle. Therefore generate a fake
@@ -874,9 +875,12 @@ __metadata_clean_incomplete_table(WT_RECOVERY *r, const char *uri, const char *c
 
 err:
 done:
-    /* Free allocated structure within the table handle. */
-    WT_WITH_TABLE_WRITE_LOCK(
-      r->session, WT_TRET(__wt_schema_close_table(r->session, &fake_table_handle)));
+    /* Free colgroup allocated structure within the table handle. */
+    if (fake_table_handle.cgroups != NULL) {
+        for (i = 0; i < WT_COLGROUPS(&fake_table_handle); i++)
+            __wti_schema_destroy_colgroup(r->session, &fake_table_handle.cgroups[i]);
+        __wt_free(r->session, fake_table_handle.cgroups);
+    }
     return (ret);
 }
 
