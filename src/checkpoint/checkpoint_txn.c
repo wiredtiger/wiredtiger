0ca00d0c89e997e89028aeefd43c663ef788199c
jchen <jie.chen@mongodb.com>
2025-12-19 01:17:13 +0000

    Move location of crypt call

--- src/checkpoint/checkpoint_txn.c
+++ src/checkpoint/checkpoint_txn.c
@@ -1536,6 +1536,12 @@ __checkpoint_db_internal(WT_SESSION_IMPL *session, const char *cfg[])
      * commit as the updates have gone from memory.
      */
     if (__wt_conn_is_disagg(session) && conn->layered_table_manager.leader) {
+        /*
+         * Gather any updated key encryption information so it can be written into the shared
+         * metadata table.
+         */
+        if (conn->key_provider != NULL)
+            WT_ERR(__wt_disagg_put_crypt_helper(session));
         WT_ERR(__wt_session_get_dhandle(session, WT_DISAGG_METADATA_URI, NULL, NULL, 0));
         if (S2BT(session)->modified)
             WT_ERR(__wt_checkpoint_file(session, cfg));
@@ -1545,14 +1551,6 @@ __checkpoint_db_internal(WT_SESSION_IMPL *session, const char *cfg[])
     saved_meta_next = session->meta_track_next;
     session->meta_track_next = NULL;
     WT_STAT_CONN_SET(session, checkpoint_state, WTI_CHECKPOINT_STATE_META_CKPT);
-
-    /*
-     * Metadata checkpoints rewrite the turtle file. Before that happens, gather any updated key
-     * encryption information so it can be written into the new turtle file.
-     */
-    if (__wt_conn_is_disagg(session) && conn->layered_table_manager.leader &&
-      conn->key_provider != NULL)
-        WT_ERR(__wt_disagg_put_crypt_helper(session));
 
     WT_WITH_DHANDLE(session, WT_SESSION_META_DHANDLE(session),
       WT_WITH_METADATA_LOCK(session, ret = __wt_checkpoint_file(session, cfg)));
