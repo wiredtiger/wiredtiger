29aa0a01c332c7c1c9397b1cfbe8a906d2c95e3a
jchen <29520270+jiechenbo@users.noreply.github.com>
2025-12-22 12:11:04 +1100

    WT-16054 Implement KEK write PALI path mechanism (#12926)
    Originally this ticket had the full write path mechanism completed but
    it was too big of a PR. I split it into two PRs. I intend to do the
    second part and write extensive testing in WT-16290 (pt.2).
    
    This ticket will focus on writing the PALI mechanism to the new key
    provider table with no header. This ticket plans to establish a write
    mechanism to PALI as such:
    - WT calls the new API get_key_blob to retrieve the current KEK blob. If
    the call indicates that no changes were made, we skip early.
    - WT calls the get_complete API to return the LSN to the encryption
    layer, and to discard the memory used to store the blob.
    
    **Note: There is minimal testing for this PR. I plan to write extensive
    testing in WT-16290.**
    The work on adding the header will be done in WT-16298.

--- src/include/extern.h
+++ src/include/extern.h
@@ -569,6 +569,8 @@ extern int __wt_disagg_copy_shared_metadata_layered(WT_SESSION_IMPL *session, co
 extern int __wt_disagg_put_checkpoint_meta(WT_SESSION_IMPL *session, const char *checkpoint_root,
   size_t checkpoint_root_size, uint64_t checkpoint_timestamp)
   WT_GCC_FUNC_DECL_ATTRIBUTE((warn_unused_result));
+extern int __wt_disagg_put_crypt_helper(WT_SESSION_IMPL *session)
+  WT_GCC_FUNC_DECL_ATTRIBUTE((warn_unused_result));
 extern int __wt_disagg_update_shared_metadata(WT_SESSION_IMPL *session, const char *key,
   const char *value) WT_GCC_FUNC_DECL_ATTRIBUTE((warn_unused_result));
 extern int __wt_encrypt(WT_SESSION_IMPL *session, WT_KEYED_ENCRYPTOR *kencryptor, size_t skip,
