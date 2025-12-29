89c6cf040fea61124b59d9eda202540c0d59ce90
jchen <jie.chen@mongodb.com>
2025-12-29 03:44:24 +0000

    Add support

--- test/format/wts.c
+++ test/format/wts.c
@@ -334,6 +334,7 @@ configure_disagg_storage(const char *home, char **p, size_t max, char *ext_cfg, 
     opts.build_dir = (char *)BUILDDIR;
     opts.palm_map_size_mb = 2048; /* 2 Gigabytes for PALM map */
     opts.page_log_verbose = GV(DISAGG_PAGE_LOG_VERBOSE);
+    opts.disagg_key_provider = GV(DISAGG_KEY_PROVIDER);
 
     /* Set page deltas. */
     opts.internal_page_delta = (bool)GV(DISAGG_INTERNAL_PAGE_DELTA);
