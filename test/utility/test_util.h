848900513e099a39bada7cf9cb4693aa8ebc0400
jchen <jie.chen@mongodb.com>
2025-12-29 03:48:16 +0000

    Add early load

--- test/utility/test_util.h
+++ test/utility/test_util.h
@@ -87,7 +87,7 @@ extern "C" {
     ",force_error=%" PRIu64 ",cache_size_mb=%" PRIu64 ",verbose=%" PRIu32 "))"
 #define TESTUTIL_ENV_CONFIG_KEY_PROVIDER_EXT                       \
     ",\"%s/ext/test/key_provider/libwiredtiger_key_provider.so\"=(" \
-    "config=(key_expires=60,verbose=-1))"
+    "early_load=true,config=(key_expires=60,verbose=-1))"
 #define TESTUTIL_ENV_CONFIG_TIERED               \
     ",tiered_storage=(bucket=%s"                 \
     ",bucket_prefix=%s,local_retention=%" PRIu32 \
