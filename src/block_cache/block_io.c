a286205de56307a3b40f73abba3e60838ae0fd85
Mariam Mojid <mariammojid@gmail.com>
2026-01-05 04:47:36 +0000

    mem leak in bc read multi

--- src/block_cache/block_io.c
+++ src/block_cache/block_io.c
@@ -624,6 +624,17 @@ __wt_blkcache_read_multi(WT_SESSION_IMPL *session, WT_ITEM **buf, size_t *buf_co
 
     if (0) {
 err:
+        /* Single read path. tmp points to a single WT_ITEM with a buffer */
+        if (bm->read_multiple == NULL)
+            /* free tmp[0].mem */
+            __wt_buf_free(session, tmp);
+
+        else {
+            /* Multi-read path. Results array is the current owner of any buffer we've allocated */
+            for (i = 0; i < count; ++i)
+                __wt_buf_free(session, &results[i]);
+        }
+
         __wt_free(session, tmp);
         __wt_scr_free(session, &etmp);
         __wt_scr_free(session, &ctmp);
