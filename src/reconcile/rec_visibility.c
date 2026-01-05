34cbcd3c585bf1decf53af9fdcd9f79eabd02aae
Linh Tran <linh.tran@mongodb.com>
2026-01-05 00:38:20 +0000

    remove macro

--- src/reconcile/rec_visibility.c
+++ src/reconcile/rec_visibility.c
@@ -1028,8 +1028,8 @@ __wti_rec_upd_select(WT_SESSION_IMPL *session, WTI_RECONCILE *r, WT_INSERT *ins,
     if (!WT_IS_HS(session->dhandle) && upd_select->tombstone != NULL &&
       !F_ISSET(upd_select->tombstone, WT_UPDATE_RESTORED_FROM_DS | WT_UPDATE_RESTORED_FROM_HS)) {
         if (upd_select->tw.start_ts > upd_select->tw.stop_ts ||
-          (WT_REC_HAS_ON_DISK(vpack) && vpack->tw.start_ts > upd_select->tw.stop_ts &&
-            upd_select->tw.stop_ts == WT_TS_NONE)) {
+          (vpack != NULL && vpack->type != WT_CELL_DEL &&
+            vpack->tw.start_ts > upd_select->tw.stop_ts && upd_select->tw.stop_ts == WT_TS_NONE)) {
             WT_ASSERT(session, upd_select->tw.stop_ts == WT_TS_NONE);
             upd_select->no_ts_tombstone = true;
         }
