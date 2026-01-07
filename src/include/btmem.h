f3ad3c556133ddc3de1ebd44ba52d1e110c801ab
Chenhao Qu <chenhao.qu@mongodb.com>
2026-01-07 11:01:40 +0000

    WT-16410 Remove image_size from block meta

--- src/include/btmem.h
+++ src/include/btmem.h
@@ -263,11 +263,9 @@ struct __wt_page_block_meta {
     uint64_t backlink_lsn;
     uint64_t base_lsn;
 
-    uint32_t checksum;
-
-    size_t image_size; /* The in-memory size of the fully constructed page image. */
-
     WT_PAGE_LOG_ENCRYPTION encryption;
+
+    uint32_t checksum;
 
     uint8_t delta_count;
 };
