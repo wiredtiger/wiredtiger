fd7d5182def8bba98a9c0423fa7bae8d5b941078
jchen <jie.chen@mongodb.com>
2026-01-16 05:24:22 +0000

    Add test

---
+++ test/catch2/ext/test_key_provider_header.cpp
@@ -0,0 +1,135 @@
+/*-
+ * Copyright (c) 2014-present MongoDB, Inc.
+ * Copyright (c) 2008-2014 WiredTiger, Inc.
+ *	All rights reserved.
+ *
+ * See the file LICENSE for redistribution information.
+ */
+#include "wt_internal.h"
+
+#include <catch2/catch.hpp>
+#include "../wrappers/mock_session.h"
+void create_crypt_key_buffer(WT_SESSION_IMPL *session, WT_CRYPT_KEYS *crypt, std::string &str);
+
+/* Fixture to initialize the checksum function needed by __wt_checksum and __wt_checksum_match. */
+struct checksum_fixture {
+    checksum_fixture()
+    {
+        // Initialize the checksum function if not already initialized.
+        if (__wt_process.checksum == nullptr)
+            __wt_process.checksum = wiredtiger_crc32c_func();
+    }
+};
+
+void
+create_crypt_key_buffer(WT_SESSION_IMPL *session, WT_CRYPT_KEYS *crypt, std::string &str)
+{
+    /* Allocate a buffer and make sure we leave enough space for the header at the start. */
+    REQUIRE(__wt_buf_initsize(session, &crypt->keys, 512) == 0);
+    memcpy(((uint8_t *)(crypt->keys.data) + sizeof(WT_CRYPT_HEADER)), str.data(), str.size());
+    crypt->keys.size = str.size();
+    crypt->keys.data = (uint8_t *)(crypt->keys.data) + sizeof(WT_CRYPT_HEADER);
+}
+
+TEST_CASE("Key provider set header function", "[key_provider_header]")
+{
+    checksum_fixture fixture;
+    WT_CRYPT_KEYS crypt;
+    std::shared_ptr<mock_session> session = mock_session::build_test_mock_session();
+
+    std::string test_string("hello");
+
+    WT_SESSION_IMPL *session_impl = session->get_wt_session_impl();
+    create_crypt_key_buffer(session_impl, &crypt, test_string);
+    uint32_t checksum = __wt_checksum(crypt.keys.data, crypt.keys.size);
+    SECTION("Validate key provider pack")
+    {
+
+        __ut_disagg_set_crypt_header(session->get_wt_session_impl(), &crypt);
+
+        /* Validate crypt header information. */
+        WT_CRYPT_HEADER write_crypt_header;
+        memcpy(&write_crypt_header, crypt.keys.data, sizeof(WT_CRYPT_HEADER));
+        REQUIRE(write_crypt_header.signature == WT_CRYPT_HEADER_SIGNATURE);
+        REQUIRE(write_crypt_header.version == WT_CRYPT_HEADER_VERSION);
+        REQUIRE(write_crypt_header.compatible_version == WT_CRYPT_HEADER_COMPATIBLE_VERSION);
+        REQUIRE(write_crypt_header.header_size == sizeof(WT_CRYPT_HEADER));
+        REQUIRE(write_crypt_header.crypt_size == test_string.size());
+        REQUIRE(write_crypt_header.checksum == checksum);
+        REQUIRE(crypt.keys.size == test_string.size() + sizeof(WT_CRYPT_HEADER));
+    }
+    __wt_buf_free(session->get_wt_session_impl(), &crypt.keys);
+}
+
+TEST_CASE("Key provider: validate crypt function", "[key_provider_header]")
+{
+    checksum_fixture fixture;
+    WT_CRYPT_KEYS crypt;
+    std::shared_ptr<mock_session> session = mock_session::build_test_mock_session();
+
+    WT_CLEAR(crypt.keys);
+    std::string test_string("hello");
+    WT_SESSION_IMPL *session_impl = session->get_wt_session_impl();
+
+    /* Set the header inside the crypt struct. */
+    create_crypt_key_buffer(session_impl, &crypt, test_string);
+    __ut_disagg_set_crypt_header(session_impl, &crypt);
+
+    WT_CRYPT_HEADER read_crypt_header;
+    SECTION("Test key provider basic unpack")
+    {
+        /* Fetch the baseline header before performing read. */
+        WT_CRYPT_HEADER write_crypt_header;
+        memcpy(&write_crypt_header, crypt.keys.data, sizeof(WT_CRYPT_HEADER));
+
+        REQUIRE(__ut_disagg_validate_crypt(session_impl, &crypt.keys, &read_crypt_header) == 0);
+
+        /* Validate that before and after validation should not change the header. */
+        REQUIRE(memcmp(&write_crypt_header, &read_crypt_header, sizeof(WT_CRYPT_HEADER)) == 0);
+    }
+
+    SECTION("Test key provider header version")
+    {
+        /* Fetch the baseline header before performing read. */
+        WT_CRYPT_HEADER write_crypt_header;
+        memcpy(&write_crypt_header, crypt.keys.data, sizeof(WT_CRYPT_HEADER));
+        write_crypt_header.version = 2;
+
+        WT_CRYPT_HEADER *header = (WT_CRYPT_HEADER *)crypt.keys.data;
+        header->compatible_version = 1;
+        header->version = 2;
+        REQUIRE(__ut_disagg_validate_crypt(session_impl, &crypt.keys, &read_crypt_header) == 0);
+
+        /* Validate that before and after validation should not change the header. */
+        REQUIRE(memcmp(&write_crypt_header, &read_crypt_header, sizeof(WT_CRYPT_HEADER)) == 0);
+    }
+
+    SECTION("Test key provider header compatibility issue")
+    {
+        WT_CRYPT_HEADER *header = (WT_CRYPT_HEADER *)crypt.keys.data;
+        header->compatible_version = 2;
+        header->version = 1;
+        REQUIRE(
+          __ut_disagg_validate_crypt(session_impl, &crypt.keys, &read_crypt_header) == ENOTSUP);
+    }
+
+    SECTION("Test key provider header size smaller than header")
+    {
+        crypt.keys.size = 1;
+        REQUIRE(__ut_disagg_validate_crypt(session_impl, &crypt.keys, &read_crypt_header) == EIO);
+    }
+
+    SECTION("Test key provider header mismatch size")
+    {
+        crypt.keys.size = 50;
+        REQUIRE(__ut_disagg_validate_crypt(session_impl, &crypt.keys, &read_crypt_header) == EIO);
+    }
+
+    SECTION("Test key provider header mismatch checksum")
+    {
+        WT_CRYPT_HEADER *header = (WT_CRYPT_HEADER *)crypt.keys.data;
+        header->checksum = 123;
+        REQUIRE(__ut_disagg_validate_crypt(session_impl, &crypt.keys, &read_crypt_header) == EIO);
+    }
+    __wt_buf_free(session_impl, &crypt.keys);
+}
