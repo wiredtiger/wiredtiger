42d725d18b4ed80add02e76a4c49c7aa58339262
Mariam Mojid <mariammojid@gmail.com>
2026-01-21 04:13:13 +0000

    simplify fs_free_Space func to take a path instead of a file handle

--- src/include/os_fs_inline.h
+++ src/include/os_fs_inline.h
@@ -227,14 +227,14 @@ __wt_fs_size(WT_SESSION_IMPL *session, const char *name, wt_off_t *sizep)
 
 /*
  * __wt_fs_free_space --
- *     Get the free disk space for the filesystem containing the given file.
+ *     Get the free disk space for the filesystem.
  */
 static WT_INLINE int
-__wt_fs_free_space(WT_SESSION_IMPL *session, WT_FH *fh, wt_off_t *freep)
+__wt_fs_free_space(WT_SESSION_IMPL *session, const char *path, wt_off_t *freep)
 {
 #ifdef _WIN32
-    return (__wt_win_fs_free_space(session, fh, freep));
+    return (__wt_win_fs_free_space(session, path, freep));
 #else
-    return (__wt_posix_fs_free_space(session, fh, freep));
+    return (__wt_posix_fs_free_space(session, path, freep));
 #endif
 }
