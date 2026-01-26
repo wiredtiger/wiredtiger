372057658bfa5d6d2947d48fdc05e3f56b846861
Etienne Petrel <etienne.petrel@mongodb.com>
2026-01-26 11:28:02 -0500

    Update FIXME

--- src/conn/conn_layered.c
+++ src/conn/conn_layered.c
@@ -1779,7 +1779,7 @@ __disagg_abandon_checkpoint(WT_SESSION_IMPL *session)
         WT_RET(EINVAL);
 
     /*
-     * FIXME-WT-XXX: This function is no longer an optional operation for testing, remove this
+     * FIXME-WT-16524: This function is no longer an optional operation for testing, remove this
      * check.
      */
     if (disagg->npage_log->page_log->pl_abandon_checkpoint == NULL) {
