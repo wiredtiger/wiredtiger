302894137f488bfd09efe7b65d0ca1e611910336
Don Anderson <dda@ddanderson.com>
2026-01-28 16:23:13 -0500

    Turn assert back to return, as disagg shared trees on follower can apply changes in some cases.

--- src/include/btree_inline.h
+++ src/include/btree_inline.h
@@ -891,11 +891,10 @@ __wt_page_only_modify_set(WT_SESSION_IMPL *session, WT_PAGE *page)
 
     /*
      * At the moment, disaggregated shared btrees on the follower are not marked as read-only,
-     * although they effectively are. Make sure we're not trying to modify such a tree.
+     * although they effectively are.
      */
-    WT_ASSERT_ALWAYS(session,
-      !F_ISSET(btree, WT_BTREE_DISAGGREGATED) || conn->layered_table_manager.leader,
-      "Attempt to modify a shared btree on a follower");
+    if (F_ISSET(btree, WT_BTREE_DISAGGREGATED) && !conn->layered_table_manager.leader)
+        return;
 
     /*
      * This is a relatively complex dance of operations so pay attention prior to modifying the code
