c432aa3708f0a41102b8a5ff2770bf43f6e8fbaf
Alex Pullen <alex.pullen@mongodb.com>
2026-01-28 01:17:01 +0000

    WT-16269 Make 'drain_threads' configurable in test format

--- test/utility/test_util.h
+++ test/utility/test_util.h
@@ -77,9 +77,9 @@ extern "C" {
 #define S3_STORE "s3_store"
 
 /* FIXME-WT-16269: Make drain_threads configurable in test format. */
-#define TESTUTIL_ENV_CONFIG_DISAGG                         \
-    ",disaggregated=(role=%s,page_log=%s,drain_threads=4)" \
-    ",precise_checkpoint=true"                             \
+#define TESTUTIL_ENV_CONFIG_DISAGG         \
+    ",disaggregated=(role=%s,page_log=%s)" \
+    ",precise_checkpoint=true"             \
     ",page_delta=(internal_page_delta=%s,leaf_page_delta=%s)"
 #define TESTUTIL_ENV_CONFIG_DISAGG_EXT                                                   \
     "\"%s/ext/page_log/%s/libwiredtiger_%s.so\"=("                                       \
