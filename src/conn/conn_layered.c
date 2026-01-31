ca0ec5a2f204105ae38712b4ebd5ea191ba50eb4
Siddhartha8899 <siddhartha.mahajan8899@gmail.com>
2026-01-31 04:55:26 +0000

    Fix schema lock deadlock

--- src/conn/conn_layered.c
+++ src/conn/conn_layered.c
@@ -1023,8 +1023,8 @@ err:
  *     Process the metadata entries stored in the shared metadata table for a new checkpoint.
  */
 static int
-__disagg_apply_checkpoint_meta(WT_SESSION_IMPL *session, WT_SESSION_IMPL *internal_session,
-  WT_CURSOR *md_cursor, const WT_DISAGG_CHECKPOINT_META *ckpt_meta)
+__disagg_apply_checkpoint_meta(
+  WT_SESSION_IMPL *session, WT_CURSOR *md_cursor, const WT_DISAGG_CHECKPOINT_META *ckpt_meta)
 {
     WT_CONFIG_ITEM cval;
     WT_CURSOR *cursor;
@@ -1156,7 +1156,7 @@ __disagg_apply_checkpoint_meta(WT_SESSION_IMPL *session, WT_SESSION_IMPL *intern
                     if (ret == WT_NOTFOUND) {
                         WT_ERR_MSG_CHK(session,
                           __layered_create_missing_ingest_table(
-                            internal_session, layered_ingest_uri, metadata_value),
+                            session, layered_ingest_uri, metadata_value),
                           "Failed to create missing ingest table \"%s\" from \"%s\"",
                           layered_ingest_uri, metadata_value);
                         new_ingest++;
@@ -1320,8 +1320,7 @@ __disagg_pick_up_checkpoint(WT_SESSION_IMPL *session, const WT_DISAGG_CHECKPOINT
     WT_ERR(__wt_open_internal_session(
       conn, "checkpoint-pick-up-shared", false, 0, 0, &shared_metadata_session));
     WT_WITH_SCHEMA_LOCK(shared_metadata_session,
-      ret = __disagg_apply_checkpoint_meta(
-        shared_metadata_session, internal_session, md_cursor, ckpt_meta));
+      ret = __disagg_apply_checkpoint_meta(shared_metadata_session, md_cursor, ckpt_meta));
     WT_TRET(__wt_session_close_internal(shared_metadata_session));
     WT_ERR(ret);
 
