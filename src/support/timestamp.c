0f383de21ec78817cd50a0cde5876d12f2979af3
Chenhao Qu <chenhao.qu@mongodb.com>
2026-02-01 23:43:51 +0000

    Fix oldest check

--- src/support/timestamp.c
+++ src/support/timestamp.c
@@ -428,7 +428,7 @@ __time_value_validate_parent(
               "%s, parent %s",
               __wt_time_window_to_string(tw, time_string[0]),
               __wt_time_aggregate_to_string(parent, time_string[1]));
-    } else if (tw->start_ts < parent->oldest_start_ts)
+    } else if (tw->start_ts != WT_TS_NONE && tw->start_ts < parent->oldest_start_ts)
         WT_TIME_VALIDATE_RET(session,
           "value time window has a start time before its parent's oldest start time; time window "
           "%s, parent %s",
@@ -458,7 +458,7 @@ __time_value_validate_parent(
               "parent %s",
               __wt_time_window_to_string(tw, time_string[0]),
               __wt_time_aggregate_to_string(parent, time_string[1]));
-    } else if (tw->stop_ts > parent->newest_stop_ts)
+    } else if (tw->stop_ts != WT_TS_NONE && tw->stop_ts > parent->newest_stop_ts)
         WT_TIME_VALIDATE_RET(session,
           "value time window has a stop time after its parent's newest stop time; time window %s, "
           "parent %s",
