1afcb9050e0976041518b50c5bf82c6d82e5c4f3
Chenhao Qu <chenhao.qu@mongodb.com>
2026-02-04 07:03:32 +0000

    WT-16605 Set WT_BTREE_IN_MEMORY for all the btrees in the in-memory database

--- src/btree/bt_page.c
+++ src/btree/bt_page.c
@@ -1094,8 +1094,7 @@ __wti_page_inmem_updates(WT_SESSION_IMPL *session, WT_REF *ref)
     WT_ASSERT(session, !F_ISSET(btree, WT_BTREE_READONLY));
 
     /* We don't handle in-memory prepare resolution here. */
-    WT_ASSERT(
-      session, !F_ISSET(S2C(session), WT_CONN_IN_MEMORY) && !F_ISSET(btree, WT_BTREE_IN_MEMORY));
+    WT_ASSERT(session, !F_ISSET(btree, WT_BTREE_IN_MEMORY));
 
     __wt_btcur_init(session, &cbt);
     __wt_btcur_open(&cbt);
