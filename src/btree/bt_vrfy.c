c74d0906a03f3f6e5b824b8660662a5872e4333f
Mariam Mojid <mariammojid@gmail.com>
2026-02-06 06:39:39 +0000

    reset total block size after each ckpt

--- src/btree/bt_vrfy.c
+++ src/btree/bt_vrfy.c
@@ -429,6 +429,9 @@ __verify_checkpoint_reset(WT_VSTUFF *vs)
 
     /* Tree depth. */
     vs->depth = 1;
+
+    /* Accumulated size of all block in the btree. */
+    vs->total_block_size = 0;
 }
 
 /*
