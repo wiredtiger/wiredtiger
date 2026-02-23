11aea7ce3b7649dc4d5c290fe14e16f79e9553b3
Chenhao Qu <chenhao.qu@mongodb.com>
2026-02-23 21:59:56 +0000

    Remove a comment

--- src/txn/txn_timestamp.c
+++ src/txn/txn_timestamp.c
@@ -454,11 +454,6 @@ set:
         stable_ts > __wt_atomic_load_uint64_relaxed(&txn_global->stable_timestamp))) {
         __wt_atomic_store_uint64_relaxed(&txn_global->stable_timestamp, stable_ts);
         WT_STAT_CONN_INCR(session, txn_set_ts_stable_upd);
-        /*
-         * Release write requires the data and destination have exactly the same size. stdbool.h
-         * only defines true as `#define true 1` so we need a bool cast to provide proper type
-         * information.
-         */
         __wt_atomic_store_bool_release(&txn_global->has_stable_timestamp, true);
         txn_global->stable_is_pinned = false;
         __wt_verbose_timestamp(session, stable_ts, "Updated global stable timestamp");
