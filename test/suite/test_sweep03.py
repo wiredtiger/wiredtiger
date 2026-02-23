260ad942918a4761d189eb1ae3949ab6c14aa076
jchen <jie.chen@mongodb.com>
2026-02-23 13:47:38 +0000

    Move ski[

--- test/suite/test_sweep03.py
+++ test/suite/test_sweep03.py
@@ -107,8 +107,6 @@ class test_sweep03(wttest.WiredTigerTestCase, suite_subprocess):
         # We expect nothing to have been closed.
         self.assertEqual(close1, 0)
 
-    # FIXME-WT-16757: Enable on disagg once issue has been investigated.
-    @wttest.skip_for_hook("disagg", "Fails with drop")
     @wttest.skip_for_hook("tiered", "Fails with tiered storage")
     def test_disable_idle_timeout_drop_force(self):
         # Create a table to drop. A drop should close its associated handle
@@ -149,6 +147,8 @@ class test_sweep03(wttest.WiredTigerTestCase, suite_subprocess):
         # Ensure that any space was reclaimed from cache.
         self.assertLess(cache2, cache1)
 
+    # FIXME-WT-16757: Enable on disagg once issue has been investigated.
+    @wttest.skip_for_hook("disagg", "Fails with disagg")
     @wttest.skip_for_hook("tiered", "Fails with tiered storage")
     def test_disable_idle_timeout_drop(self):
         # Create a table to drop. A drop should close its associated handles
