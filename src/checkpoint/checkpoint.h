660004a82da0a6e2c3ef7d2f488f3298b67a9771
luke.pearson <luke.pearson@mongodb.com>
2026-02-27 02:33:54 +0000

    s_fast

--- src/checkpoint/checkpoint.h
+++ src/checkpoint/checkpoint.h
@@ -215,7 +215,7 @@ struct __wt_checkpoint_reconcile_threads {
  * WT_PARALLEL_CHECKPOINTS_ENABLED --
  *     Check whether parallel checkpoints are enabled.
  */
-#define WT_PARALLEL_CHECKPOINTS_ENABLED(session)                                            \
+#define WT_PARALLEL_CHECKPOINTS_ENABLED(session)                                           \
     (FLD_ISSET(S2C(session)->server_flags, WT_CONN_SERVER_CHECKPOINT_RECONCILE_THREADS) && \
       (S2C(session))->ckpt_reconcile_threads->num_threads > 1)
 
