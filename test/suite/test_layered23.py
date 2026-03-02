2056b6f20537f573d64e24a22a872073020a1694
Dylan Liang <dylan.liang@mongodb.com>
2026-03-02 00:36:16 +0000

    verify checkpoint pick up by follower in test_layered23.py

--- test/suite/test_layered23.py
+++ test/suite/test_layered23.py
@@ -49,6 +49,11 @@ class test_layered23(wttest.WiredTigerTestCase):
         self.assertEqual(stat_cur[stat.conn.checkpoints_total_succeed][2], expected)
         stat_cur.close()
 
+    def check_checkpoints_picked_up_follower(self, session_follow, expected):
+        stat_cur = session_follow.open_cursor('statistics:', None, None)
+        self.assertEqual(stat_cur[stat.conn.checkpoints_picked_up_follower][2], expected)
+        stat_cur.close()
+
     # Test simple inserts to a leader/follower
     def test_leader_follower(self):
         # Create the oplog
@@ -96,6 +101,7 @@ class test_layered23(wttest.WiredTigerTestCase):
         # Then advance the checkpoint and make sure everything is still good
         self.pr('advance checkpoint')
         self.disagg_advance_checkpoint(conn_follow)
+        self.check_checkpoints_picked_up_follower(session_follow, checkpoint_count)
         oplog.check(self, session_follow, 0, 2100)
 
         # Now go back to leader, checkpoint and insert more.
@@ -139,6 +145,7 @@ class test_layered23(wttest.WiredTigerTestCase):
             # advance checkpoint
             self.pr('advance checkpoint')
             self.disagg_advance_checkpoint(conn_follow)
+            self.check_checkpoints_picked_up_follower(session_follow, checkpoint_count)
 
             # The check begins at 0, which means this test will have quadratic performance.
             self.pr(f'checking follower from pos 0 to {follower_pos} after checkpoint pick-up')
