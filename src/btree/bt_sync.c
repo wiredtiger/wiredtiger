1fe2dd1241267398b71613afa8115cabe75b3ba0
luke.pearson <luke.pearson@mongodb.com>
2026-03-02 19:53:19 +0000

    Add WT_ERR

--- src/btree/bt_sync.c
+++ src/btree/bt_sync.c
@@ -375,7 +375,7 @@ __wt_sync_file(WT_SESSION_IMPL *session, WT_CACHE_OP syncop)
                  * existing walk position will be release by the walk code.
                  */
                 WT_REF *walk_dup = NULL;
-                __sync_dup_walk(session, walk, 0, &walk_dup);
+                WT_ERR(__sync_dup_walk(session, walk, 0, &walk_dup));
                 WT_ERR(__wt_checkpoint_parallel_push_work(session, walk_dup, rec_flags, flags));
             } else
                 /* It's not an error if we make no progress. */
