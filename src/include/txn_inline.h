0841bbb5554d2aaf8ce07cb51d4e2423594aac31
Chenhao Qu <chenhao.qu@mongodb.com>
2026-03-02 04:47:55 +0000

    Rerfactor a long check

--- src/include/txn_inline.h
+++ src/include/txn_inline.h
@@ -1791,10 +1791,17 @@ __txn_incr_bytes_dirty(WT_SESSION_IMPL *session, size_t size, bool new_update)
      * only the application's own changes to page data structures. Exclude changes to internal pages
      * or changes that are the result of the application thread being co-opted into eviction work.
      */
-    if (!new_update || F_ISSET(session, WT_SESSION_INTERNAL) ||
-      (!F_ISSET(session->txn, WT_TXN_RUNNING) &&
-        !F_ISSET(&session->txn->time_point, WT_TXN_TIME_POINT_HAS_ID)) ||
-      __wt_session_gen(session, WT_GEN_EVICT) != 0)
+    if (!new_update)
+        return;
+
+    if (F_ISSET(session, WT_SESSION_INTERNAL))
+        return;
+
+    if (!F_ISSET(session->txn, WT_TXN_RUNNING) &&
+      !F_ISSET(&session->txn->time_point, WT_TXN_TIME_POINT_HAS_ID))
+        return;
+
+    if (__wt_session_gen(session, WT_GEN_EVICT) != 0)
         return;
 
     WT_STAT_CONN_INCRV_ATOMIC(session, cache_updates_txn_uncommitted_bytes, (int64_t)size);
