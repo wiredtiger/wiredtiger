fc98a97819fca5c3324a05cb4ce72684928a7148
luke.pearson <luke.pearson@mongodb.com>
2026-03-05 18:43:30 +0000

    whitespace

--- src/include/wt_internal.h
+++ src/include/wt_internal.h
@@ -393,7 +393,6 @@ struct __wt_save_upd;
 typedef struct __wt_save_upd WT_SAVE_UPD;
 struct __wt_scratch_track;
 typedef struct __wt_scratch_track WT_SCRATCH_TRACK;
-
 struct __wt_session_impl;
 typedef struct __wt_session_impl WT_SESSION_IMPL;
 struct __wt_session_stash;
