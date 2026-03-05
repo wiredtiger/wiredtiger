wiredtiger/wiredtiger

commit c3d0e64dc0dfb36469e5bc5fb5d2ebcec9b016a5

WT-16638 Improve stats for cache stuck diagnosis (#13170)
Enhance stats to understand cache stuck scenario's.
The changes are two parts:
1. stats to represent the global timestamp values in txn_global
structure. These are temporary, to understand cache stuck scenario's for
disagg.
2. Improved the stats to understand:
a) Number of older readers with read timestamp, older than the oldest
timestamp.
b) change the stat representing "transaction range of timestamps
currently pinned" to capture the lag of pinned timestamp from the oldest
timestamp.
c) change the stat representing "transaction range of timestamps pinned
by the oldest active read timestamp" to capture the lag of the oldest
reader's read timestamp from the oldest timestamp.

src/txn/txn.c
@@ -2309,45 +2309,78 @@ __wt_txn_stats_update(WT_SESSION_IMPL *session)
     WT_CONNECTION_IMPL *conn;
     WT_CONNECTION_STATS **stats;
     WT_TXN_GLOBAL *txn_global;
-    wt_timestamp_t checkpoint_timestamp;
+    wt_timestamp_t checkpoint_timestamp, checkpoint_pinned_ts_lag;
     wt_timestamp_t durable_timestamp;
-    wt_timestamp_t oldest_active_read_timestamp;
+    wt_timestamp_t oldest_active_read_timestamp, oldest_reader_lag;
     wt_timestamp_t oldest_timestamp;
     wt_timestamp_t pinned_timestamp;
     uint64_t checkpoint_pinned;
 
     conn = S2C(session);
+    checkpoint_pinned = WT_TXN_NONE;
+    checkpoint_pinned_ts_lag = oldest_reader_lag = WT_TS_NONE;
+    checkpoint_timestamp = WT_TS_NONE;
     txn_global = &conn->txn_global;
     stats = conn->stats;
-    checkpoint_pinned =
-      __wt_atomic_load_uint64_v_relaxed(&txn_global->checkpoint_txn_shared.pinned_id);
 
     WT_STATP_CONN_SET(session, stats, txn_pinned_range,
       __wt_atomic_load_uint64_v_relaxed(&txn_global->current) -
         __wt_atomic_load_uint64_v_relaxed(&txn_global->oldest_id));
 
-    checkpoint_timestamp = __wt_atomic_load_uint64_relaxed(&txn_global->checkpoint_timestamp);
+    if (__wt_atomic_load_bool_v_relaxed(&txn_global->checkpoint_running)) {
+        checkpoint_timestamp = __wt_atomic_load_uint64_relaxed(&txn_global->checkpoint_timestamp);
+        checkpoint_pinned =
+          __wt_atomic_load_uint64_v_relaxed(&txn_global->checkpoint_txn_shared.pinned_id);
+    }
+
     durable_timestamp = __wt_atomic_load_uint64_relaxed(&txn_global->durable_timestamp);
     oldest_timestamp = __wt_atomic_load_uint64_relaxed(&txn_global->oldest_timestamp);
-    pinned_timestamp = txn_global->pinned_timestamp;
+    pinned_timestamp = __wt_atomic_load_uint64_relaxed(&txn_global->pinned_timestamp);
+
     if (checkpoint_timestamp != WT_TS_NONE && checkpoint_timestamp < pinned_timestamp)
         pinned_timestamp = checkpoint_timestamp;
-    WT_STATP_CONN_SET(session, stats, txn_pinned_timestamp, durable_timestamp - pinned_timestamp);
+
+    /* Represents the lag of the pinned timestamp with respect to the oldest timestamp.*/
     WT_STATP_CONN_SET(
-      session, stats, txn_pinned_timestamp_checkpoint, durable_timestamp - checkpoint_timestamp);
+      session, stats, txn_pinned_timestamp_lag, oldest_timestamp - pinned_timestamp);
+
+    /* Represents the lag of the checkpoint timestamp with respect to the oldest timestamp.*/
+    if (checkpoint_timestamp != WT_TS_NONE && checkpoint_timestamp < oldest_timestamp)
+        checkpoint_pinned_ts_lag = oldest_timestamp - checkpoint_timestamp;
+
+    WT_STATP_CONN_SET(
+      session, stats, txn_pinned_timestamp_checkpoint_lag, checkpoint_pinned_ts_lag);
+
     WT_STATP_CONN_SET(
       session, stats, txn_pinned_timestamp_oldest, durable_timestamp - oldest_timestamp);
 
     __wti_txn_get_pinned_timestamp(session, &oldest_active_read_timestamp, 0);
-    if (oldest_active_read_timestamp == 0) {
-        WT_STATP_CONN_SET(session, stats, txn_timestamp_oldest_active_read, 0);
-        WT_STATP_CONN_SET(session, stats, txn_pinned_timestamp_reader, 0);
-    } else {
-        WT_STATP_CONN_SET(
-          session, stats, txn_timestamp_oldest_active_read, oldest_active_read_timestamp);
-        WT_STATP_CONN_SET(session, stats, txn_pinned_timestamp_reader,
-          durable_timestamp - oldest_active_read_timestamp);
-    }
+    if (oldest_active_read_timestamp != WT_TS_NONE &&
+      oldest_active_read_timestamp < oldest_timestamp)
+        oldest_reader_lag = oldest_timestamp - oldest_active_read_timestamp;
+
+    /* Represents the read timestamp of the oldest active reader.*/
+    WT_STATP_CONN_SET(
+      session, stats, txn_timestamp_oldest_active_read, oldest_active_read_timestamp);
+    /* Represents the lag of the oldest reader  with respect to the oldest timestamp, if any. */
+    WT_STATP_CONN_SET(session, stats, txn_pinned_timestamp_reader_lag, oldest_reader_lag);
+
+    /* Fetch the global timestamp values for debugging purposes.*/
+    WT_STAT_CONN_SET(session, txn_global_checkpoint_timestamp, checkpoint_timestamp);
+    WT_STAT_CONN_SET(session, txn_global_durable_timestamp, durable_timestamp);
+    WT_STAT_CONN_SET(session, txn_global_last_running_timestamp, oldest_active_read_timestamp);
+    WT_STAT_CONN_SET(session, txn_global_newest_timestamp,
+      __wt_atomic_load_uint64_relaxed(&txn_global->newest_seen_timestamp));
+    WT_STAT_CONN_SET(session, txn_global_oldest_timestamp, oldest_timestamp);
+    WT_STAT_CONN_SET(session, txn_global_pinned_timestamp, pinned_timestamp);
+    WT_STAT_CONN_SET(session, txn_global_stable_timestamp,
+      __wt_atomic_load_uint64_relaxed(&txn_global->stable_timestamp));
+
+    if (conn->version_cursor_count == 0)
+        WT_STAT_CONN_SET(session, txn_global_version_cursor_timestamp, WT_TS_NONE);
+    else
+        WT_STAT_CONN_SET(session, txn_global_version_cursor_timestamp,
+          __wt_atomic_load_uint64_relaxed(&txn_global->version_cursor_pinned_timestamp));
 
     WT_STATP_CONN_SET(session, stats, txn_pinned_checkpoint_range,
       checkpoint_pinned == WT_TXN_NONE ?

dist/stat_data.py
@@ -916,13 +916,22 @@ conn_stats = [
     ##########################################
     TxnStat('txn_begin', 'transaction begins'),
     TxnStat('txn_commit', 'transactions committed'),
+    TxnStat('txn_global_checkpoint_timestamp', ' transaction global checkpoint timestamp', 'no_clear,no_scale'),
+    TxnStat('txn_global_durable_timestamp', ' transaction global durable timestamp', 'no_clear,no_scale'),
+    TxnStat('txn_global_last_running_timestamp', 'transaction global last running timestamp', 'no_clear,no_scale'),
+    TxnStat('txn_global_newest_timestamp', 'transaction global newest timestamp', 'no_clear,no_scale'),
+    TxnStat('txn_global_oldest_timestamp', 'transaction global oldest timestamp', 'no_clear,no_scale'),
+    TxnStat('txn_global_pinned_timestamp', 'transaction global pinned timestamp', 'no_clear,no_scale'),
+    TxnStat('txn_global_stable_timestamp', 'transaction global stable timestamp', 'no_clear,no_scale'),
+    TxnStat('txn_global_version_cursor_timestamp', 'transaction global version cursor timestamp', 'no_clear,no_scale'),
     TxnStat('txn_hs_ckpt_duration', 'transaction checkpoint history store file duration (usecs)'),
     TxnStat('txn_pinned_checkpoint_range', 'transaction range of IDs currently pinned by a checkpoint', 'no_clear,no_scale'),
     TxnStat('txn_pinned_range', 'transaction range of IDs currently pinned', 'no_clear,no_scale'),
-    TxnStat('txn_pinned_timestamp', 'transaction range of timestamps currently pinned', 'no_clear,no_scale'),
-    TxnStat('txn_pinned_timestamp_checkpoint', 'transaction range of timestamps pinned by a checkpoint', 'no_clear,no_scale'),
+    TxnStat('txn_pinned_readers', 'transaction number of older readers older than oldest timestamp', 'no_clear,no_scale'),
+    TxnStat('txn_pinned_timestamp_checkpoint_lag', 'transaction range of timestamps pinned by a checkpoint', 'no_clear,no_scale'),
+    TxnStat('txn_pinned_timestamp_lag', 'transaction range of timestamps currently pinned', 'no_clear,no_scale'),
     TxnStat('txn_pinned_timestamp_oldest', 'transaction range of timestamps pinned by the oldest timestamp', 'no_clear,no_scale'),
-    TxnStat('txn_pinned_timestamp_reader', 'transaction range of timestamps pinned by the oldest active read timestamp', 'no_clear,no_scale'),
+    TxnStat('txn_pinned_timestamp_reader_lag', 'transaction range of timestamps pinned by the oldest active read timestamp', 'no_clear,no_scale'),
     TxnStat('txn_prepare', 'prepared transactions'),
     TxnStat('txn_prepare_active', 'prepared transactions currently active'),
     TxnStat('txn_prepare_commit', 'prepared transactions committed'),

src/include/stat.h
@@ -1338,6 +1338,8 @@ struct __wt_connection_stats {
     int64_t child_modify_blocked_page;
     int64_t page_split_restart;
     int64_t page_read_skip_deleted;
+    int64_t txn_global_checkpoint_timestamp;
+    int64_t txn_global_durable_timestamp;
     int64_t txn_prepared_updates;
     int64_t txn_prepared_updates_committed;
     int64_t txn_prepared_updates_key_repeated;
@@ -1387,11 +1389,18 @@ struct __wt_connection_stats {
     int64_t txn_set_ts_stable_upd;
     int64_t txn_begin;
     int64_t txn_hs_ckpt_duration;
+    int64_t txn_global_last_running_timestamp;
+    int64_t txn_global_newest_timestamp;
+    int64_t txn_global_oldest_timestamp;
+    int64_t txn_global_pinned_timestamp;
+    int64_t txn_global_stable_timestamp;
+    int64_t txn_global_version_cursor_timestamp;
+    int64_t txn_pinned_readers;
     int64_t txn_pinned_range;
     int64_t txn_pinned_checkpoint_range;
-    int64_t txn_pinned_timestamp;
-    int64_t txn_pinned_timestamp_checkpoint;
-    int64_t txn_pinned_timestamp_reader;
+    int64_t txn_pinned_timestamp_lag;
+    int64_t txn_pinned_timestamp_checkpoint_lag;
+    int64_t txn_pinned_timestamp_reader_lag;
     int64_t txn_pinned_timestamp_oldest;
     int64_t txn_timestamp_oldest_active_read;
     int64_t txn_rollback_to_stable_running;

src/include/wiredtiger.h.in
@@ -8591,167 +8591,188 @@ extern int wiredtiger_extension_terminate(WT_CONNECTION *connection);
 #define	WT_STAT_CONN_PAGE_SPLIT_RESTART			1914
 /*! thread-yield: pages skipped during read due to deleted state */
 #define	WT_STAT_CONN_PAGE_READ_SKIP_DELETED		1915
+/*! transaction:  transaction global checkpoint timestamp */
+#define	WT_STAT_CONN_TXN_GLOBAL_CHECKPOINT_TIMESTAMP	1916
+/*! transaction:  transaction global durable timestamp */
+#define	WT_STAT_CONN_TXN_GLOBAL_DURABLE_TIMESTAMP	1917
 /*! transaction: Number of prepared updates */
-#define	WT_STAT_CONN_TXN_PREPARED_UPDATES		1916
+#define	WT_STAT_CONN_TXN_PREPARED_UPDATES		1918
 /*! transaction: Number of prepared updates committed */
-#define	WT_STAT_CONN_TXN_PREPARED_UPDATES_COMMITTED	1917
+#define	WT_STAT_CONN_TXN_PREPARED_UPDATES_COMMITTED	1919
 /*! transaction: Number of prepared updates repeated on the same key */
-#define	WT_STAT_CONN_TXN_PREPARED_UPDATES_KEY_REPEATED	1918
+#define	WT_STAT_CONN_TXN_PREPARED_UPDATES_KEY_REPEATED	1920
 /*! transaction: Number of prepared updates rolled back */
-#define	WT_STAT_CONN_TXN_PREPARED_UPDATES_ROLLEDBACK	1919
+#define	WT_STAT_CONN_TXN_PREPARED_UPDATES_ROLLEDBACK	1921
 /*!
  * transaction: a reader raced with a prepared transaction commit and
  * skipped an update or updates
  */
-#define	WT_STAT_CONN_TXN_READ_RACE_PREPARE_COMMIT	1920
+#define	WT_STAT_CONN_TXN_READ_RACE_PREPARE_COMMIT	1922
 /*! transaction: number of times overflow removed value is read */
-#define	WT_STAT_CONN_TXN_READ_OVERFLOW_REMOVE		1921
+#define	WT_STAT_CONN_TXN_READ_OVERFLOW_REMOVE		1923
 /*! transaction: oldest pinned transaction ID rolled back for eviction */
-#define	WT_STAT_CONN_TXN_ROLLBACK_OLDEST_PINNED		1922
+#define	WT_STAT_CONN_TXN_ROLLBACK_OLDEST_PINNED		1924
 /*! transaction: oldest transaction ID rolled back for eviction */
-#define	WT_STAT_CONN_TXN_ROLLBACK_OLDEST_ID		1923
+#define	WT_STAT_CONN_TXN_ROLLBACK_OLDEST_ID		1925
 /*! transaction: prepared transactions */
-#define	WT_STAT_CONN_TXN_PREPARE			1924
+#define	WT_STAT_CONN_TXN_PREPARE			1926
 /*! transaction: prepared transactions committed */
-#define	WT_STAT_CONN_TXN_PREPARE_COMMIT			1925
+#define	WT_STAT_CONN_TXN_PREPARE_COMMIT			1927
 /*! transaction: prepared transactions currently active */
-#define	WT_STAT_CONN_TXN_PREPARE_ACTIVE			1926
+#define	WT_STAT_CONN_TXN_PREPARE_ACTIVE			1928
 /*! transaction: prepared transactions rolled back */
-#define	WT_STAT_CONN_TXN_PREPARE_ROLLBACK		1927
+#define	WT_STAT_CONN_TXN_PREPARE_ROLLBACK		1929
 /*! transaction: query timestamp calls */
-#define	WT_STAT_CONN_TXN_QUERY_TS			1928
+#define	WT_STAT_CONN_TXN_QUERY_TS			1930
 /*! transaction: race to read prepared update retry */
-#define	WT_STAT_CONN_TXN_READ_RACE_PREPARE_UPDATE	1929
+#define	WT_STAT_CONN_TXN_READ_RACE_PREPARE_UPDATE	1931
 /*! transaction: rollback to stable applied btrees */
-#define	WT_STAT_CONN_TXN_RTS_BTREES_APPLIED		1930
+#define	WT_STAT_CONN_TXN_RTS_BTREES_APPLIED		1932
 /*! transaction: rollback to stable calls */
-#define	WT_STAT_CONN_TXN_RTS				1931
+#define	WT_STAT_CONN_TXN_RTS				1933
 /*!
  * transaction: rollback to stable history store keys that would have
  * been swept in non-dryrun mode
  */
-#define	WT_STAT_CONN_TXN_RTS_SWEEP_HS_KEYS_DRYRUN	1932
+#define	WT_STAT_CONN_TXN_RTS_SWEEP_HS_KEYS_DRYRUN	1934
 /*!
  * transaction: rollback to stable history store records with stop
  * timestamps older than newer records
  */
-#define	WT_STAT_CONN_TXN_RTS_HS_STOP_OLDER_THAN_NEWER_START	1933
+#define	WT_STAT_CONN_TXN_RTS_HS_STOP_OLDER_THAN_NEWER_START	1935
 /*! transaction: rollback to stable inconsistent checkpoint */
-#define	WT_STAT_CONN_TXN_RTS_INCONSISTENT_CKPT		1934
+#define	WT_STAT_CONN_TXN_RTS_INCONSISTENT_CKPT		1936
 /*! transaction: rollback to stable keys removed */
-#define	WT_STAT_CONN_TXN_RTS_KEYS_REMOVED		1935
+#define	WT_STAT_CONN_TXN_RTS_KEYS_REMOVED		1937
 /*! transaction: rollback to stable keys restored */
-#define	WT_STAT_CONN_TXN_RTS_KEYS_RESTORED		1936
+#define	WT_STAT_CONN_TXN_RTS_KEYS_RESTORED		1938
 /*!
  * transaction: rollback to stable keys that would have been removed in
  * non-dryrun mode
  */
-#define	WT_STAT_CONN_TXN_RTS_KEYS_REMOVED_DRYRUN	1937
+#define	WT_STAT_CONN_TXN_RTS_KEYS_REMOVED_DRYRUN	1939
 /*!
  * transaction: rollback to stable keys that would have been restored in
  * non-dryrun mode
  */
-#define	WT_STAT_CONN_TXN_RTS_KEYS_RESTORED_DRYRUN	1938
+#define	WT_STAT_CONN_TXN_RTS_KEYS_RESTORED_DRYRUN	1940
 /*! transaction: rollback to stable pages visited */
-#define	WT_STAT_CONN_TXN_RTS_PAGES_VISITED		1939
+#define	WT_STAT_CONN_TXN_RTS_PAGES_VISITED		1941
 /*! transaction: rollback to stable restored tombstones from history store */
-#define	WT_STAT_CONN_TXN_RTS_HS_RESTORE_TOMBSTONES	1940
+#define	WT_STAT_CONN_TXN_RTS_HS_RESTORE_TOMBSTONES	1942
 /*! transaction: rollback to stable restored updates from history store */
-#define	WT_STAT_CONN_TXN_RTS_HS_RESTORE_UPDATES		1941
+#define	WT_STAT_CONN_TXN_RTS_HS_RESTORE_UPDATES		1943
 /*! transaction: rollback to stable skipped btrees */
-#define	WT_STAT_CONN_TXN_RTS_BTREES_SKIPPED		1942
+#define	WT_STAT_CONN_TXN_RTS_BTREES_SKIPPED		1944
 /*! transaction: rollback to stable skipping delete rle */
-#define	WT_STAT_CONN_TXN_RTS_DELETE_RLE_SKIPPED		1943
+#define	WT_STAT_CONN_TXN_RTS_DELETE_RLE_SKIPPED		1945
 /*! transaction: rollback to stable skipping stable rle */
-#define	WT_STAT_CONN_TXN_RTS_STABLE_RLE_SKIPPED		1944
+#define	WT_STAT_CONN_TXN_RTS_STABLE_RLE_SKIPPED		1946
 /*! transaction: rollback to stable sweeping history store keys */
-#define	WT_STAT_CONN_TXN_RTS_SWEEP_HS_KEYS		1945
+#define	WT_STAT_CONN_TXN_RTS_SWEEP_HS_KEYS		1947
 /*!
  * transaction: rollback to stable tombstones from history store that
  * would have been restored in non-dryrun mode
  */
-#define	WT_STAT_CONN_TXN_RTS_HS_RESTORE_TOMBSTONES_DRYRUN	1946
+#define	WT_STAT_CONN_TXN_RTS_HS_RESTORE_TOMBSTONES_DRYRUN	1948
 /*! transaction: rollback to stable tree walk skipping pages */
-#define	WT_STAT_CONN_TXN_RTS_TREE_WALK_SKIP_PAGES	1947
+#define	WT_STAT_CONN_TXN_RTS_TREE_WALK_SKIP_PAGES	1949
 /*! transaction: rollback to stable updates aborted */
-#define	WT_STAT_CONN_TXN_RTS_UPD_ABORTED		1948
+#define	WT_STAT_CONN_TXN_RTS_UPD_ABORTED		1950
 /*!
  * transaction: rollback to stable updates from history store that would
  * have been restored in non-dryrun mode
  */
-#define	WT_STAT_CONN_TXN_RTS_HS_RESTORE_UPDATES_DRYRUN	1949
+#define	WT_STAT_CONN_TXN_RTS_HS_RESTORE_UPDATES_DRYRUN	1951
 /*! transaction: rollback to stable updates removed from history store */
-#define	WT_STAT_CONN_TXN_RTS_HS_REMOVED			1950
+#define	WT_STAT_CONN_TXN_RTS_HS_REMOVED			1952
 /*!
  * transaction: rollback to stable updates that would have been aborted
  * in non-dryrun mode
  */
-#define	WT_STAT_CONN_TXN_RTS_UPD_ABORTED_DRYRUN		1951
+#define	WT_STAT_CONN_TXN_RTS_UPD_ABORTED_DRYRUN		1953
 /*!
  * transaction: rollback to stable updates that would have been removed
  * from history store in non-dryrun mode
  */
-#define	WT_STAT_CONN_TXN_RTS_HS_REMOVED_DRYRUN		1952
+#define	WT_STAT_CONN_TXN_RTS_HS_REMOVED_DRYRUN		1954
 /*! transaction: sessions scanned in each walk of concurrent sessions */
-#define	WT_STAT_CONN_TXN_SESSIONS_WALKED		1953
+#define	WT_STAT_CONN_TXN_SESSIONS_WALKED		1955
 /*! transaction: set timestamp calls */
-#define	WT_STAT_CONN_TXN_SET_TS				1954
+#define	WT_STAT_CONN_TXN_SET_TS				1956
 /*! transaction: set timestamp durable calls */
-#define	WT_STAT_CONN_TXN_SET_TS_DURABLE			1955
+#define	WT_STAT_CONN_TXN_SET_TS_DURABLE			1957
 /*! transaction: set timestamp durable updates */
-#define	WT_STAT_CONN_TXN_SET_TS_DURABLE_UPD		1956
+#define	WT_STAT_CONN_TXN_SET_TS_DURABLE_UPD		1958
 /*! transaction: set timestamp force calls */
-#define	WT_STAT_CONN_TXN_SET_TS_FORCE			1957
+#define	WT_STAT_CONN_TXN_SET_TS_FORCE			1959
 /*!
  * transaction: set timestamp global oldest timestamp set to be more
  * recent than the global stable timestamp
  */
-#define	WT_STAT_CONN_TXN_SET_TS_OUT_OF_ORDER		1958
+#define	WT_STAT_CONN_TXN_SET_TS_OUT_OF_ORDER		1960
 /*! transaction: set timestamp oldest calls */
-#define	WT_STAT_CONN_TXN_SET_TS_OLDEST			1959
+#define	WT_STAT_CONN_TXN_SET_TS_OLDEST			1961
 /*! transaction: set timestamp oldest updates */
-#define	WT_STAT_CONN_TXN_SET_TS_OLDEST_UPD		1960
+#define	WT_STAT_CONN_TXN_SET_TS_OLDEST_UPD		1962
 /*! transaction: set timestamp stable calls */
-#define	WT_STAT_CONN_TXN_SET_TS_STABLE			1961
+#define	WT_STAT_CONN_TXN_SET_TS_STABLE			1963
 /*! transaction: set timestamp stable updates */
-#define	WT_STAT_CONN_TXN_SET_TS_STABLE_UPD		1962
+#define	WT_STAT_CONN_TXN_SET_TS_STABLE_UPD		1964
 /*! transaction: transaction begins */
-#define	WT_STAT_CONN_TXN_BEGIN				1963
+#define	WT_STAT_CONN_TXN_BEGIN				1965
 /*!
  * transaction: transaction checkpoint history store file duration
  * (usecs)
  */
-#define	WT_STAT_CONN_TXN_HS_CKPT_DURATION		1964
+#define	WT_STAT_CONN_TXN_HS_CKPT_DURATION		1966
+/*! transaction: transaction global last running timestamp */
+#define	WT_STAT_CONN_TXN_GLOBAL_LAST_RUNNING_TIMESTAMP	1967
+/*! transaction: transaction global newest timestamp */
+#define	WT_STAT_CONN_TXN_GLOBAL_NEWEST_TIMESTAMP	1968
+/*! transaction: transaction global oldest timestamp */
+#define	WT_STAT_CONN_TXN_GLOBAL_OLDEST_TIMESTAMP	1969
+/*! transaction: transaction global pinned timestamp */
+#define	WT_STAT_CONN_TXN_GLOBAL_PINNED_TIMESTAMP	1970
+/*! transaction: transaction global stable timestamp */
+#define	WT_STAT_CONN_TXN_GLOBAL_STABLE_TIMESTAMP	1971
+/*! transaction: transaction global version cursor timestamp */
+#define	WT_STAT_CONN_TXN_GLOBAL_VERSION_CURSOR_TIMESTAMP	1972
+/*!
+ * transaction: transaction number of older readers older than oldest
+ * timestamp
+ */
+#define	WT_STAT_CONN_TXN_PINNED_READERS			1973
 /*! transaction: transaction range of IDs currently pinned */
-#define	WT_STAT_CONN_TXN_PINNED_RANGE			1965
+#define	WT_STAT_CONN_TXN_PINNED_RANGE			1974
 /*! transaction: transaction range of IDs currently pinned by a checkpoint */
-#define	WT_STAT_CONN_TXN_PINNED_CHECKPOINT_RANGE	1966
+#define	WT_STAT_CONN_TXN_PINNED_CHECKPOINT_RANGE	1975
 /*! transaction: transaction range of timestamps currently pinned */
-#define	WT_STAT_CONN_TXN_PINNED_TIMESTAMP		1967
+#define	WT_STAT_CONN_TXN_PINNED_TIMESTAMP_LAG		1976
 /*! transaction: transaction range of timestamps pinned by a checkpoint */
-#define	WT_STAT_CONN_TXN_PINNED_TIMESTAMP_CHECKPOINT	1968
+#define	WT_STAT_CONN_TXN_PINNED_TIMESTAMP_CHECKPOINT_LAG	1977
 /*!
  * transaction: transaction range of timestamps pinned by the oldest
  * active read timestamp
  */
-#define	WT_STAT_CONN_TXN_PINNED_TIMESTAMP_READER	1969
+#define	WT_STAT_CONN_TXN_PINNED_TIMESTAMP_READER_LAG	1978
 /*!
  * transaction: transaction range of timestamps pinned by the oldest
  * timestamp
  */
-#define	WT_STAT_CONN_TXN_PINNED_TIMESTAMP_OLDEST	1970
+#define	WT_STAT_CONN_TXN_PINNED_TIMESTAMP_OLDEST	1979
 /*! transaction: transaction read timestamp of the oldest active reader */
-#define	WT_STAT_CONN_TXN_TIMESTAMP_OLDEST_ACTIVE_READ	1971
+#define	WT_STAT_CONN_TXN_TIMESTAMP_OLDEST_ACTIVE_READ	1980
 /*! transaction: transaction rollback to stable currently running */
-#define	WT_STAT_CONN_TXN_ROLLBACK_TO_STABLE_RUNNING	1972
+#define	WT_STAT_CONN_TXN_ROLLBACK_TO_STABLE_RUNNING	1981
 /*! transaction: transaction walk of concurrent sessions */
-#define	WT_STAT_CONN_TXN_WALK_SESSIONS			1973
+#define	WT_STAT_CONN_TXN_WALK_SESSIONS			1982
 /*! transaction: transactions committed */
-#define	WT_STAT_CONN_TXN_COMMIT				1974
+#define	WT_STAT_CONN_TXN_COMMIT				1983
 /*! transaction: transactions rolled back */
-#define	WT_STAT_CONN_TXN_ROLLBACK			1975
+#define	WT_STAT_CONN_TXN_ROLLBACK			1984
 /*! transaction: update conflicts */
-#define	WT_STAT_CONN_TXN_UPDATE_CONFLICT		1976
+#define	WT_STAT_CONN_TXN_UPDATE_CONFLICT		1985
 
 /*!
  * @}

commit 30f7d21f2670bcc3afd730e799ef17373559b70a

WT-16752 Fix version_cursor_count data race (#13292)
`test_layered27` started to produce the following race that was
introduced by
https://github.com/wiredtiger/wiredtiger/commit/c3d0e64dc0dfb36469e5bc5fb5d2ebcec9b016a5.

This PR fixes the race.

```
==================
WARNING: ThreadSanitizer: data race (pid=564687)
  Atomic write of size 4 at 0x72940051a178 by thread T20:
    #0 __wt_atomic_add_uint32 /home/ubuntu/work/git/wiredtiger2/src/include/gcc.h:397 (libwiredtiger.so.12.0.0+0x4e26cf)
    #1 __wt_curversion_open /home/ubuntu/work/git/wiredtiger2/src/cursor/cur_version.c:932 (libwiredtiger.so.12.0.0+0x4da2c3)
    #2 __session_open_cursor_int /home/ubuntu/work/git/wiredtiger2/src/session/session_api.c:699 (libwiredtiger.so.12.0.0+0x71beea)
    #3 __wt_open_cursor /home/ubuntu/work/git/wiredtiger2/src/session/session_api.c:810 (libwiredtiger.so.12.0.0+0x71b43b)
    #4 __layered_copy_ingest_table /home/ubuntu/work/git/wiredtiger2/src/conn/conn_layered_ingest.c:139 (libwiredtiger.so.12.0.0+0x37a156)
    #5 __layered_drain_worker_run /home/ubuntu/work/git/wiredtiger2/src/conn/conn_layered_ingest.c:297 (libwiredtiger.so.12.0.0+0x3773fd)
    #6 __thread_run /home/ubuntu/work/git/wiredtiger2/src/support/thread_group.c:32 (libwiredtiger.so.12.0.0+0x840059)
    #7 <null> <null> (libtsan.so.2+0x4eb39) (BuildId: 42b5aaa136fc79e0bafafdb560e4a5bb0c885482)

  Previous read of size 4 at 0x72940051a178 by thread T1:
    #0 __wt_txn_stats_update /home/ubuntu/work/git/wiredtiger2/src/txn/txn.c:2385 (libwiredtiger.so.12.0.0+0x862fe3)
    #1 __wt_conn_stat_init /home/ubuntu/work/git/wiredtiger2/src/conn/conn_stat.c:79 (libwiredtiger.so.12.0.0+0x3aa279)
    #2 __curstat_conn_init /home/ubuntu/work/git/wiredtiger2/src/cursor/cur_stat.c:374 (libwiredtiger.so.12.0.0+0x48ea69)
    #3 __wt_curstat_init /home/ubuntu/work/git/wiredtiger2/src/cursor/cur_stat.c:597 (libwiredtiger.so.12.0.0+0x48e55d)

src/txn/txn.c
@@ -2382,7 +2382,7 @@ __wt_txn_stats_update(WT_SESSION_IMPL *session)
     WT_STAT_CONN_SET(session, txn_global_stable_timestamp,
       __wt_atomic_load_uint64_relaxed(&txn_global->stable_timestamp));
 
-    if (conn->version_cursor_count == 0)
+    if (__wt_atomic_load_uint32_relaxed(&conn->version_cursor_count) == 0)
         WT_STAT_CONN_SET(session, txn_global_version_cursor_timestamp, WT_TS_NONE);
     else
         WT_STAT_CONN_SET(session, txn_global_version_cursor_timestamp,