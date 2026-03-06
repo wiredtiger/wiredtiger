2cd1de343a01cb1562d67469481be62f6d55bd76
Etienne Petrel <etienne.petrel@mongodb.com>
2026-03-06 15:02:45 +1100

    Don't bubble up invalid 0 ret value when validating ingest table

--- src/schema/schema_worker.c
+++ src/schema/schema_worker.c
@@ -123,12 +123,19 @@ __schema_layered_ingest_worker_verify(WT_SESSION_IMPL *session, const char *inge
     WT_ERR(ret);
 
     ret = ingest_cursor->next(ingest_cursor);
-    if (ret != 0 && ret != WT_NOTFOUND)
+    if (ret == WT_NOTFOUND) {
+        ret = 0; /* Expected. */
+    } else if (ret == 0) {
+        /* We found a record in the ingest table, which is unexpected. */
+        ret = WT_ERROR;
         WT_ERR_MSG(session, ret,
           "Verify (layered): %s ingest table verification failed. Ingest on leader must be "
           "empty.",
           ingest_uri);
-    ret = 0;
+    } else
+        WT_ERR_MSG(session, ret,
+          "Verify (layered): %s ingest table verification failed. Unexpected error code.",
+          ingest_uri);
 
 err:
     if (ingest_cursor != NULL)
