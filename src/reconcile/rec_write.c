da59c4a5d58da7b6144c70f99dbc09241e626594
Ravi Prakash Giri <ravi.giri@mongodb.com>
2026-03-10 04:41:34 +0000

    Code refactor

--- src/reconcile/rec_write.c
+++ src/reconcile/rec_write.c
@@ -3343,13 +3343,11 @@ __wti_rec_cell_build_ovfl(WT_SESSION_IMPL *session, WTI_RECONCILE *r, WTI_REC_KV
     /* Track if page has overflow items. */
     r->ovfl_items = true;
 
-#ifdef HAVE_DIAGNOSTIC
     /*
      * Disaggregated trees are not allowed to create overflow keys or values. In diagnostic builds,
      * assert if reconciliation ever tries to do so.
      */
     WT_ASSERT(session, !F_ISSET(btree, WT_BTREE_DISAGGREGATED));
-#endif
 
     /*
      * See if this overflow record has already been written and reuse it if possible, otherwise
