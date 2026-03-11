fcda974fae96c9933652d5fd14d4132d9150b49d
Ravi Prakash Giri <ravi.giri@mongodb.com>
2026-03-11 22:46:40 +0000

    Added comments

--- bench/workgen/runner/cache-stuck-disagg-update-heavy.py
+++ bench/workgen/runner/cache-stuck-disagg-update-heavy.py
@@ -26,6 +26,8 @@
 # ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 # OTHER DEALINGS IN THE SOFTWARE.
 #
+# Update Heavy workload- ~90% updates, ~5% inserts, ~5% reads + checkpoints,
+# readers use read_timestamp close to stable_timestamp.
 
 from runner import *
 from wiredtiger import *
