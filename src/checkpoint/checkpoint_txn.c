9dd0206fcfabe5518aff6cd267a4ba8d82fd33a4
Dylan Liang <dylan.liang@mongodb.com>
2026-03-12 03:03:02 +0000

    expand scope of helper function to include general checkpoint initialization

--- src/checkpoint/checkpoint_txn.c
+++ src/checkpoint/checkpoint_txn.c
@@ -24,11 +24,11 @@ static int __checkpoint_presync(WT_SESSION_IMPL *, const char *[]);
 static int __checkpoint_trees(WT_SESSION_IMPL *, const char *[], struct timespec);
 static int __checkpoint_sync(WT_SESSION_IMPL *, const char *[], WT_DATA_HANDLE *, WT_DATA_HANDLE *);
 static int __checkpoint_tree_helper(WT_SESSION_IMPL *, const char *[]);
+static void __checkpoint_init(WT_SESSION_IMPL *);
 static void __checkpoint_free_drop_list(WT_SESSION_IMPL *);
 static void __checkpoint_prepare_progress(WT_SESSION_IMPL *session, bool final);
 static void __checkpoint_progress(WT_SESSION_IMPL *, bool);
 static void __checkpoint_progress_clear(WT_SESSION_IMPL *);
-static void __checkpoint_reset_stats(WT_SESSION_IMPL *);
 static void __checkpoint_timing_stress(WT_SESSION_IMPL *, uint64_t, struct timespec *);
 
 typedef struct {
@@ -286,11 +286,11 @@ err:
 }
 
 /*
- * __checkpoint_reset_stats --
- *     Reset checkpoint-related statistics that are tracked across the entire checkpoint.
+ * __checkpoint_init --
+ *     Initialize checkpoint tracking and stats.
  */
 static void
-__checkpoint_reset_stats(WT_SESSION_IMPL *session)
+__checkpoint_init(WT_SESSION_IMPL *session)
 {
     WT_CONNECTION_IMPL *conn;
     WT_EVICT *evict;
@@ -324,6 +324,18 @@ __checkpoint_reset_stats(WT_SESSION_IMPL *session)
     /* Page delta. */
     __wt_atomic_store_uint64_relaxed(&conn->page_delta.max_internal_delta_count, 0);
     __wt_atomic_store_uint64_relaxed(&conn->page_delta.max_leaf_delta_count, 0);
+
+    /* Initialize the verbose tracking timer */
+    __wt_epoch(session, &conn->ckpt.ckpt_api.timer_start);
+
+    /* Initialize the checkpoint progress tracking data */
+    __checkpoint_progress_clear(session);
+
+    /*
+     * Get a time (wall time, not a timestamp) for this checkpoint. This will be applied to all the
+     * trees so they match. The time is left in the session.
+     */
+    __checkpoint_establish_time(session);
 }
 
 /*
@@ -1499,20 +1511,8 @@ __checkpoint_db_internal(WT_SESSION_IMPL *session, const char *cfg[])
 
     logging = F_ISSET(&conn->log_mgr, WT_LOG_ENABLED);
 
-    /* Reset the statistics tracked per checkpoint. */
-    __checkpoint_reset_stats(session);
-
-    /* Initialize the verbose tracking timer */
-    __wt_epoch(session, &conn->ckpt.ckpt_api.timer_start);
-
-    /* Initialize the checkpoint progress tracking data */
-    __checkpoint_progress_clear(session);
-
-    /*
-     * Get a time (wall time, not a timestamp) for this checkpoint. This will be applied to all the
-     * trees so they match. The time is left in the session.
-     */
-    __checkpoint_establish_time(session);
+    /* Initialize checkpoint tracking and stats. */
+    __checkpoint_init(session);
 
     /*
      * Update the global oldest ID so we do all possible cleanup.
