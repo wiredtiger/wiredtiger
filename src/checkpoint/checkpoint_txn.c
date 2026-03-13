175e6d230a3350687661f0be694be5ef6d368811
Etienne Petrel <etienne.petrel@mongodb.com>
2026-03-14 10:46:16 +1100

    Update progress message with new stats

--- src/checkpoint/checkpoint_txn.c
+++ src/checkpoint/checkpoint_txn.c
@@ -722,9 +722,13 @@ __checkpoint_progress(WT_SESSION_IMPL *session, bool closing)
 
     if (closing || (time_diff / WT_PROGRESS_MSG_PERIOD) > conn->ckpt.progress.msg_count) {
         __wt_verbose_info(session, WT_VERB_CHECKPOINT_PROGRESS,
-          "Checkpoint %s for %" PRIu64 " seconds and wrote: %" PRIu64 " pages (%" PRIu64 " MB)",
+          "Checkpoint %s for %" PRIu64 " seconds and wrote: %" PRIu64 " pages (%" PRIu64
+          " MB), walked %" PRIu64 " pages, checkpointed %" PRIu64 " files, cache dirty: %"
+          PRIu64 " MB",
           closing ? "ran" : "has been running", time_diff, conn->ckpt.progress.write_pages,
-          conn->ckpt.progress.write_bytes / WT_MEGABYTE);
+          conn->ckpt.progress.write_bytes / WT_MEGABYTE, conn->ckpt.progress.pages_walked,
+          conn->ckpt.progress.files_checkpointed,
+          __wt_cache_dirty_leaf_inuse(conn->cache) / WT_MEGABYTE);
         conn->ckpt.progress.msg_count++;
     }
 }
