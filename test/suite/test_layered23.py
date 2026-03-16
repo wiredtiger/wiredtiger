081edd3fbc4698a83dc10a24bd86610e928f3b1a
Dylan Liang <dylan.liang@mongodb.com>
2026-03-16 04:31:42 +0000

    use WiredTigerCursor from helper.py

--- test/suite/test_layered23.py
+++ test/suite/test_layered23.py
@@ -29,6 +29,7 @@
 import wttest, wiredtiger
 from helper_disagg import disagg_test_class, gen_disagg_storages, Oplog
 from wiredtiger import stat
+from helper import WiredTigerCursor, statistic_uri
 
 # test_layered23.py
 #    Test the basic ability to insert on a follower.
@@ -45,14 +46,12 @@ class test_layered23(wttest.WiredTigerTestCase):
 
     # Make sure the stats agree that the leader has done each checkpoint.
     def check_checkpoint(self, expected):
-        stat_cur = self.session.open_cursor('statistics:', None, None)
-        self.assertEqual(stat_cur[stat.conn.checkpoints_total_succeed][2], expected)
-        stat_cur.close()
+        with WiredTigerCursor(self.session, statistic_uri()) as stat_cur:
+            self.assertEqual(stat_cur[stat.conn.checkpoints_total_succeed][2], expected)
 
     def check_checkpoints_picked_up(self, session_follow, expected):
-        stat_cur = session_follow.open_cursor('statistics:', None, None)
-        self.assertEqual(stat_cur[stat.conn.layered_table_manager_checkpoints_disagg_pick_up_succeed][2], expected)
-        stat_cur.close()
+        with WiredTigerCursor(session_follow, statistic_uri()) as stat_cur:
+            self.assertEqual(stat_cur[stat.conn.layered_table_manager_checkpoints_disagg_pick_up_succeed][2], expected)
 
     # Test simple inserts to a leader/follower
     def test_leader_follower(self):
