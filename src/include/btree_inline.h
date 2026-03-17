500cff7748c11e4e4e3ffb31e5162a63b082b1cf
Mariam Mojid <mariammojid@gmail.com>
2026-03-17 23:57:41 +0000

    disable assert until fix is in placE

--- src/include/btree_inline.h
+++ src/include/btree_inline.h
@@ -316,8 +316,14 @@ __wt_btree_increase_size(WT_SESSION_IMPL *session, uint64_t size)
 static WT_INLINE void
 __wt_btree_decrease_size(WT_SESSION_IMPL *session, uint64_t size)
 {
-    WT_ASSERT(session, __wt_atomic_load_uint64(&S2BT(session)->bytes_total) >= size);
-    (void)__wt_atomic_sub_uint64(&S2BT(session)->bytes_total, size);
+    /*
+     * FIXME WT-16660: re-enable this assert once the disagg delta block size accounting bug is
+     * fixed.
+     */
+    if (__wt_atomic_load_uint64(&S2BT(session)->bytes_total) < size)
+        __wt_atomic_store_uint64(&S2BT(session)->bytes_total, 0);
+    else
+        (void)__wt_atomic_sub_uint64(&S2BT(session)->bytes_total, size);
 }
 
 /*
