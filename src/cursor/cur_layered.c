809a31df554c376078f9522182491b9bdaa8e6b0
Chenhao Qu <chenhao.qu@mongodb.com>
2026-03-17 02:16:52 +0000

    Fix test_prepare04

--- src/cursor/cur_layered.c
+++ src/cursor/cur_layered.c
@@ -1362,7 +1362,8 @@ err:
         __clayered_deleted_decode(&cursor->value);
         F_CLR(cursor, WT_CURSTD_KEY_SET | WT_CURSTD_VALUE_SET);
         F_SET(cursor, WT_CURSTD_KEY_INT | WT_CURSTD_VALUE_INT);
-    } else {
+    } else if (ret != WT_PREPARE_CONFLICT) {
+        /* FIXME-WT-16880: Fix layered search_near() incorrectly resetting the cursor. */
         F_CLR(cursor, WT_CURSTD_KEY_SET | WT_CURSTD_VALUE_SET);
         clayered->current_cursor = NULL;
     }
