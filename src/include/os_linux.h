a0301a5fd5ee657bc26305236733b708835eb4ed
Peter Macko <peter.macko@mongodb.com>
2026-03-17 13:06:00 -0400

    WT-16907 Productize the implementation of semaphores (#13338)
    Implement semaphores on macOS and add the corresponding unit tests.

---
+++ src/include/os_linux.h
@@ -0,0 +1,14 @@
+/*-
+ * Copyright (c) 2014-present MongoDB, Inc.
+ * Copyright (c) 2008-2014 WiredTiger, Inc.
+ *	All rights reserved.
+ *
+ * See the file LICENSE for redistribution information.
+ */
+
+#pragma once
+
+#include <semaphore.h>
+
+/* Semaphore */
+typedef sem_t wt_sem_t;
--- src/os_darwin/os_mtx_sem.c
+++ src/os_darwin/os_mtx_sem.c
@@ -19,8 +19,16 @@ __wt_semaphore_init(WT_SESSION_IMPL *session, WT_SEMAPHORE *sem, uint32_t count,
     WT_CLEAR(*sem);
 
     sem->name = name;
-    if (sem_init(&sem->sem, 0, count) != 0)
-        return (errno);
+    sem->sem = dispatch_semaphore_create(0);
+    if (sem->sem == NULL) {
+        WT_CLEAR(*sem);
+        return (ENOMEM);
+    }
+
+    /* Workaround for semaphore creation not supporting non-zero initial count. */
+    for (uint32_t i = 0; i < count; ++i)
+        /* Ignore the return value, which indicates whether it woke up a thread. */
+        WT_IGNORE_RET(dispatch_semaphore_signal(sem->sem));
 
     return (0);
 }
@@ -34,8 +42,7 @@ __wt_semaphore_destroy(WT_SESSION_IMPL *session, WT_SEMAPHORE *sem)
 {
     WT_UNUSED(session);
 
-    if (sem_destroy(&sem->sem) != 0)
-        return (errno);
+    dispatch_release(sem->sem);
 
     WT_CLEAR(*sem);
     return (0);
@@ -50,8 +57,8 @@ __wt_semaphore_post(WT_SESSION_IMPL *session, WT_SEMAPHORE *sem)
 {
     WT_UNUSED(session);
 
-    if (sem_post(&sem->sem) != 0)
-        return (errno);
+    /* Ignore the return value, which indicates whether it woke up a thread. */
+    WT_IGNORE_RET(dispatch_semaphore_signal(sem->sem));
 
     return (0);
 }
@@ -65,8 +72,9 @@ __wt_semaphore_wait(WT_SESSION_IMPL *session, WT_SEMAPHORE *sem)
 {
     WT_UNUSED(session);
 
-    if (sem_wait(&sem->sem) != 0)
-        return (errno);
+    if (dispatch_semaphore_wait(sem->sem, DISPATCH_TIME_FOREVER) != 0)
+        /* We should not get here due to a timeout because we use DISPATCH_TIME_FOREVER. */
+        return (EINVAL);
 
     return (0);
 }
