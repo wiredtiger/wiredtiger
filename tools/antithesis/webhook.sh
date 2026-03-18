2e40e768113db88748841f3dce7f6ec663ac3430
Will Korteland <will.korteland@mongodb.com>
2026-03-18 10:07:07 -0400

    add webhook

---
+++ tools/antithesis/webhook.sh
@@ -0,0 +1,16 @@
+export WEBHOOK_PAYLOAD=$( \
+          jq -n \
+            '{
+              params: (
+                {
+                  "antithesis.description": "docker-wt-format-test",
+                  "custom.duration": "0.1",
+                  "antithesis.config_image": "wt-test-format-config:debug-have-diagnostic-willk",
+                  "antithesis.images": "wt-test-format:debug-have-diagnostic-willk",
+                  "antithesis.report.recipients": "will.korteland@mongodb.com",
+                }
+              )
+            }')
+
+          echo "Webhook payload: $WEBHOOK_PAYLOAD"
+          echo $WEBHOOK_PAYLOAD |  curl --fail -u "mongodb:${antithesis_api_password}" -X POST https://mongo.antithesis.com/api/v1/launch/disagg_storage -d @-
