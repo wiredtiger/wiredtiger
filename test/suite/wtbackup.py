82fe668654508a7d81f1c3aafa84fa0be6dc39f0
Dylan Liang <dylan.liang@mongodb.com>
2026-03-19 23:09:58 +0000

    simplify while loop using walrus operator

--- test/suite/wtbackup.py
+++ test/suite/wtbackup.py
@@ -214,11 +214,7 @@ class backup_base(wttest.WiredTigerTestCase, suite_subprocess):
         # We cannot use 'for newfile in bkup_c:' usage because backup cursors don't have
         # values and adding in get_values returns ENOTSUP and causes the usage to fail.
         # If that changes then this, and the use of the duplicate below can change.
-        ret = wiredtiger.WT_NOTFOUND
-        while True:
-            ret = bkup_c.next()
-            if ret != 0:
-                break
+        while (ret := bkup_c.next()) == 0:
             newfile = bkup_c.get_key() if home is None else os.path.join(home, bkup_c.get_key())
             sz = os.path.getsize(newfile)
             self.pr(f'Copy from: {newfile} ({sz}) to {backup_dir}')
