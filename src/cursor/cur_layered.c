b95cba38f1c851676a6b33a9100a14776f6fe785
Chenhao Qu <chenhao.qu@mongodb.com>
2026-03-26 04:00:34 +0000

    Remvoe a fixme

--- src/cursor/cur_layered.c
+++ src/cursor/cur_layered.c
@@ -1652,7 +1652,6 @@ done:
 
 err:
     if (ret != 0 && ret != WT_PREPARE_CONFLICT)
-        /* FIXME-WT-16880: Fix layered search_near() incorrectly resetting the cursor. */
         WT_TRET(__clayered_reset_cursors(clayered, false));
 
     return (ret);
