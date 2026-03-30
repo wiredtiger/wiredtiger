3cf889320f4579fdcf7af2bdabf9d64ad2b7c4fd
Hari Babu Kommi <haribabu.kommi@mongodb.com>
2026-03-30 14:21:32 +1100

    Fix the tests

--- test/suite/test_tiered08.py
+++ test/suite/test_tiered08.py
@@ -107,6 +107,8 @@ class test_tiered08(wttest.WiredTigerTestCase, TieredConfigMixin):
         c.close()
 
     def test_tiered08(self):
+        self.ignoreStdoutPattern(
+            r"oldest id .* pinned in session|^Session: ID:|^  |^session ID:|^transaction id:")
 
         cfg = self.conn_config()
         self.pr('Config is: ' + cfg)
