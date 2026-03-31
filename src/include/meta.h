c15fc17ef0a11342b1a1298a2f21e772107a2cdc
Jasmine <82012560+bzm-jas@users.noreply.github.com>
2026-03-31 16:46:03 +1100

    WT-16948 Remove chunk cache statistics (#13444)
    Remove all chunk cache statistics from WiredTiger as part of chunk cache
    deprecation.

--- src/include/meta.h
+++ src/include/meta.h
@@ -38,8 +38,7 @@
 #define WT_HS_ID 1                                           /* ID for HS */
 #define WT_HS_ID_SHARED 2                                    /* ID for shared HS */
 
-#define WT_CC_METAFILE "WiredTigerCC.wt"          /* Chunk cache metadata table */
-#define WT_CC_METAFILE_URI "file:WiredTigerCC.wt" /* Chunk cache metadata table URI */
+#define WT_CC_METAFILE "WiredTigerCC.wt" /* Chunk cache metadata table */
 
 #define WT_DISAGG_METADATA_FILE "WiredTigerShared.wt_stable"     /* Shared metadata table */
 #define WT_DISAGG_METADATA_URI "file:WiredTigerShared.wt_stable" /* Shared metadata table URI */
