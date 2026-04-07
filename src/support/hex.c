8e64145c3d5fd610300d71842aae280c046d9401
Yury Ershov <yuriy.ershov@gmail.com>
2026-04-07 08:56:00 +0000

    WT-17081 Dump corrupt page

--- src/support/hex.c
+++ src/support/hex.c
@@ -40,6 +40,48 @@ __wt_fill_hex(const uint8_t *src, size_t src_max, uint8_t *dest, size_t dest_max
 }
 
 /*
+ * __wt_log_data_dump --
+ *     Log a preamble message followed by a hex dump of a data buffer in 1KB chunks.
+ */
+void
+__wt_log_data_dump(WT_SESSION_IMPL *session, const void *data, size_t size, const char *fmt, ...)
+  WT_GCC_FUNC_ATTRIBUTE((cold)) WT_GCC_FUNC_ATTRIBUTE((format(printf, 4, 5)))
+{
+    WT_DECL_ITEM(preamble);
+    WT_DECL_ITEM(tmp);
+    WT_DECL_RET;
+    size_t chunk, i, nchunks;
+
+    WT_ERR(__wt_scr_alloc(session, 0, &preamble));
+    WT_VA_ARGS_BUF_FORMAT(session, preamble, fmt, false);
+
+    if (size == 0) {
+        __wt_errx(session, "%.*s: empty buffer, no dump available", (int)preamble->size,
+          (char *)preamble->data);
+        goto err;
+    }
+
+    WT_ERR(__wt_scr_alloc(session, 4 * 1024, &tmp));
+
+    nchunks = size / 1024 + (size % 1024 == 0 ? 0 : 1);
+    for (chunk = i = 0;;) {
+        WT_ERR(__wt_buf_catfmt(session, tmp, "%02x ", ((const uint8_t *)data)[i]));
+        if (++i == size || i % 1024 == 0) {
+            __wt_errx(session, "%.*s: (chunk %" WT_SIZET_FMT " of %" WT_SIZET_FMT "): %.*s",
+              (int)preamble->size, (char *)preamble->data, ++chunk, nchunks, (int)tmp->size,
+              (char *)tmp->data);
+            if (i == size)
+                break;
+            WT_ERR(__wt_buf_set(session, tmp, "", 0));
+        }
+    }
+
+err:
+    __wt_scr_free(session, &preamble);
+    __wt_scr_free(session, &tmp);
+}
+
+/*
  * __wt_raw_to_hex --
  *     Convert a chunk of data to a nul-terminated printable hex string.
  */
