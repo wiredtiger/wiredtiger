32497c43ae7d40f74933af05cedd1a6431b869a0
Ivan Kochin <ivan.kochin@mongodb.com>
2026-04-10 06:58:38 +0000

    Bring back reserve to testing

--- src/cursor/cur_layered.c
+++ src/cursor/cur_layered.c
@@ -2096,8 +2096,6 @@ __clayered_reserve(WT_CURSOR *cursor)
 
     CURSOR_UPDATE_API_CALL(cursor, session, ret, reserve, clayered->dhandle);
 
-    WT_ERR_MSG(session, ENOTSUP, "Reserve is not currently supported for layered cursors");
-
     /*
      * Since a search will be performed afterward that clears the iteration flags, no point to
      * retain the flags.
