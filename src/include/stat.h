32ef13fea934d766a0e9e2d21419844cdc79b532
Siddhartha Mahajan <siddhartha.mahajan8899@gmail.com>
2026-04-14 13:19:20 +1000

    WT-17145 Add statistic to track disagg reconfigure calls (#13514)
    <img width="1306" height="163" alt="Screenshot 2026-04-13 at 4 15 26 pm"
    src="https://github.com/user-attachments/assets/046d0ab8-f974-4c6a-a17d-716e43d99a40"
    />

--- src/include/stat.h
+++ src/include/stat.h
@@ -1001,6 +1001,7 @@ struct __wt_connection_stats {
     int64_t dh_sweep_skip_ckpt;
     int64_t dh_session_handles;
     int64_t dh_session_sweeps;
+    int64_t disagg_conn_reconfig;
     int64_t disagg_database_size;
     int64_t disagg_role_leader;
     int64_t disagg_step_down_time;
