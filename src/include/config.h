2e13b8e6ef4d715ea659ad6f717e1ffd346c7ffd
Don Anderson <dda@ddanderson.com>
2026-04-28 11:34:49 -0400

    Fix formatting.

--- src/include/config.h
+++ src/include/config.h
@@ -106,9 +106,9 @@ struct __wt_config_parser_impl {
 
 /*
  * To check if string values deviate from the default, we see if the value is within the default
- * string. If so, that indicates that no later configuration overrode it. This is perhaps
- * slightly more conservative that it needs to be; the caller's configuration can explicitly
- * set the default value. But this check is quite fast and works for the cases we care about.
+ * string. If so, that indicates that no later configuration overrode it. This is perhaps slightly
+ * more conservative that it needs to be; the caller's configuration can explicitly set the default
+ * value. But this check is quite fast and works for the cases we care about.
  */
 #define WT_CONFIG_MATCHES_DEFAULT(session, func, cval) \
     WT_CONFIG_STRING_WITHIN_DEFAULT((cval).str, WT_CONFIG_BASE(session, func))
@@ -121,7 +121,8 @@ static WT_INLINE bool
 __wt_config_empty(const char *cfg[])
 {
     /* If the caller's config string is NULL or "", it is empty */
-    return (cfg == NULL || cfg[0] == NULL || cfg[1] == NULL || (cfg[2] == NULL && cfg[1][0] == '\0'));
+    return (
+      cfg == NULL || cfg[0] == NULL || cfg[1] == NULL || (cfg[2] == NULL && cfg[1][0] == '\0'));
 }
 
 #define WT_CONFIG_UNSET (-1)
