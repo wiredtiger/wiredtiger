b1fb33590cc247c85480fcc8ae204ada440c07f1
Dylan Liang <112071868+DylanSHLiang@users.noreply.github.com>
2026-04-30 10:28:35 +1000

    WT-16687 Fix logic error in page id mismatch algorithm for discard verify (#13631)
    use UINT64_MAX instead of 0 for exhausted page ID

--- src/include/extern.h
+++ src/include/extern.h
@@ -2606,6 +2606,9 @@ extern int __ut_disagg_validate_checkpoint_meta_version(WT_SESSION_IMPL *session
   WT_GCC_FUNC_DECL_ATTRIBUTE((warn_unused_result));
 extern int __ut_disagg_validate_crypt(WT_SESSION_IMPL *session, WT_ITEM *key_item,
   WT_CRYPT_HEADER **header) WT_GCC_FUNC_DECL_ATTRIBUTE((warn_unused_result));
+extern int __ut_verify_compare_page_id_lists(WT_SESSION_IMPL *session, uint64_t *btree_ids,
+  size_t num_btree, const uint64_t *pali_ids, size_t num_pali)
+  WT_GCC_FUNC_DECL_ATTRIBUTE((warn_unused_result));
 extern void __ut_block_off_srch(WT_EXT **head, wt_off_t off, WT_EXT ***stack, bool skip_off);
 extern void __ut_block_off_srch_pair(
   WT_EXTLIST *el, wt_off_t off, WT_EXT **beforep, WT_EXT **afterp);
