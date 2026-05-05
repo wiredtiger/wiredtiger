6c508ac5015be7863784f02ef388d8ee621f0872
Wei Hu <wei.hu@mongodb.com>
2026-05-04 17:33:09 -0700

    WT-17379 Guard __clayered_cursor_compare against unpositioned cursor (#13686)

--- src/cursor/cur_layered.c
+++ src/cursor/cur_layered.c
@@ -1158,8 +1158,12 @@ __clayered_iterate_constituents(WT_CURSOR_LAYERED *clayered, uint32_t iter_flag)
     /*
      * If the alternate cursor's key is equal to the current one, we should move it as well. In that
      * case, the alternate must be the stable cursor.
+     *
+     * Skip the key comparison if the current cursor is unpositioned: the ingest cursor may have
+     * been exhausted and silently ignored by WT_ERR_NOTFOUND_OK.
      */
-    if (F_ISSET(c_alternate, WT_CURSTD_KEY_INT) && c_current == c_ingest) {
+    if (F_ISSET(c_alternate, WT_CURSTD_KEY_INT) && F_ISSET(c_current, WT_CURSTD_KEY_INT) &&
+      c_current == c_ingest) {
         WT_ERR(__clayered_cursor_compare(clayered, c_alternate, c_current, &cmp));
         if (cmp == 0)
             WT_ERR_NOTFOUND_OK(
