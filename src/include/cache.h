db30f4ed1eaf418fb81ce08a798ccf160d7bcaad
Etienne Petrel <etienne.petrel@mongodb.com>
2026-05-06 13:53:43 +1000

    WT-17408 Move WT_IS_HS macro from cache.h to meta.h

--- src/include/cache.h
+++ src/include/cache.h
@@ -159,11 +159,5 @@ struct __wt_cache_pool {
     uint8_t flags;
 };
 
-/*
- * Optimize comparisons against the history store URI, flag handles that reference the history store
- * file.
- */
-#define WT_IS_HS(dh) F_ISSET(dh, WT_DHANDLE_HS)
-
 /* Optimize comparisons against the shared metadata store for disaggregated storage. */
 #define WT_IS_DISAGG_META(dh) F_ISSET(dh, WT_DHANDLE_DISAGG_META)
