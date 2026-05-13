fa3bc58a5844afbf23fad239a445e1ee9ad948d7
Etienne Petrel <etienne.petrel@mongodb.com>
2026-05-14 08:23:32 +1000

    Regenerate format_config_def.c via config.sh

--- test/format/format_config_def.c
+++ test/format/format_config_def.c
@@ -425,10 +425,8 @@ CONFIG configuration_list[] = {{"assert.read_timestamp", "assert read_timestamp"
     "calls to checkpoint that are flush_tier, if tiered storage enabled (percentage)", 0x0, 0, 50,
     100, V_GLOBAL_TIERED_STORAGE_FLUSH_FREQUENCY},
 
-  {"tiered_storage.storage_source",
-    "storage source used (dir_store | gcp_store | none | off)", C_IGNORE | C_STRING, 0, 0, 0,
-    V_GLOBAL_TIERED_STORAGE_STORAGE_SOURCE},
->>>>>>> 9e09200bd1 (WT-17400 Remove azure_store cloud storage source extension (#13739))
+  {"tiered_storage.storage_source", "storage source used (dir_store | gcp_store | none | off)",
+    C_IGNORE | C_STRING, 0, 0, 0, V_GLOBAL_TIERED_STORAGE_STORAGE_SOURCE},
 
   {"transaction.implicit", "implicit, without timestamps, transactions (percentage)", 0, 0, 100,
     100, V_GLOBAL_TRANSACTION_IMPLICIT},
