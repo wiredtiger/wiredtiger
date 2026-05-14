7ab3ef0adf112bd325ee1a5fe1ceb615de228cb4
Etienne Petrel <etienne.petrel@mongodb.com>
2026-05-14 16:38:05 +1000

    s_all

--- src/conn/conn_handle.c
+++ src/conn/conn_handle.c
@@ -20,11 +20,11 @@ __wti_connection_init(WT_CONNECTION_IMPL *conn)
 
     session = conn->default_session;
 
-    TAILQ_INIT(&conn->dhqh);          /* Data handle list */
-    TAILQ_INIT(&conn->dlhqh);         /* Library list */
-    TAILQ_INIT(&conn->dsrcqh);        /* Data source list */
-    TAILQ_INIT(&conn->fhqh);          /* File list */
-    TAILQ_INIT(&conn->tieredqh);      /* Tiered work unit list */
+    TAILQ_INIT(&conn->dhqh);     /* Data handle list */
+    TAILQ_INIT(&conn->dlhqh);    /* Library list */
+    TAILQ_INIT(&conn->dsrcqh);   /* Data source list */
+    TAILQ_INIT(&conn->fhqh);     /* File list */
+    TAILQ_INIT(&conn->tieredqh); /* Tiered work unit list */
     WT_RET(__wti_conn_prefetch_init(session));
 
     /* Extension interface lists. */
