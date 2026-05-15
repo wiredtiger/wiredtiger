677bb9d97b94b0e76f6724eabc08574158e14c0a
Krishen Chovhan <krishen.chovhan@gmail.com>
2026-05-15 01:51:14 +0000

    Add passthrough to allocator mocks

--- test/catch2-fff/fakes/fake_os_alloc.cpp
+++ test/catch2-fff/fakes/fake_os_alloc.cpp
@@ -1,12 +1,25 @@
 #include "fake_os_alloc.h"
 
+#include <cerrno>
+#include <cstdlib>
+
 extern "C" {
 DEFINE_FAKE_VALUE_FUNC(int, __wt_calloc, WT_SESSION_IMPL *, size_t, size_t, void *);
+}
+
+static int
+calloc_passthrough(WT_SESSION_IMPL *, size_t count, size_t size, void *retp)
+{
+    auto *p = std::calloc(count, size);
+    *reinterpret_cast<void **>(retp) = p;
+    return (p != nullptr || count == 0 || size == 0) ? 0 : ENOMEM;
 }
 
 void
 reset_os_alloc_fakes()
 {
     RESET_FAKE(__wt_calloc);
+    __wt_calloc_fake.custom_fake = calloc_passthrough;
+
     FFF_RESET_HISTORY();
 }
--- test/catch2-fff/fakes/fake_scratch.cpp
+++ test/catch2-fff/fakes/fake_scratch.cpp
@@ -1,8 +1,40 @@
 #include "fake_scratch.h"
 
+#include <cerrno>
+#include <cstdlib>
+#include <cstring>
+
 extern "C" {
 DEFINE_FAKE_VALUE_FUNC(int, __wt_buf_set, WT_SESSION_IMPL *, WT_ITEM *, const void *, size_t);
 DEFINE_FAKE_VOID_FUNC(__wt_buf_free, WT_SESSION_IMPL *, WT_ITEM *);
+}
+
+static int
+buf_set_passthrough(WT_SESSION_IMPL *, WT_ITEM *buf, const void *data, size_t size)
+{
+    if (buf->memsize < size) {
+        void *p = std::realloc(buf->mem, size);
+
+        if (p == nullptr && size > 0)
+            return ENOMEM;
+
+        buf->mem = p;
+        buf->memsize = size;
+    }
+
+    if (size > 0)
+        std::memcpy(buf->mem, data, size);
+
+    buf->data = buf->mem;
+    buf->size = size;
+    return 0;
+}
+
+static void
+buf_free_passthrough(WT_SESSION_IMPL *, WT_ITEM *buf)
+{
+    std::free(buf->mem);
+    std::memset(buf, 0, sizeof(*buf));
 }
 
 void
@@ -10,5 +42,8 @@ reset_scratch_fakes()
 {
     RESET_FAKE(__wt_buf_set);
     RESET_FAKE(__wt_buf_free);
+    __wt_buf_set_fake.custom_fake = buf_set_passthrough;
+    __wt_buf_free_fake.custom_fake = buf_free_passthrough;
+
     FFF_RESET_HISTORY();
 }
