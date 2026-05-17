16d2e45c480def947a4f56f2efb9195dc6e79088
Salman Javed <salman.javed@mongodb.com>
2026-05-17 23:48:54 +0000

    Calloc fake: allow memory failures to be simulated

--- test/catch2-fff/fakes/fake_os_alloc.cpp
+++ test/catch2-fff/fakes/fake_os_alloc.cpp
@@ -10,8 +10,12 @@ DEFINE_FAKE_VALUE_FUNC(int, __wt_calloc, WT_SESSION_IMPL *, size_t, size_t, void
 static int
 calloc_passthrough(WT_SESSION_IMPL *, size_t count, size_t size, void *retp)
 {
+    if (__wt_calloc_fake.return_val != 0) 
+        return ENOMEM;
+
     auto *p = std::calloc(count, size);
     *reinterpret_cast<void **>(retp) = p;
+
     return (p != nullptr || count == 0 || size == 0) ? 0 : ENOMEM;
 }
 
