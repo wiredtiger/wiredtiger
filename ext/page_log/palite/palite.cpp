dd3064ea1df32e87afc54783f1d9ef73eddd5e21
Albert <whoisong@gmail.com>
2026-05-19 12:03:03 +1000

    WT-17355 Remove get complete checkpoint ext (#13807)
    Remove complete checkpoint ext, use checkpoint only.
    
    ---------
    
    Co-authored-by: Bobert <albert.song@mongodb.com>

--- ext/page_log/palite/palite.cpp
+++ ext/page_log/palite/palite.cpp
@@ -2424,7 +2424,7 @@ public:
     }
 
     int
-    get_complete_checkpoint_ext(uint64_t *checkpoint_lsn, uint64_t *checkpoint_id,
+    get_complete_checkpoint(uint64_t *checkpoint_lsn, uint64_t *checkpoint_id,
       uint64_t *checkpoint_timestamp, WT_ITEM *checkpoint_metadata)
     {
         if (checkpoint_lsn)
@@ -2539,12 +2539,12 @@ palite_complete_checkpoint(
 }
 
 static int
-palite_get_complete_checkpoint_ext(WT_PAGE_LOG *page_log, WT_SESSION *sess,
-  uint64_t *checkpoint_lsn, uint64_t *checkpoint_id, uint64_t *checkpoint_timestamp,
-  WT_ITEM *checkpoint_metadata)
+palite_get_complete_checkpoint(
+  WT_PAGE_LOG *page_log, WT_SESSION *sess, WT_PAGE_LOG_GET_COMPLETE_CHECKPOINT_ARGS *args)
 {
-    return safe_call<Palite>(sess, page_log, &Palite::get_complete_checkpoint_ext, checkpoint_lsn,
-      checkpoint_id, checkpoint_timestamp, checkpoint_metadata);
+    return safe_call<Palite>(sess, page_log, &Palite::get_complete_checkpoint,
+      &args->checkpoint_lsn, &args->checkpoint_id, &args->checkpoint_timestamp,
+      &args->checkpoint_metadata);
 }
 
 static int
@@ -2586,7 +2586,7 @@ Palite::initialize_interface()
     pl_add_reference = palite_add_reference;
     pl_abandon_checkpoint = palite_abandon_checkpoint;
     pl_complete_checkpoint = palite_complete_checkpoint;
-    pl_get_complete_checkpoint_ext = palite_get_complete_checkpoint_ext;
+    pl_get_complete_checkpoint = palite_get_complete_checkpoint;
     pl_get_last_lsn = palite_get_last_lsn;
     pl_open_handle = palite_open_handle;
     pl_set_last_materialized_lsn = palite_set_last_materialized_lsn;
