eaf4a314ed686171f7ae2c053d50dc6559fefbe1
Hari Babu Kommi <haribabu.kommi@mongodb.com>
2026-05-28 13:16:51 +1000

    WT-17638 Replace obsolete macro and TSAN-suppress store for ref->home

--- src/btree/bt_page.c
+++ src/btree/bt_page.c
@@ -1269,7 +1269,7 @@ __inmem_col_int_init_ref(WT_SESSION_IMPL *session, WT_REF *ref, WT_PAGE *home, u
 
     btree = S2BT(session);
 
-    __wt_tsan_suppress_store_wt_page_ptr_v(&ref->home, home);
+    __wt_atomic_store_ptr(&ref->home, home);
     ref->pindex_hint = hint;
     ref->addr = addr;
     ref->ref_recno = recno;
