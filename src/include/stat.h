251bc61c21a7e3a82915cd2b3294b7866d1f600e
Ivan Kochin <ivan.kochin@mongodb.com>
2026-05-28 00:09:54 +0000

    WT-17670 Avoid reopening ingest cursor on a state change

--- src/include/stat.h
+++ src/include/stat.h
@@ -1006,7 +1006,6 @@ struct __wt_connection_stats {
     int64_t layered_curs_prev_ingest;
     int64_t layered_curs_prev_stable;
     int64_t layered_curs_remove;
-    int64_t layered_curs_reopen_ingest;
     int64_t layered_curs_search_near;
     int64_t layered_curs_search_near_ingest;
     int64_t layered_curs_search_near_stable;
@@ -1737,7 +1736,6 @@ struct __wt_dsrc_stats {
     int64_t layered_curs_prev_ingest;
     int64_t layered_curs_prev_stable;
     int64_t layered_curs_remove;
-    int64_t layered_curs_reopen_ingest;
     int64_t layered_curs_search_near;
     int64_t layered_curs_search_near_ingest;
     int64_t layered_curs_search_near_stable;
