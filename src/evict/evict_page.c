3af4f8efa099c6f6dff30b57c14fea4d75885622
Chenhao Qu <chenhao.qu@mongodb.com>
2026-06-05 04:54:42 +0000

    Fix s_all

--- src/evict/evict_page.c
+++ src/evict/evict_page.c
@@ -700,7 +700,7 @@ __evict_page_dirty_update(WT_SESSION_IMPL *session, WT_REF *ref, uint32_t evict_
              */
             WT_ASSERT(session,
               ref->page->disagg_info == NULL || closing ||
-              __wt_materialization_check(session, ref->page->disagg_info->rec_lsn_max));
+                __wt_materialization_check(session, ref->page->disagg_info->rec_lsn_max));
             __wt_page_modify_clear(session, ref->page);
             __wt_ref_out(session, ref);
             WT_REF_SET_STATE(ref, WT_REF_DISK);
