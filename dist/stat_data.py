2233fba64303e18bfece0e84518878d52e5fd0f9
Etienne Petrel <etienne.petrel@mongodb.com>
2026-06-17 17:17:30 +1000

    WT-17854 Define block_cache_put_time_max statistic
    Records the worst-case latency of a single disaggregated victim cache
    put. Reset per checkpoint so the value reflects a recent collection
    period rather than ratcheting up for the lifetime of the connection.

--- dist/stat_data.py
+++ dist/stat_data.py
@@ -261,6 +261,7 @@ conn_stats = [
     BlockCacheStat('block_cache_misses', 'number of misses'),
     BlockCacheStat('block_cache_not_evicted_overhead', 'number of blocks not evicted due to overhead'),
     BlockCacheStat('block_cache_put_time', 'time spent adding pages to the disaggregated victim cache (usecs)'),
+    BlockCacheStat('block_cache_put_time_max', 'maximum time spent adding a single page to the disaggregated victim cache, reset per checkpoint (usecs)', 'no_clear,no_scale'),
     BlockCacheStat('block_cache_puts', 'pages added to the disaggregated victim cache'),
 
     ##########################################
