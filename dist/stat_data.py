85b8c5001b669000ec0accc01403934dc18c5aad
Etienne Petrel <etienne.petrel@mongodb.com>
2026-06-17 13:45:47 +1000

    Make victim cache put statistics self-contained
    Collect time and count for application threads and for all threads, so
    the per-put averages and the application-thread share are both derivable
    from WiredTiger stats alone. Drop the eviction-thread count, now obtained
    by subtracting the application-thread count from the total.

--- dist/stat_data.py
+++ dist/stat_data.py
@@ -235,6 +235,7 @@ conn_stats = [
     ##########################################
     # Block cache statistics
     ##########################################
+    BlockCacheStat('block_cache_app_thread_put_time', 'time application threads spent adding pages to the disaggregated victim cache (usecs)'),
     BlockCacheStat('block_cache_app_thread_puts', 'pages added to the disaggregated victim cache by application threads'),
     BlockCacheStat('block_cache_blocks', 'total blocks'),
     BlockCacheStat('block_cache_blocks_evicted', 'evicted blocks'),
@@ -255,12 +256,12 @@ conn_stats = [
     BlockCacheStat('block_cache_bytes_update', 'cached bytes updated'),
     BlockCacheStat('block_cache_cold_not_cached', 'cold collection pages not added to the disaggregated victim cache during eviction'),
     BlockCacheStat('block_cache_eviction_passes', 'number of eviction passes'),
-    BlockCacheStat('block_cache_eviction_thread_puts', 'pages added to the disaggregated victim cache by eviction threads'),
     BlockCacheStat('block_cache_hits', 'number of hits'),
     BlockCacheStat('block_cache_lookups', 'lookups'),
     BlockCacheStat('block_cache_misses', 'number of misses'),
     BlockCacheStat('block_cache_not_evicted_overhead', 'number of blocks not evicted due to overhead'),
     BlockCacheStat('block_cache_put_time', 'time spent adding pages to the disaggregated victim cache (usecs)'),
+    BlockCacheStat('block_cache_puts', 'pages added to the disaggregated victim cache'),
 
     ##########################################
     # Block manager statistics
