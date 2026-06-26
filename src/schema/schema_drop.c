546f97607e693f09bf24224a4c55d55164bc9753
Alex Pullen <alex.pullen@mongodb.com>
2026-06-26 00:15:37 +0000

    Fix null dereference

--- src/schema/schema_drop.c
+++ src/schema/schema_drop.c
@@ -140,10 +140,11 @@ __drop_issue_trim(WT_SESSION_IMPL *session, const char *uri)
 
     /* Get the layered data handle. */
     ret = __wt_session_get_dhandle(session, uri, NULL, NULL, WT_DHANDLE_EXCLUSIVE);
-    btree = S2BT(session);
     if (ret == EBUSY)
         WT_RET_SUB(session, ret, WT_CONFLICT_DHANDLE, WT_CONFLICT_DHANDLE_MSG);
     WT_RET(ret);
+
+    btree = S2BT(session);
 
     if (btree->page_log == NULL)
         WT_ERR(ENOTSUP);
