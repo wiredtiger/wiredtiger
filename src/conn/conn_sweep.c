3b875b09554f35c7a461bb57e6b20f5dc660181b
Don Anderson <donald.anderson@mongodb.com>
2026-06-29 19:38:53 +0000

    Fix comment.

--- src/conn/conn_sweep.c
+++ src/conn/conn_sweep.c
@@ -155,7 +155,7 @@ __sweep_close_dhandle_locked(WT_SESSION_IMPL *session)
         WT_ASSERT(session, WT_URI_IS_INGEST(dhandle->name));
 
         /*
-         * The maximum write timestamp is a potentially conservative maximum of commit timestamps
+         * The maximum write timestamp is a potentially conservative maximum of durable timestamps
          * made in this tree. Conservative or not, it guarantees that there are no writes newer than
          * that timestamp. Only if the checkpoint covers that timestamp, can we proceed with the
          * close.
