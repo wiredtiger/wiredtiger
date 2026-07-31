9f78859c3a211c34bed7459be81e9da78d90106c
Alex Pullen <93811034+LX6T@users.noreply.github.com>
2026-07-31 14:42:14 +1000

    Revert "WT-18214 Fix test/format multi node failing with low op count" (#14338)
    Reverts wiredtiger/wiredtiger#14324
    
    Seeing predictable replay failures after merging, likely to be caused by
    this commit.

--- test/format/ops.c
+++ test/format/ops.c
@@ -343,10 +343,7 @@ operations(u_int ops_seconds, u_int run_current, u_int run_total)
              */
             thread_ops = -1;
             ops_seconds = 0;
-            if (g.replay_ops_base == WT_TS_NONE)
-                g.replay_ops_base = g.timestamp;
-            g.stop_timestamp =
-              g.replay_ops_base + (GV(RUNS_OPS) * (uint64_t)run_current) / run_total;
+            g.stop_timestamp = (GV(RUNS_OPS) * (uint64_t)run_current) / run_total;
         } else
             thread_ops = GV(RUNS_OPS) / GV(RUNS_THREADS);
     }
