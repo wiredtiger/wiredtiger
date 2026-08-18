1cdf24a2f17852ca01303414bbf555f48420303b
Etienne Petrel <etienne.petrel@mongodb.com>
2026-08-18 22:09:30 +1000

    WT-18398 Add a test for hook forwarding into run_subprocess_function
    The subprocess started by run_subprocess_function does not inherit the
    parent's --hook options, so a test running under a hook exercises none of
    it in the subprocess. The new test fails under --hook disagg and
    --hook parallel_checkpoint.

---
+++ test/suite/test_hook_forward01.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+#
+# Public Domain 2014-present MongoDB, Inc.
+# Public Domain 2008-2014 WiredTiger, Inc.
+#
+# This is free and unencumbered software released into the public domain.
+#
+# Anyone is free to copy, modify, publish, use, compile, sell, or
+# distribute this software, either in source code form or as a compiled
+# binary, for any purpose, commercial or non-commercial, and by any
+# means.
+#
+# In jurisdictions that recognize copyright laws, the author or authors
+# of this software dedicate any and all copyright interest in the
+# software to the public domain. We make this dedication for the benefit
+# of the public at large and to the detriment of our heirs and
+# successors. We intend this dedication to be an overt act of
+# relinquishment in perpetuity of all present and future rights to this
+# software under copyright law.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+
+import os
+import wttest
+from suite_subprocess import suite_subprocess
+
+# test_hook_forward01.py
+#    A subprocess started by run_subprocess_function runs under the same hooks as its parent.
+# Without that, a test running under a hook silently exercises none of it in the subprocess, and a
+# hook that alters the database layout leaves the parent unable to reopen what the subprocess wrote.
+class test_hook_forward01(wttest.WiredTigerTestCase, suite_subprocess):
+    test_name = __qualname__
+    hooks_file = 'subprocess_hooks.txt'
+
+    # Subprocess body: report the hooks this process was started with.
+    def subprocess_report_hooks(self):
+        with open(self.hooks_file, 'w') as f:
+            f.write('\n'.join(self.hook_names))
+
+    def test_hook_forward(self):
+        [returncode, home] = self.run_subprocess_function('SUBPROCESS_report_hooks',
+            f'{self.test_name}.{self.test_name}.subprocess_report_hooks')
+        self.assertEqual(returncode, 0)
+
+        with open(os.path.join(home, self.hooks_file), 'r') as f:
+            subprocess_names = f.read().split()
+        self.assertEqual(sorted(subprocess_names), sorted(self.hook_names))
