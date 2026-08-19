99daad8e1fd97b661a84029e3a06d044a62e3d89
Jie Chen <jie.chen@mongodb.com>
2026-08-19 14:22:40 +1000

    WT-18402 Rename the unique table name option to -q

--- test/csuite/schema_disagg_abort/parent.c
+++ test/csuite/schema_disagg_abort/parent.c
@@ -110,7 +110,7 @@ spawn_node(const TEST_CONFIG *cfg, const char *self_path, uint32_t node_id, bool
     argv[n++] = "-u";
     argv[n++] = pool_arg;
     if (cfg->unique_tables)
-        argv[n++] = "-U";
+        argv[n++] = "-q";
     /* The node bounds how far its generator runs ahead so a hand-over drains inside a period. */
     if (cfg->switch_interval != 0) {
         argv[n++] = "-s";
