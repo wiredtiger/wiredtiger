6605d9dd4a90d5ce4b667cc685491ae8f361a841
Dylan Liang <112071868+DylanSHLiang@users.noreply.github.com>
2026-08-27 05:47:03 +0000

    WT-18147 Fix wt tool's pretty-print support for zero-length "u" data (#14485)
    `wt dump -p` printed a record's **previous** value instead of a blank
    value for a zero-length `u`/`U`-format field, because
    `__wt_buf_set_printable_format()` reused an output buffer and returned
    early without writing (or NUL-terminating) anything for the zero-length
    case.
    
    `src/support/scratch.c`: the zero-length `'U'`/`'u'` case now writes the
    field separator (still nothing else) so the buffer is refreshed instead
    of left stale. Output is blank, matching `-x`, `-j`.
    
    `test/suite/test_dump06.py` asserts the pretty dump shows blank (not
    stale/previous data).

---
+++ test/suite/test_dump06.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+#
+# Public Domain 2014-present MongoDB, Inc.
+# Public Domain 2008-2014 WiredTiger, Inc.
+#
+# This is free and unencumbered software released into the public domain.
+#
+# Anyone is free to copy, modify, publish, use, compile, sell, or
+# distribute this software, either in source code form or as a compiled
+# binary, for any purpose, commercial or non-commercial, and by any
+# means.
+#
+# In jurisdictions that recognize copyright laws, the author or authors
+# of this software dedicate any and all copyright interest in the
+# software to the public domain. We make this dedication for the benefit
+# of the public at large and to the detriment of our heirs and
+# successors. We intend this dedication to be an overt act of
+# relinquishment in perpetuity of all present and future rights to this
+# software under copyright law.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+
+import wttest
+from suite_subprocess import suite_subprocess
+
+# Test that a zero-length 'u' formatted value is dumped correctly with
+# the pretty-print (-p) option, rather than showing the previous
+# record's value.
+class test_dump06(wttest.WiredTigerTestCase, suite_subprocess):
+    uri = 'table:test_dump06'
+    table_format = 'key_format=q,value_format=u'
+
+    pretty_file = 'dump_pretty.out'
+    hex_file = 'dump_hex.out'
+    json_file = 'dump_json.out'
+
+    k_v_pairs = [(1, b''), (2, b'A'), (3, b''), (5, b'B'), (6, b'C')]
+
+    def test_dump_pretty_empty_value(self):
+        self.session.create(self.uri, self.table_format)
+        cursor = self.session.open_cursor(self.uri, None, None)
+        for k, v in self.k_v_pairs:
+            cursor[k] = v
+        cursor.close()
+
+        self.runWt(['dump', '-p', self.uri], outfilename=self.pretty_file)
+        self.runWt(['dump', '-x', self.uri], outfilename=self.hex_file)
+        self.runWt(['dump', '-j', self.uri], outfilename=self.json_file)
+
+        # Pretty-print format
+        pretty_expected = []
+        for k, v in self.k_v_pairs:
+            pretty_expected.append(f'{k}')
+            pretty_expected.append(v.decode())
+        pretty_expected = 'Data\n' + '\n'.join(pretty_expected)
+        self.check_file_contains(self.pretty_file, pretty_expected)
+
+        # Hex format
+        hex_expected = []
+        for k, v in self.k_v_pairs:
+            hex_expected.append(f'{k + 0x80:x}')
+            hex_expected.append(v.hex())
+        hex_expected = 'Data\n' + '\n'.join(hex_expected)
+        self.check_file_contains(self.hex_file, hex_expected)
+
+        # JSON format
+        self.check_file_contains(self.json_file, '"key0" : 1,\n"value0" : ""\n')
+        self.check_file_contains(self.json_file, '"key0" : 3,\n"value0" : ""\n')
+
+if __name__ == '__main__':
+    wttest.run()
