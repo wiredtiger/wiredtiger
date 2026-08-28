4be4768a8da17b7f831a28a570f45e35f1ef6ac5
Etienne Petrel <etienne.petrel@mongodb.com>
2026-08-28 15:18:28 +1000

    WT-18513 Skip tiered config stripping when tiered storage is disabled

--- src/schema/schema_create.c
+++ src/schema/schema_create.c
@@ -359,10 +359,20 @@ __create_file(
             WT_ERR(__wt_import_repair(session, uri, &fileconf));
         }
 
-        /* Strip any configuration settings that should not be persisted. */
-        filecfg[1] = fileconf;
-        filecfg[2] = NULL;
-        WT_ERR(__wt_config_tiered_strip(session, filecfg, &filestripped));
+        /*
+         * Strip any configuration settings that should not be persisted. Only tiered storage
+         * settings are stripped; there is nothing to do unless the connection uses tiered storage.
+         */
+        if (WT_CONN_TIERED_STORAGE_ENABLED(S2C(session))) {
+            filecfg[1] = fileconf;
+            filecfg[2] = NULL;
+            WT_ERR(__wt_config_tiered_strip(session, filecfg, &filestripped));
+        } else {
+            filestripped = fileconf;
+            fileconf = NULL;
+        }
+
+        /* Insert the stripped configuration into the metadata. */
         WT_ERR(__wt_metadata_insert(session, uri, filestripped));
 
         /*
