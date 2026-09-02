c6466df6b91a36f3ed45d565fd7177a7232abe06
Yury Ershov <yuriy.ershov@gmail.com>
2026-09-02 14:54:59 +1000

    WT-18518 Compile-time check for WT_DHANDLE_TYPE_NUM
    Define the dhandle type count as a literal and static_assert it against
    the last enumerator so a count mismatch fails the build.

--- src/include/dhandle.h
+++ src/include/dhandle.h
@@ -108,8 +108,10 @@ enum wt_dhandle_type {
     WT_DHANDLE_TYPE_TIERED,
     WT_DHANDLE_TYPE_TIERED_TREE
 };
-/* Number of values above. */
-#define WT_DHANDLE_TYPE_NUM (1 + WT_DHANDLE_TYPE_TIERED_TREE)
+/* Number of values above, update when adding a dhandle type. */
+#define WT_DHANDLE_TYPE_NUM 5
+static_assert(WT_DHANDLE_TYPE_NUM == WT_DHANDLE_TYPE_TIERED_TREE + 1,
+  "WT_DHANDLE_TYPE_NUM does not match the number of dhandle types");
 
 /*
  * WT_DATA_HANDLE --
